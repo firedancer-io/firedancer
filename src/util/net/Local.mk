1335f936b96327fbc3234a00d05b513dece9f9c8
Kevin J Bowers <kbowers@jumptrading.com>
2022-06-17 17:00:04 -0500

    APIs for parsing IGMP reports
    Change-Id: I5ef8b139a58a7b7831414a2d4a4a765c78fef145

--- src/util/net/Local.mk
+++ src/util/net/Local.mk
@@ -1,4 +1,5 @@
-$(call add-hdrs,fd_eth.h fd_ip4.h)
+$(call add-hdrs,fd_eth.h fd_ip4.h fd_igmp.h)
 $(call add-objs,fd_eth,fd_util)
-$(call make-unit-test,test_eth,test_eth,fd_util)
-$(call make-unit-test,test_ip4,test_ip4,fd_util)
+$(call make-unit-test,test_eth, test_eth, fd_util)
+$(call make-unit-test,test_ip4, test_ip4, fd_util)
+$(call make-unit-test,test_igmp,test_igmp,fd_util)
---
+++ src/util/net/test_igmp.c
@@ -0,0 +1,38 @@
+#include "../fd_util.h"
+#include "fd_igmp.h"
+
+FD_STATIC_ASSERT( FD_IGMP_TYPE_QUERY    ==(uchar)0x11, unit_test );
+FD_STATIC_ASSERT( FD_IGMP_TYPE_V1_REPORT==(uchar)0x12, unit_test );
+FD_STATIC_ASSERT( FD_IGMP_TYPE_V2_REPORT==(uchar)0x16, unit_test );
+FD_STATIC_ASSERT( FD_IGMP_TYPE_V2_LEAVE ==(uchar)0x17, unit_test );
+
+FD_STATIC_ASSERT( sizeof(fd_igmp_t    )== 8UL, unit_test );
+FD_STATIC_ASSERT( sizeof(fd_ip4_igmp_t)==32UL, unit_test );
+
+int
+main( int     argc,
+      char ** argv ) {
+  fd_boot( &argc, &argv );
+
+# define TEST(c) do if( !(c) ) { FD_LOG_WARNING(( "FAIL: " #c )); return 1; } while(0)
+
+  TEST( (ulong)( &(((fd_igmp_t *)NULL)->type ) )==0UL );
+  TEST( (ulong)( &(((fd_igmp_t *)NULL)->resp ) )==1UL );
+  TEST( (ulong)( &(((fd_igmp_t *)NULL)->check) )==2UL );
+  TEST( (ulong)( &(((fd_igmp_t *)NULL)->group) )==4UL );
+  TEST( (ulong)(  (((fd_igmp_t *)NULL)->u    ) )==0UL );
+
+  TEST( (ulong)( ((fd_ip4_igmp_t *)NULL)->ip4  )== 0UL );
+  TEST( (ulong)( ((fd_ip4_igmp_t *)NULL)->opt  )==20UL );
+  TEST( (ulong)( ((fd_ip4_igmp_t *)NULL)->igmp )==24UL );
+
+  /* FIXME: TEST FD_IGMP_CHECK */
+  /* FIXME: TEST FD_IP4_IGMP */
+
+# undef TEST
+
+  FD_LOG_NOTICE(( "pass" ));
+  fd_halt();
+  return 0;
+}
+
