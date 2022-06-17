df4b15aca77cc3f5af1b2762482ae9efebd7e81d
Kevin J Bowers <kbowers@jumptrading.com>
2022-06-17 18:01:21 -0500

    Code generator for high performance single threaded sorts
    Change-Id: Ia3fc8c28551cecdef238d10584f3a5c3ab6ff846

--- src/util/tmpl/Local.mk
+++ src/util/tmpl/Local.mk
@@ -1,3 +1,4 @@
-$(call add-hdrs,fd_smallset.c fd_set.c)
+$(call add-hdrs,fd_smallset.c fd_set.c fd_sort.c)
 $(call make-unit-test,test_smallset,test_smallset,fd_util)
 $(call make-unit-test,test_set,test_set,fd_util)
+$(call make-unit-test,test_sort,test_sort,fd_util)
