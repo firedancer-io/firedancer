2656d9b2385c08e706a72469c514f1958dc536a5
Kevin J Bowers <kbowers@jumptrading.com>
2022-07-07 16:49:38 -0500

    Minor unit-test naming cleanup
    Change-Id: I370e3709e3d31b692c83ab2dfed7b8ae2c0d9a99

--- src/util/Local.mk
+++ src/util/Local.mk
@@ -1,6 +1,6 @@
 $(call make-lib,fd_util)
-$(call add-hdrs,fd_util_base.h)
+$(call add-hdrs,fd_util_base.h fd_util.h)
 $(call add-objs,fd_hash fd_util,fd_util)
-$(call make-unit-test,test_base,test_base,fd_util)
+$(call make-unit-test,test_util_base,test_util_base,fd_util)
 $(call make-unit-test,test_util,test_util,fd_util)
 
