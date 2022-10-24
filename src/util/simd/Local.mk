9a912e31587b8b93495615ea1c6474e00a4edae4
Kevin J Bowers <kbowers@jumptrading.com>
2022-10-24 02:01:30 -0500

    Missed in previous checkin
    Change-Id: Idcfacbf13587ffcd7c333f036e343fa807e0bc9a
    Reviewed-on: https://git.firedancer.io/c/firedancer/+/215
    Reviewed-by: Kevin Bowers <kbowers@jumptrading.com>

--- src/util/simd/Local.mk
+++ src/util/simd/Local.mk
@@ -1,6 +1,6 @@
-$(call add-hdrs,fd_sse.h fd_sse_vc.h fd_sse_vi.h fd_sse_vf.h fd_sse_vd.h)
+$(call add-hdrs,fd_sse.h fd_sse_vc.h fd_sse_vi.h fd_sse_vf.h fd_sse_vd.h fd_sse_vl.h)
 $(call make-unit-test,test_sse,test_sse,fd_util)
 
-$(call add-hdrs,fd_avx.h fd_avx_wc.h fd_avx_wi.h fd_avx_wf.h fd_avx_wd.h)
+$(call add-hdrs,fd_avx.h fd_avx_wc.h fd_avx_wi.h fd_avx_wf.h fd_avx_wd.h fd_avx_wl.h)
 $(call make-unit-test,test_avx,test_avx,fd_util)
 
