643f3020389c86966c5fad47d1b94f6b539fcc9c
Kevin J Bowers <kbowers@jumptrading.com>
2022-10-26 19:04:12 -0500

    Allow even older hardware but tune for somewhat newer hardware
    Change-Id: I5082f0d03a431ed7be0a5522fdd71e538111f760
    Reviewed-on: https://git.firedancer.io/c/firedancer/+/219
    Reviewed-by: Kevin Bowers <kbowers@jumptrading.com>

--- config/rh8_x86_64.mk
+++ config/rh8_x86_64.mk
@@ -7,7 +7,7 @@ include config/with-optimization.mk
 include config/with-threads.mk
 
 CPPFLAGS+=-fomit-frame-pointer -falign-functions=32 -falign-jumps=32 -falign-labels=32 -falign-loops=32 \
-          -march=broadwell -mfpmath=sse -mbranch-cost=5 \
+          -march=haswell -mtune=skylake -mfpmath=sse -mbranch-cost=5 \
 	  -DFD_HAS_INT128=1 -DFD_HAS_DOUBLE=1 -DFD_HAS_ALLOCA=1 -DFD_HAS_X86=1 -DFD_HAS_SSE=1 -DFD_HAS_AVX=1
 LDFLAGS+=-lnuma
 
