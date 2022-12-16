e9f88e6bb1fa3c06e28e3e7ad2cd0e9a030b0bf8
Philip Taffet <phtaffet@jumptrading.com>
2022-12-16 17:35:25 -0600

    pack: add CU estimation table
    Change-Id: Ie1dd9eb7aded887a731656809a3e0b7249bafb0e

--- src/ballet/pack/Local.mk
+++ src/ballet/pack/Local.mk
@@ -1,2 +1,4 @@
 $(call make-unit-test,test_compute_budget_program,test_compute_budget_program,fd_ballet fd_util)
+$(call make-unit-test,test_est_tbl,test_est_tbl,fd_ballet fd_util)
 $(call run-unit-test,test_compute_budget_program,)
+$(call run-unit-test,test_est_tbl,)
