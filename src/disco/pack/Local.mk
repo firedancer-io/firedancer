e796e0f9670422f8f879eee5fef7c97628faa9a5
Philip Taffet <phtaffet@jumptrading.com>
2022-12-20 14:41:49 -0600

    pack: add parser for compute budget program
    Change-Id: I7df77148b26aa356c7c8455c0b949ab4252d13e1

--- src/disco/pack/Local.mk
+++ src/disco/pack/Local.mk
@@ -1,2 +1,3 @@
 $(call make-unit-test,test_est_tbl,test_est_tbl,fd_disco fd_tango fd_ballet fd_util)
+$(call make-unit-test,test_compute_budget_program,test_compute_budget_program,fd_disco fd_tango fd_ballet fd_util)
 
