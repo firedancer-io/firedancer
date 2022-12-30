905705f7d166aa2796fe58e4fc50b83c69898dfc
Kevin J Bowers <kbowers@jumptrading.com>
2022-12-30 13:35:59 -0600

    Unit tester for fd_wksp_ctl
    Change-Id: Iab6ad6ee1cf7284d4bd65b603e426a08f0070705
    Reviewed-on: https://git.firedancer.io/c/firedancer/+/369
    Reviewed-by: Kevin Bowers <kbowers@jumptrading.com>

--- src/util/wksp/Local.mk
+++ src/util/wksp/Local.mk
@@ -2,4 +2,5 @@ $(call add-objs,fd_wksp fd_wksp_pod,fd_util)
 $(call add-hdrs,fd_wksp.h)
 $(call make-bin,fd_wksp_ctl,fd_wksp_ctl,fd_util)
 $(call make-unit-test,test_wksp,test_wksp,fd_util)
+$(call add-test-scripts,test_wksp_ctl)
 
