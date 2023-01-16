e68d38c1ba5b0d11d15bf99d1f52db9cfa6042be
CantelopePeel <6699047+CantelopePeel@users.noreply.github.com>
2023-01-16 14:06:48 -0500

    util: add tests for fd_stat_avg2_*
    Co-authored-by: Richard Patel <ripatel@jumptrading.com>
    
    Change-Id: I42a0036b77d68861f9ffc7b6dc49be572ea1b792
    Reviewed-on: https://git.firedancer.io/c/firedancer/+/418
    Reviewed-by: Richard Patel <ripatel@jumptrading.com>

--- src/util/math/test_stat.c
+++ src/util/math/test_stat.c
@@ -17,7 +17,23 @@ main( int     argc,
 
   fd_rng_t _rng[1]; fd_rng_t * rng = fd_rng_join( fd_rng_new( _rng, 0U, 0UL ) );
 
-  /* FIXME: ADD AVG TESTS */
+# define AVG2_INT_TEST(T) do {                          \
+    FD_TEST( fd_stat_avg2_##T( (T)  0, (T)0 )==(T) 0 );  \
+    FD_TEST( fd_stat_avg2_##T( (T)  1, (T)2 )==(T) 1 );  \
+    FD_TEST( fd_stat_avg2_##T( (T)  1, (T)0 )==(T) 0 );  \
+    FD_TEST( fd_stat_avg2_##T( (T)110, (T)0 )==(T)55 );  \
+  } while(0)
+
+  AVG2_INT_TEST( schar  );
+  AVG2_INT_TEST( uchar  );
+  AVG2_INT_TEST( short  );
+  AVG2_INT_TEST( ushort );
+  AVG2_INT_TEST( int    );
+  AVG2_INT_TEST( uint   );
+  AVG2_INT_TEST( long   );
+  AVG2_INT_TEST( ulong  );
+# undef AVG2_INT_TEST
+
 
 # define FILT_TEST(T,UT)                             \
   for( ulong iter=0UL; iter<1000000UL; iter++ ) {    \
