bfd430e0a855efecf33f892d395c0e67d3f4159c
Kevin J Bowers <kbowers@jumptrading.com>
2023-01-25 16:07:05 -0600

    Minor polishing of fd_asan.h API
    This is mostly an exercise post-Gerrit workflows and do some of the
    tweaks that were pending in Gerrit that didn't make it over in the
    conversion:
    - Eliminate implicit dependencies on fd_util_base.h.
    - Allow FD_HAS_ASAN to be manually specified.
    - Use static inlines instead of macros where possible.
    - Tweak poison / unpoison APIs to be composable
    - Minor related cleanups (commenting / formatting / naming)

--- config/with-asan.mk
+++ config/with-asan.mk
@@ -1,2 +1,9 @@
+# Note: This probably needs to after machine specific targets otherwise
+# the -fomit-frame-pointer that occurs there will be silently override
+# the -fno-omit-frame-pointer here.
+#
+# FIXME: CONSIDER MANUALLY SETTING FD_HAS_ASAN IN BOTH THE CPPFLAGS AND
+# IN THE MAKE ENVIRONMENT?
+
 CPPFLAGS+=-fsanitize=address -fno-omit-frame-pointer
 LDFLAGS+=-fsanitize=address
--- src/util/sanitize/fd_sanitize.h
+++ src/util/sanitize/fd_sanitize.h
@@ -4,8 +4,8 @@
 /* APIs provided by compiler sanitizers.
 
    Sanitizers are error detection tools built from a combination of
-   hardware facilities, hooks injected into compiled code,
-   special memory mappings, and library functions.
+   hardware facilities, hooks injected into compiled code, special
+   memory mappings, and library functions.
 
    For example, the AddressSanitizer can be used to detect out-of-bounds
    memory accesses that otherwise don't crash a process and various
