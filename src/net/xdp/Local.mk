b81b83fe9640bbda5dbf979d05ab2ec692629c19
Richard Patel <ripatel@jumptrading.com>
2023-01-30 17:26:16 +0000

    add libbpf Bazel dep
    - Fixes //src/net/xdp build
    - Adds new `prepare_include_dir` Bazel macro to generate a custom
      includes dir from a list of sources

--- src/net/xdp/Local.mk
+++ src/net/xdp/Local.mk
@@ -1,9 +1,4 @@
 $(call make-ebpf-bin,ebpf_xdp_flow)
 $(call make-lib,fd_xdp)
-$(call make-lib,fd_xdp_fs)
-$(call make-bin,fd_quic_attach,fd_quic_attach fd_xdp_fs,fd_util)
-$(call make-bin,fd_quic_detach,fd_quic_detach fd_xdp_fs,fd_util)
-$(call make-bin,test_recv,test_recv fd_xdp_fs fd_xdp,fd_util)
-$(call make-bin,test_send,test_send fd_xdp_fs fd_xdp,fd_util)
-$(call make-bin,test_echo,test_echo fd_xdp_fs fd_xdp,fd_util)
+$(call add-objs,fd_xdp,fd_xdp)
 
