2224811988a7dbbd491abf0d590cd31ed7d8f365
Richard Patel <ripatel@jumptrading.com>
2023-01-30 15:31:41 +0000

    quic: minor cleanup
    - Remove fd_log wrappers
    - Remove empty files

--- src/net/fd_net.h
+++ src/net/fd_net.h
@@ -1,4 +1,8 @@
+#ifndef HEADER_fd_src_net_fd_net_h
+#define HEADER_fd_src_net_fd_net_h
+
 #include "quic/fd_quic.h"
 #include "aio/fd_aio.h"
 #include "xdp/fd_xdp.h"
 
+#endif /* HEADER_fd_src_net_fd_net_h */
