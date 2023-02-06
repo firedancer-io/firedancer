b097905c22f46196cbf8b4ad2324234848737077
Nick Bridge <nbridge@jumptrading.com>
2023-02-06 15:36:25 -0600

    xdp - fixes and some changes to allow parallism

--- src/net/xdp/fd_xdp_bpf_udp.c
+++ src/net/xdp/fd_xdp_bpf_udp.c
@@ -1,14 +1,23 @@
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
 
+/* limits the number of nic queues supported */
 #if !defined(FD_XDP_XSKS_MAP_SZ)
-#  define FD_XDP_XSKS_MAP_SZ 16
+#  define FD_XDP_XSKS_MAP_SZ 256
 #endif
 
+/* limits the number of udp ports we can listen on */
 #if !defined(FD_XDP_UDP_MAP_SZ)
 #  define FD_XDP_UDP_MAP_SZ 64
 #endif
 
+/* BPF program cannot have too many dependencies
+   so simply define these here */
+typedef unsigned char uchar;
+typedef unsigned int  uint;
+typedef unsigned long ulong;
+
+/* map for forwarding packets to an xdp socket */
 struct bpf_map_def
 __attribute__(( section( "maps" ), used ))
 xsks_map = {
@@ -18,6 +27,7 @@ xsks_map = {
 	.max_entries = FD_XDP_XSKS_MAP_SZ,
 };
 
+/* map for listening on udp port */
 struct bpf_map_def
 __attribute__(( section( "maps" ), used ))
 udp_map = {
@@ -37,40 +47,41 @@ int xdp_sock_prog( struct xdp_md *ctx )
 
   // // data[12..13] is eth type
   // // data[12] is ipproto
-  // unsigned ethtype1 = ( (unsigned)data[12] << 8u ) | (unsigned)data[13];
-  // //unsigned vlan     = ( (unsigned)data[14] << 8u ) | (unsigned)data[15];
-  // //unsigned ethtype2 = ( (unsigned)data[16] << 8u ) | (unsigned)data[17];
+  // uint ethtype1 = ( (uint)data[12] << 8u ) | (uint)data[13];
+  // //uint vlan     = ( (uint)data[14] << 8u ) | (uint)data[15];
+  // //uint ethtype2 = ( (uint)data[16] << 8u ) | (uint)data[17];
 
   // // do we have an IP packet?
   // if( ethtype1 != 0x0800u ) return XDP_PASS;
 
   // // do we have a UDP packet?
-  // unsigned ipproto = (unsigned)data[23];
+  // uint ipproto = (uint)data[23];
   // if( ipproto != 0x11 ) return XDP_PASS;
 
   // test ethtype and ipproto in 1 branch
-  unsigned test = ( (unsigned)data[12] << 16u ) | ( (unsigned)data[13] << 8u ) | (unsigned)data[23];
+  uint test = ( (uint)data[12] << 16u ) | ( (uint)data[13] << 8u ) | (uint)data[23];
   if( test != 0x080011 ) return XDP_PASS;
 
   // IP may have options, so calculate IP header length to find
   // start of UDP header
-  unsigned iplen = ( ( (unsigned)data[14] ) & 0xfu ) * 4u;
+  uint iplen = ( ( (uint)data[14] ) & 0xfu ) * 4u;
 
   uchar const * udp = data + iplen + 14;
 
   if( udp + 16ul >= data_end ) return XDP_PASS;
 
   // udp[2..3] is udp dst port
-  //unsigned udp_key = ( (unsigned)data[udp_ofs_0] << 8u ) | (unsigned)data[udp_ofs_1];
-  unsigned udp_key = ( ( (unsigned)udp[2] ) << 8u ) | (unsigned)udp[3];
+  //uint udp_key = ( (uint)data[udp_ofs_0] << 8u ) | (uint)data[udp_ofs_1];
+  uint udp_key = ( ( (uint)udp[2] ) << 8u ) | (uint)udp[3];
 
-  unsigned * udp_value = bpf_map_lookup_elem( &udp_map, &udp_key );
+  uint * udp_value = bpf_map_lookup_elem( &udp_map, &udp_key );
   if( !udp_value ) {
     // no match, pass to kernel
     return XDP_PASS;
   }
 
-  unsigned socket_key = *udp_value;
+  /* look up the interface queue to find the socket to forward to */
+  uint socket_key = ctx->rx_queue_index;
 
   return bpf_redirect_map( &xsks_map, socket_key, 0 );
 
