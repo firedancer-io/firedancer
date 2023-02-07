9b038d949a57230e82106813cee53efd458bc915
dwshaun <shaun@darkwaves.io>
2023-02-07 16:21:29 +0000

    Tidy up FD_LOG_HEXDUMP* macros w.r.t. newlines

--- src/util/log/fd_log.h
+++ src/util/log/fd_log.h
@@ -161,19 +161,50 @@
 #define FD_LOG_HEXDUMP_BLOB_DESCRIPTION_MAX_LEN   32
 #define FD_LOG_HEXDUMP_MAX_INPUT_BLOB_SZ          1500
 
-#define FD_LOG_HEXDUMP_ADD_TO_LOG_BUF(...) do {                               \
+#define FD_LOG_HEXDUMP_ADD_TO_LOG_BUF(...)  do {                              \
     num_bytes_written = sprintf ( log_buf_ptr, __VA_ARGS__ );                 \
     if( FD_LIKELY( num_bytes_written>=0 )) log_buf_ptr += num_bytes_written;  \
- } while(0)                                                                   \
+ } while(0)
                                              
-#define FD_LOG_HEXDUMP_DEBUG(a)             do { long _fd_log_msg_now = fd_log_wallclock(); char const * msg = fd_log_private_hexdump_msg a; if( msg ) fd_log_private_1( 0, _fd_log_msg_now, __FILE__, __LINE__, __func__, msg ); } while(0)
-#define FD_LOG_HEXDUMP_INFO(a)              do { long _fd_log_msg_now = fd_log_wallclock(); char const * msg = fd_log_private_hexdump_msg a; if( msg ) fd_log_private_1( 1, _fd_log_msg_now, __FILE__, __LINE__, __func__, msg ); } while(0)
-#define FD_LOG_HEXDUMP_NOTICE(a)            do { long _fd_log_msg_now = fd_log_wallclock(); char const * msg = fd_log_private_hexdump_msg a; if( msg ) fd_log_private_1( 2, _fd_log_msg_now, __FILE__, __LINE__, __func__, msg ); } while(0)
-#define FD_LOG_HEXDUMP_WARNING(a)           do { long _fd_log_msg_now = fd_log_wallclock(); char const * msg = fd_log_private_hexdump_msg a; if( msg ) fd_log_private_1( 3, _fd_log_msg_now, __FILE__, __LINE__, __func__, msg ); } while(0)
-#define FD_LOG_HEXDUMP_ERR(a)               do { long _fd_log_msg_now = fd_log_wallclock(); char const * msg = fd_log_private_hexdump_msg a; if( msg ) fd_log_private_2( 4, _fd_log_msg_now, __FILE__, __LINE__, __func__, msg ); } while(0)
-#define FD_LOG_HEXDUMP_CRIT(a)              do { long _fd_log_msg_now = fd_log_wallclock(); char const * msg = fd_log_private_hexdump_msg a; if( msg ) fd_log_private_2( 5, _fd_log_msg_now, __FILE__, __LINE__, __func__, msg ); } while(0)
-#define FD_LOG_HEXDUMP_ALERT(a)             do { long _fd_log_msg_now = fd_log_wallclock(); char const * msg = fd_log_private_hexdump_msg a; if( msg ) fd_log_private_2( 6, _fd_log_msg_now, __FILE__, __LINE__, __func__, msg ); } while(0)
-#define FD_LOG_HEXDUMP_EMERG(a)             do { long _fd_log_msg_now = fd_log_wallclock(); char const * msg = fd_log_private_hexdump_msg a; if( msg ) fd_log_private_2( 7, _fd_log_msg_now, __FILE__, __LINE__, __func__, msg ); } while(0)                                                    \
+#define FD_LOG_HEXDUMP_DEBUG(a)             do { long _fd_log_msg_now = fd_log_wallclock(); \
+    char const * msg = fd_log_private_hexdump_msg a;                                        \
+    if( msg ) fd_log_private_1( 0, _fd_log_msg_now, __FILE__, __LINE__, __func__, msg );    \
+    } while(0)                                                                              
+
+#define FD_LOG_HEXDUMP_INFO(a)              do { long _fd_log_msg_now = fd_log_wallclock(); \
+    char const * msg = fd_log_private_hexdump_msg a;                                        \
+    if( msg ) fd_log_private_1( 1, _fd_log_msg_now, __FILE__, __LINE__, __func__, msg );    \
+    } while(0)                                                                              
+
+#define FD_LOG_HEXDUMP_NOTICE(a)            do { long _fd_log_msg_now = fd_log_wallclock(); \
+    char const * msg = fd_log_private_hexdump_msg a;                                        \
+    if( msg ) fd_log_private_1( 2, _fd_log_msg_now, __FILE__, __LINE__, __func__, msg );    \
+    } while(0)                                                                              
+
+#define FD_LOG_HEXDUMP_WARNING(a)           do { long _fd_log_msg_now = fd_log_wallclock(); \
+    char const * msg = fd_log_private_hexdump_msg a;                                        \
+    if( msg ) fd_log_private_1( 3, _fd_log_msg_now, __FILE__, __LINE__, __func__, msg );    \
+    } while(0)                                                                              
+
+#define FD_LOG_HEXDUMP_ERR(a)               do { long _fd_log_msg_now = fd_log_wallclock(); \
+    char const * msg = fd_log_private_hexdump_msg a;                                        \
+    if( msg ) fd_log_private_2( 4, _fd_log_msg_now, __FILE__, __LINE__, __func__, msg );    \
+    } while(0)
+
+#define FD_LOG_HEXDUMP_CRIT(a)              do { long _fd_log_msg_now = fd_log_wallclock(); \
+    char const * msg = fd_log_private_hexdump_msg a;                                        \
+    if( msg ) fd_log_private_2( 5, _fd_log_msg_now, __FILE__, __LINE__, __func__, msg );    \
+    } while(0)
+
+#define FD_LOG_HEXDUMP_ALERT(a)             do { long _fd_log_msg_now = fd_log_wallclock(); \
+    char const * msg = fd_log_private_hexdump_msg a;                                        \
+    if( msg ) fd_log_private_2( 6, _fd_log_msg_now, __FILE__, __LINE__, __func__, msg );    \
+    } while(0)
+
+#define FD_LOG_HEXDUMP_EMERG(a)             do { long _fd_log_msg_now = fd_log_wallclock(); \
+    char const * msg = fd_log_private_hexdump_msg a;                                        \
+    if( msg ) fd_log_private_2( 7, _fd_log_msg_now, __FILE__, __LINE__, __func__, msg );    \
+    } while(0)                                                 \
 
 /* FD_TEST is a single statement that evaluates condition c and, if c
    evaluates to false, will FD_LOG_ERR that the condition failed.  It is
