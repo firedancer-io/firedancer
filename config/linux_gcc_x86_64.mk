52891ab6b1b8029adc94694fd8efd0887c4892bc
Josh Siegel <83408952+jumpsiegel@users.noreply.github.com>
2023-02-15 11:08:49 -0600

    runtime: put this back in

--- config/linux_gcc_x86_64.mk
+++ config/linux_gcc_x86_64.mk
@@ -4,7 +4,7 @@ include config/base.mk
 include config/with-gcc.mk
 include config/with-debug.mk
 include config/with-brutality.mk
-#include config/with-optimization.mk
+include config/with-optimization.mk
 include config/with-threads.mk
 include config/with-zstd.mk
 include config/with-rocksdb.mk
