7fc5d0636d4723bbe39d313dbe5e5f6df8d8d517
Josh Siegel <83408952+jumpsiegel@users.noreply.github.com>
2023-02-17 09:16:46 -0600

    runtime: stub out funk

--- src/ballet/runtime/Local.mk
+++ src/ballet/runtime/Local.mk
@@ -3,7 +3,7 @@
 ifneq ($(ROCKSDB),)
 
 $(call add-hdrs,fd_banks_solana.h fd_global_state.h fd_rocksdb.h)
-$(call add-objs,fd_banks_solana fd_rocksdb,fd_ballet)
+$(call add-objs,fd_banks_solana fd_rocksdb fd_funk_stub,fd_ballet)
 
 $(call make-unit-test,test_runtime,test_runtime,fd_ballet fd_util)
 
