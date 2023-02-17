3d21788c69cf71e2f8bbde94dd7653d5c353bb85
Josh Siegel <83408952+jumpsiegel@users.noreply.github.com>
2023-02-17 06:37:41 -0600

    Clean up some build issues

--- src/app/ledger/Local.mk
+++ src/app/ledger/Local.mk
@@ -1,4 +1,4 @@
-ifdef FD_HAS_ROCKSDB
+ifneq ($(ROCKSDB),)
 
 ifeq ($(FD_HAS_ZSTD),1)
 $(call make-bin,fd_frank_ledger,main tar,fd_util fd_ballet)
@@ -7,4 +7,6 @@ $(call make-bin,test_funk,test_funk fd_funk,fd_util)
 $(call make-bin,test_map_giant,test_map_giant,fd_util)
 endif
 
+else
+$(warning ledger tool build disabled due to lack of rocksdb)
 endif
