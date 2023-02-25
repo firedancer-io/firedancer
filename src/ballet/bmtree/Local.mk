cf579ac7de4bea2ce309a0554b80e5ad6bb36254
Richard Patel <ripatel@jumptrading.com>
2023-02-25 11:45:48 +0000

    bmtree: add FD_HAS_ALLOCA checks

--- src/ballet/bmtree/Local.mk
+++ src/ballet/bmtree/Local.mk
@@ -1,5 +1,7 @@
 $(call add-hdrs,fd_bmtree.h)
 $(call add-objs,fd_bmtree,fd_ballet)
 
+ifdef FD_HAS_ALLOCA
 $(call make-unit-test,test_bmtree20,test_bmtree20,fd_ballet fd_util)
 $(call make-unit-test,test_bmtree32,test_bmtree32,fd_ballet fd_util)
+endif
