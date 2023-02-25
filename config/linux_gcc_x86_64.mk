e1f7cd5d9fda326a63ab85f735b8b2f03a30037c
Richard Patel <ripatel@jumptrading.com>
2023-02-25 13:10:53 +0000

    config: remove with-libbpf.mk

--- config/linux_gcc_x86_64.mk
+++ config/linux_gcc_x86_64.mk
@@ -7,7 +7,6 @@ include config/with-debug.mk
 include config/with-brutality.mk
 include config/with-optimization.mk
 include config/with-threads.mk
-include config/with-libbpf.mk
 
 CPPFLAGS+=-fomit-frame-pointer -falign-functions=32 -falign-jumps=32 -falign-labels=32 -falign-loops=32 \
           -march=haswell -mtune=skylake -mfpmath=sse -mbranch-cost=5 \
