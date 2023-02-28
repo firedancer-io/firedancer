b11d9efd25f9fc16fe1e03c5ff859b94d9c53409
Richard Patel <ripatel@jumptrading.com>
2023-02-28 10:05:30 +0000

    xdp: add fd_xdp_redirect_user
    Co-authored-by: Nick Bridge <nbridge@jumptrading.com>

--- src/tango/xdp/Local.mk
+++ src/tango/xdp/Local.mk
@@ -5,7 +5,7 @@ ifdef FD_HAS_HOSTED
 ifdef FD_HAS_LIBBPF
 $(call make-lib,fd_xdp)
 $(call add-hdrs,fd_xdp.h fd_xsk.h fd_xsk_aio.h)
-$(call add-objs,fd_xsk fd_xsk_aio,fd_xdp)
+$(call add-objs,fd_xsk fd_xsk_aio fd_xdp_redirect_user,fd_xdp)
 endif
 endif
 
