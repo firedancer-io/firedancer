bf83a783edfb2b4b429315578cc0bb025335d9f8
Richard Patel <ripatel@jumptrading.com>
2023-02-28 10:06:09 +0000

    xdp: add fd_xdp_ctl

--- src/tango/xdp/Local.mk
+++ src/tango/xdp/Local.mk
@@ -6,6 +6,7 @@ ifdef FD_HAS_LIBBPF
 $(call make-lib,fd_xdp)
 $(call add-hdrs,fd_xdp.h fd_xsk.h fd_xsk_aio.h)
 $(call add-objs,fd_xsk fd_xsk_aio fd_xdp_redirect_user,fd_xdp)
+$(call make-bin,fd_xdp_ctl,fd_xdp_ctl,fd_xdp fd_util,$(EBPF_BINDIR)/fd_xdp_redirect_prog.o)
 endif
 endif
 
