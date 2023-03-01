0bab8bb4cf29684e0589a21745a1b279f1e727a5
Richard Patel <ripatel@jumptrading.com>
2023-03-01 09:01:10 +0000

    util: add --netns flag to fd_boot()

--- src/util/fd_util.c
+++ src/util/fd_util.c
@@ -1,3 +1,7 @@
+#if defined(__linux__)
+#include "fd_util_linux.c"
+#endif
+
 #include "fd_util.h"
 
 void
@@ -6,6 +10,9 @@ fd_boot( int *    pargc,
   /* At this point, we are immediately after the program start, there is
      only one thread of execution and fd has not yet been booted. */
   fd_log_private_boot  ( pargc, pargv );
+# if defined(__linux__)
+  fd_linux_private_boot( pargc, pargv );
+# endif
   fd_shmem_private_boot( pargc, pargv );
   fd_tile_private_boot ( pargc, pargv ); /* The caller is now tile 0 */
 }
