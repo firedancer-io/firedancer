2e026810b38bd9707acbab6edaad78293dd1fdc6
Liam Heeger <lheeger@jumptrading.com>
2023-03-02 14:21:47 -0600

    build, ci: add make run-unit-test
    ci: update test workflow to use make rules
    
    fix,cleanup: fix test workflow and remove dedup unit test
    
    ci: update coverage to run tests from make
    
    ci: remove fd_log_path in coverage
    
    ci: attempt to fix CI
    
    util: rm pcap unit test from runnable unit test target
    
    build: remove more broken unit tests that dont work on azure
    
    ci: fix workflow name
    
    ci: rm log unit test
    
    Update make_coverage.yml

--- src/ballet/pack/Local.mk
+++ src/ballet/pack/Local.mk
@@ -1,1 +1,2 @@
 $(call make-unit-test,test_compute_budget_program,test_compute_budget_program,fd_ballet fd_util)
+$(call run-unit-test,test_compute_budget_program,)
--- src/util/pod/Local.mk
+++ src/util/pod/Local.mk
@@ -3,4 +3,5 @@ $(call add-objs,fd_pod,fd_util)
 $(call make-unit-test,test_pod,test_pod,fd_util)
 $(call make-bin,fd_pod_ctl,fd_pod_ctl,fd_util)
 $(call add-test-scripts,test_pod_ctl)
+$(call run-unit-test,test_pod,)
 
