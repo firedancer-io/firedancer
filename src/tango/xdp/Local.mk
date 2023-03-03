559917f1baaef1358814474e2c5a12841f436381
Richard Patel <ripatel@jumptrading.com>
2023-03-03 03:39:57 +0000

    xdp: add test_xdp_ebpf.c

--- src/tango/xdp/Local.mk
+++ src/tango/xdp/Local.mk
@@ -7,6 +7,7 @@ $(call make-lib,fd_xdp)
 $(call add-hdrs,fd_xdp.h fd_xsk.h fd_xsk_aio.h)
 $(call add-objs,fd_xsk fd_xsk_aio fd_xdp_redirect_user,fd_xdp)
 $(call make-bin,fd_xdp_ctl,fd_xdp_ctl,fd_xdp fd_util,$(EBPF_BINDIR)/fd_xdp_redirect_prog.o)
+$(call make-unit-test,test_xdp_ebpf,test_xdp_ebpf,fd_xdp fd_util,$(EBPF_BINDIR)/fd_xdp_redirect_prog.o $(wildcard src/tango/xdp/fixtures/*.bin))
 endif
 endif
 
