402ca3032fa15a0b10933cc09aea6861c08fef5c
Richard Patel <ripatel@jumptrading.com>
2023-03-03 03:36:33 +0000

    config: enable compiler color output
    Sets the "-fdiagnostics-color=always" flag to make compile errors easier
    to read.

--- config/with-gcc.mk
+++ config/with-gcc.mk
@@ -5,7 +5,7 @@ LD:=g++
 # See with-clang.mk FD_USING_CLANG for behavior.  FD_USING_GCC and
 # FD_USING_CLANG should not be both set simulateously
 
-CPPFLAGS+=-DFD_USING_GCC=1
+CPPFLAGS+=-DFD_USING_GCC=1 -fdiagnostics-color=always
 
 FD_USING_GCC:=1
 
