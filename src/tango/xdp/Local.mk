91f2b31970e5fdb96aea0d7861ee62c1950faa18
Richard Patel <ripatel@jumptrading.com>
2023-03-08 12:29:03 +0000

    xdp: add fd_xdp_redirect_user
    Co-authored-by: Nick Bridge <nbridge@jumptrading.com>

--- src/tango/xdp/Local.mk
+++ src/tango/xdp/Local.mk
@@ -4,7 +4,7 @@ ifdef FD_HAS_HOSTED
 ifdef FD_HAS_LIBBPF
 $(call make-lib,fd_xdp)
 $(call add-hdrs,fd_xdp.h fd_xsk.h fd_xdp_redirect_user.h)
-$(call add-objs,fd_xsk,fd_xdp)
+$(call add-objs,fd_xsk fd_xdp_redirect_user,fd_xdp)
 
 $(call make-unit-test,test_xsk,test_xsk,fd_xdp fd_util)
 $(call run-unit-test,test_xsk)
--- src/tango/xdp/fd_xdp_redirect_user.h
+++ src/tango/xdp/fd_xdp_redirect_user.h
@@ -1,9 +1,158 @@
 #ifndef HEADER_fd_src_net_xdp_fd_xdp_redirect_user_h
 #define HEADER_fd_src_net_xdp_fd_xdp_redirect_user_h
 
+/* Userspace API for controlling the fd_xdp_redirect_prog program.
+
+   ### XDP program
+
+   This API is specific to the fd_xdp_redirect_prog.c program.
+   Loading other XDP programs is unsupported.  In short, it is
+   responsible for identifying and redirecting packets matching the
+   app's listener to the app's XSKs (e.g. by IP address/UDP port).
+   See the program's source for more info.
+
+   ### XDP program installation
+
+   The bpf(2) syscall allows loading a compiled eBPF program into the
+   kernel.  It is recommended to enable the eBPF JIT compiler for better
+   performance via `echo 1 >/proc/sys/net/core/bpf_jit_enable`.
+   Manpage: https://man7.org/linux/man-pages/man2/bpf.2.html
+
+   ### Lifecycle
+
+   This API allows separating the XDP program installation from the app
+   lifecycle, reducing the privileges/capabilities required at runtime.
+
+   The step-by-step lifecycle looks as follows:
+
+   - fd_xdp_install() (privileged)
+   - For each interface to listen on
+     - fd_xdp_hook_iface() (privileged)
+     - For each RX/TX queue pair of this interface
+       - fd_xsk_new()
+       - fd_xsk_bind()
+       - fd_xsk_join()
+   - For each UDP/IP destination to listen on
+     - fd_xdp_listen_udp_port()
+   - ... Application run ... */
+
+/* TODO: Support NUMA-aware eBPF maps */
+
 #include "fd_xsk.h"
+#include "../../util/fd_util.h"
+
+/* FD_XDP_PIN_NAME_SZ: max number of chars in an eBPF pin dir name */
+#define FD_XDP_PIN_NAME_SZ (255UL)
 
 FD_PROTOTYPES_BEGIN
+
+/* Install API (privileged) *******************************************/
+
+/* fd_xdp_init: Prepare an XDP environment by pinning shared eBPF maps
+   in bpffs.  Returns 0 on success and -1 on error.  Reasons for error
+   are logged to FD_LOG_WARNING.  Requires CAP_SYS_ADMIN and file system
+   permissions to write to /sys/fs/bpf.  The identifier app_name is used
+   to isolate multiple app instances, such that functions in this API
+   given an arbitrary app_name A cannot see the side effects of calls to
+   the same functions given an arbitrary other app_name B.  The caller
+   should ensure that multiple fd_xdp app instances do not conflict
+   (e.g. by listening on the same ports).
+
+   Assumes that /sys/fs/bpf is a valid bpffs mount.
+   Creates the following files in /sys/fs/bpf/{app_name}/
+
+     udp_dsts  BPF_MAP_TYPE_HASH map, see firedancer_udp_dsts in
+               program ebpf_xdp_flow.c */
+int
+fd_xdp_init( char const * app_name );
+
+/* fd_xdp_fini: Destroy all kernel resources installed by fd_xdp
+   corresponding to the given app name, including any XDP programs,
+   installations, eBPF maps, and links.  Returns 0 on success and -1 on
+   error.  Reasons for error are logged to FD_LOG_WARNING.  Requires
+   CAP_SYS_ADMIN and permissions to write to /sys/fs/bpf. */
+int
+fd_xdp_fini( char const * app_name );
+
+/* fd_xdp_hook_iface: Install the XDP redirect program to the network
+   device with name ifname.  Installation lifetime is until a
+   matching call to fd_xdp_unhook_iface() or until the system is
+   shut down.  xdp_mode is the XDP install mode (as defined by
+   XDP_FLAGS_{...}_MODE in <linux/if_link.h>).  The given XDP priority
+   is used to determine order of execution when multiple XDP programs
+   are installed on this interface.  Programs installed with lower
+   priority values are executed first.  The given prog_elf must point
+   to memory containing an ELF static object of fd_xdp_redirect_prog.c
+   compiled for eBPF arch, where prog_elf_sz is the size of ELF file.
+   Returns 0 on success and -1 on error.  Fails if the XDP redirect
+   program is already installed on this iface for this app_name.
+   Reasons for error are logged to FD_LOG_WARNING.
+   Requires CAP_SYS_ADMIN and permissions to write to /sys/fs/bpf.
+
+   Valid values for xdp_mode:
+
+     0                   kernel default mode
+     XDP_FLAGS_SKB_MODE  sk_buff generic mode (hardware-agnostic)
+     XDP_FLAGS_DRV_MODE  driver XDP (requires driver support)
+     XDP_FLAGS_HW_MODE   hardware-accelerated XDP
+                         (requires NIC and driver support)
+
+   Assumes that /sys/fs/bpf is a valid bpffs mount.
+   Creates the following files in /sys/fs/bpf/{app_name}/{ifname}/
+
+     xdp   fd_xdp_redirect_prog XDP program.
+     xsks  BPF_MAP_TYPE_XSKMAP assigning each active RX queue an XSK. */
+int
+fd_xdp_hook_iface( char const * app_name,
+                   char const * ifname,
+                   uint         xdp_mode,
+                   int          priority,
+                   void const * prog_elf,
+                   ulong        prog_elf_sz );
+
+/* fd_xdp_unhook_iface uninstalls the XDP redirect program from the
+   network device with name ifname.  Removes the bpffs files created by
+   the corresponding fd_xdp_hook_iface.  Returns 0 on success or if no
+   redirect program installation was found, and -1 on error.  Reasons
+   for error are logged to FD_LOG_WARNING.  Requires CAP_SYS_ADMIN. */
+int
+fd_xdp_unhook_iface( char const * app_name,
+                     char const * ifname );
+
+/* Listen API (privileged) ********************************************/
+
+/* fd_xdp_udp_dst_key returns a key for the firedancer_udp_dsts eBPF
+   map given the IPv4 dest address and UDP port number. */
+static inline ulong
+fd_xdp_udp_dst_key( uint ip4_addr,
+                    uint udp_port ) {
+  return ( (ulong)fd_uint_bswap( ip4_addr )<<16 ) | fd_ushort_bswap( (ushort)udp_port );
+}
+
+/* fd_xdp_listen_udp_port installs a listener for protocol proto on IPv4
+   destination addr ip4_dst_addr and UDP destination port udp_dst_port.
+   Installation lifetime is until a matching call to
+   fd_xdp_release_udp_port() or until the system is shut down.
+   On interfaces running the XDP redirect program, causes matching
+   traffic to get redirected to active XSKs, and ceases processing of
+   matching traffic in the Linux networking stack.  Returns 0 on success
+   or if no redirect program installation was found, and -1 on error.
+   Reasons for error are logged to FD_LOG_WARNING. */
+int
+fd_xdp_listen_udp_port( char const * app_name,
+                        uint         ip4_dst_addr,
+                        uint         udp_dst_port,
+                        uint         proto );
+
+/* fd_xdp_release_udp_port uninstalls a listener that was previously
+   installed with fd_xdp_listen_udp_port().  Restores processing of
+   matching traffic in the Linux networking stack.  Returns 0 on success
+   or if no redirect program installation was found, and -1 on error.
+   Reasons for error are logged to FD_LOG_WARNING. */
+int
+fd_xdp_release_udp_port( char const * app_name,
+                         uint         ip4_dst_addr,
+                         uint         udp_dst_port );
 
 /* Runtime API (unprivileged) *****************************************/
 
@@ -32,4 +181,3 @@ fd_xsk_deactivate( fd_xsk_t * xsk );
 FD_PROTOTYPES_END
 
 #endif /* HEADER_fd_src_net_xdp_fd_xdp_redirect_user_h */
-
