91ef5e2b1150eeaa2fa6cd918cd0b7689e804ea8
Nick Bridge <nbridge@jumptrading.com>
2023-03-08 12:21:51 +0000

    xdp: add XSK I/O logic

--- src/tango/xdp/Local.mk
+++ src/tango/xdp/Local.mk
@@ -1,2 +1,10 @@
 $(call add-hdrs,fd_xdp_redirect_prog.h)
 
+ifdef FD_HAS_HOSTED
+ifdef FD_HAS_LIBBPF
+$(call make-lib,fd_xdp)
+$(call add-hdrs,fd_xsk.h)
+$(call add-objs,fd_xsk,fd_xdp)
+endif
+endif
+
