2657a65126476c6b1676873ce906a02d4b84c21d
Richard Patel <ripatel@jumptrading.com>
2023-03-08 12:27:19 +0000

    xdp: add test_xsk

--- src/tango/xdp/Local.mk
+++ src/tango/xdp/Local.mk
@@ -5,6 +5,9 @@ ifdef FD_HAS_LIBBPF
 $(call make-lib,fd_xdp)
 $(call add-hdrs,fd_xdp.h fd_xsk.h fd_xdp_redirect_user.h)
 $(call add-objs,fd_xsk,fd_xdp)
+
+$(call make-unit-test,test_xsk,test_xsk,fd_xdp fd_util)
+$(call run-unit-test,test_xsk)
 endif
 endif
 
