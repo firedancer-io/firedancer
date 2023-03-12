1f3905d6791dfdbbe698ccc63d6f4ce836525870
Kevin J Bowers <kbowers@jumptrading.com>
2023-03-12 15:50:45 -0500

    Belt sanded the libnuma removal and additionally removed hwloc dependency
    Includes:
    
    - Added commands to fd_shmem_ctl for doing command line topological
      queries formerly kicked to hwloc and switch existing usages to use
      that instead of hwloc.
    
    - Lots of bulletproofing added around sysfs interaction (more robust
      parsing of info from sysfs, fewer assumptions about what sysfs might
      do, streamlined APIs for topology discovery instead of just imitating
      the clunkier libnuma APIs, ...).
    
    - fd_numa_linux / fd_numa_stub are in their own compile unit (selected
      at compile time as appropriate for the target).  This (along with the
      renaming of stubs more consistently) allowed elimination of weak
      aliases and should eliminate some of the ASAN build time hackery (e.g.
      FD_HAS_ASAN friendly variants of the O/S shims in fd_numa can be
      selected at compile time).
    
    - Other minor cleanups along the way (e.g. use FD_IMPORT_CSTR for the
      fd_shmem_ctl help, eliminate references to hwloc and numactl in
      documentation, activate scripts, various complex unit tests, etc).
    
    - These above changes seemed to eliminate the need for the test_numa /
      test_numa_linux tests that didn't seem to be built by default anyway
      (existing tests should already provide coverage but these tests can be
      resurrected and updated as necessary if desired).
    
    Lots of this was long overdue cleanups and, more generally speaking,
    there is probably more streamlining and better portability to non-Linux
    platforms by not just exactly imitating the behaviors of mlock / munlock
    / get_mempolicy / set_mempolicy / mbind / movepages APIs at the shims
    fd_shmem_admin.c and fd_numa_*.c

--- src/util/shmem/Local.mk
+++ src/util/shmem/Local.mk
@@ -4,3 +4,9 @@ $(call add-scripts,fd_shmem_cfg)
 $(call make-bin,fd_shmem_ctl,fd_shmem_ctl,fd_util)
 $(call make-unit-test,test_shmem,test_shmem,fd_util)
 $(call add-test-scripts,test_shmem_ctl)
+
+ifdef FD_HAS_HOSTED
+$(call add-objs,fd_numa_linux,fd_util)
+else
+$(call add-objs,fd_numa_stub,fd_util)
+endif
--- src/util/shmem/fd_numa_linux.c
+++ src/util/shmem/fd_numa_linux.c
@@ -1,142 +1,207 @@
-#ifndef SOURCE_fd_src_util_shmem_fd_shmem_admin
-#error "Do not compile this file directly"
-#endif
-
+/* syscall API requires _GNU_SOURCE */
 #define _GNU_SOURCE
-#include <unistd.h>
-#include <sys/syscall.h>
-#include <sys/sysinfo.h>
-#include <sys/types.h>
-#include <signal.h>
+#include "fd_shmem_private.h"
 #include <errno.h>
 #include <dirent.h>
+#include <sys/sysinfo.h>
 
-#include "../fd_util_base.h"
-#include "../cstr/fd_cstr.h"
+/* The below uses the sysfs API added ~2009-Dec.  See
+   https://github.com/torvalds/linux/commit/1830794ae6392ce12d36dbcc5ff52f11298ddab6 */
 
-/* Syscall wrappers are defined as weak symbols to allow
-   instrumentation like strace, ASan to override them. */
+/* fd_numa_private_parse_node_idx parses a cstr of the form
+   `node[0-9]+` into a node idx.  The value will strictly interpreted as
+   a non-negative base 10 value.  Returns -1 if the value could not be
+   parsed (e.g. s is NULL, s does not have a node prefix, s does not
+   have a base 10 suffix, the value overflows an int representation).
+   FIXME: consider having the user pass the prefix to scan for to allow
+   extracting more general indices from sysfs paths. */
 
-__attribute__((weak)) long
-get_mempolicy( int *   mode,
-               ulong * nodemask,
-               ulong   maxnode,
-               void *  addr,
-               uint    flags ) {
-  return syscall( SYS_get_mempolicy,
-                  mode, nodemask, maxnode, addr, flags );
-}
-
-__attribute__((weak)) long
-set_mempolicy( int           mode,
-               ulong const * nodemask,
-               ulong         maxnode ) {
-  return syscall( SYS_set_mempolicy,
-                  mode, nodemask, maxnode );
-}
-
-__attribute__((weak)) long
-mbind( void *        addr,
-       ulong         len,
-       int           mode,
-       ulong const * nodemask,
-       ulong         maxnode,
-       uint          flags ) {
-  return syscall( SYS_mbind,
-                  addr, len, mode, nodemask, maxnode, flags );
-}
-
-__attribute__((weak)) long
-move_pages( int         pid,
-            ulong       count,
-            void **     pages,
-            int const * nodes,
-            int *       status,
-            int         flags ) {
-  return syscall( SYS_move_pages,
-                  pid, count, pages, nodes, status, flags );
-}
-
-int
-fd_numa_available( void ) {
-  long res = get_mempolicy( NULL, NULL, 0, NULL, 0 );
-  return res == 0;
-}
-
-/* cstr_is_base10: returns 1 if cstr matches `[0-9]+`, else 0. */
-static inline int
-cstr_is_base10( char const * s ) {
-  if( FD_UNLIKELY( *s == '\0' )) return 0;
-  do {
-    if( FD_UNLIKELY( *s < '0' || *s > '9' ) )
-      return 0;
-  } while( FD_UNLIKELY( *(++s) ) );
-  return 1;
-}
-
-/* sysfs_node_idx: parses cstr `node%d` and returns %d, else -1.
-   May return negative integers for  */
-static inline int
-sysfs_node_idx( char const * s ) {
-  if( strncmp( s, "node", 4 ) ) return -1;
+FD_FN_PURE static int
+fd_numa_private_parse_node_idx( char const * s ) {
+  if( FD_UNLIKELY( !s ) ) return -1;
+  if( FD_UNLIKELY( strncmp( s, "node", 4UL ) ) ) return -1;
   s += 4;
 
-  if( FD_UNLIKELY( !cstr_is_base10( s ) ) ) return -1;
+  long val = 0L;
 
-  return fd_cstr_to_int( s );
-}
-
-int
-fd_shmem_numa_cnt_private( void ) {
-  DIR * d = opendir( "/sys/devices/system/node" );
-  if( FD_UNLIKELY( !d ) ) return -errno;
-
-  int node_cnt = 0;
-  struct dirent * ent;
-  while( (ent = readdir( d ))!=NULL ) {
-    if( sysfs_node_idx( ent->d_name )>=0 )
-      node_cnt++;
+  char const * t = s;
+  for(;;) {
+    char c = *t;
+    if( !c ) break; /* host dep branch prob */
+    if( FD_UNLIKELY( !(('0'<=c) | (c<='9')) ) ) return -1; /* non-digit encoutered */
+    val = (long)(c-'0') + 10L*val;
+    if( FD_UNLIKELY( val>(long)INT_MAX ) ) return -1; /* overflow */
+    t++;
   }
-  closedir( d );
+  if( FD_UNLIKELY( s==t ) ) return -1; /* empty idx */
 
-  return node_cnt;
+  return (int)val;
 }
 
-int
-fd_shmem_cpu_cnt_private( void ) {
-  return get_nprocs();
-}
+ulong
+fd_numa_node_cnt( void ) {
 
-int
-fd_numa_cpu_max_cnt( void ) {
-  return get_nprocs_conf();
-}
+  /* Open sysfs dir containing NUMA config.  Abort if this fails. */
 
-int
-fd_numa_node_of_cpu( int cpu_idx ) {
-  /* Uses sysfs API added ~2009-Dec
-     See https://github.com/torvalds/linux/commit/1830794ae6392ce12d36dbcc5ff52f11298ddab6 */
-
-  /* Find sysfs dir containing CPU config */
-  char node_path[38];
-  fd_cstr_printf( node_path, sizeof(node_path), NULL,
-                  "/sys/devices/system/cpu/cpu%d", cpu_idx );
-
-  /* Open dir and scan for symlinks */
-  DIR * d = opendir( node_path );
-  if( FD_UNLIKELY( !d ) ) return -errno;
-
-  /* Scan for a node%d symlink */
-  int node_idx = -ENOENT;
-  struct dirent * ent;
-  while( (ent = readdir( d ))!=NULL ) {
-    int i = sysfs_node_idx( ent->d_name );
-    if( i>=0 ) {
-      node_idx = i;
-      break;
-    }
+  char const * path = "/sys/devices/system/node";
+  DIR *        dir  = opendir( path );
+  if( FD_UNLIKELY( !dir ) ) {
+    FD_LOG_WARNING(( "opendir( \"%s\" ) failed (%i-%s)", path, errno, strerror( errno ) ));
+    return 0UL;
   }
-  closedir( d );
 
-  return node_idx;
+  /* Scan dir to get number of NUMA nodes.  Note that we do not assume
+     the system indexes numa nodes contiguously (but it almost certainly
+     does). */
+
+  int node_idx_max = INT_MIN;
+  for(;;) {
+    struct dirent * dirent = readdir( dir );
+    if( !dirent ) break;
+    node_idx_max = fd_int_max( fd_numa_private_parse_node_idx( dirent->d_name ), node_idx_max );
+  }
+
+  /* Close dir and return what was found */
+
+  if( FD_UNLIKELY( closedir( dir ) ) )
+    FD_LOG_WARNING(( "closedir( \"%s\" ) failed (%i-%s); attempting to continue", path, errno, strerror( errno ) ));
+
+  if( FD_UNLIKELY( node_idx_max<0 ) ) {
+    FD_LOG_WARNING(( "No numa nodes found in \"%s\"", path ));
+    return 0UL;
+  }
+
+  return ((ulong)node_idx_max) + 1UL;
+}
+
+ulong
+fd_numa_cpu_cnt( void ) {
+
+  /* FIXME: Consider using get_npocs_conf, syscall or sysfs director
+     scan. */
+
+  int cpu_cnt = get_nprocs();
+  if( FD_UNLIKELY( cpu_cnt<=0 ) ) {
+    FD_LOG_WARNING(( "Unexpected return (%i) from get_nprocs", cpu_cnt ));
+    return 0UL;
+  }
+
+  return (ulong)cpu_cnt;
+}
+
+ulong
+fd_numa_node_idx( ulong cpu_idx ) {
+
+  /* Open sysfs dir containing CPU config.  Abort if this fails. */
+
+  char  path[64];
+  DIR * dir = opendir( fd_cstr_printf( path, 64UL, NULL, "/sys/devices/system/cpu/cpu%lu", cpu_idx ) );
+  if( FD_UNLIKELY( !dir ) ) {
+    FD_LOG_WARNING(( "opendir( \"%s\" ) failed (%i-%s)", path, errno, strerror( errno ) ));
+    return ULONG_MAX;
+  }
+
+  /* Scan dir for symlink to numa config */
+
+  int node_idx = -1;
+  for(;;) {
+    struct dirent * dirent = readdir( dir );
+    if( !dirent ) break;
+    node_idx = fd_numa_private_parse_node_idx( dirent->d_name );
+    if( node_idx!=-1 ) break;
+  }
+
+  /* Close dir and return what was found */
+
+  if( FD_UNLIKELY( closedir( dir ) ) )
+    FD_LOG_WARNING(( "closedir( \"%s\" ) failed (%i-%s); attempting to continue", path, errno, strerror( errno ) ));
+
+  if( FD_UNLIKELY( node_idx<0 ) ) {
+    FD_LOG_WARNING(( "No numa node found in \"%s\"", path ));
+    return ULONG_MAX;
+  }
+
+  return (ulong)node_idx;
+}
+
+/* FIXME: probably should do a FD_HAS_ASAN switch for the below to use
+   the appropriate functionality when FD_HAS_ASAN is set (or maybe have
+   a separate implementation for compiling under FD_HAS_ASAN). */
+
+#include <unistd.h>
+#include <sys/syscall.h>
+
+/* Note that the LLVM AddressSanitizer (ASan) intercepts all mlock
+   calls.
+
+   This has an interesting history.  These interceptors were first added
+   in 2012 and are still present in LLVM 14.0.6:
+
+     https://github.com/llvm/llvm-project/commit/71d759d392f03025bcc8b20f060bc5c22e580ea1
+
+   They stub `mlock`, `munlock`, `mlockall`, `munlockall` to no-ops.
+
+   ASan is known to map large amounts (~16TiB) of unbacked pages.  This
+   rules out the use of `mlockall`.
+
+   `mlock` only locks selected pages, therefore should be fine.  The
+   comments in various revisions of these interceptors suggest that
+   older Linux kernels had a bug that prevented the use of `mlock`.
+
+   However, current Firedancer will use the `move_pages` syscall to
+   verify whether "allocated" pages are actually backed by DRAM.
+
+   This makes Firedancer and ASan incompatible unless we either
+
+     1) Remove the `mlock` interceptor upstream, or
+     2) Circumvent the interceptor with a raw syscall
+
+   We do option 2 below */
+
+int
+fd_numa_mlock( void const * addr,
+               ulong        len ) {
+  return (int)syscall( __NR_mlock, addr, len );
+}
+
+int
+fd_numa_munlock( void const * addr,
+                 ulong        len ) {
+  return (int)syscall( __NR_mlock, addr, len );
+}
+
+long
+fd_numa_get_mempolicy( int *   mode,
+                       ulong * nodemask,
+                       ulong   maxnode,
+                       void *  addr,
+                       uint    flags ) {
+  return syscall( SYS_get_mempolicy, mode, nodemask, maxnode, addr, flags );
+}
+
+long
+fd_numa_set_mempolicy( int           mode,
+                       ulong const * nodemask,
+                       ulong         maxnode ) {
+  return syscall( SYS_set_mempolicy, mode, nodemask, maxnode );
+}
+
+long
+fd_numa_mbind( void *        addr,
+               ulong         len,
+               int           mode,
+               ulong const * nodemask,
+               ulong         maxnode,
+               uint          flags ) {
+  return syscall( SYS_mbind, addr, len, mode, nodemask, maxnode, flags );
+}
+
+long
+fd_numa_move_pages( int         pid,
+                    ulong       count,
+                    void **     pages,
+                    int const * nodes,
+                    int *       status,
+                    int         flags ) {
+  return syscall( SYS_move_pages, pid, count, pages, nodes, status, flags );
 }
