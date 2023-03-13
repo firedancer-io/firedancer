3e7d042c98d56b251bec55f1325c8480972e1fe7
Richard Patel <ripatel@jumptrading.com>
2023-03-13 15:51:14 +0000

    Revert "temporarily disable test_quic_server"
    This reverts commit 28bcd69b5610edcb40ef59e5371b488fa964c3ff.

--- src/tango/tests/Local.mk
+++ src/tango/tests/Local.mk
@@ -1,2 +1,2 @@
-# Broken for now
-#$(call make-bin,test_quic_server,test_quic_server,fd_aio fd_xdp fd_quic fd_util)
+$(call make-bin,test_quic_server,test_quic_server,fd_aio fd_xdp fd_quic fd_util)
+
