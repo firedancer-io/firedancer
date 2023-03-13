28bcd69b5610edcb40ef59e5371b488fa964c3ff
Richard Patel <ripatel@jumptrading.com>
2023-03-13 15:34:35 +0000

    temporarily disable test_quic_server

--- src/tango/tests/Local.mk
+++ src/tango/tests/Local.mk
@@ -1,2 +1,2 @@
-$(call make-bin,test_quic_server,test_quic_server,fd_aio fd_xdp fd_quic fd_util)
-
+# Broken for now
+#$(call make-bin,test_quic_server,test_quic_server,fd_aio fd_xdp fd_quic fd_util)
