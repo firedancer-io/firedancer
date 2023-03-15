2ed5f76a52d97ce7993ed8659e64bd8b76ca7a78
Kevin J Bowers <kbowers@jumptrading.com>
2023-03-15 11:33:03 -0500

    Split up the unit tests to workaround compiler limitations
    gcc was starting complain that it was just too hard to do all the
    requested tests ("note: variable tracking size limit exceeded with
    -fvar-tracking-assignments, retrying without")
    
    As a bonus, having it split up helps speed up builds further across the
    board (especially parallel and incremental builds).

--- src/util/simd/Local.mk
+++ src/util/simd/Local.mk
@@ -1,8 +1,12 @@
 $(call add-hdrs,fd_sse.h fd_sse_vc.h fd_sse_vi.h fd_sse_vu.h fd_sse_vf.h fd_sse_vl.h fd_sse_vv.h fd_sse_vd.h fd_sse_vl.h)
-$(call make-unit-test,test_sse,test_sse,fd_util)
-$(call run-unit-test,test_sse,)
+$(call make-unit-test,test_sse_4x32,test_sse_4x32 test_sse_common,fd_util)
+$(call make-unit-test,test_sse_2x64,test_sse_2x64 test_sse_common,fd_util)
+$(call run-unit-test,test_sse_4x32,)
+$(call run-unit-test,test_sse_2x64,)
 
 $(call add-hdrs,fd_avx.h fd_avx_wc.h fd_avx_wi.h fd_avx_wu.h fd_avx_wf.h fd_avx_wl.h fd_avx_wv.h fd_avx_wd.h fd_avx_wl.h)
-$(call make-unit-test,test_avx,test_avx,fd_util)
-$(call run-unit-test,test_avx,)
+$(call make-unit-test,test_avx_8x32,test_avx_8x32 test_avx_common,fd_util)
+$(call make-unit-test,test_avx_4x64,test_avx_4x64 test_avx_common,fd_util)
+$(call run-unit-test,test_avx_8x32,)
+$(call run-unit-test,test_avx_4x64,)
 
