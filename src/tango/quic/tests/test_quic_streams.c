57e32c1d08bb4e7f5c1a30c8701ef08db1bd16c2
Nick Bridge <nbridge@jumptrading.com>
2023-03-17 16:01:21 -0500

    fix to test_quic_streams from rebase

--- src/tango/quic/tests/test_quic_streams.c
+++ src/tango/quic/tests/test_quic_streams.c
@@ -433,8 +433,8 @@ main( int argc, char ** argv ) {
   quic_config.max_in_flight_acks    = 100;
   quic_config.conn_id_sparsity      = 4;
 
-  quic_config.cert_file             = "cert.pem";
-  quic_config.key_file              = "key.pem";
+  strcpy( quic_config.cert_file, "cert.pem" );
+  strcpy( quic_config.key_file,  "key.pem"  );
 
   quic_config.cb_stream_receive     = my_stream_receive_cb;
   quic_config.cb_stream_notify      = my_stream_notify_cb;
