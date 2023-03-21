9758d52318a75ee97c5be880e1e040c7c58ce880
Richard Patel <ripatel@jumptrading.com>
2023-03-21 21:46:19 +0000

    remove contrib/ci-deps.sh

--- contrib/ci-deps.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Note: This is intended for use in GitHub Actions builds only.
-
-export DEBIAN_FRONTEND="noninteractive"
-sudo apt install -y --no-install-recommends \
-  libelf-dev \
-  lcov
