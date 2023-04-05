5601f9eed9d2958ad1eb8a1db7f8c1c2c5a6f959
Tom Pointon <tpointon@jumptrading.com>
2023-04-05 12:42:17 +0000

    runtime: fix prototype

--- src/ballet/runtime/program/fd_system_program.h
+++ src/ballet/runtime/program/fd_system_program.h
@@ -9,6 +9,6 @@ FD_PROTOTYPES_BEGIN
 /* Entry-point for the Solana System Program */
 int fd_executor_system_program_execute_instruction( instruction_ctx_t ctx ) ;
 
-FD_PROTOTYPES_BEGIN
+FD_PROTOTYPES_END
 
 #endif /* HEADER_fd_src_ballet_runtime_program_fd_system_program_h */
