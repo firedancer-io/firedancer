7383420bf936212f2fddf457ec546bc15baa2bee
Richard Patel <ripatel@jumptrading.com>
2023-04-06 01:05:00 +0000

    hmac: add sha384 support

--- src/ballet/hmac/fd_hmac.c
+++ src/ballet/hmac/fd_hmac.c
@@ -8,8 +8,12 @@
 #define HASH_SZ       FD_SHA256_HASH_SZ
 #include "fd_hmac_tmpl.c"
 
+#define HASH_ALG      sha384
+#define HASH_BLOCK_SZ FD_SHA384_BLOCK_SZ
+#define HASH_SZ       FD_SHA384_HASH_SZ
+#include "fd_hmac_tmpl.c"
+
 #define HASH_ALG      sha512
 #define HASH_BLOCK_SZ FD_SHA512_BLOCK_SZ
 #define HASH_SZ       FD_SHA512_HASH_SZ
 #include "fd_hmac_tmpl.c"
-
