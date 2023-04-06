3ab430bd78f4c6cb1c0d5709c020e7732a3ac199
Richard Patel <ripatel@jumptrading.com>
2023-04-06 01:05:00 +0000

    hmac: add sha384 support

--- src/ballet/hmac/fd_hmac.h
+++ src/ballet/hmac/fd_hmac.h
@@ -19,8 +19,12 @@ fd_hmac_sha256( void const * data,
                 ulong        key_sz,
                 void *       hash );
 
-/* fd_hmac_sha512 is like fd_hmac_sha256, but computes a HMAC-SHA512
-   digest with 64 byte output size. */
+void *
+fd_hmac_sha384( void const * data,
+                ulong        data_sz,
+                void const * key,
+                ulong        key_sz,
+                void *       hash );
 
 void *
 fd_hmac_sha512( void const * data,
