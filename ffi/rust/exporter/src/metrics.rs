21b664538982e325cf9af88df8d45d30b010b075
Richard Patel <ripatel@jumptrading.com>
2023-04-12 13:14:38 +0000

    quic: more metrics

--- ffi/rust/exporter/src/metrics.rs
+++ ffi/rust/exporter/src/metrics.rs
@@ -6,6 +6,11 @@ use std::ffi::{
 use firedancer::pod::PodIter;
 use firedancer_sys::tango::{
     fd_cnc_t,
+    fd_frag_meta_t,
+    fd_mcache_join,
+    fd_mcache_leave,
+    fd_mcache_seq_laddr_const,
+    fd_mcache_seq_query,
     quic::{
         fd_quic_t,
         FD_QUIC_MAGIC,
@@ -44,10 +49,12 @@ macro_rules! label_pairs {
         {
             vec![
                 $( new_label_pair($k, $v), )*
-            ]
+            ].into()
         }
     };
 }
+
+// TODO hacky min-effort monitor -- clean up or port to C
 
 /// Handles to various Frankendancer objects to extract monitoring data from.
 pub(crate) struct FrankSpy {
@@ -87,6 +94,10 @@ impl FrankSpy {
                 fd_log_info!("joining quic.{}.cnc", quic_name_str);
                 let quic_cnc = fd_cnc_join(fd_wksp_pod_map(quic_pod, cstr!("cnc").as_ptr()));
 
+                fd_log_info!("joining quic.{}.mcache", quic_name_str);
+                let quic_mcache =
+                    fd_mcache_join(fd_wksp_pod_map(quic_pod, cstr!("mcache").as_ptr()));
+
                 fd_log_info!("mapping quic.{}.quic", quic_name_str);
                 let quic = fd_wksp_pod_map(quic_pod, cstr!("quic").as_ptr()) as *const fd_quic_t;
                 assert!((*quic).magic == FD_QUIC_MAGIC as u64);
@@ -94,6 +105,7 @@ impl FrankSpy {
                 FrankQuicSpy {
                     name: quic_name.to_str().unwrap().to_string(),
                     cnc: quic_cnc,
+                    mcache: quic_mcache,
                     quic: quic,
                 }
             })
@@ -133,11 +145,16 @@ impl FrankSpy {
         if self.quic.is_empty() {
             return;
         }
+
+        let mut quic_seq_vec = Vec::<Metric>::with_capacity(self.quic.len());
         let mut quic_chunk_idx_vec = Vec::<Metric>::with_capacity(self.quic.len());
         let mut quic_tpu_pub_cnt_vec = Vec::<Metric>::with_capacity(self.quic.len());
         let mut quic_tpu_pub_sz_vec = Vec::<Metric>::with_capacity(self.quic.len());
         let mut quic_net_rx_pkt_cnt_vec = Vec::<Metric>::with_capacity(self.quic.len());
         let mut quic_net_tx_pkt_cnt_vec = Vec::<Metric>::with_capacity(self.quic.len());
+        let mut quic_conn_active_cnt_vec = Vec::<Metric>::with_capacity(self.quic.len());
+        let mut quic_conn_created_cnt_vec = Vec::<Metric>::with_capacity(self.quic.len());
+        let mut quic_conn_closed_cnt_vec = Vec::<Metric>::with_capacity(4 * self.quic.len());
 
         for quic in &self.quic {
             let cnc_diag = unsafe { fd_cnc_app_laddr_const(quic.cnc) } as *const u64;
@@ -147,30 +164,91 @@ impl FrankSpy {
                 unsafe { *(cnc_diag.add(FD_QUIC_CNC_DIAG_TPU_PUB_CNT as usize)) };
             let quic_tpu_pub_sz = unsafe { *(cnc_diag.add(FD_QUIC_CNC_DIAG_TPU_PUB_SZ as usize)) };
 
-            let mut m = quic.new_metric();
+            let mut m = Metric::new();
+            m.set_label(label_pairs!("tile" => &quic.name));
+            m.set_gauge(new_gauge(unsafe {
+                fd_mcache_seq_query(fd_mcache_seq_laddr_const(quic.mcache))
+            } as f64));
+            quic_seq_vec.push(m);
+
+            let mut m = Metric::new();
+            m.set_label(label_pairs!("tile" => &quic.name));
             m.set_gauge(new_gauge(quic_chunk_idx as f64));
             quic_chunk_idx_vec.push(m);
 
-            let mut m = quic.new_metric();
+            let mut m = Metric::new();
+            m.set_label(label_pairs!("tile" => &quic.name));
             m.set_counter(new_counter(quic_tpu_pub_cnt as f64));
             quic_tpu_pub_cnt_vec.push(m);
 
-            let mut m = quic.new_metric();
+            let mut m = Metric::new();
+            m.set_label(label_pairs!("tile" => &quic.name));
             m.set_counter(new_counter(quic_tpu_pub_sz as f64));
             quic_tpu_pub_sz_vec.push(m);
 
-            let mut m = quic.new_metric();
-            m.set_counter(new_counter(unsafe {
-                (*quic.quic).metrics.net_rx_pkt_cnt as f64
-            }));
+            let mut m = Metric::new();
+            m.set_label(label_pairs!("tile" => &quic.name));
+            m.set_counter(new_counter(
+                unsafe { (*quic.quic).metrics.net_rx_pkt_cnt } as f64
+            ));
             quic_net_rx_pkt_cnt_vec.push(m);
 
-            let mut m = quic.new_metric();
-            m.set_counter(new_counter(unsafe {
-                (*quic.quic).metrics.net_tx_pkt_cnt as f64
-            }));
+            let mut m = Metric::new();
+            m.set_label(label_pairs!("tile" => &quic.name));
+            m.set_counter(new_counter(
+                unsafe { (*quic.quic).metrics.net_tx_pkt_cnt } as f64
+            ));
             quic_net_tx_pkt_cnt_vec.push(m);
+
+            let mut m = Metric::new();
+            m.set_label(label_pairs!("tile" => &quic.name));
+            m.set_gauge(new_gauge(
+                unsafe { (*quic.quic).metrics.conn_active_cnt } as f64
+            ));
+            quic_conn_active_cnt_vec.push(m);
+
+            let mut m = Metric::new();
+            m.set_label(label_pairs!("tile" => &quic.name));
+            m.set_counter(new_counter(
+                unsafe { (*quic.quic).metrics.conn_created_cnt } as f64,
+            ));
+            quic_conn_created_cnt_vec.push(m);
+
+            let mut m = Metric::new();
+            m.set_label(label_pairs!("tile" => &quic.name, "reason" => "graceful"));
+            m.set_counter(new_counter(unsafe {
+                (*quic.quic).metrics.conn_closed_cnt as f64
+            }));
+            quic_conn_closed_cnt_vec.push(m);
+
+            let mut m = Metric::new();
+            m.set_label(label_pairs!("tile" => &quic.name, "reason" => "aborted"));
+            m.set_counter(new_counter(unsafe {
+                (*quic.quic).metrics.conn_closed_cnt as f64
+            }));
+            quic_conn_closed_cnt_vec.push(m);
+
+            let mut m = Metric::new();
+            m.set_label(label_pairs!("tile" => &quic.name, "reason" => "no_free_slots"));
+            m.set_counter(new_counter(unsafe {
+                (*quic.quic).metrics.conn_err_no_slots_cnt as f64
+            }));
+            quic_conn_closed_cnt_vec.push(m);
+
+            let mut m = Metric::new();
+            m.set_label(label_pairs!("tile" => &quic.name, "reason" => "tls_fail"));
+            m.set_counter(new_counter(unsafe {
+                (*quic.quic).metrics.conn_err_tls_fail_cnt as f64
+            }));
+            quic_conn_closed_cnt_vec.push(m);
         }
+
+        let mut quic_seq = MetricFamily::new();
+        quic_seq.set_name("firedancer_quic_seq".to_string());
+        quic_seq.set_help("publish mcache sequence number of QUIC tile".to_string());
+        quic_seq.set_field_type(MetricType::GAUGE);
+        quic_seq.set_metric(quic_seq_vec.into());
+        mf.push(quic_seq);
 
         let mut quic_chunk_idx = MetricFamily::new();
         quic_chunk_idx.set_name("firedancer_quic_chunk_idx".to_string());
@@ -207,6 +285,27 @@ impl FrankSpy {
         quic_net_tx_pkt_cnt.set_field_type(MetricType::COUNTER);
         quic_net_tx_pkt_cnt.set_metric(quic_net_tx_pkt_cnt_vec.into());
         mf.push(quic_net_tx_pkt_cnt);
+
+        let mut quic_conn_active_cnt = MetricFamily::new();
+        quic_conn_active_cnt.set_name("firedancer_quic_active_conns".to_string());
+        quic_conn_active_cnt.set_help("Number of active QUIC connections".to_string());
+        quic_conn_active_cnt.set_field_type(MetricType::GAUGE);
+        quic_conn_active_cnt.set_metric(quic_conn_active_cnt_vec.into());
+        mf.push(quic_conn_active_cnt);
+
+        let mut quic_conn_created_cnt = MetricFamily::new();
+        quic_conn_created_cnt.set_name("firedancer_quic_created_conns_total".to_string());
+        quic_conn_created_cnt.set_help("Number of QUIC connections created".to_string());
+        quic_conn_created_cnt.set_field_type(MetricType::COUNTER);
+        quic_conn_created_cnt.set_metric(quic_conn_created_cnt_vec.into());
+        mf.push(quic_conn_created_cnt);
+
+        let mut quic_conn_closed_cnt = MetricFamily::new();
+        quic_conn_closed_cnt.set_name("firedancer_quic_closed_conns_total".to_string());
+        quic_conn_closed_cnt.set_help("Number of QUIC connections closed".to_string());
+        quic_conn_closed_cnt.set_field_type(MetricType::COUNTER);
+        quic_conn_closed_cnt.set_metric(quic_conn_closed_cnt_vec.into());
+        mf.push(quic_conn_closed_cnt);
     }
 }
 
@@ -223,6 +322,7 @@ impl Drop for FrankSpy {
             fd_wksp_pod_unmap(fd_cnc_leave(self.pack_cnc));
             for quic in &self.quic {
                 fd_wksp_pod_unmap(fd_cnc_leave(quic.cnc));
+                fd_wksp_pod_unmap(fd_mcache_leave(quic.mcache));
                 fd_wksp_pod_unmap(quic.quic as *mut c_void);
             }
             for verify in &self.verify {
@@ -240,15 +340,8 @@ pub(crate) struct FrankVerifySpy {
 pub(crate) struct FrankQuicSpy {
     name: String,
     cnc: *mut fd_cnc_t,
+    mcache: *mut fd_frag_meta_t,
     quic: *const fd_quic_t,
-}
-
-impl FrankQuicSpy {
-    fn new_metric(&self) -> Metric {
-        let mut m = Metric::new();
-        m.set_label(label_pairs!("tile" => &self.name).into());
-        m
-    }
 }
 
 fn new_gauge(f: f64) -> Gauge {
