e98b2fc4100101afb47bcb88fe1b01ca237847e3
Kevin J Bowers <kbowers@jumptrading.com>
2023-04-13 13:33:19 -0500

    Installed funk base layer
    Extracted as part of the review of Alex's funk store.
    
    This is the foundational layer of the funk store for managing accounts
    and forks.  More layers to come ...

---
+++ src/funk/fd_funk.h
@@ -0,0 +1,9 @@
+#ifndef HEADER_fd_src_funk_fd_funk_h
+#define HEADER_fd_src_funk_fd_funk_h
+
+#include "fd_funk_base.h" /* Includes ../util/fd_util.h */
+
+/* Note; This is just trivial a wrapper temporarily in anticipation of
+   committing future layers of funk */
+
+#endif /* HEADER_fd_src_funk_fd_funk_h */
---
+++ src/funk/fd_funk_base.c
@@ -0,0 +1,16 @@
+#include "fd_funk_base.h"
+
+char const *
+fd_funk_strerror( int err ) {
+  switch( err ) {
+  case FD_FUNK_SUCCESS:   return "success";
+  case FD_FUNK_ERR_INVAL: return "inval";
+  case FD_FUNK_ERR_KEY:   return "key";
+  case FD_FUNK_ERR_MEM:   return "mem";
+  case FD_FUNK_ERR_TXN:   return "txn";
+  case FD_FUNK_ERR_IO:    return "io";
+  default: break;
+  }
+  return "unknown";
+}
+
