7b64a855713c27f9fa214ff78e5039f7a84cb9f1
Kevin J Bowers <kbowers@jumptrading.com>
2023-04-13 17:10:09 -0500

    Minor cleanup

--- src/funk/fd_funk_base.c
+++ src/funk/fd_funk_base.c
@@ -6,8 +6,8 @@ fd_funk_strerror( int err ) {
   case FD_FUNK_SUCCESS:   return "success";
   case FD_FUNK_ERR_INVAL: return "inval";
   case FD_FUNK_ERR_KEY:   return "key";
-  case FD_FUNK_ERR_MEM:   return "mem";
   case FD_FUNK_ERR_TXN:   return "txn";
+  case FD_FUNK_ERR_MEM:   return "mem";
   case FD_FUNK_ERR_IO:    return "io";
   default: break;
   }
