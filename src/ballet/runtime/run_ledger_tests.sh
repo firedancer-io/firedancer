2934dd86b59c6fff24bd1b0529880ffc431e0ec7
Josh Siegel <83408952+jumpsiegel@users.noreply.github.com>
2023-04-15 09:25:25 -0500

    runtime: add some nice comments

--- src/ballet/runtime/run_ledger_tests.sh
+++ src/ballet/runtime/run_ledger_tests.sh
@@ -6,6 +6,14 @@ if [ ! -e test-ledger-4 ]; then
   wget -q https://github.com/firedancer-io/firedancer-testbins/raw/main/test-ledger-4.tar.gz -O - | tar zxf -
 fi
 
+# We determine these values by
+#  1) Checkout https://github.com/firedancer-io/solana.git
+#  2) switch to the debug branch
+#  3) build using podman (podman build --no-cache -t solana-builder2 -f Dockerfile -v `pwd`:/solana:rw  .)
+#  4) ./target/debug/solana-test-validator --reset
+#         Do stuff
+#  5) grep 'bank frozen:' test-ledger/validator.log | grep 'solana_runtime::bank'
+
 # we could ALWAYS run it with logging except when I run this from the command line, I want less noise...
 
 build/linux/gcc/x86_64/unit-test/test_runtime --ledger test-ledger-4 --db /tmp/funk$$ --cmd replay --accounts test-ledger-4/accounts/ --pages 15 --index-max 120000000 --start-slot 0 --end-slot 25  --confirm_hash AsHedZaZkabNtB8XBiKWQkKwaeLy2y4Hrqm6MkQALT5h --confirm_parent CvgPeR54qpVRZGBuiQztGXecxSXREPfTF8wALujK4WdE --confirm_account_delta 7PL6JZgcNy5vkPSc6JsMHET9dvpvsFMWR734VtCG29xN  --confirm_signature 2  --confirm_last_block G4YL2SieHDGNZGjiwBsJESK7jMDfazg33ievuCwbkjrv
