97c683f7c410d1c6dd68e27089bc38d8d7cccd79
Kevin J Bowers <kbowers@jumptrading.com>
2023-04-15 22:02:27 -0500

    Added low level record support to fd_funk_base.h
    This tweaks some of the error codes, adds the fd_funk_xid_key_pair_t for
    representing (xid,key) pairs and then provides unit test coverage.

--- src/funk/fd_funk_base.h
+++ src/funk/fd_funk_base.h
@@ -48,12 +48,14 @@
    number of error codes used by fd_funk APIs.  These will be negative
    integers. */
 
-#define FD_FUNK_SUCCESS   (0)  /* Success */
-#define FD_FUNK_ERR_INVAL (-1) /* E.g. Failed due to invalid inputs passed by caller */
-#define FD_FUNK_ERR_KEY   (-2) /* E.g. Failed due to record key not found */
-#define FD_FUNK_ERR_TXN   (-3) /* E.g. Failed due to transaction txn not found */
-#define FD_FUNK_ERR_MEM   (-4) /* E.g. Failed due to no suitable wksp memory available */
-#define FD_FUNK_ERR_IO    (-5) /* E.g. Failed due to persistent backing store I/O issue */
+#define FD_FUNK_SUCCESS    (0)  /* Success */
+#define FD_FUNK_ERR_INVAL  (-1) /* Failed due to obviously invalid inputs */
+#define FD_FUNK_ERR_XID    (-2) /* Failed due to transaction id issue (e.g. xid present/absent when it should be absent/present) */
+#define FD_FUNK_ERR_KEY    (-3) /* Failed due to record key issue (e.g. key present/absent when it should be absent/present) */
+#define FD_FUNK_ERR_FROZEN (-4) /* Failed due to frozen issue (e.g. attempt to change records in a frozen transaction) */
+#define FD_FUNK_ERR_TXN    (-5) /* Failed due to transaction map issue (e.g. funk txn_max too small) */
+#define FD_FUNK_ERR_REC    (-6) /* Failed due to record map issue (e.g. funk rec_max too small) */
+#define FD_FUNK_ERR_MEM    (-7) /* Failed due to wksp issue (e.g. wksp too small) */
 
 /* FD_FUNK_REC_KEY_{ALIGN,FOOTPRINT} describe the alignment and
    footprint of a fd_funk_rec_key_t.  ALIGN is a positive integer power
@@ -106,6 +108,24 @@ union __attribute__((aligned(FD_FUNK_TXN_XID_ALIGN))) fd_funk_txn_xid {
 };
 
 typedef union fd_funk_txn_xid fd_funk_txn_xid_t;
+
+/* FD_FUNK_XID_KEY_PAIR_{ALIGN,FOOTPRINT} describe the alignment and
+   footprint of a fd_funk_xid_key_pair_t.  ALIGN is a positive integer
+   power of 2.  FOOTPRINT is a multiple of ALIGN.  These are provided to
+   facilitate compile time declarations. */
+
+#define FD_FUNK_XID_KEY_PAIR_ALIGN     (32UL)
+#define FD_FUNK_XID_KEY_PAIR_FOOTPRINT (96UL)
+
+/* A fd_funk_xid_key_pair_t identifies a funk record.  It is just
+   xid and key packed into the same structure. */
+
+struct fd_funk_xid_key_pair {
+  fd_funk_txn_xid_t xid[1];
+  fd_funk_rec_key_t key[1];
+};
+
+typedef struct fd_funk_xid_key_pair fd_funk_xid_key_pair_t;
 
 /* A fd_funk_t * is an opaque handle of a local join to a funk instance */
 
@@ -215,6 +235,55 @@ fd_funk_txn_xid_set_root( fd_funk_txn_xid_t * x ) {
   ulong * a = x->ul;
   a[0] = 0UL; a[1] = 0UL; a[2] = 0UL; a[3] = 0UL;
   return x;
+}
+
+/* fd_funk_xid_key_pair_hash provides a family of hashes that hash a
+   (xid,key) pair to by p to a uniform quasi-random 64-bit integer.
+   seed selects the particular hash function to use and can be an
+   arbitrary 64-bit value.  Returns the hash.  The hash functions are
+   high quality but not cryptographically secure.  Assumes p is in the
+   caller's address space and valid. */
+
+FD_FN_PURE static inline ulong
+fd_funk_xid_key_pair_hash( fd_funk_xid_key_pair_t const * p,
+                           ulong                          seed ) {
+  return fd_funk_txn_xid_hash( p->xid, seed ) ^ fd_funk_rec_key_hash( p->key, seed );
+}
+
+/* fd_funk_xid_key_pair_eq returns 1 if (xid,key) pair pointed to by pa
+   and pb are equal and 0 otherwise.  Assumes pa and pb are in the
+   caller's address space and valid. */
+
+FD_FN_PURE static inline int
+fd_funk_xid_key_pair_eq( fd_funk_xid_key_pair_t const * pa,
+                         fd_funk_xid_key_pair_t const * pb ) {
+  return fd_funk_txn_xid_eq( pa->xid, pb->xid ) & fd_funk_rec_key_eq( pa->key, pb->key );
+}
+
+/* fd_funk_xid_key_pair_copy copies the (xid,key) pair pointed to by ps
+   into the (xid,key) pair to by pd and returns pd.  Assumes pd and ps
+   are in the caller's address space and valid. */
+
+static inline fd_funk_xid_key_pair_t *
+fd_funk_xid_key_pair_copy( fd_funk_xid_key_pair_t *       pd,
+                           fd_funk_xid_key_pair_t const * ps ) {
+  fd_funk_txn_xid_copy( pd->xid, ps->xid );
+  fd_funk_rec_key_copy( pd->key, ps->key );
+  return pd;
+}
+
+/* fd_funk_xid_key_pair_init set the (xid,key) pair p to pair formed
+   from the transaction id pointed to by x and the record key pointed to
+   by k.  Assumes p, x and k are in the caller's address space and
+   valid. */
+
+static inline fd_funk_xid_key_pair_t *
+fd_funk_xid_key_pair_init( fd_funk_xid_key_pair_t *  p,
+                           fd_funk_txn_xid_t const * x,
+                           fd_funk_rec_key_t const * k ) {
+  fd_funk_txn_xid_copy( p->xid, x );
+  fd_funk_rec_key_copy( p->key, k );
+  return p;
 }
 
 /* fd_funk_strerror converts an FD_FUNK_SUCCESS / FD_FUNK_ERR_* code
