8defbc26d6d9e8cefca82e1e67b6735c06819492
Alber <arblugo@gmail.com>
2023-05-09 00:19:27 +0100

    chore: add comment on header file

--- src/ballet/chacha20/fd_chacha20_rng.h
+++ src/ballet/chacha20/fd_chacha20_rng.h
@@ -6,6 +6,10 @@ typedef uint32_t fd_chacha20_rng_t;
 #define FD_CHACHA20_KEY_SIZE 32   // 32 bytes, 256 bits
 #define FD_CHACHA20_NONCE_SIZE 12 // 12 bytes, 96 bits
 
+/*
+  This method generates a cryptographically secure random number, it expects a key (32 bytes) and nonce (12 bytes)
+  Note this random number will be stored on a buffer passed as 'random_number' param (fd_chacha20_rng_t)
+*/
 int fd_chacha20_generate_random_number(const unsigned char *key, const unsigned char *nonce, fd_chacha20_rng_t *random_number);
 
 #endif /* HEADER_fd_src_ballet_chacha20_fd_chacha20_h */
