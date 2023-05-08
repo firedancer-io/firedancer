50bd485d7ef8dd12f6d9c382d5ba5cc6d5150daf
Alber <arblugo@gmail.com>
2023-05-08 22:53:00 +0100

    refactor: add fd_chacha20_generate_random_number

--- src/ballet/chacha20/fd_chacha20_rng.c
+++ src/ballet/chacha20/fd_chacha20_rng.c
@@ -1,7 +1,7 @@
 #include <openssl/evp.h>
 #include "fd_chacha20_rng.h"
 
-int fd_chacha20_rng_init(EVP_CIPHER_CTX *ctx, unsigned char *key, unsigned char *nonce)
+int fd_chacha20_rng_init(EVP_CIPHER_CTX *ctx, const unsigned char *key, const unsigned char *nonce)
 {
   if (ctx == NULL)
   {
@@ -19,20 +19,48 @@ int fd_chacha20_rng_init(EVP_CIPHER_CTX *ctx, unsigned char *key, unsigned char 
   return 0;
 }
 
-int fd_chacha20_generate_random_number(EVP_CIPHER_CTX *ctx, fd_chacha20_rng_t *num)
+int fd_chacha20_rng_generate(EVP_CIPHER_CTX *ctx, fd_chacha20_rng_t *random_number)
 {
   unsigned char buf[4]; // 4 bytes -> 32 bits
   int outlen;
 
   // Generate a random 32-bit number
-  // we pass NULL as plaintext to generate random numbers solely based on chacha20 internal state
+  // We pass NULL as plaintext to generate random numbers solely based on chacha20 internal state
   if (!EVP_EncryptUpdate(ctx, buf, &outlen, NULL, 0))
   {
     fprintf(stderr, "Error: EVP_EncryptUpdate()\n");
     return 1;
   }
 
-  *num = *((unsigned int *)buf);
+  *random_number = *((fd_chacha20_rng_t *)buf);
+
+  return 0;
+}
+
+int fd_chacha20_generate_random_number(const unsigned char *key, const unsigned char *nonce, fd_chacha20_rng_t *random_number)
+{
+  // Create the ChaCha20 context
+  EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+
+  // Set chacha20 cipher context using key and nonce
+  if (!fd_chacha20_rng_init(*ctx, *key, *nonce))
+  {
+    fprintf(stderr, "Error: fd_chacha20_rng_init()\n");
+    return 1;
+  }
+
+  // Generates random number using chacha20 cipher context
+  if (!fd_chacha20_rng_generate(*ctx, *random_number))
+  {
+    fprintf(stderr, "Error: fd_chacha20_rng_generate()\n");
+    return 1;
+  }
+
+  // Print random number generated
+  printf("Random number generated: %u\n", random_number);
+
+  // Free cipher context from memory
+  EVP_CIPHER_CTX_free(ctx);
 
   return 0;
 }
--- src/ballet/chacha20/fd_chacha20_rng.h
+++ src/ballet/chacha20/fd_chacha20_rng.h
@@ -6,7 +6,8 @@ typedef uint32_t fd_chacha20_rng_t;
 #define FD_CHACHA20_KEY_SIZE 32   // 32 bytes, 256 bits
 #define FD_CHACHA20_NONCE_SIZE 12 // 12 bytes, 96 bits
 
-int fd_chacha20_rng_init(EVP_CIPHER_CTX *ctx, unsigned char *key, unsigned char *nonce);
-int fd_chacha20_generate_random_number(EVP_CIPHER_CTX *ctx, fd_chacha20_rng_t *num);
+int fd_chacha20_rng_init(EVP_CIPHER_CTX *ctx, const unsigned char *key, const unsigned char *nonce);
+int fd_chacha20_rng_generate(EVP_CIPHER_CTX *ctx, fd_chacha20_rng_t *random_number);
+int fd_chacha20_generate_random_number(const unsigned char *key, const unsigned char *nonce, fd_chacha20_rng_t *random_number);
 
 #endif /* HEADER_fd_src_ballet_chacha20_fd_chacha20_h */
