190c7919711a780756da2d21f84ef96ff6c53e23
Richard Patel <ripatel@jumptrading.com>
2023-05-10 16:47:54 +0000

    sbpf: minor fixes

--- src/ballet/sbpf/Local.mk
+++ src/ballet/sbpf/Local.mk
@@ -1,4 +1,4 @@
-$(call add-hdrs,fd_sbpf_loader.h)
+$(call add-hdrs,fd_sbpf_instr.h fd_sbpf_loader.h fd_sbpf_opcodes.h)
 $(call add-objs,fd_sbpf_loader,fd_ballet)
 $(call make-unit-test,test_sbpf_load_prog,test_sbpf_load_prog,fd_ballet fd_util)
 $(call make-fuzz-test,fuzz_sbpf_loader,fuzz_sbpf_loader,fd_ballet fd_util)
