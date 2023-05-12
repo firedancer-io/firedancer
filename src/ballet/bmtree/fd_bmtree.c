6e7324d88f9b9bdbceb86e9cfa79433e5346b5cc
Philip Taffet <phtaffet@jumptrading.com>
2023-05-11 22:45:11 -0500

    bmtree: Add Merkle proof generation, validation

--- src/ballet/bmtree/fd_bmtree.c
+++ src/ballet/bmtree/fd_bmtree.c
@@ -144,10 +144,39 @@ fd_bmtree_depth( ulong leaf_cnt ) {
   return fd_ulong_if( leaf_cnt<=1UL, leaf_cnt, (ulong)fd_ulong_find_msb( leaf_cnt-1UL ) + 2UL );
 }
 
+FD_FN_CONST ulong
+fd_bmtree_node_cnt( ulong leaf_cnt ) {
+  /* Compute the number of nodes in a tree with inclusion_proof_leaf_cnt
+     leaves. Based on the proposition that layer l having N_l nodes
+     implies the above layer has floor((N_l+1)/2) nodes, we know that
+     the kth layer above has floor(((N_l+2^(k-1)+2^(k-2)+...+1)/2^k)
+     nodes, which is floor((N_l+2^k - 1)/2^k) = 1+floor((N_l-1)/2^k)
+     nodes.  We stop when we get to 1 node though.  It seems like there
+     should be a bit-twiddling way to calculate this faster, especially
+     given that you can go all the way to 64 and correct with a value
+     that comes from the MSB, but I couldn't find it.  */
+  if( FD_UNLIKELY( leaf_cnt==0UL ) ) return 0UL;
+  ulong cnt = 0UL;
+  leaf_cnt--;
+  for( int i=0; i<64; i++ ) {
+    ulong term = leaf_cnt>>i;
+    cnt += term;
+  }
+  cnt += (ulong)(2+fd_ulong_find_msb_w_default(leaf_cnt, -1));
+  return cnt;
+}
+
 /* bmtree_commit_{footprint,align} return the alignment and footprint
    required for a memory region to be used as a bmtree_commit_t. */
 FD_FN_CONST ulong fd_bmtree_commit_align    ( void ) { return alignof(fd_bmtree_commit_t); }
-FD_FN_CONST ulong fd_bmtree_commit_footprint( void ) { return sizeof (fd_bmtree_commit_t); }
+
+FD_FN_CONST ulong
+fd_bmtree_commit_footprint( ulong inclusion_proof_layer_cnt ) {
+  /* A complete binary tree with n layers has (2^n)-1 nodes.  We keep 1
+     extra bmtree_node_t (included in sizeof(fd_bmtree_commit_t) to
+     avoid branches when appending commits. */
+  return sizeof(fd_bmtree_commit_t) + ((1UL<<inclusion_proof_layer_cnt)-1UL)*sizeof(fd_bmtree_node_t);
+}
 
 
 /* bmtree_commit_init starts a vector commitment calculation */
@@ -155,11 +184,11 @@ FD_FN_CONST ulong fd_bmtree_commit_footprint( void ) { return sizeof (fd_bmtree_
 fd_bmtree_commit_t *    /* Returns mem as a bmtree_commit_t *, commit will be in a calc */
 fd_bmtree_commit_init( void * mem,     /* Assumed unused with required alignment and footprint */
                        ulong hash_sz,
-                       ulong inclusion_proof_sz ) {
+                       ulong inclusion_proof_layer_cnt ) {
   fd_bmtree_commit_t * state = (fd_bmtree_commit_t *) mem;
   state->leaf_cnt = 0UL;
   state->hash_sz = hash_sz;
-  state->inclusion_proof_sz = inclusion_proof_sz;
+  state->inclusion_proof_sz = (1UL<<inclusion_proof_layer_cnt) - 1UL;
   return state;
 }
 
@@ -170,9 +199,9 @@ fd_bmtree_commit_init( void * mem,     /* Assumed unused with required alignment
 
 fd_bmtree_commit_t *                                            /* Returns state */
 fd_bmtree_commit_append( fd_bmtree_commit_t *                 state,           /* Assumed valid and in a calc */
-                        fd_bmtree_node_t const * FD_RESTRICT new_leaf,        /* Indexed [0,new_leaf_cnt) */
-                        ulong                               new_leaf_cnt ) {
-  ulong                         leaf_cnt = state->leaf_cnt;
+                         fd_bmtree_node_t const * FD_RESTRICT new_leaf,        /* Indexed [0,new_leaf_cnt) */
+                         ulong                                new_leaf_cnt ) {
+  ulong                          leaf_cnt = state->leaf_cnt;
   fd_bmtree_node_t * FD_RESTRICT node_buf = state->node_buf;
 
   for( ulong new_leaf_idx=0UL; new_leaf_idx<new_leaf_cnt; new_leaf_idx++ ) {
@@ -198,11 +227,13 @@ fd_bmtree_commit_append( fd_bmtree_commit_t *                 state,           /
        Each iteration, merges that pair of nodes into a new branch node.
        Terminates if the new branch node is the left node of a pair. */
 
-    ulong layer  = 0UL;         /* `layer` starts at 0 (leaf nodes) and increments each iteration. */
-    ulong cursor = ++leaf_cnt;  /* `cursor` is the number of known nodes in the current layer. */
-    while( !(cursor & 1UL) ) {  /* Continue while the right node in the last pair is available. */
+    ulong layer   = 0UL;           /* `layer` starts at 0 (leaf nodes) and increments each iteration. */
+    ulong inc_idx = 2UL*leaf_cnt;  /* `inc_idx` is the index of the current node in the inclusion proof array */
+    ulong cursor  = ++leaf_cnt;    /* `cursor` is the number of known nodes in the current layer. */
+    while( !(cursor & 1UL) ) {     /* Continue while the right node in the last pair is available. */
+      state->inclusion_proofs[ fd_ulong_min( inc_idx, state->inclusion_proof_sz ) ] = *tmp;
       fd_bmtree_private_merge( tmp, node_buf + layer, tmp, state->hash_sz );
-      layer++; cursor>>=1;      /* Move up one layer. */
+      inc_idx -= 1UL<<layer; layer++; cursor>>=1;      /* Move up one layer. */
     }
 
     /* Note on correctness of the above loop: The termination condition
@@ -215,6 +246,7 @@ fd_bmtree_commit_append( fd_bmtree_commit_t *                 state,           /
        instead of copying at end? (Probably a wash.) */
 
     node_buf[ layer ] = *tmp;
+    state->inclusion_proofs[ fd_ulong_min( inc_idx, state->inclusion_proof_sz ) ] = *tmp;
   }
 
   state->leaf_cnt = leaf_cnt;
@@ -243,7 +275,8 @@ fd_bmtree_commit_fini( fd_bmtree_commit_t * state ) {
 
     /* Start at the first layer where number of nodes is odd. */
     ulong layer     = (ulong)fd_ulong_find_lsb( leaf_cnt );
-    ulong layer_cnt = leaf_cnt >> layer;
+    ulong layer_cnt = leaf_cnt >> layer; /* number of nodes in this layer */
+    ulong inc_idx   = (layer_cnt<<(layer+1UL)) - (1UL<<layer) - 1UL;
 
     /* Allocate temporary node. */
     fd_bmtree_node_t tmp[1];
@@ -253,10 +286,13 @@ fd_bmtree_commit_fini( fd_bmtree_commit_t * state ) {
        along the way.  We use the fd_ulong_if to encourage inlining of
        merge and unnecessary branch elimination by cmov. */
     while( layer_cnt>1UL ) {
-      fd_bmtree_node_t const * tmp2 = (fd_bmtree_node_t const *)
-        fd_ulong_if( layer_cnt & 1UL, (ulong)tmp /* 1 child */, (ulong)(node_buf+layer) /* 2 children */ ); /* cmov */
+      fd_bmtree_node_t const * tmp2 = fd_ptr_if( layer_cnt & 1UL, &tmp[0] /* 1 child */, node_buf+layer /* 2 children */ ); /* cmov */
       fd_bmtree_private_merge( tmp, tmp2, tmp, state->hash_sz );
+
       layer++; layer_cnt = (layer_cnt+1UL) >> 1;
+
+      inc_idx   = (layer_cnt<<(layer+1UL)) - (1UL<<layer) - 1UL;
+      state->inclusion_proofs[ fd_ulong_min( inc_idx, state->inclusion_proof_sz ) ] = *tmp;
     }
 
     /* Fix up root node. */
@@ -266,3 +302,60 @@ fd_bmtree_commit_fini( fd_bmtree_commit_t * state ) {
   return root->hash;
 }
 
+int
+fd_bmtree_get_inclusion_proof( fd_bmtree_commit_t * state,
+                               uchar *              dest,
+                               ulong                leaf_idx ) {
+
+  ulong leaf_cnt = state->leaf_cnt;
+  ulong hash_sz  = state->hash_sz;
+
+  if( FD_UNLIKELY( leaf_idx >= leaf_cnt ) ) return 0UL;
+
+  ulong inc_idx   = leaf_idx * 2UL;
+  ulong layer     = 0UL;
+  ulong layer_cnt = state->leaf_cnt;
+
+  while( layer_cnt>1UL ) {
+    ulong sibling_idx = inc_idx ^ (1UL<<(layer+1UL));
+    ulong max_idx_for_layer = fd_ulong_insert_lsb( (leaf_cnt - 1UL)<<1, 1+(int)layer, (1UL<<layer)-1UL );
+    sibling_idx = fd_ulong_if( sibling_idx>max_idx_for_layer, inc_idx /* Double link */, sibling_idx );
+
+    if( FD_UNLIKELY( sibling_idx>=state->inclusion_proof_sz ) ) return -1;
+    fd_memcpy( dest + layer*hash_sz, state->inclusion_proofs + sibling_idx, hash_sz );
+
+    layer++; layer_cnt = (layer_cnt+1UL)>>1;
+    inc_idx = fd_ulong_insert_lsb( inc_idx, (int)layer+1, (1UL<<layer)-1UL );
+  }
+
+  return (int)layer;
+}
+
+fd_bmtree_node_t *
+fd_bmtree_validate_inclusion_proof( fd_bmtree_node_t const * leaf,
+                                    ulong                    leaf_idx,
+                                    fd_bmtree_node_t *       root,
+                                    uchar const *            proof,
+                                    ulong                    proof_depth,
+                                    ulong                    hash_sz ) {
+  fd_bmtree_node_t tmp[2]; /* 0 stores the generated node, 1 stores the node from the proof */
+  fd_bmtree_node_t * tmp_l;
+  fd_bmtree_node_t * tmp_r;
+
+  tmp[0] = *leaf;
+
+  if( FD_UNLIKELY( proof_depth < fd_bmtree_depth( leaf_idx+1UL )-1UL ) ) return NULL;
+
+  ulong inc_idx   = leaf_idx * 2UL;
+  for( ulong layer=0UL; layer<proof_depth; layer++ ) {
+    fd_memcpy( tmp+1, proof + layer*hash_sz, hash_sz );
+
+    tmp_l = fd_ptr_if( 0UL==(inc_idx & (1UL<<(layer+1UL))), tmp+0, tmp+1 );
+    tmp_r = fd_ptr_if( 0UL==(inc_idx & (1UL<<(layer+1UL))), tmp+1, tmp+0 );
+
+    fd_bmtree_private_merge( tmp, tmp_l, tmp_r, hash_sz );
+
+    inc_idx = fd_ulong_insert_lsb( inc_idx, (int)layer+2, (2UL<<layer)-1UL );
+  }
+  return fd_memcpy( root, tmp, 32UL );
+}
