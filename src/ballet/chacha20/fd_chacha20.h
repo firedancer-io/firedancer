dd9149b1bd6b4ff8e0cd64bfacdd8b39b3b8ece4
Alber <arblugo@gmail.com>
2023-05-15 10:48:52 +0100

    feat: rm c implementation & update header

--- src/ballet/chacha20/fd_chacha20.h
+++ src/ballet/chacha20/fd_chacha20.h
@@ -6,16 +6,11 @@
 typedef uint32_t fd_chacha20_rng_t;
 
 #define FD_CHACHA20_KEY_SIZE 32 // 32 bytes, 256 bits
-/*
-  FD_CHACHA20_NONCE_SIZE emulates nonce lenght from rand_chacha rust crate.
-  Source: https://docs.rs/rand_chacha/latest/x86_64-apple-darwin/src/rand_chacha/chacha.rs.html#104
-*/
-#define FD_CHACHA20_NONCE_SIZE 8 // 8 bytes, 64 bits
 
 /*
   This method generates a cryptographically secure random number, it expects a key (32 bytes) and nonce (12 bytes)
   Note this random number will be stored on a buffer passed as 'random_number' param (fd_chacha20_rng_t)
 */
-void fd_chacha20_generate_random_number(const uchar *key, const uchar *nonce, fd_chacha20_rng_t *random_number);
+void fd_chacha20_generate_random_number(const uchar *key, fd_chacha20_rng_t *random_number);
 
 #endif /* HEADER_fd_src_ballet_chacha20_fd_chacha20_h */
