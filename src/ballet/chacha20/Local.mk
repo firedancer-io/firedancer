432fd67262e41cc405849be4807c61c31a5228bc
Alber <arblugo@gmail.com>
2023-05-15 14:07:45 +0100

    wip: update Local.mk

--- src/ballet/chacha20/Local.mk
+++ src/ballet/chacha20/Local.mk
@@ -1,5 +1,4 @@
 $(call add-hdrs,fd_chacha20.h)
-$(call add-objs,fd_chacha20_rng)
 
-$(call make-unit-test,test_fd_chacha20_rng,test_fd_chacha20_rng,fd_ballet fd_util)
-$(call run-unit-test,test_fd_chacha20_rng)
+$(call make-unit-test,test_fd_chacha20,test_fd_chacha20,fd_ballet fd_util)
+$(call run-unit-test,test_fd_chacha20)
