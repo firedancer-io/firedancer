fbdb674fa7c31246447581ca10c76b7bc82ebc51
Alber <arblugo@gmail.com>
2023-05-15 10:44:36 +0100

    feat: update header return type

--- src/ballet/chacha20/fd_chacha20.h
+++ src/ballet/chacha20/fd_chacha20.h
@@ -16,6 +16,6 @@ typedef uint32_t fd_chacha20_rng_t;
   This method generates a cryptographically secure random number, it expects a key (32 bytes) and nonce (12 bytes)
   Note this random number will be stored on a buffer passed as 'random_number' param (fd_chacha20_rng_t)
 */
-int fd_chacha20_generate_random_number(const uchar *key, const uchar *nonce, fd_chacha20_rng_t *random_number);
+void fd_chacha20_generate_random_number(const uchar *key, const uchar *nonce, fd_chacha20_rng_t *random_number);
 
 #endif /* HEADER_fd_src_ballet_chacha20_fd_chacha20_h */
