23f1b95c5199b6f0a8a8091457d9f22dd11b38a6
Charles Li <charles.dt.li@gmail.com>
2023-05-16 16:56:43 +0000

    refactor: retry token serde

--- src/tango/quic/tests/test_quic_retry_unit.c
+++ src/tango/quic/tests/test_quic_retry_unit.c
@@ -12,34 +12,32 @@
 
 void test_token_encrypt_decrypt()
 {
-  uchar orig_dst_conn_id[FD_QUIC_MAX_CONN_ID_SZ] = "\x42";
+  ulong orig_dst_conn_id = 0x2a2a2a2a2a2a2a2a;
   ulong retry_src_conn_id = 0x2a2a2a2a2a2a2a2a;
   fd_quic_net_endpoint_t client = {.ip_addr = 42, .udp_port = 42};
-  uchar retry_token[FD_QUIC_RETRY_TOKEN_SZ];
+  uchar token[FD_QUIC_RETRY_TOKEN_SZ];
 
-  fd_quic_retry_token_encrypt(
-      orig_dst_conn_id,
-      retry_src_conn_id,
-      client.ip_addr,
-      client.udp_port,
-      retry_token);
+  fd_quic_retry_token_encrypt(orig_dst_conn_id,
+                              retry_src_conn_id,
+                              client.ip_addr,
+                              client.udp_port,
+                              token);
 
-  uchar orig_dst_conn_id_decrypt[FD_QUIC_MAX_CONN_ID_SZ];
-  long retry_token_ts_decrypt;
-  fd_quic_retry_token_decrypt(
-      retry_token,
-      retry_src_conn_id,
-      client.ip_addr,
-      client.udp_port,
-      orig_dst_conn_id_decrypt,
-      &retry_token_ts_decrypt);
+  ulong orig_dst_conn_id_decrypt;
+  long ts_nanos_decrypt;
+  fd_quic_retry_token_decrypt(retry_src_conn_id,
+                              client.ip_addr,
+                              client.udp_port,
+                              token,
+                              &orig_dst_conn_id_decrypt,
+                              &ts_nanos_decrypt);
 
   FD_TEST(orig_dst_conn_id == orig_dst_conn_id_decrypt);
 }
 
 void test_token_invalid_length() {}
 
-/* Invariant: a valid retry token should always decrypt to the original inputs. */
+/* Invariant: a valid token should always decrypt to the original inputs. */
 void test_property_token_encrypt_decrypt() {}
 
 /* Invariant: invalid-length tokens should always return an error. */
