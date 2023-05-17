9a8d8767abd34304e105efd756f431f2226c96ed
Charles Li <charles.dt.li@gmail.com>
2023-05-17 14:40:43 +0000

    chore: delete renamed file

--- src/tango/quic/tests/Local.mk
+++ src/tango/quic/tests/Local.mk
@@ -17,7 +17,7 @@ $(call make-unit-test,test_quic_tls_pcap,test_tls_pcap,fd_quic fd_ballet fd_util
 $(call make-unit-test,test_quic_tls_pcap2,test_tls_pcap2,fd_quic fd_ballet fd_util)
 $(call make-unit-test,test_quic_tls_both,test_tls_quic_both,fd_quic fd_util)
 $(call make-unit-test,test_quic_flow_control,test_quic_flow_control,fd_aio fd_quic fd_ballet fd_tango fd_util)
-$(call make-unit-test,test_quic_retry,test_quic_retry,fd_aio fd_quic fd_ballet fd_tango fd_util)
+$(call make-unit-test,test_quic_retry_unit,test_quic_retry_unit,fd_aio fd_quic fd_ballet fd_tango fd_util)
 
 $(call run-unit-test,test_quic_crypto)
 $(call run-unit-test,test_quic_frames)
