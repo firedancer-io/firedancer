5a4ea1f44515fe11e679f9ef6bba3ec585b39e0a
Charles Li <charles.dt.li@gmail.com>
2023-05-17 18:36:06 +0000

    fix: 20 byte orig dst conn id

--- src/tango/quic/tests/test_quic_retry_unit.c
+++ src/tango/quic/tests/test_quic_retry_unit.c
@@ -8,11 +8,13 @@
 #include <openssl/ssl.h>
 
 #include "../fd_quic.h"
+#include "fd_quic_test_helpers.h"
+#include "fd_quic_test_helpers.h"
 #include "../crypto/fd_quic_crypto_suites.h"
 
-void test_token_encrypt_decrypt()
+void test_retry_token_encrypt_decrypt()
 {
-  uchar orig_dst_conn_id[FD_QUIC_MAX_CONN_ID_SZ] = "\x42";
+  uchar orig_dst_conn_id[FD_QUIC_MAX_CONN_ID_SZ] = {42};
   ulong retry_src_conn_id = 0x2a2a2a2a2a2a2a2a;
   fd_quic_net_endpoint_t client = {.ip_addr = 42, .udp_port = 42};
   uchar retry_token[FD_QUIC_RETRY_TOKEN_SZ];
@@ -24,29 +26,54 @@ void test_token_encrypt_decrypt()
       client.udp_port,
       retry_token);
 
-  uchar orig_dst_conn_id_decrypt[FD_QUIC_MAX_CONN_ID_SZ];
-  long retry_token_ts_decrypt;
+  uchar *orig_dst_conn_id_decrypt;
+  long retry_token_issued_decrypt;
   fd_quic_retry_token_decrypt(
       retry_token,
       retry_src_conn_id,
       client.ip_addr,
       client.udp_port,
-      orig_dst_conn_id_decrypt,
-      &retry_token_ts_decrypt);
+      &orig_dst_conn_id_decrypt,
+      &retry_token_issued_decrypt);
+
+  int same = 1;
+  for (int i = 0; i < FD_QUIC_MAX_CONN_ID_SZ; i++) {
+    same &= (orig_dst_conn_id[i] == orig_dst_conn_id_decrypt[i]);
+  }
+}
+
+void test_retry_integrity_tag_encrypt_decrypt()
+{
+  uchar orig_dst_conn_id[FD_QUIC_MAX_CONN_ID_SZ] = {42};
+  ulong retry_src_conn_id = 0x2a2a2a2a2a2a2a2a;
+  fd_quic_net_endpoint_t client = {.ip_addr = 42, .udp_port = 42};
+  uchar retry_token[FD_QUIC_RETRY_TOKEN_SZ];
+
+  fd_quic_retry_token_encrypt(orig_dst_conn_id,
+                              retry_src_conn_id,
+                              client.ip_addr,
+                              client.udp_port,
+                              retry_token);
+
+  uchar *orig_dst_conn_id_decrypt;
+  long ts_nanos_decrypt;
+  fd_quic_retry_token_decrypt(retry_token,
+                              retry_src_conn_id,
+                              client.ip_addr,
+                              client.udp_port,
+                              &orig_dst_conn_id_decrypt,
+                              &ts_nanos_decrypt);
 
   FD_TEST(orig_dst_conn_id == orig_dst_conn_id_decrypt);
 }
 
-void test_token_invalid_length() {}
+void test_retry_token_invalid_length() {}
 
-/* Invariant: a valid retry token should always decrypt to the original inputs. */
-void test_property_token_encrypt_decrypt() {}
-
-/* Invariant: invalid-length tokens should always return an error. */
-void test_property_token_invalid_length_decrypt() {}
+/* Invariant: a valid token should always decrypt to the original inputs. */
+void test_property_retry_token_encrypt_decrypt() {}
 
 /* Invariant: invalid-length tokens should always return an error. */
-void test_integrity_tag_encrypt_decrypt() {}
+void test_property_retry_token_invalid_length_decrypt() {}
 
 int main(int argc,
          char **argv)
@@ -56,7 +83,7 @@ int main(int argc,
   if (FD_UNLIKELY(argc > 1))
     FD_LOG_ERR(("unrecognized argument: %s", argv[1]));
 
-  test_token_encrypt_decrypt();
+  test_retry_token_encrypt_decrypt();
 
   FD_LOG_NOTICE(("pass"));
   fd_halt();
