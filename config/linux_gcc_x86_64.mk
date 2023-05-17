5c987a2134370396eab1dae71dc9daef2cd948cd
Charles Li <charles.dt.li@gmail.com>
2023-05-17 18:32:48 +0000

    chore: disable SHA-256 core impl

--- config/linux_gcc_x86_64.mk
+++ config/linux_gcc_x86_64.mk
@@ -10,7 +10,7 @@ include config/with-openssl.mk
 
 CPPFLAGS+=-fomit-frame-pointer -falign-functions=32 -falign-jumps=32 -falign-labels=32 -falign-loops=32 \
           -march=haswell -mtune=skylake -mfpmath=sse -mbranch-cost=5 \
-	  -DFD_HAS_INT128=1 -DFD_HAS_DOUBLE=1 -DFD_HAS_ALLOCA=1 -DFD_HAS_X86=1 -DFD_HAS_SSE=1 -DFD_HAS_AVX=1
+	  -DFD_HAS_INT128=1 -DFD_HAS_DOUBLE=1 -DFD_HAS_ALLOCA=1 -DFD_HAS_X86=1 -DFD_HAS_SSE=1 -DFD_HAS_AVX=1 -DFD_SHA256_CORE_IMPL=0
 
 FD_HAS_INT128:=1
 FD_HAS_DOUBLE:=1
@@ -18,4 +18,3 @@ FD_HAS_ALLOCA:=1
 FD_HAS_X86:=1
 FD_HAS_SSE:=1
 FD_HAS_AVX:=1
-
