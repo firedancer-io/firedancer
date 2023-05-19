cd68368a243e9ee1f3d13f22f214cb87aebcd753
Josh Siegel <jsiegel@jumptrading.com>
2023-05-19 18:17:23 +0000

    runtime: Fix deshredder for newer blocks

--- src/ballet/shred/test_deshredder.c
+++ src/ballet/shred/test_deshredder.c
@@ -7,6 +7,8 @@
 FD_IMPORT_BINARY( localnet_shreds_0,  "src/ballet/shred/fixtures/localnet-slot0-shreds.ar"  );
 FD_IMPORT_BINARY( localnet_batch_0_0, "src/ballet/shred/fixtures/localnet-slot0-batch0.bin" );
 
+FD_IMPORT_BINARY( localnet_v14_shreds_0,  "src/ballet/shred/fixtures/localnet-v14-slot0-shreds.ar"  );
+
 int
 main( int     argc,
       char ** argv ) {
@@ -29,7 +31,7 @@ main( int     argc,
   fd_ar_meta_t hdr;
   int ar_err;
   while( (ar_err = fd_ar_read_next( file, &hdr ))==0 ) {
-    uchar shred_buf[ FD_SHRED_SZ ];
+    uchar shred_buf[ FD_SHRED_MAX_SZ ];
 
     /* Read next file from archive */
     FD_TEST(( hdr.filesz>=0 && hdr.filesz < (long)sizeof(shred_buf) ));
@@ -63,6 +65,35 @@ main( int     argc,
 
   /* Verify deshredded content */
   FD_TEST( 0==memcmp( batch, localnet_batch_0_0, localnet_batch_0_0_sz ) );
+
+  file = fmemopen( (void *)localnet_v14_shreds_0, localnet_v14_shreds_0_sz, "r" );
+  FD_TEST( file );
+  FD_TEST( 0==fd_ar_read_init( file ) );
+
+  fd_deshredder_init( &deshred, &batch, sizeof(batch), NULL, 0 );
+
+  while( (ar_err = fd_ar_read_next( file, &hdr ))==0 ) {
+    uchar shred_buf[ FD_SHRED_MAX_SZ ];
+
+    /* Read next file from archive */
+    FD_TEST(( hdr.filesz>=0 && hdr.filesz < (long)sizeof(shred_buf) ));
+
+    size_t n = fread( shred_buf, 1, (ulong)hdr.filesz, file );
+    FD_TEST(( n==(ulong)hdr.filesz ));
+
+    /* Parse shred */
+    fd_shred_t const * shred = fd_shred_parse( shred_buf, (ulong)hdr.filesz );
+
+    /* Refill deshredder with shred */
+    fd_shred_t const * const shred_list[1] = { shred };
+    deshred.shreds    = shred_list;
+    deshred.shred_cnt = 1U;
+
+    fd_deshredder_next( &deshred );
+  }
+
+  /* Did we gracefully finish consuming the archive? */
+  FD_TEST( ar_err==ENOENT );
 
   FD_LOG_NOTICE(( "pass" ));
   fd_halt();
