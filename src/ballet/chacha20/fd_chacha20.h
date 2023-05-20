8abe8709f1f13a75a0eb9013e333db5de1a3647d
Alber <arblugo@gmail.com>
2023-05-20 12:12:29 +0100

    feat: update headers (with both ffi & c impl)

--- src/ballet/chacha20/fd_chacha20.h
+++ src/ballet/chacha20/fd_chacha20.h
@@ -8,9 +8,16 @@ typedef uint32_t fd_chacha20_rng_t;
 #define FD_CHACHA20_KEY_SIZE 32 // 32 bytes, 256 bits
 
 /*
-  This method generates a cryptographically secure random number, it expects a key (32 bytes) param, as well as a buffer
+  This method generates a cryptographically secure random number.
+  It expects a key (32 bytes) param, as well as a buffer
   to store the generated number, passed as 'random_number' param (fd_chacha20_rng_t)
 */
-void fd_chacha20_generate_random_number(const uchar *key, fd_chacha20_rng_t *random_number);
+int fd_chacha20_generate_random_number(const uchar *key, fd_chacha20_rng_t *random_number);
+
+/*
+  This method implements a FFI that generates a cryptographically secure random number using the target rust crate.
+  it also expects a key (32 bytes) param, as well as a buffer to store the generated number, passed as 'random_number' param (fd_chacha20_rng_t)
+*/
+void fd_chacha20_ffi_random_number(const uchar *key, fd_chacha20_rng_t *random_number);
 
 #endif /* HEADER_fd_src_ballet_chacha20_fd_chacha20_h */
