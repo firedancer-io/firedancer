ca1647896592eb3331d67a29a0d6e3d414ec7531
Alber <arblugo@gmail.com>
2023-05-20 12:11:30 +0100

    feat: update rust ffi function name

--- src/ballet/chacha20/rust-lib/src/lib.rs
+++ src/ballet/chacha20/rust-lib/src/lib.rs
@@ -2,14 +2,21 @@ use rand::Rng;
 use rand_chacha::{rand_core::SeedableRng, ChaCha20Rng};
 
 #[no_mangle]
-pub extern "C" fn fd_chacha20_generate_random_number(
-    key: &[u8; 32],
-    random_number_buffer: &mut u32,
-) {
+pub extern "C" fn fd_chacha20_ffi_random_number(key: &[u8; 32], random_number_buffer: &mut u32) {
     // Create the RNG with the seed (key), nonce is provided internally as &[0u8; 8]
     let mut rng: ChaCha20Rng = ChaCha20Rng::from_seed(*key);
     // Generate a random number
     *random_number_buffer = rng.gen();
+
+    // Print algorithm params
+    let stream = rng.get_stream();
+    let counter = rng.get_word_pos();
+    let seed = rng.get_seed();
+
+    println!("-- Rust ChaCha20 params --");
+    println!("stream (nonce): {}", stream);
+    println!("word_pos (counter): {}", counter);
+    println!("seed (key): {:?}", seed);
 }
 
 #[cfg(test)]
@@ -28,7 +35,7 @@ mod tests {
     fn test_fd_chacha20_generate_random_number() {
         let mut result: u32 = 0;
 
-        fd_chacha20_generate_random_number(&TEST_KEY, &mut result);
+        fd_chacha20_ffi_random_number(&TEST_KEY, &mut result);
         assert_eq!(result, EXPECTED_RANDOM_NUMBER);
     }
 }
