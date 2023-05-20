7af2b93eb990e0193dbf7c30e3912453bb1f5c70
Richard Patel <ripatel@jumptrading.com>
2023-05-20 05:48:14 +0000

    config: add linux_clang_x86_64_ffi
    Adds FFI profile generating PIC and symbol defs for static inlines.

--- config/linux_clang_fuzz_asan.mk
+++ config/linux_clang_fuzz_asan.mk
@@ -1,19 +1,8 @@
 BUILDDIR:=linux/clang/fuzz_asan
+include config/linux_clang_x86_64.mk
+include config/with-pic.mk
 
-include config/base.mk
-include config/with-clang.mk
-include config/with-debug.mk
-include config/with-brutality.mk
-include config/with-optimization.mk
-include config/with-threads.mk
-
-CPPFLAGS+=-fsanitize=fuzzer-no-link,address -fomit-frame-pointer \
-      -march=native -DFD_HAS_INT128=1 -DFD_HAS_DOUBLE=1 -DFD_HAS_ALLOCA=1 -DFD_HAS_X86=1
-LDFLAGS+=-fsanitize=fuzzer,address
-
-FD_HAS_INT128:=1
-FD_HAS_DOUBLE:=1
-FD_HAS_ALLOCA:=1
-FD_HAS_X86:=1
 FD_HAS_MAIN:=0
+CPPFLAGS+=-fsanitize=fuzzer-no-link,address
+LDFLAGS+=-fsanitize=fuzzer,address
 
