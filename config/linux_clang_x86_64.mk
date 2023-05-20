131c5badb38c08db4275e71ee8129b8f9bcfc483
Richard Patel <ripatel@jumptrading.com>
2023-05-20 05:48:14 +0000

    config: add linux_clang_x86_64_ffi
    Adds FFI profile generating PIC and symbol defs for static inlines.

--- config/linux_clang_x86_64.mk
+++ config/linux_clang_x86_64.mk
@@ -1,4 +1,4 @@
-BUILDDIR:=linux/clang/x86_64
+BUILDDIR?=linux/clang/x86_64
 
 include config/base.mk
 include config/with-clang.mk
@@ -16,6 +16,7 @@ include config/with-threads.mk
 
 CPPFLAGS+=-fomit-frame-pointer -march=haswell -mtune=skylake -mfpmath=sse \
 	  -DFD_HAS_INT128=1 -DFD_HAS_DOUBLE=1 -DFD_HAS_ALLOCA=1 -DFD_HAS_X86=1 -DFD_HAS_SSE=1 -DFD_HAS_AVX=1
+LDFLAGS+=-lrt
 
 FD_HAS_INT128:=1
 FD_HAS_DOUBLE:=1
