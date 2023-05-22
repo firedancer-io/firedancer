a8f115d447a7edc89cf869b20effb84406c65906
Liam Heeger <lheeger@jumptrading.com>
2023-05-22 11:31:37 -0500

    vm: add stack for vm

--- src/vm/Local.mk
+++ src/vm/Local.mk
@@ -1,3 +1,3 @@
 $(call make-lib,fd_vm)
-$(call add-hdrs,fd_vm_log_collector.h)
-$(call add-objs,fd_vm_log_collector,fd_vm)
+$(call add-hdrs,fd_vm_log_collector.h fd_vm_stack.h)
+$(call add-objs,fd_vm_log_collector fd_vm_stack,fd_vm)
