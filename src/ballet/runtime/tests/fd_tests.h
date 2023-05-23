94fbe07e50ad44040ad3f11ef30dc19880804ce2
Josh Siegel <jsiegel@jumptrading.com>
2023-05-23 11:38:10 +0000

    jsiegel/tests: lots of tests

--- src/ballet/runtime/tests/fd_tests.h
+++ src/ballet/runtime/tests/fd_tests.h
@@ -3,38 +3,41 @@
 /* Framework for running Solana's native program tests in our runtime */
 
 struct fd_executor_test_acc {
-    fd_pubkey_t pubkey;
-    ulong       lamports;
-    ulong       data_len;
-    uchar*      data;
-    uchar       executable;
-    ulong       rent_epoch;
-    fd_pubkey_t owner;
+  fd_pubkey_t pubkey;
+  ulong       lamports;
+  ulong       data_len;
+  uchar*      data;
+  uchar       executable;
+  ulong       rent_epoch;
+  fd_pubkey_t owner;
 };
 typedef struct fd_executor_test_acc fd_executor_test_acc_t;
 #define FD_EXECUTOR_TEST_ACC_FOOTPRINT ( sizeof(fd_executor_test_acc_t) )
 
 struct fd_executor_test {
-    char*                   test_name;
-    fd_pubkey_t             program_id;
-    fd_executor_test_acc_t* accs;
-    ulong                   accs_len;
-    uchar*                  raw_tx;
-    ulong                   raw_tx_len;
-    int                     expected_result;
+  char*                   test_name;
+  fd_pubkey_t             program_id;
+  fd_executor_test_acc_t* accs;
+  ulong                   accs_len;
+  uchar*                  raw_tx;
+  ulong                   raw_tx_len;
+  int                     expected_result;
 };
 typedef struct fd_executor_test fd_executor_test_t;
 #define FD_EXECUTOR_TEST_FOOTPRINT ( sizeof(fd_executor_test_t) )
 
 struct fd_executor_test_suite {
-    fd_wksp_t* wksp;
-    fd_funk_t* funk;
+  fd_wksp_t*                 wksp;
+  fd_funk_t*                 funk;
+  fd_alloc_fun_t             allocf;
+  void *                     allocf_arg;
+  fd_free_fun_t              freef;
 };
 typedef struct fd_executor_test_suite fd_executor_test_suite_t;
 #define FD_EXECUTOR_TEST_SUITE_FOOTPRINT ( sizeof(fd_executor_test_suite_t) )
 
 void fd_executor_test_suite_new( fd_executor_test_suite_t* suite );
-void fd_executor_run_test(
-    fd_executor_test_t* test,
-    fd_executor_test_suite_t* suite) ;
+int fd_executor_run_test(
+  fd_executor_test_t*       test,
+  fd_executor_test_suite_t* suite) ;
 
