95d05ecfaa926615d9631283c7702ad7d61894df
Richard Patel <ripatel@jumptrading.com>
2023-05-24 20:39:27 +0000

    chacha20: add Solana-compatible RNG
    Co-authored-by: Alber <arblugo@gmail.com>

--- src/ballet/chacha20/Local.mk
+++ src/ballet/chacha20/Local.mk
@@ -1,4 +1,6 @@
-$(call add-hdrs,fd_chacha20.h)
-$(call add-objs,fd_chacha20,fd_ballet)
+$(call add-hdrs,fd_chacha20.h fd_chacha20rng.h)
+$(call add-objs,fd_chacha20 fd_chacha20rng,fd_ballet)
 $(call make-unit-test,test_chacha20,test_chacha20,fd_ballet fd_util)
+$(call make-unit-test,test_chacha20rng,test_chacha20rng,fd_ballet fd_util)
 $(call run-unit-test,test_chacha20)
+$(call run-unit-test,test_chacha20rng)
