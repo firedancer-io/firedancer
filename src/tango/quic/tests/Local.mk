31de8576c0f34e7b4b654cd964a00d035563c297
Richard Patel <ripatel@jumptrading.com>
2023-05-27 03:26:53 +0000

    quic: add sockets support to tests

--- src/tango/quic/tests/Local.mk
+++ src/tango/quic/tests/Local.mk
@@ -2,12 +2,12 @@ ifdef FD_HAS_HOSTED
 ifdef FD_HAS_OPENSSL
 $(call add-objs,fd_quic_stream_spam fd_quic_test_helpers,fd_quic)
 
-$(call make-unit-test,test_quic_hs,test_quic_hs,fd_aio fd_quic fd_ballet fd_util)
-$(call make-unit-test,test_quic_streams,test_quic_streams,fd_aio fd_ballet fd_quic fd_util)
-$(call make-unit-test,test_quic_conn,test_quic_conn,fd_aio fd_quic fd_ballet fd_util)
+$(call make-unit-test,test_quic_hs,test_quic_hs,fd_aio fd_quic fd_ballet fd_tango fd_util)
+$(call make-unit-test,test_quic_streams,test_quic_streams,fd_aio fd_ballet fd_tango fd_quic fd_util)
+$(call make-unit-test,test_quic_conn,test_quic_conn,fd_aio fd_quic fd_ballet fd_tango fd_util)
 $(call make-unit-test,test_quic_server,test_quic_server,fd_aio fd_ballet fd_quic fd_tango fd_util)
 $(call make-unit-test,test_quic_client_flood,test_quic_client_flood,fd_aio fd_quic fd_ballet fd_tango fd_util)
-$(call make-unit-test,test_quic_bw,test_quic_bw,fd_aio fd_quic fd_ballet fd_util)
+$(call make-unit-test,test_quic_bw,test_quic_bw,fd_aio fd_quic fd_ballet fd_tango fd_util)
 $(call make-unit-test,test_quic_handshake,test_handshake,fd_aio fd_quic fd_util)
 $(call make-unit-test,test_quic_crypto,test_crypto,fd_quic fd_ballet fd_util)
 $(call make-unit-test,test_quic_frames,test_frames,fd_quic fd_util)
