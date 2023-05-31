851c80edb8d8d0cdef76ec8a1a12e5c1ccd2f7da
Josh Siegel <83408952+jumpsiegel@users.noreply.github.com>
2023-05-31 15:53:18 +0000

    runtime: move the test_native_program

---
+++ src/ballet/runtime/tests/Local.mk
@@ -0,0 +1,7 @@
+# Unit test only works if there is an accessable rocksdb
+
+ifneq ($(FD_HAS_ROCKSDB),)
+
+$(call make-unit-test,test_native_programs,test_native_programs,fd_ballet fd_funk fd_util fd_tests)
+
+endif
