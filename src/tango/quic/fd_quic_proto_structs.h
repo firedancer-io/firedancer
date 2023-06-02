649d43779f582101173aa37c7bb1f5a49b38239d
Nick Bridge <nbridge@jumptrading.com>
2023-06-02 14:00:38 -0500

    fix merge

--- src/tango/quic/fd_quic_proto_structs.h
+++ src/tango/quic/fd_quic_proto_structs.h
@@ -7,9 +7,6 @@
 #include "templ/fd_quic_defs.h"
 #include "templ/fd_quic_templ.h"
 #include "templ/fd_quic_frames_templ.h"
-#include "templ/fd_quic_ipv4.h"
-#include "templ/fd_quic_udp.h"
-#include "templ/fd_quic_eth.h"
 #include "templ/fd_quic_undefs.h"
 
 #endif /* HEADER_fd_src_tango_quic_fd_quic_proto_structs_h */
