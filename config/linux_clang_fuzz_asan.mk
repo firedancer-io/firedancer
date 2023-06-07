d01dd55ffb5fd00342942d0f26a75a5416e74ece
Richard Patel <ripatel@jumptrading.com>
2023-06-07 04:19:06 +0000

    config: fix fuzz-test build rules
    - Switches with-clang.mk to lld (required as ffi uses ThinLTO)
    - Fixes rebase error in linux_clang_fuzz_asan.mk
    - Adds make_fuzz_test CI profile

--- config/linux_clang_fuzz_asan.mk
+++ config/linux_clang_fuzz_asan.mk
@@ -1,6 +1,6 @@
 BUILDDIR:=linux/clang/fuzz_asan
 include config/linux_clang_x86_64.mk
-include config/with-pic.mk
+include config/with-ffi.mk
 
 FD_HAS_MAIN:=0
 CPPFLAGS+=-fsanitize=fuzzer-no-link,address
