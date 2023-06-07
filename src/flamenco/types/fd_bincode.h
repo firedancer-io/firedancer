a91ebf8be4573ffc98795f4a10e8ba2f8246afc8
Alex Siegel <asiegel@jumptrading.com>
2023-06-07 14:30:30 -0500

    runtime: fix merge

--- src/flamenco/types/fd_bincode.h
+++ src/flamenco/types/fd_bincode.h
@@ -55,7 +55,7 @@ fd_bincode_uint128_decode(uint128 * self, fd_bincode_decode_ctx_t * ctx) {
 #if FD_HAS_INT128 && FD_HAS_SSE
   *(__m128i_u*)self = _mm_loadu_si128((void const *) ptr);
 #else
-  fd_memcpy(self, ptr, sizeof(uint128));
+  memcpy(self, ptr, sizeof(uint128));
 #endif
   ctx->data = ptr + 1;
 
@@ -71,7 +71,7 @@ fd_bincode_uint128_encode(uint128 const * self, fd_bincode_encode_ctx_t * ctx) {
 #if FD_HAS_INT128 && FD_HAS_SSE
   _mm_storeu_si128((__m128i *) ptr, (__m128i) *self);
 #else
-  fd_memcpy(ptr, *self, sizeof(uint128));
+  memcpy(ptr, *self, sizeof(uint128));
 #endif
   ctx->data = ptr + 1;
 
