1357ace4f0589f589586327627698fb2d43d35f8
Marcus <117692400+marcus-jump@users.noreply.github.com>
2023-06-13 21:42:46 +0000

    test with asan

---
+++ config/linux_clang_x86_64_asan.mk
@@ -0,0 +1,10 @@
+BUILDDIR:=linux/clang/test_asan
+include config/linux_clang_x86_64.mk
+# include config/with-ffi.mk
+
+FD_HAS_ASAN:=1
+CPPFLAGS+=-DFD_HAS_ASAN=1
+
+CPPFLAGS+=-fsanitize=address
+LDFLAGS+=-fsanitize=address
+
--- src/util/fd_util_base.h
+++ src/util/fd_util_base.h
@@ -169,6 +169,11 @@
 #define FD_HAS_GFNI 0
 #endif
 
+/* FD_HAS_ASAN indicates that the build targed is using ASAN. */
+#ifndef FD_HAS_ASAN
+#define FD_HAS_ASAN 0
+#endif
+
 /* Base development environment ***************************************/
 
 /* The functionality provided by these vanilla headers are always
