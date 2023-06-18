1e3696d1f8e688bc6ced5433b98bf326db3116c7
Kevin J Bowers <kbowers@jumptrading.com>
2023-06-18 07:04:44 +0000

    x25519: AVX vectorization

--- src/ballet/ed25519/fd_x25519.c
+++ src/ballet/ed25519/fd_x25519.c
@@ -1,6 +1,14 @@
 #include "fd_x25519.h"
 #include "fd_ed25519_private.h"
 
+#ifndef FD_X25519_VECTORIZE
+#if FD_ED25519_FE_IMPL==1
+#define FD_X25519_VECTORIZE 1
+#else
+#define FD_X25519_VECTORIZE 0
+#endif
+#endif
+
 /* fd_x25519_basepoint is the canonical Curve25519 base point. */
 
 static uchar const fd_x25519_basepoint[ 32 ] = { 9 };
@@ -45,21 +53,40 @@ fd_x25519_scalar_mul( void * dst,
     fd_ed25519_fe_sub( &tmp1, &x2,   &z2   );
     fd_ed25519_fe_add( &x2,   &x2,   &z2   );
     fd_ed25519_fe_add( &z2,   &x3,   &z3   );
+
+#   if FD_X25519_VECTORIZE /* Note that okay to use less efficient squaring because we get it for free in unused vector lanes */
+    fd_ed25519_fe_mul4( &z3,   &tmp0, &x2,
+                        &z2,   &z2,   &tmp1,
+                        &tmp0, &tmp1, &tmp1,
+                        &tmp1, &x2,   &x2   );
+#   else /* Use more efficient squaring if scalar implementation */
     fd_ed25519_fe_mul( &z3,   &tmp0, &x2   );
     fd_ed25519_fe_mul( &z2,   &z2,   &tmp1 );
     fd_ed25519_fe_sq ( &tmp0, &tmp1        );
     fd_ed25519_fe_sq ( &tmp1, &x2          );
+#   endif
     fd_ed25519_fe_add( &x3,   &z3,   &z2   );
     fd_ed25519_fe_sub( &z2,   &z3,   &z2   );
+#   if FD_X25519_VECTORIZE /* See note above */
+    fd_ed25519_fe_mul2( &x2,   &tmp1, &tmp0,
+                        &z2,   &z2,   &z2   );
+#   else
     fd_ed25519_fe_mul( &x2,   &tmp1, &tmp0 );
-    fd_ed25519_fe_sub( &tmp1, &tmp1, &tmp0 );
     fd_ed25519_fe_sq ( &z2,   &z2          );
+#   endif
+    fd_ed25519_fe_sub( &tmp1, &tmp1, &tmp0 );
 
     fd_ed25519_fe_mul121666( &z3, &tmp1 );
-    fd_ed25519_fe_sq ( &x3,   &x3          );
     fd_ed25519_fe_add( &tmp0, &tmp0, &z3   );
+#   if FD_X25519_VECTORIZE /* See note above */
+    fd_ed25519_fe_mul3( &x3,   &x3,   &x3,
+                        &z3,   &x1,   &z2,
+                        &z2,   &tmp1, &tmp0 );
+#   else
+    fd_ed25519_fe_sq ( &x3,   &x3          );
     fd_ed25519_fe_mul( &z3,   &x1,   &z2   );
     fd_ed25519_fe_mul( &z2,   &tmp1, &tmp0 );
+#   endif
   }
 
   fd_ed25519_fe_swap_if( &x2, &x3, swap );
