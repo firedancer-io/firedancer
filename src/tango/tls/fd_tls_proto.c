cb5f33c9f1bf9ecd61751c836b67b24e41c96720
Richard Patel <ripatel@jumptrading.com>
2023-06-18 10:37:14 +0000

    tls: rework serde, add server_hello serializer

--- src/tango/tls/fd_tls_proto.c
+++ src/tango/tls/fd_tls_proto.c
@@ -2,11 +2,62 @@
 #include "fd_tls_serde.h"
 
 long
+fd_tls_decode_record_hdr( fd_tls_record_hdr_t * out,
+                          void const *          wire,
+                          ulong                 wire_sz ) {
+
+  if( FD_UNLIKELY( wire_sz<4UL ) )
+    return -(long)FD_TLS_ALERT_DECODE_ERROR;
+
+  uchar raw[ 4 ]; memcpy( raw, wire, 4UL );
+  out->type = raw[0];
+  out->sz   = 0xFFFFFFU &
+            ( ( ((uint)raw[1])<<16 )
+            + ( ((uint)raw[2])<< 8 )
+            + (  (uint)raw[3]      ) );
+  return 4L;
+}
+
+long
+fd_tls_encode_record_hdr( fd_tls_record_hdr_t const * in,
+                          void *                      wire,
+                          ulong                       wire_sz ) {
+
+  if( FD_UNLIKELY( wire_sz<4UL ) )
+    return -(long)FD_TLS_ALERT_INTERNAL_ERROR;
+
+  uchar raw[ 4 ] = { in->type,
+                     (uchar)( in->sz >> 16 ),
+                     (uchar)( in->sz >>  8 ),
+                     (uchar)( in->sz       ) };
+  memcpy( wire, raw, 4UL );
+  return 4L;
+}
+
+#define FD_TLS_ENCODE_EXT_BEGIN( type )                         \
+  do {                                                          \
+    int valid = 1;                                              \
+    FD_TLS_SERDE_LOCATE( ext_type, _, ushort, 1 );              \
+    FD_TLS_SERDE_LOCATE( ext_sz,   _, ushort, 1 );              \
+    FD_TLS_SERDE_CHECK                                          \
+    ushort *    ext_type_ptr = (ushort *)_field_ext_type_laddr; \
+    ushort *    ext_sz_ptr   = (ushort *)_field_ext_sz_laddr;   \
+    ulong const ext_start    = wire_laddr;                      \
+    *ext_type_ptr = fd_ushort_bswap( type );
+
+#define FD_TLS_ENCODE_EXT_END                    \
+    ulong ext_sz = wire_laddr - ext_start;       \
+    if( FD_UNLIKELY( ext_sz > USHORT_MAX ) )     \
+      return -(long)FD_TLS_ALERT_INTERNAL_ERROR; \
+    *ext_sz_ptr = fd_ushort_bswap( ext_sz );     \
+  } while(0)
+
+long
 fd_tls_decode_client_hello( fd_tls_client_hello_t * out,
-                            void const *            wire,
+                            void const * const      wire,
                             ulong                   wire_sz ) {
 
-  void const * const wire_start = wire;
+  ulong wire_laddr = (ulong)wire;
 
   /* Decode static sized part of client hello.
      (Assuming that session ID field is of a certain size) */
@@ -66,42 +117,43 @@ fd_tls_decode_client_hello( fd_tls_client_hello_t * out,
   if( FD_UNLIKELY( extension_tot_sz > wire_sz ) )
     return -(long)FD_TLS_ALERT_DECODE_ERROR;
 
-  void const * const ext_start = wire;
-  void const * const ext_stop  = (void const *)( (ulong)ext_start + extension_tot_sz );
+  ulong ext_start = wire_laddr;
+  ulong ext_stop  = ext_start + extension_tot_sz;
 
   /* Read extensions */
 
-  while( wire < ext_stop ) {
+  while( wire_laddr < ext_stop ) {
     /* Read extension type and length */
     ushort ext_type;
     ushort ext_sz;
 #   define FIELDS( FIELD )             \
       FIELD( 0, &ext_type, ushort, 1 ) \
-      FIELD( 6, &ext_sz,   ushort, 1 )
+      FIELD( 1, &ext_sz,   ushort, 1 )
       FD_TLS_DECODE_STATIC_BATCH( FIELDS )
 #   undef FIELDS
 
     /* Bounds check extension data */
-    if( FD_UNLIKELY( ((ulong)ext_stop - (ulong)wire) < ext_sz ) )
+    if( FD_UNLIKELY( (ext_stop - wire_laddr) < ext_sz ) )
       return -(long)FD_TLS_ALERT_DECODE_ERROR;
 
     /* Decode extension data */
+    void const * ext_data = (void const *)wire_laddr;
     long ext_parse_res;
     switch( ext_type ) {
     case FD_TLS_EXT_SUPPORTED_VERSIONS:
-      ext_parse_res = fd_tls_decode_ext_supported_versions( &out->supported_versions, wire, wire_sz );
+      ext_parse_res = fd_tls_decode_ext_supported_versions( &out->supported_versions, ext_data, wire_sz );
       break;
     case FD_TLS_EXT_TYPE_SERVER_NAME:
-      ext_parse_res = fd_tls_decode_ext_server_name( &out->server_name, wire, wire_sz );
+      ext_parse_res = fd_tls_decode_ext_server_name( &out->server_name, ext_data, wire_sz );
       break;
     case FD_TLS_EXT_TYPE_SUPPORTED_GROUPS:
-      ext_parse_res = fd_tls_decode_ext_supported_groups( &out->supported_groups, wire, wire_sz );
+      ext_parse_res = fd_tls_decode_ext_supported_groups( &out->supported_groups, ext_data, wire_sz );
       break;
     case FD_TLS_EXT_SIGNATURE_ALGORITHMS:
-      ext_parse_res = fd_tls_decode_ext_signature_algorithms( &out->signature_algorithms, wire, wire_sz );
+      ext_parse_res = fd_tls_decode_ext_signature_algorithms( &out->signature_algorithms, ext_data, wire_sz );
       break;
     case FD_TLS_EXT_KEY_SHARE:
-      ext_parse_res = fd_tls_decode_ext_key_share( &out->key_share, wire, wire_sz );
+      ext_parse_res = fd_tls_decode_ext_key_share_client( &out->key_share, ext_data, wire_sz );
       break;
     default:
       ext_parse_res = (long)ext_sz;
@@ -110,16 +162,79 @@ fd_tls_decode_client_hello( fd_tls_client_hello_t * out,
     if( FD_UNLIKELY( ext_parse_res<0L ) )
       return ext_parse_res;
     if( FD_UNLIKELY( ext_parse_res != (long)ext_sz ) )
-      return FD_TLS_ALERT_DECODE_ERROR;
+      return -(long)FD_TLS_ALERT_DECODE_ERROR;
 
     /* Seek to next extension */
-    wire    = (void const *)( (ulong)wire + ext_sz );
-    wire_sz = wire_sz - ext_sz;
+    wire_laddr += ext_sz;
+    wire_sz    -= ext_sz;
   }
 
-  /* Assert: wire == ext_stop */
+  /* Assert: wire_laddr == ext_stop */
 
-  return (long)( (ulong)wire - (ulong)wire_start );
+  return (long)( wire_laddr - (ulong)wire );
+}
+
+long
+fd_tls_encode_server_hello( fd_tls_server_hello_t * out,
+                            void *                  wire,
+                            ulong                   wire_sz ) {
+
+  ulong wire_laddr = (ulong)wire;
+
+  /* Encode static sized part of server hello.
+     (Assuming that session ID field is of a certain size) */
+
+  ushort legacy_version       = FD_TLS_VERSION_TLS12;
+  uchar  legacy_session_id_sz = 0;
+  ushort cipher_suites_sz     = sizeof(ushort);
+  ushort cipher_suites[1]     = { FD_TLS_CIPHER_SUITE_AES_128_GCM_SHA256 };
+
+# define FIELDS( FIELD )                            \
+    FIELD( 0, &legacy_version,       ushort, 1    ) \
+    FIELD( 1, &out->random[0],       uchar,  32UL ) \
+    FIELD( 2, &legacy_session_id_sz, uchar,  1    ) \
+    FIELD( 3, &cipher_suites_sz,     ushort, 1    ) \
+    FIELD( 4, cipher_suites,         ushort, 1    )
+    FD_TLS_ENCODE_STATIC_BATCH( FIELDS )
+# undef FIELDS
+
+  /* Encode extensions */
+
+  ushort * extension_tot_sz = FD_TLS_SKIP_FIELD( ushort );
+  ulong    extension_start  = wire_laddr;
+
+# define FIELDS( FIELD )                            \
+    FIELD( 0, &legacy_version,       ushort, 1    ) \
+    FIELD( 1, &out->random[0],       uchar,  32UL ) \
+    FIELD( 2, &legacy_session_id_sz, uchar,  1    ) \
+    FIELD( 3, &cipher_suites_sz,     ushort, 1    ) \
+    FIELD( 4, cipher_suites,         ushort, 1    )
+    FD_TLS_ENCODE_STATIC_BATCH( FIELDS )
+# undef FIELDS
+
+  ushort ext_supported_versions_ext_type = FD_TLS_EXT_SUPPORTED_VERSIONS;
+  ushort ext_supported_versions[1]       = { FD_TLS_VERSION_TLS13 };
+  ushort ext_supported_versions_ext_sz   = sizeof(ext_supported_versions);
+
+  ushort ext_key_share_ext_type = FD_TLS_EXT_KEY_SHARE;
+  ushort ext_key_share_ext_sz   = sizeof(ushort) + sizeof(ushort) + 32UL;
+  ushort ext_key_share_group    = FD_TLS_GROUP_X25519;
+  ushort ext_key_share_sz       = 32UL;
+
+# define FIELDS( FIELD )                                         \
+    FIELD( 0, &ext_supported_versions_ext_type,   ushort, 1    ) \
+    FIELD( 1, &ext_supported_versions_ext_sz,     ushort, 1    ) \
+    FIELD( 2, ext_supported_versions,             ushort, 1    ) \
+    FIELD( 3, &ext_key_share_ext_type,            ushort, 1    ) \
+    FIELD( 4, &ext_key_share_ext_sz,              ushort, 1    ) \
+    FIELD( 5, &ext_key_share_group,               ushort, 1    ) \
+    FIELD( 6, &ext_key_share_sz,                  ushort, 1    ) \
+    FIELD( 7, &out->key_share.x25519[0],          uchar,  32UL )
+    FD_TLS_ENCODE_STATIC_BATCH( FIELDS )
+# undef FIELDS
+
+  *extension_tot_sz = fd_ushort_bswap( (ushort)( (ulong)wire_laddr - extension_start ) );
+  return (long)( wire_laddr - (ulong)wire );
 }
 
 long
@@ -127,18 +242,11 @@ fd_tls_decode_ext_server_name( fd_tls_ext_server_name_t * out,
                                void const *               wire,
                                ulong                      wire_sz ) {
 
-  void const * const wire_start = wire;
+  ulong wire_laddr = (ulong)wire;
 
-  /* Get size of server names list.
-     TLS v1.3 server name lists practically always have one element. */
+  /* TLS v1.3 server name lists practically always have one element. */
 
-  ushort server_name_list_sz;
-  FD_TLS_DECODE_FIELD( &server_name_list_sz, ushort );
-
-  void const * const list_start = wire;
-  void const * const list_stop  = (void const *)( (ulong)list_start + server_name_list_sz );
-
-  while( wire < list_stop ) {
+  FD_TLS_DECODE_LIST_BEGIN( ushort, alignof(uchar) ) {
     /* Read type and length */
     uchar  name_type;
     ushort name_sz;
@@ -149,7 +257,7 @@ fd_tls_decode_ext_server_name( fd_tls_ext_server_name_t * out,
 #   undef FIELDS
 
     /* Bounds check name */
-    if( FD_UNLIKELY( ( (ulong)list_stop - (ulong)wire ) < name_sz ) )
+    if( FD_UNLIKELY( list_stop - wire_laddr < name_sz ) )
       return -(long)FD_TLS_ALERT_DECODE_ERROR;
 
     /* Decode name on first use */
@@ -157,18 +265,17 @@ fd_tls_decode_ext_server_name( fd_tls_ext_server_name_t * out,
         & ( name_sz < 254                            )
         & ( out->host_name_len == 0                  ) ) ) {
       out->host_name_len = (uchar)name_sz;
-      memcpy( out->host_name, wire, name_sz );
+      memcpy( out->host_name, (void const *)wire_laddr, name_sz );
       out->host_name[ name_sz ] = '\0';
     }
 
     /* Seek to next name */
-    wire    = (void const *)( (ulong)wire + name_sz );
-    wire_sz = wire_sz - name_sz;
+    wire_laddr += name_sz;
+    wire_sz    -= name_sz;
   }
+  FD_TLS_DECODE_LIST_END
 
-  /* Assert: wire == list_stop */
-
-  return (long)( (ulong)wire - (ulong)wire_start );
+  return (long)( wire_laddr - (ulong)wire );
 }
 
 long
@@ -176,19 +283,9 @@ fd_tls_decode_ext_supported_groups( fd_tls_ext_supported_groups_t * out,
                                     void const *                    wire,
                                     ulong                           wire_sz ) {
 
-  void const * const wire_start = wire;
+  ulong wire_laddr = (ulong)wire;
 
-  ushort list_sz;
-  FD_TLS_DECODE_FIELD( &list_sz, ushort );
-
-  void const * const list_start = wire;
-  void const * const list_stop  = (void const *)( (ulong)list_start + list_sz );
-
-  if( FD_UNLIKELY( ( list_sz > wire_sz )
-                 | ( !fd_uint_is_aligned( list_sz, 2U ) ) ) )
-    return FD_TLS_ALERT_DECODE_ERROR;
-
-  while( wire < list_stop ) {
+  FD_TLS_DECODE_LIST_BEGIN( ushort, alignof(uchar) ) {
     ushort group;
     FD_TLS_DECODE_FIELD( &group, ushort );
     switch( group ) {
@@ -198,10 +295,9 @@ fd_tls_decode_ext_supported_groups( fd_tls_ext_supported_groups_t * out,
     /* Add more groups here */
     }
   }
+  FD_TLS_DECODE_LIST_END
 
-  /* Assert: wire == list_stop */
-
-  return (long)( (ulong)wire - (ulong)wire_start );
+  return (long)( wire_laddr - (ulong)wire );
 }
 
 long
@@ -209,19 +305,9 @@ fd_tls_decode_ext_supported_versions( fd_tls_ext_supported_versions_t * out,
                                       void const *                      wire,
                                       ulong                             wire_sz ) {
 
-  void const * const wire_start = wire;
+  ulong wire_laddr = (ulong)wire;
 
-  uchar list_sz;
-  FD_TLS_DECODE_FIELD( &list_sz, uchar );
-
-  void const * const list_start = wire;
-  void const * const list_stop  = (void const *)( (ulong)list_start + list_sz );
-
-  if( FD_UNLIKELY( ( list_sz > wire_sz )
-                 | ( !fd_uint_is_aligned( list_sz, 2U ) ) ) )
-    return FD_TLS_ALERT_DECODE_ERROR;
-
-  while( wire < list_stop ) {
+  FD_TLS_DECODE_LIST_BEGIN( uchar, alignof(ushort) ) {
     ushort group;
     FD_TLS_DECODE_FIELD( &group, ushort );
     switch( group ) {
@@ -231,10 +317,9 @@ fd_tls_decode_ext_supported_versions( fd_tls_ext_supported_versions_t * out,
     /* Add more groups here */
     }
   }
+  FD_TLS_DECODE_LIST_END
 
-  /* Assert: wire == list_stop */
-
-  return (long)( (ulong)wire - (ulong)wire_start );
+  return (long)( wire_laddr - (ulong)wire );
 }
 
 long
@@ -242,19 +327,9 @@ fd_tls_decode_ext_signature_algorithms( fd_tls_ext_signature_algorithms_t * out,
                                         void const *                        wire,
                                         ulong                               wire_sz ) {
 
-  void const * const wire_start = wire;
+  ulong wire_laddr = (ulong)wire;
 
-  ushort list_sz;
-  FD_TLS_DECODE_FIELD( &list_sz, ushort );
-
-  void const * const list_start = wire;
-  void const * const list_stop  = (void const *)( (ulong)list_start + list_sz );
-
-  if( FD_UNLIKELY( ( list_sz > wire_sz )
-                 | ( !fd_uint_is_aligned( list_sz, 2U ) ) ) )
-    return FD_TLS_ALERT_DECODE_ERROR;
-
-  while( wire < list_stop ) {
+  FD_TLS_DECODE_LIST_BEGIN( ushort, alignof(ushort) ) {
     ushort group;
     FD_TLS_DECODE_FIELD( &group, ushort );
     switch( group ) {
@@ -264,18 +339,17 @@ fd_tls_decode_ext_signature_algorithms( fd_tls_ext_signature_algorithms_t * out,
     /* Add more groups here */
     }
   }
+  FD_TLS_DECODE_LIST_END
 
-  /* Assert: wire == list_stop */
-
-  return (long)( (ulong)wire - (ulong)wire_start );
+  return (long)( wire_laddr - (ulong)wire );
 }
 
 static long
-fd_tls_decode_ext_key_share_entry( fd_tls_ext_key_share_t * out,
-                                   void const *             wire,
-                                   ulong                    wire_sz ) {
+fd_tls_decode_key_share_entry( fd_tls_ext_key_share_t * out,
+                               void const *             wire,
+                               ulong                    wire_sz ) {
 
-  void const * const wire_start = wire;
+  ulong wire_laddr = (ulong)wire;
 
   /* Read type and length */
   ushort group;
@@ -295,45 +369,34 @@ fd_tls_decode_ext_key_share_entry( fd_tls_ext_key_share_t * out,
     if( FD_UNLIKELY( kex_data_sz != 32UL ) )
       return -(long)FD_TLS_ALERT_DECODE_ERROR;
     out->has_x25519 = 1;
-    memcpy( out->x25519, wire, 32UL );
+    memcpy( out->x25519, (void const *)wire_laddr, 32UL );
     break;
   /* Add more groups here */
   }
 
   /* Seek to next group */
-  wire     = (void const *)( (ulong)wire + kex_data_sz );
-  wire_sz -= kex_data_sz;
+  wire_laddr += kex_data_sz;
+  wire_sz    -= kex_data_sz;
 
-  return (long)( (ulong)wire - (ulong)wire_start );
+  return (long)( wire_laddr - (ulong)wire );
 }
 
 long
-fd_tls_decode_ext_key_share( fd_tls_ext_key_share_t * out,
-                             void const *             wire,
-                             ulong                    wire_sz ) {
+fd_tls_decode_ext_key_share_client( fd_tls_ext_key_share_t * out,
+                                    void const *             wire,
+                                    ulong                    wire_sz ) {
 
-  void const * const wire_start = wire;
+  ulong wire_laddr = (ulong)wire;
 
-  ushort list_sz;
-  FD_TLS_DECODE_FIELD( &list_sz, ushort );
-
-  void const * const list_start = wire;
-  void const * const list_stop  = (void const *)( (ulong)list_start + list_sz );
-
-  if( FD_UNLIKELY( ( list_sz > wire_sz )
-                 | ( !fd_uint_is_aligned( list_sz, 2U ) ) ) )
-    return FD_TLS_ALERT_DECODE_ERROR;
-
-  while( wire < list_stop ) {
-    long res = fd_tls_decode_ext_key_share_entry( out, wire, wire_sz );
+  FD_TLS_DECODE_LIST_BEGIN( ushort, alignof(uchar) ) {
+    long res = fd_tls_decode_key_share_entry( out, (void const *)wire_laddr, wire_sz );
     if( FD_UNLIKELY( res<0L ) )
       return res;
-    wire     = (void const *)( (ulong)wire + (ulong)res );
-    wire_sz -= (ulong)res;
+    wire_laddr += (ulong)res;
+    wire_sz    -= (ulong)res;
   }
+  FD_TLS_DECODE_LIST_END
 
-  /* Assert: wire == list_stop */
-
-  return (long)( (ulong)wire - (ulong)wire_start );
+  return (long)( wire_laddr - (ulong)wire );
 }
 
--- src/tango/tls/test_tls_proto.c
+++ src/tango/tls/test_tls_proto.c
@@ -5,16 +5,14 @@
 
 FD_IMPORT_BINARY( test_client_hello, "src/tango/tls/fixtures/client_hello_labs-1.14.8.bin" );
 
-int
-main( int     argc,
-      char ** argv) {
-  fd_boot( &argc, &argv );
-
-  fd_tls_client_hello_t client_hello[1] = {0};
-  long sz = fd_tls_decode_client_hello( client_hello, test_client_hello, test_client_hello_sz );
+static void
+test_client_hello_decode( void ) {
+  fd_tls_client_hello_t client_hello = {0};
+  long sz = fd_tls_decode_client_hello( &client_hello, test_client_hello, test_client_hello_sz );
+  FD_LOG_DEBUG(( "fd_tls_decode_client_hello(%p) = %ld", (void *)&client_hello, sz ));
   FD_TEST( sz == (long)test_client_hello_sz );
 
-  fd_tls_client_hello_t client_hello_expected[1] = {{
+  fd_tls_client_hello_t client_hello_expected = {
     .random = {
       0xb5, 0x17, 0xc7, 0x84, 0xdc, 0xf1, 0x03, 0x1b, 0x4a, 0x95, 0xab, 0x98, 0x89, 0x07, 0x0f, 0x13,
       0x93, 0x69, 0xeb, 0xb7, 0x27, 0x53, 0x5b, 0xa4, 0x22, 0xfe, 0xbc, 0x21, 0x4d, 0xc1, 0xc0, 0xe7
@@ -34,8 +32,41 @@ main( int     argc,
         0x3c, 0x8b, 0x46, 0xdd, 0xb2, 0x2d, 0x1f, 0xbc, 0xef, 0x82, 0xe6, 0x71, 0x57, 0xab, 0x11, 0x3c
       }
     }
-  }};
-  FD_TEST( 0==memcmp( client_hello, client_hello_expected, sizeof(fd_tls_client_hello_t) ) );
+  };
+  FD_TEST( 0==memcmp( &client_hello, &client_hello_expected, sizeof(fd_tls_client_hello_t) ) );
+}
+
+static void
+test_server_hello_encode( void ) {
+  fd_tls_server_hello_t server_hello = {
+    .random = {
+      0x2c, 0x5d, 0x29, 0x48, 0x20, 0x08, 0xe7, 0xc6, 0x6e, 0xef, 0x18, 0x57, 0x21, 0xb8, 0x87, 0x3b,
+      0x78, 0xf8, 0x26, 0x7a, 0x14, 0x56, 0xad, 0xaa, 0x92, 0x92, 0xff, 0xdf, 0xbb, 0x59, 0x78, 0xa4
+    },
+    .cipher_suite = FD_TLS_CIPHER_SUITE_AES_128_GCM_SHA256,
+    .supported_versions = { .tls13 = 1 },
+    .key_share = {
+      .has_x25519 = 1,
+      .x25519 = {
+        0xac, 0x45, 0x04, 0x6e, 0x3a, 0x0d, 0xdc, 0x9b, 0x82, 0x7f, 0x70, 0x50, 0x0e, 0x89, 0xe5, 0xdf,
+        0x31, 0xae, 0xed, 0x42, 0xc6, 0xec, 0x48, 0xa3, 0xcb, 0x95, 0x8e, 0xe1, 0x24, 0x3a, 0x6d, 0x3f
+      }
+    }
+  };
+
+  uchar server_hello_buf[ 1280 ];
+  long sz = fd_tls_encode_server_hello( &server_hello, server_hello_buf, sizeof(server_hello_buf) );
+  FD_TEST( sz>=0L );
+  FD_LOG_HEXDUMP_DEBUG(( "fd_tls_encode_server_hello", server_hello_buf, (ulong)sz ));
+}
+
+int
+main( int     argc,
+      char ** argv) {
+  fd_boot( &argc, &argv );
+
+  test_client_hello_decode();
+  test_server_hello_encode();
 
   FD_LOG_NOTICE(( "pass" ));
   fd_halt();
