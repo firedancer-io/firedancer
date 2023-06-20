1bc0266dd70f2c7587b37dab6cf2e144d39a7304
Richard Patel <ripatel@jumptrading.com>
2023-06-20 21:43:34 +0000

    tls: derive handshake secrets

--- src/tango/tls/test_tls_openssl_client.c
+++ src/tango/tls/test_tls_openssl_client.c
@@ -12,15 +12,50 @@
 #include <openssl/tls1.h>
 
 static int
+_ossl_level_to_fdtls( OSSL_ENCRYPTION_LEVEL enc_level ) {
+  int level;
+  switch( enc_level ) {
+  case ssl_encryption_initial:     level = FD_TLS_LEVEL_INITIAL;     break;
+  case ssl_encryption_early_data:  level = FD_TLS_LEVEL_EARLY;       break;
+  case ssl_encryption_handshake:   level = FD_TLS_LEVEL_HANDSHAKE;   break;
+  case ssl_encryption_application: level = FD_TLS_LEVEL_APPLICATION; break;
+  default:                         level = (int)enc_level;           break;
+  }
+  return level;
+}
+
+static OSSL_ENCRYPTION_LEVEL
+_fdtls_level_to_ossl( int level ) {
+  OSSL_ENCRYPTION_LEVEL enc_level;
+  switch( level ) {
+  case FD_TLS_LEVEL_INITIAL:     enc_level = ssl_encryption_initial;       break;
+  case FD_TLS_LEVEL_EARLY:       enc_level = ssl_encryption_early_data;    break;
+  case FD_TLS_LEVEL_HANDSHAKE:   enc_level = ssl_encryption_handshake;     break;
+  case FD_TLS_LEVEL_APPLICATION: enc_level = ssl_encryption_application;   break;
+  default:                       enc_level = (OSSL_ENCRYPTION_LEVEL)level; break;
+  }
+  return enc_level;
+}
+
+static int
 _ssl_set_encryption_secrets( SSL *                 ssl,
                              OSSL_ENCRYPTION_LEVEL enc_level,
                              uchar const *         read_secret,
                              uchar const *         write_secret,
                              ulong                 secret_len ) {
-  (void)ssl; (void)enc_level; (void)read_secret; (void)write_secret; (void)secret_len;
-  FD_LOG_HEXDUMP_INFO(( "secret read",  read_secret,  secret_len ));
-  FD_LOG_HEXDUMP_INFO(( "secret write", write_secret, secret_len ));
-  FD_LOG_NOTICE(( "_ssl_set_encryption_secrets" ));
+
+  FD_TEST( secret_len==32UL );
+  switch( enc_level ) {
+  case ssl_encryption_handshake: {
+    void * client_hs_secret_expected = SSL_get_ex_data( ssl, 2 );
+    memcpy( client_hs_secret_expected, write_secret, 32UL );
+    void * server_hs_secret_expected = SSL_get_ex_data( ssl, 3 );
+    memcpy( server_hs_secret_expected, read_secret,  32UL );
+    break;
+  }
+  default:
+    break;
+  }
   return 1;
 }
 
@@ -35,7 +70,8 @@ _ssl_add_handshake_data( SSL *                 ssl,
   fd_tls_server_t *    server = SSL_get_ex_data( ssl, 0 );
   fd_tls_server_hs_t * hs     = SSL_get_ex_data( ssl, 1 );
 
-  FD_TEST( fd_tls_server_recvmsg( server, hs, data, data_sz )==(long)data_sz );
+  int level = _ossl_level_to_fdtls( enc_level );
+  FD_TEST( fd_tls_server_recvmsg( server, hs, data, data_sz, level )==(long)data_sz );
 
   return 1;
 }
@@ -56,6 +92,13 @@ _ssl_send_alert( SSL *                 ssl,
                SSL_alert_desc_string     ( alert ),
                SSL_alert_desc_string_long( alert ) ));
   return 1;
+}
+
+static void
+_ssl_keylog( SSL const *  ssl,
+             char const * line ) {
+  (void)ssl;
+  FD_LOG_DEBUG(( "OpenSSL: %s", line ));
 }
 
 int
@@ -95,26 +138,46 @@ main( int     argc,
     _ssl_send_alert };
   FD_TEST( 1==SSL_CTX_set_quic_method( ctx, &quic_method ) );
 
+  SSL_CTX_set_keylog_callback( ctx, _ssl_keylog );
+
   SSL * ssl = SSL_new( ctx );
   FD_TEST( ssl );
 
   SSL_set_connect_state( ssl );
 
-  SSL_set_ex_data( ssl, 0, &server );
-  SSL_set_ex_data( ssl, 1, &hs     );
+  uchar client_hs_secret_expected[ 32 ];
+  uchar server_hs_secret_expected[ 32 ];
+
+  SSL_set_ex_data( ssl, 0, &server                   );
+  SSL_set_ex_data( ssl, 1, &hs                       );
+  SSL_set_ex_data( ssl, 2, client_hs_secret_expected );
+  SSL_set_ex_data( ssl, 3, server_hs_secret_expected );
 
   /* Send ClientHello */
   SSL_do_handshake( ssl );
+  FD_TEST( ERR_get_error()==0UL );
 
   /* Send ServerHello */
   uchar record[ 1024 ];
-  long send_res = fd_tls_server_sendmsg( &server, &hs, record, sizeof(record) );
+  int   level;
+  long send_res = fd_tls_server_sendmsg( &server, &hs, record, sizeof(record), &level );
   FD_TEST( send_res>=0L );
   FD_LOG_HEXDUMP_INFO(( "server => client", record, (ulong)send_res ));
-  FD_TEST( 1==SSL_provide_quic_data( ssl, ssl_encryption_initial, record, (ulong)send_res ) );
+  FD_TEST( 1==SSL_provide_quic_data( ssl, _fdtls_level_to_ossl( level ), record, (ulong)send_res ) );
 
   /* Process ServerHello */
   SSL_do_handshake( ssl );
+  FD_TEST( ERR_get_error()==0UL );
+
+  /* Check handshake secrets */
+  FD_TEST( 0==memcmp( client_hs_secret_expected, hs.client_hs_secret, 32UL ) );
+  FD_TEST( 0==memcmp( server_hs_secret_expected, hs.server_hs_secret, 32UL ) );
+  FD_LOG_INFO(( "client handshake secret: " FD_LOG_HEX16_FMT FD_LOG_HEX16_FMT,
+                FD_LOG_HEX16_FMT_ARGS( hs.client_hs_secret    ),
+                FD_LOG_HEX16_FMT_ARGS( hs.client_hs_secret+16 ) ));
+  FD_LOG_INFO(( "server handshake secret: " FD_LOG_HEX16_FMT FD_LOG_HEX16_FMT,
+                FD_LOG_HEX16_FMT_ARGS( hs.server_hs_secret    ),
+                FD_LOG_HEX16_FMT_ARGS( hs.server_hs_secret+16 ) ));
 
   /* Clean up */
 
