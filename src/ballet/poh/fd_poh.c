7cf357dbe1d23e23cc2289dcdd2a5a9633f6b067
Richard Patel <ripatel@jumptrading.com>
2023-06-22 08:47:47 +0000

    poh: remove dep on fd_hash_t
    Fixes a circular dependency between flamenco and ballet

--- src/ballet/poh/fd_poh.c
+++ src/ballet/poh/fd_poh.c
@@ -1,8 +1,8 @@
 #include "fd_poh.h"
 
-fd_hash_t *
-fd_poh_append( fd_hash_t * poh,
-               ulong       n ) {
+void *
+fd_poh_append( void * poh,
+               ulong  n ) {
   fd_sha256_t sha;
   while( n-- ) {
     fd_sha256_init( &sha );
@@ -12,12 +12,12 @@ fd_poh_append( fd_hash_t * poh,
   return poh;
 }
 
-fd_hash_t *
-fd_poh_mixin( fd_hash_t *   FD_RESTRICT poh,
+void *
+fd_poh_mixin( void *        FD_RESTRICT poh,
               uchar const * FD_RESTRICT mixin ) {
   fd_sha256_t sha;
   fd_sha256_init( &sha );
-  fd_sha256_append( &sha, poh, FD_SHA256_HASH_SZ );
+  fd_sha256_append( &sha, poh,   FD_SHA256_HASH_SZ );
   fd_sha256_append( &sha, mixin, FD_SHA256_HASH_SZ );
   fd_sha256_fini( &sha, poh );
   return poh;
--- src/ballet/poh/fd_poh.h
+++ src/ballet/poh/fd_poh.h
@@ -10,14 +10,14 @@ FD_PROTOTYPES_BEGIN
 
 /* fd_poh_append performs n recursive hash operations. */
 
-fd_hash_t *
-fd_poh_append( fd_hash_t * poh,
-               ulong     n );
+void *
+fd_poh_append( void * poh,
+               ulong  n );
 
 /* fd_poh_mixin mixes in a 32-byte value. */
 
-fd_hash_t *
-fd_poh_mixin( fd_hash_t *   FD_RESTRICT poh,
+void *
+fd_poh_mixin( void *        FD_RESTRICT poh,
               uchar const * FD_RESTRICT mixin );
 
 FD_PROTOTYPES_END
