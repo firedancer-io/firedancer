86441b3ececd9c10f60961c0534cdd7a57b0b013
Kevin J Bowers <kbowers@jumptrading.com>
2023-06-24 23:22:14 -0500

    Update build to reflect current FD_HAS_* requirements
    FD_HAS_HOSTED
    - test_sbpf_load_prog uses fileno
    - fd_vm_tool uses fileno
    - test_pcapng uses fmemopen, fd_alloca
    
    FD_HAS_DOUBLE
    - fd_est_tbl.h makes non-trivial double use, affecting fd_frank_pack, fd_frank, test_pack.c

--- src/flamenco/vm/Local.mk
+++ src/flamenco/vm/Local.mk
@@ -1,7 +1,9 @@
 $(call add-hdrs,fd_vm_context.h fd_vm_disasm.h fd_vm_interp.h fd_vm_log_collector.h fd_vm_stack.h fd_vm_syscalls.h)
 $(call add-objs,fd_vm_context fd_vm_disasm fd_vm_interp fd_vm_log_collector fd_vm_stack fd_vm_syscalls,fd_flamenco)
 
+ifdef FD_HAS_HOSTED
 $(call make-bin,fd_vm_tool,fd_vm_tool,fd_flamenco fd_ballet fd_util)
+endif
 
 $(call make-unit-test,test_vm_interp,test_vm_interp,fd_flamenco fd_ballet fd_util)
 $(call run-unit-test,test_vm_interp)
