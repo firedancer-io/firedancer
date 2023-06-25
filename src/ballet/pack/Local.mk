ebdf458765932652f1b9ff863516a7e3a604f2f0
Kevin J Bowers <kbowers@jumptrading.com>
2023-06-24 23:22:14 -0500

    Update build to reflect current FD_HAS_* requirements
    FD_HAS_HOSTED
    - test_sbpf_load_prog uses fileno
    - fd_vm_tool uses fileno
    - test_pcapng uses fmemopen, fd_alloca
    
    FD_HAS_DOUBLE
    - fd_est_tbl.h makes non-trivial double use, affecting fd_frank_pack, fd_frank, test_pack.c

--- src/ballet/pack/Local.mk
+++ src/ballet/pack/Local.mk
@@ -1,3 +1,4 @@
+ifdef FD_HAS_DOUBLE
 $(call add-hdrs,fd_pack.h fd_est_tbl.h fd_compute_budget_program.h)
 $(call add-objs,fd_pack,fd_ballet)
 $(call make-unit-test,test_compute_budget_program,test_compute_budget_program,fd_ballet fd_util)
@@ -6,3 +7,4 @@ $(call make-unit-test,test_pack,test_pack,fd_disco fd_ballet fd_util)
 $(call run-unit-test,test_compute_budget_program,)
 $(call run-unit-test,test_est_tbl,)
 $(call run-unit-test,test_pack,)
+endif
