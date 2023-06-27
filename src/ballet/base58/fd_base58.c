b37325fa853078071e0acc224152761960f9c144
Liam Heeger <lheeger@jumptrading.com>
2023-06-27 10:26:09 -0500

    update base58

--- src/ballet/base58/fd_base58.c
+++ src/ballet/base58/fd_base58.c
@@ -5,13 +5,13 @@
 void fd_base58_print_32( uchar const * bytes ) {
   char encoded_str[FD_BASE58_ENCODED_32_SZ];
   fd_base58_encode_32( (uchar *)bytes, NULL, encoded_str );
-  printf( "%s", encoded_str );
+  printf( "%s\n", encoded_str );
 }
 
 void fd_base58_print_64( uchar const * bytes ) {
   char encoded_str[FD_BASE58_ENCODED_64_SZ];
   fd_base58_encode_64( (uchar *)bytes, NULL, encoded_str );
-  printf( "%s", encoded_str );
+  printf( "%s\n", encoded_str );
 }
 
 #if FD_HAS_AVX
