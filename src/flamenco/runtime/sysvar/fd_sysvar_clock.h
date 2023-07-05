e1bce3bdc9b1d3ebcac3828987c016dd37f3469d
Josh Siegel <83408952+jumpsiegel@users.noreply.github.com>
2023-07-05 18:44:06 +0000

    Revert "runtime: optimized"
    This reverts commit 22feba35aed26094d987538d1d330f9c4d109352.

--- src/flamenco/runtime/sysvar/fd_sysvar_clock.h
+++ src/flamenco/runtime/sysvar/fd_sysvar_clock.h
@@ -10,9 +10,10 @@
 void fd_sysvar_clock_init( fd_global_ctx_t* global );
 
 /* Update the clock sysvar account. This should be called at the start of every slot, before execution commences. */
-int fd_sysvar_clock_update( fd_global_ctx_t* global );
+void fd_sysvar_clock_update( fd_global_ctx_t* global );
 
 /* Reads the current value of the clock sysvar */
-int fd_sysvar_clock_read( fd_global_ctx_t* global, fd_sol_sysvar_clock_t* result );
+void fd_sysvar_clock_read( fd_global_ctx_t* global, fd_sol_sysvar_clock_t* result );
 
 #endif /* HEADER_fd_src_flamenco_runtime_sysvar_fd_clock_h */
+
