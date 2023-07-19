a2075e20b36247a06d52332a8ceab0567cc2f9bd
Josh Siegel <83408952+jumpsiegel@users.noreply.github.com>
2023-07-19 11:02:28 +0000

    runtime: bpf test

--- src/flamenco/runtime/run_bpf_tests.sh
+++ src/flamenco/runtime/run_bpf_tests.sh
@@ -2,8 +2,34 @@
 
 # this assumes the test_runtime has already been built
 
-if [ ! -e v13-contract-ledger ]; then
-  wget -q https://github.com/firedancer-io/firedancer-testbins/raw/main/v13-contract-ledger.tar.gz -O - | tar zxf -
+LEDGER="v13-contract-ledger"
+VERBOSE=NO
+POSITION_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -l|--ledger)
+       LEDGER="$2"
+       shift
+       shift
+       ;;
+    -v|--verbose)
+       VERBOSE=YES
+       shift
+       ;;
+    -*|--*)
+       echo "unknown option $1"
+       exit 1
+       ;;
+    *)
+       POSITION_ARGS+=("$1")
+       shift
+       ;;
+  esac
+done
+
+if [ ! -e $LEDGER ]; then
+  curl -o - -L -q https://github.com/firedancer-io/firedancer-testbins/raw/main/$LEDGER.tar.gz | tar zxf -
 fi
 
 # We determine these values by
@@ -24,19 +50,15 @@ fi
 
 # set -x
 
-if [ -e /mnt/.fd/.gigantic/test_ledger_wksp ]; then
-    build/linux/gcc/x86_64/bin/fd_wksp_ctl delete test_ledger_wksp
+if [ $VERBOSE == "YES" ]; then
+  set -x
 fi
-build/linux/gcc/x86_64/bin/fd_wksp_ctl new test_ledger_wksp 5 gigantic 0 0666
 
-build/linux/gcc/x86_64/bin/fd_frank_ledger --wksp test_ledger_wksp --reset true --persist testfunk --rocksdb v13-contract-ledger/rocksdb --cmd ingest --gaddrout testgaddr --indexmax 10000 --txnmax 100
+build/linux/gcc/x86_64/bin/fd_frank_ledger --rocksdb $LEDGER/rocksdb --genesis $LEDGER/genesis.bin --cmd ingest --indexmax 10000 --txnmax 100 --backup test_ledger_backup
 
-build/linux/gcc/x86_64/unit-test/test_runtime --ledger v13-contract-ledger --wksp test_ledger_wksp --gaddr `cat testgaddr` --cmd replay --start-slot 0 --end-slot 30 --validate true --persist testfunk --log_level 10
+build/linux/gcc/x86_64/unit-test/test_runtime --load test_ledger_backup --cmd replay --end-slot 22 --confirm_hash J1SVLicejnC67wzPpP3W3c7XqtSPj6JPSDDDuufsn2GQ    --confirm_signature 2  --confirm_last_block HyqLfiMCsXYwDTujyDLKwrTCDmkFtCoid5ehH6PDdzjt --validate true
 
 status=$?
-
-build/linux/gcc/x86_64/bin/fd_wksp_ctl delete test_ledger_wksp
-rm -f testfunk testgaddr
 
 if [ $status -ne 0 ]
 then
