40ff1812cd9e342bb438c1852f5ef13f0d04f774
Richard Patel <ripatel@jumptrading.com>
2023-08-01 16:18:13 +0000

    nanopb: add encode support

--- src/flamenco/nanopb/Local.mk
+++ src/flamenco/nanopb/Local.mk
@@ -1,2 +1,2 @@
-$(call add-hdrs,pb_firedancer.h pb_common.h pb_decode.h,fd_flamenco)
-$(call add-objs,pb_common pb_decode,fd_flamenco)
+$(call add-hdrs,pb_firedancer.h pb_common.h pb_decode.h fd_encode.h,fd_flamenco)
+$(call add-objs,pb_common pb_decode pb_encode,fd_flamenco)
