06b491777d5cf58ea5acea0cd32b1a6fdb7976a6
Logan Lamb <llamb@jumptrading.com>
2023-08-01 19:54:59 +0000

    CI test to confirm fd_frank_quic is working

--- src/tango/quic/tests/Local.mk
+++ src/tango/quic/tests/Local.mk
@@ -7,6 +7,7 @@ $(call make-unit-test,test_quic_streams,test_quic_streams,fd_aio fd_ballet fd_ta
 $(call make-unit-test,test_quic_conn,test_quic_conn,fd_aio fd_quic fd_ballet fd_tango fd_util)
 $(call make-unit-test,test_quic_server,test_quic_server,fd_aio fd_ballet fd_quic fd_tango fd_util)
 $(call make-unit-test,test_quic_client_flood,test_quic_client_flood,fd_aio fd_quic fd_ballet fd_tango fd_util)
+$(call make-unit-test,test_quic_txn,test_quic_txn,fd_aio fd_quic fd_ballet fd_tango fd_util)
 $(call make-unit-test,test_quic_bw,test_quic_bw,fd_aio fd_quic fd_ballet fd_tango fd_util)
 $(call make-unit-test,test_quic_handshake,test_handshake,fd_aio fd_ballet fd_quic fd_util)
 $(call make-unit-test,test_quic_crypto,test_crypto,fd_quic fd_ballet fd_util)
