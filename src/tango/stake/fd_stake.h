5935cc346ccf20e38b29351e855e1a476bc2953c
Charles Li <charles.dt.li@gmail.com>
2023-08-06 01:35:18 +0000

    fix: memory layout issues with fd stake

--- src/tango/stake/fd_stake.h
+++ src/tango/stake/fd_stake.h
@@ -56,16 +56,16 @@ fd_stake_align( void );
 ulong
 fd_stake_footprint( int lg_slot_cnt );
 
-/* fd_stake_new formats an unused memory region for use as an fd_stake_t. `nodes_off` points to the
+/* fd_stake_new formats an unused memory region for use as a stake object. `nodes_off` points to the
    first slot, which is past the map header. The layout is diagrammed below:
 
-   ------------------ <- (fd_stake_t * stake)
+   ------------------ <- (fd_stake_t * stake)  // returned by fd_stake_new
    private hdr region
    ------------------
    nodes map region
 
    .....   hdr  .....
-   ..... node 0 ..... <- (fd_stake_t * stake) + nodes_off
+   ..... node 0 .....
    ..... node 1 .....
    ..... ...... .....
    ..... node n .....
@@ -74,15 +74,26 @@ fd_stake_footprint( int lg_slot_cnt );
 void *
 fd_stake_new( void * mem, int lg_slot_cnt );
 
-/* fd_stake_t is designed to be shared across multiple joins.
+/* fd_stake_join joins the caller to the stake object.
 
-   Therefore, it maintains an offset for the staked nodes region (which itself requires a join),
-   which is located within the stake region itself. It uses an offset in lieu of pointers, because
-   the pointer addresses would otherwise be local to each joined process. Note this is a pointer to
-   the first slot in the map, rather than the start of the map region itself, as `fd_map_dynamic`
-   expects slot pointers in its API.
+   fd_stake_t is designed to be shared across multiple joins. Therefore, it maintains an offset for
+   the staked nodes region (which itself requires a join), which is located within the stake region
+   itself. It uses an offset in lieu of pointers, because the pointer addresses would otherwise be
+   local to each joined process. Note this is a pointer to the first slot in the map, rather than
+   the start of the map region itself, as `fd_map_dynamic` expects slot pointers in its API.
 
-   See `fd_stake_new` for the layout. */
+  -------------------
+   private hdr region
+   ------------------
+   nodes map region
+
+   .....   hdr  .....
+   ..... node 0 ..... <- (fd_stake_t * stake) + nodes_off  // set by fd_stake_join
+   ..... node 1 .....
+   ..... ...... .....
+   ..... node n .....
+
+   ------------------ */
 fd_stake_t *
 fd_stake_join( void * shstake );
 
@@ -95,9 +106,28 @@ fd_stake_version_laddr( fd_stake_t * stake );
 fd_stake_node_t *
 fd_stake_nodes_laddr( fd_stake_t * stake );
 
-/* updates using the (serialized) staked nodes from the labs client */
+/* fd_stake_read performs an mvcc-fenced read of the stake structure. `fd_stake_t` is a single-producer,
+ * multiple-consumer concurrency structure and an odd version number indicates the writer is
+ * currently writing to the structure. */
+fd_stake_t *
+fd_stake_read( fd_stake_t * stake);
+
+/* fd_stake_write performs an mvcc-fenced write of the stake structure. Assumes there is a single
+   writer and does not check for safe concurrency with multiple writers.
+
+   `data` is a pointer to a bincode-serialized byte representation of stakes from the labs client.
+
+   Serialization format:
+   -----------
+   total stake  (8 bytes, le)
+   node0 pubkey (32 bytes, le)
+   node0 stake  (8 bytes, le)
+   node1 pubkey (32 bytes, le)
+   node1 stake  (8 bytes, le)
+   ...
+   ----------- */
 void
-fd_stake_update( fd_stake_t * stake, uchar * staked_nodes_ser, ulong sz );
+fd_stake_write( fd_stake_t * stake, uchar * data, ulong sz );
 
 void
 fd_stake_dump( fd_stake_t * stake );
