cc17df378c7f2591838201f58642e6e0396a97d5
Richard Patel <ripatel@jumptrading.com>
2023-08-08 20:10:10 +0000

    Add siphash13 test

--- src/ballet/siphash13/Local.mk
+++ src/ballet/siphash13/Local.mk
@@ -1,2 +1,4 @@
 $(call add-hdrs,fd_siphash13.h)
 $(call add-objs,fd_siphash13,fd_ballet)
+$(call make-unit-test,test_siphash13,test_siphash13,fd_ballet fd_util)
+$(call run-unit-test,test_siphash13)
