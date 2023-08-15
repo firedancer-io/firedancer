9515f6466021fdecfe3835375c4e44b95da581f2
Charles Li <charles.dt.li@gmail.com>
2023-08-15 15:09:40 +0000

    chore: cargo fmt

--- ffi/rust/firedancer-sys/src/tango/mod.rs
+++ ffi/rust/firedancer-sys/src/tango/mod.rs
@@ -3,15 +3,15 @@ mod dcache;
 mod fctl;
 mod fseq;
 mod mcache;
+mod stake;
 mod tcache;
 mod xdp;
-mod stake;
 
 pub use cnc::*;
 pub use dcache::*;
 pub use fctl::*;
 pub use fseq::*;
 pub use mcache::*;
+pub use stake::*;
 pub use tcache::*;
 pub use xdp::*;
-pub use stake::*;
--- ffi/rust/firedancer-sys/src/tango/stake.rs
+++ ffi/rust/firedancer-sys/src/tango/stake.rs
@@ -1,13 +1,13 @@
 pub use crate::generated::{
-    fd_stake_t,
     fd_stake_align,
+    fd_stake_deser,
+    fd_stake_dump,
     fd_stake_footprint,
     fd_stake_join,
     fd_stake_new,
+    fd_stake_read,
+    fd_stake_t,
     fd_stake_version,
     fd_stake_version_laddr,
-    fd_stake_deser,
-    fd_stake_read,
-    fd_stake_dump,
-    FD_STAKE_ALIGN
+    FD_STAKE_ALIGN,
 };
