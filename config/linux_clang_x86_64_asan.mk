76ca9d28f3c96bbc0a2149410095b7c43b5e467e
Richard Patel <ripatel@jumptrading.com>
2023-08-17 16:44:05 +0000

    config: support -O0 for Clang ASan

--- config/linux_clang_x86_64_asan.mk
+++ config/linux_clang_x86_64_asan.mk
@@ -2,6 +2,10 @@ BUILDDIR:=linux/clang/x86_64_asan
 include config/linux_clang_x86_64.mk
 include config/with-ffi.mk
 
+ifneq ($(DISABLE_OPTIMIZATION),)
+CPPFLAGS+=-O0
+endif
+
 FD_HAS_ASAN:=1
 CPPFLAGS+=-DFD_HAS_ASAN=1
 
