eb57f188233a2ae1fd30b4ba78b5c406e420ddae
Josh Siegel <83408952+jumpsiegel@users.noreply.github.com>
2023-08-18 11:51:09 +0000

    master-merge: improve

--- src/ballet/base64/fd_base64.h
+++ src/ballet/base64/fd_base64.h
@@ -1,15 +1,19 @@
 #ifndef HEADER_fd_src_ballet_base64_fd_base64_h
 #define HEADER_fd_src_ballet_base64_fd_base64_h
 
-/* fd_base64.h provides methods for converting between binary and base64. */
-#include "../fd_ballet_base.h"
-int
-fd_base64_decode( const char *  encoded,
-                  uchar *       decoded );
+/* fd_base64.h provides methods for converting between binary and
+   base64. */
 
-ulong
-fd_base64_encode( const uchar * data,
-                  int           data_len,
-                  char *        encoded );
+#include "../fd_ballet_base.h"
+
+FD_PROTOTYPES_BEGIN
+
+long
+fd_base64_decode( uchar *      out,
+                  char const * in,
+                  ulong        in_len );
+
+FD_PROTOTYPES_END
 
 #endif /* HEADER_fd_src_ballet_base64_fd_base64_h */
+
