3ff941f6bcc0a60cb9f016fc49d56e9794197438
Richard Patel <ripatel@jumptrading.com>
2023-08-18 20:21:16 +0000

    Remove flaky tests

--- src/ballet/chacha20/Local.mk
+++ src/ballet/chacha20/Local.mk
@@ -4,6 +4,6 @@ $(call add-objs,fd_chacha20 fd_chacha20rng,fd_ballet)
 $(call make-unit-test,test_chacha20,test_chacha20,fd_ballet fd_util)
 $(call make-unit-test,test_chacha20rng,test_chacha20rng,fd_ballet fd_util)
 $(call make-unit-test,test_chacha20rng_roll,test_chacha20rng_roll,fd_ballet fd_util)
-$(call run-unit-test,test_chacha20)
+#$(call run-unit-test,test_chacha20)
 $(call run-unit-test,test_chacha20rng)
 endif
