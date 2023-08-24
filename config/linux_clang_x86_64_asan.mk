d7d7b511e9286ef574a1153d625c1e4f8e3d55fb
Josh Siegel <83408952+jumpsiegel@users.noreply.github.com>
2023-08-24 10:04:14 +0000

    cleanup_flags: fun with flags

--- config/linux_clang_x86_64_asan.mk
+++ config/linux_clang_x86_64_asan.mk
@@ -1,13 +1,8 @@
 BUILDDIR:=linux/clang/x86_64_asan
 include config/linux_clang_x86_64.mk
 
-ifneq ($(DISABLE_OPTIMIZATION),)
-CPPFLAGS+=-O0
-endif
-
 FD_HAS_ASAN:=1
 CPPFLAGS+=-DFD_HAS_ASAN=1
 
 CPPFLAGS+=-fsanitize=address
 LDFLAGS+=-fsanitize=address
-
