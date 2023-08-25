2654a4e980d6dd70935f5843bc39196c95fbdd47
Richard Patel <ripatel@jumptrading.com>
2023-08-25 04:04:49 +0000

    Regenerate native program tests (sob emoji)

---
+++ src/flamenco/runtime/tests/generated/test_native_programs_2600.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "../fd_tests.h"
+#include "../../../../ballet/base58/fd_base58.h"
+extern int fd_executor_test_suite_check_filter(fd_executor_test_suite_t *suite, fd_executor_test_t *test);
+#include "test_native_programs_2600.h"
+#include "test_native_programs_2601.h"
+#include "test_native_programs_2602.h"
+#include "test_native_programs_2603.h"
+#include "test_native_programs_2604.h"
+#include "test_native_programs_2605.h"
+#include "test_native_programs_2606.h"
+#include "test_native_programs_2607.h"
+#include "test_native_programs_2608.h"
+#include "test_native_programs_2609.h"
+#include "test_native_programs_2610.h"
