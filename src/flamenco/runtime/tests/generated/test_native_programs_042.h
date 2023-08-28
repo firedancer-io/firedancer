11bb135938a40a30f0f767f4f3cf07abe2caa65c
arjain4 <123000778+arjain4@users.noreply.github.com>
2023-08-28 14:26:44 -0500

    updated tests (#84)

--- src/flamenco/runtime/tests/generated/test_native_programs_042.h
+++ src/flamenco/runtime/tests/generated/test_native_programs_042.h
@@ -2,8 +2,8 @@
 int test_42(fd_executor_test_suite_t *suite) {
   fd_executor_test_t test;
   fd_memset( &test, 0, FD_EXECUTOR_TEST_FOOTPRINT );
-  test.disable_cnt = 161;
-  test.bt = "   2: solana_vote_program::vote_processor::tests::test_vote_process_instruction_decode_bail             at ./src/vote_processor.rs:548:9   3: solana_vote_program::vote_processor::tests::test_vote_process_instruction_decode_bail::{{closure}}             at ./src/vote_processor.rs:547:52   4: core::ops::function::FnOnce::call_once             at /rustc/83964c156db1f444050a38b2498dbd0da6d5d503/library/core/src/ops/function.rs:250:5   5: core::ops::function::FnOnce::call_once             at /rustc/83964c156db1f444050a38b2498dbd0da6d5d503/library/core/src/ops/function.rs:250:5";
+  test.disable_cnt = 0;
+  test.bt = "   2: solana_vote_program::vote_processor::tests::test_vote_process_instruction_decode_bail             at ./src/vote_processor.rs:548:9   3: solana_vote_program::vote_processor::tests::test_vote_process_instruction_decode_bail::{{closure}}             at ./src/vote_processor.rs:547:52   4: core::ops::function::FnOnce::call_once             at /rustc/84c898d65adf2f39a5a98507f1fe0ce10a2b8dbc/library/core/src/ops/function.rs:250:5   5: core::ops::function::FnOnce::call_once             at /rustc/84c898d65adf2f39a5a98507f1fe0ce10a2b8dbc/library/core/src/ops/function.rs:250:5";
   test.test_name = "vote_processor::tests::test_vote_process_instruction_decode_bail";
   test.test_number = 42;
   test.sysvar_cache.clock = "";
@@ -19,10 +19,6 @@ int test_42(fd_executor_test_suite_t *suite) {
   fd_executor_test_acc_t* test_accs = fd_alloca( 1UL, FD_EXECUTOR_TEST_ACC_FOOTPRINT * test_accs_len );
   fd_memset( test_accs, 0, FD_EXECUTOR_TEST_ACC_FOOTPRINT * test_accs_len );
 
-
-  uchar disabled_features[] = { 0,1,10,100,101,102,103,104,105,106,107,108,109,11,110,111,112,113,114,115,116,117,118,119,12,120,121,122,123,124,125,126,127,128,129,13,130,131,132,133,134,135,136,137,138,139,14,140,141,142,143,144,145,146,147,148,149,15,150,151,152,153,154,155,156,157,158,159,16,160,17,18,19,2,20,21,22,23,24,25,26,27,28,29,3,30,31,32,33,34,35,36,37,38,39,4,40,41,42,43,44,45,46,47,48,49,5,50,51,52,53,54,55,56,57,58,59,6,60,61,62,63,64,65,66,67,68,69,7,70,71,72,73,74,75,76,77,78,79,8,80,81,82,83,84,85,86,87,88,89,9,90,91,92,93,94,95,96,97,98,99 };
-  test.disable_feature = disabled_features;
-            
  // {'clock': '', 'epoch_schedule': '', 'epoch_rewards': '', 'fees': '', 'rent': '', 'slot_hashes': '', 'recent_blockhashes': '', 'stake_history': '', 'last_restart_slot': ''}
   fd_base58_decode_32( "Vote111111111111111111111111111111111111111",  (unsigned char *) &test.program_id);
   static uchar const fd_flamenco_native_prog_test_42_raw[] = { 0x00,0x00,0x00,0x01,0x01,0x07,0x61,0x48,0x1d,0x35,0x74,0x74,0xbb,0x7c,0x4d,0x76,0x24,0xeb,0xd3,0xbd,0xb3,0xd8,0x35,0x5e,0x73,0xd1,0x10,0x43,0xfc,0x0d,0xa3,0x53,0x80,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x01,0x00,0x00,0x00 };
---
+++ src/flamenco/runtime/tests/generated/test_native_programs_2645.h
@@ -0,0 +1,45 @@
+#include "../fd_tests.h"
+int test_2645(fd_executor_test_suite_t *suite) {
+  fd_executor_test_t test;
+  fd_memset( &test, 0, FD_EXECUTOR_TEST_FOOTPRINT );
+  test.disable_cnt = 0;
+  test.bt = "   2: solana_bpf_loader_program::tests::test_bpf_loader_write             at ./src/lib.rs:1803:9   3: solana_bpf_loader_program::tests::test_bpf_loader_write::{{closure}}             at ./src/lib.rs:1782:32   4: core::ops::function::FnOnce::call_once             at /rustc/84c898d65adf2f39a5a98507f1fe0ce10a2b8dbc/library/core/src/ops/function.rs:250:5   5: core::ops::function::FnOnce::call_once             at /rustc/84c898d65adf2f39a5a98507f1fe0ce10a2b8dbc/library/core/src/ops/function.rs:250:5";
+  test.test_name = "tests::test_bpf_loader_write";
+  test.test_number = 2645;
+  test.sysvar_cache.clock = "";
+  test.sysvar_cache.epoch_schedule = "";
+  test.sysvar_cache.epoch_rewards = "";
+  test.sysvar_cache.fees = "";
+  test.sysvar_cache.rent = "";
+  test.sysvar_cache.slot_hashes = "";
+  test.sysvar_cache.stake_history = "";
+  test.sysvar_cache.slot_history = "";
+  if (fd_executor_test_suite_check_filter(suite, &test)) return -9999;
+  ulong test_accs_len = 1;
+  fd_executor_test_acc_t* test_accs = fd_alloca( 1UL, FD_EXECUTOR_TEST_ACC_FOOTPRINT * test_accs_len );
+  fd_memset( test_accs, 0, FD_EXECUTOR_TEST_ACC_FOOTPRINT * test_accs_len );
+
+ // {'clock': '', 'epoch_schedule': '', 'epoch_rewards': '', 'fees': '', 'rent': '', 'slot_hashes': '', 'recent_blockhashes': '', 'stake_history': '', 'last_restart_slot': ''}
+  fd_executor_test_acc_t* test_acc = test_accs;
+  fd_base58_decode_32( "111111J5FnSBEZ2KyPFeh8CEUiLPZr6vS4ScMa7Pd",  (uchar *) &test_acc->pubkey);
+  fd_base58_decode_32( "BPFLoader2111111111111111111111111111111111",  (uchar *) &test_acc->owner);
+  fd_base58_decode_32( "BPFLoader2111111111111111111111111111111111",  (uchar *) &test_acc->result_owner);
+  test_acc->lamports        = 1UL;
+  test_acc->result_lamports = 1UL;
+  test_acc->executable      = 0;
+  test_acc->result_executable= 0;
+  test_acc->rent_epoch      = 0;
+  test_acc->result_rent_epoch      = 0;
+  test_acc++;
+  fd_base58_decode_32( "BPFLoader2111111111111111111111111111111111",  (unsigned char *) &test.program_id);
+  static uchar const fd_flamenco_native_prog_test_2645_raw[] = { 0x00,0x00,0x00,0x01,0x02,0x00,0x00,0x00,0x00,0x00,0x00,0x09,0x9d,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x02,0xa8,0xf6,0x91,0x4e,0x88,0xa1,0x6e,0x39,0x5a,0xe1,0x28,0x94,0x8f,0xfa,0x69,0x56,0x93,0x37,0x68,0x18,0xdd,0x47,0x43,0x52,0x21,0xf3,0xc6,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x01,0x01,0x01,0x00,0x13,0x00,0x00,0x00,0x00,0x03,0x00,0x00,0x00,0x03,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x01,0x02,0x03 };
+  test.raw_tx = fd_flamenco_native_prog_test_2645_raw;
+  test.raw_tx_len = 125UL;
+  test.expected_result = -8;
+  test.custom_err = 0;
+
+  test.accs_len = test_accs_len;
+  test.accs = test_accs;
+
+  return fd_executor_run_test( &test, suite );
+}
