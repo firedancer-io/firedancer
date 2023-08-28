5ddeb2a1ff5b5027c0fd36a480aeca5ebda7c600
Logan Lamb <llamb@jumptrading.com>
2023-08-28 16:44:21 +0000

    update test_mvcc.c to pass

--- src/tango/mvcc/test_mvcc.c
+++ src/tango/mvcc/test_mvcc.c
@@ -9,17 +9,17 @@ main( int argc, char ** argv ) {
 
   fd_mvcc_t mvcc = { .version = 0 };
   FD_TEST( fd_mvcc_begin_read( &mvcc ) == 0 );
-  FD_TEST( fd_mvcc_end_read( &mvcc ) != 0 );
+  FD_TEST( fd_mvcc_end_read( &mvcc ) == 0 );
 
-  FD_TEST( fd_mvcc_begin_write( &mvcc ) == 1 );
+  FD_TEST( fd_mvcc_begin_write( &mvcc ) == 0 );
   FD_TEST( fd_mvcc_begin_read( &mvcc ) == 1 );
   FD_TEST( fd_mvcc_end_read( &mvcc ) == 1 );
-  FD_TEST( fd_mvcc_end_write( &mvcc ) == 2 );
+  FD_TEST( fd_mvcc_end_write( &mvcc ) == 1 );
 
   FD_TEST( fd_mvcc_begin_read( &mvcc ) == 2 );
-  FD_TEST( fd_mvcc_begin_write( &mvcc ) == 3 );
+  FD_TEST( fd_mvcc_begin_write( &mvcc ) == 2 );
   FD_TEST( fd_mvcc_end_read( &mvcc ) == 3 );
-  FD_TEST( fd_mvcc_end_write( &mvcc ) == 4 );
+  FD_TEST( fd_mvcc_end_write( &mvcc ) == 3 );
 
   FD_LOG_NOTICE( ( "pass" ) );
   fd_halt();
