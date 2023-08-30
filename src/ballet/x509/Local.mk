190392370e70cc46148981dff0f6e90674bf3a9d
Richard Patel <ripatel@jumptrading.com>
2023-08-30 02:07:34 +0000

    x509: import ANSSI x509-parser

--- src/ballet/x509/Local.mk
+++ src/ballet/x509/Local.mk
@@ -1,2 +1,6 @@
+$(call add-hdrs,fd_x509_cert_parser.h fd_x509_common.h fd_x509_config.h fd_x509_utils.h)
+$(call add-objs,fd_x509_cert_parser fd_x509_common,fd_ballet)
+ifdef FD_HAS_OPENSSL
 $(call add-hdrs,fd_x509_openssl.h)
 $(call add-objs,fd_x509_openssl,fd_ballet)
+endif
