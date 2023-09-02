a35cac2f933997454421c0b3b92fc60e436bb394
Richard Patel <ripatel@jumptrading.com>
2023-09-02 23:51:37 +0000

    x509: depfree cert gen

--- src/ballet/x509/Local.mk
+++ src/ballet/x509/Local.mk
@@ -1,5 +1,13 @@
+# X.509 cert parser
 $(call add-hdrs,fd_x509_cert_parser.h fd_x509_common.h fd_x509_config.h fd_x509_utils.h)
 $(call add-objs,fd_x509_cert_parser fd_x509_common,fd_ballet)
+
+# X.509 mock cert generator
+$(call add-hdrs,fd_x509_mock.h)
+$(call add-objs,fd_x509_mock,fd_ballet)
+$(call make-unit-test,test_x509_mock,test_x509_mock,fd_ballet fd_util)
+$(call run-unit-test,test_x509_mock)
+
 ifdef FD_HAS_OPENSSL
 $(call add-hdrs,fd_x509_openssl.h)
 $(call add-objs,fd_x509_openssl,fd_ballet)
