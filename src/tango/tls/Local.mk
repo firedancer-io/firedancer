5d5035854897d5c146d2bff830b146e51d342a62
Richard Patel <ripatel@jumptrading.com>
2023-09-04 05:14:11 +0000

    tls: add fuzz_tls_msg_parser

--- src/tango/tls/Local.mk
+++ src/tango/tls/Local.mk
@@ -3,6 +3,7 @@ $(call add-hdrs,fd_tls.h fd_tls_proto.h fd_tls_asn1.h fd_tls_estate_srv.h fd_tls
 $(call add-objs,fd_tls fd_tls_proto fd_tls_asn1,fd_tls)
 $(call make-unit-test,test_tls,test_tls,fd_tls fd_ballet fd_util)
 $(call run-unit-test,test_tls)
+$(call fuzz-test,fuzz_tls_msg_parser,fuzz_tls_msg_parser,fd_tls fd_ballet fd_util)
 ifdef FD_HAS_OPENSSL
 $(call make-unit-test,test_tls_openssl,test_tls_openssl,fd_quic fd_tls fd_ballet fd_util)
 $(call run-unit-test,test_tls_openssl)
