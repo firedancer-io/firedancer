8aeda8c2de20258b144c5c82f15b7b7cfd9e84a5
Richard Patel <ripatel@jumptrading.com>
2023-09-06 05:13:39 +0000

    app: update fd_quic API usages

--- src/app/fdctl/Local.mk
+++ src/app/fdctl/Local.mk
@@ -6,7 +6,7 @@ ifdef FD_HAS_DOUBLE
 .PHONY: fdctl run monitor cargo
 
 $(call add-objs,main1 config security utility run/run run/tiles/tiles run/tiles/net run/tiles/netmux run/tiles/dedup run/tiles/pack run/tiles/quic run/tiles/verify run/tiles/bank keygen ready monitor/monitor monitor/helper configure/configure configure/large_pages configure/sysctl configure/shmem configure/xdp configure/xdp_leftover configure/ethtool configure/workspace_leftover configure/workspace,fd_fdctl)
-$(call make-bin-rust,fdctl,main,fd_fdctl fd_disco fd_ballet fd_tango fd_util fd_quic solana_validator_fd)
+$(call make-bin-rust,fdctl,main,fd_fdctl fd_disco fd_quic fd_tls fd_ballet fd_tango fd_util solana_validator_fd)
 $(OBJDIR)/obj/app/fdctl/configure/xdp.o: src/tango/xdp/fd_xdp_redirect_prog.o
 $(OBJDIR)/obj/app/fdctl/config.o: src/app/fdctl/config/default.toml
 
