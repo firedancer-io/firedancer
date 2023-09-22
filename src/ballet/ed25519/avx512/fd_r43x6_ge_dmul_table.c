23e1fd6dd151b3c4236a61021824100019416921
Kevin J Bowers <kbowers@jumptrading.com>
2023-09-21 19:05:13 -0500

    AVX512 accelerated ED25519 group element operations
    `fd_r43x6_ge.h` represents a curve point / group element in extended
    homogenous coordinates (X,Y,Z,T) as a FD_R43X6_QUAD.  Highed tuned APIs
    for low level manipulations (e.g. GE_ADD, GE_DBL) provided as are high
    level algorithms that (e.g. SMUL_BASE) form the "inner loop" of
    protocols like ED25519.
    
    E.g.  Performance of fd_ed25519_public_from_private (this is almost a
    pure "SMUL_BASE" so it is a good indicator of both realistic end-to-end
    performance and of group element operation performance) on a 2.3 GHz
    icelake-server core under gcc-12 circa 2023 Sep:
    
      using fd_r43x6_ge_smul_base_large:  ~9.2 us ("vartime" style)
      using fd_r43x6_ge_smul_base_small: ~11.3 us (w/timing attack mitigations)
    
    For reference:
    
      existing scalar implementation: ~46.0 us ("small" style impl w/timing attack mitigations)
      existing AVX-2  implementation: ~24.9 us ("small" style impl w/timing attack mitigations)
    
    That is, ~4-5x faster than the existing scalar implementation and
    ~2.2-2.7x faster than the existing AVX-2 accelerated implementation.
    YMMV for other processors but performance should roughly scale with core
    clock speed.

---
+++ src/ballet/ed25519/avx512/fd_r43x6_ge_dmul_table.c
@@ -0,0 +1,41 @@
+#include "../../fd_ballet.h"
+#include "fd_r43x6_ge.h"
+
+#include <stdio.h>
+
+int
+main( int     argc,
+      char ** argv ) {
+  fd_boot( &argc, &argv );
+
+  /* This is the code that was used to generate the table for
+     dmul_sparse. */
+
+  char const * path = fd_env_strip_cmdline_cstr( &argc, &argv, "--path", NULL, NULL );
+  int          max  = fd_env_strip_cmdline_int(  &argc, &argv, "--max",  NULL, 2047 );
+
+  if( !path      ) FD_LOG_ERR(( "--path not specified" ));
+  if( !(max & 1) ) FD_LOG_ERR(( "--max should be odd" ));
+
+  ulong sz = ((ulong)(3*(max+1)))*sizeof(wwl_t);
+
+  FD_LOG_NOTICE(( "Using --path %s --max %i (sz %luB)", path, max, sz ));
+
+  FD_LOG_NOTICE(( "Computing table" ));
+
+  wwl_t * table = (wwl_t *)fd_alloca( 128UL, sz );
+  FD_R43X6_QUAD_DECL( B );  FD_R43X6_GE_ONE( B );
+  fd_r43x6_ge_sparse_table( table, B03, B14, B25, max );
+
+  FD_LOG_NOTICE(( "Saving table" ));
+
+  FILE * file = fopen( path, "wb" );
+  if( FD_UNLIKELY( !file ) ) FD_LOG_ERR(( "fopen faield" ));
+  if( FD_UNLIKELY( fwrite( table, sz, 1UL, file )!=1UL ) ) FD_LOG_ERR(( "fwrite failed" ));
+  if( FD_UNLIKELY( fclose( file ) ) ) FD_LOG_WARNING(( "fclose failed; attempting to continue" ));
+
+  FD_LOG_NOTICE(( "pass" ));
+
+  fd_halt();
+  return 0;
+}
