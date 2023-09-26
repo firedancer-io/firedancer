c663cd7ba97072639186fe5aeb934d6dc067292a
Philip Taffet <phtaffet@jumptrading.com>
2023-09-26 19:56:02 +0000

    shred: add shred destination computation

--- src/ballet/shred/Local.mk
+++ src/ballet/shred/Local.mk
@@ -1,4 +1,4 @@
 $(call add-hdrs,fd_shred.h)
 $(call add-objs,fd_shred,fd_ballet)
-$(call make-unit-test,test_shred,test_shred,fd_ballet fd_util)
+$(call make-unit-test,test_shred,test_shred,fd_ballet fd_util fd_reedsol)
 $(call run-unit-test,test_shred,)
