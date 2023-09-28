18fbb6d22d3d17b7e50e72379070b350a514f2b1
Fire30 <tj@tcorley.me>
2023-09-28 10:24:58 -0400

    Disable -Wdangling-pointer in GCC 13
    Compilation fails with the following error:
    
    ```
    src/util/tpool/../scratch/fd_scratch.h:176:32: error: storing the
    address of local variable ‘scratch_frame’ in ‘fd_scratch_private_frame’
    [-Werror=dangling-pointer=]
      176 |   fd_scratch_private_frame     = (ulong *)fmem;
    ```
    
    This is due to the fact that we are storing a stack pointer to a global
    variable. However there is a matching call to `fd_scratch_detach` that
    ensures the pointer is cleared.
    
    To fix this in the brutality configuration we downgrade the error into
    a warning. In the regular configuration we just disable the warning.

--- config/with-gcc.mk
+++ config/with-gcc.mk
@@ -9,3 +9,9 @@ CPPFLAGS+=-DFD_USING_GCC=1
 
 FD_USING_GCC:=1
 
+# Check for GCC version and disable -Wdangling-pointer if we are using 
+# gcc 13 as it is giving a false positive 
+ifeq "$(CC_MAJOR_VERSION)" "13"
+CPPFLAGS+="-Wno-dangling-pointer"
+endif
+
