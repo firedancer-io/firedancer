b350299758fe02b9895f900bc0efded91d902fb9
nbridge-jump <123656380+nbridge-jump@users.noreply.github.com>
2023-10-02 14:45:43 -0500

    Update src/tango/quic/tests/txn/test-transactions.sh
    Co-authored-by: llamb-jump <135640272+llamb-jump@users.noreply.github.com>

--- src/tango/quic/tests/txn/test-transactions.sh
+++ src/tango/quic/tests/txn/test-transactions.sh
@@ -9,6 +9,6 @@ NUM_TRANSACTIONS=${1:-1000}
 TX_FILE=${2:-tx}
 head -n $NUM_TRANSACTIONS $TX_FILE | while read line; do
     echo $line
-    sudo nsenter --net=/var/run/netns/veth_test_xdp_1 fddeva txn --payload-base64-encoded $line 2>&1 | tee -a /tmp/test-transactions.log
+    sudo nsenter --net=/var/run/netns/veth_test_xdp_1 fddev txn --payload-base64-encoded $line 2>&1 | tee -a /tmp/test-transactions.log
 done
 echo " $(grep -E 'rc 1|rc 2|rc 3' /tmp/test-transactions.log | wc -l) / $NUM_TRANSACTIONS successfully transmitted by clients"
