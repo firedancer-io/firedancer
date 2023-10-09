127ae2874428958fba8f3fe1d70ee7ff66195cf4
Richard Patel <ripatel@jumptrading.com>
2023-10-09 02:24:38 +0000

    util: add fd_tar

--- src/util/archive/Local.mk
+++ src/util/archive/Local.mk
@@ -1,5 +1,5 @@
-$(call add-hdrs,fd_ar.h)
-$(call add-objs,fd_ar,fd_util)
+$(call add-hdrs,fd_ar.h fd_tar.h)
+$(call add-objs,fd_ar fd_tar,fd_util)
 $(call make-unit-test,test_ar,test_ar,fd_util)
 $(call run-unit-test,test_ar,)
 
