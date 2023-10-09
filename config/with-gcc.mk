644c89728ef40a09f3f35ba30ce3c7c918b3978d
Kevin J Bowers <kbowers@jumptrading.com>
2023-10-09 10:14:55 -0500

    Tweaked tpool to eliminate dangling-pointer false positive under gcc-13
    This allows dangling pointer checks to be reenabled under gcc-13.  Hat
    tip for to Fire30 in PR #708 for catching this.

--- config/with-gcc.mk
+++ config/with-gcc.mk
@@ -3,7 +3,7 @@ CXX:=g++
 LD:=g++
 
 # See with-clang.mk FD_USING_CLANG for behavior.  FD_USING_GCC and
-# FD_USING_CLANG should not be both set simulateously
+# FD_USING_CLANG should not be both set simultaneously
 
 CPPFLAGS+=-DFD_USING_GCC=1
 
@@ -12,10 +12,4 @@ CPPFLAGS+=-fdiagnostics-color=always
 endif
 
 FD_USING_GCC:=1
-
-# Check for GCC version and disable -Wdangling-pointer if we are using 
-# gcc 13 as it is giving a false positive 
-ifeq "$(CC_MAJOR_VERSION)" "13"
-CPPFLAGS+="-Wno-dangling-pointer"
-endif
 
