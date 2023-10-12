598884a00b1f6f33cd25da983ce84ed3892748ad
Theodoros Kasampalis <kasampalis.theodoros@gmail.com>
2023-10-12 14:24:54 -0500

    Fuzzers for hashing algorithms in ballet

--- src/ballet/hmac/Local.mk
+++ src/ballet/hmac/Local.mk
@@ -1,4 +1,5 @@
 $(call add-hdrs,fd_hmac.h)
 $(call add-objs,fd_hmac,fd_ballet)
 $(call make-unit-test,test_hmac,test_hmac,fd_ballet fd_util)
+$(call fuzz-test,fuzz_hmac,fuzz_hmac,fd_ballet fd_util)
 $(call run-unit=test,test_hmac)
---
+++ src/ballet/keccak256/fuzz_keccak256.c
@@ -0,0 +1,50 @@
+#if !FD_HAS_HOSTED
+#error "This target requires FD_HAS_HOSTED"
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../../util/fd_util.h"
+#include "fd_keccak256.h"
+
+int
+LLVMFuzzerInitialize( int  *   argc,
+                      char *** argv ) {
+  /* Set up shell without signal handlers */
+  putenv( "FD_LOG_BACKTRACE=0" );
+  fd_boot( argc, argv );
+  atexit( fd_halt );
+  return 0;
+}
+
+int
+LLVMFuzzerTestOneInput( uchar const * data,
+                        ulong         size ) {
+  // hash single message
+  char const * msg = ( char const * ) data;
+
+  uchar hash1[ 32 ] __attribute__((aligned(32)));
+  uchar hash2[ 32 ] __attribute__((aligned(32)));
+
+  fd_keccak256_t sha[1];
+  if( FD_UNLIKELY( fd_keccak256_init( sha )!=sha ) ) {
+    __builtin_trap();
+  }
+  if( FD_UNLIKELY( fd_keccak256_append( sha, msg, size )!=sha ) ) {
+    __builtin_trap();
+  }
+  if( FD_UNLIKELY( fd_keccak256_fini( sha, hash1 )!=hash1 ) ) {
+    __builtin_trap();
+  }
+
+  if( FD_UNLIKELY( fd_keccak256_hash( data, size, hash2 )!=hash2 ) ) {
+    __builtin_trap();
+  }
+
+  if( FD_UNLIKELY( memcmp( hash1, hash2, 32UL ) ) ) {
+    __builtin_trap();
+  }
+
+  return 0;
+}
