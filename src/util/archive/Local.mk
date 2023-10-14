db65a396a9f323a8a753ce19e131070dd6f110d5
Richard Patel <ripatel@jumptrading.com>
2023-10-14 01:46:19 +0000

    tar: add fuzzer

--- src/util/archive/Local.mk
+++ src/util/archive/Local.mk
@@ -2,4 +2,5 @@ $(call add-hdrs,fd_ar.h fd_tar.h)
 $(call add-objs,fd_ar fd_tar,fd_util)
 $(call make-unit-test,test_ar,test_ar,fd_util)
 $(call run-unit-test,test_ar,)
+$(call fuzz-test,fuzz_tar,fuzz_tar,fd_ballet fd_util)
 
