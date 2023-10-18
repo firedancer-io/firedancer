9189acafcaf248c57653584b4964b9fab400b1e3
Theodoros Kasampalis <kasampalis.theodoros@gmail.com>
2023-10-18 14:37:10 -0500

    Improvements to ballet encoding/decoding fuzzers
    - replaced inline comments
    
    - renamed base58/base64 fuzzer targets, adding roundtrip suffix
    
    - adding fuzzers for decoding garbage inputs with base58/64

--- src/ballet/base64/Local.mk
+++ src/ballet/base64/Local.mk
@@ -1,4 +1,5 @@
 $(call add-hdrs,fd_base64.h)
 $(call add-objs,fd_base64,fd_ballet)
 $(call make-unit-test,test_base64,test_base64,fd_ballet fd_util)
-$(call fuzz-test,fuzz_base64,fuzz_base64,fd_ballet fd_util)
+$(call fuzz-test,fuzz_base64_roundtrip,fuzz_base64_roundtrip,fd_ballet fd_util)
+$(call fuzz-test,fuzz_base64_garbage,fuzz_base64_garbage,fd_ballet fd_util)
