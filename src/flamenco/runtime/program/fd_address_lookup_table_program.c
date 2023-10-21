713ab8c1d72a276d4392274b2bba80b9cb3224db
Richard Patel <ripatel@jumptrading.com>
2023-10-21 12:05:14 +0000

    addrlut create

--- src/flamenco/runtime/program/fd_address_lookup_table_program.c
+++ src/flamenco/runtime/program/fd_address_lookup_table_program.c
@@ -1,26 +1,146 @@
+#include "fd_program_util.h"
 #include "fd_zk_token_proof_program.h"
 #include "../fd_executor.h"
 #include "../context/fd_exec_txn_ctx.h"
 #include "../fd_acc_mgr.h"
+#include "../fd_pubkey_utils.h"
 #include <string.h>
 
 static int
-create_lookup_table( fd_exec_instr_ctx_t         ctx,
+create_lookup_table( fd_exec_instr_ctx_t *       ctx,
                      fd_addrlut_create_t const * create ) {
+
+  /* https://github.com/solana-labs/solana/blob/56ccffdaa5394f179dce6c0383918e571aca8bff/programs/address-lookup-table/src/processor.rs#L58-L62 */
+  fd_borrowed_account_t * lut_acct;
+  int acct_err = fd_instr_ctx_try_borrow_instruction_account( ctx, ctx->txn_ctx, 0, &lut_acct );
+  if( FD_UNLIKELY( acct_err ) ) {
+    /* TODO return code */
+    return FD_EXECUTOR_INSTR_ERR_ACC_BORROW_FAILED;
+  }
+  ulong               lut_lamports = lut_acct->const_meta->info.lamports;
+  fd_pubkey_t const * lut_key      = lut_acct->pubkey;
+  uchar const *       lut_owner    = lut_acct->const_meta->info.owner;
+
+  /* https://github.com/solana-labs/solana/blob/56ccffdaa5394f179dce6c0383918e571aca8bff/programs/address-lookup-table/src/processor.rs#L63-L70 */
+  if( !FD_FEATURE_ACTIVE( ctx->slot_ctx, relax_authority_signer_check_for_lookup_table_creation )
+      && lut_acct->const_meta->dlen != 0UL ) {
+    /* TODO Log: "Table account must not be allocated" */
+    return FD_EXECUTOR_INSTR_ERR_ACC_ALREADY_INITIALIZED;
+  }
+  /* TODO release lut_acct borrow
+     https://github.com/solana-labs/solana/blob/56ccffdaa5394f179dce6c0383918e571aca8bff/programs/address-lookup-table/src/processor.rs#L71 */
+
+  /* https://github.com/solana-labs/solana/blob/56ccffdaa5394f179dce6c0383918e571aca8bff/programs/address-lookup-table/src/processor.rs#L73-L75 */
+  fd_borrowed_account_t * authority_acct;
+  acct_err = fd_instr_ctx_try_borrow_instruction_account( ctx, ctx->txn_ctx, 1, &authority_acct );
+  fd_pubkey_t const * authority_key = authority_acct->pubkey;
+
+  /* https://github.com/solana-labs/solana/blob/56ccffdaa5394f179dce6c0383918e571aca8bff/programs/address-lookup-table/src/processor.rs#L76-L83 */
+  if( !FD_FEATURE_ACTIVE( ctx->slot_ctx, relax_authority_signer_check_for_lookup_table_creation )
+      && !fd_instr_acc_is_signer_idx( ctx->instr, 1 ) ) {
+    /* TODO Log: "Authority account must be a signer" */
+    return FD_EXECUTOR_INSTR_ERR_MISSING_REQUIRED_SIGNATURE;
+  }
+  /* TODO release authority_acct borrow
+     https://github.com/solana-labs/solana/blob/56ccffdaa5394f179dce6c0383918e571aca8bff/programs/address-lookup-table/src/processor.rs#L84 */
+
+  /* https://github.com/solana-labs/solana/blob/56ccffdaa5394f179dce6c0383918e571aca8bff/programs/address-lookup-table/src/processor.rs#L86-L88 */
+  fd_borrowed_account_t * payer_acct;
+  acct_err = fd_instr_ctx_try_borrow_instruction_account( ctx, ctx->txn_ctx, 2, &payer_acct );
+  if( FD_UNLIKELY( acct_err ) ) {
+    /* TODO return code */
+    return FD_EXECUTOR_INSTR_ERR_ACC_BORROW_FAILED;
+  }
+  fd_pubkey_t const * payer_key = payer_acct->pubkey; (void)payer_key;
+
+  /* https://github.com/solana-labs/solana/blob/56ccffdaa5394f179dce6c0383918e571aca8bff/programs/address-lookup-table/src/processor.rs#L89-L92 */
+  if( !fd_instr_acc_is_signer_idx( ctx->instr, 2 ) ) {
+    /* TODO Log: "Payer account must be a signer" */
+    return FD_EXECUTOR_INSTR_ERR_MISSING_REQUIRED_SIGNATURE;
+  }
+
+  /* TODO release payer_acct borrow
+     https://github.com/solana-labs/solana/blob/56ccffdaa5394f179dce6c0383918e571aca8bff/programs/address-lookup-table/src/processor.rs#L93 */
+
+  fd_slot_hashes_t slot_hashes = {0};
+  int slot_hashes_err = fd_sysvar_slot_hashes_read( ctx->slot_ctx, &slot_hashes );
+  if( FD_UNLIKELY( slot_hashes_err ) ) {
+    /* TODO what error to return if sysvar read fails? */
+    return FD_EXECUTOR_INSTR_ERR_GENERIC_ERR;
+  }
+
+  /* TODO Binary search derivation slot in slot hashes sysvar */
+  ulong derivation_slot = 1UL;
+  if( 1 ) {
+    /* TODO Log: {} is not a recent slot */
+    return FD_EXECUTOR_INSTR_ERR_INVALID_INSTR_DATA;
+  }
+
+  /* https://github.com/solana-labs/solana/blob/56ccffdaa5394f179dce6c0383918e571aca8bff/programs/address-lookup-table/src/processor.rs#L109-L118 */
+  fd_pubkey_t derived_tbl_key[1];
+  do {
+    fd_sha256_t sha[1]; fd_sha256_init( sha );
+    fd_sha256_append( sha, authority_key->key, 32UL );
+    fd_sha256_append( sha, &derivation_slot,    8UL );
+    fd_sha256_append( sha, &create->bump_seed,  1UL );
+    fd_sha256_append( sha, fd_solana_address_lookup_table_program_id.key, 32UL );
+    fd_sha256_append( sha, "ProgramDerivedAddress", 21UL );
+    fd_sha256_fini( sha, derived_tbl_key->key );
+  } while(0);
+  if( FD_UNLIKELY( !fd_ed25519_validate_public_key( derived_tbl_key->key ) ) )
+    return FD_EXECUTOR_INSTR_ERR_INVALID_SEEDS;
+
+  /* https://github.com/solana-labs/solana/blob/56ccffdaa5394f179dce6c0383918e571aca8bff/programs/address-lookup-table/src/processor.rs#L120-L127 */
+  if( FD_UNLIKELY( 0!=memcmp( lut_key->key, derived_tbl_key->key, 32UL ) ) ) {
+    /* TODO Log: "Table address must match derived address: {}" */
+    return FD_EXECUTOR_INSTR_ERR_INVALID_ARG;
+  }
+
+  /* https://github.com/solana-labs/solana/blob/56ccffdaa5394f179dce6c0383918e571aca8bff/programs/address-lookup-table/src/processor.rs#L129-L135 */
+  if( FD_FEATURE_ACTIVE( ctx->slot_ctx, relax_authority_signer_check_for_lookup_table_creation )
+      && 0==memcmp( lut_owner, fd_solana_address_lookup_table_program_id.key, 32UL ) ) {
+    return FD_EXECUTOR_INSTR_SUCCESS;
+  }
+
+  /* https://github.com/solana-labs/solana/blob/56ccffdaa5394f179dce6c0383918e571aca8bff/programs/address-lookup-table/src/processor.rs#L137-L142 */
+  ulong tbl_acct_data_len = 0x38UL;
+  ulong required_lamports = fd_rent_exempt_minimum_balance( ctx->slot_ctx, tbl_acct_data_len );
+        required_lamports = fd_ulong_max( required_lamports, 1UL );
+        required_lamports = fd_ulong_sat_sub( required_lamports, lut_lamports );
+
+  /* https://github.com/solana-labs/solana/blob/56ccffdaa5394f179dce6c0383918e571aca8bff/programs/address-lookup-table/src/processor.rs#L144-L149 */
+  if( required_lamports > 0UL ) {
+    FD_LOG_WARNING(( "TODO: CPI to system program" ));
+    return FD_EXECUTOR_INSTR_ERR_UNSUPPORTED_PROGRAM_ID;  /* transfer */
+  }
+
+  FD_LOG_WARNING(( "TODO: CPI to system program" ));  /* allocate */
+  FD_LOG_WARNING(( "TODO: CPI to system program" ));  /* assign */
+
+  /* TODO Native cross program invocations ... */
+
+  /* TODO: Acquire writable handle */
+  fd_address_lookup_table_state_t state[1];
+  fd_address_lookup_table_state_new( state );
+  state->discriminant = fd_address_lookup_table_state_enum_lookup_table;
+  fd_address_lookup_table_new( &state->inner.lookup_table );
+  fd_memcpy( state->inner.lookup_table.meta.authority->key, authority_key->key, 32UL );
+  /* TODO set state */
+
   FD_LOG_WARNING(( "TODO" ));
   (void)ctx; (void)create;
   return FD_EXECUTOR_INSTR_ERR_UNSUPPORTED_PROGRAM_ID;
 }
 
 static int
-freeze_lookup_table( fd_exec_instr_ctx_t ctx ) {
+freeze_lookup_table( fd_exec_instr_ctx_t * ctx ) {
   FD_LOG_WARNING(( "TODO" ));
   (void)ctx;
   return FD_EXECUTOR_INSTR_ERR_UNSUPPORTED_PROGRAM_ID;
 }
 
 static int
-extend_lookup_table( fd_exec_instr_ctx_t         ctx,
+extend_lookup_table( fd_exec_instr_ctx_t *       ctx,
                      fd_addrlut_extend_t const * extend ) {
   FD_LOG_WARNING(( "TODO" ));
   (void)ctx; (void)extend;
@@ -28,24 +148,24 @@ extend_lookup_table( fd_exec_instr_ctx_t         ctx,
 }
 
 static int
-deactivate_lookup_table( fd_exec_instr_ctx_t ctx ) {
+deactivate_lookup_table( fd_exec_instr_ctx_t * ctx ) {
   FD_LOG_WARNING(( "TODO" ));
   (void)ctx;
   return FD_EXECUTOR_INSTR_ERR_UNSUPPORTED_PROGRAM_ID;
 }
 
 static int
-close_lookup_table( fd_exec_instr_ctx_t ctx ) {
+close_lookup_table( fd_exec_instr_ctx_t * ctx ) {
   FD_LOG_WARNING(( "TODO" ));
   (void)ctx;
   return FD_EXECUTOR_INSTR_ERR_UNSUPPORTED_PROGRAM_ID;
 }
 
 int
-fd_executor_address_lookup_table_program_execute_instruction( fd_exec_instr_ctx_t ctx ) {
+fd_executor_address_lookup_table_program_execute_instruction( fd_exec_instr_ctx_t * ctx ) {
 
-  uchar const * instr_data    = ctx.instr->data;
-  ulong         instr_data_sz = ctx.instr->data_sz;
+  uchar const * instr_data    = ctx->instr->data;
+  ulong         instr_data_sz = ctx->instr->data_sz;
 
   FD_SCRATCH_SCOPED_FRAME;
 
