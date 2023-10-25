5b9038faf7d5c6c03be08fa39c02f12a1e857cbf
Marcus <117692400+marcus-jump@users.noreply.github.com>
2023-10-25 14:51:23 +0000

    Revert "clang as default compiler"
    This reverts commit b8a5f66a8b60c34ba51c99a95b9224c61e737a8b.

--- config/base.mk
+++ config/base.mk
@@ -2,11 +2,11 @@ BASEDIR?=build
 
 SHELL:=bash
 CPPFLAGS:=-isystem ./opt/include -DFD_LOG_UNCLEAN_EXIT=1 -DFD_WKSP_NO_LOCK_RECLAIM=1
-CC:=clang
+CC:=gcc
 CFLAGS:=-std=c17
-CXX:=clang++
+CXX:=g++
 CXXFLAGS:=-std=c++17
-LD:=clang++
+LD:=g++
 LDFLAGS:=-lm -lrt -ldl -L./opt/lib
 AR:=ar
 ARFLAGS:=rv
