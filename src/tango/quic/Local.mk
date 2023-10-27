8327433b991a1dcfc891c2a56a52731aff2aa284
Richard Patel <ripatel@jumptrading.com>
2023-10-27 20:59:31 +0000

    quic: add ctl test

--- src/tango/quic/Local.mk
+++ src/tango/quic/Local.mk
@@ -4,4 +4,5 @@ $(call add-objs,fd_quic fd_quic_conn fd_quic_conn_id fd_quic_conn_map fd_quic_pr
   fd_quic_stream tls/fd_quic_tls crypto/fd_quic_crypto_suites templ/fd_quic_transport_params \
   templ/fd_quic_parse_util fd_quic_pkt_meta,fd_quic)
 $(call make-bin,fd_quic_ctl,fd_quic_ctl,fd_quic fd_ballet fd_tango fd_util)
+$(call add-test-scripts,test_quic_ctl)
 endif
