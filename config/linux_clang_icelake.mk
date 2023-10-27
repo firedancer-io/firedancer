0d05c9af6d14534d893cc6c088dc49509182f58d
Richard Patel <ripatel@jumptrading.com>
2023-10-27 20:19:53 +0000

    Disable AVX-512 on linux_clang_icelake

--- config/linux_clang_icelake.mk
+++ config/linux_clang_icelake.mk
@@ -15,15 +15,19 @@ include config/with-openssl.mk
 # -falign-functions=32 -falign-jumps=32 -falign-labels=32 -falign-loops=32
 # -mbranch-cost=5
 
-CPPFLAGS+=-fomit-frame-pointer -march=icelake-server -mtune=icelake-server -mfpmath=sse \
-	  -DFD_HAS_INT128=1 \
-	  -DFD_HAS_DOUBLE=1 \
-	  -DFD_HAS_ALLOCA=1 \
-	  -DFD_HAS_X86=1 \
-	  -DFD_HAS_SSE=1 \
-	  -DFD_HAS_AVX=1 \
-	  -DFD_HAS_SHANI=1 \
-	  -DFD_HAS_GFNI=1
+CPPFLAGS+=-fomit-frame-pointer \
+    -march=icelake-server \
+    -mtune=icelake-server \
+    -mfpmath=sse \
+    -mno-avx512f \
+    -DFD_HAS_INT128=1 \
+    -DFD_HAS_DOUBLE=1 \
+    -DFD_HAS_ALLOCA=1 \
+    -DFD_HAS_X86=1 \
+    -DFD_HAS_SSE=1 \
+    -DFD_HAS_AVX=1 \
+    -DFD_HAS_SHANI=1 \
+    -DFD_HAS_GFNI=1
 
 FD_HAS_INT128:=1
 FD_HAS_DOUBLE:=1
