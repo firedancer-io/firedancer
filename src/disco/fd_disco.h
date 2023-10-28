64ba1b21866afe784390935c51b3780485f6c6c2
Richard Patel <ripatel@jumptrading.com>
2023-10-28 08:03:16 +0000

    Delete tiles that were refactored

--- src/disco/fd_disco.h
+++ src/disco/fd_disco.h
@@ -4,9 +4,6 @@
 //#include "fd_disco_base.h"  /* includes ../tango/fd_tango.h */
 #include "net/fd_net.h"       /* includes fd_disco_base.h */
 #include "dedup/fd_dedup.h"   /* includes fd_disco_base.h */
-#include "verify/fd_verify.h" /* includes fd_disco_base.h */
-#include "quic/fd_quic.h"     /* includes fd_disco_base.h */
-#include "pack/fd_pack.h"     /* includes fd_disco_base.h */
 #include "mux/fd_mux.h"       /* includes fd_disco_base.h */
 #include "replay/fd_replay.h" /* includes fd_disco_base.h */
 
