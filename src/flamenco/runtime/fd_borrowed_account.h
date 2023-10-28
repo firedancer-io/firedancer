9aef861626b78082ec9fc915a79b63f498f2d908
Richard Patel <ripatel@jumptrading.com>
2023-10-28 09:34:37 +0000

    borrowed_account: add refcounting

--- src/flamenco/runtime/fd_borrowed_account.h
+++ src/flamenco/runtime/fd_borrowed_account.h
@@ -5,6 +5,8 @@
 #include "../types/fd_types.h"
 #include "../../funk/fd_funk_rec.h"
 
+/* TODO This should be called fd_txn_acct. */
+
 struct __attribute__((aligned(8UL))) fd_borrowed_account {
   ulong                       magic;
 
@@ -20,6 +22,13 @@ struct __attribute__((aligned(8UL))) fd_borrowed_account {
 
   ulong                       starting_dlen;
   ulong                       starting_lamports;
+
+  /* Provide read/write mutual exclusion semantics.
+     Used for single-threaded logic only, thus not comparable to a
+     data synchronization lock. */
+
+  ushort refcnt_excl;
+  ushort refcnt_shared;
 };
 typedef struct fd_borrowed_account fd_borrowed_account_t;
 #define FD_BORROWED_ACCOUNT_FOOTPRINT (sizeof(fd_borrowed_account_t))
@@ -30,7 +39,49 @@ typedef struct fd_borrowed_account fd_borrowed_account_t;
 
 FD_PROTOTYPES_BEGIN
 
-fd_borrowed_account_t * fd_borrowed_account_init( void * ptr );
+fd_borrowed_account_t *
+fd_borrowed_account_init( void * ptr );
+
+/* fd_borrowed_account_acquire_write acquires write/exclusive access.
+   Causes all other write or read acquire attempts will fail.  Returns 1
+   on success, 0 on failure. */
+
+static inline int
+fd_borrowed_account_acquire_write( fd_borrowed_account_t * rw ) {
+  if( FD_UNLIKELY( (!!rw->refcnt_excl) | (!!rw->refcnt_shared ) ) )
+    return 0;
+  rw->refcnt_excl = (ushort)1;
+  return 1;
+}
+
+/* fd_borrowed_account_release_write releases a write/exclusive access
+handle. */
+
+static inline void
+fd_borrowed_account_release_write( fd_borrowed_account_t * rw ) {
+  FD_TEST( rw->refcnt_excl==1U );
+  rw->refcnt_excl = (ushort)0;
+}
+
+/* fd_borrowed_account_acquire_read acquires read/shared access.  Causes
+   write attempts to fail.  Further attempts to read will succeed. */
+
+static inline int
+fd_borrowed_account_acquire_read( fd_borrowed_account_t * rw ) {
+  if( FD_UNLIKELY( !!rw->refcnt_excl ) )
+    return 0;
+  rw->refcnt_shared = (ushort)( rw->refcnt_shared + 1U );
+  return 1;
+}
+
+/* fd_borrowed_account_release_read releases a read/shared access
+handle. */
+
+static inline void
+fd_borrowed_account_release_read( fd_borrowed_account_t * rw ) {
+  FD_TEST( rw->refcnt_shared>0U );
+  rw->refcnt_shared--;
+}
 
 FD_PROTOTYPES_END
 
