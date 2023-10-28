7adc4e32f7f870510aba780e3f881a00fe32680f
Richard Patel <ripatel@jumptrading.com>
2023-10-28 08:15:35 +0000

    Remove all tiles

--- src/disco/Local.mk
+++
@@ -1,4 +0,0 @@
-$(call make-lib,fd_disco)
-$(call add-hdrs,fd_disco_base.h fd_disco.h)
-$(call make-unit-test,test_disco_base,test_disco_base,fd_disco fd_tango fd_util)
-$(call run-unit-test,test_disco_base,)
