ad8cf06b7837811beb2e4f6453e3fabef9e9341a
Alex Siegel <77687153+asiegel-jt@users.noreply.github.com>
2023-11-03 09:46:29 -0500

    Poseidon support (#395)
    * added params
    
    * poseidon
    
    * poseidon
    
    * comments
    
    * testing

--- src/ballet/bn254/Local.mk
+++ src/ballet/bn254/Local.mk
@@ -1,8 +1,9 @@
 ifneq ($(FD_HAS_LIBFF),)
 
-$(call add-hdrs,fd_bn254.h)
-$(call add-objs,fd_bn254,fd_ballet)
+$(call add-hdrs,fd_bn254.h fd_poseidon.h)
+$(call add-objs,fd_bn254 fd_poseidon_params fd_poseidon,fd_ballet)
 $(call make-unit-test,test_bn254,test_bn254,fd_ballet fd_util)
+$(call make-unit-test,test_poseidon,test_poseidon,fd_ballet fd_util)
 
 else
 
