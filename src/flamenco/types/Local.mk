80059af69cc3eb8ae9b50fb14aa8125ee22cd1fb
Richard Patel <ripatel@jumptrading.com>
2023-11-08 13:26:54 +0000

    types: add initial meta API

--- src/flamenco/types/Local.mk
+++ src/flamenco/types/Local.mk
@@ -1,4 +1,5 @@
 ifdef FD_HAS_INT128
-$(call add-hdrs,fd_bincode.h fd_types.h fd_types_custom.h)
+$(call add-hdrs,fd_bincode.h fd_types.h fd_types_custom.h fd_types_meta.h)
 $(call add-objs,fd_types,fd_flamenco)
+$(call make-unit-test,test_types_meta,test_types_meta,fd_flamenco fd_ballet fd_util)
 endif
