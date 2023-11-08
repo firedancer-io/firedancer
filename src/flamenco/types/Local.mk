be988ecb93aab723945a8126aee15e7c8a26077f
Richard Patel <ripatel@jumptrading.com>
2023-11-08 15:37:54 +0000

    types: import ConfirmedBlock Protobuf

--- src/flamenco/types/Local.mk
+++ src/flamenco/types/Local.mk
@@ -4,3 +4,6 @@ $(call add-objs,fd_types fd_types_yaml,fd_flamenco)
 $(call make-unit-test,test_types_meta,test_types_meta,fd_flamenco fd_ballet fd_util)
 $(call make-unit-test,test_types_yaml,test_types_yaml,fd_flamenco fd_ballet fd_util)
 endif
+
+# "ConfirmedBlock" Protobuf definitions
+$(call add-objs,fd_solana_block.pb,fd_flamenco)
