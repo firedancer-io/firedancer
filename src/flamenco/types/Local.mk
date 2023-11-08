b18f1c2cda31717dc7bafdd4f8996dd9845b98f5
Richard Patel <ripatel@jumptrading.com>
2023-11-08 13:36:30 +0000

    types: add YAML serializer

--- src/flamenco/types/Local.mk
+++ src/flamenco/types/Local.mk
@@ -1,5 +1,6 @@
 ifdef FD_HAS_INT128
-$(call add-hdrs,fd_bincode.h fd_types.h fd_types_custom.h fd_types_meta.h)
-$(call add-objs,fd_types,fd_flamenco)
+$(call add-hdrs,fd_bincode.h fd_types.h fd_types_custom.h fd_types_meta.h fd_types_yaml.h)
+$(call add-objs,fd_types fd_types_yaml,fd_flamenco)
 $(call make-unit-test,test_types_meta,test_types_meta,fd_flamenco fd_ballet fd_util)
+$(call make-unit-test,test_types_yaml,test_types_yaml,fd_flamenco fd_ballet fd_util)
 endif
