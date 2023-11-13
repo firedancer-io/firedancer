48a45dce2a7a33c8bbd1f2177fb6e1b44d875a5f
Richard Patel <ripatel@jumptrading.com>
2023-11-13 23:52:54 +0000

    quic: add tpu_reasm class
    Co-authored-by: Kevin J Bowers <kbowers@jumptrading.com>

--- src/disco/quic/Local.mk
+++ src/disco/quic/Local.mk
@@ -1,3 +1,5 @@
+$(call add-hdrs,fd_tpu.h)
+$(call add-objs,fd_tpu_reasm,fd_disco)
 ifdef FD_HAS_OPENSSL
 # $(call make-unit-test,test_quic_tile,test_quic_tile,fd_disco fd_tango fd_ballet fd_quic fd_util)
 endif
