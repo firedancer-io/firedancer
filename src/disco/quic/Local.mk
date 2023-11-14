0ad82dda8a726cd2e1f30103947389486bf2f50e
Richard Patel <ripatel@jumptrading.com>
2023-11-14 21:21:40 +0000

    quic: add test_tpu_reasm

--- src/disco/quic/Local.mk
+++ src/disco/quic/Local.mk
@@ -1,5 +1,6 @@
 $(call add-hdrs,fd_tpu.h)
 $(call add-objs,fd_tpu_reasm,fd_disco)
+$(call make-unit-test,test_tpu_reasm,test_tpu_reasm,fd_disco fd_tango fd_ballet fd_util)
 ifdef FD_HAS_OPENSSL
 # $(call make-unit-test,test_quic_tile,test_quic_tile,fd_disco fd_tango fd_ballet fd_quic fd_util)
 endif
