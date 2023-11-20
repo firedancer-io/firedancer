21fa947d00c4faa664c57767e7f4f2ab3a57b53a
Marc-André Tremblay <1619947+marctrem@users.noreply.github.com>
2023-11-20 19:13:11 +0000

    coverage: publish to gcloud

--- .github/workflows/scripts/testcov_gen_index.sh
+++ .github/workflows/scripts/testcov_gen_index.sh
@@ -11,31 +11,32 @@ TEST_TARGETS=$(ls -1 build/pages/cov/test/)
 for TARGET in $FEATURE_SETS_BUILT; do
   DIRS=$(echo build/pages/cov/fuzz/$TARGET/)
   PAGE="build/pages/cov/fuzz/$TARGET/index.html"
-  echo "<h1>Fuzz / $TARGET</h1>" > $PAGE
+  echo "<!DOCTYPE html><html><head><meta charset="UTF-8"/></head><body><h1>Fuzz / $TARGET</h1>" > $PAGE
   echo '<ul>' >> $PAGE
   for FT in $(ls -1 $DIRS); do
-    echo "<li><a href=\"./${FT}\">${FT}</a></li>" >> $PAGE
+    echo "<li><a href=\"./${FT}/index.html\">${FT}</a></li>" >> $PAGE
   done
 
   echo '</ul>' >> $PAGE
 done
+echo '</body></html>' >> $PAGE
 
 
 PAGE="build/pages/cov/fuzz/index.html"
-echo "<h1>Fuzz /</h1>" > $PAGE
+echo "<!DOCTYPE html><html><head><meta charset="UTF-8"/></head><body><h1>Fuzz /</h1>" > $PAGE
 echo '<ul>' >> $PAGE
 for TARGET in $FUZZ_TARGETS; do
-  echo "<li><a href=\"./${TARGET}\">${TARGET}</a></li>" >> $PAGE
+  echo "<li><a href=\"./${TARGET}/index.html\">${TARGET}</a></li>" >> $PAGE
 done
-echo '</ul>' >> $PAGE
+echo '</body></html>' >> $PAGE
 
 PAGE="build/pages/cov/test/index.html"
-echo "<h1>Test /</h1>" > $PAGE
+echo "<!DOCTYPE html><html><head><meta charset="UTF-8"/></head><body><h1>Test /</h1>" > $PAGE
 echo '<ul>' >> $PAGE
 for TARGET in $TEST_TARGETS; do
-  echo "<li><a href=\"./${TARGET}\">${TARGET}</a></li>" >> $PAGE
+  echo "<li><a href=\"./${TARGET}/index.html\">${TARGET}</a></li>" >> $PAGE
 done
-echo '</ul>' >> $PAGE
+echo '</body></html>' >> $PAGE
 
 
 # Generate cov index
@@ -43,6 +44,7 @@ cat <<EOS > build/pages/cov/index.html
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="UTF-8"/>
 <title>🔥💃 Coverage</title>
 </head>
 
@@ -50,8 +52,8 @@ cat <<EOS > build/pages/cov/index.html
 <h1>🔥💃 Coverage</h1>
 <h2>Links 🔗</h2>
 <ul>
-<li><a href="./test/">Test Coverage</li>
-<li><a href="./fuzz/">Fuzzing Coverage</li>
+<li><a href="./test/index.html">Test Coverage</li>
+<li><a href="./fuzz/index.html">Fuzzing Coverage</li>
 </ul>
 </body>
 
@@ -63,6 +65,7 @@ cat <<EOS > build/pages/index.html
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="UTF-8">
 <title>🔥💃 Pages</title>
 </head>
 
@@ -71,7 +74,7 @@ cat <<EOS > build/pages/index.html
 <p>From rev: $(git rev-parse HEAD)</p>
 <h2>Links 🔗</h2>
 <ul>
-<li><a href="./cov/">Coverage</li>
+<li><a href="./cov/index.html">Coverage</li>
 </ul>
 </body>
 
