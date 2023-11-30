5fdba9ece4bb25380472753226384cbe063c8c16
Marc-André Tremblay <1619947+marctrem@users.noreply.github.com>
2023-11-30 17:52:10 +0000

    fuzz: introduce canaries

---
+++ src/util/sanitize/fd_fuzz.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_fd_src_util_fuzz_fd_fuzz_h
+#define HEADER_fd_src_util_fuzz_fd_fuzz_h
+
+#if FD_HAS_FUZZ
+#define FD_FUZZ_MUST_BE_COVERED ((void) 0)
+#else
+#define FD_FUZZ_MUST_BE_COVERED
+#endif
+
+
+#endif
