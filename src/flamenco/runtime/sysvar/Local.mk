6d3ed92fc6edaa9abfcf4e382477b6a86d4ae61e
Richard Patel <ripatel@jumptrading.com>
2023-12-15 21:55:37 +0000

    Add fd_sysvar_rent query helper

--- src/flamenco/runtime/sysvar/Local.mk
+++ src/flamenco/runtime/sysvar/Local.mk
@@ -1,2 +1,7 @@
+ifdef FD_HAS_INT128
 $(call add-hdrs,fd_sysvar_rent.h)
- 
+$(call add-objs,fd_sysvar_rent,fd_flamenco)
+
+$(call make-unit-test,test_sysvar_rent,test_sysvar_rent,fd_flamenco fd_funk fd_ballet fd_util)
+$(call run-unit-test,test_sysvar_rent)
+endif
