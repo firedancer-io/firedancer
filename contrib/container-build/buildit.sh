d847fad49fc5769393c96555603a83a8a2963bea
Gearl Brace <gbrace@jumptrading.com>
2023-12-21 10:26:04 -0600

    some minor cleanup

--- contrib/container-build/buildit.sh
+++ contrib/container-build/buildit.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 OUTDIR=~/build/out
-SCRIPTSDIR=$PWD/scripts
+SCRIPTDIR=$PWD/scripts
 
 # Allow for a tag, release, or branch
 [ -n "$1" ] && TAG="$1"
 
-chmod +x "$SCRIPTSDIR/*.sh"
-mkdir -p $OUTDIR
+chmod +x "$SCRIPTDIR/fdbuild.sh"
+mkdir -p "$OUTDIR"
 
 if which podman >/dev/null 2>&1
   then DOCKER=podman
@@ -23,6 +23,6 @@ $DOCKER image exists fdbuilder:latest || $DOCKER build -t fdbuilder:latest .
 
 $DOCKER run --rm \
         --volume "$OUTDIR:/build/out:Z" \
-        --volume "$SCRIPTSDIR:/build/scripts:ro,Z" \
+        --volume "$SCRIPTDIR:/build/scripts:ro,Z" \
         fdbuilder:latest \
-        /build/scripts/fdbuild.sh $TAG
+        /build/scripts/fdbuild.sh "$TAG"
