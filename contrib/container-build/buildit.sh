638682d7f19d69146c49566b91007c1f3bba2f0a
Gearl Brace <gbrace@jumptrading.com>
2023-12-21 16:52:55 -0600

    Add option to select a machine type

--- contrib/container-build/buildit.sh
+++ contrib/container-build/buildit.sh
@@ -1,10 +1,37 @@
 #!/bin/sh
 
-OUTDIR=~/build/out
-SCRIPTDIR=$PWD/scripts
+OUTDIR=~/build
+SCRIPTDIR=${PWD}/scripts
+MACHINE=linux_gcc_x86_64
 
-# Allow for a tag, release, or branch
-[ -n "$1" ] && TAG="$1"
+GETOPT=$(getopt -o :t:m:h -l tag:,machine:,help -n $0 -- "$@")
+eval set -- "$GETOPT"
+
+usage () {
+echo -e "\nUsage: $0 [ -t|--tag release|tag|branchname ] [ -m|--machine machinetype ]
+\nWhere machinetype is one of the targets in ../../config and defaults to
+linux_gcc_x86_64 if not specified\n"
+}
+
+while : ; do
+  case "${1}" in
+    -t | --tag )  TAG="$2" ; shift 2
+      ;;
+    -m | --machine )
+        if [ -f ../../config/${2}.mk ]
+          then MACHINE="$2" ; shift 2
+        else echo "$2 does not appear to be a valid machine type, please review types in ../../config"
+          exit 1
+        fi
+      ;;
+    -h | --help ) usage ; exit 1
+      ;;
+    -- ) shift ; break
+      ;;
+    * ) usage ; exit 1
+      ;;
+  esac
+done
 
 chmod +x "$SCRIPTDIR/fdbuild.sh"
 mkdir -p "$OUTDIR"
@@ -25,4 +52,4 @@ $DOCKER run --rm \
         --volume "$OUTDIR:/build/out:Z" \
         --volume "$SCRIPTDIR:/build/scripts:ro,Z" \
         fdbuilder:latest \
-        /build/scripts/fdbuild.sh "$TAG"
+        /build/scripts/fdbuild.sh "$MACHINE" "$TAG"
