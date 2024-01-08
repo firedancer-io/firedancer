ffc0e59174aced4110fb55ac0e536d5373af4adf
Richard Patel <ripatel@jumptrading.com>
2024-01-08 12:02:03 +0000

    Fuzz fd_compute_budget_program_parse
    Fixes https://github.com/firedancer-io/firedancer/issues/150

--- src/ballet/pack/Local.mk
+++ src/ballet/pack/Local.mk
@@ -7,4 +7,5 @@ $(call make-unit-test,test_pack,test_pack,fd_disco fd_ballet fd_util)
 $(call run-unit-test,test_compute_budget_program,)
 $(call run-unit-test,test_est_tbl,)
 $(call run-unit-test,test_pack,)
+$(call fuzz-test,fuzz_compute_budget_program_parse,fuzz_compute_budget_program_parse,fd_ballet fd_util)
 endif
