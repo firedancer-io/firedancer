3a747089e3ff47044b44c50085361037e2ed81ea
Richard Patel <ripatel@jumptrading.com>
2024-01-11 12:44:30 +0000

    runtime: add clock sysvar

--- src/flamenco/runtime/sysvar/Local.mk
+++ src/flamenco/runtime/sysvar/Local.mk
@@ -1,4 +1,7 @@
 ifdef FD_HAS_INT128
+$(call add-hdrs,fd_sysvar_clock.h)
+$(call add-objs,fd_sysvar_clock,fd_flamenco)
+
 $(call add-hdrs,fd_sysvar_rent.h)
 $(call add-objs,fd_sysvar_rent,fd_flamenco)
 $(call make-unit-test,test_sysvar_rent,test_sysvar_rent,fd_flamenco fd_funk fd_ballet fd_util)
---
+++ src/flamenco/runtime/sysvar/fd_sysvar_clock.c
@@ -0,0 +1,28 @@
+#include "fd_sysvar_clock.h"
+#include "../fd_executor.h"
+#include "../fd_acc_mgr.h"
+#include "../fd_system_ids.h"
+#include "../context/fd_exec_slot_ctx.h"
+
+int
+fd_sysvar_clock_read( fd_exec_slot_ctx_t *    slot_ctx,
+                      fd_sol_sysvar_clock_t * result ) {
+  FD_BORROWED_ACCOUNT_DECL(acc);
+  int rc = fd_acc_mgr_view( slot_ctx->acc_mgr, slot_ctx->funk_txn, &fd_sysvar_clock_id, acc );
+
+  switch( rc ) {
+  case FD_ACC_MGR_SUCCESS:
+    break;
+  case FD_ACC_MGR_ERR_UNKNOWN_ACCOUNT:
+    return FD_EXECUTOR_INSTR_ERR_CUSTOM_ERR;
+  default:
+    return FD_EXECUTOR_INSTR_ERR_ACC_BORROW_FAILED;
+  }
+
+  fd_bincode_decode_ctx_t ctx =
+    { .data    = acc->const_data,
+      .dataend = acc->const_data + acc->const_meta->dlen,
+      .valloc  = {0}  /* valloc not required */ };
+
+  return fd_sol_sysvar_clock_decode( result, &ctx );
+}
