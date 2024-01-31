0d39f9af12228a605384bc899b733e89585f573b
Richard Patel <ripatel@jumptrading.com>
2024-01-31 18:57:05 +0000

    runtime: add additional test checks
    - Adds further account diff functionality to fd_exec_instr_test
    - Renames fd_addr_cstr to fd_acct_addr_cstr

--- src/flamenco/runtime/tests/fd_exec_instr_test.h
+++ src/flamenco/runtime/tests/fd_exec_instr_test.h
@@ -44,12 +44,14 @@ fd_exec_instr_test_runner_delete( fd_exec_instr_test_runner_t * runner );
 
 /* fd_exec_instr_fixture_run executes the given instruction processing
    fixture and validates that the actual result matches the expected.
-   Returns 1 on success.  On failure, returns 0 and logs reason for
-   error to warning log.  Uses fd_scratch. */
+   log_name is the name of the test to mention in logs.  Returns 1 on
+   success.  On failure, returns 0 and logs reason for error to warning
+   log.  Uses fd_scratch. */
 
 int
 fd_exec_instr_fixture_run( fd_exec_instr_test_runner_t *        runner,
-                           fd_exec_test_instr_fixture_t const * test );
+                           fd_exec_test_instr_fixture_t const * test,
+                           char const *                         log_name );
 
 /* fd_exec_instr_test_run executes a given instruction context (input)
    and returns the effects of executing that instruction to the caller.
