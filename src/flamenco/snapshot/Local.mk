02a6a45fe065c0fe2828c59ee7562aa178cf1b2b
Richard Patel <ripatel@jumptrading.com>
2024-02-02 23:24:08 +0000

    snapshot: add fuzzer for HTTP downloader

--- src/flamenco/snapshot/Local.mk
+++ src/flamenco/snapshot/Local.mk
@@ -6,6 +6,7 @@ $(call add-hdrs,fd_snapshot_http.h)
 $(call add-objs,fd_snapshot_http,fd_flamenco)
 $(call make-unit-test,test_snapshot_http,test_snapshot_http,fd_flamenco fd_funk fd_ballet fd_util)
 $(call run-unit-test,test_snapshot_http)
+$(call make-fuzz-test,fuzz_snapshot_http,fuzz_snapshot_http,fd_flamenco fd_funk fd_ballet fd_util)
 
 $(call add-hdrs,fd_snapshot_load.h)
 $(call add-objs,fd_snapshot_load,fd_flamenco)
