20fb15c63c8be17afdb36945981760b40074a90f
Richard Patel <ripatel@jumptrading.com>
2024-02-04 18:28:32 +0000

    ci: re-enable test_{ipc,tpool}

--- contrib/test/run_script_tests.sh
+++ contrib/test/run_script_tests.sh
@@ -24,8 +24,18 @@ $UNIT_TEST/test_tango_ctl   > $LOG_PATH/tango_ctl   2>&1
 $UNIT_TEST/test_wksp_helper > $LOG_PATH/wksp_helper 2>&1 # allocates a gigantic page
 
 # Multi-tile tests
-$UNIT_TEST/test_cnc  --tile-cpus 0,2   2> $LOG_PATH/cnc
-$UNIT_TEST/test_tile --tile-cpus 0-8/2 2> $LOG_PATH/tile_multi
+$UNIT_TEST/test_cnc   --tile-cpus 0,2   2> $LOG_PATH/cnc
+$UNIT_TEST/test_tile  --tile-cpus 0-8/2 2> $LOG_PATH/tile_multi
+$UNIT_TEST/test_tpool --tile-cpus 0-7   2> $LOG_PATH/tpool_large
+
+if $UNIT_TEST/test_ipc_init $OBJDIR && \
+    $UNIT_TEST/test_ipc_meta 16     && \
+    $UNIT_TEST/test_ipc_full 16     && \
+    $UNIT_TEST/test_ipc_fini; then
+  echo pass > $LOG_PATH/ipc
+else
+  echo FAIL > $LOG_PATH/ipc
+fi
 
 # FIXME: USE FD_IMPORT PCAP FILE
 #$UNIT_TEST/test_pcap --in tmp/test_in.pcap --out tmp/test_out.pcap 2> $LOG_PATH/pcap
@@ -42,21 +52,8 @@ $UNIT_TEST/test_tile --tile-cpus 0-8/2 2> $LOG_PATH/tile_multi
 # FIXME: Needs a /tmp/test.pcap file
 #$UNIT_TEST/test_replay         --tile-cpus 38-42/2 --tx-pcap /tmp/test.pcap 2> $LOG_PATH/replay
 
-# FIXME: Spawn the tpool job across all available tiles but 0
-#nice -n -19 $UNIT_TEST/test_tpool --tile-cpus 2-64/2,3-65/2 2> $LOG_PATH/tpool_large
 
 # todo(mmcgee-jump): reenable those tests https://github.com/firedancer-io/firedancer/issues/761
-# # Needs at least 1 free gigantic page on numa 0
-# if $UNIT_TEST/test_ipc_init $OBJDIR && \
-#     $UNIT_TEST/test_ipc_meta 16     && \
-#     $UNIT_TEST/test_ipc_full 16     && \
-#     $UNIT_TEST/test_ipc_fini; then
-#   echo pass > $LOG_PATH/ipc
-# else
-#   echo FAIL > $LOG_PATH/ipc
-# fi
-
-# # Needs at least 1 free gigantic page on numa 0
 # if $UNIT_TEST/test_mux_ipc_init $OBJDIR && \
 #     $UNIT_TEST/test_mux_ipc_meta 16 16  && \
 #     $UNIT_TEST/test_mux_ipc_full 16 16  && \
