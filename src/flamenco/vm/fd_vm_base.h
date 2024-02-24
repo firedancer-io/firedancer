5a40eef0e4e35e64af0d20b4ee7d72fa9e879d12
Kevin J Bowers <kbowers@jumptrading.com>
2024-02-24 14:33:28 -0600

    flamenco, vm: Belt sanded fd_vm_stack API
    - Made header only and moved into fd_vm_base.
    - Streamlined implementation
    - Tweaked API conventions to match other FD APIs more closely.
    - Started making unified error codes

--- src/flamenco/vm/fd_vm_base.h
+++ src/flamenco/vm/fd_vm_base.h
@@ -7,7 +7,8 @@
    necessary) in a somewhat haphazard fashion (include no-no things that
    are only available in hosted environments like stdio and stdlib) */
 
-/* TODO: Separate into public-use / private-implementation APIs */
+/* TODO: Separate into external-public-user /
+   internal-private-implementor APIs */
 
 #include "../../ballet/sbpf/fd_sbpf_instr.h"
 #include "../../ballet/sbpf/fd_sbpf_loader.h"
@@ -15,7 +16,18 @@
 #include "../../ballet/murmur3/fd_murmur3.h"
 #include "../runtime/fd_runtime.h"
 
+/* FD_VM_SUCCESS is zero and returned to indicate that an operation
+   completed successfully.  FD_VM_ERR_* are negative integers and
+   returned to indicate an operation that failed and why. */
+
+#define FD_VM_SUCCESS           (0)
+#define FD_VM_ERR_PUSH_OVERFLOW (-2)
+#define FD_VM_ERR_POP_UNDERFLOW (-3)
+
+//#define FD_VM_ERR_POP_EMPTY   (-1) /* FIXME: WHY WAS THIS DEFINED BEFORE ... NOT USED, SEEMS REDUNDANT WITH POP_UNDERFLOW */
+
 /* fd_vm_log_collector API ********************************************/
+
 /* TODO: ADD SPECIFIC UNIT TEST COVERAGE OF THIS API */
 
 /* A fd_vm_log_collector_t is used by the vm for storing text/bytes
@@ -95,6 +107,113 @@ FD_FN_PURE static inline ulong
 fd_vm_log_collector_buf_avail( fd_vm_log_collector_t const * collector ) {
   return FD_VM_LOG_COLLECTOR_BUF_MAX - collector->buf_used;
 }
+
+FD_PROTOTYPES_END
+
+/* fd_vm_stack API ****************************************************/
+
+/* FIXME: ADD SPECIFIC UNIT TEST COVERAGE OF THIS API */
+
+/* FIXME: The max depth of the stack is configurable by the compute
+   budget */
+
+/* FIXME: DOCUMENT SOLANA PROTOCOL REQUIRMENTS HERE */
+
+#define FD_VM_STACK_DEPTH_MAX           (64UL)
+#define FD_VM_STACK_FRAME_SZ            (0x1000UL)
+#define FD_VM_STACK_FRAME_WITH_GUARD_SZ (0x2000UL)
+
+#define FD_VM_STACK_DATA_MAX (FD_VM_STACK_DEPTH_MAX*FD_VM_STACK_FRAME_WITH_GUARD_SZ)
+
+/* A fd_vm_stack_private_shadow_t holds stack frame information hidden
+   from VM program execution. */
+
+struct fd_vm_stack_private_shadow {
+  ulong ret_instr_ptr;
+  ulong saved_reg[4];
+};
+
+typedef struct fd_vm_stack_private_shadow fd_vm_stack_private_shadow_t;
+
+/* A fd_vm_stack_t gives the VM program stack state. */
+
+struct fd_vm_stack {
+  ulong                        stack_pointer;                   /* FIXME: DOCUMENT THIS BETTER */
+  ulong                        depth;                           /* In [0,DEPTH_MAX] */
+  fd_vm_stack_private_shadow_t shadow[ FD_VM_STACK_DEPTH_MAX ]; /* Indexed [0,depth), if not empty, bottom at 0, top at depth-1 */
+  uchar                        data  [ FD_VM_STACK_DATA_MAX  ]; /* FIXME: DOCUMENT THIS BETTER */
+};
+
+typedef struct fd_vm_stack fd_vm_stack_t;
+
+FD_PROTOTYPES_BEGIN
+
+/* fd_vm_stack_wipe zeros out stack data, shadow frames, stack_pointer
+   and depth.  Assumes stack is valid.  Returns stack.  Use this to
+   initialize a newly allocated VM stack. */
+
+static inline fd_vm_stack_t *
+fd_vm_stack_wipe( fd_vm_stack_t * stack ) {
+  fd_memset( stack, 0, sizeof(fd_vm_stack_t) );
+  return stack;
+}
+
+/* fd_vm_stack_push pushes a new frame onto the VM stack.  Assumes
+   stack, ret_instr_ptr and saved_reg is valid.  Returns FD_VM_SUCCESS
+   (0) on success or FD_VM_ERR_PUSH_OVERFLOW (negative) on failure. */
+
+static inline int
+fd_vm_stack_push( fd_vm_stack_t * stack,
+                  ulong           ret_instr_ptr,
+                  ulong const     saved_reg[4] ) {
+  ulong top_idx = stack->depth;
+  if( FD_UNLIKELY( top_idx>=FD_VM_STACK_DEPTH_MAX ) ) return FD_VM_ERR_PUSH_OVERFLOW;
+  fd_vm_stack_private_shadow_t * shadow = stack->shadow + top_idx;
+  shadow->ret_instr_ptr = ret_instr_ptr;
+  shadow->saved_reg[0]  = saved_reg[0];
+  shadow->saved_reg[1]  = saved_reg[1];
+  shadow->saved_reg[2]  = saved_reg[2];
+  shadow->saved_reg[3]  = saved_reg[3];
+  stack->depth = top_idx + 1UL;
+  return FD_VM_SUCCESS;
+}
+
+/* fd_vm_stack_pop pops a frame off the VM stack.  Assumes stack,
+   ret_instr_ptr and saved_reg is valid.  Returns FD_VM_SUCCESS (0) on
+   success and FD_VM_ERR_POP_UNDERFLOW (negative) on failure.  On
+   success, *_ret_instr_ptr and saved_reg[0:3] hold the values popped
+   off the stack on return.  These are unchanged otherwise. */
+/* TODO: CONSIDER ZERO COPY API? */
+
+static inline int
+fd_vm_stack_pop( fd_vm_stack_t * stack,
+                 ulong *         _ret_instr_ptr,
+                 ulong           saved_reg[4] ) {
+  ulong top_idx = stack->depth;
+  if( FD_UNLIKELY( !top_idx ) ) return FD_VM_ERR_POP_UNDERFLOW;
+  top_idx--;
+  fd_vm_stack_private_shadow_t * shadow = stack->shadow + top_idx;
+  *_ret_instr_ptr = shadow->ret_instr_ptr;
+  saved_reg[0]    = shadow->saved_reg[0];
+  saved_reg[1]    = shadow->saved_reg[1];
+  saved_reg[2]    = shadow->saved_reg[2];
+  saved_reg[3]    = shadow->saved_reg[3];
+  stack->depth = top_idx;
+  return FD_VM_SUCCESS;
+}
+
+/* fd_vm_stack_data returns a pointer to the first byte a VM's program
+   stack region in the hosts's address space.  There are
+   FD_VM_STACK_DATA_MAX bytes available at this region.  Assumes stack
+   is valid.  The lifetime of the returned pointer is the lifetime of
+   the stack. */
+
+FD_FN_CONST static inline void * fd_vm_stack_data( fd_vm_stack_t * stack ) { return stack->data; }
+
+/* fd_vm_stack_empty returns 1 if the stack is empty and 0 if not.
+   Assumes stack is valid. */
+
+FD_FN_PURE static inline int fd_vm_stack_is_empty( fd_vm_stack_t const * stack ) { return !stack->depth; }
 
 FD_PROTOTYPES_END
 
