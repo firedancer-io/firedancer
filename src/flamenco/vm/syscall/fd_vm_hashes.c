67556f84580a24e1741c917511381897fff7c16f
Kevin J Bowers <kbowers@jumptrading.com>
2024-02-24 21:17:57 -0600

    Belt sanding pass fd_vm_syscalls
    * Started as converting FD_VM_MEM_MAP_ERR codes to common integer error
      codes consistent with the rest of the APIs.
    * Getting rid of all the spurious silent conversions between int and ulong
      required updating all the syscall returns.  Which then turned into
      a belt sanding pass on the syscalls.  Notable changes include:
      - returns a int for an error code
      - consistent naming an enumeration of the syscall arguments
      - eliminated the unused redundant syscall function pointer type
      - got rid of a bunch of int / ulong casting
      - got rid of a bunch of unnecessary function pointer casting
      - consume_compute API cleaned up, optimized, proper error hanlding and documented (similarly for mem_op_consume)
      - bunch of linting code in fd_vm_syscall (plenty more to do), including annotations of likely locations of latent (or
        not so latent) issues
    * Some very minor changes to runtime and ballet/sbpf got caught up in
      this (actually fixed a silent cast in runtime)
    
    Note that this has uncovered some worrisome bugs in the vm.  When
    starting from a clean build on head of tree, I had to disable a bunch of
    failing tests in test_vm_interp and test_vm_syscalls to begin work.
    
    With this pass, though nothing here should have affected had an
    functional impact (it is running about 2% faster from cleanups alone),
    now all but 1 test in test_vm_syscall are passing.  But that 1 test
    (log_sol_data) now seg faults.  Except when all the unrelated tests are
    disabled.  Then the test fails in a different way.  All this and various
    diagnostics collected to troubleshoot there are pre-existing latent
    memory corruption issues (maybe in the test itself, maybe in the VM).
    
    Unit test coverage here is pretty thin currently.  So, the VM will
    almost certainly need a lot of security auditing and what not
    after the belt sanding.
    
    Plenty more belt sanding to do (especially at a high level, e.g. should
    break fd_vm_syscall.c into a bunch of files to make auditing and testing
    all this or manageable).

--- src/flamenco/vm/syscall/fd_vm_hashes.c
+++ src/flamenco/vm/syscall/fd_vm_hashes.c
@@ -2,81 +2,65 @@
 
 #include "../../../ballet/keccak256/fd_keccak256.h"
 
-ulong
-fd_vm_syscall_sol_sha256(
-    void *  _ctx,
-    ulong   slices_vaddr,
-    ulong   slices_cnt,
-    ulong   res_vaddr,
-    ulong   r4 __attribute__((unused)),
-    ulong   r5 __attribute__((unused)),
-    ulong * pr0
-) {
-
+int
+fd_vm_syscall_sol_sha256( /**/            void *  _ctx,
+                          /**/            ulong   slices_vaddr,
+                          /**/            ulong   slices_cnt,
+                          /**/            ulong   res_vaddr,
+                          FD_PARAM_UNUSED ulong   arg3,
+                          FD_PARAM_UNUSED ulong   arg4,
+                          /**/            ulong * _ret ) {
   fd_vm_exec_context_t * ctx = (fd_vm_exec_context_t *) _ctx;
 
-  if( FD_UNLIKELY( slices_cnt > vm_compute_budget.sha256_max_slices ) )
-    return FD_VM_SYSCALL_ERR_INVAL;
+  if( FD_UNLIKELY( slices_cnt > vm_compute_budget.sha256_max_slices ) ) return FD_VM_SYSCALL_ERR_INVAL;
 
-  ulong err = fd_vm_consume_compute_meter(ctx, vm_compute_budget.sha256_base_cost);
-  if ( FD_UNLIKELY( err ) ) return err;
+  int err = fd_vm_consume_compute( ctx, vm_compute_budget.sha256_base_cost );
+  if( FD_UNLIKELY( err ) ) return err;
+
   ulong slices_sz = slices_cnt * sizeof(fd_vm_vec_t);
 
-  fd_vm_vec_t const * slices =
-      fd_vm_translate_vm_to_host_const( ctx, slices_vaddr, slices_sz, FD_VM_VEC_ALIGN );
-  void * hash =
-      fd_vm_translate_vm_to_host      ( ctx, res_vaddr,    32UL,      alignof(uchar)  );
+  fd_vm_vec_t const * slices = fd_vm_translate_vm_to_host_const( ctx, slices_vaddr, slices_sz, FD_VM_VEC_ALIGN );
+  void *              hash   = fd_vm_translate_vm_to_host      ( ctx, res_vaddr,    32UL,      alignof(uchar)  );
 
-  if( FD_UNLIKELY( (!slices) | (!hash) ) ) return FD_VM_MEM_MAP_ERR_ACC_VIO;
+  if( FD_UNLIKELY( (!slices) | (!hash) ) ) return FD_VM_ERR_ACC_VIO;
 
   fd_sha256_t sha;
   fd_sha256_init( &sha );
 
   for( ulong i = 0; i < slices_cnt; i++ ) {
     uchar const * slice = fd_vm_translate_vm_to_host_const( ctx, slices[i].addr, slices[i].len, alignof(uchar) );
-    if( FD_UNLIKELY( !slice && slices[i].len ) ) { 
-      return FD_VM_MEM_MAP_ERR_ACC_VIO;
-    }
+    if( FD_UNLIKELY( !slice ) ) return FD_VM_ERR_ACC_VIO;
 
     ulong cost = fd_ulong_max(vm_compute_budget.mem_op_base_cost, fd_ulong_sat_mul(vm_compute_budget.sha256_byte_cost, slices[i].len) / 2);
-    ulong err = fd_vm_consume_compute_meter(ctx, cost);
-    if ( FD_UNLIKELY( err ) ) return err;
+    int err = fd_vm_consume_compute( ctx, cost );
+    if( FD_UNLIKELY( err ) ) return err;
 
     fd_sha256_append( &sha, slice, slices[i].len );
   }
 
   fd_sha256_fini( &sha, hash );
-  *pr0 = 0UL;
+  *_ret = 0UL;
   return FD_VM_SYSCALL_SUCCESS;
 }
 
-ulong
-fd_vm_syscall_sol_keccak256(
-    void *  _ctx,
-    ulong   slices_vaddr,
-    ulong   slices_cnt,
-    ulong   res_vaddr,
-    ulong   r4 __attribute__((unused)),
-    ulong   r5 __attribute__((unused)),
-    ulong * pr0
-) {
-
+int
+fd_vm_syscall_sol_keccak256( /**/            void *  _ctx,
+                             /**/            ulong   slices_vaddr,
+                             /**/            ulong   slices_cnt,
+                             /**/            ulong   res_vaddr,
+                             FD_PARAM_UNUSED ulong   arg3,
+                             FD_PARAM_UNUSED ulong   arg4,
+                             /**/            ulong * _ret ) {
   fd_vm_exec_context_t * ctx = (fd_vm_exec_context_t *) _ctx;
 
   if( FD_UNLIKELY( slices_cnt > vm_compute_budget.sha256_max_slices ) )
     return FD_VM_SYSCALL_ERR_INVAL;
 
-  ulong err = fd_vm_consume_compute_meter(ctx, vm_compute_budget.sha256_base_cost);
-  if ( FD_UNLIKELY( err ) ) {
-    return err;
-  }
+  int err = fd_vm_consume_compute( ctx, vm_compute_budget.sha256_base_cost );
+  if( FD_UNLIKELY( err ) ) return err;
 
-  void * hash =
-      fd_vm_translate_vm_to_host      ( ctx, res_vaddr,    32UL,      alignof(uchar)  );
-
-  if( FD_UNLIKELY( (!hash) ) ) {
-    return FD_VM_MEM_MAP_ERR_ACC_VIO;
-  }
+  void * hash = fd_vm_translate_vm_to_host( ctx, res_vaddr, 32UL, alignof(uchar) );
+  if( FD_UNLIKELY( !hash ) ) return FD_VM_ERR_ACC_VIO;
 
   fd_keccak256_t sha;
   fd_keccak256_init(&sha);
@@ -88,47 +72,45 @@ fd_vm_syscall_sol_keccak256(
         fd_vm_translate_vm_to_host_const( ctx, slices_vaddr, slices_sz, FD_VM_VEC_ALIGN );
 
     if( FD_UNLIKELY( (!slices) ) ) {
-      return FD_VM_MEM_MAP_ERR_ACC_VIO;
+      return FD_VM_ERR_ACC_VIO;
     }
 
     for (ulong i = 0; i < slices_cnt; i++) {
       void const * slice = fd_vm_translate_vm_to_host_const( ctx, slices[i].addr, slices[i].len, alignof(uchar) );
       if( FD_UNLIKELY( !slice && slices[i].len ) ) {
         FD_LOG_DEBUG(("Translate slice failed %lu %lu %lu", i, slices[i].addr, slices[i].len));
-        return FD_VM_MEM_MAP_ERR_ACC_VIO;
+        return FD_VM_ERR_ACC_VIO;
       }
 
       ulong cost = fd_ulong_max(vm_compute_budget.mem_op_base_cost, fd_ulong_sat_mul(vm_compute_budget.sha256_byte_cost, slices[i].len / 2));
-      ulong err = fd_vm_consume_compute_meter(ctx, cost);
-      if ( FD_UNLIKELY( err ) ) return err;
+      int err = fd_vm_consume_compute( ctx, cost );
+      if( FD_UNLIKELY( err ) ) return err;
 
       fd_keccak256_append( &sha, slice, slices[i].len );
     }
   }
 
   fd_keccak256_fini(&sha, hash);
-  *pr0 = 0UL;
+  *_ret = 0UL;
   return FD_VM_SYSCALL_SUCCESS;
 }
 
-ulong
-fd_vm_syscall_sol_blake3(
-    void *  _ctx,
-    ulong   slices_vaddr,
-    ulong   slices_cnt,
-    ulong   res_vaddr,
-    ulong   r4 __attribute__((unused)),
-    ulong   r5 __attribute__((unused)),
-    ulong * pr0
-) {
+int
+fd_vm_syscall_sol_blake3( /**/            void *  _ctx,
+                          /**/            ulong   slices_vaddr,
+                          /**/            ulong   slices_cnt,
+                          /**/            ulong   res_vaddr,
+                          FD_PARAM_UNUSED ulong   arg3,
+                          FD_PARAM_UNUSED ulong   arg4,
+                          /**/            ulong * _ret ) {
   fd_vm_exec_context_t * ctx = (fd_vm_exec_context_t *) _ctx;
 
   /* TODO don't hardcode limit */
   if( FD_UNLIKELY( slices_cnt > vm_compute_budget.sha256_max_slices ) )
     return FD_VM_SYSCALL_ERR_INVAL;
 
-  ulong err = fd_vm_consume_compute_meter(ctx, vm_compute_budget.sha256_base_cost);
-  if ( FD_UNLIKELY( err ) ) return err;
+  int err = fd_vm_consume_compute( ctx, vm_compute_budget.sha256_base_cost );
+  if( FD_UNLIKELY( err ) ) return err;
   ulong slices_sz = slices_cnt * sizeof(fd_vm_vec_t);
 
   fd_vm_vec_t const * slices =
@@ -136,25 +118,23 @@ fd_vm_syscall_sol_blake3(
   void * hash =
       fd_vm_translate_vm_to_host      ( ctx, res_vaddr,    32UL,      alignof(uchar)  );
 
-  if( FD_UNLIKELY( (!slices) | (!hash) ) ) return FD_VM_MEM_MAP_ERR_ACC_VIO;
+  if( FD_UNLIKELY( (!slices) | (!hash) ) ) return FD_VM_ERR_ACC_VIO;
 
   fd_blake3_t b3;
   fd_blake3_init(&b3);
 
   for (ulong i = 0; i < slices_cnt; i++) {
     void const * slice = fd_vm_translate_vm_to_host( ctx, slices[i].addr, slices[i].len, alignof(uchar) );
-    if( FD_UNLIKELY( !slice && slices[i].len ) ) { 
-      return FD_VM_MEM_MAP_ERR_ACC_VIO;
-    }
+    if( FD_UNLIKELY( !slice ) ) return FD_VM_ERR_ACC_VIO;
 
     ulong cost = fd_ulong_max(vm_compute_budget.mem_op_base_cost, fd_ulong_sat_mul(vm_compute_budget.sha256_byte_cost, slices[i].len) / 2);
-    ulong err = fd_vm_consume_compute_meter(ctx, cost);
-    if ( FD_UNLIKELY( err ) ) return err;
+    int err = fd_vm_consume_compute( ctx, cost );
+    if( FD_UNLIKELY( err ) ) return err;
 
     fd_blake3_append( &b3, slice, slices[i].len );
   }
 
   fd_blake3_fini( &b3, hash );
-  *pr0 = 0UL;
+  *_ret = 0UL;
   return FD_VM_SYSCALL_SUCCESS;
 }
