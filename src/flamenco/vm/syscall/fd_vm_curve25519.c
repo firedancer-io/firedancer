c53fcd295be9bc88f47e0f28cd729d2af187556c
Kevin J Bowers <kbowers@jumptrading.com>
2024-02-24 21:17:57 -0600

    flamenco, vm: Belt sanding pass fd_vm_syscalls
    * Started as converting FD_VM_MEM_MAP_ERR codes to common integer error
      codes consistent with the rest of the APIs.
    * Getting rid of all the spurious silent conversions between int and ulong
      required updating all the syscall returns.  Which then turned into
      a belt sanding pass on the syscalls.  Notable changes include:
      - returns a int for an error code
      - consistent naming an enumeration of the syscall arguments
      - eliminated the unused redundant syscall function pointer type
      - got rid of a bunch of int / ulong casting
      - got rid of a bunch of unnecessary function pointer casting
      - consume_compute API cleaned up, optimized, proper error hanlding and documented (similarly for mem_op_consume)
      - bunch of linting code in fd_vm_syscall (plenty more to do), including annotations of likely locations of latent (or
        not so latent) issues
    * Some very minor changes to runtime and ballet/sbpf got caught up in
      this (actually fixed a silent cast in runtime)
    
    Note that this has uncovered some worrisome bugs in the vm.  When
    starting from a clean build on head of tree, I had to disable a bunch of
    failing tests in test_vm_interp and test_vm_syscalls to begin work.
    
    With this pass, though nothing here should have affected had an
    functional impact (it is running about 2% faster from cleanups alone),
    now all but 1 test in test_vm_syscall are passing.  But that 1 test
    (log_sol_data) now seg faults.  Except when all the unrelated tests are
    disabled.  Then the test fails in a different way.  All this and various
    diagnostics collected to troubleshoot there are pre-existing latent
    memory corruption issues (maybe in the test itself, maybe in the VM).
    
    Unit test coverage here is pretty thin currently.  So, the VM will
    almost certainly need a lot of security auditing and what not
    after the belt sanding.
    
    Plenty more belt sanding to do (especially at a high level, e.g. should
    break fd_vm_syscall.c into a bunch of files to make auditing and testing
    all this or manageable).

--- src/flamenco/vm/syscall/fd_vm_curve25519.c
+++ src/flamenco/vm/syscall/fd_vm_curve25519.c
@@ -10,17 +10,14 @@
 #define SCALAR_ALIGN ( 1UL)
 #define MSM_BATCH_SZ (32UL)
 
-ulong
-fd_vm_syscall_sol_curve_validate_point(
-    void *  _ctx,
-    ulong   curve_id,
-    ulong   point_addr,
-    ulong   r3 FD_PARAM_UNUSED,
-    ulong   r4 FD_PARAM_UNUSED,
-    ulong   r5 FD_PARAM_UNUSED,
-    ulong * pret
-) {
-
+int
+fd_vm_syscall_sol_curve_validate_point( /**/            void *  _ctx,
+                                        /**/            ulong   curve_id,
+                                        /**/            ulong   point_addr,
+                                        FD_PARAM_UNUSED ulong   arg2,
+                                        FD_PARAM_UNUSED ulong   arg3,
+                                        FD_PARAM_UNUSED ulong   arg4,
+                                        /**/            ulong * _ret ) {
   fd_vm_exec_context_t * ctx = (fd_vm_exec_context_t *) _ctx;
 
   ulong ret = 1UL;
@@ -30,7 +27,7 @@ fd_vm_syscall_sol_curve_validate_point(
        https://github.com/solana-labs/solana/blob/c0fbfc6422fa5b739049c01bfda48a0da1bf6a46/programs/bpf_loader/src/syscalls/mod.rs#L967  */
 
     uchar const * point = fd_vm_translate_vm_to_host_const( ctx, point_addr, POINT_SZ, POINT_ALIGN );
-    if( FD_UNLIKELY( !point ) ) return FD_VM_MEM_MAP_ERR_ACC_VIO;
+    if( FD_UNLIKELY( !point ) ) return FD_VM_ERR_ACC_VIO;
 
     ret = !!fd_ed25519_point_validate( point );  /* 1 if ok, 0 if not */
     break;
@@ -40,7 +37,7 @@ fd_vm_syscall_sol_curve_validate_point(
        https://github.com/solana-labs/solana/blob/c0fbfc6422fa5b739049c01bfda48a0da1bf6a46/programs/bpf_loader/src/syscalls/mod.rs#L985 */
 
     uchar const * point = fd_vm_translate_vm_to_host_const( ctx, point_addr, POINT_SZ, POINT_ALIGN );
-    if( FD_UNLIKELY( !point ) ) return FD_VM_MEM_MAP_ERR_ACC_VIO;
+    if( FD_UNLIKELY( !point ) ) return FD_VM_ERR_ACC_VIO;
 
     ret = !!fd_ristretto255_point_validate( point );  /* 1 if ok, 0 if not */
     break;
@@ -48,21 +45,19 @@ fd_vm_syscall_sol_curve_validate_point(
   default:
     break;
   }
-  *pret = ret;
+  *_ret = ret;
   return FD_VM_SYSCALL_SUCCESS;
 }
 
-ulong
-fd_vm_syscall_sol_curve_group_op(
-    void *  _ctx,
-    ulong   curve_id,
-    ulong   group_op,
-    ulong   in0_addr,
-    ulong   in1_addr,
-    ulong   out_addr,
-    ulong * pret
-) {
-  fd_vm_exec_context_t * ctx = (fd_vm_exec_context_t *) _ctx;
+int
+fd_vm_syscall_sol_curve_group_op( void *  _ctx,
+                                  ulong   curve_id,
+                                  ulong   group_op,
+                                  ulong   in0_addr,
+                                  ulong   in1_addr,
+                                  ulong   out_addr,
+                                  ulong * _ret ) {
+  fd_vm_exec_context_t * ctx = (fd_vm_exec_context_t *)_ctx;
 
   ulong ret = 1UL;
 
@@ -74,9 +69,9 @@ fd_vm_syscall_sol_curve_group_op(
          https://github.com/solana-labs/solana/blob/c0fbfc6422fa5b739049c01bfda48a0da1bf6a46/programs/bpf_loader/src/syscalls/mod.rs#L1027 */
 
       uchar const * p0c = fd_vm_translate_vm_to_host_const( ctx, in0_addr, POINT_SZ, POINT_ALIGN );
-      if( FD_UNLIKELY( !p0c ) ) return FD_VM_MEM_MAP_ERR_ACC_VIO;
+      if( FD_UNLIKELY( !p0c ) ) return FD_VM_ERR_ACC_VIO;
       uchar const * p1c = fd_vm_translate_vm_to_host_const( ctx, in1_addr, POINT_SZ, POINT_ALIGN );
-      if( FD_UNLIKELY( !p1c ) ) return FD_VM_MEM_MAP_ERR_ACC_VIO;
+      if( FD_UNLIKELY( !p1c ) ) return FD_VM_ERR_ACC_VIO;
 
       fd_ed25519_point_t p0[1];
       fd_ed25519_point_t p1[1];
@@ -85,7 +80,7 @@ fd_vm_syscall_sol_curve_group_op(
 
       if( FD_LIKELY( p0v && p1v ) ) {
         uchar * hc = fd_vm_translate_vm_to_host( ctx, out_addr, POINT_SZ, POINT_ALIGN );
-        if( FD_UNLIKELY( !hc ) ) return FD_VM_MEM_MAP_ERR_ACC_VIO;
+        if( FD_UNLIKELY( !hc ) ) return FD_VM_ERR_ACC_VIO;
 
         fd_ed25519_point_t h[1];
         fd_ed25519_point_add( h, p0, p1 );
@@ -99,9 +94,9 @@ fd_vm_syscall_sol_curve_group_op(
          https://github.com/solana-labs/solana/blob/c0fbfc6422fa5b739049c01bfda48a0da1bf6a46/programs/bpf_loader/src/syscalls/mod.rs#L1055 */
 
       uchar const * p0c = fd_vm_translate_vm_to_host_const( ctx, in0_addr, POINT_SZ, POINT_ALIGN );
-      if( FD_UNLIKELY( !p0c ) ) return FD_VM_MEM_MAP_ERR_ACC_VIO;
+      if( FD_UNLIKELY( !p0c ) ) return FD_VM_ERR_ACC_VIO;
       uchar const * p1c = fd_vm_translate_vm_to_host_const( ctx, in1_addr, POINT_SZ, POINT_ALIGN );
-      if( FD_UNLIKELY( !p1c ) ) return FD_VM_MEM_MAP_ERR_ACC_VIO;
+      if( FD_UNLIKELY( !p1c ) ) return FD_VM_ERR_ACC_VIO;
 
       fd_ed25519_point_t p0[1];
       fd_ed25519_point_t p1[1];
@@ -110,7 +105,7 @@ fd_vm_syscall_sol_curve_group_op(
 
       if( FD_LIKELY( p0v && p1v ) ) {
         uchar * hc = fd_vm_translate_vm_to_host( ctx, out_addr, POINT_SZ, POINT_ALIGN );
-        if( FD_UNLIKELY( !hc ) ) return FD_VM_MEM_MAP_ERR_ACC_VIO;
+        if( FD_UNLIKELY( !hc ) ) return FD_VM_ERR_ACC_VIO;
 
         fd_ed25519_point_t h[1];
         fd_ed25519_point_sub( h, p0, p1 );
@@ -124,9 +119,9 @@ fd_vm_syscall_sol_curve_group_op(
          https://github.com/solana-labs/solana/blob/c0fbfc6422fa5b739049c01bfda48a0da1bf6a46/programs/bpf_loader/src/syscalls/mod.rs#L1083 */
 
       uchar const * s  = fd_vm_translate_vm_to_host_const( ctx, in0_addr, SCALAR_SZ, SCALAR_ALIGN );
-      if( FD_UNLIKELY( !s  ) ) return FD_VM_MEM_MAP_ERR_ACC_VIO;
+      if( FD_UNLIKELY( !s  ) ) return FD_VM_ERR_ACC_VIO;
       uchar const * pc = fd_vm_translate_vm_to_host_const( ctx, in1_addr, POINT_SZ,  POINT_ALIGN  );
-      if( FD_UNLIKELY( !pc ) ) return FD_VM_MEM_MAP_ERR_ACC_VIO;
+      if( FD_UNLIKELY( !pc ) ) return FD_VM_ERR_ACC_VIO;
 
       fd_ed25519_point_t p[1];
       int pv = !!fd_ed25519_point_frombytes( p, pc );
@@ -134,7 +129,7 @@ fd_vm_syscall_sol_curve_group_op(
 
       if( FD_LIKELY( pv && sv ) ) {
         uchar * hc = fd_vm_translate_vm_to_host( ctx, out_addr, POINT_SZ, POINT_ALIGN );
-        if( FD_UNLIKELY( !hc ) ) return FD_VM_MEM_MAP_ERR_ACC_VIO;
+        if( FD_UNLIKELY( !hc ) ) return FD_VM_ERR_ACC_VIO;
 
         fd_ed25519_point_t h[1];
         fd_ed25519_scalar_mul( h, s, p );
@@ -153,9 +148,9 @@ fd_vm_syscall_sol_curve_group_op(
          https://github.com/solana-labs/solana/blob/c0fbfc6422fa5b739049c01bfda48a0da1bf6a46/programs/bpf_loader/src/syscalls/mod.rs#L1115 */
 
       uchar const * p0c = fd_vm_translate_vm_to_host_const( ctx, in0_addr, POINT_SZ, POINT_ALIGN );
-      if( FD_UNLIKELY( !p0c ) ) return FD_VM_MEM_MAP_ERR_ACC_VIO;
+      if( FD_UNLIKELY( !p0c ) ) return FD_VM_ERR_ACC_VIO;
       uchar const * p1c = fd_vm_translate_vm_to_host_const( ctx, in1_addr, POINT_SZ, POINT_ALIGN );
-      if( FD_UNLIKELY( !p1c ) ) return FD_VM_MEM_MAP_ERR_ACC_VIO;
+      if( FD_UNLIKELY( !p1c ) ) return FD_VM_ERR_ACC_VIO;
 
       fd_ristretto255_point_t p0[1];
       fd_ristretto255_point_t p1[1];
@@ -164,7 +159,7 @@ fd_vm_syscall_sol_curve_group_op(
 
       if( FD_LIKELY( p0v && p1v ) ) {
         uchar * hc = fd_vm_translate_vm_to_host( ctx, out_addr, POINT_SZ, POINT_ALIGN );
-        if( FD_UNLIKELY( !hc ) ) return FD_VM_MEM_MAP_ERR_ACC_VIO;
+        if( FD_UNLIKELY( !hc ) ) return FD_VM_ERR_ACC_VIO;
 
         fd_ristretto255_point_t h[1];
         fd_ristretto255_point_add( h, p0, p1 );
@@ -179,9 +174,9 @@ fd_vm_syscall_sol_curve_group_op(
          https://github.com/solana-labs/solana/blob/c0fbfc6422fa5b739049c01bfda48a0da1bf6a46/programs/bpf_loader/src/syscalls/mod.rs#L1143 */
 
       uchar const * p0c = fd_vm_translate_vm_to_host_const( ctx, in0_addr, POINT_SZ, POINT_ALIGN );
-      if( FD_UNLIKELY( !p0c ) ) return FD_VM_MEM_MAP_ERR_ACC_VIO;
+      if( FD_UNLIKELY( !p0c ) ) return FD_VM_ERR_ACC_VIO;
       uchar const * p1c = fd_vm_translate_vm_to_host_const( ctx, in1_addr, POINT_SZ, POINT_ALIGN );
-      if( FD_UNLIKELY( !p1c ) ) return FD_VM_MEM_MAP_ERR_ACC_VIO;
+      if( FD_UNLIKELY( !p1c ) ) return FD_VM_ERR_ACC_VIO;
 
       fd_ristretto255_point_t p0[1];
       fd_ristretto255_point_t p1[1];
@@ -190,7 +185,7 @@ fd_vm_syscall_sol_curve_group_op(
 
       if( FD_LIKELY( p0v && p1v ) ) {
         uchar * hc = fd_vm_translate_vm_to_host( ctx, out_addr, POINT_SZ, POINT_ALIGN );
-        if( FD_UNLIKELY( !hc ) ) return FD_VM_MEM_MAP_ERR_ACC_VIO;
+        if( FD_UNLIKELY( !hc ) ) return FD_VM_ERR_ACC_VIO;
 
         fd_ristretto255_point_t h[1];
         fd_ristretto255_point_sub( h, p0, p1 );
@@ -204,9 +199,9 @@ fd_vm_syscall_sol_curve_group_op(
          https://github.com/solana-labs/solana/blob/c0fbfc6422fa5b739049c01bfda48a0da1bf6a46/programs/bpf_loader/src/syscalls/mod.rs#L1173 */
 
       uchar const * s  = fd_vm_translate_vm_to_host_const( ctx, in0_addr, SCALAR_SZ, SCALAR_ALIGN );
-      if( FD_UNLIKELY( !s  ) ) return FD_VM_MEM_MAP_ERR_ACC_VIO;
+      if( FD_UNLIKELY( !s  ) ) return FD_VM_ERR_ACC_VIO;
       uchar const * pc = fd_vm_translate_vm_to_host_const( ctx, in1_addr, POINT_SZ,  POINT_ALIGN  );
-      if( FD_UNLIKELY( !pc ) ) return FD_VM_MEM_MAP_ERR_ACC_VIO;
+      if( FD_UNLIKELY( !pc ) ) return FD_VM_ERR_ACC_VIO;
 
       fd_ristretto255_point_t p[1];
       int pv = !!fd_ristretto255_point_frombytes( p, pc );
@@ -214,7 +209,7 @@ fd_vm_syscall_sol_curve_group_op(
 
       if( FD_LIKELY( pv && sv ) ) {
         uchar * hc = fd_vm_translate_vm_to_host( ctx, out_addr, POINT_SZ, POINT_ALIGN );
-        if( FD_UNLIKELY( !hc ) ) return FD_VM_MEM_MAP_ERR_ACC_VIO;
+        if( FD_UNLIKELY( !hc ) ) return FD_VM_ERR_ACC_VIO;
 
         fd_ristretto255_point_t h[1];
         fd_ed25519_scalar_mul( h, s, p );
@@ -230,7 +225,7 @@ fd_vm_syscall_sol_curve_group_op(
     break;
   }
 
-  *pret = ret;
+  *_ret = ret;
   return FD_VM_SYSCALL_SUCCESS;
 }
 
@@ -297,16 +292,14 @@ multi_scalar_mul_ristretto( fd_ristretto255_point_t * r,
   return r;
 }
 
-ulong
-fd_vm_syscall_sol_curve_multiscalar_mul(
-    void *  _ctx,
-    ulong   curve_id,
-    ulong   scalar_addr,
-    ulong   point_addr,
-    ulong   point_cnt,
-    ulong   result_point_addr,
-    ulong * pret
-) {
+int
+fd_vm_syscall_sol_curve_multiscalar_mul( void *  _ctx,
+                                         ulong   curve_id,
+                                         ulong   scalar_addr,
+                                         ulong   point_addr,
+                                         ulong   point_cnt,
+                                         ulong   result_point_addr,
+                                         ulong * _ret ) {
   fd_vm_exec_context_t * ctx = (fd_vm_exec_context_t *) _ctx;
 
   ulong ret = 1UL;
@@ -321,15 +314,15 @@ fd_vm_syscall_sol_curve_multiscalar_mul(
     /* TODO consume CU
        https://github.com/solana-labs/solana/blob/d6aba9dc483a79ab569b47b7f3df19e6535f6722/programs/bpf_loader/src/syscalls/mod.rs#L1233 */
     uchar const * s  = fd_vm_translate_vm_to_host_const( ctx, scalar_addr, scalar_list_sz, SCALAR_ALIGN );
-    if( FD_UNLIKELY( !s  ) ) return FD_VM_MEM_MAP_ERR_ACC_VIO;
+    if( FD_UNLIKELY( !s  ) ) return FD_VM_ERR_ACC_VIO;
     uchar const * pc = fd_vm_translate_vm_to_host_const( ctx, point_addr,  point_list_sz,  POINT_ALIGN  );
-    if( FD_UNLIKELY( !pc ) ) return FD_VM_MEM_MAP_ERR_ACC_VIO;
+    if( FD_UNLIKELY( !pc ) ) return FD_VM_ERR_ACC_VIO;
 
     fd_ed25519_point_t r_[1];
     fd_ed25519_point_t * r = multi_scalar_mul_edwards( r_, s, pc, point_cnt );
     if( FD_LIKELY( r ) ) {
       uchar * rc = fd_vm_translate_vm_to_host( ctx, result_point_addr, POINT_SZ, POINT_ALIGN );
-      if( FD_UNLIKELY( !rc ) ) return FD_VM_MEM_MAP_ERR_ACC_VIO;
+      if( FD_UNLIKELY( !rc ) ) return FD_VM_ERR_ACC_VIO;
       fd_ed25519_point_tobytes( rc, r );
       ret = 0UL;
     }
@@ -340,15 +333,15 @@ fd_vm_syscall_sol_curve_multiscalar_mul(
        https://github.com/solana-labs/solana/blob/d6aba9dc483a79ab569b47b7f3df19e6535f6722/programs/bpf_loader/src/syscalls/mod.rs#L1273 */
 
     uchar const * s  = fd_vm_translate_vm_to_host_const( ctx, scalar_addr, scalar_list_sz, SCALAR_ALIGN );
-    if( FD_UNLIKELY( !s  ) ) return FD_VM_MEM_MAP_ERR_ACC_VIO;
+    if( FD_UNLIKELY( !s  ) ) return FD_VM_ERR_ACC_VIO;
     uchar const * pc = fd_vm_translate_vm_to_host_const( ctx, point_addr,  point_list_sz,  POINT_ALIGN  );
-    if( FD_UNLIKELY( !pc ) ) return FD_VM_MEM_MAP_ERR_ACC_VIO;
+    if( FD_UNLIKELY( !pc ) ) return FD_VM_ERR_ACC_VIO;
 
     fd_ristretto255_point_t r_[1];
     fd_ristretto255_point_t * r = multi_scalar_mul_ristretto( r_, s, pc, point_cnt );
     if( FD_LIKELY( r ) ) {
       uchar * rc = fd_vm_translate_vm_to_host( ctx, result_point_addr, POINT_SZ, POINT_ALIGN );
-      if( FD_UNLIKELY( !rc ) ) return FD_VM_MEM_MAP_ERR_ACC_VIO;
+      if( FD_UNLIKELY( !rc ) ) return FD_VM_ERR_ACC_VIO;
       fd_ristretto255_point_tobytes( rc, r );
       ret = 0UL;
     }
@@ -358,6 +351,6 @@ fd_vm_syscall_sol_curve_multiscalar_mul(
     break;
   }
 
-  *pret = ret;
+  *_ret = ret;
   return FD_VM_SYSCALL_SUCCESS;
 }
