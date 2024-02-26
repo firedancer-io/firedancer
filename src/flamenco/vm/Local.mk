9203f7b9fc4a33e0ff485d81fcfc59f57cf80fbc
Aryaman Jain <arjain@jumptrading.com>
2024-02-26 21:55:35 +0000

    flamenco, vm: Fixes vm tests and sol_log_data - Fix for spaces in log messages - Adds safety checks to sol_log_data - Fixes test setup code - Adds vm tests to CI

--- src/flamenco/vm/Local.mk
+++ src/flamenco/vm/Local.mk
@@ -8,6 +8,10 @@ $(call make-bin,fd_vm_tool,fd_vm_tool,fd_flamenco fd_funk fd_ballet fd_util,$(SE
 endif
 endif
 
+$(call make-unit-test,test_vm_interp,test_vm_interp,fd_flamenco fd_funk fd_ballet fd_util)
+$(call run-unit-test,test_vm_interp)
+$(call make-unit-test,test_vm_syscalls,test_vm_syscalls,fd_flamenco fd_funk fd_ballet fd_util)
+$(call run-unit-test,test_vm_syscalls)
 $(call make-unit-test,test_vm_cpi,test_vm_cpi,fd_util)
 $(call run-unit-test,test_vm_cpi)
 endif
