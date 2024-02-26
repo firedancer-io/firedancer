0d3ca4012fe7f14a7eb3d204d9cfc25246e44347
Kevin J Bowers <kbowers@jumptrading.com>
2024-02-26 15:09:01 -0600

    flamenco, vm: Belt sanding syscalls
    - Moved utility-ish syscalls from fd_vm_syscalls.c to sys/fd_vm_syscall_util.c
    - Documented the detailed behavior of these syscalls
    - Ton of annotations of potential issues in the API, potential
      divergences with Solana, implementation performance issues, etc.
    - Minor comment cleanups and additional unrelated fixmes encountered.

--- src/flamenco/vm/syscall/Local.mk
+++ src/flamenco/vm/syscall/Local.mk
@@ -1,6 +1,6 @@
 ifdef FD_HAS_INT128
 $(call add-hdrs,fd_vm_syscall.h)
-$(call add-objs,fd_vm_syscall_crypto,fd_flamenco)
+$(call add-objs,fd_vm_syscall_util fd_vm_syscall_crypto,fd_flamenco)
 $(call make-unit-test,test_vm_syscall_crypto,test_vm_syscall_crypto,fd_flamenco fd_funk fd_ballet fd_util)
 $(call run-unit-test,test_vm_syscall_crypto)
 endif
