6c9fdde341d3c251fbf55a153249ddfddf6ef5d7
Kevin J Bowers <kbowers@jumptrading.com>
2024-02-29 15:16:11 -0600

    flamenco, vm: Belt sanding syscall organization
    - Finish organizing the syscalls into more manageable chunks by pulling
      fd_vm_syscalls.c into syscall/fd_vm_syscall_admin and
      syscall/fd_vm_syscall_cpi.
    - Renamed register_syscall to syscall_register to be consistent with
      other naming.
    - Updated build accordingly.
    - Various annotations addition cleanups needed
    - Minor include cleanups

--- src/flamenco/vm/syscall/Local.mk
+++ src/flamenco/vm/syscall/Local.mk
@@ -1,6 +1,6 @@
 ifdef FD_HAS_INT128
 $(call add-hdrs,fd_vm_syscall.h)
-$(call add-objs,fd_vm_syscall_util fd_vm_syscall_pda fd_vm_syscall_crypto fd_vm_syscall_curve,fd_flamenco)
+$(call add-objs,fd_vm_syscall_admin fd_vm_syscall_cpi fd_vm_syscall_crypto fd_vm_syscall_curve fd_vm_syscall_pda fd_vm_syscall_util,fd_flamenco)
 $(call make-unit-test,test_vm_syscall_curve,test_vm_syscall_curve,fd_flamenco fd_funk fd_ballet fd_util)
 $(call run-unit-test,test_vm_syscall_curve)
 endif
