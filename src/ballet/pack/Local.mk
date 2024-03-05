76ec99ec530c7ed790c795b45634cddf2a519f05
nlgripto <155116201+nlgripto@users.noreply.github.com>
2024-03-05 17:32:29 +0000

    pack fuzzer

--- src/ballet/pack/Local.mk
+++ src/ballet/pack/Local.mk
@@ -9,5 +9,6 @@ $(call run-unit-test,test_compute_budget_program,)
 $(call run-unit-test,test_est_tbl,)
 $(call run-unit-test,test_pack,)
 $(call make-fuzz-test,fuzz_compute_budget_program_parse,fuzz_compute_budget_program_parse,fd_ballet fd_util)
+$(call make-fuzz-test,fuzz_pack,fuzz_pack,fd_ballet fd_util fd_disco)
 $(call run-unit-test,test_pack_bitset,)
 endif
