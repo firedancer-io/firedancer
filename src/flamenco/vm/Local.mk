91da3723b43fd902c90ad1f7b098982c1dc6c099
Tom Pointon <topointon@jumptrading.com>
2024-03-07 12:40:36 +0000

    Fix build

--- src/flamenco/vm/Local.mk
+++ src/flamenco/vm/Local.mk
@@ -3,7 +3,7 @@ $(call add-hdrs,fd_vm_base.h fd_vm_cpi.h fd_vm_trace.h fd_vm_context.h fd_vm_int
 $(call add-objs,fd_vm_context fd_vm_interp fd_vm_trace,fd_flamenco)
 
 ifdef FD_HAS_HOSTED
-$(call make-bin,fd_vm_tool,fd_vm_tool,fd_flamenco fd_funk fd_ballet fd_util,$(SECP256K1_LIBS))
+$(call make-bin,fd_vm_tool,fd_vm_tool fd_vm_disasm,fd_flamenco fd_funk fd_ballet fd_util,$(SECP256K1_LIBS))
 endif
 
 $(call make-unit-test,test_vm_interp,test_vm_interp,fd_flamenco fd_funk fd_ballet fd_util)
