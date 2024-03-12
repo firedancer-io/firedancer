d72e3a1d8099dfd477300577002bfb34f4ee6cb4
Kevin J Bowers <kbowers@jumptrading.com>
2024-03-11 21:48:47 -0500

    Belt sanding fd_vm_t internals
    - Renamed instrs/instr_sz/instrs_off field to text/text_cnt/test_off to
      be consistent with general API conventions and the fields already used
      elsewhere (e.g. fd_sbpf_program_t).  This exposed a very nasty issue:
      instr_sz was actually a word count but instrs_off is a byte offset
      (which is very very confusing ... sz are byte counts).  While all
      existing code was treating instrs_off as a byte offset correctly
      AFAICT, there seems to be a unaddressed risk of misalignment here ...
      suspect this probably should be made a word offset (faster and less
      error prone and only need to check it upfront in the ELF loader).
    
    - Renamed fd_vm_interp_instrs / fd_vm_interp_instrs_trace to fd_vm_exec
      / fd_vm_exec_trace as the callers really don't care how the BPF gets
      done (and thus makes it easier to do a drop in replacement API with a
      JIT if we ever end up needing it).
    
    - Renamed syscall_map field to syscalls for similar reasons.
    
    - There was lot of type confusion in the code as to whether or not the
      text section was a pointer to a ulong or a pointer to a
      fd_sbpf_instr_t.  Text sections are handled consistenly now (as a
      ulong * text / ulong text_cnt), which has the pleasant side effect of
      getting rid of a bunch of grotty type puns, eliminating a bunch of
      spurious dependencies and (maybe) making the code faster as type puns
      inhibit compiler optimization (but probably not meaningfully in this
      particular case).
    
    - Made cond_fault explicitly an FD_VM_ERR code (it was being used
      inconsistently before) and added to the strerror and unit tests.  This
      exposed some nasty magic number cond_fault values in the tracer (e.g.
      cond_fault = 994??) and possibly misassigned cond_faults in general
      (MEM_TRANS seems to be assigned in places where it shouldn't).  These
      haven't been fully investigated yet.
    
    - Added hopefully temporary scaffolding to convert between
      fd_sbpf_instr_t and ulong to support above.  Longer term is to get rid
      of explicit nested bit fields in fd_sbpf_instr_t as the compiler
      historically does a poor job optimizing these (and this is very
      critical path set of operations).
    
    - Plumbed all this through existing users.
    
    - Fixed a couple of minor latent bugs, added more documentation and
      more annotations for future fixmes as per usual.

--- src/ballet/sbpf/fd_sbpf_instr.h
+++ src/ballet/sbpf/fd_sbpf_instr.h
@@ -40,4 +40,24 @@ struct fd_sbpf_instr {
 };
 typedef struct fd_sbpf_instr fd_sbpf_instr_t;
 
+FD_PROTOTYPES_BEGIN
+
+/* FIXME: Use proper fast ulong decoding and unions */
+
+FD_FN_CONST static inline fd_sbpf_instr_t
+fd_sbpf_instr( ulong u ) {
+  union { ulong u; fd_sbpf_instr_t instr; } _;
+  _.u = u;
+  return _.instr;
+}
+
+FD_FN_CONST static inline ulong
+fd_sbpf_ulong( fd_sbpf_instr_t instr ) {
+  union { ulong u; fd_sbpf_instr_t instr; } _;
+  _.instr = instr;
+  return _.u;
+}
+
+FD_PROTOTYPES_END
+
 #endif /* HEADER_fd_src_ballet_sbpf_fd_sbpf_instr_h */
