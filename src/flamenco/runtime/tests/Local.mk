222d229b661d0a82e999987a06c8c92de579b359
jumpsiegel <83408952+jumpsiegel@users.noreply.github.com>
2024-03-13 08:12:15 -0500

    epoch-586: add new epoch boundry (#681)
    * epoch-586: add new epoch boundry
    
    * epoch-586: progress
    
    * epoch-586: Lets see if this passes
    
    * epoch-586: new test

--- src/flamenco/runtime/tests/Local.mk
+++ src/flamenco/runtime/tests/Local.mk
@@ -12,17 +12,18 @@ $(call make-unit-test,test_rent_lists,test_rent_lists,fd_flamenco fd_funk fd_bal
 $(call make-unit-test,test_hashes,test_hashes,fd_ballet fd_funk fd_util fd_flamenco)
 $(call make-unit-test,bench_tps,bench_tps,fd_aio fd_quic fd_tls fd_ballet fd_tango fd_util)
 
-# WARNING 03-07 16:26:44.035818 569410 f0   0    src/flamenco/runtime/fd_runtime.c(3593): Bank hash mismatch! slot=251418232 expected=H44FqPbX6Q9ZWKjzQppz8tEdQnKxkbFQms7VA2ZWE4sN, got=matVdNPdYycwL3PJ5zB7bHKQv2zuv43SH8fAz9xwKP5
+# WARNING 03-13 12:03:41.058581 3173957 f0   0    src/flamenco/runtime/fd_runtime.c(3602): Bank hash mismatch! slot=253152051 expected=45XyxUMojrigueXJUYmCqhP2T39yUiBuCEUzHHRtux3U, got=DkT3gsRX46hMVEgepTqHwrdhsyGi3K4bntDSSoHwUDTY
 
 run-runtime-test: $(OBJDIR)/unit-test/test_native_programs $(OBJDIR)/unit-test/test_runtime $(OBJDIR)/bin/fd_frank_ledger
-	OBJDIR=$(OBJDIR) src/flamenco/runtime/tests/run_ledger_tests.sh -l mainnet-251418170 -s snapshot-251418231-EkDdaUmskEdPnTtooQMUaFY8tNPBAqy5dDf8HyqiatLe.tar.zst -p 64 -m 2000000 -e 251418233
+	# OBJDIR=$(OBJDIR) src/flamenco/runtime/tests/run_ledger_tests.sh -l mainnet-586 -s snapshot-253151900-HVhfam8TtRFVwFto5fWkhgR4mbBJmUxcnxeKZoW5MrSD.tar.zst  -p 64 -m 5000000 -e 253152100
+	OBJDIR=$(OBJDIR) src/flamenco/runtime/tests/run_ledger_tests.sh -l mainnet-586 -s snapshot-253151900-HVhfam8TtRFVwFto5fWkhgR4mbBJmUxcnxeKZoW5MrSD.tar.zst  -p 64 -m 5000000 -e 253152050
 	OBJDIR=$(OBJDIR) src/flamenco/runtime/tests/run_ledger_tests.sh -t 2 -X 1
 	OBJDIR=$(OBJDIR) src/flamenco/runtime/tests/run_ledger_tests.sh
 	OBJDIR=$(OBJDIR) src/flamenco/runtime/tests/run_ledger_tests.sh -l helloworld -s snapshot-100-92rXQxDb3gbNU4YEjof4PjAQ9wDvqAXL4Ma3757kHPRs.tar.zst
 	OBJDIR=$(OBJDIR) src/flamenco/runtime/tests/run_ledger_tests.sh -l v118-multi
 	OBJDIR=$(OBJDIR) src/flamenco/runtime/tests/run_ledger_tests.sh -l mainnet-579 -s snapshot-250127990-2qrtKDgYDP7xSzT4LkVDVfDTb42JHyn7b6L88kwaiL9E.tar.zst -p 64 -m 5000000 -e 250128010
 	OBJDIR=$(OBJDIR) src/flamenco/runtime/tests/run_ledger_tests.sh -l testnet-519 -s snapshot-255311992-Fju7xb3XaTY6SBxkGcsKko15EGAqnvdfkXBd1o6agPDq.tar.zst -p 64 -m 1000000 -e 255312010
-	OBJDIR=$(OBJDIR) src/flamenco/runtime/tests/run_ledger_tests.sh -l mainnet-251418170 -s snapshot-251418170-8sAkojR9PYTZvqiQZ1VWu27ewX5tXeVdC97wMXAtgHnT.tar.zst -p 64 -m 2000000 -e 251418231
+	OBJDIR=$(OBJDIR) src/flamenco/runtime/tests/run_ledger_tests.sh -l mainnet-251418170 -s snapshot-251418170-8sAkojR9PYTZvqiQZ1VWu27ewX5tXeVdC97wMXAtgHnT.tar.zst -p 64 -m 2000000 -e 251418233
 	OBJDIR=$(OBJDIR) src/flamenco/runtime/tests/run_native_tests.sh
 #	src/flamenco/runtime/run_bpf_tests.sh
 
