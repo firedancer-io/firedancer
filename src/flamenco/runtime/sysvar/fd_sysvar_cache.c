ee592091a05c6cab95e1e4eb9cc0b21036af15c7
Richard Patel <ripatel@jumptrading.com>
2024-03-17 04:21:29 +0000

    runtime: simplify sysvar cache

--- src/flamenco/runtime/sysvar/fd_sysvar_cache.c
+++ src/flamenco/runtime/sysvar/fd_sysvar_cache.c
@@ -2,8 +2,7 @@
 #include "../fd_acc_mgr.h"
 #include "../fd_system_ids.h"
 
-#define FD_SYSVAR_CACHE_MAGIC    (0x195a0e78828cacd5UL)
-#define FD_SYSVAR_CACHE_WKSP_TAG (25UL)
+#define FD_SYSVAR_CACHE_MAGIC (0x195a0e78828cacd5UL)
 
 /* Reuse this table to avoid code duplication */
 #define FD_SYSVAR_CACHE_ITER(X) \
@@ -27,8 +26,8 @@
    allowing for safe idempotent calls to fd_sol_sysvar_{...}_destroy() */
 
 struct __attribute__((aligned(16UL))) fd_sysvar_cache_private {
-  ulong        magic;  /* ==FD_SYSVAR_CACHE_MAGIC */
-  fd_alloc_t * alloc;
+  ulong       magic;  /* ==FD_SYSVAR_CACHE_MAGIC */
+  fd_valloc_t valloc;
 
   /* Declare the val_{...} values */
 # define X( type, name ) \
@@ -55,29 +54,17 @@ fd_sysvar_cache_footprint( void ) {
 
 fd_sysvar_cache_t *
 fd_sysvar_cache_new( void *      mem,
-                     fd_wksp_t * wksp,
-                     ulong       cgroup_hint ) {
+                     fd_valloc_t valloc ) {
 
   if( FD_UNLIKELY( !mem ) ) {
     FD_LOG_WARNING(( "NULL mem" ));
     return NULL;
   }
 
-  if( FD_UNLIKELY( !wksp ) ) {
-    FD_LOG_WARNING(( "NULL wksp" ));
-    return NULL;
-  }
-
   fd_sysvar_cache_t * cache = (fd_sysvar_cache_t *)mem;
   fd_memset( cache, 0, sizeof(fd_sysvar_cache_t) );
 
-  ulong  wksp_tag  = FD_SYSVAR_CACHE_WKSP_TAG;
-  void * alloc_mem = fd_wksp_alloc_laddr( wksp, fd_alloc_align(), fd_alloc_footprint(), cgroup_hint );
-
-  fd_alloc_t * alloc = fd_alloc_join( fd_alloc_new( alloc_mem, wksp_tag ), cgroup_hint );
-  if( FD_UNLIKELY( !alloc ) ) return NULL;
-
-  cache->alloc = alloc;
+  cache->valloc = valloc;
 
   FD_COMPILER_MFENCE();
   cache->magic = FD_SYSVAR_CACHE_MAGIC;
@@ -98,7 +85,7 @@ fd_sysvar_cache_delete( fd_sysvar_cache_t * cache ) {
     return NULL;
   }
 
-  if( FD_UNLIKELY( !cache->alloc ) ) {
+  if( FD_UNLIKELY( !cache->valloc.vt ) ) {
     FD_LOG_WARNING(( "NULL alloc" ));
     return NULL;
   }
@@ -107,9 +94,7 @@ fd_sysvar_cache_delete( fd_sysvar_cache_t * cache ) {
   cache->magic = 0UL;
   FD_COMPILER_MFENCE();
 
-  /* fd_alloc_delete is not guaranteed to delete all allocations */
-  fd_alloc_t * alloc = cache->alloc;
-  fd_bincode_destroy_ctx_t ctx = { .valloc = fd_alloc_virtual( alloc ) };
+  fd_bincode_destroy_ctx_t ctx = { .valloc = cache->valloc };
 
   /* Call destroy on all objects.
      This is safe even if these objects logically don't exist
@@ -119,117 +104,59 @@ fd_sysvar_cache_delete( fd_sysvar_cache_t * cache ) {
   FD_SYSVAR_CACHE_ITER(X)
 # undef X
 
-  fd_alloc_delete( fd_alloc_leave( cache->alloc ) );
   return (void *)cache;
 }
 
-/* Define accessor methods */
+/* Provide accessor methods */
 
 #define X( type, name )                                                \
   type##_t const *                                                     \
   fd_sysvar_cache_##name( fd_sysvar_cache_t const * cache ) {          \
-    return cache->val_##name;                                          \
+    type##_t const * val = cache->val_##name;                          \
+    return (cache->has_##name) ? val : NULL;                           \
   }
 FD_SYSVAR_CACHE_ITER(X)
 #undef X
 
-/* Declare functions to restore sysvars */
-
-#define X( type, name )                                                \
-  static void                                                          \
-  restore_##name( fd_sysvar_cache_t * cache,                           \
-                  uchar const *       data,                            \
-                  ulong               data_sz );
-FD_SYSVAR_CACHE_ITER(X)
-#undef X
-
-static void
-restore_one( fd_sysvar_cache_t * cache,
-             fd_pubkey_t const * address,
-             uchar const *       data,
-             ulong               data_sz ) {
-  /* Generate a chain of ifs that reloads the requested sysvar */
-  /* TODO it would be nice to use the perfect hash table here to turn
-          this into a switch */
-# define X( type, name )                                                    \
-  if( 0==memcmp( address, &fd_sysvar_##name##_id, sizeof(fd_pubkey_t) ) ) { \
-    restore_##name( cache, data, data_sz );                                 \
-    return;                                                                 \
-  }
-  FD_SYSVAR_CACHE_ITER(X)
-# undef X
-}
+/* Restore sysvars */
 
 void
-fd_sysvar_cache_restore_one( fd_sysvar_cache_t * cache,
-                             fd_pubkey_t const * address,
-                             uchar const *       data,
-                             ulong               data_sz ) {
-  if( !fd_pubkey_is_sysvar_id( address ) ) return;
-  restore_one( cache, address, data, data_sz );
-}
+fd_sysvar_cache_restore( fd_sysvar_cache_t * cache,
+                         fd_acc_mgr_t *      acc_mgr,
+                         fd_funk_txn_t *     funk_txn ) {
 
-static void
-restore_one_managed( fd_sysvar_cache_t *  cache,
-                     fd_exec_slot_ctx_t * slot_ctx,
-                     fd_pubkey_t const *  pubkey ) {
-
-  fd_acc_mgr_t *  acc_mgr  = slot_ctx->acc_mgr;
-  fd_funk_txn_t * funk_txn = slot_ctx->funk_txn;
-
-  FD_BORROWED_ACCOUNT_DECL( account );
-  int view_err = fd_acc_mgr_view( acc_mgr, funk_txn, pubkey, account );
-  if( view_err==FD_ACC_MGR_ERR_UNKNOWN_ACCOUNT ) return;
-
-  if( FD_UNLIKELY( view_err!=FD_ACC_MGR_SUCCESS ) ) {
-    char pubkey_cstr[ FD_BASE58_ENCODED_32_SZ ];
-    FD_LOG_ERR(( "fd_acc_mgr_view(%s) failed (%d-%s)",
-                 fd_acct_addr_cstr( pubkey_cstr, pubkey->key ),
-                 view_err, fd_acc_mgr_strerror( view_err ) ));
-  }
-
-  restore_one( cache, pubkey, account->const_data, account->const_meta->dlen );
-}
-
-void
-fd_sysvar_cache_restore( fd_sysvar_cache_t *  cache,
-                         fd_exec_slot_ctx_t * slot_ctx ) {
-
-  static fd_pubkey_t const * pubkeys[] = {
-#   define X( type, name ) &fd_sysvar_##name##_id,
-    FD_SYSVAR_CACHE_ITER(X)
-#   undef X
-  };
-  ulong const pubkey_cnt = sizeof(pubkeys)/sizeof(fd_pubkey_t const *);
-
-  for( ulong j=0UL; j<pubkey_cnt; j++ ) {
-    restore_one_managed( cache, slot_ctx, pubkeys[j] );
-  }
-}
-
-/* Generate functions to restore sysvars */
-
-#define X( type, name )                                                \
-  static void                                                          \
-  restore_##name( fd_sysvar_cache_t * cache,                           \
-                  uchar const *       data,                            \
-                  ulong               data_sz ) {                      \
+# define X( type, name )                                               \
+  do {                                                                 \
+    fd_pubkey_t const * pubkey = &fd_sysvar_##name##_id;               \
+    FD_BORROWED_ACCOUNT_DECL( account );                               \
+    int view_err = fd_acc_mgr_view( acc_mgr, funk_txn, pubkey, account );\
+    if( view_err==FD_ACC_MGR_ERR_UNKNOWN_ACCOUNT ) break;              \
+                                                                       \
+    if( view_err!=FD_ACC_MGR_SUCCESS ) {                               \
+      char pubkey_cstr[ FD_BASE58_ENCODED_32_SZ ];                     \
+      FD_LOG_ERR(( "fd_acc_mgr_view(%s) failed (%d-%s)",               \
+                  fd_acct_addr_cstr( pubkey_cstr, pubkey->key ),       \
+                  view_err, fd_acc_mgr_strerror( view_err ) ));        \
+    }                                                                  \
+                                                                       \
+    if( account->const_meta->info.lamports == 0UL ) break;             \
                                                                        \
     /* Destroy previous value */                                       \
                                                                        \
-    fd_bincode_destroy_ctx_t destroy =                                 \
-      { .valloc = fd_alloc_virtual( cache->alloc ) };                  \
+    fd_bincode_destroy_ctx_t destroy = { .valloc = cache->valloc };    \
     type##_destroy( cache->val_##name, &destroy );                     \
                                                                        \
     /* Decode new value                                                \
-       type##_decode() does not do heap allocations on failure */      \
+      type##_decode() does not do heap allocations on failure */       \
                                                                        \
     fd_bincode_decode_ctx_t decode =                                   \
-      { .data    = data,                                               \
-        .dataend = data + data_sz,                                     \
-        .valloc  = fd_alloc_virtual( cache->alloc ) };                 \
+      { .data    = account->const_data,                                \
+        .dataend = account->const_data + account->const_meta->dlen,    \
+        .valloc  = cache->valloc };                                    \
     int err = type##_decode( cache->val_##name, &decode );             \
     cache->has_##name = (err==FD_BINCODE_SUCCESS);                     \
-  }
-FD_SYSVAR_CACHE_ITER(X)
-#undef X
+  } while(0);
+
+  FD_SYSVAR_CACHE_ITER(X)
+# undef X
+}
--- src/flamenco/runtime/sysvar/fd_sysvar_cache.h
+++ src/flamenco/runtime/sysvar/fd_sysvar_cache.h
@@ -32,7 +32,7 @@
    fd_sysvar_cache_t is contained by a continuous memory region and
    embeds a heap allocator. */
 
-#include "../context/fd_exec_slot_ctx.h"
+#include "../fd_acc_mgr.h"
 
 struct fd_sysvar_cache_private;
 typedef struct fd_sysvar_cache_private fd_sysvar_cache_t;
@@ -49,52 +49,46 @@ ulong
 fd_sysvar_cache_footprint( void );
 
 /* fd_sysvar_cache_new creates a new sysvar cache object.  mem is the
-   memory region that will back the fd_sysvar_cache_t.  Also creates a
-   new fd_alloc_t in the given wksp with cgroup_hint that is managed by
-   the fd_sysvar_cache_t.  Returns object (in mem) on success and NULL
-   on failure.  Logs reasons for failure. */
+   memory region that will back the fd_sysvar_cache_t.  Attaches to the
+   given valloc for use as a heap allocator for sysvar data.  Returns
+   object (in mem) on success and NULL on failure.  Logs reasons for
+   failure. */
 
 fd_sysvar_cache_t *
 fd_sysvar_cache_new( void *      mem,
-                     fd_wksp_t * wksp,
-                     ulong       cgroup_hint );
+                     fd_valloc_t valloc );
 
-/* fd_sysvar_cache_delete destroys a given sysvar cache object, any
-   fd_alloc allocations made, and the fd_alloc_t allocator itself.
-   Returns the memory region that previously backed cache back to the
-   caller. */
+/* fd_sysvar_cache_delete destroys a given sysvar cache object and any
+   heap allocations made.  Detaches from the valloc provided in
+   fd_sysvar_cache_new.  Returns the memory region that previously
+   backed cache back to the caller. */
 
 void *
 fd_sysvar_cache_delete( fd_sysvar_cache_t * cache );
 
 /* fd_sysvar_cache_restore restores all sysvars from the given slot
-   context. */
+   context.
+
+   Roughly compatible with Agave's
+   solana_program_runtime::sysvar_cache::SysvarCache::fill_missing_entries
+   https://github.com/solana-labs/solana/blob/v1.17.23/program-runtime/src/sysvar_cache.rs#L137-L208 */
 
 void
-fd_sysvar_cache_restore( fd_sysvar_cache_t *  cache,
-                         fd_exec_slot_ctx_t * slot_ctx );
-
-/* fd_sysvar_cache_restore_one attempts to restore a sysvar from
-   account data.  It's fine to pass an account address that is not a
-   sysvar. */
-
-void
-fd_sysvar_cache_restore_one( fd_sysvar_cache_t * cache,
-                             fd_pubkey_t const * address,
-                             uchar const *       data,
-                             ulong               data_sz );
+fd_sysvar_cache_restore( fd_sysvar_cache_t * cache,
+                         fd_acc_mgr_t *      acc_mgr,
+                         fd_funk_txn_t *     funk_txn );
 
 /* Accessors for sysvars.  May return NULL. */
 
-FD_FN_PURE fd_sol_sysvar_clock_t             const * fd_sysvar_cache_clock             ( fd_sysvar_cache_t const * cache );
-FD_FN_PURE fd_epoch_schedule_t               const * fd_sysvar_cache_epoch_schedule    ( fd_sysvar_cache_t const * cache );
-FD_FN_PURE fd_sysvar_epoch_rewards_t         const * fd_sysvar_cache_epoch_rewards     ( fd_sysvar_cache_t const * cache );
-FD_FN_PURE fd_sysvar_fees_t                  const * fd_sysvar_cache_fees              ( fd_sysvar_cache_t const * cache );
-FD_FN_PURE fd_rent_t                         const * fd_sysvar_cache_rent              ( fd_sysvar_cache_t const * cache );
-FD_FN_PURE fd_slot_hashes_t                  const * fd_sysvar_cache_slot_hashes       ( fd_sysvar_cache_t const * cache );
-FD_FN_PURE fd_recent_block_hashes_t          const * fd_sysvar_cache_recent_blockhashes( fd_sysvar_cache_t const * cache );
-FD_FN_PURE fd_stake_history_t                const * fd_sysvar_cache_stake_history     ( fd_sysvar_cache_t const * cache );
-FD_FN_PURE fd_sol_sysvar_last_restart_slot_t const * fd_sysvar_cache_last_restart_slot ( fd_sysvar_cache_t const * cache );
+FD_FN_PURE fd_sol_sysvar_clock_t             const * fd_sysvar_cache_clock              ( fd_sysvar_cache_t const * cache );
+FD_FN_PURE fd_epoch_schedule_t               const * fd_sysvar_cache_epoch_schedule     ( fd_sysvar_cache_t const * cache );
+FD_FN_PURE fd_sysvar_epoch_rewards_t         const * fd_sysvar_cache_epoch_rewards      ( fd_sysvar_cache_t const * cache );
+FD_FN_PURE fd_sysvar_fees_t                  const * fd_sysvar_cache_fees               ( fd_sysvar_cache_t const * cache );
+FD_FN_PURE fd_rent_t                         const * fd_sysvar_cache_rent               ( fd_sysvar_cache_t const * cache );
+FD_FN_PURE fd_slot_hashes_t                  const * fd_sysvar_cache_slot_hashes        ( fd_sysvar_cache_t const * cache );
+FD_FN_PURE fd_recent_block_hashes_t          const * fd_sysvar_cache_recent_block_hashes( fd_sysvar_cache_t const * cache );
+FD_FN_PURE fd_stake_history_t                const * fd_sysvar_cache_stake_history      ( fd_sysvar_cache_t const * cache );
+FD_FN_PURE fd_sol_sysvar_last_restart_slot_t const * fd_sysvar_cache_last_restart_slot  ( fd_sysvar_cache_t const * cache );
 
 FD_PROTOTYPES_END
 
