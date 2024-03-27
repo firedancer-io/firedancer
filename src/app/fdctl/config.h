df5d436a2af0f2c4f8d403a740d8a504227ddf70
Philip Taffet <phtaffet@jumptrading.com>
2024-03-27 16:26:20 +0000

    bench: add consensus breaking option to raise shred limits

--- src/app/fdctl/config.h
+++ src/app/fdctl/config.h
@@ -162,6 +162,7 @@ typedef struct {
       uint benchg_tile_count;
       char affinity[ AFFINITY_SZ ];
       int  larger_max_cost_per_block;
+      int  larger_shred_limits_per_block;
     } bench;
   } development;
 
