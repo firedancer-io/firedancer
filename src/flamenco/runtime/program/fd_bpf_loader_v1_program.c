5afc301eec28e2e6369e7e9252e8b11d1b5b59da
Richard Patel <ripatel@jumptrading.com>
2024-04-05 17:25:11 +0000

    runtime: polish BPF Loader v2

--- src/flamenco/runtime/program/fd_bpf_loader_v1_program.c
+++ src/flamenco/runtime/program/fd_bpf_loader_v1_program.c
@@ -1,17 +1,12 @@
 #include "fd_bpf_loader_v1_program.h"
+#include "../context/fd_exec_txn_ctx.h"
 
 int
 fd_bpf_loader_v1_program_execute( fd_exec_instr_ctx_t ctx ) {
-  uchar const * data = ctx.instr->data;
+  do {
+    int err = fd_exec_consume_cus( ctx.txn_ctx, 1140UL );
+    if( FD_UNLIKELY( err ) ) return err;
+  } while(0);
 
-  fd_bpf_loader_program_instruction_t instruction;
-  fd_bincode_decode_ctx_t decode =
-    { .data    = data,
-      .dataend = data + ctx.instr->data_sz,
-      .valloc  = ctx.valloc };
-
-  if( fd_bpf_loader_program_instruction_decode( &instruction, &decode ) )
-    return FD_EXECUTOR_INSTR_ERR_INVALID_ACC_DATA;
-
-  return FD_EXECUTOR_INSTR_ERR_GENERIC_ERR;
+  return FD_EXECUTOR_INSTR_ERR_UNSUPPORTED_PROGRAM_ID;
 }
