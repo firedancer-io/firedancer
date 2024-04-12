389ebf47ab15b16aea6ca989e605a9a982589d39
Richard Patel <ripatel@jumptrading.com>
2024-04-12 16:24:24 +0000

    remove unused code

--- src/flamenco/types/fd_types_custom.c
+++ src/flamenco/types/fd_types_custom.c
@@ -54,56 +54,6 @@ fd_flamenco_txn_decode_unsafe( fd_flamenco_txn_t *       self,
   ctx->data = (void *)( (ulong)ctx->data + sz );
 }
 
-void fd_option_slot_new(fd_option_slot_t* self) {
-  fd_memset(self, 0, sizeof(fd_option_slot_t));
-}
-int fd_option_slot_decode(fd_option_slot_t* self, fd_bincode_decode_ctx_t * ctx) {
-  int err;
-  err = fd_bincode_bool_decode(&self->is_some, ctx);
-  if ( FD_UNLIKELY(err) ) return err;
-  if ( !self->is_some ) return FD_BINCODE_SUCCESS;
-  err = fd_bincode_uint64_decode(&self->slot, ctx);
-  if( FD_UNLIKELY( err!=FD_BINCODE_SUCCESS ) ) return err;
-  return FD_BINCODE_SUCCESS;
-}
-int fd_option_slot_decode_preflight(fd_bincode_decode_ctx_t * ctx) {
-  int err;
-  uchar is_some;
-  err = fd_bincode_bool_decode(&is_some, ctx);
-  if ( FD_UNLIKELY(err) ) return err;
-  if ( !is_some ) return FD_BINCODE_SUCCESS;
-  err = fd_bincode_uint64_decode_preflight(ctx);
-  if( FD_UNLIKELY( err!=FD_BINCODE_SUCCESS ) ) return err;
-  return FD_BINCODE_SUCCESS;
-}
-void fd_option_slot_decode_unsafe(fd_option_slot_t* self, fd_bincode_decode_ctx_t * ctx) {
-  fd_bincode_bool_decode_unsafe(&self->is_some, ctx);
-  if ( !self->is_some ) return;
-  fd_bincode_uint64_decode_unsafe(&self->slot, ctx);
-}
-int fd_option_slot_encode(fd_option_slot_t const * self, fd_bincode_encode_ctx_t * ctx) {
-  int err;
-  err = fd_bincode_bool_encode(self->is_some, ctx);
-  if ( FD_UNLIKELY(err) ) return err;
-  if ( !self->is_some ) return FD_BINCODE_SUCCESS;
-  err = fd_bincode_uint64_encode(self->slot, ctx);
-  if ( FD_UNLIKELY(err) ) return err;
-  return FD_BINCODE_SUCCESS;
-}
-void fd_option_slot_destroy(fd_option_slot_t* self, fd_bincode_destroy_ctx_t * ctx) {
-}
-ulong fd_option_slot_footprint( void ){ return FD_OPTION_SLOT_FOOTPRINT; }
-ulong fd_option_slot_align( void ){ return FD_OPTION_SLOT_ALIGN; }
-void fd_option_slot_walk(void * w, fd_option_slot_t const * self, fd_types_walk_fn_t fun, const char *name, uint level) {
-  fun( w, &self->slot, name, FD_FLAMENCO_TYPE_ULONG,   "ulong",     level );
-}
-ulong fd_option_slot_size(fd_option_slot_t const * self) {
-  ulong size = 0;
-  size += sizeof(char);
-  if (self->is_some) size += sizeof(ulong);
-  return size;
-}
-
 void
 fd_gossip_ip4_addr_walk( void *                       w,
                          fd_gossip_ip4_addr_t const * self,
