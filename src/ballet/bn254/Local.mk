7c113e0f0380af11ad08d0552ebd56171a5a4194
Richard Patel <ripatel@jumptrading.com>
2024-04-13 21:59:31 +0000

    Don't compile bn254 in FFI

--- src/ballet/bn254/Local.mk
+++ src/ballet/bn254/Local.mk
@@ -1,10 +1,12 @@
 ifneq ($(FD_HAS_LIBFF),)
+ifndef FD_HAS_FFI
 
 $(call add-hdrs,fd_bn254.h fd_poseidon.h)
 $(call add-objs,fd_bn254 fd_poseidon_params fd_poseidon,fd_ballet)
 $(call make-unit-test,test_bn254,test_bn254,fd_ballet fd_util)
 $(call make-unit-test,test_poseidon,test_poseidon,fd_ballet fd_util)
 
+endif
 else
 
 $(info bn254 disabled due to lack of libff)
