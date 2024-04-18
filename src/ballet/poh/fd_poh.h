d8dbaac144240c90d4de34781c5b141ad34d5676
Firedancer Team <firedancer-devs@jumptrading.com>
2024-04-18 16:18:53 +0000

    simplify use of the poh apis

--- src/ballet/poh/fd_poh.h
+++ src/ballet/poh/fd_poh.h
@@ -5,27 +5,29 @@
 
 #include "../sha256/fd_sha256.h"
 
-#define FD_POH_STATE_ALIGN (32UL)
-
-struct __attribute__((aligned(32))) fd_poh_state {
-  uchar state[FD_SHA256_HASH_SZ];
-};
-
-typedef struct fd_poh_state fd_poh_state_t;
-
 FD_PROTOTYPES_BEGIN
 
-/* fd_poh_append performs n recursive hash operations. */
+/* fd_poh_append performs n recursive hash operations.
 
-fd_poh_state_t *
-fd_poh_append( fd_poh_state_t * poh,
-               ulong            n );
+   poh points to a 32 byte memory region which stores the current poh state
 
-/* fd_poh_mixin mixes in a 32-byte value. */
+   TODO: Improve documentation on documentation day
+*/
 
-fd_poh_state_t *
-fd_poh_mixin( fd_poh_state_t * FD_RESTRICT poh,
-              uchar const *    FD_RESTRICT mixin );
+void *
+fd_poh_append( void * poh,
+               ulong  n );
+
+/* fd_poh_mixin mixes in a 32-byte value.
+
+   poh points to a 32 byte memory region which stores the current poh state
+
+   TODO: Improve documentation on documentation day
+*/
+
+void *
+fd_poh_mixin( void *        FD_RESTRICT poh,
+              uchar const * FD_RESTRICT mixin );
 
 FD_PROTOTYPES_END
 
--- src/ballet/poh/test_poh.c
+++ src/ballet/poh/test_poh.c
@@ -3,18 +3,18 @@
 /* Ensure that calling fd_poh_append with zero iterations is nop. */
 static void
 test_poh_append_nop( void ) {
-  fd_poh_state_t poh = {0};
+  uchar poh[FD_SHA256_HASH_SZ] = {0};
 
   /* Make up a pattern */
   uchar want[FD_SHA256_HASH_SZ];
   for( ulong i=0UL; i<FD_SHA256_HASH_SZ; i++ ) want[i] = (uchar)(0x40UL+i);
-  fd_memcpy( poh.state, want, FD_SHA256_HASH_SZ );
+  fd_memcpy( &poh, want, FD_SHA256_HASH_SZ );
 
   /* Call fd_poh_append with zero iters */
   fd_poh_append( &poh, 0UL );
 
   /* PoH state should be untouched */
-  FD_TEST( !memcmp( poh.state, want, FD_SHA256_HASH_SZ ) );
+  FD_TEST( !memcmp( &poh, want, FD_SHA256_HASH_SZ ) );
 }
 
 /* Ensure that one round of fd_poh_append matches the simple hashing API. */
@@ -25,8 +25,8 @@ test_poh_append_one( void ) {
   for( ulong i=0UL; i<FD_SHA256_HASH_SZ; i++ ) pre[i] = (uchar)(0x40UL+i);
 
   /* One round of PoH append */
-  fd_poh_state_t poh = {0};
-  fd_memcpy( poh.state, pre, FD_SHA256_HASH_SZ );
+  uchar poh[FD_SHA256_HASH_SZ] = {0};
+  fd_memcpy( &poh, pre, FD_SHA256_HASH_SZ );
   fd_poh_append( &poh, 1UL );
 
   /* SHA-256 simple API */
@@ -36,14 +36,14 @@ test_poh_append_one( void ) {
   uchar expected[FD_SHA256_HASH_SZ];
   fd_sha256_fini( &sha, &expected );
 
-  if( FD_UNLIKELY( memcmp( poh.state, expected, FD_SHA256_HASH_SZ ) ) ) {
+  if( FD_UNLIKELY( memcmp( &poh, expected, FD_SHA256_HASH_SZ ) ) ) {
     FD_LOG_ERR(( "FAIL (test_poh_append_one)"
                  "\n\tGot"
                  "\n\t\t" FD_LOG_HEX16_FMT "  " FD_LOG_HEX16_FMT
                  "\n\tExpected"
                  "\n\t\t" FD_LOG_HEX16_FMT "  " FD_LOG_HEX16_FMT,
-                 FD_LOG_HEX16_FMT_ARGS( poh.state ), FD_LOG_HEX16_FMT_ARGS( poh.state+16 ),
-                 FD_LOG_HEX16_FMT_ARGS(  expected ), FD_LOG_HEX16_FMT_ARGS(  expected+16 ) ));
+                 FD_LOG_HEX16_FMT_ARGS( poh      ), FD_LOG_HEX16_FMT_ARGS( poh     +16 ),
+                 FD_LOG_HEX16_FMT_ARGS( expected ), FD_LOG_HEX16_FMT_ARGS( expected+16 ) ));
   }
 }
 
@@ -58,8 +58,8 @@ test_poh_mixin( void ) {
   for( ulong i=0UL; i<FD_SHA256_HASH_SZ; i++ ) mixin[i]=(uchar)(0x60UL+i);
 
   /* Execute a PoH mixin */
-  fd_poh_state_t poh = {0};
-  fd_memcpy( poh.state, pre, FD_SHA256_HASH_SZ );
+  uchar poh[FD_SHA256_HASH_SZ] = {0};
+  fd_memcpy( &poh, pre, FD_SHA256_HASH_SZ );
   fd_poh_mixin( &poh, mixin );
 
   /* SHA-256 simple API */
@@ -70,14 +70,14 @@ test_poh_mixin( void ) {
   uchar expected[FD_SHA256_HASH_SZ];
   fd_sha256_fini( &sha, &expected );
 
-  if( FD_UNLIKELY( memcmp( poh.state, expected, FD_SHA256_HASH_SZ ) ) ) {
+  if( FD_UNLIKELY( memcmp( &poh, expected, FD_SHA256_HASH_SZ ) ) ) {
     FD_LOG_ERR(( "FAIL (test_poh_mixin)"
                  "\n\tGot"
                  "\n\t\t" FD_LOG_HEX16_FMT "  " FD_LOG_HEX16_FMT
                  "\n\tExpected"
                  "\n\t\t" FD_LOG_HEX16_FMT "  " FD_LOG_HEX16_FMT,
-                 FD_LOG_HEX16_FMT_ARGS( poh.state ), FD_LOG_HEX16_FMT_ARGS( poh.state+16 ),
-                 FD_LOG_HEX16_FMT_ARGS(  expected ), FD_LOG_HEX16_FMT_ARGS(  expected+16 ) ));
+                 FD_LOG_HEX16_FMT_ARGS( poh      ), FD_LOG_HEX16_FMT_ARGS( poh     +16 ),
+                 FD_LOG_HEX16_FMT_ARGS( expected ), FD_LOG_HEX16_FMT_ARGS( expected+16 ) ));
   }
 }
 
@@ -94,8 +94,8 @@ struct fd_poh_test_step {
 typedef struct fd_poh_test_step fd_poh_test_step_t;
 
 struct fd_poh_test_vector {
-  fd_poh_state_t pre   __attribute__((aligned(32)));
-  fd_poh_state_t post  __attribute__((aligned(32)));
+  uchar pre [32]  __attribute__((aligned(32)));
+  uchar post[32]  __attribute__((aligned(32)));
   char const * name;
   fd_poh_test_step_t const * steps;
 };
@@ -104,7 +104,8 @@ typedef struct fd_poh_test_vector fd_poh_test_vector_t;
 
 static void
 test_poh_vector( fd_poh_test_vector_t const * t ) {
-  fd_poh_state_t poh = t->pre;
+  uchar poh[32];
+  memcpy( poh, t->pre, 32UL );
   for( fd_poh_test_step_t const * step = t->steps; step->n >= 0; step++ ) {
     if( FD_UNLIKELY( step->n == 0 ) ) {
       fd_poh_mixin( &poh, step->mixin );
@@ -113,15 +114,15 @@ test_poh_vector( fd_poh_test_vector_t const * t ) {
     }
   }
 
-  if( FD_UNLIKELY( memcmp( poh.state, t->post.state, FD_SHA256_HASH_SZ ) ) ) {
+  if( FD_UNLIKELY( memcmp( &poh, &t->post, FD_SHA256_HASH_SZ ) ) ) {
     FD_LOG_ERR(( "FAIL (%s)"
                  "\n\tGot"
                  "\n\t\t" FD_LOG_HEX16_FMT "  " FD_LOG_HEX16_FMT
                  "\n\tExpected"
                  "\n\t\t" FD_LOG_HEX16_FMT "  " FD_LOG_HEX16_FMT,
                  t->name,
-                 FD_LOG_HEX16_FMT_ARGS(     poh.state ), FD_LOG_HEX16_FMT_ARGS(     poh.state+16 ),
-                 FD_LOG_HEX16_FMT_ARGS( t->post.state ), FD_LOG_HEX16_FMT_ARGS( t->post.state+16 ) ));
+                 FD_LOG_HEX16_FMT_ARGS( poh     ), FD_LOG_HEX16_FMT_ARGS( poh    +16 ),
+                 FD_LOG_HEX16_FMT_ARGS( t->post ), FD_LOG_HEX16_FMT_ARGS( t->post+16 ) ));
   } else {
     FD_LOG_NOTICE(( "OK (%s)", t->name ));
   }
@@ -150,14 +151,14 @@ static fd_poh_test_step_t const solana_mainnet_block_1_steps[] = {
 static fd_poh_test_vector_t const poh_test_vectors[] = {
   {
     .name  = "Solana mainnet block 0",
-    .pre   = {{ _(45),_(29),_(69),_(98),_(a6),_(f8),_(e2),_(a7),_(84),_(db),_(5d),_(9f),_(95),_(e1),_(8f),_(c2),_(3f),_(70),_(44),_(1a),_(10),_(39),_(44),_(68),_(01),_(08),_(98),_(79),_(b0),_(8c),_(7e),_(f0) }},
-    .post  = {{ _(39),_(73),_(e3),_(30),_(c2),_(9b),_(83),_(1f),_(3f),_(cb),_(0e),_(49),_(37),_(4e),_(d8),_(d0),_(38),_(8f),_(41),_(0a),_(23),_(e4),_(eb),_(f2),_(33),_(28),_(50),_(50),_(36),_(ef),_(bd),_(03) }},
+    .pre   = { _(45),_(29),_(69),_(98),_(a6),_(f8),_(e2),_(a7),_(84),_(db),_(5d),_(9f),_(95),_(e1),_(8f),_(c2),_(3f),_(70),_(44),_(1a),_(10),_(39),_(44),_(68),_(01),_(08),_(98),_(79),_(b0),_(8c),_(7e),_(f0) },
+    .post  = { _(39),_(73),_(e3),_(30),_(c2),_(9b),_(83),_(1f),_(3f),_(cb),_(0e),_(49),_(37),_(4e),_(d8),_(d0),_(38),_(8f),_(41),_(0a),_(23),_(e4),_(eb),_(f2),_(33),_(28),_(50),_(50),_(36),_(ef),_(bd),_(03) },
     .steps = solana_mainnet_block_0_steps
   },
   {
     .name  = "Solana mainnet block 1",
-    .pre   = {{ _(39),_(73),_(e3),_(30),_(c2),_(9b),_(83),_(1f),_(3f),_(cb),_(0e),_(49),_(37),_(4e),_(d8),_(d0),_(38),_(8f),_(41),_(0a),_(23),_(e4),_(eb),_(f2),_(33),_(28),_(50),_(50),_(36),_(ef),_(bd),_(03) }},
-    .post  = {{ _(8e),_(e2),_(06),_(07),_(dc),_(f1),_(d9),_(39),_(3c),_(f5),_(a2),_(f2),_(c9),_(f7),_(ba),_(be),_(16),_(7d),_(bd),_(d2),_(67),_(49),_(1b),_(51),_(3c),_(73),_(d2),_(cb),_(f8),_(74),_(13),_(f5) }},
+    .pre   = { _(39),_(73),_(e3),_(30),_(c2),_(9b),_(83),_(1f),_(3f),_(cb),_(0e),_(49),_(37),_(4e),_(d8),_(d0),_(38),_(8f),_(41),_(0a),_(23),_(e4),_(eb),_(f2),_(33),_(28),_(50),_(50),_(36),_(ef),_(bd),_(03) },
+    .post  = { _(8e),_(e2),_(06),_(07),_(dc),_(f1),_(d9),_(39),_(3c),_(f5),_(a2),_(f2),_(c9),_(f7),_(ba),_(be),_(16),_(7d),_(bd),_(d2),_(67),_(49),_(1b),_(51),_(3c),_(73),_(d2),_(cb),_(f8),_(74),_(13),_(f5) },
     .steps = solana_mainnet_block_1_steps
   },
   { .name = NULL }
@@ -167,8 +168,7 @@ static fd_poh_test_vector_t const poh_test_vectors[] = {
 
 static void
 bench_poh_sequential( void ) {
-  fd_poh_state_t poh;
-  fd_memset( poh.state, 0, FD_SHA256_HASH_SZ );
+  uchar poh[FD_SHA256_HASH_SZ] = {0};
 
   ulong batch_sz = 1024;
 
@@ -179,7 +179,7 @@ bench_poh_sequential( void ) {
   dt = fd_log_wallclock() - dt;
 
   /* for real */
-  iter = 10000UL;
+  iter = 100000UL;
   dt = fd_log_wallclock();
   for( ulong rem=iter; rem; rem-- ) fd_poh_append( &poh, batch_sz );
   dt = fd_log_wallclock() - dt;
