1246d8173cae1486816fd248b173b0c5ca2e3645
Firedancer Team <firedancer-devs@jumptrading.com>
2024-04-18 16:12:13 +0000

    batched bmtree processing

--- src/ballet/bmtree/Local.mk
+++ src/ballet/bmtree/Local.mk
@@ -1,5 +1,6 @@
-$(call add-hdrs,fd_bmtree.h)
-$(call add-objs,fd_bmtree,fd_ballet)
+$(call add-hdrs,fd_bmtree.h fd_wbmtree.h)
+$(call add-objs,fd_bmtree fd_wbmtree,fd_ballet)
+
 $(call make-unit-test,test_bmtree,test_bmtree,fd_ballet fd_util)
 $(call run-unit-test,test_bmtree)
 $(call make-fuzz-test,fuzz_bmtree,fuzz_bmtree,fd_ballet fd_util)
