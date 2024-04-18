3af7f5d3703729444a6f142f3ad850e24819df83
Firedancer Team <firedancer-devs@jumptrading.com>
2024-04-18 16:18:53 +0000

    simplify use of the poh apis

--- src/ballet/poh/fd_poh.c
+++ src/ballet/poh/fd_poh.c
@@ -1,24 +1,21 @@
 #include "fd_poh.h"
 
-fd_poh_state_t *
-fd_poh_append( fd_poh_state_t * poh,
-               ulong            n ) {
-  fd_sha256_t sha;
+void *
+fd_poh_append( void * poh,
+               ulong  n ) {
   while( n-- ) {
-    fd_sha256_init( &sha );
-    fd_sha256_append( &sha, poh->state, FD_SHA256_HASH_SZ );
-    fd_sha256_fini( &sha, poh->state );
+    fd_sha256_hash_32( poh, poh );
   }
   return poh;
 }
 
-fd_poh_state_t *
-fd_poh_mixin( fd_poh_state_t * FD_RESTRICT poh,
-              uchar const *    FD_RESTRICT mixin ) {
+void *
+fd_poh_mixin( void *        FD_RESTRICT poh,
+              uchar const * FD_RESTRICT mixin ) {
   fd_sha256_t sha;
   fd_sha256_init( &sha );
-  fd_sha256_append( &sha, poh->state, FD_SHA256_HASH_SZ );
-  fd_sha256_append( &sha, mixin,      FD_SHA256_HASH_SZ );
-  fd_sha256_fini( &sha, poh->state );
+  fd_sha256_append( &sha, poh,   FD_SHA256_HASH_SZ );
+  fd_sha256_append( &sha, mixin, FD_SHA256_HASH_SZ );
+  fd_sha256_fini( &sha, poh );
   return poh;
 }
