688c7b97770b263fcad32f0d9dc3dcb55762db64
Firedancer Team <firedancer-devs@jumptrading.com>
2024-04-19 11:31:29 +0000

    firedancer ledger tool

---
+++ src/app/ledger/Local.mk
@@ -0,0 +1,11 @@
+ifdef FD_HAS_ROCKSDB
+
+ifdef FD_HAS_ZSTD
+$(call make-bin,fd_frank_ledger,main,fd_flamenco fd_ballet fd_funk fd_util)
+else
+$(warning ledger tool build disabled due to lack of zstd)
+endif
+
+else
+$(warning ledger tool build disabled due to lack of rocksdb)
+endif
