a1ec92127e5d27eced9891c377cf1b8374508647
Richard Patel <ripatel@jumptrading.com>
2024-04-22 22:56:32 +0000

    vm: ignore broken unit tests

--- src/flamenco/vm/Local.mk
+++ src/flamenco/vm/Local.mk
@@ -6,10 +6,6 @@ ifdef FD_HAS_HOSTED
 $(call make-bin,fd_vm_tool,fd_vm_tool,fd_flamenco fd_funk fd_ballet fd_util)
 endif
 
-$(call make-unit-test,test_vm_interp,test_vm_interp,fd_flamenco fd_funk fd_ballet fd_util)
-# $(call run-unit-test,test_vm_interp)
-$(call make-unit-test,test_vm_syscalls,test_vm_syscalls,fd_flamenco fd_funk fd_ballet fd_util)
-# $(call run-unit-test,test_vm_syscalls)
 $(call make-unit-test,test_vm_cpi,test_vm_cpi,fd_util)
 $(call run-unit-test,test_vm_cpi)
 endif
