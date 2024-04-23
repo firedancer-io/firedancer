dd7b0c4bf6d5a41ea44a82febc58a1d2932d2856
Firedancer Team <firedancer-devs@jumptrading.com>
2024-04-23 05:10:45 +0000

    Add stake program public API

--- src/flamenco/runtime/program/fd_stake_program.h
+++ src/flamenco/runtime/program/fd_stake_program.h
@@ -22,6 +22,12 @@ FD_PROTOTYPES_BEGIN
 int
 fd_stake_program_execute( fd_exec_instr_ctx_t ctx );
 
+/* Initializes an account which holds configuration used by the stake program.
+   https://github.com/solana-labs/solana/blob/8f2c8b8388a495d2728909e30460aa40dcc5d733/sdk/program/src/stake/config.rs
+ */
+void
+fd_stake_program_config_init( fd_exec_slot_ctx_t * global );
+
 int
 fd_stake_get_state( fd_borrowed_account_t const * self,
                     fd_valloc_t const *           valloc,
@@ -32,6 +38,10 @@ fd_stake_activating_and_deactivating( fd_delegation_t const *    self,
                                       ulong                      target_epoch,
                                       fd_stake_history_t const * stake_history,
                                       ulong *                    new_rate_activation_epoch );
+
+void
+fd_store_stake_delegation( fd_exec_slot_ctx_t *    slot_ctx,
+                           fd_borrowed_account_t * stake_account );
 
 FD_PROTOTYPES_END
 
