70ad287a08af414fcd7e463e5965540033e4cbb1
Richard Patel <ripatel@jumptrading.com>
2024-04-23 01:31:32 +0000

    clean up compute budget program

--- src/flamenco/runtime/fd_executor.c
+++ src/flamenco/runtime/fd_executor.c
@@ -62,8 +62,6 @@ fd_executor_lookup_native_program( fd_pubkey_t const * pubkey ) {
     return fd_bpf_loader_v2_program_execute;
   } else if ( !memcmp( pubkey, fd_solana_bpf_loader_deprecated_program_id.key, sizeof( fd_pubkey_t ) ) ) {
     return fd_bpf_loader_v1_program_execute;
-  } else if ( !memcmp( pubkey, fd_solana_compute_budget_program_id.key, sizeof( fd_pubkey_t ) ) ) {
-    return fd_executor_compute_budget_program_execute_instruction_nop;
   } else if( !memcmp( pubkey, fd_solana_address_lookup_table_program_id.key, sizeof(fd_pubkey_t) ) ) {
     return fd_address_lookup_table_program_execute;
   //} else if( !memcmp( pubkey, fd_solana_zk_token_proof_program_id.key, sizeof(fd_pubkey_t) ) ) {
