99564c698ade104f77cd00f947acbe6a8abb09af
Firedancer Team <firedancer-devs@jumptrading.com>
2024-04-23 04:51:47 +0000

    runtime: add block info

---
+++ src/flamenco/runtime/info/fd_block_info.h
@@ -0,0 +1,24 @@
+#ifndef HEADER_fd_src_flamenco_runtime_info_fd_block_info_h
+#define HEADER_fd_src_flamenco_runtime_info_fd_block_info_h
+
+#include "../../../util/fd_util_base.h"
+#include "../../../ballet/block/fd_microblock.h"
+#include "../../../ballet/txn/fd_txn.h"
+
+#include "fd_microblock_batch_info.h"
+
+struct fd_block_info {
+  ulong microblock_batch_cnt;
+  ulong microblock_cnt;
+  ulong signature_cnt;
+  ulong txn_cnt;
+  ulong account_cnt;
+
+  fd_microblock_batch_info_t * microblock_batch_infos;
+
+  void const *  raw_block;
+  ulong         raw_block_sz;
+};
+typedef struct fd_block_info fd_block_info_t;
+
+#endif /* HEADER_fd_src_flamenco_runtime_info_fd_block_info_h */
