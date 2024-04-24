0afbb3501b6cac7c0c199a56422e05a9103088c0
Tom Pointon <topointon@jumptrading.com>
2024-04-24 15:37:45 +0000

    Belt sanding pass fd_vm.h / fd_vm.c
    - Started turning fd_vm_exec_context_t into fd_vm_t with an appropriate
      object lifecycle and plumbed through the changes except where Tom
      is working (scaffolding so doesn't impact Tom's code).
    
    - Accordingly moved fd_vm_context.c to fd_vm.c and moved some other
      "vm scope" functions (like fd_vm_strerror vm_compute_budget) there and
      updated build accordingly.
    
    - Proper integer error code returns for fd_vm_context_validate (now
      fd_vm_validate) and fd_vm_interp_instrs* and plumbing through.
    
    - Did a belt sanding pass on fd_vm_validation implementation as per
      Richie's request.
    
    - Moved the heap allocator API into fd_vm_base.h.
    
    - Moved the compute budget API into fd_vm_base.h.  Fixed the translation
      unit scoping issue around vm_compute_budget here (probably still want
      to covert to macros though).
    
    - Moved SBPF_VALIDATE error codes into fd_vm_base.h and included
      coverage in fd_vm_strerror / test_vm_base.  Also included coverage for
      the syscall related error codes.
    
    - Plumbed through proper prefixing on
      MAX_LOADED_ACCOUNTS_DATA_SIZE_BYTES and MAX_COMPUTE_UNIT_LIMIT (but
      these still probably need more cleanup).
    
    - As per usual, lots of comment cleanups and fixme annotations for
      future cleanup.

--- src/flamenco/vm/Local.mk
+++ src/flamenco/vm/Local.mk
@@ -2,7 +2,7 @@ ifdef FD_HAS_INT128
 ifdef FD_HAS_HOSTED
 
 $(call add-hdrs,fd_vm_base.h fd_vm_cpi.h fd_vm.h)
-$(call add-objs,fd_vm_context fd_vm_interp fd_vm_disasm fd_vm_trace,fd_flamenco)
+$(call add-objs,fd_vm fd_vm_interp fd_vm_disasm fd_vm_trace,fd_flamenco)
 
 $(call make-bin,fd_vm_tool,fd_vm_tool,fd_flamenco fd_funk fd_ballet fd_util,$(SECP256K1_LIBS))
 
