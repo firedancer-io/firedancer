f1cf72afa24baf6708743bccfbabc5311256439f
Tom Pointon <topointon@jumptrading.com>
2024-04-24 15:55:37 +0000

    Add missing imports

--- src/flamenco/vm/syscall/test_vm_syscall_curve.c
+++ src/flamenco/vm/syscall/test_vm_syscall_curve.c
@@ -1,4 +1,5 @@
 #include "fd_vm_syscall.h"
+#include "../../runtime/context/fd_exec_instr_ctx.h"
 
 static inline void set_memory_region( uchar * mem, ulong sz ) { for( ulong i=0UL; i<sz; i++ ) mem[i] = (uchar)(i & 0xffUL); }
 
