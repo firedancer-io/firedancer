ce8fa4ccf53b031eea12638741725c6442707187
Emanuele Cesena <ecesena@jumptrading.com>
2024-04-30 18:08:07 +0000

    ballet, bn254: impl based on fiat-crypto (libff no longer needed)

--- src/ballet/bn254/test_poseidon.c
+++ src/ballet/bn254/test_poseidon.c
@@ -1,6 +1,25 @@
 #include "fd_poseidon.h"
 #include "../../util/fd_util.h"
 
+/* Swap bytes in a 32-byte big int */
+static inline void
+byte_swap_32(uchar * v) {
+  for( ulong i=0; i<FD_POSEIDON_HASH_SZ/2; i++ ) {
+    uchar t = v[i];
+    v[i] = v[FD_POSEIDON_HASH_SZ-1U-i];
+    v[FD_POSEIDON_HASH_SZ-1U-i] = t;
+  }
+}
+
+void
+log_bench( char const * descr,
+           ulong        iter,
+           long         dt ) {
+  float khz = 1e6f *(float)iter/(float)dt;
+  float tau = (float)dt /(float)iter;
+  FD_LOG_NOTICE(( "%-31s %11.3fK/s/core %10.3f ns/call", descr, (double)khz, (double)tau ));
+}
+
 int main( int     argc,
           char ** argv ) {
   fd_boot( &argc, &argv );
@@ -8,126 +27,176 @@ int main( int     argc,
   uchar bytes[13*32];
 
   {
+    static const uchar _a[] = { 0, 122, 243, 70, 226, 211, 4, 39, 158, 121, 224, 169, 243, 2, 63, 119, 18, 148, 167, 138, 203, 112, 231, 63, 144, 175, 226, 124, 173, 64, 30, 129 };
+    static const uchar _b[] = { 230, 117, 27, 127, 210, 224, 145, 185, 157, 99, 172, 7, 132, 30, 241, 130, 136, 166, 99, 99, 197, 198, 25, 204, 119, 97, 238, 129, 229, 172, 191, 5 };
+
+    fd_bn254_scalar_t _r[1];
+    fd_bn254_scalar_t const * a = fd_type_pun_const( _a );
+    fd_bn254_scalar_t const * b = fd_type_pun_const( _b );
+    fd_bn254_scalar_t * r = _r;
+
+    ulong iter = 1000000UL;
+    long dt = fd_log_wallclock();
+    for( ulong rem=iter; rem; rem-- ) {
+      FD_COMPILER_FORGET( r ); FD_COMPILER_FORGET( a ); FD_COMPILER_FORGET( b );
+      fd_bn254_scalar_mul( r, a, b );
+    }
+    dt = fd_log_wallclock() - dt;
+    log_bench( "fd_bn254_scalar_mul", iter, dt );
+  }
+
+  {
     fd_memset(bytes, 1, 32);
     fd_poseidon_hash_result_t res;
-    fd_poseidon_hash(bytes, 32, 0, &res);
+    fd_poseidon_hash(&res, bytes, 32, 0);
     static fd_poseidon_hash_result_t gold = { .v= {
         230, 117, 27, 127, 210, 224, 145, 185, 157, 99, 172, 7, 132, 30, 241, 130, 136,
         166, 99, 99, 197, 198, 25, 204, 119, 97, 238, 129, 229, 172, 191, 5
       } };
-    FD_TEST(memcmp(res.v, gold.v, FD_POSEIDON_HASH_SIZE) == 0);
+    FD_TEST(memcmp(res.v, gold.v, FD_POSEIDON_HASH_SZ) == 0);
+
+    fd_poseidon_t pos[1];
+    fd_poseidon_fini( fd_poseidon_append( fd_poseidon_init( pos, 0 ), bytes, 32 ), res.v );
+    FD_TEST(memcmp(res.v, gold.v, FD_POSEIDON_HASH_SZ) == 0);
   }
-  
+
   {
     fd_memset(bytes, 1, 32);
     fd_poseidon_hash_result_t res;
-    fd_poseidon_hash(bytes, 32, 1, &res);
+    fd_poseidon_hash(&res, bytes, 32, 1);
     static fd_poseidon_hash_result_t gold = { .v= {
         5, 191, 172, 229, 129, 238, 97, 119, 204, 25, 198, 197, 99, 99, 166, 136, 130, 241,
         30, 132, 7, 172, 99, 157, 185, 145, 224, 210, 127, 27, 117, 230
       } };
-    FD_TEST(memcmp(res.v, gold.v, FD_POSEIDON_HASH_SIZE) == 0);
+    FD_TEST(memcmp(res.v, gold.v, FD_POSEIDON_HASH_SZ) == 0);
+
+    fd_poseidon_t pos[1];
+    fd_poseidon_fini( fd_poseidon_append( fd_poseidon_init( pos, 1 ), bytes, 32 ), res.v );
+    FD_TEST(memcmp(res.v, gold.v, FD_POSEIDON_HASH_SZ) == 0);
   }
-    
+
   {
     fd_memset(bytes, 1, 32);
     fd_memset(bytes+32, 2, 32);
     fd_poseidon_hash_result_t res;
-    fd_poseidon_hash(bytes, 64, 1, &res);
+    fd_poseidon_hash(&res, bytes, 64, 1);
     static fd_poseidon_hash_result_t gold = { .v= {
         13, 84, 225, 147, 143, 138, 140, 28, 125, 235, 94, 3, 85, 242, 99, 25, 32, 123,
         132, 254, 156, 162, 206, 27, 38, 231, 53, 200, 41, 130, 25, 144
       } };
-    FD_TEST(memcmp(res.v, gold.v, FD_POSEIDON_HASH_SIZE) == 0);
+    FD_TEST(memcmp(res.v, gold.v, FD_POSEIDON_HASH_SZ) == 0);
   }
-  
 
   {
     static const uchar input[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 };
     static const uchar output[] = { 0, 122, 243, 70, 226, 211, 4, 39, 158, 121, 224, 169, 243, 2, 63, 119, 18, 148, 167, 138, 203, 112, 231, 63, 144, 175, 226, 124, 173, 64, 30, 129 };
     fd_poseidon_hash_result_t res;
-    fd_poseidon_hash(input, sizeof(input), 1, &res);
-    FD_TEST(memcmp(res.v, output, FD_POSEIDON_HASH_SIZE) == 0);
+    fd_poseidon_hash(&res, input, sizeof(input), 1);
+    FD_TEST(memcmp(res.v, output, FD_POSEIDON_HASH_SZ) == 0);
+
+    fd_poseidon_t pos[1];
+    fd_poseidon_init( pos, 1 );
+    fd_poseidon_append( pos, input, 32 );
+    fd_poseidon_append( pos, input+32, 32 );
+    fd_poseidon_fini( pos, res.v );
+    FD_TEST(memcmp(res.v, output, FD_POSEIDON_HASH_SZ) == 0);
+
+    static const uchar input1[] = { 1 };
+    fd_poseidon_init( pos, 1 );
+    fd_poseidon_append( pos, input1, 1 );
+    fd_poseidon_append( pos, input1, 1 );
+    fd_poseidon_fini( pos, res.v );
+    FD_TEST(memcmp(res.v, output, FD_POSEIDON_HASH_SZ) == 0);
+
+    static uchar output_le[] = { 0, 122, 243, 70, 226, 211, 4, 39, 158, 121, 224, 169, 243, 2, 63, 119, 18, 148, 167, 138, 203, 112, 231, 63, 144, 175, 226, 124, 173, 64, 30, 129 };
+    byte_swap_32(output_le);
+
+    fd_poseidon_init( pos, 0 );
+    fd_poseidon_append( pos, input1, 1 );
+    fd_poseidon_append( pos, input1, 1 );
+    fd_poseidon_fini( pos, res.v );
+    FD_TEST(memcmp(res.v, output_le, FD_POSEIDON_HASH_SZ) == 0);
+
   }
 
   {
     static const uchar input[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 };
     static const uchar output[] = { 2, 192, 6, 110, 16, 167, 42, 189, 43, 51, 195, 178, 20, 203, 62, 129, 188, 177, 182, 227, 9, 97, 205, 35, 194, 2, 177, 134, 115, 191, 37, 67 };
     fd_poseidon_hash_result_t res;
-    fd_poseidon_hash(input, sizeof(input), 1, &res);
-    FD_TEST(memcmp(res.v, output, FD_POSEIDON_HASH_SIZE) == 0);
+    fd_poseidon_hash(&res, input, sizeof(input), 1);
+    FD_TEST(memcmp(res.v, output, FD_POSEIDON_HASH_SZ) == 0);
   }
 
   {
     static const uchar input[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 };
     static const uchar output[] = { 8, 44, 156, 55, 10, 13, 36, 244, 65, 111, 188, 65, 74, 55, 104, 31, 120, 68, 45, 39, 216, 99, 133, 153, 28, 23, 214, 252, 12, 75, 125, 113 };
     fd_poseidon_hash_result_t res;
-    fd_poseidon_hash(input, sizeof(input), 1, &res);
-    FD_TEST(memcmp(res.v, output, FD_POSEIDON_HASH_SIZE) == 0);
+    fd_poseidon_hash(&res, input, sizeof(input), 1);
+    FD_TEST(memcmp(res.v, output, FD_POSEIDON_HASH_SZ) == 0);
   }
 
   {
     static const uchar input[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 };
     static const uchar output[] = { 16, 56, 150, 5, 174, 104, 141, 79, 20, 219, 133, 49, 34, 196, 125, 102, 168, 3, 199, 43, 65, 88, 156, 177, 191, 134, 135, 65, 178, 6, 185, 187 };
     fd_poseidon_hash_result_t res;
-    fd_poseidon_hash(input, sizeof(input), 1, &res);
-    FD_TEST(memcmp(res.v, output, FD_POSEIDON_HASH_SIZE) == 0);
+    fd_poseidon_hash(&res, input, sizeof(input), 1);
+    FD_TEST(memcmp(res.v, output, FD_POSEIDON_HASH_SZ) == 0);
   }
 
   {
     static const uchar input[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 };
     static const uchar output[] = { 42, 115, 246, 121, 50, 140, 62, 171, 114, 74, 163, 229, 189, 191, 80, 179, 144, 53, 215, 114, 159, 19, 91, 151, 9, 137, 15, 133, 197, 220, 94, 118 };
     fd_poseidon_hash_result_t res;
-    fd_poseidon_hash(input, sizeof(input), 1, &res);
-    FD_TEST(memcmp(res.v, output, FD_POSEIDON_HASH_SIZE) == 0);
+    fd_poseidon_hash(&res, input, sizeof(input), 1);
+    FD_TEST(memcmp(res.v, output, FD_POSEIDON_HASH_SZ) == 0);
   }
 
   {
     static const uchar input[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 };
     static const uchar output[] = { 34, 118, 49, 10, 167, 243, 52, 58, 40, 66, 20, 19, 157, 157, 169, 89, 190, 42, 49, 178, 199, 8, 165, 248, 25, 84, 178, 101, 229, 58, 48, 184 };
     fd_poseidon_hash_result_t res;
-    fd_poseidon_hash(input, sizeof(input), 1, &res);
-    FD_TEST(memcmp(res.v, output, FD_POSEIDON_HASH_SIZE) == 0);
+    fd_poseidon_hash(&res, input, sizeof(input), 1);
+    FD_TEST(memcmp(res.v, output, FD_POSEIDON_HASH_SZ) == 0);
   }
 
   {
     static const uchar input[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 };
     static const uchar output[] = { 23, 126, 20, 83, 196, 70, 225, 176, 125, 43, 66, 51, 66, 81, 71, 9, 92, 79, 202, 187, 35, 61, 35, 11, 109, 70, 162, 20, 217, 91, 40, 132 };
     fd_poseidon_hash_result_t res;
-    fd_poseidon_hash(input, sizeof(input), 1, &res);
-    FD_TEST(memcmp(res.v, output, FD_POSEIDON_HASH_SIZE) == 0);
+    fd_poseidon_hash(&res, input, sizeof(input), 1);
+    FD_TEST(memcmp(res.v, output, FD_POSEIDON_HASH_SZ) == 0);
   }
 
   {
     static const uchar input[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 };
     static const uchar output[] = { 14, 143, 238, 47, 228, 157, 163, 15, 222, 235, 72, 196, 46, 187, 68, 204, 110, 231, 5, 95, 97, 251, 202, 94, 49, 59, 138, 95, 202, 131, 76, 71 };
     fd_poseidon_hash_result_t res;
-    fd_poseidon_hash(input, sizeof(input), 1, &res);
-    FD_TEST(memcmp(res.v, output, FD_POSEIDON_HASH_SIZE) == 0);
+    fd_poseidon_hash(&res, input, sizeof(input), 1);
+    FD_TEST(memcmp(res.v, output, FD_POSEIDON_HASH_SZ) == 0);
   }
 
   {
     static const uchar input[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 };
     static const uchar output[] = { 46, 196, 198, 94, 99, 120, 171, 140, 115, 48, 133, 79, 74, 112, 119, 193, 255, 146, 96, 228, 72, 133, 196, 184, 29, 209, 49, 173, 58, 134, 205, 150 };
     fd_poseidon_hash_result_t res;
-    fd_poseidon_hash(input, sizeof(input), 1, &res);
-    FD_TEST(memcmp(res.v, output, FD_POSEIDON_HASH_SIZE) == 0);
+    fd_poseidon_hash(&res, input, sizeof(input), 1);
+    FD_TEST(memcmp(res.v, output, FD_POSEIDON_HASH_SZ) == 0);
   }
 
   {
     static const uchar input[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 };
     static const uchar output[] = { 0, 113, 61, 65, 236, 166, 53, 241, 23, 212, 236, 188, 235, 95, 58, 102, 220, 65, 66, 235, 112, 181, 103, 101, 188, 53, 143, 27, 236, 64, 187, 155 };
     fd_poseidon_hash_result_t res;
-    fd_poseidon_hash(input, sizeof(input), 1, &res);
-    FD_TEST(memcmp(res.v, output, FD_POSEIDON_HASH_SIZE) == 0);
+    fd_poseidon_hash(&res, input, sizeof(input), 1);
+    FD_TEST(memcmp(res.v, output, FD_POSEIDON_HASH_SZ) == 0);
   }
 
   {
     static const uchar input[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 };
     static const uchar output[] = { 20, 57, 11, 224, 186, 239, 36, 155, 212, 124, 101, 221, 172, 101, 194, 229, 46, 133, 19, 192, 129, 193, 205, 114, 201, 128, 6, 9, 142, 154, 143, 190 };
     fd_poseidon_hash_result_t res;
-    fd_poseidon_hash(input, sizeof(input), 1, &res);
-    FD_TEST(memcmp(res.v, output, FD_POSEIDON_HASH_SIZE) == 0);
+    fd_poseidon_hash(&res, input, sizeof(input), 1);
+    FD_TEST(memcmp(res.v, output, FD_POSEIDON_HASH_SZ) == 0);
   }
 
   {
@@ -149,8 +218,47 @@ int main( int     argc,
     for (ulong i = 0; i < 12; ++i) {
       fd_memcpy(bytes + i*32U, FLIST[i], 32U);
       fd_poseidon_hash_result_t res;
-      fd_poseidon_hash(bytes, (i+1U)*32U, 1, &res);
-      FD_TEST(memcmp(res.v, FLIST[i+1], FD_POSEIDON_HASH_SIZE) == 0);
+      fd_poseidon_hash(&res, bytes, (i+1U)*32U, 1);
+      FD_TEST(memcmp(res.v, FLIST[i+1], FD_POSEIDON_HASH_SZ) == 0);
+    }
+
+    fd_poseidon_t pos[1];
+    fd_poseidon_init( pos, 1 );
+    for (ulong i = 0; i < 12; ++i) {
+      fd_poseidon_append( pos, FLIST[i], 32 );
+    }
+    uchar FD_ALIGNED res[32];
+    fd_poseidon_fini( pos, res );
+    FD_TEST(memcmp(res, FLIST[12], FD_POSEIDON_HASH_SZ) == 0);
+
+    /*
+       fd_poseidon_hash(1)   9.530K/s/core 104927.617 ns/call
+       fd_poseidon_hash(2)   5.372K/s/core 186157.594 ns/call
+       fd_poseidon_hash(4)   2.213K/s/core 451814.312 ns/call
+       fd_poseidon_hash(6)   1.159K/s/core 862762.500 ns/call
+       fd_poseidon_hash(12)  0.349K/s/core 2869402.500 ns/call
+
+       fd_poseidon_hash(1)   25.282K/s/core  39553.863 ns/call
+       fd_poseidon_hash(2)   15.764K/s/core  63437.676 ns/call
+       fd_poseidon_hash(4)    7.002K/s/core 142807.094 ns/call
+       fd_poseidon_hash(6)    3.658K/s/core 273403.594 ns/call
+       fd_poseidon_hash(12)   1.116K/s/core 896352.188 ns/call
+    */
+    /* benchmark */
+    char cstr[128];
+    ulong iter = 1000UL;
+    for( ulong j=1; j<=12; j*=2 ) {
+      long dt = fd_log_wallclock();
+      for( ulong rem=iter; rem; rem-- ) {
+        fd_poseidon_init( pos, 1 );
+        for (ulong i = 0; i < j; ++i) {
+          fd_poseidon_append( pos, FLIST[i], 32 );
+        }
+        fd_poseidon_fini( pos, res );
+      }
+      dt = fd_log_wallclock() - dt;
+      log_bench( fd_cstr_printf( cstr, 128UL, NULL,"fd_poseidon_hash(%ld)", j), iter, dt );
+      if( j==4 ) j=3; /* mini-hack to get 1, 2, 4, 6, 12 */
     }
   }
 
