06a4282e2312ae148d06cc3101b3b4f7e8617af6
Emanuele Cesena <ecesena@jumptrading.com>
2024-04-30 18:56:18 +0000

    simd-0129: alt_bn128 syscalls (ops, compression, poseidon)

--- src/ballet/bn254/fd_bn254.h
+++ src/ballet/bn254/fd_bn254.h
@@ -10,18 +10,18 @@
 FD_PROTOTYPES_BEGIN
 
 int
-fd_bn254_g1_add_syscall( uchar       out[64], /* must be FD_ALIGNED */
-                         uchar const in[],    /* should be FD_ALIGNED, in_sz bytes */
+fd_bn254_g1_add_syscall( uchar       out[64],
+                         uchar const in[],
                          ulong       in_sz );
 
 int
-fd_bn254_g1_scalar_mul_syscall( uchar       out[64], /* must be FD_ALIGNED */
-                                uchar const in[],    /* should be FD_ALIGNED, in_sz bytes */
+fd_bn254_g1_scalar_mul_syscall( uchar       out[64],
+                                uchar const in[],
                                 ulong       in_sz );
 
 int
-fd_bn254_pairing_is_one_syscall( uchar       out[32], /* should be FD_ALIGNED */
-                                 uchar const in[],    /* should be FD_ALIGNED, in_sz bytes */
+fd_bn254_pairing_is_one_syscall( uchar       out[32],
+                                 uchar const in[],
                                  ulong       in_sz );
 
 /* fd_bn254_g1_compress compresses a point in G1.
@@ -32,8 +32,8 @@ fd_bn254_pairing_is_one_syscall( uchar       out[32], /* should be FD_ALIGNED */
    Returns out on success, NULL on failure.
    Note: this function does NOT check that (x, y) is in G1. */
 uchar *
-fd_bn254_g1_compress( uchar       out[32],   /* must be FD_ALIGNED */
-                      uchar const in [64] ); /* must be FD_ALIGNED */
+fd_bn254_g1_compress( uchar       out[32],
+                      uchar const in [64] );
 
 /* fd_bn254_g1_decompress decompresses a point in G1.
    Input in is a 32-byte big endian buffer representing the x coord of a point,
@@ -43,8 +43,8 @@ fd_bn254_g1_compress( uchar       out[32],   /* must be FD_ALIGNED */
    Returns out on success, NULL on failure.
    (Success implies that (x, y) is in G1.) */
 uchar *
-fd_bn254_g1_decompress( uchar       out[64],   /* must be FD_ALIGNED */
-                        uchar const in [32] ); /* must be FD_ALIGNED */
+fd_bn254_g1_decompress( uchar       out[64],
+                        uchar const in [32] );
 
 /* fd_bn254_g2_compress compresses a point in G2.
    Same as fd_bn254_g1_compress, but x, y are in Fp2, so twice as long.
@@ -55,8 +55,8 @@ fd_bn254_g1_decompress( uchar       out[64],   /* must be FD_ALIGNED */
    Returns out on success, NULL on failure.
    Note: this function does NOT check that (x, y) is in G2. */
 uchar *
-fd_bn254_g2_compress( uchar       out[64],   /* must be FD_ALIGNED */
-                      uchar const in[128] ); /* must be FD_ALIGNED */
+fd_bn254_g2_compress( uchar       out[64],
+                      uchar const in[128] );
 
 /* fd_bn254_g2_decompress decompresses a point in G2.
    Same as fd_bn254_g1_decompress, but x, y are in Fp2, so twice as long.
@@ -68,8 +68,8 @@ fd_bn254_g2_compress( uchar       out[64],   /* must be FD_ALIGNED */
    Note: this function does NOT check that (x, y) is in G2 (success does NOT
    imply that). */
 uchar *
-fd_bn254_g2_decompress( uchar       out[128],   /* must be FD_ALIGNED */
-                        uchar const in  [64] ); /* must be FD_ALIGNED */
+fd_bn254_g2_decompress( uchar       out[128],
+                        uchar const in  [64] );
 
 FD_PROTOTYPES_END
 
