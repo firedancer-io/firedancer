69fa519c74965827c7043cd74c45d5c37f15f1b1
Emanuele Cesena <ecesena@jumptrading.com>
2024-05-09 16:09:26 +0000

    flamenco, vm: belt sand syscalls sha256, blake3, keccak256

--- src/flamenco/vm/syscall/Local.mk
+++ src/flamenco/vm/syscall/Local.mk
@@ -1,7 +1,7 @@
 ifdef FD_HAS_INT128
 ifdef FD_HAS_HOSTED
 $(call add-hdrs,fd_vm_syscall.h fd_vm_cpi.h)
-$(call add-objs,fd_vm_syscall fd_vm_syscall_cpi fd_vm_syscall_crypto fd_vm_syscall_curve fd_vm_syscall_pda fd_vm_syscall_runtime fd_vm_syscall_util,fd_flamenco)
+$(call add-objs,fd_vm_syscall fd_vm_syscall_cpi fd_vm_syscall_hash fd_vm_syscall_crypto fd_vm_syscall_curve fd_vm_syscall_pda fd_vm_syscall_runtime fd_vm_syscall_util,fd_flamenco)
 
 $(call make-unit-test,test_vm_syscalls,test_vm_syscalls,fd_flamenco fd_funk fd_ballet fd_util)
 $(call make-unit-test,test_vm_syscall_cpi,test_vm_syscall_cpi,fd_flamenco fd_funk fd_ballet fd_util)
