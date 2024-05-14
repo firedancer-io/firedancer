d728b24c39eb9e04191e81c369926a72de008ce1
Ravyu Sivakumaran <rsivakumaran@jumptrading.com>
2024-05-14 22:36:11 +0000

    ballet, sbpf loader: manually register entrypoint to calldests
    address PR feedback
    
    add unit test
    
    incorrect makefile params
    
    reformat test file

--- src/ballet/sbpf/Local.mk
+++ src/ballet/sbpf/Local.mk
@@ -2,5 +2,7 @@ $(call add-hdrs,fd_sbpf_instr.h fd_sbpf_loader.h fd_sbpf_opcodes.h)
 $(call add-objs,fd_sbpf_loader,fd_ballet)
 ifdef FD_HAS_HOSTED
 $(call make-unit-test,test_sbpf_load_prog,test_sbpf_load_prog,fd_ballet fd_util)
+$(call make-unit-test,test_sbpf_loader,test_sbpf_loader,fd_ballet fd_util)
+$(call run-unit-test,test_sbpf_loader)
 endif
 $(call make-fuzz-test,fuzz_sbpf_loader,fuzz_sbpf_loader,fd_ballet fd_util)
