e5e345ad9b508c18a0be5c18df356a881a5bb94e
Yunhao Zhang <yunzhang@jumptrading.com>
2024-05-14 21:07:31 +0000

    Add test_gossip_vote

--- src/disco/consensus/Local.mk
+++ src/disco/consensus/Local.mk
@@ -1,4 +1,5 @@
 ifdef FD_HAS_INT128
 $(call make-unit-test,test_vote_txn,test_vote_txn,fd_disco fd_choreo fd_flamenco fd_funk fd_tango fd_util fd_ballet fd_reedsol fd_waltz,$(SECP256K1_LIBS))
 $(call make-unit-test,test_consensus,test_consensus,fd_disco fd_choreo fd_flamenco fd_funk fd_tango fd_util fd_ballet fd_reedsol fd_waltz,$(SECP256K1_LIBS))
+$(call make-unit-test,test_gossip_vote,test_gossip_vote,fd_disco fd_choreo fd_flamenco fd_funk fd_tango fd_util fd_ballet fd_reedsol fd_waltz,$(SECP256K1_LIBS))
 endif
