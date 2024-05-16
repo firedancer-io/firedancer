340401dadc1e738ee153b90065b25429b533e43d
Aryaman Jain <arjain@jumptrading.com>
2024-05-16 21:05:08 +0000

    flamenco: add instrs reset to txn generation

--- src/flamenco/txn/fd_txn_generate.h
+++ src/flamenco/txn/fd_txn_generate.h
@@ -10,9 +10,9 @@
 /* Struct used to define a list of accounts supplied in a txn.
    Also provides information on number of signers/writeable accounts. */
 struct fd_txn_accounts {
-  /* signature cnt <= 256 */
+  /* signature cnt <= 128 */
   uchar  signature_cnt;
-  /* readonly signed/unsigned <= 256 */
+  /* readonly signed/unsigned <= 128 */
   uchar  readonly_signed_cnt;
   uchar  readonly_unsigned_cnt;
   ushort acct_cnt;
@@ -26,7 +26,9 @@ typedef struct fd_txn_accounts fd_txn_accounts_t;
 
 FD_PROTOTYPES_BEGIN
 
-/* Method used to create a template for a txn (useful for pre-staging and re-use) */
+/* Method used to create a template for a txn (useful for pre-staging and re-use)
+   Num signatures is restricted to < 128. Returns the offset to the start of the
+   insructions. */
 ulong
 fd_txn_base_generate( uchar out_txn_meta[ static FD_TXN_MAX_SZ ],
                       uchar out_txn_payload[ static FD_TXN_MTU ],
@@ -37,7 +39,8 @@ fd_txn_base_generate( uchar out_txn_meta[ static FD_TXN_MAX_SZ ],
 /* Method used for adding an instruction to a txn being generated.
    The accounts param is a list of indices to the accounts in the txn.
    The instruction buffer contains the data for the instruction to
-   be added. */
+   be added. Returns the offset to the start of the instruction added
+   in the transaction buffer. */
 ulong
 fd_txn_add_instr( uchar * txn_meta_ptr,
                   uchar out_txn_payload[ static FD_TXN_MTU ],
@@ -46,4 +49,10 @@ fd_txn_add_instr( uchar * txn_meta_ptr,
                   ulong accounts_sz,
                   uchar const * instr_buf,
                   ulong instr_buf_sz );
+
+/* Helper method to reset the list of instrs in the metadata and
+   remove all instrs from the txn payload. */
+void
+fd_txn_reset_instrs( uchar * txn_meta_ptr,
+                     uchar out_txn_payload[ static FD_TXN_MTU ] );
 FD_PROTOTYPES_END
