071e7b506884942db019073ad0629bd1cfe06a10
Alex Peng <alpeng@jumptrading.com>
2024-06-12 19:26:32 +0000

    contrib: setup solcap and instr command to identify failed instr

--- contrib/ledger-tests/solcap.sh
+++ contrib/ledger-tests/solcap.sh
@@ -3,29 +3,50 @@
 # This script can be used to produce a solcap file for a given ledger for both firedancer
 # and solana. It can also be used to produce a diff between the two solcap files.
 
-solcap_script="src/flamenco/capture/solcap.sh"
-solcap_fd_ledger_dump="$FIREDANCER/dump"
-solcap_ledger_min_basename=$(basename "$LEDGER_MIN")
-solcap_diff_output="${solcap_ledger_min_basename}_diff"
+solcap_firedancer_repo="$REPO_ROOT/firedancer"
 
-if [ ! -d "$FIREDANCER" ]; then
-  echo "[-] $FIREDANCER not found"
-  exit 1
-fi
+cd $solcap_firedancer_repo || exit
 
-if [ ! -f "$FIREDANCER/$solcap_script" ]; then
-  echo "[-] $FIREDANCER/$solcap_script not found"
-  exit 1
-fi
+solcap_fd_solcap_import="build/native/gcc/bin/fd_solcap_import"
+solcap_fd_solcap_diff="build/native/gcc/bin/fd_solcap_diff"
 
-cp -rf $LEDGER_MIN $solcap_fd_ledger_dump
+solcap_snap_root_path=$(find dump -maxdepth 2 -name "snapshot-*" -type f -print0 | xargs -0 ls -t | head -n 1)
+solcap_rocksdb_path=$(echo "$solcap_snap_root_path" | sed 's|/snapshot-.*|/rocksdb|')
+solcap_bank_hash_details_path=$(echo "$solcap_snap_root_path" | sed 's|/snapshot-.*|/ledger_tool/bank_hash_details|')
+solcap_dump_path=$(echo "$solcap_snap_root_path" | sed 's|/snapshot-.*|/|')
+solcap_mismatch_slot=$(echo "$solcap_snap_root_path" | awk -F'/' '{print $2}' | awk -F'-' '{print $2}')
+solcap_end_slot=$((solcap_mismatch_slot + 1))
 
-set +x
-cd $FIREDANCER || exit
-SOLANADIR=$SOLANA_BUILD_DIR $solcap_script --firedancer-solcap $FIREDANCER_SOLCAP --solana-solcap $SOLANA_SOLCAP \
-  --end-slot $END_SLOT --ledger dump/$solcap_ledger_min_basename/ \
-  --checkpoint $CHECKPOINT --output $solcap_diff_output --verbosity $VERBOSITY
+
+# produce firedancer solcap
+# this would usually be enough since we have copy-txn-status set to 1
+# but there is a bug in rocksdb minify so we also need to create the solana solcap below
+build/native/gcc/bin/fd_ledger --cmd replay \
+                               --rocksdb $solcap_rocksdb_path \
+                               --index-max $INDEX_MAX \
+                               --end-slot $solcap_end_slot \
+                               --funk-only 1 \
+                               --txn-max 100 \
+                               --page-cnt $PAGES \
+                               --funk-page-cnt $FUNK_PAGES \
+                               --verify-acc-hash 1 \
+                               --snapshot $solcap_snap_root_path \
+                               --slot-history 5000 \
+                               --allocator wksp \
+                               --on-demand-block-ingest 1 \
+                               --tile-cpus 5-21 \
+                               --capture-txns 1 \
+                               --copy-txn-status 1 \
+                               --capture-solcap solcap.solcap
+
+# produce solana solcap 
+$SOLANA_LEDGER_TOOL verify --ledger $solcap_dump_path --halt-at-slot $solcap_end_slot
+$solcap_fd_solcap_import $solcap_bank_hash_details_path solana.solcap
+$solcap_fd_solcap_import $solcap_bank_hash_details_path solana.solcap
+
+## Get the solcap Diff
 set -x
-
 echo -e "[~] solcap diff \n"
-cat $FIREDANCER/$solcap_diff_output
+$solcap_fd_solcap_diff fd.solcap solana.solcap -v 4
+set +x
+
