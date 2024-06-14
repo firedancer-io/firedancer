369222de9f73dbfe6903c97bb215cb32957a0dba
Anway De <ande@jumptrading.com>
2024-06-14 15:04:49 +0000

    ci: add back noarch build

--- contrib/test/ci_tests.sh
+++ contrib/test/ci_tests.sh
@@ -13,6 +13,10 @@ if [[ -z "$MACHINES" ]]; then
   exit 1
 fi
 
+if [[ -z "$TARGETS" ]]; then
+  TARGETS="all integration-test fdctl"
+fi
+
 for extra in $EXTRAS; do
   if [[ $extra == "llvm-cov" ]]; then
     HAS_LLVM_COV=1
@@ -29,7 +33,7 @@ for MACHINE in ${MACHINES[*]}; do
   OBJDIR="$(make help | grep OBJDIR | awk '{print $4}')"
   OBJDIRS+=( "${OBJDIR}" )
   make clean --silent >/dev/null
-  contrib/make-j all integration-test fdctl
+  contrib/make-j $TARGETS
   if [[ "$NOTEST" != 1 ]]; then
     make run-unit-test
     if [[ "$EXTRAS" != *"ubsan"* && "$EXTRAS" != *"asan"* ]]; then
