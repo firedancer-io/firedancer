d787bef5f954456a734d244bc68e4900246802a3
Charles Li <charles.dt.li@gmail.com>
2024-06-18 14:51:57 +0000

    refactor(ghost): init, node inset, slot key, publish, print, more tests
    Introduce fd_ghost_init to the API for initializing a ghost.
    
    Make parent a required argument for node_insert, given init
    will handle the special case of initializing the root.
    
    Change the key to use slot instead of slot_hash. This leverages the
    invariant that a vote will only be upserted after successful execution
    by the vote program, so the bank hash is implied to be our own.
    
    Reimplement publish (renamed from prune) to not use an additional buffer
    as well as provide a generally cleaner and better commented impl.
    
    Rework the pretty-print API to support functionality like in the demo
    directly (vs. having to be done in the caller).
    
    Add more tests relating to all the above.

--- src/choreo/ghost/fd_ghost.h
+++ src/choreo/ghost/fd_ghost.h
@@ -1,36 +1,45 @@
 #ifndef HEADER_fd_src_choreo_ghost_fd_ghost_h
 #define HEADER_fd_src_choreo_ghost_fd_ghost_h
 
-/* fd_ghost ("greedy heaviest-observed subtree") implements Solana's LMD-GHOST fork choice rule.
+/* fd_ghost implements Solana's LMD-GHOST ("latest message-driven greedy
+   heaviest-observed subtree") fork choice rule.
 
    Protocol details:
 
-   - GHOST is an acronym for "greedy heaviest-observed subtree":
-     - greedy: pick the locally optimal subtree / fork based on our current view, which may not be globally optimal.
-     - heaviest: pick based on the highest stake weight.
-     - observed: this is the validator's local view, and other validators may have differing views.
-     - subtree: pick a subtree, not an individual node.
+   - LMD is an acronym for "latest message-driven". It denotes the
+     specific flavor of GHOST implementation, ie. only a
+     validator's latest vote counts.
 
-   - LMD is an acronym for "latest message-driven". It denotes the specific flavor of GHOST
-     implementation: in this case, only a validator's latest vote counts.
+   - GHOST is an acronym for "greedy heaviest-observed subtree":
+     - greedy:   pick the locally optimal subtree / fork based on our
+                 current view (which may not be globally optimal).
+     - heaviest: pick based on the highest stake weight.
+     - observed: this is the validator's local view, and other
+                 validators may have differing views.
+     - subtree:  pick a subtree, not an individual node.
 
    In-memory representation:
 
-   - fd_ghost_node_t implements a left-child right-sibling n-ary tree. Each node holds a pointer to
-     its left-most child (`child`), and a pointer to its right sibling (`sibling`).
+   - fd_ghost_node_t implements a left-child right-sibling n-ary tree.
+     Each node holds a pointer to its left-most child (`child`), and a
+     pointer to its right sibling (`sibling`).
 
-   - Each tree node is keyed by slot_hash, which is a tuple (slot, bank_hash).
+   - Each tree node is keyed by slot number.
 
-   - Each tree node tracks the amount of stake (`stake`) that has voted for that key, as well as the
-     recursive sum of stake for the subtree rooted at that node (`weight`).
+   - Each tree node tracks the amount of stake (`stake`) that has voted
+     for its slot, as well as the recursive sum of stake for the subtree
+     rooted at that node (`weight`).
 
-   - fd_ghost_t is the top-level structure that holds the root of the tree, as well as the memory
-     pools and maps for nodes and votes.
+   - fd_ghost_t is the top-level structure that holds the root of the
+     tree, as well as the memory pools and map structures for nodes and
+     votes.
 
-   - fd_ghost_vote_t represents a validator's vote. This includes a slot hash and the validator's pubkey
-     and stake.
+   - fd_ghost_vote_t represents a validator's vote.  This includes a
+     slot hash and the validator's pubkey and stake.
 
-   [1] Original GHOST paper: https://eprint.iacr.org/2013/881.pdf */
+   Link to original GHOST paper: https://eprint.iacr.org/2013/881.pdf.
+   This is simply a reference for those curious about the etymology, and
+   not prerequisite reading for understanding this implementation. */
 
 #include "../fd_choreo_base.h"
 
@@ -41,17 +50,18 @@
 #define FD_GHOST_USE_HANDHOLDING 1
 #endif
 
+#define FD_GHOST_PRINT_DEPTH_DEFAULT 8
+
 /* clang-format off */
 typedef struct fd_ghost_node fd_ghost_node_t;
 struct __attribute__((aligned(128UL))) fd_ghost_node {
-  fd_slot_hash_t    slot_hash;    /* (slot, bank_hash), also the ghost key */
+  ulong             slot;         /* both the ghost and map key */
   ulong             next;         /* reserved for internal use by fd_pool and fd_map_chain */
-  ulong             weight;       /* sum of stake for the subtree rooted at this slot hash */
-  ulong             stake;        /* vote stake for this slot hash (replay votes only) */
-  ulong             gossip_stake; /* vote stake from gossip (sans replay overlap) for this slot hash */
+  ulong             weight;       /* amount of stake that has voted for this slot hash or descendants */
+  ulong             stake;        /* amount of stake that has voted for this slot hash */
+  ulong             gossip_stake; /* amount of stake from gossip votes (sans replay overlap) */
+  ulong             rooted_stake; /* amount of stake that has rooted this slot */
   int               eqv;          /* flag for equivocation (multiple blocks) in this slot */
-  int               eqv_safe;     /* flag for equivocation safety (ie. 52% of stake has voted for this slot hash) */
-  int               opt_conf;     /* flag for optimistic confirmation (ie. 2/3 of stake has voted for this slot hash ) */
   fd_ghost_node_t * parent;       /* pointer to the parent */
   fd_ghost_node_t * child;        /* pointer to the left-most child */
   fd_ghost_node_t * sibling;      /* pointer to next sibling */
@@ -60,26 +70,19 @@ struct __attribute__((aligned(128UL))) fd_ghost_node {
 #define FD_GHOST_EQV_SAFE ( 0.52 )
 #define FD_GHOST_OPT_CONF ( 2.0 / 3.0 )
 
-/* fork a's weight > fork b's weight, with lower slot # as tie-break */
-#define FD_GHOST_NODE_MAX(a,b) (fd_ptr_if(fd_int_if(a->weight==b->weight, a->slot_hash.slot<b->slot_hash.slot, a->weight>b->weight),a,b))
-#define FD_GHOST_NODE_EQ(a,b)  (FD_SLOT_HASH_EQ(&a->slot_hash,&b->slot_hash))
-
 #define POOL_NAME fd_ghost_node_pool
 #define POOL_T    fd_ghost_node_t
 #include "../../util/tmpl/fd_pool.c"
 
 #define MAP_NAME               fd_ghost_node_map
 #define MAP_ELE_T              fd_ghost_node_t
-#define MAP_KEY                slot_hash
-#define MAP_KEY_T              fd_slot_hash_t
-#define MAP_KEY_EQ(k0,k1)      FD_SLOT_HASH_EQ(k0,k1)
-#define MAP_KEY_HASH(key,seed) (key->slot^seed)
+#define MAP_KEY                slot
 #include "../../util/tmpl/fd_map_chain.c"
 
 struct fd_ghost_vote {
-  fd_pubkey_t    pubkey;    /* validator's pubkey, also the map key */
+  fd_pubkey_t    pubkey;    /* validator identity, also the map key */
   ulong          next;      /* reserved for internal use by fd_pool and fd_map_chain */
-  fd_slot_hash_t slot_hash; /* slot hash being voted for */
+  ulong          slot;      /* slot being voted for */
   ulong          stake;     /* validator's stake */
 };
 typedef struct fd_ghost_vote fd_ghost_vote_t;
@@ -96,28 +99,22 @@ typedef struct fd_ghost_vote fd_ghost_vote_t;
 #define MAP_KEY_HASH(key,seed) (key->ui[0]^seed)
 #include "../../util/tmpl/fd_map_chain.c"
 
-#define DEQUE_NAME fd_ghost_prune_deque
-#define DEQUE_T    fd_ghost_node_t *
-#include "../../util/tmpl/fd_deque_dynamic.c"
-
 struct __attribute__((aligned(128UL))) fd_ghost {
   fd_ghost_node_t *     root;
-  ulong                 total_stake; /* total amount of stake */
-
   fd_ghost_node_t *     node_pool; /* memory pool of ghost nodes */
   fd_ghost_node_map_t * node_map;  /* map of slot_hash->fd_ghost_node_t */
   fd_ghost_vote_t *     vote_pool; /* memory pool of ghost votes */
   fd_ghost_vote_map_t * vote_map;  /* each node's latest vote. map of pubkey->fd_ghost_vote_t */
-
-  fd_ghost_node_t **    prune_deque;     /* deque reserved for BFS */
 };
 typedef struct fd_ghost fd_ghost_t;
+
 /* clang-format on */
 
 FD_PROTOTYPES_BEGIN
 
-/* fd_ghost_{align,footprint} return the required alignment and footprint of a memory region
-   suitable for use as ghost with up to node_max nodes and vote_max votes. */
+/* fd_ghost_{align,footprint} return the required alignment and
+   footprint of a memory region suitable for use as ghost with up to
+   node_max nodes and vote_max votes. */
 
 FD_FN_CONST static inline ulong
 fd_ghost_align( void ) {
@@ -133,123 +130,125 @@ fd_ghost_footprint( ulong node_max, ulong vote_max ) {
     FD_LAYOUT_APPEND(
     FD_LAYOUT_APPEND(
     FD_LAYOUT_APPEND(
-    FD_LAYOUT_APPEND(
     FD_LAYOUT_INIT,
-      alignof( fd_ghost_t ), sizeof( fd_ghost_t ) ),
+      alignof( fd_ghost_t ),      sizeof( fd_ghost_t ) ),
       fd_ghost_node_pool_align(), fd_ghost_node_pool_footprint( node_max ) ),
-      fd_ghost_node_map_align(), fd_ghost_node_map_footprint( node_max ) ),
+      fd_ghost_node_map_align(),  fd_ghost_node_map_footprint( node_max ) ),
       fd_ghost_vote_pool_align(), fd_ghost_vote_pool_footprint( vote_max ) ),
-      fd_ghost_vote_map_align(), fd_ghost_vote_map_footprint( vote_max ) ),
-      fd_ghost_prune_deque_align(), fd_ghost_prune_deque_footprint( node_max ) ),
+      fd_ghost_vote_map_align(),  fd_ghost_vote_map_footprint( vote_max ) ),
     alignof( fd_ghost_t ) );
 }
 /* clang-format on */
 
-/* fd_ghost_new formats an unused memory region for use as a ghost. mem is a non-NULL pointer to
-   this region in the local address space with the required footprint and alignment. */
+/* fd_ghost_new formats an unused memory region for use as a ghost.
+   mem is a non-NULL pointer to this region in the local address space
+   with the required footprint and alignment. */
 
 void *
 fd_ghost_new( void * mem, ulong node_max, ulong vote_max, ulong seed );
 
-/* fd_ghost_join joins the caller to the ghost. ghost points to the first byte of the memory region
-   backing the ghost in the caller's address space.
+/* fd_ghost_join joins the caller to the ghost.  ghost points to the
+   first byte of the memory region backing the ghost in the caller's
+   address space.
 
    Returns a pointer in the local address space to ghost on success. */
 
 fd_ghost_t *
 fd_ghost_join( void * ghost );
 
-/* fd_ghost_leave leaves a current local join. Returns a pointer to the underlying shared memory
-   region on success and NULL on failure (logs details). Reasons for failure include ghost is NULL.
+/* fd_ghost_leave leaves a current local join.  Returns a pointer to the
+   underlying shared memory region on success and NULL on failure (logs
+   details).  Reasons for failure include ghost is NULL.
  */
 
 void *
 fd_ghost_leave( fd_ghost_t const * ghost );
 
-/* fd_ghost_delete unformats a memory region used as a ghost. Assumes only the local process is
-   joined to the region. Returns a pointer to the underlying shared memory region or NULL if used
-   obviously in error (e.g. ghost is obviously not a ghost ... logs details). The ownership of the
-   memory region is transferred to the caller. */
+/* fd_ghost_delete unformats a memory region used as a ghost.
+   Assumes only the nobody is joined to the region.  Returns a
+   pointer to the underlying shared memory region or NULL if used
+   obviously in error (e.g. ghost is obviously not a ghost ... logs
+   details).  The ownership of the memory region is transferred to the
+   caller. */
 
 void *
 fd_ghost_delete( void * ghost );
 
-/* fd_ghost_head_query returns the head of the ghost. Ghost is always non-empty, ie. a root must exist. */
+/* fd_ghost_init initializes a ghost.  Assumes fd_ghost_new and 
+   fd_ghost_join and ghost is a valid local join root_slot_hash is the
+   initial root ghost will use.  This is the snapshot slot if booting
+   with a snapshot, genesis slot if not.
 
-static inline fd_ghost_node_t *
-fd_ghost_head_query( fd_ghost_t * ghost ) {
-  fd_ghost_node_t * head = ghost->root;
-  while( head->child ) {
-    fd_ghost_node_t * curr = head;
-    while( curr ) {
-      head = FD_GHOST_NODE_MAX( curr, head );
-      curr = curr->sibling;
-    }
-    head = head->child;
-  }
-  return head;
-}
+   This should generally not be called by anyone other than the owner of
+   ghost's memory (ie. same caller as fd_ghost_new.) */
+void
+fd_ghost_init( fd_ghost_t * ghost, ulong slot );
 
-/* fd_ghost_leaf_insert inserts a new leaf node with key into the ghost, with parent_slot_hash
-   optionally specified. The caller promises the key is not currently in the map and that the
-   parent_slot_hash, if specified, is in the map.
-
-   There are three cases to consider:
-      1. there is no parent, implying key is the root and the fork head.
-      2. key is the only child of parent, implying key is the new fork head.
-      2. key has siblings. Then, key is compared in stake (>), slot (<) priority
-         respectively with its siblings, and the fork head is updated if the key has the
-         highest priority.
-*/
+/* fd_ghost_head_query returns the head of the ghost.  Ghost is always
+   non-empty, ie. a root must exist. */
 
 fd_ghost_node_t *
-fd_ghost_node_insert( fd_ghost_t *           ghost,
-                      fd_slot_hash_t const * slot_hash,
-                      fd_slot_hash_t const * parent_slot_hash_opt );
+fd_ghost_head_query( fd_ghost_t * ghost );
 
-/* fd_ghost_node_query finds the node corresponding to key. */
+/* fd_ghost_leaf_insert inserts a new leaf node with key into the ghost,
+   with parent_slot_hash optionally specified.  The caller promises
+   slot_hash is not currently in the map and parent_slot_hash is. */
 
 fd_ghost_node_t *
-fd_ghost_node_query( fd_ghost_t * ghost, fd_slot_hash_t const * slot_hash );
+fd_ghost_node_insert( fd_ghost_t * ghost, ulong slot, ulong parent_slot );
 
-/* fd_ghost_replay_vote_insert inserts a replay vote into ghost.
+/* fd_ghost_node_query queries and returns the node keyed by slot_hash.
+   Returns NULL if not found. */
 
-   The stake associated with pubkey is added to the ancestry chain beginning at slot hash
-   ("insert"). If pubkey has previously voted, the previous vote's stake is removed from the
-   previous vote slot hash's ancestry chain ("update").
+fd_ghost_node_t *
+fd_ghost_node_query( fd_ghost_t * ghost, ulong slot );
 
-   TODO the implementation can be made more efficient by short-circuiting and doing fewer
-   traversals, but as it exists this is bounded to O(h), where h is the height of ghost.
+/* fd_ghost_replay_vote_upsert inserts a replay vote into ghost.
 
-   Note it is specific to the replay vote case that stake is propagated up the ancestry
-   chain.
-*/
+   The stake associated with pubkey is added to the ancestry chain
+   beginning at slot hash ("insert").  If pubkey has previously voted,
+   the previous vote's stake is removed from the previous vote slot
+   hash's ancestry chain ("update").
 
-void
-fd_ghost_replay_vote_insert( fd_ghost_t *           ghost,
-                             fd_slot_hash_t const * slot_hash,
-                             fd_pubkey_t const *    pubkey,
-                             ulong                  stake );
-
-/* fd_ghost_gossip_vote_insert inserts a gossip vote into ghost.
-
-   Unlike fd_ghost_replay_vote_upsert, the stake associated with pubkey is not propagated. It is
-   only counted towards the individual slot hash voted for.
-*/
+   TODO the implementation can be made more efficient by
+   short-circuiting and doing fewer traversals, but as it exists this is
+   bounded to O(h), where h is the height of ghost. */
 
 void
-fd_ghost_gossip_vote_insert( fd_ghost_t *           ghost,
-                             fd_slot_hash_t const * slot_hash,
-                             fd_pubkey_t const *    pubkey,
-                             ulong                  stake );
+fd_ghost_replay_vote_upsert( fd_ghost_t * ghost, ulong slot, fd_pubkey_t const * pubkey, ulong stake );
+
+/* fd_ghost_gossip_vote_upsert inserts a gossip vote into ghost.
+
+   Unlike fd_ghost_replay_vote_upsert, the stake associated with pubkey
+   is not propagated to ancestors of slot_hash.  It is only counted
+   towards slot_hash itself. */
 
 void
-fd_ghost_prune( fd_ghost_t * ghost, fd_ghost_node_t const * root );
+fd_ghost_gossip_vote_upsert( fd_ghost_t * ghost, ulong slot, fd_pubkey_t const * pubkey, ulong stake );
 
-/* fd_ghost_print calls printf to generate a pretty-printed ghost. */
+/* fd_ghost_publish publishes the subtree at root as the new ghost tree,
+   pruning all nodes prior to root. */
+void
+fd_ghost_publish( fd_ghost_t * ghost, fd_ghost_node_t * root );
+
+/* fd_ghost_print pretty-prints a formatted ghost tree.  start controls
+   which node to begin printing from.  depth controls how many
+   additional ancestors to walk back from start to begin printing from.
+   total_stake controls how much total stake is active to enable
+   percentage calculations.
+
+   FD_SLOT_NULL, 0 and 0 are valid defaults for the above, respectively.
+   In that case, ghost would begin printing from the root without
+   percentages (not recommended because the output will be difficult
+   to read).
+   
+   Typical usage is to pass in the most recently executed slot hash for
+   start, so that start is always in a leaf position, and pick an
+   appropriate depth for visualization (FD_GHOST_PRINT_DEPTH_DEFAULT
+   is the recommended default). */
 
 void
-fd_ghost_print( fd_ghost_t const * ghost, fd_ghost_node_t const * root );
+fd_ghost_print( fd_ghost_t * ghost, ulong start, ulong depth, ulong total_stake );
 
 FD_PROTOTYPES_END
 
--- src/choreo/ghost/test_ghost.c
+++ src/choreo/ghost/test_ghost.c
@@ -1,16 +1,14 @@
 #include "fd_ghost.h"
 #include <stdlib.h>
 
-#define INSERT( c, p )                                                           \
-  slot_hashes[i] = ( fd_slot_hash_t ){ .slot = c, .hash = pubkey_null };          \
-  if( p < ULONG_MAX ) {                                                          \
-    parent_slot_hashes[i] = ( fd_slot_hash_t ){ .slot = p, .hash = pubkey_null }; \
-    fd_ghost_node_insert( ghost, &slot_hashes[i], &parent_slot_hashes[i] );        \
-  } else {                                                                       \
-    parent_slot_hashes[i] = ( fd_slot_hash_t ){ .slot = p, .hash = pubkey_null }; \
-    fd_ghost_node_insert( ghost, &slot_hashes[i], NULL );                         \
-  }                                                                              \
-  i++;
+#define INSERT( c, p )                                                                                                                     \
+  slots[i]        = c;                                                                                                                     \
+  parent_slots[i] = p;                                                                                                                     \
+  if( p < ULONG_MAX ) {                                                                                                                    \
+    fd_ghost_node_insert( ghost, slots[i], parent_slots[i] );                                                                              \
+  } else {                                                                                                                                 \
+    fd_ghost_init( ghost, slots[i] );                                                                                                      \
+  }
 
 /*
          slot 0
@@ -25,10 +23,16 @@
             slot 6
 */
 void
-test_ghost_simple( fd_ghost_t * ghost ) {
-  fd_slot_hash_t slot_hashes[fd_ghost_node_pool_max( ghost->node_pool )];
-  fd_slot_hash_t parent_slot_hashes[fd_ghost_node_pool_max( ghost->node_pool )];
-  ulong          i = 0;
+test_ghost_simple( fd_wksp_t * wksp ) {
+  ulong  node_max = 8;
+  ulong  vote_max = 8;
+  void * mem      = fd_wksp_alloc_laddr( wksp, fd_ghost_align(), fd_ghost_footprint( node_max, vote_max ), 1UL );
+  FD_TEST( mem );
+  fd_ghost_t * ghost = fd_ghost_join( fd_ghost_new( mem, node_max, vote_max, 0UL ) );
+
+  ulong slots[fd_ghost_node_pool_max( ghost->node_pool )];
+  ulong parent_slots[fd_ghost_node_pool_max( ghost->node_pool )];
+  ulong i = 0;
 
   INSERT( 0, ULONG_MAX );
   INSERT( 1, 0 );
@@ -38,53 +42,175 @@ test_ghost_simple( fd_ghost_t * ghost ) {
   INSERT( 5, 3 );
   INSERT( 6, 5 );
 
-  fd_ghost_print( ghost, ghost->root );
+  fd_ghost_print( ghost, FD_SLOT_NULL, 0, 0 );
 
-  fd_pubkey_t    pk1 = { .key = { 1 } };
-  fd_slot_hash_t sh2 = { .slot = 2, .hash = pubkey_null };
-  fd_ghost_replay_vote_insert( ghost, &sh2, &pk1, 1 );
+  fd_pubkey_t pk1  = { .key = { 1 } };
+  ulong       key2 = 2;
+  fd_ghost_replay_vote_upsert( ghost, key2, &pk1, 1 );
 
-  fd_ghost_print( ghost, ghost->root );
+  fd_ghost_print( ghost, FD_SLOT_NULL, 0, 0 );
 
-  fd_slot_hash_t sh3 = { .slot = 3, .hash = pubkey_null };
-  fd_ghost_replay_vote_insert( ghost, &sh3, &pk1, 1 );
+  ulong key3 = 3;
+  fd_ghost_replay_vote_upsert( ghost, key3, &pk1, 1 );
 
-  ghost->total_stake = 1;
-  fd_ghost_print( ghost, ghost->root );
+  fd_ghost_print( ghost, FD_SLOT_NULL, 0, 0 );
+
+  fd_wksp_free_laddr( mem );
+}
+
+/*
+         slot 0
+           |
+         slot 1
+         /    \
+    slot 2    |
+       |    slot 3
+    slot 4    |
+            slot 5
+              |
+            slot 6
+
+          ...
+
+         slot 2
+           |
+         slot 4
+*/
+void
+test_ghost_publish_left( fd_wksp_t * wksp ) {
+  ulong  node_max = 8;
+  ulong  vote_max = 8;
+  void * mem      = fd_wksp_alloc_laddr( wksp, fd_ghost_align(), fd_ghost_footprint( node_max, vote_max ), 1UL );
+  FD_TEST( mem );
+  fd_ghost_t * ghost = fd_ghost_join( fd_ghost_new( mem, node_max, vote_max, 0UL ) );
+
+  ulong slots[node_max];
+  ulong parent_slots[node_max];
+  ulong i = 0;
+
+  INSERT( 0, ULONG_MAX );
+  INSERT( 1, 0 );
+  INSERT( 2, 1 );
+  INSERT( 3, 1 );
+  INSERT( 4, 2 );
+  INSERT( 5, 3 );
+  INSERT( 6, 5 );
+
+  fd_pubkey_t pk1  = { .key = { 1 } };
+  ulong       key2 = 2;
+  fd_ghost_replay_vote_upsert( ghost, key2, &pk1, 1 );
+
+  ulong key3 = 3;
+  fd_ghost_replay_vote_upsert( ghost, key3, &pk1, 1 );
+  fd_ghost_node_t * node2 = fd_ghost_node_query( ghost, key2 );
+  FD_TEST( node2 );
+
+  fd_ghost_print( ghost, FD_SLOT_NULL, 0, 0 );
+  fd_ghost_publish( ghost, node2 );
+  FD_TEST( ghost->root->slot == 2 );
+  FD_TEST( ghost->root->child->slot == 4 );
+  FD_TEST( fd_ghost_node_pool_free( ghost->node_pool ) == node_max - 2 );
+  fd_ghost_print( ghost, FD_SLOT_NULL, 0, 0 );
+
+  fd_wksp_free_laddr( mem );
+}
+
+/*
+         slot 0
+           |
+         slot 1
+         /    \
+    slot 2    |
+       |    slot 3
+    slot 4    |
+            slot 5
+              |
+            slot 6
+
+          ...
+
+         slot 3
+           |
+         slot 5
+           |
+         slot 6
+*/
+void
+test_ghost_publish_right( fd_wksp_t * wksp ) {
+  ulong  node_max = 8;
+  ulong  vote_max = 8;
+  void * mem      = fd_wksp_alloc_laddr( wksp, fd_ghost_align(), fd_ghost_footprint( node_max, vote_max ), 1UL );
+  FD_TEST( mem );
+  fd_ghost_t * ghost = fd_ghost_join( fd_ghost_new( mem, node_max, vote_max, 0UL ) );
+
+  ulong slots[node_max];
+  ulong parent_slots[node_max];
+  ulong i = 0;
+
+  INSERT( 0, ULONG_MAX );
+  INSERT( 1, 0 );
+  INSERT( 2, 1 );
+  INSERT( 3, 1 );
+  INSERT( 4, 2 );
+  INSERT( 5, 3 );
+  INSERT( 6, 5 );
+
+  fd_pubkey_t pk1  = { .key = { 1 } };
+  ulong       key2 = 2;
+  fd_ghost_replay_vote_upsert( ghost, key2, &pk1, 1 );
+
+  ulong key3 = 3;
+  fd_ghost_replay_vote_upsert( ghost, key3, &pk1, 1 );
+  fd_ghost_node_t * node3 = fd_ghost_node_query( ghost, key3 );
+  FD_TEST( node3 );
+
+  fd_ghost_print( ghost, FD_SLOT_NULL, 0, 0 );
+  fd_ghost_publish( ghost, node3 );
+  FD_TEST( ghost->root->slot == 3 );
+  FD_TEST( ghost->root->child->slot == 5 );
+  FD_TEST( ghost->root->child->child->slot == 6 );
+  FD_TEST( fd_ghost_node_pool_free( ghost->node_pool ) == node_max - 3 );
+  fd_ghost_print( ghost, FD_SLOT_NULL, 0, 0 );
+
+  fd_wksp_free_laddr( mem );
 }
 
 void
-test_ghost_print( fd_ghost_t * ghost ) {
-  fd_slot_hash_t slot_hashes[fd_ghost_node_pool_max( ghost->node_pool )];
-  fd_slot_hash_t parent_slot_hashes[fd_ghost_node_pool_max( ghost->node_pool )];
-  ulong          i = 0;
+test_ghost_print( fd_wksp_t * wksp ) {
+  ulong  node_max = 16;
+  ulong  vote_max = 16;
+  void * mem      = fd_wksp_alloc_laddr( wksp, fd_ghost_align(), fd_ghost_footprint( node_max, vote_max ), 1UL );
+  FD_TEST( mem );
+  fd_ghost_t * ghost = fd_ghost_join( fd_ghost_new( mem, node_max, vote_max, 0UL ) );
+
+  ulong slots[node_max];
+  ulong parent_slots[node_max];
+  ulong i = 0;
 
   INSERT( 268538758, ULONG_MAX );
   INSERT( 268538759, 268538758 );
   INSERT( 268538760, 268538759 );
   INSERT( 268538761, 268538758 );
 
-  fd_slot_hash_t query = {
-    .slot = 268538758,
-    .hash = pubkey_null
-  };
-  fd_ghost_node_t * node = fd_ghost_node_query( ghost, &query );
-  node->weight = 32;
+  ulong             query = 268538758;
+  fd_ghost_node_t * node  = fd_ghost_node_query( ghost, query );
+  node->weight            = 32;
 
-  query.slot = 268538759;
-  node = fd_ghost_node_query( ghost, &query );
+  query        = 268538759;
+  node         = fd_ghost_node_query( ghost, query );
   node->weight = 8;
 
-  query.slot = 268538760;
-  node = fd_ghost_node_query( ghost, &query );
+  query        = 268538760;
+  node         = fd_ghost_node_query( ghost, query );
   node->weight = 9;
 
-  query.slot = 268538761;
-  node = fd_ghost_node_query( ghost, &query );
+  query        = 268538761;
+  node         = fd_ghost_node_query( ghost, query );
   node->weight = 10;
 
-  ghost->total_stake = 100;
-  fd_ghost_print( ghost, ghost->root );
+  fd_ghost_print( ghost, query, FD_GHOST_PRINT_DEPTH_DEFAULT, 100 );
+
+  fd_wksp_free_laddr( mem );
 }
 
 int
@@ -94,29 +220,14 @@ main( int argc, char ** argv ) {
   ulong  page_cnt = 1;
   char * _page_sz = "gigantic";
   ulong  numa_idx = fd_shmem_numa_idx( 0 );
-  FD_LOG_NOTICE( ( "Creating workspace (--page-cnt %lu, --page-sz %s, --numa-idx %lu)",
-                   page_cnt,
-                   _page_sz,
-                   numa_idx ) );
-  fd_wksp_t * wksp = fd_wksp_new_anonymous(
-      fd_cstr_to_shmem_page_sz( _page_sz ), page_cnt, fd_shmem_cpu_idx( numa_idx ), "wksp", 0UL );
+  FD_LOG_NOTICE( ( "Creating workspace (--page-cnt %lu, --page-sz %s, --numa-idx %lu)", page_cnt, _page_sz, numa_idx ) );
+  fd_wksp_t * wksp = fd_wksp_new_anonymous( fd_cstr_to_shmem_page_sz( _page_sz ), page_cnt, fd_shmem_cpu_idx( numa_idx ), "wksp", 0UL );
   FD_TEST( wksp );
 
-  ulong  node_max = 16;
-  ulong  vote_max = 16;
-  void * mem =
-      fd_wksp_alloc_laddr( wksp, fd_ghost_align(), fd_ghost_footprint( node_max, vote_max ), 1UL );
-  FD_TEST( mem );
-  fd_ghost_t * ghost = fd_ghost_join( fd_ghost_new( mem, node_max, vote_max, 0UL ) );
-  FD_TEST( ghost );
-  FD_TEST( !ghost->root );
-  FD_TEST( ghost->node_pool );
-  FD_TEST( ghost->node_map );
-  FD_TEST( ghost->vote_pool );
-  FD_TEST( ghost->vote_map );
-
-  test_ghost_print(ghost);
-  test_ghost_simple( ghost );
+  test_ghost_print( wksp );
+  test_ghost_simple( wksp );
+  test_ghost_publish_left( wksp );
+  test_ghost_publish_right( wksp );
 
   fd_halt();
   return 0;
