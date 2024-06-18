b6b268e7aea0f8a33b617f59f18dd79cf97cb892
Richard Patel <ripatel@jumptrading.com>
2024-06-18 21:25:56 +0000

    xdp: remove fd_xdp_ctl

--- src/waltz/xdp/Local.mk
+++ src/waltz/xdp/Local.mk
@@ -4,8 +4,6 @@ ifdef FD_HAS_HOSTED
 $(call add-hdrs,fd_xdp_redirect_prog.h)
 $(call add-objs,fd_xsk fd_xsk_aio fd_xdp_redirect_user,fd_waltz)
 
-$(call make-bin,fd_xdp_ctl,fd_xdp_ctl,fd_waltz fd_util)
-
 $(call make-unit-test,test_xsk,test_xsk,fd_waltz fd_util)
 $(call run-unit-test,test_xsk)
 
@@ -13,7 +11,6 @@ $(call make-unit-test,test_xdp_ebpf,test_xdp_ebpf,fd_waltz fd_util)
 $(call run-unit-test,test_xdp_ebpf)
 
 $(call make-unit-test,test_xsk_aio_echo_rx,test_xsk_aio_echo_rx,fd_waltz fd_tango fd_util)
-$(call add-test-scripts,test_xdp_ctl test_xdp_init test_xdp_full test_xdp_fini)
 $(call add-test-scripts,test_xsk_aio_echo_tx)
 endif # FD_HAS_HOSTED
 
