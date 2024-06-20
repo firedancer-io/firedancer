5230b479fc85577037d5624ec48b7bbea9bdbc80
Richard Patel <ripatel@jumptrading.com>
2024-06-20 23:14:07 +0200

    waltz: fd_aio build fixes
    - Removes mentions of non-existent fd_aio API from fd_quic build rules
    - Fix typo that included fd_aio.o in fd_util instead of fd_waltz

--- src/waltz/aio/Local.mk
+++ src/waltz/aio/Local.mk
@@ -1,4 +1,4 @@
 $(call add-hdrs,fd_aio.h fd_aio_pcapng.h)
-$(call add-objs,fd_aio fd_aio_pcapng,fd_util)
+$(call add-objs,fd_aio fd_aio_pcapng,fd_waltz)
 $(call make-unit-test,test_aio,test_aio,fd_waltz fd_util)
 $(call run-unit-test,test_aio)
