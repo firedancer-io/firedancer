fe5481d5b98d37056b5ba30fc97c453e39917b81
Tom Pointon <topointon@jumptrading.com>
2024-06-20 14:24:45 +0000

    Tail log file if test vectors fail

--- contrib/test/run_test_vectors.sh
+++ contrib/test/run_test_vectors.sh
@@ -26,8 +26,16 @@ else
 fi
 
 find dump/test-vectors/instr/fixtures -type f -name '*.fix' -exec ./$OBJDIR/unit-test/test_exec_instr --log-path $LOG_PATH/test_exec_instr --log-level-stderr 4 {} + 
+if [ $? -ne 0 ]; then
+  tail -n 100 $LOG_PATH/test_exec_instr
+  exit 1
+fi;
 zstd -df dump/test-vectors/elf_loader/fixtures/*.zst
 find dump/test-vectors/elf_loader/fixtures -type f -name '*.fix' -exec ./$OBJDIR/unit-test/test_elf_loader --log-path $LOG_PATH/test_elf_loader --log-level-stderr 4 {} + 
+if [ $? -ne 0 ]; then
+  tail -n 100 $LOG_PATH/test_elf_loader
+  exit 1
+fi;
 
 num_exec_instr_tests=`find dump/test-vectors/instr/fixtures -type f -name '*.fix' | wc -l`
 num_elf_tests=`find dump/test-vectors/elf_loader/fixtures -type f -name '*.fix' | wc -l`
