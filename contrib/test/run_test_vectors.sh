e6afef67216c1eca411865d2779038c0aa5cfd0e
Josh Siegel <jsiegel@jumptrading.com>
2024-06-21 13:54:01 +0000

    test_list: address naming feedback

--- contrib/test/run_test_vectors.sh
+++ contrib/test/run_test_vectors.sh
@@ -28,16 +28,16 @@ else
 fi
 
 # find dump/test-vectors/instr/fixtures -type f -name '*.fix' -exec ./$OBJDIR/unit-test/test_exec_instr --log-path $LOG_PATH/test_exec_instr --log-level-stderr 4 {} +
-./$OBJDIR/unit-test/test_exec_instr --log-path $LOG_PATH/test_exec_instr --log-level-stderr 4 `cat contrib/test/test-vectors.list`
+./$OBJDIR/unit-test/test_exec_instr --log-path $LOG_PATH/test_exec_instr --log-level-stderr 4 `cat contrib/test/instr-fixtures.list`
 
 zstd -df dump/test-vectors/elf_loader/fixtures/*.zst
 # find dump/test-vectors/elf_loader/fixtures -type f -name '*.fix' -exec ./$OBJDIR/unit-test/test_elf_loader --log-path $LOG_PATH/test_elf_loader --log-level-stderr 4 {} +
-./$OBJDIR/unit-test/test_elf_loader --log-path $LOG_PATH/test_elf_loader --log-level-stderr 4 `cat contrib/test/elf-vectors.list`
+./$OBJDIR/unit-test/test_elf_loader --log-path $LOG_PATH/test_elf_loader --log-level-stderr 4 `cat contrib/test/elf-loader-fixtures.list`
 
 num_exec_instr_tests_raw=`find dump/test-vectors/instr/fixtures -type f -name '*.fix' | wc -l`
 num_elf_tests_raw=`find dump/test-vectors/elf_loader/fixtures -type f -name '*.fix' | wc -l`
-num_exec_instr_tests="`cat contrib/test/test-vectors.list | wc -l`"
-num_elf_tests="`cat contrib/test/elf-vectors.list | wc -l`"
+num_exec_instr_tests="`cat contrib/test/instr-fixtures.list | wc -l`"
+num_elf_tests="`cat contrib/test/elf-loader-fixtures.list | wc -l`"
 total_tests=$((num_exec_instr_tests + num_elf_tests))
 total_tests_missing=$((num_exec_instr_tests_raw + num_elf_tests_raw - total_tests))
 
