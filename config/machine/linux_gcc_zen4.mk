b3dc1a00b486c2365671335357835b13eaa6139b
nlgripto <155116201+nlgripto@users.noreply.github.com>
2024-06-21 13:53:18 +0000

    Add zen4 machine target.
    Co-authored by: SEJeff <jeffschroeder@computer.org>

---
+++ config/machine/linux_gcc_zen4.mk
@@ -0,0 +1,41 @@
+BUILDDIR?=linux/gcc/zen4
+
+include config/base.mk
+include config/extra/with-security.mk
+include config/extra/with-gcc.mk
+include config/extra/with-x86-64.mk
+include config/extra/with-debug.mk
+include config/extra/with-brutality.mk
+include config/extra/with-optimization.mk
+include config/extra/with-threads.mk
+
+# GCC 8 (Firedancer's minimum supported GCC version) only supports znver1.
+ifeq ($(shell $(CC) -dumpversion),8)
+CPPFLAGS+=-march=znver1 -mtune=znver1
+else
+CPPFLAGS+=-march=znver4 -mtune=znver4
+
+endif
+
+CPPFLAGS+=\
+  -DFD_HAS_INT128=1 \
+  -DFD_HAS_DOUBLE=1 \
+  -DFD_HAS_ALLOCA=1 \
+  -DFD_HAS_X86=1 \
+  -DFD_HAS_SSE=1 \
+  -DFD_HAS_AVX=1 \
+  -DFD_HAS_SHANI=1 \
+  -DFD_HAS_AESNI=1 \
+  -DFD_HAS_AVX512=1 \
+  -DFD_HAS_GFNI=1
+
+FD_HAS_INT128:=1
+FD_HAS_DOUBLE:=1
+FD_HAS_ALLOCA:=1
+FD_HAS_X86:=1
+FD_HAS_SSE:=1
+FD_HAS_AVX:=1
+FD_HAS_SHANI:=1
+FD_HAS_AESNI:=1
+FD_HAS_AVX512:=1
+FD_HAS_GFNI:=1
