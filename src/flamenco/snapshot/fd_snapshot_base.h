3233dc68bc5d7951fa1159286a78595689df68aa
Richard Patel <ripatel@jumptrading.com>
2024-06-23 12:30:59 +0000

    Unbreak linux_gcc_noarch128 build

--- src/flamenco/snapshot/fd_snapshot_base.h
+++ src/flamenco/snapshot/fd_snapshot_base.h
@@ -13,4 +13,34 @@
 
 #define FD_SNAPSHOT_ACC_ALIGN (8UL)
 
+#define FD_SNAPSHOT_TYPE_UNSPECIFIED 0
+#define FD_SNAPSHOT_TYPE_FULL        1
+#define FD_SNAPSHOT_TYPE_INCREMENTAL 2
+
+struct fd_snapshot_name {
+  int       type;
+  ulong     slot;
+  ulong     incremental_slot;
+  fd_hash_t fhash;
+  char      file_ext[ 16 ];
+};
+
+typedef struct fd_snapshot_name fd_snapshot_name_t;
+
+FD_PROTOTYPES_BEGIN
+
+
+fd_snapshot_name_t *
+fd_snapshot_name_from_cstr( fd_snapshot_name_t * id,
+                            char const *         cstr,
+                            ulong                base_slot );
+
+fd_snapshot_name_t *
+fd_snapshot_name_from_buf( fd_snapshot_name_t * id,
+                           char const *         str,
+                           ulong                str_len,
+                           ulong                base_slot );
+
+FD_PROTOTYPES_END
+
 #endif /* HEADER_fd_src_flamenco_snapshot_fd_snapshot_base_h */
