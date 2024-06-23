7c990cb2536bab97e8d65a5de5977f122e78eeed
Richard Patel <ripatel@jumptrading.com>
2024-06-23 09:15:43 +0000

    config: add 'hosted' support for FreeBSD

--- config/machine/freebsd_clang_noarch128.mk
+++ config/machine/freebsd_clang_noarch128.mk
@@ -3,13 +3,11 @@ BUILDDIR:=freebsd/clang/noarch128
 include config/base.mk
 include config/extra/with-clang.mk
 include config/extra/with-debug.mk
-include config/extra/with-brutality.mk
+#include config/extra/with-brutality.mk
 include config/extra/with-optimization.mk
+include config/extra/with-hosted.mk
 
 CPPFLAGS+=-DFD_HAS_INT128=1 -DFD_HAS_DOUBLE=1 -DFD_HAS_ALLOCA=1
-
-# Remove this when we support FD_HAS_HOSTED for FreeBSD
-CPPFLAGS+=-DFD_ENV_STYLE=0 -DFD_IO_STYLE=0 -DFD_LOG_STYLE=0
 
 # Remove this when we support FD_HAS_THREADS for FreeBSD
 CPPFLAGS+=-DFD_HAS_ATOMIC=1
