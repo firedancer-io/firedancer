4cf598ac7763294d27ede4aca247ba593169e198
Richard Patel <ripatel@jumptrading.com>
2024-06-23 10:34:01 +0000

    FreeBSD threads support

--- config/extra/with-threads.mk
+++ config/extra/with-threads.mk
@@ -2,6 +2,9 @@ include config/extra/with-hosted.mk
 
 CPPFLAGS+=-pthread -DFD_HAS_THREADS=1 -DFD_HAS_ATOMIC=1
 LDFLAGS+=-pthread
+ifdef FD_HAS_FREEBSD
+LDFLAGS+=-lstdthreads
+endif
 
 FD_HAS_THREADS:=1
 FD_HAS_ATOMIC:=1
--- config/machine/freebsd_clang_noarch128.mk
+++ config/machine/freebsd_clang_noarch128.mk
@@ -5,17 +5,11 @@ include config/extra/with-clang.mk
 include config/extra/with-debug.mk
 include config/extra/with-brutality.mk
 include config/extra/with-optimization.mk
-include config/extra/with-hosted.mk
+include config/extra/with-threads.mk
 
 CPPFLAGS+=-DFD_HAS_INT128=1 -DFD_HAS_DOUBLE=1 -DFD_HAS_ALLOCA=1
-
-# Remove this when we support FD_HAS_THREADS for FreeBSD
-CPPFLAGS+=-DFD_HAS_ATOMIC=1
-
-LDFLAGS+=-pthread
 
 FD_HAS_INT128:=1
 FD_HAS_DOUBLE:=1
 FD_HAS_ALLOCA:=1
-FD_HAS_ATOMIC:=1
 
