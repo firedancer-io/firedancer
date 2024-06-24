74b8a476dfcf7b2a3580807f1da36a9c05810703
Alex Siegel <asiegel@jumptrading.com>
2024-06-24 20:26:06 +0000

    Use original hostname in http header

--- src/flamenco/snapshot/fuzz_snapshot_http.c
+++ src/flamenco/snapshot/fuzz_snapshot_http.c
@@ -45,7 +45,7 @@ target_task( void * ctx ) {
 
   fd_snapshot_name_t name[1] = {{0}};
   fd_snapshot_http_t  _http[1];
-  fd_snapshot_http_t * http = fd_snapshot_http_new( _http, FD_IP4_ADDR( 127, 0, 0, 1 ), 80, name );
+  fd_snapshot_http_t * http = fd_snapshot_http_new( _http, "localhost:80", FD_IP4_ADDR( 127, 0, 0, 1 ), 80, name );
 
   /* Hijack the HTTP state and make it think there is a successful connection */
   assert( http->socket_fd == -1 );
