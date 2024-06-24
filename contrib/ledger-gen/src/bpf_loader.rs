d7fcbfee580e6eb9962d5b0d37a0e57c75d8cc9d
Kunal Bhargava <kbhargava@jumptrading.com>
2024-06-24 22:25:35 +0000

    contrib: add bpf close program and redeploy behavior

--- contrib/ledger-gen/src/bpf_loader.rs
+++ contrib/ledger-gen/src/bpf_loader.rs
@@ -83,8 +83,6 @@ pub fn example_commands(client: &RpcClient, arc_client: &Arc<RpcClient>, payer: 
 pub fn deploy_invoke_same_slot(client: &RpcClient, arc_client: &Arc<RpcClient>, payer: &Keypair, program_data: &Vec<u8>, account_data: &Vec<u8>) {
     let buffer_account_deploy = instructions::set_up_buffer_account(&arc_client, &payer, &program_data);
 
-    utils::wait_atleast_n_slots(&client, 1);
-
     // Deploy Program
     let (program_account, deploy_program_instructions) = instructions::deploy_program_instructions(&client, &payer, None, &buffer_account_deploy, program_data.len());
     let transaction = utils::create_message_and_sign(&deploy_program_instructions, &payer, vec![&payer, &program_account], client.get_latest_blockhash().unwrap());
@@ -104,8 +102,6 @@ pub fn deploy_invoke_same_slot(client: &RpcClient, arc_client: &Arc<RpcClient>, 
 
 pub fn deploy_invoke_diff_slot(client: &RpcClient, arc_client: &Arc<RpcClient>, payer: &Keypair, program_data: &Vec<u8>, account_data: &Vec<u8>) {
     let buffer_account_deploy = instructions::set_up_buffer_account(&arc_client, &payer, &program_data);
-
-    utils::wait_atleast_n_slots(&client, 1);
 
     // Deploy Program
     let (program_account, deploy_program_instructions) = instructions::deploy_program_instructions(&client, &payer, None, &buffer_account_deploy, program_data.len());
@@ -185,8 +181,6 @@ pub fn upgrade_invoke_diff_slot(client: &RpcClient, arc_client: &Arc<RpcClient>,
 pub fn deploy_close_same_slot(client: &RpcClient, arc_client: &Arc<RpcClient>, payer: &Keypair, program_data: &Vec<u8>, _account_data: &Vec<u8>) {
     let buffer_account_deploy = instructions::set_up_buffer_account(&arc_client, &payer, &program_data);
 
-    utils::wait_atleast_n_slots(&client, 1);
-
     // Deploy Program
     let (program_account, deploy_program_instructions) = instructions::deploy_program_instructions(&client, &payer, None, &buffer_account_deploy, program_data.len());
     let transaction = utils::create_message_and_sign(&deploy_program_instructions, &payer, vec![&payer, &program_account], client.get_latest_blockhash().unwrap());
@@ -204,8 +198,6 @@ pub fn deploy_close_same_slot(client: &RpcClient, arc_client: &Arc<RpcClient>, p
 
 pub fn deploy_close_diff_slot(client: &RpcClient, arc_client: &Arc<RpcClient>, payer: &Keypair, program_data: &Vec<u8>, _account_data: &Vec<u8>) {
     let buffer_account_deploy = instructions::set_up_buffer_account(&arc_client, &payer, &program_data);
-
-    utils::wait_atleast_n_slots(&client, 1);
 
     // Deploy Program
     let (program_account, deploy_program_instructions) = instructions::deploy_program_instructions(&client, &payer, None, &buffer_account_deploy, program_data.len());
@@ -225,15 +217,13 @@ pub fn deploy_close_diff_slot(client: &RpcClient, arc_client: &Arc<RpcClient>, p
 pub fn close_invoke_same_slot(client: &RpcClient, arc_client: &Arc<RpcClient>, payer: &Keypair, program_data: &Vec<u8>, account_data: &Vec<u8>) {
     let buffer_account_deploy = instructions::set_up_buffer_account(&arc_client, &payer, &program_data);
 
-    utils::wait_atleast_n_slots(&client, 1);
-
     // Deploy Program
     let (program_account, deploy_program_instructions) = instructions::deploy_program_instructions(&client, &payer, None, &buffer_account_deploy, program_data.len());
     let transaction = utils::create_message_and_sign(&deploy_program_instructions, &payer, vec![&payer, &program_account], client.get_latest_blockhash().unwrap());
     let _ = client.send_transaction_with_config(&transaction, *utils::SKIP_PREFLIGHT_CONFIG).unwrap();
     println!("Deployed Program Signature: {:?} - Slot: {:?}", transaction.signatures[0], client.get_slot_with_commitment(CommitmentConfig::processed()).unwrap());
 
-    utils::wait_atleast_n_slots(&client, 1);
+    utils::wait_atleast_n_slots(&client, 2);
 
     // Close Program
     let close_program_instructions = instructions::close_program_instructions(&payer, &program_account);
@@ -253,15 +243,13 @@ pub fn close_invoke_same_slot(client: &RpcClient, arc_client: &Arc<RpcClient>, p
 pub fn close_invoke_diff_slot(client: &RpcClient, arc_client: &Arc<RpcClient>, payer: &Keypair, program_data: &Vec<u8>, account_data: &Vec<u8>) {
     let buffer_account_deploy = instructions::set_up_buffer_account(&arc_client, &payer, &program_data);
 
-    utils::wait_atleast_n_slots(&client, 1);
-
     // Deploy Program
     let (program_account, deploy_program_instructions) = instructions::deploy_program_instructions(&client, &payer, None, &buffer_account_deploy, program_data.len());
     let transaction = utils::create_message_and_sign(&deploy_program_instructions, &payer, vec![&payer, &program_account], client.get_latest_blockhash().unwrap());
     let _ = client.send_transaction_with_config(&transaction, *utils::SKIP_PREFLIGHT_CONFIG).unwrap();
     println!("Deployed Program Signature: {:?} - Slot: {:?}", transaction.signatures[0], client.get_slot_with_commitment(CommitmentConfig::processed()).unwrap());
 
-    utils::wait_atleast_n_slots(&client, 1);
+    utils::wait_atleast_n_slots(&client, 2);
 
     // Close Program
     let close_program_instructions = instructions::close_program_instructions(&payer, &program_account);
@@ -276,4 +264,58 @@ pub fn close_invoke_diff_slot(client: &RpcClient, arc_client: &Arc<RpcClient>, p
     let transaction = utils::create_message_and_sign(&invoke_program_instructions, &payer, vec![&payer, &run_account], client.get_latest_blockhash().unwrap());
     let _ = client.send_transaction_with_config(&transaction, *utils::SKIP_PREFLIGHT_CONFIG).unwrap();
     println!("Invoked Program Signature: {:?} - Slot: {:?}", transaction.signatures[0], client.get_slot_with_commitment(CommitmentConfig::processed()).unwrap());
+}
+
+pub fn close_redeploy_same_slot(client: &RpcClient, arc_client: &Arc<RpcClient>, payer: &Keypair, program_data: &Vec<u8>, account_data: &Vec<u8>) {
+    let buffer_account_deploy = instructions::set_up_buffer_account(&arc_client, &payer, &program_data);
+    let buffer_account_redeploy = instructions::set_up_buffer_account(&arc_client, &payer, &program_data);
+
+    // Deploy Program
+    let (program_account, deploy_program_instructions) = instructions::deploy_program_instructions(&client, &payer, None, &buffer_account_deploy, program_data.len());
+    let transaction = utils::create_message_and_sign(&deploy_program_instructions, &payer, vec![&payer, &program_account], client.get_latest_blockhash().unwrap());
+    let _ = client.send_transaction_with_config(&transaction, *utils::SKIP_PREFLIGHT_CONFIG).unwrap();
+    println!("Deployed Program Signature: {:?} - Slot: {:?}", transaction.signatures[0], client.get_slot_with_commitment(CommitmentConfig::processed()).unwrap());
+
+    utils::wait_atleast_n_slots(&client, 2);
+
+    // Close Program
+    let close_program_instructions = instructions::close_program_instructions(&payer, &program_account);
+    let transaction = utils::create_message_and_sign(&close_program_instructions, &payer, vec![&payer], client.get_latest_blockhash().unwrap());
+    let _ = client.send_transaction_with_config(&transaction, *utils::SKIP_PREFLIGHT_CONFIG).unwrap();
+    println!("Closed Program Signature: {:?} - Slot: {:?}", transaction.signatures[0], client.get_slot_with_commitment(CommitmentConfig::processed()).unwrap());
+
+    thread::sleep(Duration::from_millis(250));
+
+    // Redeploy Program
+    let (program_account, deploy_program_instructions) = instructions::deploy_program_instructions(&client, &payer, Some(program_account), &buffer_account_redeploy, program_data.len());
+    let transaction = utils::create_message_and_sign(&deploy_program_instructions, &payer, vec![&payer, &program_account], client.get_latest_blockhash().unwrap());
+    let _ = client.send_transaction_with_config(&transaction, *utils::SKIP_PREFLIGHT_CONFIG).unwrap();
+    println!("Attempted Redeploy Program Signature: {:?} - Slot: {:?}", transaction.signatures[0], client.get_slot_with_commitment(CommitmentConfig::processed()).unwrap());
+}
+
+pub fn close_redeploy_diff_slot(client: &RpcClient, arc_client: &Arc<RpcClient>, payer: &Keypair, program_data: &Vec<u8>, account_data: &Vec<u8>) {
+    let buffer_account_deploy = instructions::set_up_buffer_account(&arc_client, &payer, &program_data);
+    let buffer_account_redeploy = instructions::set_up_buffer_account(&arc_client, &payer, &program_data);
+
+    // Deploy Program
+    let (program_account, deploy_program_instructions) = instructions::deploy_program_instructions(&client, &payer, None, &buffer_account_deploy, program_data.len());
+    let transaction = utils::create_message_and_sign(&deploy_program_instructions, &payer, vec![&payer, &program_account], client.get_latest_blockhash().unwrap());
+    let _ = client.send_transaction_with_config(&transaction, *utils::SKIP_PREFLIGHT_CONFIG).unwrap();
+    println!("Deployed Program Signature: {:?} - Slot: {:?}", transaction.signatures[0], client.get_slot_with_commitment(CommitmentConfig::processed()).unwrap());
+
+    utils::wait_atleast_n_slots(&client, 2);
+
+    // Close Program
+    let close_program_instructions = instructions::close_program_instructions(&payer, &program_account);
+    let transaction = utils::create_message_and_sign(&close_program_instructions, &payer, vec![&payer], client.get_latest_blockhash().unwrap());
+    let _ = client.send_transaction_with_config(&transaction, *utils::SKIP_PREFLIGHT_CONFIG).unwrap();
+    println!("Closed Program Signature: {:?} - Slot: {:?}", transaction.signatures[0], client.get_slot_with_commitment(CommitmentConfig::processed()).unwrap());
+
+    utils::wait_atleast_n_slots(&client, 2);
+
+    // Redeploy Program
+    let (program_account, deploy_program_instructions) = instructions::deploy_program_instructions(&client, &payer, Some(program_account), &buffer_account_redeploy, program_data.len());
+    let transaction = utils::create_message_and_sign(&deploy_program_instructions, &payer, vec![&payer, &program_account], client.get_latest_blockhash().unwrap());
+    let _ = client.send_transaction_with_config(&transaction, *utils::SKIP_PREFLIGHT_CONFIG).unwrap();
+    println!("Attempted Redeploy Program Signature: {:?} - Slot: {:?}", transaction.signatures[0], client.get_slot_with_commitment(CommitmentConfig::processed()).unwrap());
 }
