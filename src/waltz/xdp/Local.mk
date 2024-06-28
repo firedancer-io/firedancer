d682dbaee5431c701d0b07f2dd298463b3061ebd
Richard Patel <ripatel@jumptrading.com>
2024-06-28 22:37:31 +0000

    xdp: remove defunct test_xsk_aio_echo tests
    test_xsk_aio_echo is no longer running in CI.  Because it is no
    longer possible to create a wksp with a fd_xsk_t from a shell
    script, it is also difficult to run this test by hand.

--- src/waltz/xdp/Local.mk
+++ src/waltz/xdp/Local.mk
@@ -10,9 +10,6 @@ $(call run-unit-test,test_xsk)
 
 $(call make-unit-test,test_xdp_ebpf,test_xdp_ebpf,fd_waltz fd_util)
 $(call run-unit-test,test_xdp_ebpf)
-
-$(call make-unit-test,test_xsk_aio_echo_rx,test_xsk_aio_echo_rx,fd_waltz fd_tango fd_util)
-$(call add-test-scripts,test_xsk_aio_echo_tx)
 endif # FD_HAS_LINUX
 endif # FD_HAS_HOSTED
 
