11f095cecfb47891b59865056fec342552e64e3e
Richard Patel <ripatel@jumptrading.com>
2024-07-03 10:28:24 +0000

    xdp: add test_xsk_rxdrop
    test_xsk_drop is a test app that sets up a multi-queue AF_XDP
    configuration.  It immediately drops incoming traffic and counts
    the number of packets received.  This tool is mostly useful for
    benchmarking.

--- src/waltz/xdp/Local.mk
+++ src/waltz/xdp/Local.mk
@@ -10,6 +10,8 @@ $(call run-unit-test,test_xsk)
 
 $(call make-unit-test,test_xdp_ebpf,test_xdp_ebpf,fd_waltz fd_util)
 $(call run-unit-test,test_xdp_ebpf)
+
+$(call make-unit-test,test_xsk_rxdrop,test_xsk_rxdrop,fd_waltz fd_tango fd_util)
 endif # FD_HAS_LINUX
 endif # FD_HAS_HOSTED
 
