f1968843f34dd7fc0b26bbfb5ed1c89685e7b8b2
Emanuele Cesena <ecesena@jumptrading.com>
2024-07-08 18:59:34 +0000

    zk-sdk: close_context_state

--- src/flamenco/runtime/program/zksdk/Local.mk
+++ src/flamenco/runtime/program/zksdk/Local.mk
@@ -1,5 +1,5 @@
 ifdef FD_HAS_INT128
 $(call add-hdrs,fd_zksdk.h)
 $(call add-objs,fd_zksdk,fd_flamenco)
-$(call make-unit-test,test_zksdk,test_zksdk,fd_flamenco fd_ballet fd_util)
+$(call make-unit-test,test_zksdk,test_zksdk,fd_flamenco fd_funk fd_ballet fd_util)
 endif
