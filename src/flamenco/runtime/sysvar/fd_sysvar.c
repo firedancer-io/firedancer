03df9441f015ff5683f012bfec848862bd8426bd
Tom Pointon <topointon@jumptrading.com>
2024-07-12 12:47:47 +0000

    flamenco, sysvar: properly handle the case where sysvar data size changes

--- src/flamenco/runtime/sysvar/fd_sysvar.c
+++ src/flamenco/runtime/sysvar/fd_sysvar.c
@@ -3,6 +3,7 @@
 #include "../context/fd_exec_slot_ctx.h"
 #include "fd_sysvar_rent.h"
 
+/* https://github.com/anza-xyz/agave/blob/cbc8320d35358da14d79ebcada4dfb6756ffac79/runtime/src/bank.rs#L1813 */
 int
 fd_sysvar_set( fd_exec_slot_ctx_t * slot_ctx,
                uchar const *        owner,
@@ -22,19 +23,19 @@ fd_sysvar_set( fd_exec_slot_ctx_t * slot_ctx,
     return FD_ACC_MGR_ERR_READ_FAILED;
 
   fd_memcpy(rec->data, data, sz);
-  // What is the correct behavior here?  Where is this code in the
-  // solana code base?  Do I only adjust the lamports if the data
-  // increases but not decreases?  I am inventing money here...
+  
+  /* https://github.com/anza-xyz/agave/blob/cbc8320d35358da14d79ebcada4dfb6756ffac79/runtime/src/bank.rs#L1825 */
   fd_acc_lamports_t lamports_before = rec->meta->info.lamports;
   fd_epoch_bank_t * epoch_bank = fd_exec_epoch_ctx_epoch_bank( slot_ctx->epoch_ctx );
-  rec->meta->info.lamports = (lamports == 0UL) ? fd_rent_exempt_minimum_balance2( &epoch_bank->rent, sz ) : lamports;
-  slot_ctx->slot_bank.capitalization = fd_ulong_sat_sub(
-      fd_ulong_sat_add(
-        slot_ctx->slot_bank.capitalization,
-        rec->meta->info.lamports),
-      lamports_before);
-  // FD_LOG_DEBUG(("fd_sysvar_set: capitalization={%lu} increased by lamports: %lu for pubkey %32J", slot_ctx->slot_bank.capitalization, (rec->meta->info.lamports - lamports_before), pubkey));
+  fd_acc_lamports_t lamports_after = fd_ulong_max( lamports, fd_rent_exempt_minimum_balance2( &epoch_bank->rent, sz ) );
+  rec->meta->info.lamports = lamports_after;
 
+  /* https://github.com/anza-xyz/agave/blob/cbc8320d35358da14d79ebcada4dfb6756ffac79/runtime/src/bank.rs#L1826 */
+  if ( lamports_after > lamports_before ) {
+    slot_ctx->slot_bank.capitalization += ( lamports_after - lamports_before );
+  } else if ( lamports_before < lamports_after ) {
+    slot_ctx->slot_bank.capitalization -= ( lamports_before - lamports_after );
+  }
 
   rec->meta->dlen = sz;
   fd_memcpy(rec->meta->info.owner, owner, 32);
