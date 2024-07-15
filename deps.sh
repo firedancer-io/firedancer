7b2ca17c7721885bd0df4635ac708a02a626b3d1
ripatel-fd <ripatel+git@jumptrading.com>
2024-07-15 15:25:07 +0200

    deps.sh: OpenSSL 3.3.1

--- deps.sh
+++ deps.sh
@@ -105,7 +105,7 @@ fetch () {
   checkout_repo zstd      https://github.com/facebook/zstd          "v1.5.5"
   checkout_repo lz4       https://github.com/lz4/lz4                "v1.9.4"
   checkout_repo secp256k1 https://github.com/bitcoin-core/secp256k1 "v0.5.0"
-  #checkout_repo openssl   https://github.com/openssl/openssl        "openssl-3.3.0"
+  #checkout_repo openssl   https://github.com/openssl/openssl        "openssl-3.3.1"
   if [[ $DEVMODE == 1 ]]; then
     checkout_repo zlib      https://github.com/madler/zlib            "v1.2.13"
     checkout_repo rocksdb   https://github.com/facebook/rocksdb       "v9.2.1"
