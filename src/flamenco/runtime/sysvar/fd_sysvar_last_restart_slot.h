6a238083596f66961b50f8ee9c08930c69a97921
Josh Siegel <jsiegel@jumptrading.com>
2024-07-15 14:03:37 +0000

    last_restart_slot_errs: fd_sysvar_last_restart_slot_read needs to cleanly log a error and exit

--- src/flamenco/runtime/sysvar/fd_sysvar_last_restart_slot.h
+++ src/flamenco/runtime/sysvar/fd_sysvar_last_restart_slot.h
@@ -13,19 +13,6 @@ FD_PROTOTYPES_BEGIN
 void
 fd_sysvar_last_restart_slot_init( fd_exec_slot_ctx_t * slot_ctx );
 
-/* fd_sysvar_last_restart_slot_read queries the sysvar cache (?) for the
-   slot number at which the last hard fork occurred.  This matches the
-   highest slot number in the bank's "hard forks" list that is not in
-   the future.  On success, returns 0 and writes *result.  On failure,
-   returns an FD_ACC_MGR error code.  Reasons for error include that the
-   "last restart slot sysvar" feature is not yet activated
-   (FD_ACC_MGR_ERR_UNKNOWN_ACCOUNT), or that there is a critical runtime
-   error. */
-
-fd_sol_sysvar_last_restart_slot_t *
-fd_sysvar_last_restart_slot_read( fd_sol_sysvar_last_restart_slot_t * result,
-                                  fd_exec_slot_ctx_t const *          slot_ctx );
-
 /* fd_sysvar_last_restart_slot_update performs a sysvar update before
    transaction processing.  TODO not completely implemented. */
 
@@ -35,4 +22,3 @@ fd_sysvar_last_restart_slot_update( fd_exec_slot_ctx_t * slot_ctx );
 FD_PROTOTYPES_END
 
 #endif /* HEADER_fd_src_flamenco_runtime_fd_sysvar_last_restart_slot_h */
-
