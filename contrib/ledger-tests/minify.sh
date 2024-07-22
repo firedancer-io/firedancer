6854e66225e4b356fad9a860fb4f2ae7e2112d60
Alex Peng <alpeng@jumptrading.com>
2024-07-22 07:27:49 +0000

    contrib: enable minimized ledgers

--- contrib/ledger-tests/minify.sh
+++ contrib/ledger-tests/minify.sh
@@ -62,15 +62,11 @@ minimize_snapshot_frank() {
     exit 1
   fi
 
-  # Extend rocksdb to 10 slots after the mismatch
-  end_slot=$((end_slot + 10))
-  if [ $end_slot -gt $minf_rocksdb_max ]; then
-    end_slot=$minf_rocksdb_max
-  fi
-
   set -x
+  
   "$minf_fd_frank_minimize_tool" --cmd minify --rocksdb "$in_dir/rocksdb" --minified-rocksdb "$out_dir/rocksdb" --start-slot $start_slot --end-slot $end_slot --page-cnt $GIGANTIC_PAGES --copy-txn-status 1
   set +x
+  
   cd "$in_dir" || exit
   cp gen* "$out_dir"
 
@@ -105,6 +101,9 @@ minimize_snapshot_solana() {
   rm -rf "$out_dir"
   cd "$in_dir" || exit
 
+  rm -rf ledger_tool
+  "$SOLANA_LEDGER_TOOL" create-snapshot -l . $start_slot --minimized --ending-slot $end_slot
+  
   rm -rf ledger_tool
   minimize_snapshot_frank $in_dir $out_dir $start_slot $end_slot
 }
