0571f1a5f95cd2ad963eb5b7ff1299165191aea9
Manik Jain <mjain@jumptrading.com>
2024-07-24 19:25:49 +0000

    Scripts for building and running conformance and conformance targets

---
+++ contrib/conformance/debug.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+set -ex
+
+# Usage: ./run_tests.sh -t <agave/fd> -i <input_file>
+
+OBJDIR=${OBJDIR:-build/native/gcc}
+
+INPUT_FILE=""
+TARGET="fd"
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -i|--input)
+       INPUT_FILE="-i $2"
+       shift
+       shift
+       ;;
+    -t|--target)
+       TARGET="$2"
+       shift
+       shift
+       ;;
+    -*|--*)
+       echo "unknown option $1"
+       exit 1
+       ;;
+    *)
+       POSITION_ARGS+=("$1")
+       shift
+       ;;
+  esac
+done
+
+TARGET_SHARED_OBJ_FILE=""
+case $TARGET in
+    agave)
+        TARGET_SHARED_OBJ_FILE="-t dump/solfuzz-agave/target/debug/libsolfuzz_agave.so"
+        DEBUGGER="rust-gdb"
+        ;;
+    fd)
+        TARGET_SHARED_OBJ_FILE="-t $OBJDIR/lib/libfd_exec_sol_compat.so"
+        DEBUGGER="gdb"
+        ;;
+    *)
+        echo "unknown target $TARGET"
+        exit 1
+        ;;
+esac
+
+# Build / update solfuzz-agave and solana-conformance
+./contrib/conformance/build.sh
+
+source dump/solana-conformance/test_suite_env/bin/activate
+HARNESS_TYPE="TxnHarness" $DEBUGGER --args python3.11 -m test_suite.test_suite exec-instr $TARGET_SHARED_OBJ_FILE $INPUT_FILE
---
+++ contrib/conformance/run_tests.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -ex
+
+# Usage: ./run_tests.sh -i <input-dir> -p <num-processes>
+
+OBJDIR=${OBJDIR:-build/native/gcc}
+
+INPUT_DIR=""
+NUM_PROCESSES=""
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -i|--input-dir)
+       INPUT_DIR="-i $2"
+       shift
+       shift
+       ;;
+    -p|--num-processes)
+       NUM_PROCESSES="-p $2"
+       shift
+       shift
+       ;;
+    -*|--*)
+       echo "unknown option $1"
+       exit 1
+       ;;
+    *)
+       POSITION_ARGS+=("$1")
+       shift
+       ;;
+  esac
+done
+
+# Build / update solfuzz-agave and solana-conformance
+./contrib/conformance/build.sh
+
+source dump/solana-conformance/test_suite_env/bin/activate
+HARNESS_TYPE="TxnHarness" solana-test-suite run-tests -s dump/solfuzz-agave/target/debug/libsolfuzz_agave.so -t $OBJDIR/lib/libfd_exec_sol_compat.so -f $INPUT_DIR $NUM_PROCESSES
