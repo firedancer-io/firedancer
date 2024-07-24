b9357466f2de297856fd60a511e8be5491c5ae9c
Kunal Bhargava <kbhargava@jumptrading.com>
2024-07-24 18:15:32 +0000

    ledger-gen: make run_cluster more configurable

--- contrib/ledger-gen/run_cluster.py
+++ contrib/ledger-gen/run_cluster.py
@@ -41,9 +41,9 @@ def parse_genesis_output(output):
 
     return genesis_hash, shred_version
 
-async def run_genesis(output_dir, solana_source_directory):
+async def run_genesis(output_dir, solana_source_directory, tick_duration):
     process = await asyncio.create_subprocess_shell(
-        f"{solana_binary('solana-genesis', solana_source_directory)} --cluster-type mainnet-beta --ledger node-ledger-0 --bootstrap-validator 'keys-0/id.json' 'keys-0/vote.json' 'keys-0/stake.json' --bootstrap-stake-authorized-pubkey 'keys-0/id.json' --bootstrap-validator-lamports 10000000000 --bootstrap-validator-stake-lamports 32282880 --faucet-pubkey 'faucet.json' --faucet-lamports 5000000000000000 --hashes-per-tick sleep --target-tick-duration 100000 --slots-per-epoch 128",
+        f"{solana_binary('solana-genesis', solana_source_directory)} --cluster-type mainnet-beta --ledger node-ledger-0 --bootstrap-validator 'keys-0/id.json' 'keys-0/vote.json' 'keys-0/stake.json' --bootstrap-stake-authorized-pubkey 'keys-0/id.json' --bootstrap-validator-lamports 10000000000 --bootstrap-validator-stake-lamports 32282880 --faucet-pubkey 'faucet.json' --faucet-lamports 5000000000000000 --hashes-per-tick sleep --target-tick-duration {tick_duration} --slots-per-epoch 128",
          stdout=asyncio.subprocess.PIPE,
          stderr=asyncio.subprocess.PIPE,
          cwd=output_dir)
@@ -75,7 +75,7 @@ async def get_pubkey(vote_key, solana_source_directory):
     return stdout.decode().strip()
 
 @asynccontextmanager
-async def first_cluster_validator(expected_shred_version, expected_genesis_hash, solana_source_directory, output_dir):
+async def first_cluster_validator(expected_shred_version, expected_genesis_hash, solana_source_directory, output_dir, snapshot_interval, snapshots_to_retain):
     ledger_path = os.path.join(output_dir, "node-ledger-0")
     identity_key = os.path.join(output_dir, "keys-0", "id.json")
     vote_key = os.path.join(output_dir, "keys-0", "vote.json")
@@ -83,7 +83,7 @@ async def first_cluster_validator(expected_shred_version, expected_genesis_hash,
     vote_pubkey = await get_pubkey(vote_key, solana_source_directory)
 
     process = await asyncio.create_subprocess_shell(
-        f"{solana_binary('agave-validator', solana_source_directory)} --allow-private-addr --identity {identity_key}  --ledger {ledger_path} --limit-ledger-size 100000000 --dynamic-port-range 8000-8100 --no-snapshot-fetch --no-poh-speed-test --no-os-network-limits-test --vote-account {vote_pubkey} --expected-shred-version {expected_shred_version} --expected-genesis-hash {expected_genesis_hash} --no-wait-for-vote-to-start-leader --incremental-snapshots --full-snapshot-interval-slots 10 --incremental-snapshot-interval-slots 5 ----maximum-full-snapshots-to-retain 20 --rpc-port 8899 --gossip-port 8010 --full-rpc-api --tpu-enable-udp --log {ledger_path}/validator.log",
+        f"{solana_binary('agave-validator', solana_source_directory)} --enable-rpc-transaction-history --allow-private-addr --identity {identity_key}  --ledger {ledger_path} --limit-ledger-size 100000000 --dynamic-port-range 8000-8100 --no-snapshot-fetch --no-poh-speed-test --no-os-network-limits-test --vote-account {vote_pubkey} --expected-shred-version {expected_shred_version} --expected-genesis-hash {expected_genesis_hash} --no-wait-for-vote-to-start-leader --incremental-snapshots --full-snapshot-interval-slots {snapshot_interval} --incremental-snapshot-interval-slots {snapshot_interval // 2} --maximum-full-snapshots-to-retain {snapshots_to_retain} --rpc-port 8899 --gossip-port 8010 --full-rpc-api --tpu-enable-udp --log {ledger_path}/validator.log",
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE
     )
@@ -140,11 +140,11 @@ async def solana_cluster_validators(count, expected_shred_version, expected_gene
             await process.wait()
 
 @asynccontextmanager
-async def spawn_solana_cluster(nodes, output_dir, solana_source_directory):
+async def spawn_solana_cluster(nodes, output_dir, solana_source_directory, tick_duration, snapshot_interval, snapshots_to_retain):
     await generate_cluster_keys(nodes, output_dir, solana_source_directory)
-    genesis_hash, shred_version = await run_genesis(output_dir, solana_source_directory)
+    genesis_hash, shred_version = await run_genesis(output_dir, solana_source_directory, tick_duration)
 
-    async with first_cluster_validator(shred_version, genesis_hash, solana_source_directory, output_dir):
+    async with first_cluster_validator(shred_version, genesis_hash, solana_source_directory, output_dir, snapshot_interval, snapshots_to_retain):
 
         # Wait for the first validator to be ready
         print("Waiting for the first validator to be ready", flush=True)
@@ -171,7 +171,7 @@ async def spawn_solana_cluster(nodes, output_dir, solana_source_directory):
             await shell(f"{solana_binary('solana', solana_source_directory)} -ul create-vote-account -k {id_key} --allow-unsafe-authorized-withdrawer {vote_key} {id_key} {id_key}")
 
         # Wait for the output_directory/node_ledger_0/snapshot/200/state_complete file to exist
-        print("Waiting for the first validator to create a snapshot at slot 10", flush=True)
+        print(f"Waiting for the first validator to create a snapshot at slot {snapshot_interval}", flush=True)
         while True:
             process = await asyncio.create_subprocess_shell(
                 f"{solana_binary('solana', solana_source_directory)} -ul validators",
@@ -180,7 +180,7 @@ async def spawn_solana_cluster(nodes, output_dir, solana_source_directory):
             )
             stdout, _ = await process.communicate()
 
-            if os.path.exists(os.path.join(output_dir, "node-ledger-0", "snapshot", "10", "state_complete")):
+            if os.path.exists(os.path.join(output_dir, "node-ledger-0", "snapshot", str(snapshot_interval), "state_complete")):
                 break
             await asyncio.sleep(1)
 
@@ -215,11 +215,11 @@ async def spawn_solana_test_validator(solana_source_directory, output_dir):
         await process.wait()
 
 @asynccontextmanager
-async def solana(cluster_nodes, output_dir, solana_source_directory, skip_build_solana):
+async def solana(cluster_nodes, output_dir, solana_source_directory, skip_build_solana, tick_duration, snapshot_interval, snapshots_to_retain):
     if skip_build_solana:
         await build_solana(solana_source_directory)
     try:
-        async with spawn_solana_cluster(cluster_nodes, output_dir, solana_source_directory):
+        async with spawn_solana_cluster(cluster_nodes, output_dir, solana_source_directory, tick_duration, snapshot_interval, snapshots_to_retain):
             yield
     finally:
         pass
@@ -234,12 +234,15 @@ async def main():
     parser.add_argument("--solana-source-directory", required=True, type=str, help="Absolute path to the Solana checkout")
     parser.add_argument("--skip-build-solana", action='store_false', help="Skip solana build")
     parser.add_argument("--solana-cluster-nodes", required=False, type=int, help="Number of nodes to use for the multi-node Solana cluster")
+    parser.add_argument("--tick-duration", required=False, type=int, help="Duration of ticks", default=100000)
+    parser.add_argument("--snapshot-interval", required=False, type=int, help="Interval between snapshots", default=10)
+    parser.add_argument("--snapshots-to-retain", required=False, type=int, help="Number of snapshots to retain", default=20)
     parser.add_argument("--output-dir", required=True, type=str, help="Output directory where validator keys and ledgers are written to")
     args = parser.parse_args()
 
     clean(args.output_dir)
 
-    async with solana(args.solana_cluster_nodes, args.output_dir, args.solana_source_directory, args.skip_build_solana):
+    async with solana(args.solana_cluster_nodes, args.output_dir, args.solana_source_directory, args.skip_build_solana, args.tick_duration, args.snapshot_interval, args.snapshots_to_retain):
         while True:
             await asyncio.sleep(1)
 
