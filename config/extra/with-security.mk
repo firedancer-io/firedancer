fad19792ef18183de18f969550726aa04c67aa29
Richard Patel <ripatel@jumptrading.com>
2024-08-01 09:24:19 +0000

    config: enable -z relro
    The relro link flag protects more memory regions as read-only post
    startup.  Makes binary sizes and startup times slightly worse.
    
    Closes https://github.com/firedancer-io/auditor-runtimeverification/issues/54

--- config/extra/with-security.mk
+++ config/extra/with-security.mk
@@ -1,2 +1,13 @@
-CPPFLAGS+=-D_FORTIFY_SOURCE=2 -fPIC -Wl,-z,now -fstack-protector-strong
+CPPFLAGS+=-fPIC
 LDFLAGS+=-fPIC
+
+CPPFLAGS+=-Wl,-z,relro,-z,now
+LDFLAGS+=-Wl,-z,relro,-z,now
+
+CPPFLAGS+=-fstack-protector-strong
+LDFLAGS+=-fstack-protector-strong
+
+# _FORTIFY_SOURCE only works when optimization is enabled
+ifeq ($(FD_DISABLE_OPTIMIZATION),)
+CPPFLAGS+=-D_FORTIFY_SOURCE=2
+endif
