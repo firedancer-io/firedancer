45c53fe8975d1faafeb8f7630bf6b69cb3200fa7
Michael McGee <mmcgee@jumptrading.com>
2024-08-06 20:17:00 +0000

    http: add http server

---
+++ src/ballet/http/fd_sha1.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_fd_src_ballet_http_fd_sha1_h
+#define HEADER_fd_src_ballet_http_fd_sha1_h
+
+#include "../fd_ballet_base.h"
+
+/* fd_sha1_hash computes the SHA1 hash of the input data provided and
+   writes the result into the hash buffer.  The output buffer must be at
+   least 20 bytes.  Returns the output buffer. */
+
+uchar *
+fd_sha1_hash( uchar const * data,
+              ulong         data_len,
+              uchar *       hash );
+
+#endif /* HEADER_fd_src_ballet_http_fd_sha1_h */
