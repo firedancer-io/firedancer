4315f4afb10e36588e5d0139b176496d8ed9a731
Alex Siegel <asiegel@jumptrading.com>
2024-08-08 13:32:34 +0000

    asiegel/new-http: converting to new http service

--- src/app/rpcserver/fd_webserver.h
+++ src/app/rpcserver/fd_webserver.h
@@ -2,12 +2,16 @@
 #define HEADER_fd_src_tango_webserver_fd_webserver_h
 
 #include "fd_methods.h"
-#include "../../util/textstream/fd_textstream.h"
 #include "../../ballet/http/fd_http_server.h"
+#include "../../ballet/http/fd_hcache.h"
 
 struct fd_webserver {
   fd_http_server_t * server;
-  void * cb_arg;
+  fd_hcache_t *      hcache;
+  void *             cb_arg;
+#define FD_WEBSERVER_QUICK_MAX 1U<<16U
+  ulong              quick_len;
+  char               quick_buf[FD_WEBSERVER_QUICK_MAX];
 };
 typedef struct fd_webserver fd_webserver_t;
 
@@ -23,24 +27,43 @@ void fd_webserver_poll(fd_webserver_t * ws);
 long fd_webserver_json_keyword(const char* keyw, size_t keyw_sz);
 const char* un_fd_webserver_json_keyword(long id);
 
-struct fd_web_replier;
-void fd_webserver_method_generic(struct fd_web_replier* replier, struct json_values* values, void * cb_arg);
-
-fd_textstream_t * fd_web_replier_textstream(struct fd_web_replier* replier);
-void fd_web_replier_done(struct fd_web_replier* replier);
+void fd_webserver_method_generic(struct json_values* values, void * cb_arg);
 
 int fd_webserver_ws_subscribe(struct json_values* values, ulong conn_id, void * cb_arg);
 
 void fd_webserver_ws_closed(ulong conn_id, void * cb_arg);
 
-void fd_web_ws_reply( fd_webserver_t * ws, ulong conn_id, fd_textstream_t * ts);
+void fd_web_ws_send( fd_webserver_t * ws, ulong conn_id );
 
-void fd_web_replier_error( struct fd_web_replier* replier, const char* format, ... )
+void fd_web_error( fd_webserver_t * ws, const char* format, ... )
   __attribute__ ((format (printf, 2, 3)));
-void fd_web_replier_simple_error( struct fd_web_replier* replier, const char* text, uint text_size);
+void fd_web_simple_error( fd_webserver_t * ws, const char* text, uint text_size );
 
 void fd_web_ws_error( fd_webserver_t * ws, ulong conn_id, const char* format, ... )
   __attribute__ ((format (printf, 3, 4)));
-void fd_web_ws_simple_error( fd_webserver_t * ws, ulong conn_id, const char* text, uint text_size);
+void fd_web_ws_simple_error( fd_webserver_t * ws, ulong conn_id, const char* text, uint text_size );
+
+int fd_web_reply_append( fd_webserver_t * ws,
+                         const char *     text,
+                         ulong            text_sz );
+
+int fd_web_reply_encode_utf8( fd_webserver_t * ws,
+                              const uint *     chars,
+                              ulong            chars_sz );
+
+int fd_web_reply_encode_base58( fd_webserver_t * ws,
+                                const void *     data,
+                                ulong            data_sz );
+
+int fd_web_reply_encode_base64( fd_webserver_t * ws,
+                                const void *     data,
+                                ulong            data_sz );
+
+int fd_web_reply_encode_hex( fd_webserver_t * ws,
+                             const void *     data,
+                             ulong            data_sz );
+
+int fd_web_reply_sprintf( fd_webserver_t * ws, const char* format, ... )
+  __attribute__ ((format (printf, 2, 3)));
 
 #endif /* HEADER_fd_src_tango_webserver_fd_webserver_h */
