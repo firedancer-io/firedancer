9d1287ab56d49e4d65224ee0885bb52095f4eb31
Liam Heeger <lheeger@jumptrading.com>
2024-08-09 21:40:06 +0000

    fix: add pack blackholes

--- src/app/fdctl/run/topos/fd_firedancer.c
+++ src/app/fdctl/run/topos/fd_firedancer.c
@@ -77,6 +77,7 @@ fd_topo_firedancer( config_t * _config ) {
   fd_topob_wksp( topo, "voter_gossip" );
   fd_topob_wksp( topo, "voter_dedup"  );
   fd_topob_wksp( topo, "poh_replay"   );
+  fd_topob_wksp( topo, "bank_poh"   );
 
   fd_topob_wksp( topo, "net"        );
   fd_topob_wksp( topo, "quic"       );
@@ -146,6 +147,7 @@ fd_topo_firedancer( config_t * _config ) {
   /**/                 fd_topob_link( topo, "pack_replay",  "pack_replay",  0,        65536UL,                                  USHORT_MAX,                    1UL   );
   /**/                 fd_topob_link( topo, "poh_pack",     "replay_poh",   0,        128UL,                                    sizeof(fd_became_leader_t) ,   1UL   );
   /**/                 fd_topob_link( topo, "poh_replay",   "poh_replay",   0,        128UL,                                    USHORT_MAX,                    1UL   ); /* TODO: not properly sized yet */
+  /**/                 fd_topob_link( topo, "bank_poh",   "bank_poh",   0,        128UL,                                    USHORT_MAX,                    1UL   ); /* TODO: not properly sized yet */
 
   /**/                 fd_topob_link( topo, "replay_voter", "replay_voter", 0,        128UL,                                    FD_TPU_DCACHE_MTU,             1UL   );
   /**/                 fd_topob_link( topo, "voter_gossip", "voter_gossip", 0,        128UL,                                    FD_TXN_MTU,                    1UL   );
@@ -366,6 +368,9 @@ fd_topo_firedancer( config_t * _config ) {
   /**/                 fd_topob_tile_out( topo, "repair",   0UL,                       "repair_sign",  0UL                                                  );
   /**/                 fd_topob_tile_in(  topo, "repair",   0UL,          "metric_in", "sign_repair",  0UL,          FD_TOPOB_UNRELIABLE, FD_TOPOB_UNPOLLED );
   /**/                 fd_topob_tile_out( topo, "sign",     0UL,                       "sign_repair",  0UL                                                  );
+
+  /**/                 fd_topob_tile_out( topo, "bhole",  0UL,                        "bank_poh",    0UL                                                  );
+  /**/                 fd_topob_tile_in(  topo, "pack",   0UL,           "metric_in",  "bank_poh",      0UL,          FD_TOPOB_UNRELIABLE, FD_TOPOB_POLLED );
 
 
   /* Hacky: Reserve a ulong to allow net0 to pass its PID to its neighbors */
