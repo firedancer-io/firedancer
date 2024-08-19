79ae1cc94419126cf1a8f49701477e6f265096ad
nbridge-jump <123656380+nbridge-jump@users.noreply.github.com>
2024-08-19 22:53:18 +0000

    qos - move qos into waltz library

--- src/waltz/qos/Local.mk
+++ src/waltz/qos/Local.mk
@@ -1,4 +1,3 @@
-$(call make-lib,fd_qos)
-$(call add-objs,fd_qos fd_qos_local fd_qos_entry,fd_qos)
-$(call make-unit-test,test_qos,test_qos,fd_qos fd_tls fd_ballet fd_waltz fd_util)
-$(call make-unit-test,test_qos_vm,test_qos_vm,fd_qos fd_tls fd_ballet fd_waltz fd_util)
+$(call add-objs,fd_qos fd_qos_local fd_qos_entry,fd_waltz)
+$(call make-unit-test,test_qos,test_qos,fd_tls fd_ballet fd_waltz fd_util)
+$(call make-unit-test,test_qos_vm,test_qos_vm,fd_tls fd_ballet fd_waltz fd_util)
