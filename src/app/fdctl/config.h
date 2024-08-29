d517651d7dcf9e8cd205dde400320bab06dc491b
Philip Taffet <phtaffet@jumptrading.com>
2024-08-29 20:52:27 +0000

    bench: add option to disable status cache for benchmarking

--- src/app/fdctl/config.h
+++ src/app/fdctl/config.h
@@ -178,6 +178,7 @@ typedef struct {
       int  larger_max_cost_per_block;
       int  larger_shred_limits_per_block;
       int  disable_blockstore;
+      int  disable_status_cache;
     } bench;
   } development;
 
