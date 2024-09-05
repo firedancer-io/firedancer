839dcbb5c66da4690a81d0cbb2acfd09a066c7cd
Kunal Bhargava <kbhargava@jumptrading.com>
2024-09-05 15:51:00 +0000

    Revert "ballet: move sh_type check"
    This reverts commit 3986ea04530309bba98ae168c39a54b0d0af7041.

--- src/ballet/sbpf/fd_sbpf_loader.c
+++ src/ballet/sbpf/fd_sbpf_loader.c
@@ -735,16 +735,21 @@ fd_sbpf_load_dynamic( fd_sbpf_loader_t *         loader,
     for( ulong i=0; i<elf->ehdr.e_shnum; i++ ) {
       if( shdrs[ i ].sh_addr == loader->dt_symtab ) {
         /* TODO: verify this ... */
-        /* Check section type */
         uint sh_type = shdrs[ i ].sh_type;
-        // https://github.com/solana-labs/rbpf/blob/v0.8.5/src/elf_parser/mod.rs#L500
-        REQUIRE( (sh_type==FD_ELF_SHT_SYMTAB) | (sh_type==FD_ELF_SHT_DYNSYM) );
+        if( !( (sh_type==FD_ELF_SHT_SYMTAB) | (sh_type==FD_ELF_SHT_DYNSYM) ) ) {
+          continue;
+        }
 
         shdr_dynsym = &shdrs[ i ];
         break;
       }
     }
     REQUIRE( shdr_dynsym );
+
+    /* Check section type */
+
+    uint sh_type = shdr_dynsym->sh_type;
+    REQUIRE( (sh_type==FD_ELF_SHT_SYMTAB) | (sh_type==FD_ELF_SHT_DYNSYM) );
 
     /* Check if out of bounds or misaligned */
 
