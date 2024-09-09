eaed6b3d0ffd4f2886729ec0b7e72bab10ccfa1d
Emanuele Cesena <ecesena@jumptrading.com>
2024-09-09 10:50:37 -0500

    syscalls: add logs to runtime syscalls

--- src/flamenco/vm/syscall/fd_vm_syscall_runtime.c
+++ src/flamenco/vm/syscall/fd_vm_syscall_runtime.c
@@ -192,17 +192,6 @@ fd_vm_syscall_sol_get_stack_height( /**/            void *  _vm,
   return FD_VM_SUCCESS;
 }
 
-/* FIXME: PREFIX? */
-/* FIXME: BRANCHLESS? */
-/* FIXME: MAKE MORE BROADLY AVAILABLE, REPLACE WITH IMPL IN MEMCPY TOO? */
-
-static inline int
-is_nonoverlapping( ulong src, ulong src_sz,
-                   ulong dst, ulong dst_sz ) {
-  if( src>dst ) return (src-dst)>=dst_sz;
-  else          return (dst-src)>=src_sz;
-}
-
 int
 fd_vm_syscall_sol_get_return_data( /**/            void *  _vm,
                                    /**/            ulong   dst_vaddr,
@@ -211,13 +200,10 @@ fd_vm_syscall_sol_get_return_data( /**/            void *  _vm,
                                    FD_PARAM_UNUSED ulong   r4,
                                    FD_PARAM_UNUSED ulong   r5,
                                    /**/            ulong * _ret ) {
+  /* https://github.com/anza-xyz/agave/blob/v2.0.8/programs/bpf_loader/src/syscalls/mod.rs#L1345 */
   fd_vm_t * vm = (fd_vm_t *)_vm;
 
-  /* FIXME: The original version didn't have the same crashing FD_TEST
-     all the others had to tell if the vm was attached to an instruction
-     context.  If the VM is being run outside the Solana runtime, it
-     should never invoke this syscall in the first place.  So we treat
-     this as a SIGCALL in a non-crashing way for the time being. */
+  FD_VM_CU_UPDATE( vm, FD_VM_SYSCALL_BASE_COST );
 
   fd_exec_instr_ctx_t const * instr_ctx = vm->instr_ctx;
   if( FD_UNLIKELY( !instr_ctx ) ) return FD_VM_ERR_SIGCALL;
@@ -225,24 +211,20 @@ fd_vm_syscall_sol_get_return_data( /**/            void *  _vm,
   fd_txn_return_data_t const * return_data    = &instr_ctx->txn_ctx->return_data;
   ulong                        return_data_sz = return_data->len;
 
-  /* FIXME: SAT ADDS PROBABLY NOT NECESSARY (SUSPECT RETURN_DATA_SZ
-     ALREADY HAS A REASONABLE BOUND OF RETURN_DATA_MAX, WHICH IS
-     REASONABLE). */
-
-  /* FIXME: maybe factor these out? */
-
   ulong cpy_sz = fd_ulong_min( return_data_sz, dst_max );
-  FD_VM_CU_UPDATE( vm, fd_ulong_sat_add( FD_VM_SYSCALL_BASE_COST,
-                                         fd_ulong_sat_add( cpy_sz, sizeof(fd_pubkey_t) ) / FD_VM_CPI_BYTES_PER_UNIT ) );
 
   if( FD_LIKELY( cpy_sz ) ) {
-    /* FIXME: CHECK alignof(fd_pubkey_t) IS CORRECT */
-    void * dst        = FD_VM_MEM_SLICE_HADDR_ST( vm, dst_vaddr, 1UL, cpy_sz );
-    void * program_id = FD_VM_MEM_HADDR_ST( vm, program_id_vaddr, alignof(fd_pubkey_t), sizeof(fd_pubkey_t) );
 
-    if( FD_UNLIKELY( !is_nonoverlapping( (ulong)dst, cpy_sz, (ulong)program_id, sizeof(fd_pubkey_t) ) ) ) {
-      return FD_VM_ERR_MEM_OVERLAP;
-    }
+    FD_VM_CU_UPDATE( vm, fd_ulong_sat_add( cpy_sz, sizeof(fd_pubkey_t) ) / FD_VM_CPI_BYTES_PER_UNIT );
+
+    void * dst        = FD_VM_MEM_SLICE_HADDR_ST( vm, dst_vaddr, FD_VM_ALIGN_RUST_U8, cpy_sz );
+
+    /* https://github.com/anza-xyz/agave/blob/v2.0.8/programs/bpf_loader/src/syscalls/mod.rs#L1376-L1381
+       These can never happen. */
+
+    void * program_id = FD_VM_MEM_HADDR_ST( vm, program_id_vaddr, FD_VM_ALIGN_RUST_PUBKEY, sizeof(fd_pubkey_t) );
+
+    FD_VM_MEM_CHECK_NON_OVERLAPPING( vm, dst_vaddr, cpy_sz, program_id_vaddr, sizeof(fd_pubkey_t) );
 
     memcpy( dst,         return_data->data,       cpy_sz              );
     memcpy( program_id, &return_data->program_id, sizeof(fd_pubkey_t) );
@@ -260,34 +242,32 @@ fd_vm_syscall_sol_set_return_data( /**/            void *  _vm,
                                    FD_PARAM_UNUSED ulong   r4,
                                    FD_PARAM_UNUSED ulong   r5,
                                    /**/            ulong * _ret ) {
+  /* https://github.com/anza-xyz/agave/blob/v2.0.8/programs/bpf_loader/src/syscalls/mod.rs#L1297 */
   fd_vm_t * vm = (fd_vm_t *)_vm;
-
-  /* FIXME: The original version didn't have the same crashing FD_TEST
-     all the others had to tell if the vm was attached to an instruction
-     context.  If the VM is being run outside the Solana runtime, it
-     should never invoke this syscall in the first place.  So we treat
-     this as a SIGCALL in a non-crashing way for the time being. */
 
   fd_exec_instr_ctx_t const * instr_ctx = vm->instr_ctx;
   if( FD_UNLIKELY( !instr_ctx ) ) return FD_VM_ERR_SIGCALL;
 
-  /* https://github.com/firedancer-io/solana/blob/06ec63044892e5ee14b6fa15d8c55da9953d0c09/programs/bpf_loader/src/syscalls/mod.rs#L1287 */
   FD_VM_CU_UPDATE( vm, fd_ulong_sat_add( FD_VM_SYSCALL_BASE_COST, src_sz / FD_VM_CPI_BYTES_PER_UNIT ) );
 
-  /* https://github.com/firedancer-io/solana/blob/06ec63044892e5ee14b6fa15d8c55da9953d0c09/programs/bpf_loader/src/syscalls/mod.rs#L1293 */
+  /* https://github.com/anza-xyz/agave/blob/v2.0.8/programs/bpf_loader/src/syscalls/mod.rs#L1316 */
   if( FD_UNLIKELY( src_sz>FD_VM_RETURN_DATA_MAX ) ) {
+    /* TODO: this is a bit annoying, we may want to unify return codes...
+       - FD_VM_ERR_SYSCALL_RETURN_DATA_TOO_LARGE is Agave's return code,
+         also used for logging
+       - FD_VM_ERR_RETURN_DATA_TOO_LARGE is Firedancer return code */
+    FD_VM_ERR_FOR_LOG_SYSCALL( vm, FD_VM_ERR_SYSCALL_RETURN_DATA_TOO_LARGE );
     return FD_VM_ERR_RETURN_DATA_TOO_LARGE;
   }
 
+  /* src_sz == 0 is ok */
   void const * src = FD_VM_MEM_SLICE_HADDR_LD( vm, src_vaddr, 1UL, src_sz );
 
   fd_pubkey_t const    * program_id  = &instr_ctx->instr->program_id_pubkey;
   fd_txn_return_data_t * return_data = &instr_ctx->txn_ctx->return_data;
 
   return_data->len = src_sz;
-  if( FD_LIKELY( src_sz ) ) {
-    memcpy( return_data->data, src, src_sz );
-  }
+  fd_memcpy( return_data->data, src, src_sz );
   memcpy( &return_data->program_id, program_id, sizeof(fd_pubkey_t) );
 
   *_ret = 0;
@@ -373,17 +353,15 @@ fd_vm_syscall_sol_get_processed_sibling_instruction(
    */
   ulong stack_height = fd_ulong_sat_add( vm->instr_ctx->depth, 1UL );
 
-  ulong instruction_trace_length = vm->instr_ctx->txn_ctx->instr_trace_length;
-  if( FD_UNLIKELY( instruction_trace_length == 0 ) ) {
-    *ret = 0UL;
-    return FD_VM_SUCCESS;
-  }
-
   /* Reverse iterate through the instruction trace, ignoring anything except instructions on the same level.
   https://github.com/anza-xyz/agave/blob/70089cce5119c9afaeb2986e2ecaa6d4505ec15d/programs/bpf_loader/src/syscalls/mod.rs#L1422 */
+  ulong instruction_trace_length = vm->instr_ctx->txn_ctx->instr_trace_length;
   ulong reverse_index_at_stack_height = 0UL;
   fd_exec_instr_trace_entry_t * trace_entry = NULL;
   for( ulong index_in_trace = instruction_trace_length; index_in_trace > 0UL; index_in_trace-- ) {
+
+    /* https://github.com/anza-xyz/agave/blob/v2.0.8/programs/bpf_loader/src/syscalls/mod.rs#L1432-L1434
+       This error can never happen */
 
     fd_exec_instr_trace_entry_t * candidate_trace_entry = &vm->instr_ctx->txn_ctx->instr_trace[ index_in_trace - 1UL ];
     if( FD_LIKELY( candidate_trace_entry->stack_height < stack_height ) ) {
@@ -414,64 +392,53 @@ fd_vm_syscall_sol_get_processed_sibling_instruction(
     /* https://github.com/anza-xyz/agave/blob/70089cce5119c9afaeb2986e2ecaa6d4505ec15d/programs/bpf_loader/src/syscalls/mod.rs#L1447 */
     if( ( result_meta_haddr->data_len == trace_entry->instr_info->data_sz ) && 
         ( result_meta_haddr->accounts_len == trace_entry->instr_info->acct_cnt ) ) {
-      
+
       fd_pubkey_t * result_program_id_haddr = FD_VM_MEM_HADDR_ST( 
         vm,
         result_program_id_vaddr,
-        FD_PUBKEY_ALIGN,
-        FD_PUBKEY_FOOTPRINT);
+        FD_VM_ALIGN_RUST_PUBKEY,
+        sizeof(fd_pubkey_t) );
 
       uchar * result_data_haddr = FD_VM_MEM_SLICE_HADDR_ST(
         vm,
         result_data_vaddr,
-        alignof(uchar),
+        FD_VM_ALIGN_RUST_U8,
         result_meta_haddr->data_len);
 
       ulong accounts_meta_total_size = fd_ulong_sat_mul( result_meta_haddr->accounts_len, FD_VM_RUST_ACCOUNT_META_SIZE );
       fd_vm_rust_account_meta_t * result_accounts_haddr = FD_VM_MEM_SLICE_HADDR_ST(
         vm,
         result_accounts_vaddr,
-        FD_VM_RUST_ACCOUNT_META_ALIGN,
+        FD_VM_RUST_ACCOUNT_META_ALIGN, //TODO: sync FD_VM_ALIGN_RUST_* and FD_VM_RUST_*
         accounts_meta_total_size);
 
       /* Check for memory overlaps
          https://github.com/anza-xyz/agave/blob/70089cce5119c9afaeb2986e2ecaa6d4505ec15d/programs/bpf_loader/src/syscalls/mod.rs#L1469 */
-      if ( FD_UNLIKELY( !is_nonoverlapping(
-                          (ulong)result_meta_haddr,
-                          FD_VM_SYSCALL_PROCESSED_SIBLING_INSTRUCTION_SIZE,
-                          (ulong)result_program_id_haddr,
-                          FD_PUBKEY_FOOTPRINT
-                         ) || !is_nonoverlapping (
-                          (ulong)result_meta_haddr,
-                          FD_VM_SYSCALL_PROCESSED_SIBLING_INSTRUCTION_SIZE,
-                          (ulong)result_accounts_haddr,
-                          accounts_meta_total_size
-                         ) || !is_nonoverlapping(
-                           (ulong)result_meta_haddr,
-                           FD_VM_SYSCALL_PROCESSED_SIBLING_INSTRUCTION_SIZE,
-                           (ulong)result_data_haddr,
-                           result_meta_haddr->data_len
-                         ) || !is_nonoverlapping(
-                           (ulong)result_program_id_haddr,
-                           FD_PUBKEY_FOOTPRINT,
-                           (ulong)result_data_haddr,
-                           result_meta_haddr->data_len
-                         ) || !is_nonoverlapping(
-                           (ulong)result_program_id_haddr,
-                           FD_PUBKEY_FOOTPRINT,
-                           (ulong)result_accounts_haddr,
-                           accounts_meta_total_size
-                         ) || !is_nonoverlapping(
-                           (ulong)result_data_haddr,
-                           result_meta_haddr->data_len,
-                           (ulong)result_accounts_haddr,
-                           accounts_meta_total_size
-                         ) ) ) {
-        return FD_VM_ERR_MEM_OVERLAP;
-      }
+
+      FD_VM_MEM_CHECK_NON_OVERLAPPING( vm,
+        result_meta_vaddr, FD_VM_SYSCALL_PROCESSED_SIBLING_INSTRUCTION_SIZE,
+        result_program_id_vaddr, sizeof(fd_pubkey_t) );
+      FD_VM_MEM_CHECK_NON_OVERLAPPING( vm,
+        result_meta_vaddr, FD_VM_SYSCALL_PROCESSED_SIBLING_INSTRUCTION_SIZE,
+        result_accounts_vaddr, accounts_meta_total_size );
+      FD_VM_MEM_CHECK_NON_OVERLAPPING( vm,
+        result_meta_vaddr, FD_VM_SYSCALL_PROCESSED_SIBLING_INSTRUCTION_SIZE,
+        result_data_vaddr, result_meta_haddr->data_len );
+      FD_VM_MEM_CHECK_NON_OVERLAPPING( vm,
+        result_program_id_vaddr, sizeof(fd_pubkey_t),
+        result_data_vaddr, result_meta_haddr->data_len );
+      FD_VM_MEM_CHECK_NON_OVERLAPPING( vm,
+        result_program_id_vaddr, sizeof(fd_pubkey_t),
+        result_accounts_vaddr, accounts_meta_total_size );
+      FD_VM_MEM_CHECK_NON_OVERLAPPING( vm,
+        result_data_vaddr, result_meta_haddr->data_len,
+        result_accounts_vaddr, accounts_meta_total_size );
 
       /* Copy the instruction into the result addresses
-         https://github.com/anza-xyz/agave/blob/70089cce5119c9afaeb2986e2ecaa6d4505ec15d/programs/bpf_loader/src/syscalls/mod.rs#L1506-L1528 */
+         https://github.com/anza-xyz/agave/blob/70089cce5119c9afaeb2986e2ecaa6d4505ec15d/programs/bpf_loader/src/syscalls/mod.rs#L1506-L1528
+
+         Note: we assume that the instr accounts are already correct at this point.
+         Agave has many error checks. */
       fd_memcpy( result_program_id_haddr->key, instr_info->program_id_pubkey.key, FD_PUBKEY_FOOTPRINT );
       fd_memcpy( result_data_haddr, instr_info->data, instr_info->data_sz );
       for( ulong i = 0UL; i < instr_info->acct_cnt; i++ ) {
