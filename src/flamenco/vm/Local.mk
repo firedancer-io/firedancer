db72efd6da33eac3feeb4f233d6555830db09de9
Richard Patel <ripatel@jumptrading.com>
2024-09-14 23:25:22 +0000

    vm: compile without secp256k1

--- src/flamenco/vm/Local.mk
+++ src/flamenco/vm/Local.mk
@@ -1,6 +1,5 @@
 ifdef FD_HAS_INT128
 ifdef FD_HAS_HOSTED
-ifdef FD_HAS_SECP256K1
 
 $(call add-hdrs,fd_vm_base.h fd_vm.h fd_vm_private.h) # FIXME: PRIVATE TEMPORARILY HERE DUE TO SOME MESSINESS IN FD_VM_SYSCALL.H
 $(call add-objs,fd_vm fd_vm_interp fd_vm_disasm fd_vm_trace,fd_flamenco)
@@ -8,9 +7,10 @@ $(call add-objs,fd_vm fd_vm_interp fd_vm_disasm fd_vm_trace,fd_flamenco)
 $(call add-hdrs,test_vm_util.h)
 $(call add-objs,test_vm_util,fd_flamenco)
 
+ifdef FD_HAS_SECP256K1
 $(call make-bin,fd_vm_tool,fd_vm_tool,fd_flamenco fd_funk fd_ballet fd_util fd_disco,$(SECP256K1_LIBS))
-
 $(call make-unit-test,test_vm_interp,test_vm_interp,fd_flamenco fd_funk fd_ballet fd_util fd_disco,$(SECP256K1_LIBS))
+endif
 
 $(call make-unit-test,test_vm_base,test_vm_base,fd_flamenco fd_funk fd_ballet fd_util)
 
@@ -19,6 +19,5 @@ $(call run-unit-test,test_vm_instr)
 
 $(call run-unit-test,test_vm_base)
 $(call run-unit-test,test_vm_interp)
-endif
 endif
 endif
