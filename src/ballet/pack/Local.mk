2d476d54b6825d4df2620916925fabda0f07d264
Richard Patel <ripatel@jumptrading.com>
2024-09-26 11:27:06 +0000

    Don't run test_pack twice

--- src/ballet/pack/Local.mk
+++ src/ballet/pack/Local.mk
@@ -5,14 +5,13 @@ $(call make-unit-test,test_compute_budget_program,test_compute_budget_program,fd
 $(call make-unit-test,test_est_tbl,test_est_tbl,fd_ballet fd_util)
 $(call make-unit-test,test_pack_bitset,test_pack_bitset,fd_ballet fd_util)
 $(call make-unit-test,test_chkdup,test_chkdup,fd_ballet fd_util)
-$(call run-unit-test,test_compute_budget_program,)
-$(call run-unit-test,test_est_tbl,)
-$(call run-unit-test,test_pack,)
-$(call run-unit-test,test_pack_bitset,)
-$(call run-unit-test,test_chkdup,)
+$(call run-unit-test,test_compute_budget_program)
+$(call run-unit-test,test_est_tbl)
+$(call run-unit-test,test_pack_bitset)
+$(call run-unit-test,test_chkdup)
 ifdef FD_HAS_HOSTED
 $(call make-fuzz-test,fuzz_compute_budget_program_parse,fuzz_compute_budget_program_parse,fd_ballet fd_util)
 $(call make-unit-test,test_pack,test_pack,fd_disco fd_ballet fd_util)
-$(call run-unit-test,test_pack,)
+$(call run-unit-test,test_pack)
 endif
 endif
