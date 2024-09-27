498be4b2ce38f9f490a9d6a5e7a314a3e3ee9d41
Richard Patel <ripatel@jumptrading.com>
2024-09-27 16:18:36 +0000

    config: make opt directory configurable

--- config/extra/with-lz4.mk
+++ config/extra/with-lz4.mk
@@ -1,7 +1,7 @@
-ifneq (,$(wildcard opt/lib/liblz4.a))
+ifneq (,$(wildcard $(OPT)/lib/liblz4.a))
 FD_HAS_LZ4:=1
 CFLAGS+=-DFD_HAS_LZ4=1
-LDFLAGS+=opt/lib/liblz4.a
+LDFLAGS+=$(OPT)/lib/liblz4.a
 else
 $(info "lz4 not installed, skipping")
 endif
--- config/extra/with-msan.mk
+++ config/extra/with-msan.mk
@@ -1,6 +1,12 @@
 FD_HAS_MSAN:=1
 CPPFLAGS+=-DFD_HAS_MSAN=1
 
-CPPFLAGS+=-fsanitize=memory -fsanitize-memory-track-origins
+CPPFLAGS+=-fsanitize=memory
+#CPPFLAGS+=-fsanitize-memory-track-origins
 
-LDFLAGS+=-fsanitize=memory -fsanitize-memory-track-origins
+LDFLAGS+=-fsanitize=memory
+#LDFLAGS+=-fsanitize-memory-track-origins
+
+# MemorySanitizer requires all dependencies to be recompiled with
+# -fsanitize=memory.  Run ./deps.sh +msan to create opt/msan.
+OPT=opt/msan
