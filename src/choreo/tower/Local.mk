8295a08438fe19ab1a53a25ce760af4456113ec2
cavemanloverboy <93507302+cavemanloverboy@users.noreply.github.com>
2024-10-11 09:53:26 -0700

    include secp256k1 in test_tower unit test

--- src/choreo/tower/Local.mk
+++ src/choreo/tower/Local.mk
@@ -1,5 +1,5 @@
 ifdef FD_HAS_INT128
 $(call add-hdrs,fd_tower.h)
 $(call add-objs,fd_tower,fd_choreo)
-$(call make-unit-test,test_tower,test_tower,fd_choreo fd_flamenco fd_ballet fd_funk fd_util)
+$(call make-unit-test,test_tower,test_tower,fd_choreo fd_flamenco fd_ballet fd_funk fd_util,$(SECP256K1_LIBS))
 endif
