81a21cb27198a72e326cd2f13193b920c1d62efa
Richard Patel <ripatel@jumptrading.com>
2024-11-01 20:13:50 +0000

    tar: support OLDGNU large file extension

--- src/util/archive/Local.mk
+++ src/util/archive/Local.mk
@@ -1,7 +1,9 @@
 $(call add-hdrs,fd_ar.h fd_tar.h)
 $(call add-objs,fd_ar fd_tar,fd_util)
 $(call make-unit-test,test_ar,test_ar,fd_util)
-$(call run-unit-test,test_ar,)
+$(call run-unit-test,test_ar)
+$(call make-unit-test,test_tar,test_tar,fd_util)
+$(call run-unit-test,test_tar)
 ifdef FD_HAS_HOSTED
 $(call make-fuzz-test,fuzz_tar,fuzz_tar,fd_ballet fd_util)
 endif
