6f2075bc713c56d73659412b03e6c23d3eb88bc3
Charles Moyes <cmoyes@jumptrading.com>
2024-11-04 12:01:44 +0000

    Add linux_gcc_arm_n1 machine target
    Co-authored-by: Richard Patel <ripatel@jumptrading.com>

---
+++ config/extra/with-arm.mk
@@ -0,0 +1,6 @@
+include config/extra/with-ucontext.mk
+include config/extra/with-secp256k1.mk
+include config/extra/with-zstd.mk
+include config/extra/with-lz4.mk
+include config/extra/with-openssl.mk
+include config/extra/with-rocksdb.mk
