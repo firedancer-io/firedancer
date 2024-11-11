fb7a06ae0eb4cfac1fa826f50e46a9a9020355ba
Anway De <ande@jumptrading.com>
2024-11-11 12:58:49 +0000

    deps: install rustup version as specified in agave submodule

--- deps.sh
+++ deps.sh
@@ -292,7 +292,11 @@ check () {
         echo "[+] Installing rustup"
         curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
         source "$HOME/.cargo/env"
-        rustup toolchain add 1.75.0
+        rust_toolchain=$(grep "channel" agave/rust-toolchain.toml | awk '{print $NF}' | tr -d '"')
+        if [[ ! $(rustup toolchain list | grep $rust_toolchain) ]]; then
+          echo "[+] Updating rustup toolchain"
+          rustup toolchain add $rust_toolchain
+        fi
         ;;
       *)
         echo "[-] Skipping rustup install"
