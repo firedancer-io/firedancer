c7c70351fd571fe1e00bd9983e6362d4797a5a43
Richard Patel <ripatel@jumptrading.com>
2024-11-12 19:16:17 +0000

    quic: fix compat test build issue

--- src/waltz/quic/Local.mk
+++ src/waltz/quic/Local.mk
@@ -19,6 +19,8 @@ $(call add-objs,fd_quic_pkt_meta,fd_quic)
 $(call add-hdrs,fd_quic_retry.h)
 $(call add-objs,fd_quic_retry,fd_quic)
 
+$(call add-hdrs,fd_quic_proto.h fd_quic_proto_structs.h fd_quic_types.h)
+
 $(call add-hdrs,fd_quic_stream_pool.h)
 $(call add-objs,fd_quic_stream_pool,fd_quic)
 
