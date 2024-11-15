4730099c1207c505222aad01984bb690ec895c50
Richard Patel <ripatel@jumptrading.com>
2024-11-15 00:19:33 +0000

    config: always link s2n-bignum

--- config/extra/with-s2nbignum.mk
+++ config/extra/with-s2nbignum.mk
@@ -1,7 +1,7 @@
 ifneq (,$(wildcard $(OPT)/lib/libs2nbignum.a))
 FD_HAS_S2NBIGNUM:=1
 CFLAGS+=-DFD_HAS_S2NBIGNUM=1
-S2NBIGNUM_LIBS:=$(OPT)/lib/libs2nbignum.a
+LDFLAGS+=$(OPT)/lib/libs2nbignum.a
 else
 $(warning "s2n-bignum not installed, skipping")
 endif
--- src/ballet/secp256r1/Local.mk
+++ src/ballet/secp256r1/Local.mk
@@ -2,7 +2,7 @@ ifdef FD_HAS_S2NBIGNUM
 
 $(call add-hdrs,fd_secp256r1.h)
 $(call add-objs,fd_secp256r1,fd_ballet)
-$(call make-unit-test,test_secp256r1,test_secp256r1,fd_ballet fd_util,$(S2NBIGNUM_LIBS))
+$(call make-unit-test,test_secp256r1,test_secp256r1,fd_ballet fd_util)
 $(call run-unit-test,test_secp256r1)
 
 else
