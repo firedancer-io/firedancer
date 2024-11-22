69bd2899dc374b9afb3fe82e3cdfec23c713345d
Richard Patel <ripatel@jumptrading.com>
2024-11-22 23:47:45 +0000

    Update test quic tile

--- src/disco/quic/test_quic_tile.c
+++ src/disco/quic/test_quic_tile.c
@@ -1,11 +1,12 @@
 /* test_quic_tile.c implements end-to-end QUIC tile tests.
-   It ensures that basic TPU server functionality via UDP and QUIC  */
+   It ensures that basic TPU server functionality via UDP and QUIC. */
 
 #include "../topo/fd_topo.h"
 #include "../metrics/fd_metrics.h"
+#include "../../waltz/aio/fd_aio_tango.h"
 #include "../../waltz/quic/fd_quic.h"
-
-#include <setjmp.h>
+#include "../../waltz/quic/tests/fd_quic_test_helpers.h"
+#include "../../util/net/fd_ip4.h"
 
 extern fd_topo_run_tile_t fd_tile_quic;
 
@@ -22,6 +23,8 @@ static __attribute__((aligned(FD_MCACHE_ALIGN))) uchar net_quic_mcache_mem[ FD_M
 
 #define LINK_NET_QUIC_DATA_SZ (FD_DCACHE_REQ_DATA_SZ( LINK_NET_QUIC_MTU, LINK_NET_QUIC_DEPTH, 1UL, 1 ))
 static __attribute__((aligned(FD_DCACHE_ALIGN))) uchar net_quic_dcache_mem[ FD_DCACHE_FOOTPRINT( LINK_NET_QUIC_DATA_SZ, 0UL ) ];
+
+static __attribute__((aligned(FD_FSEQ_ALIGN))) uchar net_quic_fseq_mem[ FD_FSEQ_FOOTPRINT ];
 
 #define LINK_QUIC_TXN_DEPTH  (128UL)
 #define LINK_QUIC_TXN_APP_SZ   (0UL)
@@ -57,9 +60,12 @@ test_server_init( void ) {
   ulong obj_cnt  = 0UL;
   ulong wksp_cnt = 0UL;
 
+  strcpy( tile->name, "quic" );
+
   tile->in_cnt = LINK_IN_CNT;
 
   tile->in_link_id[0] = link_cnt++;
+  tile->in_link_poll[0] = 1;
   fd_topo_link_t * net_quic = &topo->links[ tile->in_link_id[0] ];
   strcpy( net_quic->name, "net_quic" );
   net_quic->mcache_obj_id = obj_cnt++;
@@ -93,6 +99,7 @@ test_server_init( void ) {
   topo->objs[ tile->quic.cnc_obj_id ].wksp_id = wksp_cnt++;
 
   /* End topo */
+  topo->tile_cnt = 1;
   topo->link_cnt = link_cnt;
   topo->obj_cnt  = obj_cnt;
 
@@ -105,6 +112,9 @@ test_server_init( void ) {
   topo->objs[ net_quic->mcache_obj_id ].footprint = sizeof(net_quic_mcache_mem);
   topo->workspaces[ topo->objs[ net_quic->mcache_obj_id ].wksp_id ].wksp = (void *)net_quic_mcache_mem;
   FD_TEST( net_quic->mcache );
+
+  tile->in_link_fseq[0] = fd_fseq_join( fd_fseq_new( net_quic_fseq_mem, 0UL ) );
+  FD_TEST( tile->in_link_fseq[0] );
 
   net_quic->dcache = fd_dcache_join( fd_dcache_new( net_quic_dcache_mem, LINK_NET_QUIC_DATA_SZ, 0UL ) );
   topo->objs[ net_quic->dcache_obj_id ].footprint = sizeof(net_quic_dcache_mem);
@@ -139,9 +149,11 @@ test_server_init( void ) {
   tile_obj->offset    = (ulong)scratch_mem;
   tile_obj->footprint = sizeof(scratch_mem);
 
-  tile->quic.ack_delay_millis             =    50;
-  tile->quic.idle_timeout_millis          = 10000;
-  tile->quic.quic_transaction_listen_port =  8000;
+  tile->quic.lazy                         = (ulong) 10e6;
+  tile->quic.ack_delay_millis             =         50;
+  tile->quic.idle_timeout_millis          = (ulong)100e6;
+  tile->quic.quic_transaction_listen_port =       8000;
+  tile->quic.ip_addr                      = FD_IP4_ADDR( 127, 50, 0, 1 );
 
 }
 
@@ -170,74 +182,77 @@ test_server_start( ulong tile_idx ) {
 
 static __attribute__((aligned(FD_METRICS_ALIGN))) uchar client_metrics_mem[ FD_METRICS_FOOTPRINT( LINK_IN_CNT, LINK_OUT_CNT ) ];
 
-struct client_ctx {
-  fd_quic_t * quic;
-  void *      rx_base;
-  fd_cnc_t *  cnc;
-  jmp_buf     bail;
-};
-
-typedef struct client_ctx client_ctx_t;
-
-static void
-client_during_housekeeping( client_ctx_t * ctx ) {
-  ulong s = fd_cnc_signal_query( ctx->cnc );
-  if( FD_UNLIKELY( s!=FD_CNC_SIGNAL_RUN ) ) longjmp( ctx->bail, 1 );
-}
-
-static void
-client_during_frag( client_ctx_t * ctx,
-                    ulong          in_idx FD_PARAM_UNUSED,
-                    ulong          seq    FD_PARAM_UNUSED,
-                    ulong          sig    FD_PARAM_UNUSED,
-                    ulong          chunk,
-                    ulong          sz ) {
-  uchar * frag = fd_chunk_to_laddr( ctx->rx_base, chunk );
-  fd_quic_process_packet( ctx->quic, frag, sz );
-  fd_quic_service( ctx->quic );
-}
-
-#define STEM_NAME client_stem
-#define STEM_BURST (1UL)
-#define STEM_CALLBACK_DURING_HOUSEKEEPING client_during_housekeeping
-#define STEM_CALLBACK_DURING_FRAG client_during_frag
-#define STEM_CALLBACK_CONTEXT_TYPE  client_ctx_t
-#define STEM_CALLBACK_CONTEXT_ALIGN alignof(client_ctx_t)
-#include "../stem/fd_stem.c"
-
 static int
 test_client_main( int     argc,
                   char ** argv ) {
   (void)argc; (void)argv;
 
-  client_ctx_t ctx[1] = {{0}};
-
   fd_metrics_register( fd_metrics_join( fd_metrics_new( client_metrics_mem, 1, 1 ) ) );
-  fd_rng_t _rng[1]; fd_rng_t * rng = fd_rng_join( fd_rng_new( _rng, 999, 0UL ) );
 
-  fd_cnc_t *             cnc       = fd_cnc_join( client_cnc_mem );
-  fd_frag_meta_t const * rx_mcache = fd_mcache_join( quic_net_mcache_mem );
-  fd_frag_meta_t *       tx_mcache = fd_mcache_join( net_quic_mcache_mem );
-  long                   lazy      = 1000L;
+  fd_cnc_t *             cnc       = fd_cnc_join( client_cnc_mem );         FD_TEST( cnc );
+  fd_frag_meta_t const * rx_mcache = fd_mcache_join( quic_net_mcache_mem ); FD_TEST( rx_mcache );
+  fd_frag_meta_t *       tx_mcache = fd_mcache_join( net_quic_mcache_mem ); FD_TEST( tx_mcache );
+  void *                 tx_dcache = fd_dcache_join( net_quic_dcache_mem ); FD_TEST( tx_dcache );
 
-  static __attribute__((aligned(FD_FSEQ_ALIGN))) uchar rx_fseq_mem[ FD_FSEQ_FOOTPRINT ];
-  ulong * fseq = fd_fseq_join( fd_fseq_new( rx_fseq_mem, 0UL ) );
+  void * rx_base = quic_net_dcache_mem;
 
-  FD_LOG_INFO(( "client_stem_footprint(1,1,0)==%lu", client_stem_scratch_footprint( 1UL, 1UL, 0UL ) ));
-  static __attribute__((aligned(FD_STEM_SCRATCH_ALIGN))) uchar scratch_mem[ 128 ];
-  FD_TEST( sizeof(scratch_mem)==client_stem_scratch_footprint( 1UL, 1UL, 0UL ) );
+  fd_quic_limits_t limits = {
+    .conn_cnt         =   4,
+    .handshake_cnt    =   2,
+    .conn_id_cnt      =   4,
+    .stream_id_cnt    =   4,
+    .stream_pool_cnt  =   8,
+    .inflight_pkt_cnt =  16,
+    .tx_buf_sz        = 500,
+  };
+  FD_LOG_INFO(( "fd_quic client footprint: %lu bytes", fd_quic_footprint( &limits ) ));
+  static __attribute__((aligned(FD_QUIC_ALIGN))) uchar quic_mem[ 76928 ];
+  FD_TEST( sizeof(quic_mem)==fd_quic_footprint( &limits ) );
+  fd_quic_t * quic = fd_quic_join( fd_quic_new( quic_mem, &limits ) );
+  FD_TEST( quic );
 
-  ctx->rx_base = quic_net_dcache_mem;
-  ctx->cnc     = cnc;
+  uint   src_addr = FD_IP4_ADDR( 127, 50, 0, 2 );
+  ushort src_port = 8000;
+  uint   dst_addr = FD_IP4_ADDR( 127, 50, 0, 1 );
+  ulong  l4off    = 14+20+8;  /* eth + ip4(ihl=5) + udp */
+  ulong  tx_sig   = fd_disco_netmux_sig( src_addr, src_port, dst_addr, DST_PROTO_TPU_QUIC, l4off );
+
+  fd_aio_tango_rx_t aio_rx[1];
+  FD_TEST( fd_aio_tango_rx_new( aio_rx, fd_quic_get_aio_net_rx( quic ), rx_mcache, 0UL, rx_base ) );
+  fd_aio_tango_tx_t aio_tx[1];
+  FD_TEST( fd_aio_tango_tx_new( aio_tx, tx_mcache, tx_dcache, net_quic_dcache_mem, LINK_NET_QUIC_MTU, 0UL, tx_sig ) );
+  fd_quic_set_aio_net_tx( quic, fd_aio_tango_tx_aio( aio_tx ) );
+
+  quic->config.role                    = FD_QUIC_ROLE_CLIENT;
+  quic->config.idle_timeout            = (ulong)100e6;
+  quic->config.identity_public_key[10] = 10;
+  quic->config.net.ip_addr             = src_addr;
+  quic->config.net.ephem_udp_port.lo   = src_port;
+  quic->config.net.ephem_udp_port.hi   = src_port+1;
+  quic->cb.now = fd_quic_test_now;
+
+  FD_TEST( fd_quic_init( quic ) );
 
   fd_cnc_signal( cnc, FD_CNC_SIGNAL_RUN );
-  if( !setjmp( ctx->bail ) ) {
-    client_stem_run1( 1UL, &rx_mcache, &fseq, 1UL, &tx_mcache, 0UL, NULL, NULL, 1UL, lazy, rng, scratch_mem, ctx );
-    __builtin_unreachable();
+
+  fd_quic_conn_t * conn = fd_quic_connect( quic, dst_addr, 8000 );
+
+  for(;;) {
+    fd_quic_service( quic );
+    fd_aio_tango_rx_poll( aio_rx );
+    if( conn->state == FD_QUIC_CONN_STATE_INVALID ) break;
   }
+
+  for(;;) {
+    ulong s = fd_cnc_signal_query( cnc );
+    if( FD_UNLIKELY( s!=FD_CNC_SIGNAL_RUN ) ) break;
+  }
+
   fd_cnc_signal( cnc, FD_CNC_SIGNAL_BOOT );
 
-  fd_fseq_leave( fseq );
+  fd_aio_tango_rx_delete( aio_rx );
+  fd_quic_delete( fd_quic_leave( quic ) );
+  fd_aio_tango_tx_delete( aio_tx );
   fd_mcache_leave( rx_mcache );
   fd_mcache_leave( tx_mcache );
   fd_cnc_leave( cnc );
@@ -271,6 +286,8 @@ main( int     argc,
 
   FD_TEST( !fd_cnc_open( server_cnc ) );
   FD_TEST( !fd_cnc_open( client_cnc ) );
+
+  fd_log_sleep( 10e9 );
 
   fd_cnc_signal( server_cnc, FD_CNC_SIGNAL_HALT );
   fd_cnc_signal( client_cnc, FD_CNC_SIGNAL_HALT );
