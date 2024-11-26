50cca6a0c762ee3be4ddc49cccb33b9bb0ca809e
Yufeng Zhou <yufzhou@jumptrading.com>
2024-11-26 01:40:29 +0000

    flamenco: init parent_signature_cnt and lamports_per_signature to non 0
    These values are involved in the computation of new derived
    lamports_per_signature values in sysvar.  We derive incorrect
    lamports_per_signature if these two are not initialized properly.
    
    Also bump the limit on the number of microblocks per block in the
    blockstore.  The runtime has no similar limits and can handle blocks
    with a large number of microblocks just fine.  There's no reason for
    the blockstore to have such a limit.

--- src/flamenco/runtime/context/fd_exec_slot_ctx.c
+++ src/flamenco/runtime/context/fd_exec_slot_ctx.c
@@ -251,8 +251,9 @@ fd_exec_slot_ctx_recover_( fd_exec_slot_ctx_t *   slot_ctx,
   slot_bank->prev_slot = oldbank->parent_slot;
   fd_memcpy(&slot_bank->banks_hash, &oldbank->hash, sizeof(oldbank->hash));
   fd_memcpy(&slot_bank->fee_rate_governor, &oldbank->fee_rate_governor, sizeof(oldbank->fee_rate_governor));
-  slot_bank->lamports_per_signature = oldbank->fee_calculator.lamports_per_signature;
-  slot_ctx->prev_lamports_per_signature = oldbank->fee_calculator.lamports_per_signature;
+  slot_bank->lamports_per_signature = manifest->lamports_per_signature;
+  slot_ctx->prev_lamports_per_signature = manifest->lamports_per_signature;
+  slot_ctx->parent_signature_cnt = oldbank->signature_count;
   if( oldbank->hashes_per_tick )
     epoch_bank->hashes_per_tick = *oldbank->hashes_per_tick;
   else
