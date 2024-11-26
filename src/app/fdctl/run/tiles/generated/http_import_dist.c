bf4ceb5755451f30e36719d21ebc4355551fdecf
Michael McGee <mmcgee@jumptrading.com>
2024-11-26 20:24:24 +0000

    gui: fix cu pct rounding

--- src/app/fdctl/run/tiles/generated/http_import_dist.c
+++ src/app/fdctl/run/tiles/generated/http_import_dist.c
@@ -3,10 +3,10 @@
 
 FD_IMPORT_BINARY( file0, "src/app/fdctl/dist/assets/firedancer-D_J0EzUc.svg" );
 FD_IMPORT_BINARY( file1, "src/app/fdctl/dist/assets/firedancer_logo-CrgwxzPk.svg" );
-FD_IMPORT_BINARY( file2, "src/app/fdctl/dist/assets/index-CaAZrKi3.js" );
+FD_IMPORT_BINARY( file2, "src/app/fdctl/dist/assets/index-DkwHoDZp.js" );
 FD_IMPORT_BINARY( file3, "src/app/fdctl/dist/assets/index-Dl4iw6mh.css" );
 FD_IMPORT_BINARY( file4, "src/app/fdctl/dist/assets/index-Dz6VwGqc.css" );
-FD_IMPORT_BINARY( file5, "src/app/fdctl/dist/assets/index-ka5eIsuW.js" );
+FD_IMPORT_BINARY( file5, "src/app/fdctl/dist/assets/index-NBFTeNj5.js" );
 FD_IMPORT_BINARY( file6, "src/app/fdctl/dist/assets/inter-tight-latin-400-normal-BW_APOAv.woff" );
 FD_IMPORT_BINARY( file7, "src/app/fdctl/dist/assets/inter-tight-latin-400-normal-CjmW70MP.woff2" );
 FD_IMPORT_BINARY( file8, "src/app/fdctl/dist/assets/privateYou-DxC3xn7b.svg" );
@@ -25,7 +25,7 @@ fd_http_static_file_t STATIC_FILES[] = {
         .data_len = &file1_sz,
     },
     {
-        .name = "/assets/index-CaAZrKi3.js",
+        .name = "/assets/index-DkwHoDZp.js",
         .data = file2,
         .data_len = &file2_sz,
     },
@@ -40,7 +40,7 @@ fd_http_static_file_t STATIC_FILES[] = {
         .data_len = &file4_sz,
     },
     {
-        .name = "/assets/index-ka5eIsuW.js",
+        .name = "/assets/index-NBFTeNj5.js",
         .data = file5,
         .data_len = &file5_sz,
     },
