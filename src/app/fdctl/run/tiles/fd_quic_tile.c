83c09ba6d314d58caab99d541c07703024b7c453
Richard Patel <ripatel@jumptrading.com>
2024-11-26 16:11:21 +0000

    tpu: wire up FrameFailParse metric

--- src/app/fdctl/run/tiles/fd_quic_tile.c
+++ src/app/fdctl/run/tiles/fd_quic_tile.c
@@ -209,7 +209,8 @@ metrics_write( fd_quic_ctx_t * ctx ) {
   FD_MCNT_SET(  QUIC, STREAM_RECEIVED_EVENTS, ctx->quic->metrics.stream_rx_event_cnt );
   FD_MCNT_SET(  QUIC, STREAM_RECEIVED_BYTES,  ctx->quic->metrics.stream_rx_byte_cnt );
 
-  FD_MCNT_ENUM_COPY( QUIC, RECEIVED_FRAMES, ctx->quic->metrics.frame_rx_cnt );
+  FD_MCNT_ENUM_COPY( QUIC, RECEIVED_FRAMES,  ctx->quic->metrics.frame_rx_cnt );
+  FD_MCNT_SET      ( QUIC, FRAME_FAIL_PARSE, ctx->quic->metrics.frame_rx_err_cnt );
 
   FD_MCNT_ENUM_COPY( QUIC, ACK_TX, ctx->quic->metrics.ack_tx );
 
