ae406816ff8d301334ed02cf877fa7c69b8f163a
Emanuele Cesena <ecesena@jumptrading.com>
2024-11-27 23:55:12 +0000

    secp256k1: bug fixes: pubkey deser, 0 signatures

--- src/ballet/secp256r1/test_secp256r1.c
+++ src/ballet/secp256r1/test_secp256r1.c
@@ -225,21 +225,46 @@ test_secp256r1_point_eq_x( FD_FN_UNUSED fd_rng_t * rng ) {
 static void
 test_secp256r1_verify( FD_FN_UNUSED fd_rng_t * rng ) {
 
-  // test correctness (r,s)
-  uchar * msg = (uchar *)"hello";
+  uchar _msg[ 10 ] = { 0 }; uchar * msg = _msg;
+  ulong msg_sz;
   uchar _sig[ 64 ] = { 0 }; uchar * sig = _sig;
-  fd_hex_decode( sig, "a940d67c9560a47c5dafb45ab1f39eb68c8fac9b51fc8c4e30b1f0e63e4967d3586569a56364c3b03eefd421aa7fc750f6fa187210c3206c55602f96e0ecaa4d", 64 );
   uchar _pub[ 33 ] = { 0 }; uchar * pub = _pub;
-  fd_hex_decode( pub, "02d8c82b3791c8b51cfe44aa50226217159596ca26e6075aaf8bf8be2d351b96ae", 33 );
   fd_sha256_t sha[1];
-  FD_TEST( fd_secp256r1_verify( msg, 5, sig, pub, sha )==FD_SECP256R1_SUCCESS );
+
+  // test correctness (r,s)
+  {
+    msg_sz = 6;
+    fd_hex_decode( msg, "deadbeef0000", msg_sz );
+    fd_hex_decode( sig, "65f479af7700ea826cdf4a2d30bbbfd5be5a8abb4dd6e8ef0bb0d5018b5f08160856e32671be561383d7eb408c6d24c28fd05141fd247dd8e67fc511d4f2ace9", 64 );
+    fd_hex_decode( pub, "030f5183ccd84510385acc742f2d9d83771190c83cd0a36c42b0877c1666598a31", 33 );
+    FD_TEST( fd_secp256r1_verify( msg, msg_sz, sig, pub, sha )==FD_SECP256R1_SUCCESS );
+  }
+  {
+    msg_sz = 6;
+    fd_hex_decode( msg, "deadbeef0001", msg_sz );
+    fd_hex_decode( sig, "dde6de58059a2edc745f3757a45b527c6a838e2f9944e7985cdbce18a9831444662257cde953020a5ba3dbd77dabc0e7ecf35dadf35754dd5c014e3197173ca7", 64 );
+    fd_hex_decode( pub, "032a18f703b754f728b4faa2cd9e81d82647b86fb4e22bce7348ddf2a977a4e9d9", 33 );
+    FD_TEST( fd_secp256r1_verify( msg, msg_sz, sig, pub, sha )==FD_SECP256R1_SUCCESS );
+  }
+  {
+    msg_sz = 6;
+    fd_hex_decode( msg, "deadbeef0002", msg_sz );
+    fd_hex_decode( sig, "d852239f6cdd19f530636fed1736f6c1fff499e988ffc14faf9098b6c359f53f24d8918494d158e562643da21939e3d8f4f733b2e135c63f205281c3cbae7cc1", 64 );
+    fd_hex_decode( pub, "025241d2133264e7d4b0f91c0d2b08d7b8e4c015cc84d68eafe8c5dfe4b8bf6753", 33 );
+    FD_TEST( fd_secp256r1_verify( msg, msg_sz, sig, pub, sha )==FD_SECP256R1_SUCCESS );
+  }
 
   // test malleability (r,-s)
+  msg = (uchar *)"hello";
+  msg_sz = 5;
   {
-    uchar _sig[ 64 ] = { 0 }; uchar * sig = _sig;
     fd_hex_decode( sig, "a940d67c9560a47c5dafb45ab1f39eb68c8fac9b51fc8c4e30b1f0e63e4967d3a79a96599c9b3c50c1102bde558038aec5ece23b96547e189e599b2c1b767b04", 64 );
-    FD_TEST( fd_secp256r1_verify( msg, 5, sig, pub, sha )==FD_SECP256R1_FAILURE );
+    FD_TEST( fd_secp256r1_verify( msg, msg_sz, sig, pub, sha )==FD_SECP256R1_FAILURE );
   }
+
+  fd_hex_decode( sig, "a940d67c9560a47c5dafb45ab1f39eb68c8fac9b51fc8c4e30b1f0e63e4967d3586569a56364c3b03eefd421aa7fc750f6fa187210c3206c55602f96e0ecaa4d", 64 );
+  fd_hex_decode( pub, "02d8c82b3791c8b51cfe44aa50226217159596ca26e6075aaf8bf8be2d351b96ae", 33 );
+  FD_TEST( fd_secp256r1_verify( msg, msg_sz, sig, pub, sha )==FD_SECP256R1_SUCCESS );
 
   // bench
   {
