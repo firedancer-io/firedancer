a43b888f29183ba807c821bc5f76a74f38627c53
Richard Patel <ripatel@jumptrading.com>
2024-11-29 02:32:49 +0000

    quic: add high-performance event logger
    Adds fd_quic_log, a shared memory event logging engine.
    It allows fd_quic to trace any errors that occur in production with
    minimal performance impact.
    
    Currently based on fd_mcache for simplicity.  Internal APIs are
    structured in a way that the shm ring layout can easily be replaced
    with something more tailored to logging in the future.
    
    The API is separated into two components:
    
    - fd_quic_log, the semi-stable high-level API (fd_quic_log_user.h)
      Makes minimal assumptions on things like memory layout, message
      MTU, and data allocation policy.  Optimized for ease of use.
    
    - fd_quic_logger, the log writer for internal use.
      (fd_quic_log_private.h).  Optimized for performance.

---
+++ src/waltz/quic/log/fd_quic_log_event.h
@@ -0,0 +1,50 @@
+#ifndef HEADER_fd_src_waltz_quic_log_fd_quic_log_event_h
+#define HEADER_fd_src_waltz_quic_log_fd_quic_log_event_h
+
+/* fd_quic_log_event.h contains fd_quic event definitions. */
+
+#include "fd_quic_log_user.h"
+
+/* Event IDs **********************************************************/
+
+/* Event group: Connection events */
+
+#define FD_QUIC_EVENT_CONN_NEW         (0x01) /* connection opened */
+#define FD_QUIC_EVENT_CONN_ESTABLISHED (0x02) /* connection established (handshake done) */
+#define FD_QUIC_EVENT_CONN_QUIC_CLOSE  (0x03) /* connection closed (due to QUIC) */
+#define FD_QUIC_EVENT_CONN_APP_CLOSE   (0x04) /* connection closed (due to app request) */
+#define FD_QUIC_EVENT_TLS_HS_FAIL      (0x06) /* TLS handshake fail */
+
+/* Event group: Object pool alloc failures */
+
+#define FD_QUIC_EVENT_ALLOC_FAIL_PKT_META (0x101) /* fd_quic_pkt_meta_t */
+#define FD_QUIC_EVENT_ALLOC_FAIL_CONN     (0x102) /* fd_quic_conn_t */
+#define FD_QUIC_EVENT_ALLOC_FAIL_STREAM   (0x103) /* fd_quic_stream_t */
+
+/* Event structs ******************************************************/
+
+/* fd_quic_log_error_t is a generic error code container.
+   src_file:src_line point to the source line of code that threw this
+   error.  flags&1==0 if the error originated locally.  flags&1==1 if
+   the error originated on the peer's side and was sent to us.
+
+   code is defined depending on the FD_QUIC_EVENT_{...} error:
+
+     CONN_QUIC_CLOSE: code[0] is a QUIC Transport Error Code
+                        (https://www.iana.org/assignments/quic/quic.xhtml#quic-transport-error-codes)
+     CONN_APP_CLOSE:  code[0] is the error code set by the application
+     TLS_HS_FAIL:     code[0] is the TLS Alert code
+                        (https://www.iana.org/assignments/tls-parameters/tls-parameters.xhtml#tls-parameters-6)
+                      code[1] is an fd_tls specific error code (only if flags&1==0) */
+
+struct fd_quic_log_error {
+  /* 0x00 */ fd_quic_log_hdr_t hdr;
+  /* 0x18 */ ulong code[2];       /* protocol-specific error codes */
+  /* 0x28 */ char  src_file[16];  /* e.g. "fd_quic.c" */
+  /* 0x38 */ uint  src_line;
+  /* 0x3c */ uchar flags;
+};
+
+typedef struct fd_quic_log_error fd_quic_log_error_t;
+
+#endif /* HEADER_fd_src_waltz_quic_log_fd_quic_log_event_h */
