ec4d60a037cd82d5bbb0669fe0afe3e547efd3ef
Richard Patel <ripatel@jumptrading.com>
2024-12-02 07:02:55 +0000

    deps: lz4 v0.10.0

--- deps.sh
+++ deps.sh
@@ -132,7 +132,7 @@ fetch () {
     checkout_llvm
   fi
   checkout_repo zstd      https://github.com/facebook/zstd          "v1.5.6"
-  checkout_repo lz4       https://github.com/lz4/lz4                "v1.9.4"
+  checkout_repo lz4       https://github.com/lz4/lz4                "v1.10.0"
   checkout_repo secp256k1 https://github.com/bitcoin-core/secp256k1 "v0.5.0"
   checkout_repo s2n       https://github.com/awslabs/s2n-bignum     "" "efa579c"
   #checkout_repo openssl   https://github.com/openssl/openssl        "openssl-3.3.1"
