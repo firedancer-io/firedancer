8eb0a035e438e1d802ffa9276036ba7e0c50b59e
Emily Wang <emwang@jumptrading.com>
2024-12-03 23:09:34 +0000

    contrib: set agave path

--- contrib/test/setup_fd_cluster_stakes.sh
+++ contrib/test/setup_fd_cluster_stakes.sh
@@ -7,29 +7,29 @@ cd ../test-ledger
 RPC_URL="http://localhost:8899/"
 AGAVE_PATH=${AGAVE_PATH:='./agave/target/release'}
 
-solana-keygen new --no-bip39-passphrase --silent --outfile fd-identity-keypair.json
-solana-keygen new --no-bip39-passphrase --silent --outfile fd-stake-keypair.json
-solana-keygen new --no-bip39-passphrase --silent --outfile fd-vote-keypair.json
-solana-keygen new --no-bip39-passphrase --silent --outfile fd-withdrawer-keypair.json
+$AGAVE_PATH/solana-keygen new --no-bip39-passphrase --silent --outfile fd-identity-keypair.json
+$AGAVE_PATH/solana-keygen new --no-bip39-passphrase --silent --outfile fd-stake-keypair.json
+$AGAVE_PATH/solana-keygen new --no-bip39-passphrase --silent --outfile fd-vote-keypair.json
+$AGAVE_PATH/solana-keygen new --no-bip39-passphrase --silent --outfile fd-withdrawer-keypair.json
 
-solana -u $RPC_URL --keypair faucet-keypair.json transfer --allow-unfunded-recipient fd-identity-keypair.json 400000
-solana -u $RPC_URL --keypair fd-identity-keypair.json create-vote-account fd-vote-keypair.json fd-identity-keypair.json fd-withdrawer-keypair.json
-solana -u $RPC_URL --keypair fd-identity-keypair.json create-stake-account fd-stake-keypair.json 300000
-solana -u $RPC_URL --keypair fd-identity-keypair.json delegate-stake fd-stake-keypair.json fd-vote-keypair.json
+$AGAVE_PATH/solana -u $RPC_URL --keypair faucet-keypair.json transfer --allow-unfunded-recipient fd-identity-keypair.json 400000
+$AGAVE_PATH/solana -u $RPC_URL --keypair fd-identity-keypair.json create-vote-account fd-vote-keypair.json fd-identity-keypair.json fd-withdrawer-keypair.json
+$AGAVE_PATH/solana -u $RPC_URL --keypair fd-identity-keypair.json create-stake-account fd-stake-keypair.json 300000
+$AGAVE_PATH/solana -u $RPC_URL --keypair fd-identity-keypair.json delegate-stake fd-stake-keypair.json fd-vote-keypair.json
 
-solana -u $RPC_URL --keypair fd-identity-keypair.json vote-account fd-vote-keypair.json
-solana -u $RPC_URL --keypair fd-identity-keypair.json stake-account fd-stake-keypair.json
+$AGAVE_PATH/solana -u $RPC_URL --keypair fd-identity-keypair.json vote-account fd-vote-keypair.json
+$AGAVE_PATH/solana -u $RPC_URL --keypair fd-identity-keypair.json stake-account fd-stake-keypair.json
 
 exit 0
-solana-keygen new --no-bip39-passphrase --silent --outfile fd-identity-keypair-2.json
-solana-keygen new --no-bip39-passphrase --silent --outfile fd-stake-keypair-2.json
-solana-keygen new --no-bip39-passphrase --silent --outfile fd-vote-keypair-2.json
-solana-keygen new --no-bip39-passphrase --silent --outfile fd-withdrawer-keypair-2.json
+$AGAVE_PATH/solana-keygen new --no-bip39-passphrase --silent --outfile fd-identity-keypair-2.json
+$AGAVE_PATH/solana-keygen new --no-bip39-passphrase --silent --outfile fd-stake-keypair-2.json
+$AGAVE_PATH/solana-keygen new --no-bip39-passphrase --silent --outfile fd-vote-keypair-2.json
+$AGAVE_PATH/solana-keygen new --no-bip39-passphrase --silent --outfile fd-withdrawer-keypair-2.json
 
-solana -u $RPC_URL --keypair faucet-keypair.json transfer --allow-unfunded-recipient fd-identity-keypair-2.json 4000000
-solana -u $RPC_URL --keypair fd-identity-keypair-2.json create-vote-account fd-vote-keypair-2.json fd-identity-keypair-2.json fd-withdrawer-keypair-2.json
-solana -u $RPC_URL --keypair fd-identity-keypair-2.json create-stake-account fd-stake-keypair-2.json 100000
-solana -u $RPC_URL --keypair fd-identity-keypair-2.json delegate-stake fd-stake-keypair-2.json fd-vote-keypair-2.json
+$AGAVE_PATH/solana -u $RPC_URL --keypair faucet-keypair.json transfer --allow-unfunded-recipient fd-identity-keypair-2.json 4000000
+$AGAVE_PATH/solana -u $RPC_URL --keypair fd-identity-keypair-2.json create-vote-account fd-vote-keypair-2.json fd-identity-keypair-2.json fd-withdrawer-keypair-2.json
+$AGAVE_PATH/solana -u $RPC_URL --keypair fd-identity-keypair-2.json create-stake-account fd-stake-keypair-2.json 100000
+$AGAVE_PATH/solana -u $RPC_URL --keypair fd-identity-keypair-2.json delegate-stake fd-stake-keypair-2.json fd-vote-keypair-2.json
 
-solana -u $RPC_URL --keypair fd-identity-keypair-2.json vote-account fd-vote-keypair-2.json
-solana -u $RPC_URL --keypair fd-identity-keypair-2.json stake-account fd-stake-keypair-2.json
+$AGAVE_PATH/solana -u $RPC_URL --keypair fd-identity-keypair-2.json vote-account fd-vote-keypair-2.json
+$AGAVE_PATH/solana -u $RPC_URL --keypair fd-identity-keypair-2.json stake-account fd-stake-keypair-2.json
