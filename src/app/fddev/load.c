b6ba905b2915d9412016312af719eabf7295296b
Richard Patel <ripatel@jumptrading.com>
2024-12-04 07:00:29 +0000

    fddev load fixes

--- src/app/fddev/load.c
+++ src/app/fddev/load.c
@@ -32,16 +32,16 @@ void
 load_cmd_args( int *    pargc,
                   char *** pargv,
                   args_t * args ) {
-  const char * tpu_ip      = fd_env_strip_cmdline_cstr  ( pargc, pargv, "--tpu-ip",      NULL,  NULL );
-  const char * rpc_ip      = fd_env_strip_cmdline_cstr  ( pargc, pargv, "--rpc-ip",      NULL,  NULL );
-  const char * affinity    = fd_env_strip_cmdline_cstr  ( pargc, pargv, "--affinity",     "-a", NULL );
+  const char * tpu_ip      = fd_env_strip_cmdline_cstr  ( pargc, pargv, "--tpu-ip",   NULL, NULL );
+  const char * rpc_ip      = fd_env_strip_cmdline_cstr  ( pargc, pargv, "--rpc-ip",   NULL, NULL );
+  const char * affinity    = fd_env_strip_cmdline_cstr  ( pargc, pargv, "--affinity", NULL, NULL );
 
-  args->load.tpu_port    = fd_env_strip_cmdline_ushort( pargc, pargv, "--tpu-port",    NULL,     0 );
-  args->load.rpc_port    = fd_env_strip_cmdline_ushort( pargc, pargv, "--rpc-port",    NULL,     0 );
-  args->load.benchg      = fd_env_strip_cmdline_ulong ( pargc, pargv, "--num-benchg",   "-g",    0 );
-  args->load.benchs      = fd_env_strip_cmdline_ulong ( pargc, pargv, "--num-benchs",   "-s",    0 );
-  args->load.accounts    = fd_env_strip_cmdline_ulong ( pargc, pargv, "--num-accounts", NULL,    0 );
-  args->load.connections = fd_env_strip_cmdline_ulong ( pargc, pargv, "--connections",  "-c",    0 );
+  args->load.tpu_port    = fd_env_strip_cmdline_ushort( pargc, pargv, "--tpu-port",     NULL, 0 );
+  args->load.rpc_port    = fd_env_strip_cmdline_ushort( pargc, pargv, "--rpc-port",     NULL, 0 );
+  args->load.benchg      = fd_env_strip_cmdline_ulong ( pargc, pargv, "--num-benchg",   NULL, 0 );
+  args->load.benchs      = fd_env_strip_cmdline_ulong ( pargc, pargv, "--num-benchs",   NULL, 0 );
+  args->load.accounts    = fd_env_strip_cmdline_ulong ( pargc, pargv, "--num-accounts", NULL, 0 );
+  args->load.connections = fd_env_strip_cmdline_ulong ( pargc, pargv, "--connections",  NULL, 0 );
   args->load.transaction_mode    = fd_env_strip_cmdline_int  ( pargc, pargv, "--transaction-mode",    NULL, 0    );
   args->load.contending_fraction = fd_env_strip_cmdline_float( pargc, pargv, "--contending-fraction", NULL, 0.0f );
   args->load.cu_price_spread     = fd_env_strip_cmdline_float( pargc, pargv, "--cu-price-spread",     NULL, 0.0f );
@@ -82,6 +82,8 @@ load_cmd_fn( args_t *         args,
 
   if( FD_UNLIKELY( !args->load.rpc_port ) )
     args->load.rpc_port    = config->rpc.port;
+  if( FD_UNLIKELY( !args->load.rpc_port ) )
+    FD_LOG_ERR(( "Missing --rpc-port" ));
 
   if( FD_UNLIKELY( !strcmp( args->load.affinity, "" ) ) )
     fd_cstr_append_cstr_safe( args->load.affinity, config->development.bench.affinity, sizeof( args->load.affinity )-1UL );
@@ -124,15 +126,25 @@ load_cmd_fn( args_t *         args,
         configure_args.configure.stages[ 0 ] = STAGES[ i ];
     }
   }
-  configure_args.configure.stages[ 2 ] = NULL;
+  configure_args.configure.stages[ 1 ] = NULL;
   configure_cmd_fn( &configure_args, config );
 
-  run_firedancer_init( config, 1 );
+  initialize_workspaces( config );
+  initialize_stacks( config );
 
-  fd_xdp_fds_t fds = fd_topo_install_xdp( &config->topo );
-  (void)fds;
-
-  // Do we need a sandbox?
+  FD_LOG_NOTICE(( "Running" ));
+  FD_LOG_NOTICE(( "  --tpu-ip " FD_IP4_ADDR_FMT, FD_IP4_ADDR_FMT_ARGS( args->load.tpu_ip ) ));
+  FD_LOG_NOTICE(( "  --tpu-port %d",             args->load.tpu_port                       ));
+  FD_LOG_NOTICE(( "  --rpc-ip " FD_IP4_ADDR_FMT, FD_IP4_ADDR_FMT_ARGS( args->load.rpc_ip ) ));
+  FD_LOG_NOTICE(( "  --rpc-port %d",             args->load.rpc_port                       ));
+  FD_LOG_NOTICE(( "  --affinity %s",             args->load.affinity                       ));
+  FD_LOG_NOTICE(( "  --num-benchg %lu",          args->load.benchg                         ));
+  FD_LOG_NOTICE(( "  --num-benchs %lu",          args->load.benchs                         ));
+  FD_LOG_NOTICE(( "  --num-accounts %lu",        args->load.accounts                       ));
+  FD_LOG_NOTICE(( "  --connections %lu",         args->load.connections                    ));
+  FD_LOG_NOTICE(( "  --transaction-mode %d",     args->load.transaction_mode               ));
+  FD_LOG_NOTICE(( "  --contending-fraction %g",  (double)args->load.contending_fraction    ));
+  FD_LOG_NOTICE(( "  --cu-price-spread %g",      (double)args->load.cu_price_spread        ));
 
   fd_topo_run_single_process( &config->topo, 0, config->uid, config->gid, fdctl_tile_run, NULL );
 
