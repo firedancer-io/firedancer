77cbdc61bd1630cad31c33175a51d451d0205b9e
two-heart <12869538+two-heart@users.noreply.github.com>
2024-12-05 21:04:38 +0100

    fuzz: enable all handholding

--- config/extra/with-fuzz.mk
+++ config/extra/with-fuzz.mk
@@ -1,5 +1,13 @@
 include config/extra/with-fuzz-stubs.mk
+
 FD_HAS_FUZZ:=1
+
+FD_EQVOC_USE_HANDHOLDING:=1
+FD_FORKS_USE_HANDHOLDING:=1
+FD_GHOST_USE_HANDHOLDING:=1
+FD_SCRATCH_USE_HANDHOLDING:=1
+FD_TOWER_USE_HANDHOLDING:=1
+FD_TXN_HANDHOLDING:=1
 
 CPPFLAGS+=-fno-omit-frame-pointer
 CPPFLAGS+=-fsanitize=fuzzer-no-link
