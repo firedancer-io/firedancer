d31c64e0938bb3f49136a378f424888ec335a711
Anoushk Kharangate <kharangateanoushk04@gmail.com>
2024-12-17 13:52:11 +0000

    monitor: separate tables into pages
    Signed-off-by: Anoushk Kharangate <kharangateanoushk04@gmail.com>

--- src/app/shared/commands/monitor/helper.h
+++ src/app/shared/commands/monitor/helper.h
@@ -107,4 +107,11 @@ printf_pct( char ** buf,
             ulong  den_then,
             double lhopital_den );
 
-#endif /* HEADER_fd_src_app_shared_commands_monitor_helper_h */
+/* fd_getchar does a non-blocking read of one byte from stdin.
+   Temporarily disables canonical and echo mode while doing the read.
+   Returns the byte in [1,256) on success. Returns 0 if stdin was not
+   ready for reading (select(2)) or a null byte was read. */
+int
+fd_getchar( void );
+
+#endif /* HEADER_fd_src_app_fdctl_monitor_helper_h */
