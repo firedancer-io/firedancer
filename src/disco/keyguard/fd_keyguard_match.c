1c0873787b81195e16d74057db07e481312e00a8
Michael McGee <mmcgee@jumptrading.com>
2024-12-18 22:10:13 +0000

    sign: add signing function for events

--- src/disco/keyguard/fd_keyguard_match.c
+++ src/disco/keyguard/fd_keyguard_match.c
@@ -292,6 +292,18 @@ fd_keyguard_payload_matches_bundle( uchar const * data,
   return 1;
 }
 
+FD_FN_PURE int
+fd_keyguard_payload_matches_event( uchar const * data,
+                                   ulong         sz,
+                                   int           sign_type ) {
+  (void)data;
+
+  if( sign_type != FD_KEYGUARD_SIGN_TYPE_FD_METRICS_REPORT_CONCAT_ED25519 ) return 0;
+  if( sz!=32UL ) return 0;
+
+  return 1;
+}
+
 FD_FN_PURE ulong
 fd_keyguard_payload_match( uchar const * data,
                            ulong         sz,
@@ -305,5 +317,6 @@ fd_keyguard_payload_match( uchar const * data,
   res |= fd_ulong_if( fd_keyguard_payload_matches_tls_cv    ( data, sz, sign_type ), FD_KEYGUARD_PAYLOAD_TLS_CV, 0 );
   res |= fd_ulong_if( fd_keyguard_payload_matches_ping_msg  ( data, sz, sign_type ), FD_KEYGUARD_PAYLOAD_PING,   0 );
   res |= fd_ulong_if( fd_keyguard_payload_matches_bundle    ( data, sz, sign_type ), FD_KEYGUARD_PAYLOAD_BUNDLE, 0 );
+  res |= fd_ulong_if( fd_keyguard_payload_matches_event     ( data, sz, sign_type ), FD_KEYGUARD_PAYLOAD_EVENT,  0 );
   return res;
 }
