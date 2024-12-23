14ec6534999dccfd62ffa0ad41e0a8b734a13d0f
Emily Wang <emwang@jumptrading.com>
2024-12-23 13:45:01 +0000

    separate batch and entry

--- src/flamenco/runtime/fd_blockstore_ctl.c
+++ src/flamenco/runtime/fd_blockstore_ctl.c
@@ -1,6 +1,41 @@
 #include "../../flamenco/runtime/fd_blockstore.h"
 #include "../../flamenco/runtime/fd_rocksdb.h"
 
+#define INITIALIZE_BLOCKSTORE( blockstore )                                              \
+    ulong shred_max = 1 << 15;                                                           \
+    ulong idx_max = 1 << 15;                                                             \
+    ulong block_max = 1 << 15;                                                           \
+    ulong txn_max = 1 << 15;                                                             \
+    void * mem = fd_wksp_alloc_laddr( wksp,                                              \
+                                        fd_blockstore_align(),                           \
+                                        fd_blockstore_footprint( shred_max,              \
+                                                                 block_max,              \
+                                                                 idx_max,                \
+                                                                 txn_max ),              \
+                                        1UL );                                           \
+    FD_TEST( mem );                                                                      \
+    fd_blockstore_t * blockstore = fd_blockstore_join( fd_blockstore_new( mem,           \
+                                                                            1,           \
+                                                                            0,           \
+                                                                            shred_max,   \
+                                                                            block_max,   \
+                                                                            idx_max,     \
+                                                                            txn_max ) ); \
+    FD_TEST( blockstore );                                                               \
+    fd_slot_bank_t slot_bank = {                                                         \
+        .slot = 1,                                                                       \
+        .prev_slot = 0,                                                                  \
+        .banks_hash = {.hash = {0}},                                                     \
+        .block_height = 1,                                                               \
+    };                                                                                   \
+    fd_slot_bank_new( &slot_bank );                                                      \
+    fd_hash_t fake_hash = {.hash = {1}};                                                 \
+    slot_bank.block_hash_queue.last_hash = &fake_hash;                                   \
+    slot_bank.block_hash_queue.last_hash_index = 0;                                      \
+    int fd = open("test.txt", O_RDWR | O_CREAT, 0666);                                   \            
+    FD_TEST( fd > 0 );
+
+
 
 struct fd_entry_row {
   ulong slot;
@@ -9,6 +44,134 @@ struct fd_entry_row {
   ulong shred_cnt;
 };
 typedef struct fd_entry_row fd_entry_row_t;
+
+static void
+aggregate_entries( fd_wksp_t * wksp ){
+    INITIALIZE_BLOCKSTORE( blockstore );
+
+    FD_TEST(fd_blockstore_init(blockstore, fd, FD_BLOCKSTORE_ARCHIVE_MIN_SIZE, &slot_bank));
+
+    fd_rocksdb_t           rocks_db         = {0};
+    fd_rocksdb_root_iter_t iter             = {0};
+
+    char * err = fd_rocksdb_init( &rocks_db, "/data/ledgers/mainnet-307987557/rocksdb" );
+    FD_LOG_NOTICE(( "rocksdb init: %s", err ));
+
+    fd_rocksdb_root_iter_new( &iter );
+    void *       alloc_mem = fd_wksp_alloc_laddr( wksp, fd_alloc_align(), fd_alloc_footprint(), 1UL );
+    fd_alloc_t * alloc     = fd_alloc_join( fd_alloc_new( alloc_mem, 1UL ), 1UL );
+    fd_valloc_t  valloc    = fd_alloc_virtual( alloc );
+
+    fd_slot_meta_t slot_meta = { 0 };
+    uchar trash_hash_buf[32];
+    memset( trash_hash_buf, 0xFE, sizeof(trash_hash_buf) );
+
+    ulong st  = 308015637;
+    ulong end = 308016637;
+
+    ulong populated_slots[end - st + 1];
+    memset( populated_slots, -1, sizeof(populated_slots) );
+    int slot_idx = 0;
+    for (ulong slot = st; slot <= end; slot++) {
+      int err = fd_rocksdb_root_iter_seek( &iter, &rocks_db, slot, &slot_meta, valloc );
+
+      if( err < 0 ) continue;
+
+      err = fd_rocksdb_import_block_blockstore( &rocks_db, &slot_meta, blockstore, 1, trash_hash_buf );
+      if( FD_UNLIKELY( err != 0) ) {
+        FD_LOG_ERR(( "Failed to import block %lu", slot ));
+      }
+      populated_slots[slot_idx++] = slot;
+    }
+
+    fd_entry_row_t row = {0};
+    fd_block_t * block = NULL;
+    // iterate the blocks:
+    for( int i = 0; i < slot_idx; i++ ) {
+      ulong slot = populated_slots[i];
+      row.slot   = slot;
+
+      block = fd_blockstore_block_query( blockstore, slot );
+      FD_TEST( block );
+    }
+}
+
+static void
+aggregate_batch_entries( fd_wksp_t * wksp ){
+  INITIALIZE_BLOCKSTORE( blockstore );
+
+  FD_TEST(fd_blockstore_init(blockstore, fd, FD_BLOCKSTORE_ARCHIVE_MIN_SIZE, &slot_bank));
+
+  fd_rocksdb_t           rocks_db         = {0};
+  fd_rocksdb_root_iter_t iter             = {0};
+
+  char * err = fd_rocksdb_init( &rocks_db, "/data/ledgers/mainnet-307987557/rocksdb" );
+  FD_LOG_NOTICE(( "rocksdb init: %s", err ));
+
+  fd_rocksdb_root_iter_new( &iter );
+  void *       alloc_mem = fd_wksp_alloc_laddr( wksp, fd_alloc_align(), fd_alloc_footprint(), 1UL );
+  fd_alloc_t * alloc     = fd_alloc_join( fd_alloc_new( alloc_mem, 1UL ), 1UL );
+  fd_valloc_t  valloc    = fd_alloc_virtual( alloc );
+
+  fd_slot_meta_t slot_meta = { 0 };
+  uchar trash_hash_buf[32];
+  memset( trash_hash_buf, 0xFE, sizeof(trash_hash_buf) );
+
+  ulong st  = 308015637;
+  ulong end = 308016637;
+
+  ulong populated_slots[end - st + 1];
+  memset( populated_slots, -1, sizeof(populated_slots) );
+  int slot_idx = 0;
+  for (ulong slot = st; slot <= end; slot++) {
+    int err = fd_rocksdb_root_iter_seek( &iter, &rocks_db, slot, &slot_meta, valloc );
+
+    if( err < 0 ) continue;
+
+    err = fd_rocksdb_import_block_blockstore( &rocks_db, &slot_meta, blockstore, 1, trash_hash_buf );
+    if( FD_UNLIKELY( err != 0) ) {
+      FD_LOG_ERR(( "Failed to import block %lu", slot ));
+    }
+    populated_slots[slot_idx++] = slot;
+  }
+
+  fd_entry_row_t row = {0};
+  fd_block_t * block = NULL;
+  // iterate the blocks:
+  for( int i = 0; i < slot_idx; i++ ) {
+    ulong slot = populated_slots[i];
+    row.slot   = slot;
+
+    block = fd_blockstore_block_query( blockstore, slot );
+    FD_TEST( block );
+    
+    fd_block_shred_t * shreds = fd_wksp_laddr_fast( wksp, block->shreds_gaddr );
+    ulong batch_start         = 0;
+    ulong batch_sz            = 0;
+    for ( ulong shred_idx = 0; shred_idx < block->shreds_cnt; shred_idx++ ) {
+      fd_block_shred_t * shred = &shreds[shred_idx];
+      batch_sz += fd_shred_payload_sz( &shred->hdr );
+
+      if( shred->hdr.data.flags & FD_SHRED_DATA_FLAG_DATA_COMPLETE ) {
+        row.shred_cnt = shred_idx - batch_start + 1;
+        row.ref_tick  = (uchar)( (int)shred->hdr.data.flags &
+                                      (int)FD_SHRED_DATA_REF_TICK_MASK );
+        row.sz        = batch_sz;
+        batch_start   = shred_idx + 1;
+
+        FD_LOG_NOTICE(( "Entry | slot: %lu, ref_tick: %d, payload_sz: %lu, shred_cnt: %lu",
+                row.slot, (int) row.ref_tick, row.sz, row.shred_cnt ));
+      }
+    }
+  }
+  fd_block_micro_t *    micros     = fd_wksp_laddr_fast( wksp, block->micros_gaddr );
+  
+  uchar *               data       = fd_wksp_laddr_fast( wksp, block->data_gaddr );
+  fd_microblock_hdr_t * last_micro = (fd_microblock_hdr_t *)( data +
+                                                                micros[block->micros_cnt - 1].off );
+
+  FD_LOG_NOTICE(( "Micro Block txn_cnt: %lu, hash_cnt: %lu", last_micro->txn_cnt, last_micro->hash_cnt ));
+}
 
 int
 main( int argc, char ** argv ) {
@@ -28,104 +191,7 @@ main( int argc, char ** argv ) {
                                             0UL );
   FD_TEST( wksp );
 
-  ulong shred_max = 1 << 15;
-  ulong idx_max = 1 << 15;
-  ulong block_max = 1 << 15;
-  ulong txn_max = 1 << 15;
-  void * mem = fd_wksp_alloc_laddr( wksp,                                              
-                                    fd_blockstore_align(),                             
-                                    fd_blockstore_footprint( shred_max,                
-                                                             block_max,                
-                                                             idx_max,                  
-                                                             txn_max ),                
-                                    1UL );                                             
-  FD_TEST( mem );                                                                      
-  fd_blockstore_t * blockstore = fd_blockstore_join( fd_blockstore_new( mem,           
-                                                                        1,             
-                                                                        0,             
-                                                                        shred_max,     
-                                                                        block_max,     
-                                                                        idx_max,       
-                                                                        txn_max ) );   
-  FD_TEST( blockstore );                                                               
-  fd_slot_bank_t slot_bank = {                                                         
-      .slot = 1,                                                                       
-      .prev_slot = 0,                                                                  
-      .banks_hash = {.hash = {0}},                                                     
-      .block_height = 1,                                                               
-  };                                                                                   
-  fd_slot_bank_new( &slot_bank );                                                      
-  fd_hash_t fake_hash = {.hash = {1}};                                                 
-  slot_bank.block_hash_queue.last_hash = &fake_hash;                                   
-  slot_bank.block_hash_queue.last_hash_index = 0;
-
-  int fd = open("test.txt", O_RDWR | O_CREAT, 0666);
-  FD_TEST( fd > 0 );
-
-  FD_TEST(fd_blockstore_init(blockstore, fd, FD_BLOCKSTORE_ARCHIVE_MIN_SIZE, &slot_bank));
-
-
-
-  fd_rocksdb_t           rocks_db         = {0};
-  fd_rocksdb_root_iter_t iter             = {0};
-  char * err = fd_rocksdb_init( &rocks_db, "/data/ledgers/mainnet-307987557/rocksdb" );
-  FD_LOG_NOTICE(( "rocksdb init: %s", err ));
-  //FD_TEST( err );
-  fd_rocksdb_root_iter_new( &iter );
-
-  void *       alloc_mem = fd_wksp_alloc_laddr( wksp, fd_alloc_align(), fd_alloc_footprint(), 1UL );
-  fd_alloc_t * alloc     = fd_alloc_join( fd_alloc_new( alloc_mem, 1UL ), 1UL );
-  fd_valloc_t  valloc    = fd_alloc_virtual( alloc );
-
-  fd_slot_meta_t slot_meta = { 0 };
-  uchar trash_hash_buf[32];
-
-  ulong st = 308015637;
-  ulong end = 308016637;
-
-  memset( trash_hash_buf, 0xFE, sizeof(trash_hash_buf) );
-  for (ulong slot = st; slot <= end; slot++) {
-    //slot_meta.slot = slot;
-    int err = fd_rocksdb_root_iter_seek( &iter, &rocks_db, slot, &slot_meta, valloc );
-    //yet__asm__("int $3");
-
-    FD_LOG_NOTICE(( "block found: %d", err ));
-    //__asm__("int $3");
-    err = fd_rocksdb_import_block_blockstore( &rocks_db, &slot_meta, blockstore, 1, trash_hash_buf );
-    if( FD_UNLIKELY( err != 0) ) {
-      FD_LOG_ERR(( "Failed to import block %lu", slot ));
-    }
-  }
-
-  fd_entry_row_t row = {0};
-
-  // iterate the blocks:
-  for( ulong slot = st; slot <= end; slot++ ) {
-    fd_block_t * block = fd_blockstore_block_query( blockstore, slot );
-    FD_TEST( block );
-
-    row.slot = slot;
-    
-    fd_block_shred_t * shreds = fd_wksp_laddr_fast( wksp, block->shreds_gaddr );
-    ulong batch_start         = 0;
-    ulong batch_sz            = 0;
-    for ( ulong shred_idx = 0; shred_idx < block->shreds_cnt; shred_idx++ ) {
-      fd_block_shred_t * shred = &shreds[shred_idx];
-      batch_sz += fd_shred_payload_sz( &shred->hdr );
-
-      if( shred->hdr.data.flags & FD_SHRED_DATA_FLAG_DATA_COMPLETE ) {
-        row.shred_cnt = shred_idx - batch_start + 1;
-        row.ref_tick  = (uchar)( (int)shred->hdr.data.flags &
-                                      (int)FD_SHRED_DATA_REF_TICK_MASK );
-        row.sz        = batch_sz;
-        
-        FD_LOG_NOTICE(( "Batch | slot: %lu, ref_tick: %c, sz: %lu, shred_cnt: %lu",
-                        row.slot, row.ref_tick, row.sz, row.shred_cnt ));
-
-        batch_start   = shred_idx + 1;
-      }
-    }
-  }
+  aggregate_batch_entries( wksp );
   
   fd_halt();
   return 0;
