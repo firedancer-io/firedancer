18d8d622470515740873480f2d5f5f7dd2050249
Richard Patel <ripatel@jumptrading.com>
2025-01-03 12:13:19 +0000

    Improve handling of enum metrics
    - Add internal API to prepare for Prometheus enum outputting
    - Deduplicate code in code generator

--- src/disco/metrics/generate/write_codegen.py
+++ src/disco/metrics/generate/write_codegen.py
@@ -4,33 +4,26 @@ from typing import TextIO
 import os
 import re
 
+def camel2snake(str):
+    return re.sub(r'(?<!^)(?=[A-Z])', '_', str).upper()
+
 def _write_metric(f: TextIO, metric: Metric, prefix: str):
-    full_name = re.sub(r'(?<!^)(?=[A-Z])', '_', metric.name).upper()
+    full_name = camel2snake(metric.name)
     description = ' '.join([line.strip() for line in metric.description.split('\n')]).strip()
     converter = 'NONE'
     if isinstance(metric, HistogramMetric):
         converter = metric.converter.name
 
+    f.write(f'#define FD_METRICS_{metric.type.name.upper()}_{prefix.upper()}_{full_name}_OFF  ({metric.offset}UL)\n')
+    f.write(f'#define FD_METRICS_{metric.type.name.upper()}_{prefix.upper()}_{full_name}_NAME "{prefix}_{full_name.lower()}"\n')
+    f.write(f'#define FD_METRICS_{metric.type.name.upper()}_{prefix.upper()}_{full_name}_TYPE (FD_METRICS_TYPE_{metric.type.name})\n')
+    f.write(f'#define FD_METRICS_{metric.type.name.upper()}_{prefix.upper()}_{full_name}_DESC "{description}"\n')
+    f.write(f'#define FD_METRICS_{metric.type.name.upper()}_{prefix.upper()}_{full_name}_CVT  (FD_METRICS_CONVERTER_{converter})\n')
+
     if isinstance(metric, GaugeEnumMetric) or isinstance(metric, CounterEnumMetric):
-        f.write(f'#define FD_METRICS_{metric.type.name.upper()}_{prefix.upper()}_{full_name}_OFF  ({metric.offset}UL)\n')
         f.write(f'#define FD_METRICS_{metric.type.name.upper()}_{prefix.upper()}_{full_name}_CNT  ({len(metric.enum.values)}UL)\n\n')
-
-        offset: int = 0
-
-        for value in metric.enum.values:
-            value_name = re.sub(r'(?<!^)(?=[A-Z])', '_', value.name).upper()
-            f.write(f'#define FD_METRICS_{metric.type.name.upper()}_{prefix.upper()}_{full_name}_{value_name.upper()}_OFF  ({metric.offset+offset}UL)\n')
-            f.write(f'#define FD_METRICS_{metric.type.name.upper()}_{prefix.upper()}_{full_name}_{value_name.upper()}_NAME "{prefix}_{full_name.lower()}_{value_name.lower()}"\n')
-            f.write(f'#define FD_METRICS_{metric.type.name.upper()}_{prefix.upper()}_{full_name}_{value_name.upper()}_TYPE (FD_METRICS_TYPE_{metric.type.name})\n')
-            f.write(f'#define FD_METRICS_{metric.type.name.upper()}_{prefix.upper()}_{full_name}_{value_name.upper()}_DESC "{description} ({value.label})"\n')
-            f.write(f'#define FD_METRICS_{metric.type.name.upper()}_{prefix.upper()}_{full_name}_{value_name.upper()}_CVT  (FD_METRICS_CONVERTER_{converter})\n\n')
-            offset += 1
-    else:
-        f.write(f'#define FD_METRICS_{metric.type.name.upper()}_{prefix.upper()}_{full_name}_OFF  ({metric.offset}UL)\n')
-        f.write(f'#define FD_METRICS_{metric.type.name.upper()}_{prefix.upper()}_{full_name}_NAME "{prefix}_{full_name.lower()}"\n')
-        f.write(f'#define FD_METRICS_{metric.type.name.upper()}_{prefix.upper()}_{full_name}_TYPE (FD_METRICS_TYPE_{metric.type.name})\n')
-        f.write(f'#define FD_METRICS_{metric.type.name.upper()}_{prefix.upper()}_{full_name}_DESC "{description}"\n')
-        f.write(f'#define FD_METRICS_{metric.type.name.upper()}_{prefix.upper()}_{full_name}_CVT  (FD_METRICS_CONVERTER_{converter})\n')
+        for idx, value in enumerate(metric.enum.values):
+            f.write(f'#define FD_METRICS_{metric.type.name.upper()}_{prefix.upper()}_{full_name}_{camel2snake(value.name)}_OFF ({metric.offset+idx}UL)\n')
 
     if isinstance(metric, HistogramMetric):
         if metric.converter == HistogramConverter.SECONDS:
@@ -44,6 +37,28 @@ def _write_metric(f: TextIO, metric: Metric, prefix: str):
         f.write(f'#define FD_METRICS_{metric.type.name.upper()}_{prefix.upper()}_{full_name}_MAX  ({max_str})\n')
 
     f.write('\n')
+
+def _write_metric_descriptor(f, full_name, metric: Metric):
+    if isinstance(metric, CounterMetric):
+        f.write(f'    DECLARE_METRIC( {full_name}, COUNTER ),\n')
+    elif isinstance(metric, GaugeMetric):
+        f.write(f'    DECLARE_METRIC( {full_name}, GAUGE ),\n')
+    elif isinstance(metric, CounterEnumMetric):
+        for value in metric.enum.values:
+            f.write(f'    DECLARE_METRIC_ENUM( {full_name}, COUNTER, {camel2snake(metric.enum.name)}, {camel2snake(value.name)} ),\n')
+    elif isinstance(metric, GaugeEnumMetric):
+        for value in metric.enum.values:
+            f.write(f'    DECLARE_METRIC_ENUM( {full_name}, GAUGE, {camel2snake(metric.enum.name)}, {camel2snake(value.name)} ),\n')
+    elif isinstance(metric, HistogramMetric):
+        if metric.converter == HistogramConverter.SECONDS:
+            f.write(f'    DECLARE_METRIC_HISTOGRAM_SECONDS( {full_name} ),\n')
+        elif metric.converter == HistogramConverter.NONE:
+            f.write(f'    DECLARE_METRIC_HISTOGRAM_NONE( {full_name} ),\n')
+        else:
+            raise Exception(f'Unknown histogram converter: {metric.converter}')
+    else:
+        raise ValueError("Unknown metric type")
+    pass
 
 def _write_common(metrics: Metrics):
     with open(Path(__file__).parent / '../generated' / 'fd_metrics_all.h', 'w') as f:
@@ -92,77 +107,20 @@ def _write_common(metrics: Metrics):
 
         f.write('const fd_metrics_meta_t FD_METRICS_ALL[FD_METRICS_ALL_TOTAL] = {\n')
         for metric in metrics.common:
-            declare: str = ''
-            if metric.type == MetricType.COUNTER:
-                declare = 'DECLARE_METRIC_COUNTER'
-            elif metric.type == MetricType.GAUGE:
-                declare = 'DECLARE_METRIC_GAUGE'
-            elif metric.type == MetricType.HISTOGRAM:
-                assert isinstance(metric, HistogramMetric)
-                if metric.converter == HistogramConverter.SECONDS:
-                    declare = 'DECLARE_METRIC_HISTOGRAM_SECONDS'
-                elif metric.converter == HistogramConverter.NONE:
-                    declare = 'DECLARE_METRIC_HISTOGRAM_NONE'
-                else:
-                    raise Exception(f'Unknown histogram converter: {metric.converter}')
-
-            full_name = re.sub(r'(?<!^)(?=[A-Z])', '_', metric.name).upper()
-            if isinstance(metric, GaugeEnumMetric) or isinstance(metric, CounterEnumMetric):
-                for value in metric.enum.values:
-                    value_name = re.sub(r'(?<!^)(?=[A-Z])', '_', value.name).upper()
-                    f.write(f'    {declare}( TILE, {full_name}_{value_name} ),\n')
-            else:
-                f.write(f'    {declare}( TILE, {full_name} ),\n')
+            full_name = f'TILE_{camel2snake(metric.name)}'
+            _write_metric_descriptor(f, full_name, metric)
         f.write('};\n\n')
 
         f.write('const fd_metrics_meta_t FD_METRICS_ALL_LINK_IN[FD_METRICS_ALL_LINK_IN_TOTAL] = {\n')
         for metric in metrics.link_in:
-            declare: str = ''
-            if metric.type == MetricType.COUNTER:
-                declare = 'DECLARE_METRIC_COUNTER'
-            elif metric.type == MetricType.GAUGE:
-                declare = 'DECLARE_METRIC_GAUGE'
-            elif metric.type == MetricType.HISTOGRAM:
-                assert isinstance(metric, HistogramMetric)
-                if metric.converter == HistogramConverter.SECONDS:
-                    declare = 'DECLARE_METRIC_HISTOGRAM_SECONDS'
-                elif metric.converter == HistogramConverter.NONE:
-                    declare = 'DECLARE_METRIC_HISTOGRAM_NONE'
-                else:
-                    raise Exception(f'Unknown histogram converter: {metric.converter}')
-
-            full_name = re.sub(r'(?<!^)(?=[A-Z])', '_', metric.name).upper()
-            if isinstance(metric, GaugeEnumMetric) or isinstance(metric, CounterEnumMetric):
-                for value in metric.enum.values:
-                    value_name = re.sub(r'(?<!^)(?=[A-Z])', '_', value.name).upper()
-                    f.write(f'    {declare}( LINK, {full_name}_{value_name} ),\n')
-            else:
-                f.write(f'    {declare}( LINK, {full_name} ),\n')
+            full_name = f'LINK_{camel2snake(metric.name)}'
+            _write_metric_descriptor(f, full_name, metric)
         f.write('};\n\n')
 
         f.write(f'const fd_metrics_meta_t FD_METRICS_ALL_LINK_OUT[FD_METRICS_ALL_LINK_OUT_TOTAL] = {{\n')
         for metric in metrics.link_out:
-            declare: str = ''
-            if metric.type == MetricType.COUNTER:
-                declare = 'DECLARE_METRIC_COUNTER'
-            elif metric.type == MetricType.GAUGE:
-                declare = 'DECLARE_METRIC_GAUGE'
-            elif metric.type == MetricType.HISTOGRAM:
-                assert isinstance(metric, HistogramMetric)
-                if metric.converter == HistogramConverter.SECONDS:
-                    declare = 'DECLARE_METRIC_HISTOGRAM_SECONDS'
-                elif metric.converter == HistogramConverter.NONE:
-                    declare = 'DECLARE_METRIC_HISTOGRAM_NONE'
-                else:
-                    raise Exception(f'Unknown histogram converter: {metric.converter}')
-
-            full_name = re.sub(r'(?<!^)(?=[A-Z])', '_', metric.name).upper()
-            if isinstance(metric, GaugeEnumMetric) or isinstance(metric, CounterEnumMetric):
-                for value in metric.enum.values:
-                    value_name = re.sub(r'(?<!^)(?=[A-Z])', '_', value.name).upper()
-                    f.write(f'    {declare}( LINK, {full_name}_{value_name} ),\n')
-            else:
-                f.write(f'    {declare}( LINK, {full_name} ),\n')
+            full_name = f'LINK_{camel2snake(metric.name)}'
+            _write_metric_descriptor(f, full_name, metric)
         f.write('};\n\n')
 
         f.write(f'const char * FD_METRICS_TILE_KIND_NAMES[FD_METRICS_TILE_KIND_CNT] = {{\n')
@@ -187,7 +145,8 @@ def _write_common(metrics: Metrics):
 def _write_tile(tile: Tile, metrics: List[Metric]):
     with open(Path(__file__).parent / '../generated' / f'fd_metrics_{tile.name.lower()}.h', 'w') as f:
         f.write('/* THIS FILE IS GENERATED BY gen_metrics.py. DO NOT HAND EDIT. */\n\n')
-        f.write('#include "../fd_metrics_base.h"\n\n')
+        f.write('#include "../fd_metrics_base.h"\n')
+        f.write('#include "fd_metrics_enums.h"\n\n')
 
         for metric in metrics:
             _write_metric(f, metric, tile.name.lower())
@@ -202,28 +161,19 @@ def _write_tile(tile: Tile, metrics: List[Metric]):
 
         f.write(f'const fd_metrics_meta_t FD_METRICS_{tile.name}[FD_METRICS_{tile.name}_TOTAL] = {{\n')
         for metric in metrics:
-            declare: str = ''
-            if metric.type == MetricType.COUNTER:
-                declare = 'DECLARE_METRIC_COUNTER'
-            elif metric.type == MetricType.GAUGE:
-                declare = 'DECLARE_METRIC_GAUGE'
-            elif metric.type == MetricType.HISTOGRAM:
-                assert isinstance(metric, HistogramMetric)
-                if metric.converter == HistogramConverter.SECONDS:
-                    declare = 'DECLARE_METRIC_HISTOGRAM_SECONDS'
-                elif metric.converter == HistogramConverter.NONE:
-                    declare = 'DECLARE_METRIC_HISTOGRAM_NONE'
-                else:
-                    raise Exception(f'Unknown histogram converter: {metric.converter}')
-
-            full_name = re.sub(r'(?<!^)(?=[A-Z])', '_', metric.name).upper()
-            if isinstance(metric, GaugeEnumMetric) or isinstance(metric, CounterEnumMetric):
-                for value in metric.enum.values:
-                    value_name = re.sub(r'(?<!^)(?=[A-Z])', '_', value.name).upper()
-                    f.write(f'    {declare}( {tile.name}, {full_name}_{value_name} ),\n')
-            else:
-                f.write(f'    {declare}( {tile.name}, {full_name} ),\n')
+            full_name = f'{tile.name}_{camel2snake(metric.name)}'
+            _write_metric_descriptor(f, full_name, metric)
         f.write('};\n')
+
+def _write_enums(enums: List[MetricEnum]):
+    with open(Path(__file__).parent / '../generated' / 'fd_metrics_enums.h', 'w') as f:
+        f.write('/* THIS FILE IS GENERATED BY gen_metrics.py. DO NOT HAND EDIT. */\n\n')
+        for enum in enums:
+            f.write(f'#define FD_METRICS_ENUM_{camel2snake(enum.name)}_NAME "{camel2snake(enum.name).lower()}"\n')
+            for idx, enum_value in enumerate(enum.values):
+                f.write(f'#define FD_METRICS_ENUM_{camel2snake(enum.name)}_V_{camel2snake(enum_value.name)}_IDX  {idx}\n')
+                f.write(f'#define FD_METRICS_ENUM_{camel2snake(enum.name)}_V_{camel2snake(enum_value.name)}_NAME "{camel2snake(enum_value.name).lower()}"\n')
+            f.write('\n')
 
 def write_codegen(metrics: Metrics):
     os.makedirs(Path(__file__).parent / '../generated', exist_ok=True)
@@ -231,6 +181,6 @@ def write_codegen(metrics: Metrics):
     _write_common(metrics)
     for (tile, tile_metrics) in metrics.tiles.items():
         _write_tile(tile, tile_metrics)
-
+    _write_enums(metrics.enums.values())
 
     print(f'Generated {metrics.count()} metrics for {len(metrics.tiles)} tiles')
