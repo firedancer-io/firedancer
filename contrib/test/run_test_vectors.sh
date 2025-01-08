1560eae769a51fa7ef9e277bb039193de30319a0
Yufeng Zhou <yufzhou@jumptrading.com>
2025-01-08 18:19:42 +0000

    ci: bump number of parallel jobs for test vectors

--- contrib/test/run_test_vectors.sh
+++ contrib/test/run_test_vectors.sh
@@ -34,31 +34,31 @@ git checkout -q $GIT_REF
 cd ../..
 
 LOG=$LOG_PATH/test_exec_syscall
-cat contrib/test/test-vectors-fixtures/syscall-fixtures/*.list | xargs -P 8 -n 1000 ./$OBJDIR/unit-test/test_exec_sol_compat --log-path $LOG --wksp-page-sz 1073741824
+cat contrib/test/test-vectors-fixtures/syscall-fixtures/*.list | xargs -P 12 -n 1000 ./$OBJDIR/unit-test/test_exec_sol_compat --log-path $LOG --wksp-page-sz 1073741824
 
 LOG=$LOG_PATH/test_exec_interp
-cat contrib/test/test-vectors-fixtures/vm-interp-fixtures/*.list | xargs -P 8 -n 1000 ./$OBJDIR/unit-test/test_exec_sol_compat --log-path $LOG --wksp-page-sz 1073741824
-find dump/test-vectors/vm_interp/fixtures/v0 -type f -name '*.fix' | xargs -P 8 -n 1000 ./$OBJDIR/unit-test/test_exec_sol_compat --log-path $LOG --wksp-page-sz 1073741824
-find dump/test-vectors/vm_interp/fixtures/v1 -type f -name '*.fix' | xargs -P 8 -n 1000 ./$OBJDIR/unit-test/test_exec_sol_compat --log-path $LOG --wksp-page-sz 1073741824
-find dump/test-vectors/vm_interp/fixtures/v2 -type f -name '*.fix' | xargs -P 8 -n 1000 ./$OBJDIR/unit-test/test_exec_sol_compat --log-path $LOG --wksp-page-sz 1073741824
+cat contrib/test/test-vectors-fixtures/vm-interp-fixtures/*.list | xargs -P 12 -n 1000 ./$OBJDIR/unit-test/test_exec_sol_compat --log-path $LOG --wksp-page-sz 1073741824
+find dump/test-vectors/vm_interp/fixtures/v0 -type f -name '*.fix' | xargs -P 12 -n 1000 ./$OBJDIR/unit-test/test_exec_sol_compat --log-path $LOG --wksp-page-sz 1073741824
+find dump/test-vectors/vm_interp/fixtures/v1 -type f -name '*.fix' | xargs -P 12 -n 1000 ./$OBJDIR/unit-test/test_exec_sol_compat --log-path $LOG --wksp-page-sz 1073741824
+find dump/test-vectors/vm_interp/fixtures/v2 -type f -name '*.fix' | xargs -P 12 -n 1000 ./$OBJDIR/unit-test/test_exec_sol_compat --log-path $LOG --wksp-page-sz 1073741824
 
 LOG=$LOG_PATH/test_exec_precompiles
-cat contrib/test/test-vectors-fixtures/precompile-fixtures/*.list | xargs -P 4 -n 1000 ./$OBJDIR/unit-test/test_exec_sol_compat --log-path $LOG --wksp-page-sz 1073741824
+cat contrib/test/test-vectors-fixtures/precompile-fixtures/*.list | xargs -P 12 -n 1000 ./$OBJDIR/unit-test/test_exec_sol_compat --log-path $LOG --wksp-page-sz 1073741824
 
 LOG=$LOG_PATH/test_exec_txn
-cat contrib/test/test-vectors-fixtures/txn-fixtures/*.list | xargs -P 4 ./$OBJDIR/unit-test/test_exec_sol_compat --log-path $LOG --wksp-page-sz 1073741824
+cat contrib/test/test-vectors-fixtures/txn-fixtures/*.list | xargs -P 12 ./$OBJDIR/unit-test/test_exec_sol_compat --log-path $LOG --wksp-page-sz 1073741824
 
 zstd -df dump/test-vectors/elf_loader/fixtures/*.zst
 LOG=$LOG_PATH/test_elf_loader
-cat contrib/test/test-vectors-fixtures/elf-loader-fixtures/*.list | xargs -P 4 -n 1000 ./$OBJDIR/unit-test/test_exec_sol_compat --log-path $LOG --wksp-page-sz 1073741824
+cat contrib/test/test-vectors-fixtures/elf-loader-fixtures/*.list | xargs -P 12 -n 1000 ./$OBJDIR/unit-test/test_exec_sol_compat --log-path $LOG --wksp-page-sz 1073741824
 
 LOG=$LOG_PATH/test_exec_instr
-cat contrib/test/test-vectors-fixtures/instr-fixtures/*.list | xargs -P 8 -n 1000 ./$OBJDIR/unit-test/test_exec_sol_compat --log-path $LOG --wksp-page-sz 1073741824
+cat contrib/test/test-vectors-fixtures/instr-fixtures/*.list | xargs -P 12 -n 1000 ./$OBJDIR/unit-test/test_exec_sol_compat --log-path $LOG --wksp-page-sz 1073741824
 
 # check if ./$OBJDIR/unit-test/test_exec_sol_compat_stubbed exists
 if [ -f ./$OBJDIR/unit-test/test_exec_sol_compat_stubbed ]; then
   LOG=$LOG_PATH/test_exec_cpi
-  cat contrib/test/test-vectors-fixtures/cpi-fixtures/*.list | xargs -P 4 -n 1000 ./$OBJDIR/unit-test/test_exec_sol_compat_stubbed --log-path $LOG --wksp-page-sz 1073741824
+  cat contrib/test/test-vectors-fixtures/cpi-fixtures/*.list | xargs -P 12 -n 1000 ./$OBJDIR/unit-test/test_exec_sol_compat_stubbed --log-path $LOG --wksp-page-sz 1073741824
 else
   # skip stubbed tests if the binary does not exist
   echo -e "\e[38;5;214mNOTICE:\e[0m Skipping stubbed tests due to missing stub binary"
