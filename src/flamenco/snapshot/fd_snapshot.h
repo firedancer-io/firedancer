ffdfdae40ba924a0203f5e047f5dfe5df2685007
Richard Patel <ripatel@jumptrading.com>
2025-01-11 14:10:38 +0000

    snapshot: fix broken includes

--- src/flamenco/snapshot/fd_snapshot.h
+++ src/flamenco/snapshot/fd_snapshot.h
@@ -1,7 +1,6 @@
 #ifndef HEADER_fd_src_flamenco_snapshot_fd_snapshot_h
 #define HEADER_fd_src_flamenco_snapshot_fd_snapshot_h
 
-#include "../../funk/fd_funk_txn.h"
 #if FD_HAS_ZSTD
 
 /* fd_snapshot.h provides high-level blocking APIs for Solana snapshots. */
@@ -14,7 +13,7 @@ struct fd_snapshot_load_ctx;
 typedef struct fd_snapshot_load_ctx fd_snapshot_load_ctx_t;
 
 /* fd_snapshot_load_all does a blocking load of a snapshot. It is a wrapper
-   around fd_snapshot_load_new, fd_snapshot_load_init, 
+   around fd_snapshot_load_new, fd_snapshot_load_init,
    fd_snapshot_load_manifest_and_status_cache, and fd_snapshot_load_fini.
 
    fd_snapshot_load_new sets up the context that is passed around for the
@@ -32,8 +31,8 @@ typedef struct fd_snapshot_load_ctx fd_snapshot_load_ctx_t;
    fd_snapshot_load_fini will use the slot context and funk which are now
    populated to setup the runtime and finalize the snapshot load.
 
-   The reason these are broken out is to support loading in the manifest as 
-   quickly as possible, allowing for other operations to start (like 
+   The reason these are broken out is to support loading in the manifest as
+   quickly as possible, allowing for other operations to start (like
    stake-weighted repair) while the rest of the snapshot is being loaded. This
    is needed as loading in the manifest only takes a few seconds and the overall
    time to load in a snapshot is dominated by loading in the append vecs.
@@ -59,7 +58,7 @@ fd_snapshot_load_ctx_align( void );
 ulong
 fd_snapshot_load_ctx_footprint( void );
 
-fd_snapshot_load_ctx_t * 
+fd_snapshot_load_ctx_t *
 fd_snapshot_load_new( uchar *                mem,
                       const char *           snapshot_file,
                       fd_exec_slot_ctx_t *   slot_ctx,
