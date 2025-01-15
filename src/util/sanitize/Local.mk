2bcc4310778fb7371194b294f182d1130e2df109
Alex Siegel <asiegel@jumptrading.com>
2025-01-15 16:33:20 +0000

    added address watch support to deepasan

--- src/util/sanitize/Local.mk
+++ src/util/sanitize/Local.mk
@@ -1,3 +1,8 @@
 $(call add-hdrs,fd_asan.h fd_msan.h fd_sanitize.h)
 $(call make-lib,fd_fuzz_stub)
 $(call add-objs,fd_fuzz_stub,fd_fuzz_stub)
+
+ifdef FD_HAS_DEEPASAN_WATCH
+CPPFLAGS+=-DFD_HAS_DEEPASAN_WATCH=1
+$(call add-objs,fd_asan fd_backtrace,fd_util)
+endif
