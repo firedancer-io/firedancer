43791b805c57ee0ce9387a5dd31873a04f323edb
Ravyu Sivakumaran <rsivakumaran@jumptrading.com>
2025-01-21 17:24:56 -0600

    flamenco, gossip: add metrics

--- src/disco/metrics/generated/fd_metrics_all.h
+++ src/disco/metrics/generated/fd_metrics_all.h
@@ -14,6 +14,7 @@
 #include "fd_metrics_store.h"
 #include "fd_metrics_replay.h"
 #include "fd_metrics_storei.h"
+#include "fd_metrics_gossip.h"
 /* Start of LINK OUT metrics */
 
 #define FD_METRICS_COUNTER_LINK_SLOW_COUNT_OFF  (0UL)
@@ -150,7 +151,7 @@ extern const fd_metrics_meta_t FD_METRICS_ALL_LINK_OUT[FD_METRICS_ALL_LINK_OUT_T
 
 #define FD_METRICS_TOTAL_SZ (8UL*221UL)
 
-#define FD_METRICS_TILE_KIND_CNT 12
+#define FD_METRICS_TILE_KIND_CNT 13
 extern const char * FD_METRICS_TILE_KIND_NAMES[FD_METRICS_TILE_KIND_CNT];
 extern const ulong FD_METRICS_TILE_KIND_SIZES[FD_METRICS_TILE_KIND_CNT];
 extern const fd_metrics_meta_t * FD_METRICS_TILE_KIND_METRICS[FD_METRICS_TILE_KIND_CNT];
