7d4afa8f17ad263661f7042d85d914c864f9bf05
Michael McGee <mmcgee@jumptrading.com>
2025-01-23 23:47:43 +0000

    gui: add rate to sankey display

--- src/app/fdctl/run/tiles/generated/http_import_dist.c
+++ src/app/fdctl/run/tiles/generated/http_import_dist.c
@@ -3,10 +3,10 @@
 
 FD_IMPORT_BINARY( file0, "src/app/fdctl/dist/assets/firedancer-D_J0EzUc.svg" );
 FD_IMPORT_BINARY( file1, "src/app/fdctl/dist/assets/firedancer_logo-CrgwxzPk.svg" );
-FD_IMPORT_BINARY( file2, "src/app/fdctl/dist/assets/index-CYIi2PlC.js" );
-FD_IMPORT_BINARY( file3, "src/app/fdctl/dist/assets/index-Dz6VwGqc.css" );
-FD_IMPORT_BINARY( file4, "src/app/fdctl/dist/assets/index-k9a39Q54.css" );
-FD_IMPORT_BINARY( file5, "src/app/fdctl/dist/assets/index-ZFYaRyym.js" );
+FD_IMPORT_BINARY( file2, "src/app/fdctl/dist/assets/index-Cpm7kmGE.js" );
+FD_IMPORT_BINARY( file3, "src/app/fdctl/dist/assets/index-DmEXEFlt.css" );
+FD_IMPORT_BINARY( file4, "src/app/fdctl/dist/assets/index-DpcOWpAi.js" );
+FD_IMPORT_BINARY( file5, "src/app/fdctl/dist/assets/index-Dz6VwGqc.css" );
 FD_IMPORT_BINARY( file6, "src/app/fdctl/dist/assets/inter-tight-latin-400-normal-BW_APOAv.woff" );
 FD_IMPORT_BINARY( file7, "src/app/fdctl/dist/assets/inter-tight-latin-400-normal-CjmW70MP.woff2" );
 FD_IMPORT_BINARY( file8, "src/app/fdctl/dist/assets/privateYou-DxC3xn7b.svg" );
@@ -25,22 +25,22 @@ fd_http_static_file_t STATIC_FILES[] = {
         .data_len = &file1_sz,
     },
     {
-        .name = "/assets/index-CYIi2PlC.js",
+        .name = "/assets/index-Cpm7kmGE.js",
         .data = file2,
         .data_len = &file2_sz,
     },
     {
-        .name = "/assets/index-Dz6VwGqc.css",
+        .name = "/assets/index-DmEXEFlt.css",
         .data = file3,
         .data_len = &file3_sz,
     },
     {
-        .name = "/assets/index-k9a39Q54.css",
+        .name = "/assets/index-DpcOWpAi.js",
         .data = file4,
         .data_len = &file4_sz,
     },
     {
-        .name = "/assets/index-ZFYaRyym.js",
+        .name = "/assets/index-Dz6VwGqc.css",
         .data = file5,
         .data_len = &file5_sz,
     },
