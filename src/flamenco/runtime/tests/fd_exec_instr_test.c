b75e74015ba10f46943d47583297efabac85f7c8
cali-jumptrading <cali@jumptrading.com>
2025-01-29 20:31:47 +0000

    flamenco: make transaction parsing errors return sanitize failure error in test harness

--- src/flamenco/runtime/tests/fd_exec_instr_test.c
+++ src/flamenco/runtime/tests/fd_exec_instr_test.c
@@ -781,6 +781,7 @@ _txn_context_create_and_exec( fd_exec_instr_test_runner_t *      runner,
   /* Create the raw txn (https://solana.com/docs/core/transactions#transaction-size) */
   uchar * txn_raw_begin = fd_scratch_alloc( alignof(uchar), 10000 ); // max txn size is 1232 but we allocate extra for safety
   uchar * txn_raw_cur_ptr = txn_raw_begin;
+  int txn_parse_error     = 0;
 
   /* Compact array of signatures (https://solana.com/docs/core/transactions#transaction)
      Note that although documentation interchangably refers to the signature cnt as a compact-u16
@@ -842,6 +843,11 @@ _txn_context_create_and_exec( fd_exec_instr_test_runner_t *      runner,
     // Compact array of 8-bit data
     pb_bytes_array_t * data = test_ctx->tx.message.instructions[i].data;
     if( data ) {
+      uint data_len_raw = data->size;
+      if( data_len_raw > USHORT_MAX ) {
+        txn_parse_error = -1;
+      }
+
       ushort data_len = (ushort) data->size;
       _add_compact_u16( &txn_raw_cur_ptr, data_len );
       _add_to_data( &txn_raw_cur_ptr, data->bytes, data_len );
@@ -907,6 +913,12 @@ _txn_context_create_and_exec( fd_exec_instr_test_runner_t *      runner,
 
   /* Setup the spad for account allocation */
   task_info->txn_ctx->spad = runner->spad;
+
+  /* Set the sanitize error if the txn was incorrectly formatted. */
+  if( txn_parse_error ) {
+    txn->flags          = 0U;
+    task_info->exec_res = FD_RUNTIME_TXN_ERR_SANITIZE_FAILURE;
+  }
 
   fd_runtime_pre_execute_check( task_info );
 
