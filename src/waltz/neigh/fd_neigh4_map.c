229ec83441f3c91fcec1405691b6d8ecad9d3f4c
Richard Patel <ripatel@jumptrading.com>
2025-02-12 20:53:50 +0000

    Trigger ARP requests via sockets, not rtnetlink
    Improves sandbox architecture

--- src/waltz/neigh/fd_neigh4_map.c
+++ src/waltz/neigh/fd_neigh4_map.c
@@ -6,11 +6,10 @@
 #define MAP_IMPL_STYLE 2
 #include "../../util/tmpl/fd_map_slot_para.c"
 
-#if FD_HAS_HOSTED && FD_HAS_SSE
+#if FD_HAS_HOSTED
 
 #include <errno.h>
 #include <stdio.h>
-#include <xmmintrin.h>
 #include "../../util/net/fd_ip4.h"
 #include "../../util/net/fd_eth.h"
 
@@ -23,14 +22,25 @@ fd_neigh4_hmap_fprintf( fd_neigh4_hmap_t const * map,
   fd_neigh4_entry_t const * ele     = fd_neigh4_hmap_shele_const( map );
 
   for( ulong j=0UL; j<ele_max; j++ ) {
-    union {
-      fd_neigh4_entry_t e;
-      __m128i           m128;
-    } l;
-    l.m128 = _mm_load_si128( (__m128i const *)(ele+j) );
-    if( l.e.ip4_addr ) {
+    /* Peek key (atomic due to fd_neigh4_entry_t alignment) */
+    uint ip4_addr = ele[j].ip4_addr;
+
+    /* Speculative read */
+    fd_neigh4_hmap_query_t query[1];
+    if( fd_neigh4_hmap_query_try( map, &ip4_addr, NULL, query, 0 )!=FD_MAP_SUCCESS ) {
+      continue;
+    }
+
+    fd_neigh4_entry_t e[1]; memcpy( e, fd_neigh4_hmap_query_ele( query ), sizeof(fd_neigh4_entry_t) );
+
+    /* Check if read was overrun */
+    if( FD_UNLIKELY( fd_neigh4_hmap_query_test( query )!=FD_MAP_SUCCESS ) ) {
+      continue;
+    }
+
+    if( e->ip4_addr ) {
       int print_res = fprintf( file, FD_IP4_ADDR_FMT " " FD_ETH_MAC_FMT "\n",
-                               FD_IP4_ADDR_FMT_ARGS( l.e.ip4_addr ), FD_ETH_MAC_FMT_ARGS( l.e.mac_addr ) );
+                               FD_IP4_ADDR_FMT_ARGS( e->ip4_addr ), FD_ETH_MAC_FMT_ARGS( e->mac_addr ) );
       if( FD_UNLIKELY( print_res<0 ) ) return errno;
     }
   }
@@ -38,4 +48,4 @@ fd_neigh4_hmap_fprintf( fd_neigh4_hmap_t const * map,
   return 0;
 }
 
-#endif /* FD_HAS_HOSTED && FD_HAS_SSE */
+#endif /* FD_HAS_HOSTED */
