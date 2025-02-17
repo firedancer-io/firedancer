2863f360550493a014dbd96a52d8d765cc839d55
Richard Patel <ripatel@jumptrading.com>
2025-02-17 17:13:56 +0000

    Fix build if Agave submodule is not checked out

--- src/app/fdctl/with-version.mk
+++ src/app/fdctl/with-version.mk
@@ -14,6 +14,11 @@ FIREDANCER_VERSION_MINOR := $(VERSION_MINOR)$(shell printf "%02d" $(VERSION_PATC
 # Agave version of "1.18.7" to a minor version of "11807".
 FIREDANCER_VERSION_PATCH := $(shell grep -Po "(?<=^version = \").*(?=\")" "agave/Cargo.toml" | awk -F. '{ printf "%d%02d%02d\n", $$1, $$2, $$3 }')
 
+# If the Agave submodule is not checked out, the above command fails
+ifeq ($(FIREDANCER_VERSION_PATCH),)
+FIREDANCER_VERSION_PATCH := 9999
+endif
+
 export FIREDANCER_VERSION_MAJOR
 export FIREDANCER_VERSION_MINOR
 export FIREDANCER_VERSION_PATCH
