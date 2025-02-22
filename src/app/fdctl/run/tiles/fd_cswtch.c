bebc4a0979a9e086d9908dcf2b03260cce253d10
Richard Patel <ripatel@jumptrading.com>
2025-02-22 23:28:09 +0000

    Reduce cswtch tile CPU usage
    Also fix netlnk tile heartbeat flickering

--- src/app/fdctl/run/tiles/fd_cswtch.c
+++ src/app/fdctl/run/tiles/fd_cswtch.c
@@ -1,13 +1,14 @@
-#include "../../../../disco/tiles.h"
-
-#include <sys/types.h> /* SEEK_SET */
-#include <unistd.h>
-#include "generated/cswtch_seccomp.h"
-
 #include "../../../../disco/metrics/fd_metrics.h"
+#include "../../../../disco/stem/fd_stem.h"
+#include "../../../../disco/topo/fd_topo.h"
 
 #include <fcntl.h>
 #include <errno.h>
+#include <sys/types.h> /* SEEK_SET */
+#include <time.h>
+#include <unistd.h>
+
+#include "generated/cswtch_seccomp.h"
 
 #define REPORT_INTERVAL_MILLIS (100L)
 
@@ -40,7 +41,16 @@ before_credit( fd_cswtch_ctx_t *   ctx,
   (void)mux;
 
   long now = fd_log_wallclock();
-  if( FD_UNLIKELY( now<ctx->next_report_nanos ) ) return;
+  if( now<ctx->next_report_nanos ) {
+    long diff = ctx->next_report_nanos - now;
+    diff = fd_long_min( diff, 2e6 /* 2ms */ );
+    struct timespec const ts = {
+      .tv_sec  = diff / (long)1e9,
+      .tv_nsec = diff % (long)1e9
+    };
+    clock_nanosleep( CLOCK_REALTIME, 0, &ts, NULL );
+    return;
+  }
   ctx->next_report_nanos += REPORT_INTERVAL_MILLIS*1000L*1000L;
 
   *charge_busy = 1;
@@ -210,9 +220,7 @@ populate_allowed_fds( fd_topo_t const *      topo,
 }
 
 #define STEM_BURST (1UL)
-
-/* See explanation in fd_pack */
-#define STEM_LAZY  (128L*3000L)
+#define STEM_LAZY  ((long)10e6) /* 10ms */
 
 #define STEM_CALLBACK_CONTEXT_TYPE  fd_cswtch_ctx_t
 #define STEM_CALLBACK_CONTEXT_ALIGN alignof(fd_cswtch_ctx_t)
