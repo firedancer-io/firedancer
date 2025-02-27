c809a272ea30f2651dc64484e54dc6119e22013d
Michael McGee <mmcgee@jumptrading.com>
2025-02-27 21:31:22 +0000

    pack, bundle: prevent regular backpressure

--- src/disco/keyguard/fd_keyguard_client.c
+++ src/disco/keyguard/fd_keyguard_client.c
@@ -2,18 +2,20 @@
 
 void *
 fd_keyguard_client_new( void *         shmem,
-                      fd_frag_meta_t * request_mcache,
-                      uchar *          request_data,
-                      fd_frag_meta_t * response_mcache,
-                      uchar *          response_data ) {
+                        fd_frag_meta_t * request_mcache,
+                        uchar *          request_data,
+                        fd_frag_meta_t * response_mcache,
+                        uchar *          response_data ) {
   fd_keyguard_client_t * client = (fd_keyguard_client_t*)shmem;
-  client->request      = request_mcache;
-  client->request_seq  = 0UL;
-  client->request_data = request_data;
+  client->request       = request_mcache;
+  client->request_depth = fd_mcache_depth( request_mcache );
+  client->request_seq   = 0UL;
+  client->request_data  = request_data;
 
-  client->response      = response_mcache;
-  client->response_seq  = 0UL;
-  client->response_data = response_data;
+  client->response       = response_mcache;
+  client->response_depth = fd_mcache_depth( response_mcache );
+  client->response_seq   = 0UL;
+  client->response_data  = response_data;
   return shmem;
 }
 
@@ -27,7 +29,7 @@ fd_keyguard_client_sign( fd_keyguard_client_t * client,
   fd_memcpy( client->request_data, sign_data, sign_data_len );
 
   ulong sig = (ulong)(uint)sign_type;
-  fd_mcache_publish( client->request, 128UL, client->request_seq, sig, 0UL, sign_data_len, 0UL, 0UL, 0UL );
+  fd_mcache_publish( client->request, client->request_depth, client->request_seq, sig, 0UL, sign_data_len, 0UL, 0UL, 0UL );
   client->request_seq = fd_seq_inc( client->request_seq, 1UL );
 
   fd_frag_meta_t meta;
@@ -35,7 +37,7 @@ fd_keyguard_client_sign( fd_keyguard_client_t * client,
   ulong seq_found;
   long seq_diff;
   ulong poll_max = ULONG_MAX;
-  FD_MCACHE_WAIT( &meta, mline, seq_found, seq_diff, poll_max, client->response, 128UL, client->response_seq );
+  FD_MCACHE_WAIT( &meta, mline, seq_found, seq_diff, poll_max, client->response, client->response_depth, client->response_seq );
   if( FD_UNLIKELY( !poll_max ) ) FD_LOG_ERR(( "sign request timed out while polling" ));
   if( FD_UNLIKELY( seq_diff ) ) FD_LOG_ERR(( "sign request was overrun while polling" ));
 
