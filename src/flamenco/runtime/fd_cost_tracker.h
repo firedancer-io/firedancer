05202c6b80846f8b0f1c9811514b64c14386cbe8
Manik Jain <mjain@jumptrading.com>
2025-03-05 16:20:38 +0000

    spacing?

--- src/flamenco/runtime/fd_cost_tracker.h
+++ src/flamenco/runtime/fd_cost_tracker.h
@@ -11,28 +11,28 @@
 #include "../../disco/pack/fd_pack_cost.h"
 
 // https://github.com/anza-xyz/agave/blob/v2.2.0/cost-model/src/block_cost_limits.rs#L20
-#define WRITE_LOCK_UNITS ( 300UL )
+#define FD_WRITE_LOCK_UNITS ( 300UL )
 
 // https://github.com/anza-xyz/agave/blob/v2.2.0/cost-model/src/block_cost_limits.rs#L42
-#define MAX_BLOCK_ACCOUNTS_DATA_SIZE_DELTA ( 100000000UL )
+#define FD_MAX_BLOCK_ACCOUNTS_DATA_SIZE_DELTA ( 100000000UL )
 
 // https://github.com/anza-xyz/agave/blob/v2.2.0/cost-model/src/block_cost_limits.rs#L34
-#define MAX_WRITABLE_ACCOUNT_UNITS ( 12000000UL )
+#define FD_MAX_WRITABLE_ACCOUNT_UNITS ( 12000000UL )
 
 // https://github.com/anza-xyz/agave/blob/v2.2.0/cost-model/src/block_cost_limits.rs#L28
-#define MAX_BLOCK_UNITS ( 48000000UL )
+#define FD_MAX_BLOCK_UNITS ( 48000000UL )
 
 // https://github.com/anza-xyz/agave/blob/v2.2.0/cost-model/src/block_cost_limits.rs#L29C11-L29C36
-#define MAX_BLOCK_UNITS_SIMD_0207 ( 50000000UL )
+#define FD_MAX_BLOCK_UNITS_SIMD_0207 ( 50000000UL )
 
 // https://github.com/anza-xyz/agave/blob/v2.2.0/cost-model/src/block_cost_limits.rs#L38
-#define MAX_VOTE_UNITS ( 36000000UL )
+#define FD_MAX_VOTE_UNITS ( 36000000UL )
 
 // https://github.com/anza-xyz/agave/blob/v2.2.0/cost-model/src/cost_tracker.rs#L15
-#define WRITABLE_ACCOUNTS_PER_BLOCK ( 4096UL )
+#define FD_WRITABLE_ACCOUNTS_PER_BLOCK ( 4096UL )
 
 /* https://github.com/anza-xyz/agave/blob/v2.2.0/cost-model/src/cost_tracker.rs#L18-L33 */
-#define FD_COST_TRACKER_SUCCESS																		  ( 0 )
+#define FD_COST_TRACKER_SUCCESS								                      ( 0 )
 #define FD_COST_TRACKER_ERROR_WOULD_EXCEED_BLOCK_MAX_LIMIT          ( 1 )
 #define FD_COST_TRACKER_ERROR_WOULD_EXCEED_VOTE_MAX_LIMIT           ( 2 )
 #define FD_COST_TRACKER_ERROR_WOULD_EXCEED_ACCOUNT_MAX_LIMIT        ( 3 )
@@ -43,9 +43,9 @@ FD_PROTOTYPES_BEGIN
 
 /* Initializes the cost tracker and allocates enough memory for the map */
 void
-fd_cost_tracker_init( fd_cost_tracker_t *  		   self,
-											fd_exec_slot_ctx_t const * slot_ctx,
-											fd_spad_t * 				  		 spad );
+fd_cost_tracker_init( fd_cost_tracker_t *  	     self,
+                      fd_exec_slot_ctx_t const * slot_ctx,
+                      fd_spad_t * 		     	     spad );
 
 /* Modeled after `CostModel::calculate_cost_for_executed_transaction()`.
    Used to compute transaction cost information for executed transactions.
@@ -53,12 +53,12 @@ fd_cost_tracker_init( fd_cost_tracker_t *  		   self,
    https://github.com/anza-xyz/agave/blob/v2.2.0/cost-model/src/cost_model.rs#L69-L95 */
 fd_transaction_cost_t
 fd_calculate_cost_for_executed_transaction( fd_exec_txn_ctx_t const * txn_ctx,
-																					  fd_spad_t * 							spad );
+                                            fd_spad_t * 							spad );
 
 /* Modeled after `CostTracker::try_add()`. Checks to see if the transaction cost
    would fit in this block. Returns an error on failure.
 
-	 https://github.com/anza-xyz/agave/blob/v2.2.0/cost-model/src/cost_tracker.rs#L163-L173 */
+    https://github.com/anza-xyz/agave/blob/v2.2.0/cost-model/src/cost_tracker.rs#L163-L173 */
 int
 fd_cost_tracker_try_add( fd_cost_tracker_t *  				 self,
                          fd_exec_txn_ctx_t const *     txn_ctx,
