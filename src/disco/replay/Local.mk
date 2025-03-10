260fc6aa4b180adce4557d3d81d56ceec45c1dc1
Charles Li <chali@jumptrading.com>
2025-03-10 20:38:09 +0000

    feat(replay): add replay module to disco

--- src/disco/replay/Local.mk
+++ src/disco/replay/Local.mk
@@ -1,4 +1,6 @@
+ifdef FD_HAS_INT128
 $(call add-hdrs,fd_replay.h)
 $(call add-objs,fd_replay,fd_disco)
-$(call make-unit-test,test_replay,test_replay,fd_disco fd_flamenco fd_tango fd_util)
-$(call make-bin,fd_replay_tile,fd_replay_tile,fd_disco fd_flamenco fd_tango fd_util)
+ifdef FD_HAS_HOSTED
+endif
+endif
