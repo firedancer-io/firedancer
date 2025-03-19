5f8c2eb9b49e02b18d89fce10e0ee64b0e050280
Richard Patel <ripatel@jumptrading.com>
2025-03-19 21:04:55 +0000

    Optionally make XDP prog listen on specific IP

--- src/waltz/xdp/fd_xdp1.h
+++ src/waltz/xdp/fd_xdp1.h
@@ -13,16 +13,18 @@ typedef struct fd_xdp_fds fd_xdp_fds_t;
 ulong
 fd_xdp_gen_program( ulong          code_buf[ 512 ],
                     int            xsks_fd,
+                    uint           listen_ip4_addr,
                     ushort const * ports,
                     ulong          ports_cnt );
 
 /* fd_xdp_install installs a BPF program onto the given interface which
    only passes through UDP traffic on the provided ports to rings on an
-   XSK map.  The XSK map is created and returned in the fd_xdp_fds_t,
-   along with a bpf link file descriptor (from BPF_LINK_CREATE).  This
-   link must not be closed or the XDP program will be uninstalled from
-   the device, and no more packets will be received.  This happens
-   automatically when the process exits.
+   XSK map.  If listen_ip4_addr is not zero, specifies a net order IPv4
+   address to filter the dest address for.  The XSK map is created and
+   returned in the fd_xdp_fds_t, along with a bpf link file descriptor
+   (from BPF_LINK_CREATE).  This link must not be closed or the XDP
+   program will be uninstalled from the device, and no more packets will
+   be received.  This happens automatically when the process exits.
 
    The XSK map returned in xsk_map_fd simply needs to have socket file
    descriptors inserted, one per each queue, with BPF_MAP_UPDATE_ELEM,
@@ -33,6 +35,7 @@ fd_xdp_gen_program( ulong          code_buf[ 512 ],
 
 fd_xdp_fds_t
 fd_xdp_install( uint           if_idx,
+                uint           listen_ip4_addr,
                 ulong          ports_cnt,
                 ushort const * ports,
                 char const *   xdp_mode );
