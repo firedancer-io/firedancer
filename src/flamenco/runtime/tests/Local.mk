1b110734bd7a93548162218970315bafd87365b4
Kunal Bhargava <kbhargava@jumptrading.com>
2025-03-24 14:43:15 +0000

    nightly: replace ledger

--- src/flamenco/runtime/tests/Local.mk
+++ src/flamenco/runtime/tests/Local.mk
@@ -26,12 +26,14 @@ run-runtime-test: $(OBJDIR)/bin/fd_ledger
 	python3.11 ./src/flamenco/runtime/tests/run_ledger_tests_all.py ./src/flamenco/runtime/tests/run_ledger_tests_all.txt
 
 run-runtime-test-nightly: $(OBJDIR)/bin/fd_ledger
-	OBJDIR=$(OBJDIR) src/flamenco/runtime/tests/run_nightly_test.sh -l mainnet-257033306 -s snapshot-257033306-EE3WdRoE4J1LTjegJMK3ZzxKZbSMQhLMaTM5Jp4SygMU.tar.zst -p 100 -y 450 -m 500000000 -e 257213306 -c 2.0.0
-	OBJDIR=$(OBJDIR) src/flamenco/runtime/tests/run_nightly_test.sh -l mainnet-296243940 -s snapshot-296400651-HDt9Gf1YKcruvuBV4q442qV4xjHer4KZ9sZao9XQspZP.tar.zst -p 100 -y 750 -m 700000000 -e 296550651 -c 2.0.0
-	OBJDIR=$(OBJDIR) src/flamenco/runtime/tests/run_nightly_test.sh -l devnet-340941576  -s snapshot-340924320-8j9h6EKmuZ3G93Y3Pb3FqrNdCDTGE5PKowHMY3xkXG1K.tar.zst -p 100 -y 400 -m 200000000 -e 340941580 -c 2.0.0
-	OBJDIR=$(OBJDIR) src/flamenco/runtime/tests/run_nightly_test.sh -l testnet-305516256 -s snapshot-305516254-C4oM7ajmCMo1aDakR8Q8FriSVpXW53jwbb3da37jm7bN.tar.zst -p 100 -y 400 -m 150000000 -e 305516292 -c 2.0.0
-	OBJDIR=$(OBJDIR) src/flamenco/runtime/tests/run_nightly_test.sh -l devnet-346032000  -s snapshot-346031900-2EyfK3LCFoA69PPJ9JBPNDXV9ShDMLok7Vo6sr8LfdFc.tar.zst -p 100 -y 400 -m 200000000 -e 346032005 -c 2.0.15
+	# OBJDIR=$(OBJDIR) src/flamenco/runtime/tests/run_nightly_test.sh -l mainnet-257033306 -s snapshot-257033306-EE3WdRoE4J1LTjegJMK3ZzxKZbSMQhLMaTM5Jp4SygMU.tar.zst -p 100 -y 450 -m 500000000 -e 257213306 -c 2.0.0
+	# OBJDIR=$(OBJDIR) src/flamenco/runtime/tests/run_nightly_test.sh -l mainnet-296243940 -s snapshot-296400651-HDt9Gf1YKcruvuBV4q442qV4xjHer4KZ9sZao9XQspZP.tar.zst -p 100 -y 750 -m 700000000 -e 296550651 -c 2.0.0
+	# OBJDIR=$(OBJDIR) src/flamenco/runtime/tests/run_nightly_test.sh -l devnet-340941576  -s snapshot-340924320-8j9h6EKmuZ3G93Y3Pb3FqrNdCDTGE5PKowHMY3xkXG1K.tar.zst -p 100 -y 400 -m 200000000 -e 340941580 -c 2.0.0
+	# OBJDIR=$(OBJDIR) src/flamenco/runtime/tests/run_nightly_test.sh -l testnet-305516256 -s snapshot-305516254-C4oM7ajmCMo1aDakR8Q8FriSVpXW53jwbb3da37jm7bN.tar.zst -p 100 -y 400 -m 150000000 -e 305516292 -c 2.0.0
+	# OBJDIR=$(OBJDIR) src/flamenco/runtime/tests/run_nightly_test.sh -l devnet-346032000  -s snapshot-346031900-2EyfK3LCFoA69PPJ9JBPNDXV9ShDMLok7Vo6sr8LfdFc.tar.zst -p 100 -y 400 -m 200000000 -e 346032005 -c 2.0.15
+	OBJDIR=$(OBJDIR) src/flamenco/runtime/tests/run_nightly_test.sh -l mainnet-327443157  -s snapshot-327493391-9z5sYZhTCUbMKvXKotuCqN1y5TTt9T4PpxJzE6FLoQiz.tar.zst -p 100 -y 750 -m 950000000 -e 327593391 -c 2.1.11
+
 
 run-runtime-test-nightly-asan: $(OBJDIR)/bin/fd_ledger
-	OBJDIR=$(OBJDIR) src/flamenco/runtime/tests/run_nightly_test.sh -l v201-small        -s snapshot-100-38CM8ita1fT5SmSLUEeqQZffn2xsy9vKz3WJmsFSnhrJ.tar.zst       -p 100 -y 16  -m 500000    -e 120       -c 2.0.1
+	# OBJDIR=$(OBJDIR) src/flamenco/runtime/tests/run_nightly_test.sh -l v201-small        -s snapshot-100-38CM8ita1fT5SmSLUEeqQZffn2xsy9vKz3WJmsFSnhrJ.tar.zst       -p 100 -y 16  -m 500000    -e 120       -c 2.0.1
 	# OBJDIR=$(OBJDIR) src/flamenco/runtime/tests/run_nightly_test.sh -l devnet-330914784  -s snapshot-330914783-BujhdWiXTfRPfFYMG3GZdEcNc18KyvCcAq9QL9e1i1Fk.tar.zst -p 100 -y 16  -m 500000    -e 330914785 -c 2.0.8
