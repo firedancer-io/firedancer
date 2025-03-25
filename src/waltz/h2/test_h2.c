e63b1bac9340db0f222ab1ae6bd75cdc2025c465
Richard Patel <ripatel@jumptrading.com>
2025-03-25 01:32:55 +0000

    h2: ring buffer

--- src/waltz/h2/test_h2.c
+++ src/waltz/h2/test_h2.c
@@ -1,14 +1,22 @@
 #include "../../util/fd_util.h"
 
 #include "test_hpack.c"
+#include "test_h2_rbuf.c"
 
 int
 main( int     argc,
       char ** argv ) {
   fd_boot( &argc, &argv );
 
+  fd_rng_t _rng[1]; fd_rng_t * rng = fd_rng_join( fd_rng_new( _rng, 0U, 0UL ) );
+
   FD_LOG_NOTICE(( "Testing hpack" ));
   test_hpack();
+
+  FD_LOG_NOTICE(( "Testing h2_buf" ));
+  test_h2_rbuf( rng );
+
+  fd_rng_delete( fd_rng_leave( rng ) );
 
   FD_LOG_NOTICE(( "pass" ));
   fd_halt();
