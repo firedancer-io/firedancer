f55536e301f075bb7ec0317eecc954ce27171bd4
Charles Li <chali@jumptrading.com>
2025-03-26 18:56:17 +0000

    feat(shred,repair): shred_repair link

--- src/discof/repair/Local.mk
+++ src/discof/repair/Local.mk
@@ -1,5 +1,8 @@
+ifdef FD_HAS_INT128
+$(call add-objs,fd_fec_repair,fd_discof)
 ifdef FD_HAS_SSE
 $(call add-objs,fd_repair_tile,fd_discof)
 $(call add-objs,fd_fec_chainer,fd_discof)
 $(call make-unit-test,test_fec_chainer,test_fec_chainer,fd_discof fd_flamenco fd_ballet fd_util)
+endif
 endif
