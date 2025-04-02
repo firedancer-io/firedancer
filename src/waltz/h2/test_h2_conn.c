e5e105ad9f032a8915d478249d4cd4d20a635a5d
Richard Patel <ripatel@jumptrading.com>
2025-04-02 10:24:20 +0000

    h2: connection logic

---
+++ src/waltz/h2/test_h2_conn.c
@@ -0,0 +1,192 @@
+#include "fd_h2_callback.h"
+#include "fd_h2_conn.h"
+#include "../../util/sanitize/fd_asan.h"
+
+struct test_h2_callback_rec {
+  uint cb_established_cnt;
+};
+
+typedef struct test_h2_callback_rec test_h2_callback_rec_t;
+
+static test_h2_callback_rec_t cb_rec;
+
+static void
+test_cb_conn_established( fd_h2_conn_t * conn ) {
+  (void)conn;
+  cb_rec.cb_established_cnt++;
+}
+
+/* test_h2_client_handshake exercises various client-side handshake
+   state logic.  There are three possible successful client handshake
+   sequences:
+
+   Sequence 1:
+   - Client: Preface, SETTINGS
+   - Server: SETTINGS
+   - Client: SETTINGS ACK
+   - Server: SETTINGS ACK
+
+   Sequence 3:
+   - Client: Preface, SETTINGS
+   - Server: SETTINGS ACK
+   - Server: SETTINGS
+   - Client: SETTINGS ACK */
+
+static void
+test_h2_client_handshake( void ) {
+  uchar scratch[256];
+  uchar rbuf_rx_b[128];
+  uchar rbuf_tx_b[128];
+
+  fd_h2_conn_t conn[1];
+  FD_TEST( fd_h2_conn_init_client( conn )==conn );
+  conn->self_settings.initial_window_size    = 65535U;
+  conn->self_settings.max_frame_size         = 16384U;
+  conn->self_settings.max_header_list_size   =  4096U;
+  conn->self_settings.max_concurrent_streams =   128U;
+
+  fd_h2_callbacks_t cb[1];
+  fd_h2_callbacks_init( cb );
+  cb->conn_established = test_cb_conn_established;
+
+  /* Verify that the client initiates the conn */
+
+  FD_TEST( conn->flags == FD_H2_CONN_FLAGS_CLIENT_INITIAL );
+
+  fd_h2_rbuf_t rbuf_tx[1];
+  fd_h2_rbuf_init( rbuf_tx, rbuf_tx_b, sizeof(rbuf_tx_b) );
+  fd_h2_tx_control( conn, rbuf_tx );
+  FD_TEST( fd_h2_rbuf_used_sz( rbuf_tx )==69 );
+  uchar * hello = fd_h2_rbuf_pop( rbuf_tx, scratch, 69 );
+  FD_TEST( fd_memeq( hello, "PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n", 24 ) );
+  static uchar const settings_frame_expected[ 45 ] = {
+    /* payload size: 24 bytes */
+    0x00, 0x00, 0x24,
+    /* frame type: SETTINGS */
+    0x04,
+    /* flags: none */
+    0x00,
+    /* stream id: 0 */
+    0x00, 0x00, 0x00, 0x00,
+
+    /* HEADER_TABLE_SIZE: 0 */
+    0x00, 0x01,  0x00, 0x00, 0x00, 0x00,
+    /* ENABLE_PUSH: 0 */
+    0x00, 0x02,  0x00, 0x00, 0x00, 0x00,
+    /* MAX_CONCURRENT_STREAMS: 128 */
+    0x00, 0x03,  0x00, 0x00, 0x00, 0x80,
+    /* INITIAL_WINDOW_SIZE: 65535 */
+    0x00, 0x04,  0x00, 0x00, 0xff, 0xff,
+    /* MAX_FRAME_SIZE: 16384 */
+    0x00, 0x05,  0x00, 0x00, 0x40, 0x00,
+    /* MAX_HEADER_LIST_SIZE: 4096 */
+    0x00, 0x06,  0x00, 0x00, 0x10, 0x00
+  };
+  FD_TEST( fd_memeq( hello+24, settings_frame_expected, sizeof(settings_frame_expected) ) );
+
+  FD_TEST( conn->flags == (FD_H2_CONN_FLAGS_WAIT_SETTINGS_0 | FD_H2_CONN_FLAGS_WAIT_SETTINGS_ACK_0) );
+  fd_h2_tx_control( conn, rbuf_tx );
+  fd_h2_tx_control( conn, rbuf_tx );
+  FD_TEST( fd_h2_rbuf_used_sz( rbuf_tx )==0 );
+
+  /* Server: SETTINGS, SETTINGS ACK */
+
+  static uchar const server_settings[ 45 ] = {
+    /* payload size: 36 bytes */
+    0x00, 0x00, 0x24,
+    /* frame type: SETTINGS */
+    0x04,
+    /* flags: none */
+    0x00,
+    /* stream id: 0 */
+    0x00, 0x00, 0x00, 0x00,
+
+    /* HEADER_TABLE_SIZE: 2 */
+    0x00, 0x01,  0x00, 0x00, 0x00, 0x02,
+    /* ENABLE_PUSH: 1 */
+    0x00, 0x02,  0x00, 0x00, 0x00, 0x01,
+    /* MAX_CONCURRENT_STREAMS: 256 */
+    0x00, 0x03,  0x00, 0x00, 0x01, 0x00,
+    /* INITIAL_WINDOW_SIZE: 131071 */
+    0x00, 0x04,  0x00, 0x01, 0xff, 0xff,
+    /* MAX_FRAME_SIZE: 32768 */
+    0x00, 0x05,  0x00, 0x00, 0x80, 0x00,
+    /* MAX_HEADER_LIST_SIZE: 8192 */
+    0x00, 0x06,  0x00, 0x00, 0x20, 0x00
+  };
+  fd_h2_rbuf_t rbuf_rx[1];
+  FD_TEST( fd_h2_rbuf_init( rbuf_rx, rbuf_rx_b, sizeof(rbuf_rx_b) )==rbuf_rx );
+  fd_h2_rbuf_push( rbuf_rx, server_settings, sizeof(server_settings) );
+  fd_h2_rx( conn, rbuf_rx, rbuf_tx, scratch, sizeof(scratch), cb );
+  FD_TEST( fd_h2_rbuf_used_sz( rbuf_tx )==9 );
+
+  static uchar const settings_ack_expected[ 9 ] = {
+    /* payload size: 0 bytes */
+    0x00, 0x00, 0x00,
+    /* frame type: SETTINGS */
+    0x04,
+    /* flags: ACK */
+    0x01,
+    /* stream id: 0 */
+    0x00, 0x00, 0x00, 0x00
+  };
+  uchar * settings_ack = fd_h2_rbuf_pop( rbuf_tx, scratch, 9UL );
+  FD_TEST( fd_memeq( settings_ack, settings_ack_expected, sizeof(settings_ack_expected) ) );
+
+  FD_TEST( conn->flags == FD_H2_CONN_FLAGS_WAIT_SETTINGS_ACK_0 );
+  fd_h2_tx_control( conn, rbuf_tx );
+  FD_TEST( fd_h2_rbuf_used_sz( rbuf_tx )==0 );
+
+  FD_TEST( cb_rec.cb_established_cnt==0 );
+  fd_h2_rbuf_push( rbuf_rx, settings_ack_expected, sizeof(settings_ack_expected) );
+  fd_h2_rx( conn, rbuf_rx, rbuf_tx, scratch, sizeof(scratch), cb );
+  FD_TEST( fd_h2_rbuf_used_sz( rbuf_tx )==0 );
+  FD_TEST( conn->flags == 0 );
+  fd_h2_tx_control( conn, rbuf_tx );
+  FD_TEST( fd_h2_rbuf_used_sz( rbuf_tx )==0 );
+  FD_TEST( cb_rec.cb_established_cnt==1 );
+
+  /* Retry the scenario, but this time:
+     Server: SETTINGS ACK, SETTINGS */
+
+  cb_rec.cb_established_cnt = 0;
+  FD_TEST( fd_h2_conn_init_client( conn )==conn );
+  conn->self_settings.initial_window_size    = 65535U;
+  conn->self_settings.max_frame_size         = 16384U;
+  conn->self_settings.max_header_list_size   =  4096U;
+  conn->self_settings.max_concurrent_streams =   128U;
+
+  /* Pretend we just sent a preface and a settings frame, and are now
+     waiting on the server's response */
+  conn->flags = FD_H2_CONN_FLAGS_WAIT_SETTINGS_0 | FD_H2_CONN_FLAGS_WAIT_SETTINGS_ACK_0;
+  conn->setting_tx = 1;
+
+  fd_h2_rbuf_push( rbuf_rx, settings_ack_expected, sizeof(settings_ack_expected) );
+  fd_h2_rx( conn, rbuf_rx, rbuf_tx, scratch, sizeof(scratch), cb );
+  FD_TEST( fd_h2_rbuf_used_sz( rbuf_tx )==0 );
+  FD_TEST( conn->flags == FD_H2_CONN_FLAGS_WAIT_SETTINGS_0 );
+  FD_TEST( cb_rec.cb_established_cnt==0 );
+
+  fd_h2_rbuf_push( rbuf_rx, server_settings, sizeof(server_settings) );
+  fd_h2_rx( conn, rbuf_rx, rbuf_tx, scratch, sizeof(scratch), cb );
+  FD_TEST( fd_h2_rbuf_used_sz( rbuf_tx )==9 );
+  settings_ack = fd_h2_rbuf_pop( rbuf_tx, scratch, 9UL );
+  FD_TEST( fd_memeq( settings_ack, settings_ack_expected, sizeof(settings_ack_expected) ) );
+  FD_TEST( conn->flags == 0 );
+  FD_TEST( cb_rec.cb_established_cnt==1 );
+}
+
+static void
+test_h2_rx_partial_suppress( void ) {
+}
+
+static void
+test_h2_rx_partial_incremental( void ) {
+}
+
+static void
+test_h2_conn( void ) {
+  test_h2_client_handshake();
+  test_h2_rx_partial_suppress();
+  test_h2_rx_partial_incremental();
+}
