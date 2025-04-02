1d7d8fea669abaa6d027513a298e6a255addda30
Richard Patel <ripatel@jumptrading.com>
2025-04-02 10:24:20 +0000

    h2: connection logic

---
+++ src/waltz/h2/fd_h2.h
@@ -0,0 +1,12 @@
+#ifndef HEADER_fd_src_waltz_h2_fd_h2_h
+#define HEADER_fd_src_waltz_h2_fd_h2_h
+
+//#include "fd_h2_base.h"
+#include "fd_h2_callback.h"  /* includes fd_h2_base.h */
+//#include "fd_h2_proto.h"   /* includes fd_h2_base.h */
+//#include "fd_h2_rbuf.h"    /* includes fd_h2_base.h */
+//#include "fd_h2_conn.h"    /* includes fd_h2_proto.h fd_h2_rbuf.h */
+#include "fd_h2_stream.h"    /* includes fd_h2_proto.h fd_h2_conn.h */
+#include "fd_hpack.h"        /* includes fd_h2_base.h */
+
+#endif /* HEADER_fd_src_waltz_h2_fd_h2_h */
