52383464a3b2aedc66b0c201618fc46d5ef999ba
Emily Wang <emwang@jumptrading.com>
2025-04-04 15:38:39 +0000

    repair: initialize fec chainer and link with fec repair

--- src/discof/replay/Local.mk
+++ src/discof/replay/Local.mk
@@ -1,6 +1,6 @@
 ifdef FD_HAS_INT128
 ifdef FD_HAS_SSE
-$(call add-objs,fd_replay_tile fd_replay,fd_discof)
+$(call add-objs,fd_replay_tile,fd_discof)
 $(call make-unit-test,test_replay,test_replay,fd_discof fd_disco fd_flamenco fd_tango fd_util)
 endif
 endif
