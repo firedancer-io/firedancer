1849928362f21ec315c15e0aea1ff8b4e92bd021
Aman Khinvasara <akhinvasara@jumptrading.com>
2025-04-07 20:36:59 +0000

    sender: landing UDP votes

--- src/discof/replay/fd_replay_tile.c
+++ src/discof/replay/fd_replay_tile.c
@@ -59,7 +59,6 @@
 #define SHRED_IN_IDX   (4UL)
 
 #define STAKE_OUT_IDX  (0UL)
-#define SENDER_OUT_IDX (1UL)
 #define POH_OUT_IDX    (2UL)
 
 #define EXEC_BOOT_WAIT  (0UL)
@@ -1249,9 +1248,12 @@ send_tower_sync( fd_replay_tile_ctx_t * ctx ) {
   ulong vote_slot = fd_tower_votes_peek_tail_const( ctx->tower )->slot;
   fd_hash_t vote_bank_hash[1]  = { 0 };
   fd_hash_t vote_block_hash[1] = { 0 };
-  int err = fd_blockstore_bank_hash_query( ctx->blockstore, vote_slot, vote_bank_hash );
-  if( err ) FD_LOG_ERR(( "invariant violation: missing bank hash for tower vote" ));
-  err = fd_blockstore_block_hash_query( ctx->blockstore, vote_slot, vote_block_hash );
+
+  /* guaranteed to be on frontier from caller check */
+  const fd_fork_t * fork = fd_forks_query_const( ctx->forks, vote_slot );
+  fd_memcpy( vote_bank_hash, &fork->slot_ctx->slot_bank.banks_hash, sizeof(fd_hash_t) );
+
+  int err = fd_blockstore_block_hash_query( ctx->blockstore, vote_slot, vote_block_hash );
   if( err ) FD_LOG_ERR(( "invariant violation: missing block hash for tower vote" ));
 
   /* Build a vote state update based on current tower votes. */
@@ -1269,10 +1271,11 @@ send_tower_sync( fd_replay_tile_ctx_t * ctx ) {
 
   /* TODO: Can use a smaller size, adjusted for payload length */
   ulong msg_sz     = sizeof( fd_txn_p_t );
+  ulong sig        = vote_slot;
   fd_mcache_publish( ctx->sender_out_mcache,
                      ctx->sender_out_depth,
                      ctx->sender_out_seq,
-                     1UL,
+                     sig,
                      ctx->sender_out_chunk,
                      msg_sz,
                      0UL,
@@ -1772,7 +1775,6 @@ exec_slice( fd_replay_tile_ctx_t * ctx,
      FD_COMPILER_MFENCE();
      block_info->flags = fd_uchar_clear_bit( block_info->flags, FD_BLOCK_FLAG_REPLAYING );
      memcpy( &block_info->block_hash, hdr->hash, sizeof(fd_hash_t) );
-     memcpy( &block_info->bank_hash, &fork->slot_ctx->slot_bank.banks_hash, sizeof(fd_hash_t) );
 
      fd_block_map_publish( query );
      ctx->flags = EXEC_FLAG_FINISHED_SLOT;
@@ -2506,6 +2508,13 @@ after_credit( fd_replay_tile_ctx_t * ctx,
                                             ctx->runtime_spad,
                                             &exec_para_ctx_block_finalize );
 
+    /* Update blockstore with the freshly computed bank hash */
+    fd_block_map_query_t query[1] = { 0 };
+    fd_block_map_prepare( ctx->blockstore->block_map, &curr_slot, NULL, query, FD_MAP_FLAG_BLOCKING );
+    fd_block_info_t * block_info = fd_block_map_query_ele( query );
+    block_info->bank_hash = ctx->slot_ctx->slot_bank.banks_hash;
+    fd_block_map_publish( query );
+
     fd_spad_pop( ctx->runtime_spad );
     FD_LOG_NOTICE(( "Spad memory after executing block %lu", ctx->runtime_spad->mem_used ));
     /**********************************************************************/
@@ -3230,7 +3239,10 @@ unprivileged_init( fd_topo_t *      topo,
     ctx->notif_out_mcache = NULL;
   }
 
-  fd_topo_link_t * sender_out = &topo->links[ tile->out_link_id[ SENDER_OUT_IDX ] ];
+  /* Setup sender output */
+  ulong send_out_idx = fd_topo_find_tile_out_link( topo, tile, "replay_send", 0 );
+  FD_TEST( send_out_idx!=ULONG_MAX );
+  fd_topo_link_t * sender_out = &topo->links[ tile->out_link_id[ send_out_idx ] ];
   ctx->sender_out_mcache      = sender_out->mcache;
   ctx->sender_out_sync        = fd_mcache_seq_laddr( ctx->sender_out_mcache );
   ctx->sender_out_depth       = fd_mcache_depth( ctx->sender_out_mcache );
