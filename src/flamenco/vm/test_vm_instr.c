9cdd63771937dee2e2a0c3437994e25574c76b41
Tom Pointon <topointon@jumptrading.com>
2025-04-07 19:12:23 +0000

    flamenco, tests: fix memory leaks in unit tests

--- src/flamenco/vm/test_vm_instr.c
+++ src/flamenco/vm/test_vm_instr.c
@@ -442,7 +442,12 @@ run_input( test_input_t const * input,
       fd_sbpf_syscalls_new(
       aligned_alloc( fd_sbpf_syscalls_align(), fd_sbpf_syscalls_footprint() ) ) );
 
-  fd_exec_instr_ctx_t * instr_ctx = test_vm_minimal_exec_instr_ctx( fd_libc_alloc_virtual() );
+  fd_valloc_t valloc = fd_libc_alloc_virtual();
+  fd_exec_slot_ctx_t  * slot_ctx  = fd_valloc_malloc( valloc, FD_EXEC_SLOT_CTX_ALIGN,    FD_EXEC_SLOT_CTX_FOOTPRINT );
+  fd_exec_epoch_ctx_t * epoch_ctx = fd_valloc_malloc( valloc, fd_exec_epoch_ctx_align(), sizeof(fd_exec_epoch_ctx_t) );
+
+  fd_exec_instr_ctx_t * instr_ctx = test_vm_minimal_exec_instr_ctx(
+    valloc, epoch_ctx, slot_ctx );
 
   int vm_ok = !!fd_vm_init(
       /* vm               */ vm,
@@ -476,6 +481,8 @@ run_input( test_input_t const * input,
   run_input2( out, vm, force_exec );
 
   /* Clean up */
+  fd_valloc_free( valloc, epoch_ctx );
+  fd_valloc_free( valloc, slot_ctx );
   test_vm_exec_instr_ctx_delete( instr_ctx, fd_libc_alloc_virtual() );
   free( fd_sbpf_syscalls_delete ( fd_sbpf_syscalls_leave ( syscalls  ) ) );
   free( fd_sbpf_calldests_delete( fd_sbpf_calldests_leave( calldests ) ) );
