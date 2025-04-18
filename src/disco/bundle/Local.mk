aec6ead820bb7085cb125d693c517bd7f837d5b7
Richard Patel <ripatel@jumptrading.com>
2025-04-18 17:44:42 +0000

    Move bundle_crank to src/disco/bundle

---
+++ src/disco/bundle/Local.mk
@@ -0,0 +1,4 @@
+$(call add-hdrs,fd_bundle_crank.h)
+$(call add-objs,fd_bundle_crank,fd_disco,fd_flamenco)
+$(call make-unit-test,test_bundle_crank,test_bundle_crank,fd_disco fd_flamenco fd_ballet fd_util)
+$(call run-unit-test,test_bundle_crank)
