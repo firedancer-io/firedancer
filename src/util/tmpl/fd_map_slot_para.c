42a9e546dc80dda838b4321c6f8064bbf1e9b9bb
Richard Patel <ripatel@jumptrading.com>
2025-04-20 02:21:48 +0000

    util: move map flags to a common header
    Fixes build issues when including different map types into the same
    compile unit.

--- src/util/tmpl/fd_map_slot_para.c
+++ src/util/tmpl/fd_map_slot_para.c
@@ -862,6 +862,8 @@
      are nice compact streaming access patterns.  And similarly for the
      element store access patterns. */
 
+#include "fd_map.h"
+
 /* MAP_NAME gives the API prefix to use for map */
 
 #ifndef MAP_NAME
@@ -1046,26 +1048,6 @@
 #ifndef MAP_IMPL_STYLE
 #define MAP_IMPL_STYLE 0
 #endif
-
-/* Common map error codes (FIXME: probably should get around to making
-   unified error codes, error strings and/or flags across util at least
-   so we don't have to do this in the generator itself) */
-
-#define FD_MAP_SUCCESS     (0)
-#define FD_MAP_ERR_INVAL   (-1)
-#define FD_MAP_ERR_AGAIN   (-2)
-//#define FD_MAP_ERR_CORRUPT (-3)
-//#define FD_MAP_ERR_EMPTY   (-4)
-#define FD_MAP_ERR_FULL    (-5)
-#define FD_MAP_ERR_KEY     (-6)
-
-#define FD_MAP_FLAG_BLOCKING      (1<<0)
-#define FD_MAP_FLAG_USE_HINT      (1<<2)
-#define FD_MAP_FLAG_PREFETCH_NONE (0<<3)
-#define FD_MAP_FLAG_PREFETCH_META (1<<3)
-#define FD_MAP_FLAG_PREFETCH_DATA (2<<3)
-#define FD_MAP_FLAG_PREFETCH      (3<<3)
-#define FD_MAP_FLAG_RDONLY        (1<<5)
 
 /* Implementation *****************************************************/
 
