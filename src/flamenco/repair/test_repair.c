e0ea9d2c33650340791eefcd14a25d43f22ff80b
Emily Wang <emwang@jumptrading.com>
2025-04-24 21:54:03 +0000

    iterative repair reqs

--- src/flamenco/repair/test_repair.c
+++ src/flamenco/repair/test_repair.c
@@ -76,6 +76,46 @@ test_repair_tcache( fd_wksp_t * wksp ){
   fd_wksp_free_laddr( fd_repair_delete( fd_repair_leave( repair ) ) );
 }
 
+void
+test_repair_tag( fd_wksp_t * wksp ){
+  ulong needed_max = 128;
+  void * mem = fd_wksp_alloc_laddr( wksp, fd_repair_align(), fd_repair_footprint(needed_max), 1UL );
+  FD_TEST( mem );
+  fd_repair_t * repair = fd_repair_join( fd_repair_new( mem, 128, 42 ) );
+  FD_TEST( repair );
+
+  ulong tag = fd_repair_req_tag( 1, FD_SHRED_IDX_MAX, fd_needed_orphan );
+  ulong slot = fd_repair_req_tag_slot( tag );
+  uint shred_idx = fd_repair_req_tag_shred_idx( tag );
+
+  FD_LOG_WARNING(("tag %lu | slot : %lu, idx %u", tag, slot, shred_idx));
+
+  FD_TEST( slot == 1UL );
+  FD_TEST( shred_idx == FD_SHRED_IDX_MAX );
+
+  tag = fd_repair_req_tag( 3716053, 2, fd_needed_window_index );
+  slot = fd_repair_req_tag_slot( tag );
+  shred_idx = fd_repair_req_tag_shred_idx( tag );
+  FD_LOG_WARNING(("tag %lu | slot : %lu, idx %u", tag, slot, shred_idx));
+  FD_TEST( slot == 3716053UL );
+  FD_TEST( shred_idx == 2UL );
+
+  tag = fd_repair_req_tag( 3716053, FD_SHRED_IDX_MAX, fd_needed_window_index );
+  slot = fd_repair_req_tag_slot( tag );
+  shred_idx = fd_repair_req_tag_shred_idx( tag );
+  FD_LOG_WARNING(("tag %lu | slot : %lu, idx %u", tag, slot, shred_idx));
+  FD_TEST( slot == 3716053UL );
+  FD_TEST( shred_idx == FD_SHRED_IDX_MAX );
+
+  tag = fd_repair_req_tag( 3717534, 0, fd_needed_highest_window_index );
+
+
+  //1 00 0000 0000 1110 0010 1110 0110 0111 10   00 0000 0000 0000 0000 0000 0000 0000
+
+
+  fd_wksp_free_laddr( fd_repair_delete( fd_repair_leave( repair ) ) );
+}
+
 
 int
 main( int argc, char ** argv ) {
@@ -87,7 +127,8 @@ main( int argc, char ** argv ) {
   fd_wksp_t * wksp = fd_wksp_new_anonymous( fd_cstr_to_shmem_page_sz( _page_sz ), page_cnt, fd_shmem_cpu_idx( numa_idx ), "wksp", 0UL );
   FD_TEST( wksp );
 
-  test_repair_tcache( wksp );
+  //test_repair_tcache( wksp );
+  test_repair_tag( wksp );
 
   fd_halt();
   return 0;
