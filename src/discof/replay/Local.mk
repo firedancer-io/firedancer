a4427e38b15562ac36dfb2e6d1b1b3e5b2ab484d
Aman Khinvasara <akhinvasara@jumptrading.com>
2025-04-25 20:18:50 +0000

    complete build when zstd missing

--- src/discof/replay/Local.mk
+++ src/discof/replay/Local.mk
@@ -1,5 +1,9 @@
 ifdef FD_HAS_INT128
 ifdef FD_HAS_SSE
+ifdef FD_HAS_ZSTD # required to load snapshot
 $(call add-objs,fd_replay_tile,fd_discof)
+else
+$(warning "zstd not installed, skipping replay")
+endif
 endif
 endif
