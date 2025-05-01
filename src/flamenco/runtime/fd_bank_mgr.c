adccadc112b68c834e97434dcb7ce6267794986a
ibhatt-jumptrading <ibhatt@jumptrading.com>
2025-05-01 20:06:12 +0000

    reorganizing into macros

--- src/flamenco/runtime/fd_bank_mgr.c
+++ src/flamenco/runtime/fd_bank_mgr.c
@@ -1,9 +1,5 @@
 #include "fd_bank_mgr.h"
 
-#define FD_BANK_MGR_BLOCK_HASH_QUEUE           (0)
-#define FD_BANK_MGR_BLOCK_HASH_QUEUE_FOOTPRINT (50000UL)
-#define FD_BANK_MGR_BLOCK_HASH_QUEUE_ALIGN     (1024UL)
-
 static inline fd_funk_rec_key_t
 fd_bank_mgr_cache_key( ulong entry_id ) {
   fd_funk_rec_key_t id;
@@ -47,85 +43,79 @@ fd_bank_mgr_join( void * mem, fd_funk_t * funk, fd_funk_txn_t * funk_txn ) {
   return bank_mgr;
 }
 
-fd_block_hash_queue_global_t *
-fd_bank_mgr_block_hash_queue_query( fd_bank_mgr_t * bank_mgr ) {
-
-  /* A query from the bank manager is a simple read-only wrapper over
-     fd_funk_rec_query_try_global. This will return the value from the
-     funk record that can live anywhere in the funk txn tree. */
-  fd_funk_rec_query_t query = {0};
-  fd_funk_rec_key_t   key   = fd_bank_mgr_cache_key( FD_BANK_MGR_BLOCK_HASH_QUEUE );
-
-  fd_funk_rec_t const * rec = fd_funk_rec_query_try_global( bank_mgr->funk,
-                                                            bank_mgr->funk_txn,
-                                                            &key,
-                                                            NULL,
-                                                            &query );
-
-  if( FD_UNLIKELY( !rec ) ) {
-    FD_LOG_WARNING(( "Could not find block hash queue" ));
-    return NULL;
-  }
-
-  return (fd_block_hash_queue_global_t *)fd_ulong_align_up( (ulong)fd_funk_val( rec, fd_funk_wksp( bank_mgr->funk ) ), FD_BANK_MGR_BLOCK_HASH_QUEUE_ALIGN );
+#define BANK_MGR_FUNCTION_IMPL(type, name, uppername)                                             \
+type *                                                                                            \
+fd_bank_mgr_##name##_query( fd_bank_mgr_t * bank_mgr ) {                                          \
+  fd_funk_rec_query_t   query = {0};                                                              \
+  fd_funk_rec_key_t     key   = fd_bank_mgr_cache_key( FD_BANK_MGR_##uppername##_ID );            \
+  fd_funk_rec_t const * rec   = fd_funk_rec_query_try_global( bank_mgr->funk,                     \
+                                                              bank_mgr->funk_txn,                 \
+                                                              &key,                               \
+                                                              NULL,                               \
+                                                              &query );                           \
+  if( FD_UNLIKELY( !rec ) ) {                                                                     \
+    FD_LOG_WARNING(( "Could not find block hash queue" ));                                        \
+    return NULL;                                                                                  \
+  }                                                                                               \
+  return (type *)fd_ulong_align_up( (ulong)fd_funk_val( rec, fd_funk_wksp( bank_mgr->funk ) ),    \
+                                    FD_BANK_MGR_##uppername##_ALIGN );                            \
+}                                                                                                 \
+                                                                                                  \
+type *                                                                                            \
+fd_bank_mgr_##name##_modify( fd_bank_mgr_t * bank_mgr ) {                                         \
+  memset( &bank_mgr->prepare, 0, sizeof(fd_funk_rec_prepare_t) );                                 \
+  fd_funk_rec_query_t   query   = {0};                                                            \
+  fd_funk_rec_key_t     key     = fd_bank_mgr_cache_key( FD_BANK_MGR_##uppername##_ID );          \
+  fd_funk_txn_t const * txn_out = NULL;                                                           \
+  fd_funk_rec_t const * rec     = fd_funk_rec_query_try_global( bank_mgr->funk,                   \
+                                                                bank_mgr->funk_txn,               \
+                                                                &key,                             \
+                                                                &txn_out,                         \
+                                                                &query );                         \
+                                                                                                  \
+  uchar * old_data = !!rec ? fd_funk_val( rec, fd_funk_wksp( bank_mgr->funk ) ) : NULL;           \
+  fd_funk_rec_t * in_prep_rec = fd_funk_rec_prepare( bank_mgr->funk,                              \
+                                                     bank_mgr->funk_txn,                          \
+                                                     &key,                                        \
+                                                     &bank_mgr->prepare,                          \
+                                                     NULL );                                      \
+                                                                                                  \
+  if( FD_UNLIKELY( !in_prep_rec ) ) {                                                             \
+    FD_LOG_ERR(( "Could not clone" ));                                                            \
+  }                                                                                               \
+                                                                                                  \
+  int     err      = 0;                                                                           \
+  uchar * new_data = fd_funk_val_truncate( in_prep_rec,                                           \
+                                           FD_BANK_MGR_##uppername##_FOOTPRINT,                   \
+                                           fd_funk_alloc( bank_mgr->funk ),                       \
+                                           fd_funk_wksp( bank_mgr->funk ),                        \
+                                           &err );                                                \
+                                                                                                  \
+  if( FD_UNLIKELY( err ) ) {                                                                      \
+    FD_LOG_ERR(( "Could not resize err=%d", err ));                                               \
+  }                                                                                               \
+                                                                                                  \
+  uchar * new_data_start = (uchar*)fd_ulong_align_up( (ulong)new_data,                            \
+                                                      FD_BANK_MGR_##uppername##_ALIGN );          \
+                                                                                                  \
+  if( rec ) {                                                                                     \
+    uchar * old_data_start = (uchar*)fd_ulong_align_up( (ulong)old_data,                          \
+                                                        FD_BANK_MGR_##uppername##_ALIGN );        \
+    fd_memcpy( new_data_start,                                                                    \
+               old_data_start,                                                                    \
+               FD_BANK_MGR_##uppername##_FOOTPRINT - ((ulong)new_data_start - (ulong)new_data) ); \
+  }                                                                                               \
+                                                                                                  \
+  if( txn_out == bank_mgr->funk_txn ) {                                                           \
+    fd_funk_rec_hard_remove( bank_mgr->funk, bank_mgr->funk_txn, &key );                          \
+  }                                                                                               \
+                                                                                                  \
+  return (type *)new_data_start;                                                                  \
+}                                                                                                 \
+int                                                                                               \
+fd_bank_mgr_##name##_save( fd_bank_mgr_t * bank_mgr ) {                                           \
+  fd_funk_rec_publish( bank_mgr->funk, &bank_mgr->prepare );                                      \
+  fd_memset( &bank_mgr->prepare, 0, sizeof(fd_funk_rec_prepare_t) );                              \
+  return 0;                                                                                       \
 }
-
-fd_block_hash_queue_global_t *
-fd_bank_mgr_block_hash_queue_modify( fd_bank_mgr_t * bank_mgr ) {
-  /* We first try to query the current iteration of the record if it
-     exists in the current transaction. If it exists in the current
-     funk transaction, then we can assume that the record was modified
-     in the current transaction. */
-  memset( &bank_mgr->prepare, 0, sizeof(fd_funk_rec_prepare_t) );
-  fd_funk_rec_query_t   query   = {0};
-  fd_funk_rec_key_t     key     = fd_bank_mgr_cache_key( FD_BANK_MGR_BLOCK_HASH_QUEUE );
-  fd_funk_txn_t const * txn_out = NULL;
-  fd_funk_rec_t const * rec     = fd_funk_rec_query_try_global( bank_mgr->funk,
-                                                                bank_mgr->funk_txn,
-                                                                &key,
-                                                                &txn_out,
-                                                                &query );
-
-  uchar * old_data = !!rec ? fd_funk_val( rec, fd_funk_wksp( bank_mgr->funk ) ) : NULL;
-
-  fd_funk_rec_t * in_prep_rec = fd_funk_rec_prepare( bank_mgr->funk, bank_mgr->funk_txn, &key, &bank_mgr->prepare, NULL );
-  if( FD_UNLIKELY( !in_prep_rec ) ) {
-    FD_LOG_ERR(( "Could not clone block hash queue" ));
-  }
-
-  /* TODO: Replace new_val_sz with a closely bounded macro. */
-  int     err      = 0;
-  uchar * new_data = fd_funk_val_truncate( in_prep_rec,
-                                           FD_BANK_MGR_BLOCK_HASH_QUEUE_FOOTPRINT,
-                                           fd_funk_alloc( bank_mgr->funk ),
-                                           fd_funk_wksp( bank_mgr->funk ),
-                                           &err );
-
-  if( FD_UNLIKELY( err ) ) {
-  FD_LOG_ERR(( "Could not resize block hash queue %d", err ));
-  }
-
-  uchar * new_data_start = (uchar*)fd_ulong_align_up( (ulong)new_data, FD_BANK_MGR_BLOCK_HASH_QUEUE_ALIGN );
-
-  if( rec ) {
-    uchar * old_data_start = (uchar*)fd_ulong_align_up( (ulong)old_data, FD_BANK_MGR_BLOCK_HASH_QUEUE_ALIGN );
-    fd_memcpy( new_data_start, old_data_start, FD_BANK_MGR_BLOCK_HASH_QUEUE_FOOTPRINT - ((ulong)new_data_start - (ulong)new_data) );
-  }
-
-  /* If the record already exists in the current funk transaction,
-     remove it at this point. */
-  if( txn_out == bank_mgr->funk_txn ) {
-    fd_funk_rec_hard_remove( bank_mgr->funk, bank_mgr->funk_txn, &key );
-  }
-
-  return (fd_block_hash_queue_global_t *)new_data_start;
-}
-
-int
-fd_bank_mgr_block_hash_queue_save( fd_bank_mgr_t * bank_mgr ) {
-  /* Publish the record into the current funk transaction and clear the
-     now stale funk record prepare. */
-  fd_funk_rec_publish( bank_mgr->funk, &bank_mgr->prepare );
-  fd_memset( &bank_mgr->prepare, 0, sizeof(fd_funk_rec_prepare_t) );
-  return 0;
-}
+FD_BANK_MGR_ITER(BANK_MGR_FUNCTION_IMPL)
--- src/flamenco/runtime/fd_bank_mgr.h
+++ src/flamenco/runtime/fd_bank_mgr.h
@@ -2,6 +2,47 @@
 #include "../types/fd_types.h"
 #include "../../funk/fd_funk.h"
 
+/* The bank manager is a wrapper on top of funk that manages on-chain
+   state not represented by accounts. In practice, this on-chain state
+   is a direct parallel to the Bank data structure in the Agave client.
+
+   Each "member" of the bank manager is represented by a funk record.
+   The rationale behind this is to make the bank fork-aware by
+   leveraging the fact that funk already is. Also, by separating out
+   each member of the bank into its own funk record means that we will
+   not need to copy the entire bank while forking. A record will only
+   be copied if it is modified in a slot. Each bank manager record must
+   not contain gaddrs or local pointers -- all data must be accessed
+   directly or with an offset.
+
+   The standard usage pattern of the bank manager is to first refresh a
+   local join for each slot via a call to fd_bank_mgr_join().
+   If this join is not refreshed with the latest funk txn, then the
+   caller will receive stale data. There are two ways to access the
+   data after a join has been established:
+   1. non-mutable: call _query(). The data returned by this call should
+      not be modified and will return a direct pointer to the data in
+      the funk record map.
+   2. mutable: call _modfiy() followed by a _save(). _modify() will
+      return a pointer to the data structure of the previous incarnation
+      of record. This record can be freely modified, but it must be
+      followed by a call to _save() or the changes will be discarded.
+      For a single join of the bank manager, _modify() and _save()
+      should not be interleaved.
+
+   TODO: Consider supporting multiple funk rec prepares per join.
+   TODO: Replace the below ID, FOOTPRINT, ALIGN with macros and ideally
+   a function that generates the worst case footprint and align given
+   some parameters. */
+
+#define FD_BANK_MGR_BLOCK_HASH_QUEUE_ID        (0)
+#define FD_BANK_MGR_BLOCK_HASH_QUEUE_FOOTPRINT (50000UL)
+#define FD_BANK_MGR_BLOCK_HASH_QUEUE_ALIGN     (1024UL)
+
+#define FD_BANK_MGR_SLOT_ID        (1)
+#define FD_BANK_MGR_SLOT_FOOTPRINT (8UL)
+#define FD_BANK_MGR_SLOT_ALIGN     (8UL)
+
 /* TODO: make this struct opaque. */
 struct fd_bank_mgr {
   fd_funk_t *           funk;
@@ -22,12 +63,18 @@ fd_bank_mgr_new( void * mem );
 fd_bank_mgr_t *
 fd_bank_mgr_join( void * mem, fd_funk_t * funk, fd_funk_txn_t * funk_txn );
 
-/* TODO: Turn all of these into an iter macro (see sysvar cache). */
-fd_block_hash_queue_global_t *
-fd_bank_mgr_block_hash_queue_query( fd_bank_mgr_t * bank_mgr );
+#define BANK_MGR_FUNCTIONS(type, name, uppername)        \
+type*                                                    \
+fd_bank_mgr_##name##_query(fd_bank_mgr_t* bank_mgr);     \
+                                                         \
+type*                                                    \
+fd_bank_mgr_##name##_modify(fd_bank_mgr_t* bank_mgr);    \
+                                                         \
+int                                                      \
+fd_bank_mgr_##name##_save(fd_bank_mgr_t* bank_mgr);
 
-fd_block_hash_queue_global_t *
-fd_bank_mgr_block_hash_queue_modify( fd_bank_mgr_t * bank_mgr );
+#define FD_BANK_MGR_ITER(X)                                           \
+  X(fd_block_hash_queue_global_t, block_hash_queue, BLOCK_HASH_QUEUE) \
+  X(ulong,                        slot,             SLOT)
 
-int
-fd_bank_mgr_block_hash_queue_save( fd_bank_mgr_t * bank_mgr );
+FD_BANK_MGR_ITER(BANK_MGR_FUNCTIONS)
