78301c15085190e4d12c225ceee79185f31319c3
Josh Siegel <jsiegel@jumptrading.com>
2025-05-12 19:34:31 +0000

    airlift across resolv stubbing out required functions

--- src/app/firedancer/main.c
+++ src/app/firedancer/main.c
@@ -59,6 +59,7 @@ extern fd_topo_run_tile_t fd_tile_quic;
 extern fd_topo_run_tile_t fd_tile_verify;
 extern fd_topo_run_tile_t fd_tile_dedup;
 extern fd_topo_run_tile_t fd_tile_pack;
+extern fd_topo_run_tile_t fd_tile_resolv;
 extern fd_topo_run_tile_t fd_tile_shred;
 extern fd_topo_run_tile_t fd_tile_sign;
 extern fd_topo_run_tile_t fd_tile_metric;
@@ -87,6 +88,7 @@ fd_topo_run_tile_t * TILES[] = {
   &fd_tile_verify,
   &fd_tile_dedup,
   &fd_tile_pack,
+  &fd_tile_resolv,
   &fd_tile_shred,
   &fd_tile_sign,
   &fd_tile_metric,
