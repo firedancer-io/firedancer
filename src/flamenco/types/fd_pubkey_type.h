3fda3cca46fe1936ccf4828e6f0b62814ab57b9c
Ravyu Sivakumaran <rsivakumaran@jumptrading.com>
2025-05-16 07:33:23 +0000

    gossip: implement details of gossip protocol
    fix gossip crds filter mask
    
    merge fd_bloom and bloom tmpl
    
    fd_contact_info convert pubkey to fd_pubkey_t for easy map key use
    
    granular CRDS metrics
    
    wire in stake info
    
    re-enable TPU, TVU info in my gossip contact info
    
    clean up failed_inserts
    
    push state refactor:
    
    Implemented with fd_pool and fd_dlist to support individual-expiry-based flushing
    
    Refactor gossip_send_fn to use fd_stem_context instead
    
    PR feedback
    
    gossip: ping tracker checks re-ordering
    
    downgrade log warnings
    
    gossip: rx filters (shred and duplicate instance)
    
    gossip: implement gossip_out link
    
    migrate repair and shredcap to new gossip_out link
    
    migrate send tile to new gossip out link
    
    migrate tower tile to use gossip out link
    
    move update to subfolder
    
    publish snapshot hashes
    
    gossip: implement metrics
    
    gossip: push set refactor
    
    cleanup tx_push
    
    gossip: out link to shred
    
    gossip: correct metrics in CRDS insertion
    
    gossip: fd_crds_advance
    
    gossip: unit tests for ping tracker and active set
    
    scaffolding for gossip>snaprd
    
    fix double release
    
    stem shutdown cons fix + monitoring undetermined counts
    
    remove CHECK_UNDETERMINED in fd_crds_checks, bring hash compare to checks_fast in non-happy path instead
    
    bloom filter init fixes
    
    didn't I commit this already??
    
    more fresh list management fixes
    
    refactor stakes update with simd-0180 changes
    
    type check
    
    set version in contact info

---
+++ src/flamenco/types/fd_pubkey_type.h
@@ -0,0 +1,42 @@
+#ifndef src_flamenco_types_fd_pubkey_type_h
+#define src_flamenco_types_fd_pubkey_type_h
+
+#include "../fd_flamenco_base.h"
+#include "../../ballet/bmtree/fd_bmtree.h"
+
+#define FD_HASH_FOOTPRINT (32UL)
+#define FD_HASH_ALIGN (8UL)
+#define FD_PUBKEY_FOOTPRINT FD_HASH_FOOTPRINT
+#define FD_PUBKEY_ALIGN FD_HASH_ALIGN
+/* TODO this should not have packed alignment, but it's misused everywhere */
+
+union __attribute__((packed)) fd_hash {
+  uchar hash[ FD_HASH_FOOTPRINT ];
+  uchar key [ FD_HASH_FOOTPRINT ]; // Making fd_hash and fd_pubkey interchangeable
+
+  // Generic type specific accessors
+  ulong ul  [ FD_HASH_FOOTPRINT / sizeof(ulong) ];
+  uint  ui  [ FD_HASH_FOOTPRINT / sizeof(uint)  ];
+  uchar uc  [ FD_HASH_FOOTPRINT ];
+};
+
+typedef union fd_hash fd_hash_t;
+typedef union fd_hash fd_pubkey_t;
+
+FD_STATIC_ASSERT( sizeof(fd_hash_t) == sizeof(fd_bmtree_node_t), hash incompatibility ); /* various areas of Firedancer code use fd_hash_t as the type for merkle roots */
+
+FD_FN_PURE static inline int
+fd_hash_eq( fd_hash_t const * a,
+            fd_hash_t const * b ) {
+  return 0==memcmp( a, b, sizeof(fd_hash_t) );
+}
+
+FD_FN_PURE static inline int
+fd_hash_eq1( fd_hash_t a,
+             fd_hash_t b ) {
+  return
+    ( a.ul[0]==b.ul[0] ) & ( a.ul[1]==b.ul[1] ) &
+    ( a.ul[2]==b.ul[2] ) & ( a.ul[3]==b.ul[3] );
+}
+
+#endif
--- src/flamenco/types/fd_types_custom.h
+++ src/flamenco/types/fd_types_custom.h
@@ -7,43 +7,9 @@
 #include "../../ballet/bmtree/fd_bmtree.h"
 #include "../../ballet/ed25519/fd_ed25519.h"
 #include "../../ballet/txn/fd_txn.h"
+#include "fd_pubkey_type.h"
 
-#define FD_HASH_FOOTPRINT (32UL)
-#define FD_HASH_ALIGN (8UL)
-#define FD_PUBKEY_FOOTPRINT FD_HASH_FOOTPRINT
-#define FD_PUBKEY_ALIGN FD_HASH_ALIGN
 #define FD_SIGNATURE_ALIGN (8UL)
-
-/* TODO this should not have packed alignment, but it's misused everywhere */
-
-union __attribute__((packed)) fd_hash {
-  uchar hash[ FD_HASH_FOOTPRINT ];
-  uchar key [ FD_HASH_FOOTPRINT ]; // Making fd_hash and fd_pubkey interchangeable
-
-  // Generic type specific accessors
-  ulong ul  [ FD_HASH_FOOTPRINT / sizeof(ulong) ];
-  uint  ui  [ FD_HASH_FOOTPRINT / sizeof(uint)  ];
-  uchar uc  [ FD_HASH_FOOTPRINT ];
-};
-typedef union fd_hash fd_hash_t;
-typedef union fd_hash fd_pubkey_t;
-
-FD_STATIC_ASSERT( sizeof(fd_hash_t) == sizeof(fd_bmtree_node_t), hash incompatibility ); /* various areas of Firedancer code use fd_hash_t as the type for merkle roots */
-
-FD_FN_PURE static inline int
-fd_hash_eq( fd_hash_t const * a,
-            fd_hash_t const * b ) {
-  return 0==memcmp( a, b, sizeof(fd_hash_t) );
-}
-
-FD_FN_PURE static inline int
-fd_hash_eq1( fd_hash_t a,
-             fd_hash_t b ) {
-  return
-    ( a.ul[0]==b.ul[0] ) & ( a.ul[1]==b.ul[1] ) &
-    ( a.ul[2]==b.ul[2] ) & ( a.ul[3]==b.ul[3] );
-}
-
 union fd_signature {
   uchar uc[ 64 ];
   ulong ul[  8 ];
