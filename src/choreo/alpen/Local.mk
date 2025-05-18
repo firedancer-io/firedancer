70e882ca1bf75b8d91932ba261a862f0b90ca829
jvarela-jump <jvarela@jumptrading.com>
2025-05-18 01:31:12 +0000

    choreo: alpen Local.mk TODOs

--- src/choreo/alpen/Local.mk
+++ src/choreo/alpen/Local.mk
@@ -1,7 +1,9 @@
 $(call add-hdrs,fd_alpen.h)
 $(call add-objs,fd_alpen,fd_choreo)
 ifdef FD_HAS_SSE
-$(call add-objs,fd_alpen_tile,fd_discof)
-$(call add-objs,fd_alpen_verify_tile,fd_discof)
+# TODO remove?
+# $(call add-objs,fd_alpen_tile,fd_discof)
+# $(call add-objs,fd_alpen_verify_tile,fd_discof)
 endif
-$(call make-unit-test,test_alpen_vote,test_alpen_vote,fd_discof fd_ballet fd_util)
+# TODO move test_alpen_vote.c to choreo/alpen?
+# $(call make-unit-test,test_alpen_vote,test_alpen_vote,fd_discof fd_ballet fd_util)
