23266c93c710cfeb7369ed1d3dcbc7a20944526d
Aman Khinvasara <akhinvasara@jumptrading.com>
2025-06-11 22:52:38 +0000

    flamenco, gossip: split contact info from gossip into its own header

--- src/flamenco/gossip/Local.mk
+++ src/flamenco/gossip/Local.mk
@@ -1,7 +1,9 @@
 ifdef FD_HAS_HOSTED
 ifdef FD_HAS_INT128
-$(call add-hdrs,fd_gossip.h)
-$(call add-objs,fd_gossip,fd_flamenco)
+$(call add-hdrs,fd_gossip.h fd_contact_info.h)
+$(call add-objs,fd_gossip fd_contact_info,fd_flamenco)
 $(call make-bin,fd_gossip_spy,fd_gossip_spy,fd_flamenco fd_ballet fd_util)
+
+$(call make-unit-test,test_contact_info,test_contact_info,fd_flamenco fd_ballet fd_util)
 endif
 endif
