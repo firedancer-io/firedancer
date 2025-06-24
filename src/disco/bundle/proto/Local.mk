b916a0fa88702a4e854eb9a9be0d5ab273fa66a1
Richard Patel <ripatel@jumptrading.com>
2025-06-24 19:31:27 +0000

    wip

--- src/disco/bundle/proto/Local.mk
+++ src/disco/bundle/proto/Local.mk
@@ -1,1 +1,1 @@
-$(call add-objs,auth.pb block_engine.pb bundle.pb packet.pb shared.pb timestamp.pb,fd_disco)
+$(call add-objs,auth.pb block_engine.pb bundle.pb packet.pb shared.pb shredstream.pb timestamp.pb,fd_disco)
