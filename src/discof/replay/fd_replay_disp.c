e3cf1a5c5999ab1b3fdf5471e62867469a6a5b02
phtaffet-jump <phtaffet@jumptrading.com>
2025-07-01 09:27:00 -0500

    wip

--- src/discof/replay/fd_replay_disp.c
+++ src/discof/replay/fd_replay_disp.c
@@ -14,10 +14,14 @@
    regions.  The tricky part about representing a graph like this
    without dynamic memory allocation is that nodes may have arbitrary
    in-degree and out-degree.  Thus, we use a pretty standard trick and
-   use sibling pointers, denoted with dotted lines.  Each node maintains
-   at most one successor pointer and at most one sibling pointer.
-   Although not shown below, the sibling pointers are circularly linked,
-   so 5's sibling is 3.
+   use sibling pointers, denoted below with dotted lines.  Each node
+   maintains at most one successor pointer and at most one sibling
+   pointer.  Although not shown below, the sibling pointers are
+   circularly linked, so 5's sibling is 3.  Additionally, though not
+   shown below, the child of the last node (7) stores information about
+   what account address all this is for, which facilitates deleting the
+   last node in the graph.
+
 
                              ---> 3 --
                             /     :    \
@@ -51,6 +55,238 @@
    that last referenced the account. */
 
 
+/* The following structs are all very local to this compilation unit,
+   and so they don't have globally acceptable type names (e.g.
+   fd_replay_disp_edge2_t). */
+
+/* edge_t: Fields typed edge_t are actually a bitfield, but C bitfields
+   are gross, so we just do it manually with macros.  If the high bit is
+   set, that means it's the last in the DAG, and the lower 31 bits are
+   an index in the map_pool for the pubkey that points to it.  If the
+   high bit is not set, then the next 23 bits are the transaction index,
+   and the lowest 8 bits are the edge number within that transaction of
+   the edge that corresponds to the same account address as this edge.
+   The proper type would be something like
+   typedef union {
+     struct {
+       uint is_last:1;
+       uint map_pool_idx:31;
+     } last;
+     struct {
+       uint is_last:1;
+       uint txn_idx:23;
+       uint edge_idx:8;
+     } e;
+   } edge_t;
+
+   */
+typedef uint edge_t;
+
+#define EDGE_IS_LAST(x) ((x)&0x80000000U)
+/* FOLLOW_EDGE must not be called on last */
+#define FOLLOW_EDGE(base, x) (__extension__({ uint __e = (x); ((base)+(__e>>8))->edges + (__e & 0xFFU); }))
+#define FOLLOW_EDGE_TXN(base, x) ( (base)+((x)>>8) )
+
+struct edge2 {
+  edge_t child;
+  edge_t neigh;
+};
+typedef struct edge2 edge2_t;
+
+/* txn_node_t is the representation of a transaction as a node in the
+   DAG. */
+struct txn_node {
+  /* in_degree: in the worst case, all the other transactions in the
+     pool read from each of the max number of accounts that this
+     transaction writes to,  so there are MAX_ACCT_LOCKS*depth edges
+     that come into this node, which is less than UINT_MAX. */
+  uint    in_degree;
+  float   score;
+  uint    edge_cnt; /* also stores concurrency lane */
+  edge2_t edges[128]; /* addressed [0, edge_cnt) */
+};
+typedef struct txn_node txn_node_t;
+
+/* ACCT_INFO_FLAG: It's a bit unfortunate that we have to maintain these
+   flags, but basically we need to be able to distinguish the case where
+   there are only readers so that we don't increment in_degree when
+   adding a new txn_node.  If we have any writers, the only way to
+   transition into a state where there are only readers is to complete
+   the last writer.  We know we are in this case when the completed
+   node's child doesn't have a child, and the completed node's child is
+   a reader, as indicated by the LAST_REF_WAS_WRITE bit.
+   LAST_REFERENCE_WAS_WRITE also has the advantage of being easy to
+   maintain. */
+#define ACCT_INFO_FLAG_LAST_REF_WAS_WRITE ((uchar)1)
+#define ACCT_INFO_FLAG_ANY_WRITERS        ((uchar)2)
+
+/* acct_info_t is a node in a map_chain that contains the metadata for a
+   single account address's conflict graph DAG.  In particular, it
+   contains the information needed to know where to insert a node that
+   reads from or writes to the account. */
+struct acct_info {
+  fd_pubkey_t key;
+  uint next;
+  uint prev;
+
+  union {
+    struct {
+      /* This is effectively a pointer to the last node in the DAG for
+         this pubkey.  EDGE_IS_LAST(FOLLOW_EDGE(base, last_reference))
+         is non-zero. */
+      edge_t last_reference;
+
+      /* flags: a combination of ACCT_INFO_FLAG_* bitfields above. */
+      uchar flags;
+      /* 3 bytes and 6 bits of unused space here */
+
+    }; /* When in the main map_chain */
+    struct {
+      uint free_ll_next;
+      uint free_ll_prev;
+    }; /* When not in the main map_chain */
+  };
+
+
+  /* We want to dispatch the READY transactions in an order that
+     maximizes parallelism, but we also want to be able to start
+     dispatching transactions decently well before we have the full
+     conflict graph.  We can do that because we know that contentious
+     accounts tend to stay contentious and uncontentious accounts tend
+     to stay uncontentious.  To that end, we maintain an EMA of the
+     number of transactions that touch the account per block.  This kind
+     of EMA needs 3 variables: the current value, an accumulator for the
+     current block, and some kind of indicator of how old the current
+     block is.  When adding a new transaction, if the transaction is in
+     the same block as the accumulator, it's just an increment.  If it's
+     in a different block, then we mix the accumulator into the EMA
+     value and reset the accumulator before incrementing. */
+  ushort compressed_slotidx;
+  ushort ref_cnt;
+  float  ema_refs;
+};
+typedef acct_info acct_info_t;
+
+/* For the acct_map and the free_acct_map */
+#define MAP_NAME          acct_map
+#define MAP_ELE_T         acct_info_t
+#define MAP_IDX_T         uint
+#define MAP_KEY_T         fd_pubkey_t
+#define MAP_KEY_HASH(k,s) fd_hash( (s), (k)->b, 32UL )
+#define MAP_KEY_EQ(k0,k1) (!memcmp((*(k0))->b, (*(k1))->b, 32UL))
+#define MAP_OPTIMIZE_RANDOM_ACCESS_REMOVAL 1
+#include "../../util/tmpl/fd_map_chain.c"
+
+
+#define DLIST_IDX_T uint
+#define DLIST_PREV  free_ll_prev
+#define DLIST_NEXT  free_ll_next
+#define DLIST_NAME  free_dlist
+#define DLIST_ELE_T acct_info_t
+#include "../../util/tmpl/fd_dlist.c"
+
+
+/* For the freelist of txn indices */
+#define QUEUE_NAME txn_freelist
+#define QUEUE_T    uint
+#include "util/tmpl/fd_queue.c"
+
+
+struct pending_prq_ele {
+  /* higher score means should be scheduled sooner */
+  float score;
+
+  /* idx_slot: The high 23 bits are the transaction index.  The low 9
+     bits are the compressed slot index.  As long as the pending
+     transactions span less than 256 slots, we can always tell which one
+     is actually the lower number, in spite of overflow:
+        slot1 = slot1_hi * 2^9 + slot1_lo, where slot1_lo in [0, 2^9)
+        slot2 = slot2_hi * 2^9 + slot2_lo, where slot2_lo in [0, 2^9)
+     Put x = slot1-slot2.  |x|<2^8, which means x can be identified
+     uniquely by knowing its equivalence class mod 2^9.
+
+     x = slot1-slot2 = (slot1_hi-slot2_hi)*2^9 + slot1_lo - slot2_lo
+               == slot1_lo - slot2_lo  (mod 2^9)
+
+     However, C's % operator is a bit funky when it comes to negative
+     numbers.  x+256 is strictly positive, so we can compute the
+     equivalence class of x+256 with bitwise operations.
+     Then consider the cases:
+        -2^8<x<0:
+             Then 0<x+256<256,
+             so   0<(slot1_lo-slot2_lo+256) & (2^9-1)<256
+         x==0:
+             Then x+256==256,
+             so   (slot1_lo-slot2_lo+256) & (2^9-1) == 256
+         0<x<2^8:
+             Then 256<x+256<2^9,
+             so   256<(slot1_lo-slot2_lo+256) & (2^9-1)<2^9.
+
+     If we're only interested in returning the right sign, ala strcmp,
+     then ((slot1_lo - slot2_lo + 256) & (2^9-1))-256 gives the right
+     answer. */
+
+  uint  idx_slot;
+};
+
+typedef struct pending_prq_ele pending_prq_ele_t;
+
+
+#define PRQ_NAME pending_prq
+#define PRQ_T    pending_prq_ele_t
+#define PRQ_EXPLICIT_TIMEOUT 0
+#define PRQ_AFTER(x,y) (__extension__( {                   \
+            uint __slot1_lo = x.idx_slot & 0x1FFU;         \
+            uint __slot2_lo = y.idx_slot & 0x1FFU;         \
+            int slot_cmp = -256 + (int)(((256U + slot1_lo - slot2_lo) & ((1U<<9)-1U))); \
+            fd_int_if( slot_cmp!=0, slot_cmp>0, x.score<y.score ); \
+            }))
+
+/* per_slot_t maintains a little metadata about transactions for each
+   slot.  It's primary use is to identify when we've finished
+   dispatching transactions for slot N so that we know to pause until
+   the slot is advanced before dispatching transactions for slot N+1. */
+struct per_slot {
+  ulong slot_idx; /* the compressed slot_idx */
+  ulong opaque_slot_number;
+  ulong inserted_cnt;
+  ulong dispatched_cnt;
+};
+typedef struct per_slot per_slot_t;
+
+
+/* We maintain two maps from pubkeys to acct_info_t.  The first one is
+   the main acct_map, just called acct_map.  All pubkeys in this map
+   have >0 references in the current DAG.  When an account goes to 0
+   references, it gets removed from main map_chain and moved to free
+   map_chain, called free_acct_map.  The free_acct_map exists to
+   maintain the reference count EMA information lazily.  Unless we need
+   the acct_info_t for something in the DAG, we might as well maintain
+   the EMA info.
+
+   When we start up, all the acct_info_t structs are in the
+   free_acct_dlist.  Whenever something is added to the free_acct_map,
+   it's also added to the tail of the free_acct_dlist.  When we need an
+   acct_info_t that's not in the free_acct_map, we pop the head of the
+   free_acct_dlist.  In general, the free_acct_dlist contains everything
+   in the free_acct_map, potentially plus some elements that have never
+   been used; all acct_info_t objects are in exactly one of the main
+   acct_map and the free_acct_dlist (not free_acct_map). */
+
+struct fd_replay_disp {
+  acct_map_t   * acct_map;
+  acct_map_t   * free_acct_map;
+  acct_info_t  * acct_pool;
+  free_dlist_t * free_acct_dlist;
+
+  ulong        max_txns;
+  txn_node_t * txns; /* indexed [0, max_txns), with 0 being a sentinel */
+  uint       * txn_freelist; /* an fd_queue_dynamic */
+
+  pending_prq_ele_t * pending_prq;
+  per_slot_t * per_slot_q; /* an fd_queue_dynamic */
+};
+
 
 e = 2*( 1 bit is last, 23 bits txn idx, 8 bits edge num )
 
@@ -58,21 +294,32 @@ base_ptr + (129*8)*(x>>8) + 8 + 8*(x&0xFF)
 
 If high bit set for child, then 31 bits are index in map
 
-struct acct_info {
-  pubkey key;
-  chaining fields;
-  e last_reference;
-  bool last_reference_was_write;
-  bool any_writers;
-};
+#define ACCT_ITER_TO_PTR( iter ) (__extension__( {                                             \
+      ulong __idx = fd_txn_acct_iter_idx( iter );                                              \
+      fd_ptr_if( __idx<fd_txn_account_cnt( txn, FD_TXN_ACCT_CAT_IMM ), accts, alt_adj )+__idx; \
+      }))
 
-struct txn {
-  uint in_degree;
-  uint edge_cnt;
-  e edges[128];
-};
+ulong
+fd_replay_disp_add_txn( fd_replay_disp_t     * disp,
+                        fd_txn_t const       * txn,
+                        uchar const          * payload,
+                        fd_acct_addr_t const * alt_expanded ) {
 
+  ulong in_degree = 0UL;
 
+  fd_acct_addr_t const * accts   = fd_txn_get_acct_addrs( txn, payload );
+  /* alt_adj is the pointer to the ALT expansion, adjusted so that if
+     account address n is the first that comes from the ALT, it can be
+     accessed with adj_lut[n]. */
+  fd_acct_addr_t const * alt_adj = alt_expanded - fd_txn_account_cnt( txn, FD_TXN_ACCT_CAT_IMM );
+
+  for( fd_txn_acct_iter_t iter=fd_txn_acct_iter_init( txn, FD_TXN_ACCT_CAT_WRITABLE );
+      iter!=fd_txn_acct_iter_end(); iter=fd_txn_acct_iter_next( iter ) ) {
+    fd_acct_addr_t const * acct = ACCT_ITER_TO_PTR( iter );
+    acct_info_t * a_info = acct_map_ele_query( disp->acct_map, acct, NULL, disp->acct_pool );
+    if( FD_LIKELY( a_
+
+  }
 Adding:
 
 iterate over all accounts. Look up in map.
@@ -135,3 +382,10 @@ Do the same for any siblings of child
 If my child's "child" is the map field, set any_writers to last_reference_was_write
 
 If I don't have a child, delete from map
+
+
+
+When trying to allocate an account, call map_remove on it in the free list.
+If hit, remove from free_list.
+Otherwise, remove the head of the free_list.
+  If free_next != UINT_MAX, remove it
--- src/discof/replay/fd_replay_disp.h
+++ src/discof/replay/fd_replay_disp.h
@@ -13,8 +13,8 @@
    not independent, the one that comes earlier in the block is a
    predecessor transaction of the one that comes later in the block.  In
    order to ensure correct execution, a transaction cannot be executed
-   until all its precessor transactions have completed.  In this file, a
-   transaction completing means that the results of its account writes
+   until all its predecessor transactions have completed.  In this file,
+   a transaction completing means that the results of its account writes
    will be visible to a subsequent transaction executing on any core.
 
    Shockingly, this dispatcher does not need to keep a copy of
@@ -22,7 +22,7 @@
    entirely on transaction indices.  An index is a positive integer
    (uint) up to some maximum.  The 0 index is a sentinel, and doesn't
    correspond to a real transaction.  It's up to the caller to maintain
-   the mapping between transaction indices and the transaction themself.
+   the mapping between each transaction index and the transaction itself.
 
    A transaction index is in one of the following states:
      * FREE, which means it doesn't correspond to a transaction
@@ -34,9 +34,62 @@
        get_next_ready yet
      * DISPATCHED, which means this transaction index was returned by
        get_next_ready but has not been completed yet.
-*/
+
+
+                        --------------> PENDING
+                add_txn |                  |
+         FREE  ---------|                  |
+          ^             |                  V    get_next_ready
+          |             -------------->  READY ----------------
+          |                                                   |
+          |                                                   |
+          |                                                   |
+          |                  complete_txn                     V
+          ---------------------------------------------- DISPATCHED
+
+
+  Additionally, fd_replay_disp is slot-aware, and even somewhat
+  fork-aware.  Prior to inserting a transaction, an insert_slot must be
+  init-ed, optionally chaining it to a parent. Then, when a transaction
+  is inserted, the caller must specify the insert_slot it is part of.
+
+  Using the parent information, fd_replay_disp stores these slots in a
+  "linear forest" representation, that is, a collection of path graphs.
+  In each path in the path graph, transactions can only be added to the
+  last slot and scheduled from the first slot.  Each path graph may
+  occupy a resource that fd_replay_disp calls a concurrency lane.
+
+  Consider the following example:
+   - Slot 10's parent is not specified
+   - Slot 11's parent is slot 10
+   - Slot 13's parent is slot 11
+   - Slot 14's parent is also slot 11
+
+  That produces the following linear forest representation.
+
+                    10 --> 11 --> 13
+                    14
+
+  Note that the information about slot 14's parent being slot 11 is lost
+  in this representation.
+
+  Concurrency lane occupation is lazy, and only happens when actual
+  transactions are inserted for the slots.  If all four slots mentioned
+  have transactions in them, then this occupies two concurrency lanes.
+  In this state, calls to schedule transactions from slot 10 can be
+  intermixed with calls to schedule transactions from slot 14
+  aribitrarily.  In this state, calls to schedule from slot 11 or 13 are
+  not allowed.
+
+  Slots stop occupying a concurrency lane as soon as there are no
+  transactions for that insert_slot in any of the PENDING, READY, or
+  DISPATCHED states.  The caller doesn't need to do anything special to
+  free it.  This means the exact concurrency lane a slot occupies can
+  change, but this is not exposed at all to the caller. */
 
 #define FD_REPLAY_DISP_MAX_DEPTH 0x7FFFFFUL /* 23 bit numbers, approx 8M */
+
+FD_PROTOTYPES_BEGIN
 
 /* fd_replay_disp_{align,footprint} return the required alignment and
    footprint in bytes for a region of memory to be used as a dispatcher.
@@ -44,20 +97,25 @@
    tracked at a time.  Depth must be at least 2 and cannot exceed
    FD_REPLAY_DISP_MAX_DEPTH. */
 ulong fd_replay_disp_align    ( void        );
-ulong fd_replay_disp_footprint( ulong depth );
+ulong fd_replay_disp_footprint( ulong depth, ulong slot_depth );
 
 
 /* TODO: document */
 void *
 fd_replay_disp_new( void * mem,
-                    ulong  depth );
+                    ulong  depth,
+                    ulong  slot_depth );
 
 fd_replay_disp_t *
 fd_replay_disp_join( void * mem );
 
-/* fd_replay_disp_add_txn adds a transaction in serial order.  That
-   means that this dispatcher will ensure this transaction appears to
-   execute after each transaction added in a prior call.
+
+fd_deplay
+
+/* fd_replay_disp_add_txn adds a transaction to an insert_slot in serial
+   order.  That means that this dispatcher will ensure this transaction
+   appears to execute after each transaction added to this insert_slot
+   in a prior call.
 
    Shockingly, this dispatcher does not retain any read interest (much
    less write interest) in the transaction.  On success, it returns a
@@ -77,14 +135,18 @@ fd_replay_disp_join( void * mem );
    will be in the PENDING or READY state. */
 ulong
 fd_replay_disp_add_txn( fd_replay_disp_t     * disp,
+                        void                 * insert_slot,
                         fd_txn_t const       * txn,
                         uchar const          * payload,
                         fd_acct_addr_t const * alts );
 
 /* fd_replay_disp_get_next_ready returns a READY transaction if one
-   exists, and 0 otherwise.  disp must be a valid local join.  At the
-   time this function returns, the returned transaction index will be in
-   the DISPATCHED state. */
+   exists, and 0 otherwise.  If there are multiple READY transactions,
+   which exact one is returned is arbitrary.  That said, this function
+   does make some effort to pick one that (upon completion) will unlock
+   more parallelism.  disp must be a valid local join.  At the time this
+   function returns, the returned transaction index (if nonzero) will
+   transition to the DISPATCHED state. */
 ulong
 fd_replay_disp_get_next_ready( fd_replay_disp_t * disp );
 
@@ -103,5 +165,7 @@ fd_replay_disp_leave( fd_replay_disp_t * disp );
 
 void *
 fd_replay_disp_delete( void * mem );
+
+FD_PROTOTYPES_END
 
 #endif /* HEADER_fd_src_discof_replay_fd_replay_disp_h */
