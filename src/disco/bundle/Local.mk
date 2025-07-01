fa523a86667edf48721d4f0bfe282067cd7fd369
Anway De <ande@jumptrading.com>
2025-07-01 06:43:30 +0000

    make test_bundle_client only if FD_HAS_SSE

--- src/disco/bundle/Local.mk
+++ src/disco/bundle/Local.mk
@@ -8,7 +8,9 @@ endif
 $(call add-hdrs,fd_bundle_tile.h)
 $(call add-objs,fd_bundle_auth fd_bundle_client,fd_disco)
 ifdef FD_HAS_HOSTED
+ifdef FD_HAS_SSE
 $(call make-unit-test,test_bundle_client,test_bundle_client,fd_disco fd_waltz fd_flamenco fd_tango fd_ballet fd_util,$(OPENSSL_LIBS))
+endif
 $(call make-fuzz-test,fuzz_bundle_auth_resp,fuzz_bundle_auth_resp,fd_disco fd_waltz fd_flamenco fd_tango fd_ballet fd_util,$(OPENSSL_LIBS))
 endif
 
