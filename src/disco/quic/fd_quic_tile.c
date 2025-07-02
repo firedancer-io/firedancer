b887419aeb9f33ea17acb30204bb2511b6d85aa8
Richard Patel <ripatel@jumptrading.com>
2025-07-02 23:50:31 +0000

    quic: fix infrequent metrics (stem lazy bug)

--- src/disco/quic/fd_quic_tile.c
+++ src/disco/quic/fd_quic_tile.c
@@ -612,6 +612,7 @@ populate_allowed_fds( fd_topo_t const *      topo,
 }
 
 #define STEM_BURST (1UL)
+#define STEM_LAZY  ((long)10e6) /* 10ms */
 
 #define STEM_CALLBACK_CONTEXT_TYPE  fd_quic_ctx_t
 #define STEM_CALLBACK_CONTEXT_ALIGN alignof(fd_quic_ctx_t)
