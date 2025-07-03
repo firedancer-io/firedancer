573a338613728cf6c7d343331e2c6a31a19efff0
Richard Patel <ripatel@jumptrading.com>
2025-07-03 08:29:15 +0000

    bundle: disable HTTP/2 pings

--- src/disco/bundle/fd_bundle_tile.c
+++ src/disco/bundle/fd_bundle_tile.c
@@ -546,7 +546,7 @@ unprivileged_init( fd_topo_t *      topo,
   ctx->so_rcvbuf = (int)so_rcvbuf;
 
   /* Set idle ping timer */
-  fd_bundle_client_set_ping_interval( ctx, (long)tile->bundle.keepalive_interval_nanos );
+  // fd_bundle_client_set_ping_interval( ctx, (long)tile->bundle.keepalive_interval_nanos );
 
   ctx->bundle_status_plugin = 127;
   ctx->bundle_status_recent = FD_PLUGIN_MSG_BLOCK_ENGINE_UPDATE_STATUS_DISCONNECTED;
