7cb65ef9c76a79b96cf9e9f177f5550665058d4c
Richard Patel <ripatel@jumptrading.com>
2025-07-09 20:52:55 +0000

    runtime: nuke unused code

--- src/discof/replay/Local.mk
+++ src/discof/replay/Local.mk
@@ -1,6 +1,5 @@
 ifdef FD_HAS_INT128
-$(call add-hdrs,fd_replay.h fd_replay_notif.h)
-$(call add-objs,fd_replay,fd_discof)
+$(call add-hdrs,fd_replay_notif.h)
 $(call add-objs,fd_exec,fd_discof)
 ifdef FD_HAS_ZSTD # required to load snapshot
 $(call add-objs,fd_replay_tile,fd_discof)
