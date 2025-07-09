407ba4d7c7d825e53f41d3f0abd4b877f755a850
cali-jumptrading <cali@jumptrading.com>
2025-07-09 21:21:18 +0000

    snapshots: configure stage

---
+++ src/app/shared/commands/configure/snapshots.c
@@ -0,0 +1,38 @@
+#include "../../../shared/commands/configure/configure.h"
+
+#include "../../../platform/fd_file_util.h"
+
+#include <errno.h>
+#include <sys/stat.h>
+
+static void
+init( config_t const * config ) {
+  if( FD_UNLIKELY( -1==fd_file_util_mkdir_all( config->paths.snapshots, config->uid, config->gid ) ) )
+    FD_LOG_ERR(( "could not create snapshots directory `%s` (%i-%s)", config->paths.snapshots, errno, fd_io_strerror( errno ) ));
+}
+
+static configure_result_t
+check( config_t const * config ) {
+  struct stat st;
+  if( FD_UNLIKELY( stat( config->paths.snapshots, &st ) && errno==ENOENT ) )
+    NOT_CONFIGURED( "`%s` does not exist", config->paths.snapshots );
+
+  CHECK( check_dir( config->paths.snapshots, config->uid, config->gid, S_IFDIR | S_IRUSR | S_IWUSR | S_IXUSR ) );
+  CONFIGURE_OK();
+}
+
+static void
+fini( config_t const * config,
+      int              pre_init ) {
+  (void)pre_init;
+  if( -1==fd_file_util_rmtree( config->paths.snapshots, 1 ) )
+    FD_LOG_ERR(( "rmtree `%s` failed (%i-%s)", config->paths.snapshots, errno, fd_io_strerror( errno ) ));
+}
+
+configure_stage_t fd_cfg_stage_snapshots = {
+  .name            = "snapshots",
+  .always_recreate = 0,
+  .init            = init,
+  .fini            = fini,
+  .check           = check,
+};
