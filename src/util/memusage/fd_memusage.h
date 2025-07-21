46dcda8322bb3da07edb033d04ef4ec90ea2f015
Alex Siegel <asiegel@jumptrading.com>
2025-07-21 14:55:11 +0000

    memory usage tool

---
+++ src/util/memusage/fd_memusage.h
@@ -0,0 +1,8 @@
+#ifndef HEADER_fd_src_util_memusage_fd_memusage_h
+#define HEADER_fd_src_util_memusage_fd_memusage_h
+
+#include "../fd_util.h"
+
+void fd_memusage_printout( char const * filename );
+
+#endif /* HEADER_fd_src_util_memusage_fd_memusage_h */
