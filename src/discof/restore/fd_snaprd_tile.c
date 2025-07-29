d259593dd21d3db2fb23f729c11d92f2e41ef958
Alex Peng <alpeng@jumptrading.com>
2025-07-29 21:53:16 +0000

    snaprd: add ips

--- src/discof/restore/fd_snaprd_tile.c
+++ src/discof/restore/fd_snaprd_tile.c
@@ -647,12 +647,13 @@ unprivileged_init( fd_topo_t *      topo,
   FD_TEST( ctx->sshttp );
 
   if( FD_LIKELY( !strcmp( tile->snaprd.cluster, "testnet" ) ) ) {
-    fd_ip4_port_t initial_peers[ 2UL ] = {
-      { .addr = FD_IP4_ADDR( 145, 40, 95, 69 ), .port = 8899 }, /* Solana testnet peer */
-      { .addr = FD_IP4_ADDR( 35, 209, 131, 19 ), .port = 8899 }
+    fd_ip4_port_t initial_peers[ 3UL ] = {
+      { .addr = FD_IP4_ADDR( 145, 40 , 95 , 69  ), .port = 8899 }, /* Solana testnet peer */
+      { .addr = FD_IP4_ADDR( 35 , 209, 131, 19  ), .port = 8899 },
+      { .addr = FD_IP4_ADDR( 35 , 214, 172, 227 ), .port = 8899 }
     };
 
-    for( ulong i=0UL; i<2UL; i++ ) fd_ssping_add( ctx->ssping, initial_peers[ i ] );
+    for( ulong i=0UL; i<3UL; i++ ) fd_ssping_add( ctx->ssping, initial_peers[ i ] );
   } else if( FD_LIKELY( !strcmp( tile->snaprd.cluster, "private" ) ) ) {
     fd_ip4_port_t initial_peers[ 1UL ] = {
       { .addr = FD_IP4_ADDR( 147, 28, 185, 47 ), .port = 8899 } /* A private cluster peer */
@@ -660,11 +661,12 @@ unprivileged_init( fd_topo_t *      topo,
 
     for( ulong i=0UL; i<1UL; i++ ) fd_ssping_add( ctx->ssping, initial_peers[ i ] );
   } else if (FD_LIKELY( !strcmp( tile->snaprd.cluster, "mainnet" ) ) ) {
-    fd_ip4_port_t initial_peers[ 2UL ] = {
-      { .addr = FD_IP4_ADDR( 149, 255, 37, 130 ), .port = 8899 },
-      { .addr = FD_IP4_ADDR( 34, 1, 238, 227 ), .port = 8899 }
+    fd_ip4_port_t initial_peers[ 3UL ] = {
+      { .addr = FD_IP4_ADDR( 149, 255, 37 , 130 ), .port = 8899 },
+      { .addr = FD_IP4_ADDR( 34 , 1  , 238, 227 ), .port = 8899 },
+      { .addr = FD_IP4_ADDR( 34 , 1  , 139, 131 ), .port = 8899 }
     };
-    for( ulong i=0UL; i<2UL; i++ ) fd_ssping_add( ctx->ssping, initial_peers[ i ] );
+    for( ulong i=0UL; i<3UL; i++ ) fd_ssping_add( ctx->ssping, initial_peers[ i ] );
   }
   else {
     FD_LOG_ERR(( "unexpected cluster %s", tile->snaprd.cluster ));
