a6a2569085df7d2d1828ab98dfe05923e7da5f00
Aman Khinvasara <akhinvasara@jumptrading.com>
2025-07-30 00:28:47 +0000

    quic metrics: count conns by state, rename active to alloc

--- src/disco/gui/fd_gui.c
+++ src/disco/gui/fd_gui.c
@@ -499,7 +499,7 @@ fd_gui_tile_stats_snap( fd_gui_t *                     gui,
     fd_topo_tile_t const * quic = &topo->tiles[ fd_topo_find_tile( topo, "quic", i ) ];
     volatile ulong * quic_metrics = fd_metrics_tile( quic->metrics );
 
-    stats->quic_conn_cnt += quic_metrics[ MIDX( GAUGE, QUIC, CONNECTIONS_ACTIVE ) ];
+    stats->quic_conn_cnt += quic_metrics[ MIDX( GAUGE, QUIC, CONNECTIONS_ALLOC ) ];
   }
 
   ulong bundle_tile_idx = fd_topo_find_tile( topo, "bundle", 0UL );
