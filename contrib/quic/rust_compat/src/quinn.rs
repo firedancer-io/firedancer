3e10a26261cf8928998703ece1ff120ea287140f
Richard Patel <ripatel@jumptrading.com>
2025-08-08 00:11:17 +0000

    contrib: repair quiche interop test

--- contrib/quic/rust_compat/src/quinn.rs
+++ contrib/quic/rust_compat/src/quinn.rs
@@ -151,7 +151,7 @@ pub(crate) unsafe fn quinn_to_fdquic(crypto_provider: CryptoProvider) {
         assert!(metrics.net_rx_pkt_cnt < 64);
         assert!(metrics.net_tx_pkt_cnt <= metrics.net_rx_pkt_cnt);
         assert!(metrics.net_tx_byte_cnt < metrics.net_rx_byte_cnt);
-        assert!(metrics.conn_active_cnt <= 1);
+        assert!(metrics.conn_state_cnt[3] <= 1);
         assert!(metrics.conn_created_cnt == 1);
         assert!(metrics.conn_closed_cnt <= 1);
         assert!(metrics.conn_aborted_cnt <= 1);
