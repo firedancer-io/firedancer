6e6577e83ad8ccfd0329d7a1cb9ef88067d105a1
Anway De <ande@jumptrading.com>
2025-08-11 10:03:02 +0000

    fix minimal builds

--- src/discof/ipecho/Local.mk
+++ src/discof/ipecho/Local.mk
@@ -1,3 +1,4 @@
+ifdef FD_HAS_ALLOCA
 $(call add-objs,fd_ipecho_tile,fd_discof)
 $(call add-objs,fd_ipecho_client,fd_discof)
 $(call add-objs,fd_ipecho_server,fd_discof)
@@ -6,4 +7,5 @@ $(call make-unit-test,test_ipecho_client,test_ipecho_client,fd_discof fd_disco f
 
 ifdef FD_HAS_HOSTED
 $(call make-fuzz-test,fuzz_ipecho_client,fuzz_ipecho_client,fd_discof fd_flamenco fd_ballet fd_util)
+endif
 endif
