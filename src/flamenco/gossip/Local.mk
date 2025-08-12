c6af592edaf27d75e5c06200e611faea39064537
Ravyu Sivakumaran <rsivakumaran@jumptrading.com>
2025-08-12 21:47:30 +0000

    gossip parser fuzzer

--- src/flamenco/gossip/Local.mk
+++ src/flamenco/gossip/Local.mk
@@ -16,3 +16,7 @@ $(call run-unit-test,test_active_set)
 
 $(call make-unit-test,test_ping_tracker,test_ping_tracker,fd_flamenco fd_ballet fd_util)
 $(call run-unit-test,test_ping_tracker)
+
+ifdef FD_HAS_HOSTED
+$(call make-fuzz-test,fuzz_gossip_msg_parse,fuzz_gossip_msg_parse,fd_flamenco fd_ballet fd_util)
+endif
