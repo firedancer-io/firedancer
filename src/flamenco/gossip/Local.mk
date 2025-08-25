f680d1eb12c5c250f72f11885e77224603e8ceed
Ravyu Sivakumaran <rsivakumaran@jumptrading.com>
2025-08-25 22:49:01 +0000

    gossip: activate active set unit tests

--- src/flamenco/gossip/Local.mk
+++ src/flamenco/gossip/Local.mk
@@ -8,8 +8,8 @@ $(call add-objs,fd_bloom fd_active_set fd_ping_tracker,fd_flamenco)
 $(call make-unit-test,test_bloom,test_bloom,fd_flamenco fd_util)
 $(call run-unit-test,test_bloom)
 
-# $(call make-unit-test,test_active_set,test_active_set,fd_flamenco fd_ballet fd_util)
-# $(call run-unit-test,test_active_set)
+$(call make-unit-test,test_active_set,test_active_set,fd_flamenco fd_ballet fd_util)
+$(call run-unit-test,test_active_set)
 
 # $(call make-unit-test,test_ping_tracker,test_ping_tracker,fd_flamenco fd_ballet fd_util)
 # $(call run-unit-test,test_ping_tracker)
