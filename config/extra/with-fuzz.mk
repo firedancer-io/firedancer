3e8457e63da285197011cd4cabb668a2f11e7bc9
Hrvoje Misetic <misetic@osec.io>
2025-08-29 16:26:37 +0000

    Add memory log support

--- config/extra/with-fuzz.mk
+++ config/extra/with-fuzz.mk
@@ -2,6 +2,8 @@ include config/extra/with-handholding.mk
 
 FD_HAS_FUZZ:=1
 
+CPPFLAGS+=-DFD_HAS_FUZZ=1
+
 CPPFLAGS+=-fno-omit-frame-pointer
 CPPFLAGS+=-fsanitize=fuzzer-no-link
 CPPFLAGS+=-fsanitize-coverage=inline-8bit-counters
