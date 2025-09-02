76efaf752670181062cacc66e7ab8bc8624f102b
Manik Jain <mjain@jumptrading.com>
2025-09-02 20:03:19 +0000

    flamenco: clean up block finalize api

--- src/flamenco/runtime/fd_runtime.h
+++ src/flamenco/runtime/fd_runtime.h
@@ -485,8 +485,7 @@ fd_runtime_block_execute_prepare( fd_exec_slot_ctx_t * slot_ctx,
                                   fd_spad_t *          runtime_spad );
 
 void
-fd_runtime_block_execute_finalize( fd_exec_slot_ctx_t *            slot_ctx,
-                                   fd_runtime_block_info_t const * block_info );
+fd_runtime_block_execute_finalize( fd_exec_slot_ctx_t * slot_ctx );
 
 /* Transaction Level Execution Management *************************************/
 
