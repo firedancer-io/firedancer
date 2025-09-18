b1f180ddd2d65c725d8462a515324069a8163b49
Andrew Mass <amass@jumptrading.com>
2025-09-18 15:51:44 +0000

    ethtool: disable tx-gre-segmentation features
    Renamed ethtool-gro into ethtool-offloads and added logic for GRE
    segmentation.  Also refactored ethtool-loopback to use the
    fd_ethtool_ioctl API.

--- src/app/shared/commands/configure/configure.h
+++ src/app/shared/commands/configure/configure.h
@@ -71,7 +71,7 @@ extern configure_stage_t fd_cfg_stage_hugetlbfs;
 extern configure_stage_t fd_cfg_stage_sysctl;
 extern configure_stage_t fd_cfg_stage_hyperthreads;
 extern configure_stage_t fd_cfg_stage_ethtool_channels;
-extern configure_stage_t fd_cfg_stage_ethtool_gro;
+extern configure_stage_t fd_cfg_stage_ethtool_offloads;
 extern configure_stage_t fd_cfg_stage_ethtool_loopback;
 extern configure_stage_t fd_cfg_stage_snapshots;
 
