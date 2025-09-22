e23321cdad050217dd351215dec2aa9aff106b5a
Manik Jain <mjain@jumptrading.com>
2025-09-22 16:17:14 +0000

    flamenco, feature: raise_account_cu_limit

--- src/flamenco/features/fd_features_generated.h
+++ src/flamenco/features/fd_features_generated.h
@@ -8,7 +8,7 @@
 #endif
 
 /* FEATURE_ID_CNT is the number of features in ids */
-#define FD_FEATURE_ID_CNT (242UL)
+#define FD_FEATURE_ID_CNT (243UL)
 union fd_features {
   ulong f[ FD_FEATURE_ID_CNT ];
   struct {
@@ -254,5 +254,6 @@ union fd_features {
     /* 0xa9e3bfbaf8d67260 */ ulong require_static_nonce_account;
     /* 0x3711b30f40730240 */ ulong enable_vote_address_leader_schedule;
     /* 0xc1309d1b0ae3e80c */ ulong enshrine_slashing_program;
+    /* 0x5c64cc1a9be3790a */ ulong raise_account_cu_limit;
   };
 };
