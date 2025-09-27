83438349194e7d050aacc8dcca917ca3be484388
jherrera-jump <jherrera@jumptrading.com>
2025-09-27 22:36:08 +0000

    gui: precomress frontend, txn search

--- src/disco/gui/Local.mk
+++ src/disco/gui/Local.mk
@@ -4,3 +4,36 @@ $(call add-objs,fd_gui fd_gui_printf fd_gui_tile generated/http_import_dist,fd_d
 $(OBJDIR)/obj/disco/gui/fd_gui_tile.o: book/public/fire.svg
 $(call make-unit-test,test_live_table,test_live_table,fd_disco fd_util)
 endif
+
+src/disco/gui/dist_stable_cmp/%.zst: src/disco/gui/dist_stable/%
+	@if [ ! -f $@ ]; then \
+		mkdir -p $(@D); \
+		zstd -19 $< -o $@; \
+	fi
+
+src/disco/gui/dist_stable_cmp/%.gz: src/disco/gui/dist_stable/%
+	@if [ ! -f $@ ]; then \
+		mkdir -p $(@D); \
+		gzip -c -9 $< > $@; \
+	fi
+
+src/disco/gui/dist_alpha_cmp/%.zst: src/disco/gui/dist_alpha/%
+	@if [ ! -f $@ ]; then \
+		mkdir -p $(@D); \
+		zstd -19 $< -o $@; \
+	fi
+
+src/disco/gui/dist_alpha_cmp/%.gz: src/disco/gui/dist_alpha/%
+	@if [ ! -f $@ ]; then \
+		mkdir -p $(@D); \
+		gzip -c -9 $< > $@; \
+	fi
+
+FD_GUI_FRONTEND_STABLE_FILES := $(shell find src/disco/gui/dist_stable -type f)
+FD_GUI_FRONTEND_ALPHA_FILES := $(shell find src/disco/gui/dist_alpha -type f)
+FD_GUI_FRONTEND_STABLE_GZ_FILES := $(patsubst src/disco/gui/dist_stable/%, src/disco/gui/dist_stable_cmp/%.gz, $(FD_GUI_FRONTEND_STABLE_FILES))
+FD_GUI_FRONTEND_STABLE_ZST_FILES := $(patsubst src/disco/gui/dist_stable/%, src/disco/gui/dist_stable_cmp/%.zst, $(FD_GUI_FRONTEND_STABLE_FILES))
+FD_GUI_FRONTEND_ALPHA_GZ_FILES := $(patsubst src/disco/gui/dist_alpha/%, src/disco/gui/dist_alpha_cmp/%.gz, $(FD_GUI_FRONTEND_ALPHA_FILES))
+FD_GUI_FRONTEND_ALPHA_ZST_FILES := $(patsubst src/disco/gui/dist_alpha/%, src/disco/gui/dist_alpha_cmp/%.zst, $(FD_GUI_FRONTEND_ALPHA_FILES))
+
+$(OBJDIR)/obj/disco/gui/generated/http_import_dist.d: $(FD_GUI_FRONTEND_STABLE_GZ_FILES) $(FD_GUI_FRONTEND_STABLE_ZST_FILES) $(FD_GUI_FRONTEND_ALPHA_GZ_FILES) $(FD_GUI_FRONTEND_ALPHA_ZST_FILES)
