094cded825e7b974233d9e5bbf82ca528fcf7cfc
Kevin J Bowers <kbowers@jumptrading.com>
2025-10-07 17:06:08 -0500

    vinyl base layer
    This is the lowest layer of vinyl.

---
+++ src/vinyl/fd_vinyl.h
@@ -0,0 +1,6 @@
+#ifndef HEADER_fd_src_vinyl_fd_vinyl_h
+#define HEADER_fd_src_vinyl_fd_vinyl_h
+
+#include "fd_vinyl_base.h" /* includes ../tango/fd_tango.h, ../util/tmpl/fd_map.h */
+
+#endif /* HEADER_fd_src_vinyl_fd_vinyl_h */
