9e34ef1706aedcf2ab7c2b73a869c801d88de782
Kevin J Bowers <kbowers@jumptrading.com>
2025-10-07 17:41:20 -0500

    vinyl completion queue and ABI
    This provides the vinyl interprocess shared lockfree SPMC completion
    queue and specifies the client-vinyl batch completion ABI.

--- src/vinyl/fd_vinyl.h
+++ src/vinyl/fd_vinyl.h
@@ -8,5 +8,6 @@
 //#include "data/fd_vinyl_data.h"       /* includes io/fd_vinyl_io.h */
 #include "line/fd_vinyl_line.h"         /* includes meta/fd_vinyl_meta.h data/fd_vinyl_data.h */
 #include "rq/fd_vinyl_rq.h"             /* includes fd_vinyl_base.h */
+#include "cq/fd_vinyl_cq.h"             /* includes fd_vinyl_base.h */
 
 #endif /* HEADER_fd_src_vinyl_fd_vinyl_h */
