1a0e630a5b7ce1707584fd3be8c3c5a2ed63448a
Charles Moyes <cmoyes@jumptrading.com>
2025-10-10 18:30:02 +0000

    Add fallbacks to leader schedule fields

--- src/flamenco/runtime/tests/fd_block_harness.c
+++ src/flamenco/runtime/tests/fd_block_harness.c
@@ -510,6 +510,19 @@ fd_runtime_fuzz_block_ctx_exec( fd_solfuzz_runner_t *      runner,
   return res;
 }
 
+void fd_hash_epoch_leaders(
+  const fd_epoch_leaders_t * leaders,
+  ulong seed,
+  uchar out[16] ) {
+  /* Append pubkey array */
+  ulong h = fd_hash(seed, leaders->pub, leaders->pub_cnt * sizeof(fd_pubkey_t));
+  fd_memcpy(out, &h, sizeof(ulong));
+
+  /* Append schedule array */
+  h = fd_hash(seed, leaders->sched, leaders->sched_cnt * sizeof(uint));
+  fd_memcpy(out + sizeof(ulong), &h, sizeof(ulong));
+}
+
 ulong
 fd_solfuzz_block_run( fd_solfuzz_runner_t * runner,
                       void const *          input_,
@@ -559,9 +572,39 @@ fd_solfuzz_block_run( fd_solfuzz_runner_t * runner,
     effects->has_cost_tracker = 1;
     effects->cost_tracker = (fd_exec_test_cost_tracker_t) {
       .block_cost = cost_tracker ? cost_tracker->block_cost : 0UL,
-      .vote_cost  = cost_tracker ? cost_tracker->vote_cost : 0UL,
+      .vote_cost  = cost_tracker ? cost_tracker->vote_cost  : 0UL,
     };
     fd_bank_cost_tracker_end_locking_query( runner->bank );
+
+    /* Capture leader schedule */
+    fd_epoch_leaders_t const * leaders = fd_bank_epoch_leaders_locking_query( runner->bank );
+    if( FD_LIKELY( leaders ) ) {
+      effects->has_leader_schedule = true;
+
+      /* Get epoch schedule for better fallback values */
+      fd_epoch_schedule_t epoch_schedule;
+      fd_epoch_schedule_t * schedule = fd_sysvar_epoch_schedule_read( runner->funk, &xid, &epoch_schedule );
+
+      ulong fallback_epoch = leaders->epoch ? leaders->epoch : 1UL;
+
+      effects->leader_schedule = (fd_exec_test_leader_schedule_effects_t) {
+        .leaders_epoch = fallback_epoch,
+        .leaders_slot0 =  leaders->slot0 ? leaders->slot0 :
+          (schedule ? fd_epoch_slot0( schedule, fallback_epoch ) : 0UL),
+        .leaders_slot_cnt = leaders->slot_cnt ? leaders->slot_cnt :
+          (schedule ? fd_epoch_slot_cnt( schedule, fallback_epoch ) : FD_RUNTIME_SLOTS_PER_EPOCH),
+        .leader_pub_cnt = leaders->pub_cnt,
+        .leaders_sched_cnt = leaders->sched_cnt*4,
+      };
+
+      fd_hash_epoch_leaders(
+        leaders,
+        0xDEADFACE, /* Seed */
+        effects->leader_schedule.leader_schedule_hash );
+      fd_bank_epoch_leaders_end_locking_query( runner->bank );
+    } else {
+      effects->has_leader_schedule = false;
+    }
 
     ulong actual_end = FD_SCRATCH_ALLOC_FINI( l, 1UL );
     fd_runtime_fuzz_block_ctx_destroy( runner );
