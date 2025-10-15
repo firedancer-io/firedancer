a4e480a563747f01391c4672540e0b366299ca90
David Kleymann <david@asymmetric.re>
2025-10-15 10:41:56 +0200

    firedancer-dev: read gossvf config parameters
    Signed-off-by: David Kleymann <david@asymmetric.re>

--- src/app/firedancer-dev/commands/gossip.c
+++ src/app/firedancer-dev/commands/gossip.c
@@ -86,8 +86,8 @@ fd_gossip_subtopo( config_t * config, ulong tile_to_cpu[ FD_TILE_MAX ] FD_PARAM_
     fd_topo_tile_t * gossvf_tile = fd_topob_tile( topo, "gossvf", "gossvf", "metric_in", 0UL, 0, 1 );
     strncpy( gossvf_tile->gossvf.identity_key_path, config->paths.identity_key, sizeof(gossvf_tile->gossvf.identity_key_path) );
     gossvf_tile->gossvf.tcache_depth = 1UL<<22UL;
-    gossvf_tile->gossvf.shred_version = 0;
-    gossvf_tile->gossvf.allow_private_address = 0;
+    gossvf_tile->gossvf.shred_version = config->consensus.expected_shred_version;
+    gossvf_tile->gossvf.allow_private_address = config->development.gossip.allow_private_address;
     gossvf_tile->gossvf.entrypoints_cnt = config->gossip.entrypoints_cnt;
     gossvf_tile->gossvf.boot_timestamp_nanos = config->boot_timestamp_nanos;
     for( ulong i=0UL; i<config->gossip.entrypoints_cnt; i++ ) {
