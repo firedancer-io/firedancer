7767fd20bc93959c06f157c7a304d1a46d059ea3
Michael McGee <mmcgee@jumptrading.com>
2025-10-22 20:20:45 +0000

    genesis: always load genesis, verify hashes

--- config/extra/with-x86-64.mk
+++ config/extra/with-x86-64.mk
@@ -16,6 +16,7 @@ include config/extra/with-ucontext.mk
 include config/extra/with-secp256k1.mk
 include config/extra/with-s2nbignum.mk
 include config/extra/with-zstd.mk
+include config/extra/with-bzip2.mk
 include config/extra/with-lz4.mk
 include config/extra/with-openssl.mk
 include config/extra/with-rocksdb.mk
