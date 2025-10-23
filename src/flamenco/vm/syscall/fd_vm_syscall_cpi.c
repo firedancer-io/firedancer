2982c2a6dfc78f0d35c6d69c68e5140a6d0bb137
Tom Pointon <topointon@jumptrading.com>
2025-10-23 20:39:26 +0000

    vm, cpi: fix serialized pubkey offset in unaligned case

--- src/flamenco/vm/syscall/fd_vm_syscall_cpi.c
+++ src/flamenco/vm/syscall/fd_vm_syscall_cpi.c
@@ -406,19 +406,19 @@ fd_vm_syscall_cpi_check_authorized_program( fd_pubkey_t const *       program_id
 #define VM_SERIALIZED_OWNER_OFFSET    (40UL)
 #define VM_SERIALIZED_LAMPORTS_OFFSET (72UL)
 
-#define VM_SERIALIZED_UNALIGNED_PUBKEY_OFFSET   (3UL)
-#define VM_SERIALIZED_UNALIGNED_LAMPORTS_OFFSET (35UL)
+#define VM_SERIALIZED_UNALIGNED_PUBKEY_OFFSET   (44UL)
+#define VM_SERIALIZED_UNALIGNED_LAMPORTS_OFFSET (76UL)
 
 static inline
 ulong serialized_pubkey_vaddr( fd_vm_t * vm, ulong region_idx ) {
-fd_vm_input_region_t * region = &vm->input_mem_regions[ region_idx ];
+  fd_vm_input_region_t * region = &vm->input_mem_regions[ region_idx ];
   return FD_VM_MEM_MAP_INPUT_REGION_START + region->vaddr_offset + region->padding +
     (vm->is_deprecated ? VM_SERIALIZED_UNALIGNED_PUBKEY_OFFSET : VM_SERIALIZED_PUBKEY_OFFSET);
 }
 
 static inline
 ulong serialized_owner_vaddr( fd_vm_t * vm, ulong region_idx ) {
-fd_vm_input_region_t * region = &vm->input_mem_regions[ region_idx ];
+  fd_vm_input_region_t * region = &vm->input_mem_regions[ region_idx ];
   if ( vm->is_deprecated ) {
     /* For deprecated loader programs, the owner is serialized into the start of the region
        following the account data region (if present) at a fixed offset.
@@ -430,7 +430,7 @@ fd_vm_input_region_t * region = &vm->input_mem_regions[ region_idx ];
       fd_vm_input_region_t * metadata_region = &vm->input_mem_regions[ region_idx+1U ];
       return FD_VM_MEM_MAP_INPUT_REGION_START + metadata_region->vaddr_offset + metadata_region->padding;
   } else {
-  return FD_VM_MEM_MAP_INPUT_REGION_START + region->vaddr_offset + region->padding;
+      return FD_VM_MEM_MAP_INPUT_REGION_START + region->vaddr_offset + region->padding;
     }
   }
   return FD_VM_MEM_MAP_INPUT_REGION_START + region->vaddr_offset + region->padding + VM_SERIALIZED_OWNER_OFFSET;
