f3551f0b9d8c240c0592c3b605bd90a045241dd6
Charles Li <chali@jumptrading.com>
2025-10-25 19:20:29 +0000

    feat(choreo): dup handling, gossip int, bug fixes, missing logic

--- src/disco/metrics/generated/fd_metrics_all.h
+++ src/disco/metrics/generated/fd_metrics_all.h
@@ -40,6 +40,7 @@
 #include "fd_metrics_backt.h"
 #include "fd_metrics_exec.h"
 #include "fd_metrics_benchs.h"
+#include "fd_metrics_tower.h"
 /* Start of LINK OUT metrics */
 
 #define FD_METRICS_COUNTER_LINK_SLOW_COUNT_OFF  (0UL)
@@ -176,7 +177,7 @@ extern const fd_metrics_meta_t FD_METRICS_ALL_LINK_OUT[FD_METRICS_ALL_LINK_OUT_T
 
 #define FD_METRICS_TOTAL_SZ (8UL*254UL)
 
-#define FD_METRICS_TILE_KIND_CNT 35
+#define FD_METRICS_TILE_KIND_CNT 36
 extern const char * FD_METRICS_TILE_KIND_NAMES[FD_METRICS_TILE_KIND_CNT];
 extern const ulong FD_METRICS_TILE_KIND_SIZES[FD_METRICS_TILE_KIND_CNT];
 extern const fd_metrics_meta_t * FD_METRICS_TILE_KIND_METRICS[FD_METRICS_TILE_KIND_CNT];
