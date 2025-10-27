5fcf61aef74b7f33b6a81f1b9d23c05427d2bbdf
Anway De <ande@jumptrading.com>
2025-10-27 10:44:46 +0000

    builds: fix test_accdb compilation rules for minimal builds

--- src/flamenco/accdb/Local.mk
+++ src/flamenco/accdb/Local.mk
@@ -6,5 +6,7 @@ $(call add-objs,fd_accdb_admin,fd_flamenco)
 $(call add-hdrs,fd_accdb_user.h fd_accdb_sync.h)
 $(call add-objs,fd_accdb_user,fd_flamenco)
 
+ifdef FD_HAS_ATOMIC
 $(call make-unit-test,test_accdb,test_accdb,fd_flamenco fd_funk fd_util)
 $(call run-unit-test,test_accdb)
+endif
