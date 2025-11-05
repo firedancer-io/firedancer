8116aeabf9f637a8e37beba8d28dfeefa6dbddf1
Anway De <ande@jumptrading.com>
2025-11-05 11:09:13 +0000

    fix minimal, noarch64, noarch128 and arm builds

--- src/vinyl/io/Local.mk
+++ src/vinyl/io/Local.mk
@@ -1,6 +1,8 @@
 $(call add-hdrs,fd_vinyl_io.h)
 $(call add-objs,fd_vinyl_io fd_vinyl_io_bd fd_vinyl_io_mm,fd_vinyl)
+ifdef FD_HAS_LZ4
 $(call make-unit-test,test_vinyl_io_bd,test_vinyl_io_bd,fd_vinyl fd_tango fd_util)
 $(call make-unit-test,test_vinyl_io_mm,test_vinyl_io_mm,fd_vinyl fd_tango fd_util)
 $(call run-unit-test,test_vinyl_io_bd)
 $(call run-unit-test,test_vinyl_io_mm)
+endif
