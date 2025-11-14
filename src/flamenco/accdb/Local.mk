c6393517ab96bbed638b5ef779614bbff875b733
Richard Patel <ripatel@jumptrading.com>
2025-11-14 14:04:35 -0500

    vinyl: fix FD_HAS_LZ4 guards

--- src/flamenco/accdb/Local.mk
+++ src/flamenco/accdb/Local.mk
@@ -9,7 +9,9 @@ $(call add-objs,fd_accdb_user,fd_flamenco)
 # Debug APIs
 $(call add-hdrs,fd_accdb_fsck.h)
 $(call add-objs,fd_accdb_fsck_funk fd_accdb_fsck_vinyl,fd_flamenco)
+ifdef FD_HAS_LZ4
 $(call make-bin,fd_accdb_ctl,fd_accdb_ctl,fd_vinyl fd_tango fd_ballet fd_util)
+endif
 
 ifdef FD_HAS_ATOMIC
 $(call make-unit-test,test_accdb,test_accdb,fd_flamenco fd_funk fd_util)
--- src/vinyl/Local.mk
+++ src/vinyl/Local.mk
@@ -1,3 +1,4 @@
+ifdef FD_HAS_LZ4
 $(call make-lib,fd_vinyl)
 $(call add-hdrs,fd_vinyl_base.h fd_vinyl.h)
 $(call add-objs,fd_vinyl_base fd_vinyl_recover fd_vinyl_compact fd_vinyl_cmd fd_vinyl fd_vinyl_exec,fd_vinyl)
@@ -9,3 +10,4 @@ ifdef FD_HAS_HOSTED
 $(call make-unit-test,test_vinyl_req,test_vinyl_req,fd_vinyl fd_tango fd_util)
 endif
 $(call run-unit-test,test_vinyl_base)
+endif
