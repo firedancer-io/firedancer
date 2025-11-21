248005664b6806ad930c090d94fb6933fdeb9b0a
Ishan Bhatt <ibhatt@jumptrading.com>
2025-11-21 18:35:40 +0000

    fixing gcc13 compiler issue

--- src/flamenco/log_collector/test_log_collector.c
+++ src/flamenco/log_collector/test_log_collector.c
@@ -13,9 +13,8 @@
 void
 test_log_messages_bytes_limit_agave( fd_runtime_t * runtime ) {
   fd_exec_instr_ctx_t ctx[1];
-  fd_log_collector_t  log[1];
   ctx->runtime = runtime;
-  runtime->log.log_collector = log;
+  fd_log_collector_t * log = runtime->log.log_collector;
   fd_log_collector_init( log, 1 );
 
   for( ulong i=0; i<20000; i++ ) {
@@ -32,9 +31,8 @@ test_log_messages_bytes_limit_agave( fd_runtime_t * runtime ) {
 static void
 test_log_messages_bytes_limit( fd_runtime_t * runtime ) {
   fd_exec_instr_ctx_t ctx[1];
-  fd_log_collector_t  log[1];
   ctx->runtime = runtime;
-  runtime->log.log_collector = log;
+  fd_log_collector_t * log = runtime->log.log_collector;
 
   fd_log_collector_init( log, 1 );
 
@@ -52,9 +50,8 @@ test_log_messages_bytes_limit( fd_runtime_t * runtime ) {
 static void
 test_log_messages_single_log_limit( fd_runtime_t * runtime ) {
   fd_exec_instr_ctx_t ctx[1];
-  fd_log_collector_t  log[1];
   ctx->runtime = runtime;
-  runtime->log.log_collector = log;
+  fd_log_collector_t * log = runtime->log.log_collector;
 
   char msg10k[ 10000+1 ]; sprintf( msg10k, "%0*d", 10000, 0 );
   char msg9999[ 9999+1 ]; sprintf( msg9999, "%0*d", 9999, 0 );
@@ -99,9 +96,8 @@ test_log_messages_weird_behavior( fd_runtime_t * runtime ) {
 static void
 test_log_messages_equivalences( fd_runtime_t * runtime ) {
   fd_exec_instr_ctx_t ctx[1];
-  fd_log_collector_t  log[1];
   ctx->runtime = runtime;
-  runtime->log.log_collector = log;
+  fd_log_collector_t * log = runtime->log.log_collector;
 
   uchar msg[17] = { 0x67, 0x72, 0xc3, 0xbc, 0x65, 0x7a, 0x69, 0x00, 0x0a, 0xf0, 0x9f, 0x94, 0xa5, 0xf0, 0x9f, 0x92, 0x83 };
 
@@ -156,7 +152,9 @@ main( int     argc,
   fd_runtime_t * runtime = fd_wksp_alloc_laddr( wksp, alignof(fd_runtime_t), sizeof(fd_runtime_t), wksp_tag );
   FD_TEST( runtime );
 
-  // test_log_messages_bytes_limit_agave();
+  fd_log_collector_t log[1];
+  runtime->log.log_collector = log;
+
   test_log_messages_bytes_limit( runtime );
   test_log_messages_single_log_limit( runtime );
   test_log_messages_weird_behavior( runtime );
