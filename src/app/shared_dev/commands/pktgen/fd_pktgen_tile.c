3ad30b03207431462e10bb8f5cc55a73b8271346
Aman Khinvasara <akhinvasara@jumptrading.com>
2025-12-09 15:33:26 +0000

    pktgen: valid Ethtype and IP VERIHL

--- src/app/shared_dev/commands/pktgen/fd_pktgen_tile.c
+++ src/app/shared_dev/commands/pktgen/fd_pktgen_tile.c
@@ -1,7 +1,7 @@
 /* fd_pktgen_tile floods a net tile with small outgoing packets.
 
-   Each packet is a minimum size Ethernet frame. An invalid
-   Ethertype is used to avoid leaking the flood out to the Internet.
+   Each packet is a minimum size Ethernet frame. An invalid ip4 hdr
+   is used to avoid leaking the flood out to the Internet.
 
    Each packet contains a 64 bit sequence number such that each
    payload is different.  Experiments revealed that some NICs stop
@@ -61,17 +61,21 @@ before_credit( fd_pktgen_tile_ctx_t * ctx,
   *charge_busy = 1;
 
   /* Select an arbitrary public IP as the fake destination.  The outgoing
-     packet is not an Internet packet, so it will not reach that
-     destination.  The net tile, however, needs a valid dst IP to select
+     packet has an an invalid ip header, so it will not reach that
+     destination. The net tile, however, needs a valid dst IP to select
      the dst MAC address. */
   ulong sig = fd_disco_netmux_sig( 0U, 0U, ctx->fake_dst_ip, DST_PROTO_OUTGOING, FD_NETMUX_SIG_MIN_HDR_SZ );
 
   /* Send an Ethernet frame */
-  ulong   chunk = ctx->chunk;
-  uchar * frame = fd_chunk_to_laddr( ctx->out_base, chunk );
-  ulong   tag   = ctx->tag;
-  ulong   sz    = sizeof(fd_eth_hdr_t) + 46;
-  FD_STORE( ulong, frame+sizeof(fd_eth_hdr_t), tag );
+  ulong          chunk = ctx->chunk;
+  uchar *        frame = fd_chunk_to_laddr( ctx->out_base, chunk );
+  fd_eth_hdr_t * eth   = (fd_eth_hdr_t *)frame;
+  fd_ip4_hdr_t * ip4   = (fd_ip4_hdr_t *)(eth+1);
+  ulong          tag   = ctx->tag;
+  ulong          sz    = sizeof(fd_eth_hdr_t) + 46;
+  eth->net_type        = fd_ushort_bswap( FD_ETH_HDR_TYPE_IP );
+  ip4->verihl          = FD_IP4_VERIHL( 4, 5 );
+  FD_STORE( ulong, ip4+1, tag ); /* Unique seq number */
   fd_stem_publish( stem, 0UL, sig, chunk, sz, 0UL, 0UL, 0UL );
 
   /* Wind up for next iteration */
