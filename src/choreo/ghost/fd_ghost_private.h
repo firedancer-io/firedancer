b5998320da479962f6ad197d283a0e4bd538043b
Charles Li <chali@jumptrading.com>
2026-01-12 20:57:59 +0000

    feat(ghost): use gaddrs and revive unit tests. add to tower cli tool

--- src/choreo/ghost/fd_ghost_private.h
+++ src/choreo/ghost/fd_ghost_private.h
@@ -1,17 +1,6 @@
 #include "fd_ghost.h"
 
-/* fd_ghost_vtr_t keeps track of what a voter's previously voted for. */
-
-struct fd_ghost_vtr {
-  fd_pubkey_t addr;          /* map key, vote account address */
-  uint        hash;          /* reserved for fd_map_dynamic */
-  ulong       prev_stake;    /* previous vote stake (vote can be from prior epoch) */
-  ulong       prev_slot;     /* previous vote slot */
-  fd_hash_t   prev_block_id; /* previous vote block_id  */
-};
-typedef struct fd_ghost_vtr fd_ghost_vtr_t;
-
-#define POOL_NAME pool
+#define POOL_NAME blk_pool
 #define POOL_T    fd_ghost_blk_t
 #include "../../util/tmpl/fd_pool.c"
 
@@ -24,16 +13,18 @@ typedef struct fd_ghost_vtr fd_ghost_vtr_t;
 #define MAP_NEXT               next
 #include "../../util/tmpl/fd_map_chain.c"
 
-#define MAP_NAME              vtr_map
-#define MAP_T                 fd_ghost_vtr_t
-#define MAP_KEY               addr
-#define MAP_KEY_T             fd_pubkey_t
-#define MAP_KEY_NULL          pubkey_null
-#define MAP_KEY_EQUAL_IS_SLOW 1
-#define MAP_KEY_INVAL(k)      MAP_KEY_EQUAL((k),MAP_KEY_NULL)
-#define MAP_KEY_EQUAL(k0,k1)  (!memcmp( (k0).key, (k1).key, 32UL ))
-#define MAP_KEY_HASH(key,s)   ((MAP_HASH_T)( (key).ul[1] ))
-#include "../../util/tmpl/fd_map_dynamic.c"
+#define POOL_NAME vtr_pool
+#define POOL_T    fd_ghost_vtr_t
+#include "../../util/tmpl/fd_pool.c"
+
+#define MAP_NAME               vtr_map
+#define MAP_ELE_T              fd_ghost_vtr_t
+#define MAP_KEY                addr
+#define MAP_KEY_T              fd_pubkey_t
+#define MAP_KEY_EQ(k0,k1)      (!memcmp((k0),(k1), sizeof(fd_hash_t)))
+#define MAP_KEY_HASH(key,seed) (fd_hash((seed),(key),sizeof(fd_hash_t)))
+#define MAP_NEXT               next
+#include "../../util/tmpl/fd_map_chain.c"
 
 /* fd_ghost_t is the top-level structure that holds the root of the
    tree, as well as the memory pools and map structures for tracking
@@ -48,17 +39,68 @@ typedef struct fd_ghost_vtr fd_ghost_vtr_t;
    ----------------------
    | pool               |
    ----------------------
-   | map                |
+   | blk_map            |
    ----------------------
-   | bid                |
-   ----------------------
-   | vtr                |
+   | vtr_map            |
    ---------------------- */
 
 struct __attribute__((aligned(128UL))) fd_ghost {
-  ulong            root;    /* pool idx of the root tree element */
-  fd_ghost_blk_t * pool;    /* pool of tree elements (blocks) */
-  blk_map_t *      blk_map; /* map of block_id->ghost_blk for fast O(1) querying */
-  fd_ghost_vtr_t * vtr_map; /* map of pubkey->prior vote */
+  ulong root;           /* pool idx of the root tree element */
+  ulong ghost_gaddr;    /* memory offset of the beginning of ghost region in the wksp */
+  ulong blk_pool_gaddr; /* memory offset of the blk_pool */
+  ulong blk_map_gaddr;  /* memory offset of the blk_map */
+  ulong vtr_pool_gaddr; /* memory offset of the vtr_pool */
+  ulong vtr_map_gaddr;  /* memory offset of the vtr_map */
 };
 typedef struct fd_ghost fd_ghost_t;
+
+typedef fd_ghost_blk_t blk_pool_t;
+typedef fd_ghost_vtr_t vtr_pool_t;
+
+static inline blk_pool_t *
+blk_pool( fd_ghost_t * ghost ) {
+  fd_wksp_t * wksp = fd_wksp_containing( ghost );
+  return (blk_pool_t *)fd_wksp_laddr_fast( wksp, ghost->blk_pool_gaddr );
+}
+
+static inline blk_pool_t const *
+blk_pool_const( fd_ghost_t const * ghost ) {
+  fd_wksp_t * wksp = fd_wksp_containing( ghost );
+  return (blk_pool_t const *)fd_wksp_laddr_fast( wksp, ghost->blk_pool_gaddr );
+}
+
+static inline blk_map_t *
+blk_map( fd_ghost_t * ghost ) {
+  fd_wksp_t * wksp = fd_wksp_containing( ghost );
+  return (blk_map_t *)fd_wksp_laddr_fast( wksp, ghost->blk_map_gaddr );
+}
+
+static inline blk_map_t const *
+blk_map_const( fd_ghost_t const * ghost ) {
+  fd_wksp_t * wksp = fd_wksp_containing( ghost );
+  return (blk_map_t const *)fd_wksp_laddr_fast( wksp, ghost->blk_map_gaddr );
+}
+
+static inline vtr_pool_t *
+vtr_pool( fd_ghost_t * ghost ) {
+  fd_wksp_t * wksp = fd_wksp_containing( ghost );
+  return (vtr_pool_t *)fd_wksp_laddr_fast( wksp, ghost->vtr_pool_gaddr );
+}
+
+static inline vtr_pool_t const *
+vtr_pool_const( fd_ghost_t const * ghost ) {
+  fd_wksp_t * wksp = fd_wksp_containing( ghost );
+  return (vtr_pool_t const *)fd_wksp_laddr_fast( wksp, ghost->vtr_pool_gaddr );
+}
+
+static inline vtr_map_t *
+vtr_map( fd_ghost_t * ghost ) {
+  fd_wksp_t * wksp = fd_wksp_containing( ghost );
+  return (vtr_map_t *)fd_wksp_laddr_fast( wksp, ghost->vtr_map_gaddr );
+}
+
+static inline vtr_map_t const *
+vtr_map_const( fd_ghost_t const * ghost ) {
+  fd_wksp_t * wksp = fd_wksp_containing( ghost );
+  return (vtr_map_t const *)fd_wksp_laddr_fast( wksp, ghost->vtr_map_gaddr );
+}
