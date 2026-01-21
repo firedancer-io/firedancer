abd92ca7418c93bbe225a49665c23f708c6a0de1
Jeremy Miller <jemiller@jumptrading.com>
2026-01-21 23:15:14 +0000

    rpc: wire up tower_out for health and commitment tracking

--- src/app/firedancer/topology.c
+++ src/app/firedancer/topology.c
@@ -1057,6 +1057,7 @@ fd_topo_initialize( config_t * config ) {
     fd_topob_tile_out( topo, "rpc", 0UL, "rpc_replay", 0UL );
     fd_topob_tile_in( topo, "rpc",  0UL, "metric_in", "replay_out",  0UL, FD_TOPOB_RELIABLE, FD_TOPOB_POLLED );
     fd_topob_tile_in( topo, "rpc",  0UL, "metric_in", "genesi_out", 0UL, FD_TOPOB_RELIABLE, FD_TOPOB_POLLED );
+    fd_topob_tile_in( topo, "rpc",  0UL, "metric_in", "tower_out",  0UL, FD_TOPOB_RELIABLE, FD_TOPOB_POLLED );
     fd_topob_tile_in( topo, "replay", 0UL, "metric_in", "rpc_replay", 0UL, FD_TOPOB_RELIABLE, FD_TOPOB_POLLED );
   }
 
