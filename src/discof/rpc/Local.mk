59b45fb92ecc77a46f211963bc99f8d6cd235655
Richard Patel <ripatel@jumptrading.com>
2026-01-21 20:15:03 +0000

    rpc: test scaffold

--- src/discof/rpc/Local.mk
+++ src/discof/rpc/Local.mk
@@ -1,3 +1,4 @@
 ifdef FD_HAS_ALLOCA
 $(call add-objs,fd_rpc_tile,fd_discof)
+$(call make-unit-test,test_rpc_tile,test_rpc_tile,fd_discof firedancer_version fd_disco fd_flamenco fd_waltz fd_vinyl fd_funk fd_tango fd_ballet fd_util)
 endif
