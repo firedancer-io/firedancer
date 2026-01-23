bd4b64a0a61d4f9a154ba66241dc3e1bfc27ab70
Kunal Bhargava <kbhargava@jumptrading.com>
2026-01-23 20:56:48 +0000

    increase backtest exec tiles

--- src/flamenco/runtime/tests/run_backtest_nightly.sh
+++ src/flamenco/runtime/tests/run_backtest_nightly.sh
@@ -2,6 +2,6 @@
 set -e
 
 src/flamenco/runtime/tests/run_ledger_backtest.sh -l mainnet-391824000 -y 600 -m 1100000000 -e 391824001 -v true
-src/flamenco/runtime/tests/run_ledger_backtest.sh -l devnet-433989075 -y 400 -m 200000000 -e 434592005
+src/flamenco/runtime/tests/run_ledger_backtest.sh -l devnet-433989075 -y 400 -m 200000000 -e 434592005 --exec 64
 src/flamenco/runtime/tests/run_ledger_backtest.sh -l mainnet-393520696 -y 600 -m 1100000000 -e 393984037
-src/flamenco/runtime/tests/run_ledger_backtest.sh -l testnet-380636816 -y 50 -m 200000000 -e 381116303
+src/flamenco/runtime/tests/run_ledger_backtest.sh -l testnet-380636816 -y 50 -m 200000000 -e 381116303 --exec 64
