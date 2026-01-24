28f26cb4061c73e18a218904b48a4206d55f2e1a
Richard Patel <ripatel@jumptrading.com>
2026-01-24 01:52:09 +0000

    deps.sh: remove liburing
    Remove unused dependency

--- config/extra/with-x86-64.mk
+++ config/extra/with-x86-64.mk
@@ -17,7 +17,6 @@ include config/extra/with-secp256k1.mk
 include config/extra/with-s2nbignum.mk
 include config/extra/with-blst.mk
 include config/extra/with-zstd.mk
-include config/extra/with-liburing.mk
 include config/extra/with-bzip2.mk
 include config/extra/with-lz4.mk
 include config/extra/with-openssl.mk
