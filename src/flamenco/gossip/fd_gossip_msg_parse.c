1882d023e79f2638e1eab235edef28db3e1daf18
Emily Wang <emwang@jumptrading.com>
2026-01-29 23:46:23 +0000

    remove gossip log

--- src/flamenco/gossip/fd_gossip_msg_parse.c
+++ src/flamenco/gossip/fd_gossip_msg_parse.c
@@ -592,7 +592,7 @@ fd_gossip_msg_crds_data_parse( fd_gossip_view_crds_value_t * crds_val,
     case FD_GOSSIP_VALUE_NODE_INSTANCE:
       return fd_gossip_msg_crds_node_instance_parse( crds_val, payload, payload_sz, start_offset );
     case FD_GOSSIP_VALUE_DUPLICATE_SHRED: {
-      FD_LOG_NOTICE(("duplicate shred gossip msg detected"));
+      //FD_LOG_NOTICE(("duplicate shred gossip msg detected"));
       return fd_gossip_msg_crds_duplicate_shred_parse( crds_val, payload, payload_sz, start_offset );
     }
     case FD_GOSSIP_VALUE_INC_SNAPSHOT_HASHES:
