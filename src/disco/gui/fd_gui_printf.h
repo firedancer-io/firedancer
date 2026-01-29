662dca3ec752120c67ac5a6890b2a1f60d77a6d1
jherrera-jump <jherrera@jumptrading.com>
2026-01-29 23:18:38 +0000

    gui: refactor late votes history

--- src/disco/gui/fd_gui_printf.h
+++ src/disco/gui/fd_gui_printf.h
@@ -22,6 +22,7 @@ void fd_gui_printf_slot_caught_up( fd_gui_t * gui );
 void fd_gui_printf_skipped_history( fd_gui_t * gui );
 void fd_gui_printf_skipped_history_cluster( fd_gui_t * gui );
 void fd_gui_printf_vote_latency_history( fd_gui_t * gui );
+void fd_gui_printf_late_votes_history( fd_gui_t * gui );
 void fd_gui_printf_tps_history( fd_gui_t * gui );
 void fd_gui_printf_startup_progress( fd_gui_t * gui );
 void fd_gui_printf_boot_progress( fd_gui_t * gui );
