8ca0220a51611f624a8c3b3afcedbd927885dcd7
mubariz <mubarizharoonmulpk@gmail.com>
2026-02-10 12:51:29 +0500

    fix

--- src/flamenco/runtime/fd_cost_tracker.c
+++ src/flamenco/runtime/fd_cost_tracker.c
@@ -188,7 +188,7 @@ get_signature_cost( fd_txn_in_t const * txn_in, fd_bank_t * bank ) {
   ulong secp256k1_verify_cost = fd_ulong_sat_mul( FD_PACK_COST_PER_SECP256K1_SIGNATURE, num_secp256k1_instruction_signatures );
 
   /* https://github.com/anza-xyz/agave/blob/v2.2.0/cost-model/src/cost_model.rs#L155-L160 */
-  ulong ed25519_verify_cost = d_ulong_sat_mul( FD_PACK_COST_PER_ED25519_SIGNATURE, num_ed25519_instruction_signatures );
+  ulong ed25519_verify_cost = fd_ulong_sat_mul( FD_PACK_COST_PER_ED25519_SIGNATURE, num_ed25519_instruction_signatures );
   
   /* https://github.com/anza-xyz/agave/blob/v2.2.0/cost-model/src/cost_model.rs#L162-L167 */
   ulong secp256r1_verify_cost = 0UL;
