47c3fd666669fbc952e022ba993d5f68fef4172c
Kunal Bhargava <154450524+kbhargava-jump@users.noreply.github.com>
2026-02-16 12:58:14 -0600

    snapshot: initialize node_account for historical vote stakes from snapshots (#8315)

--- src/flamenco/runtime/tests/run_backtest_nightly.sh
+++ src/flamenco/runtime/tests/run_backtest_nightly.sh
@@ -6,3 +6,4 @@ src/flamenco/runtime/tests/run_ledger_backtest.sh -l devnet-433989075 -y 400 -m 
 src/flamenco/runtime/tests/run_ledger_backtest.sh -l mainnet-393520696 -y 600 -m 1100000000 -e 393984037
 src/flamenco/runtime/tests/run_ledger_backtest.sh -l testnet-380636816 -y 50 -m 200000000 -e 381116303 --exec 64
 src/flamenco/runtime/tests/run_ledger_backtest.sh -l testnet-387596256 -y 30 -m 90000000 -e 387596373 -lt -v true --exec 64
+src/flamenco/runtime/tests/run_ledger_backtest.sh -l mainnet-400468865 -y 600 -m 1200000000 -e 400468870 -v true
