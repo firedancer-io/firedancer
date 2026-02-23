ec5f46302d96664958abd11af2b7831b38d7625b
Ishan Bhatt <ibhatt@jumptrading.com>
2026-02-23 18:50:13 +0000

    wip

--- src/flamenco/runtime/tests/fd_block_harness.c
+++ src/flamenco/runtime/tests/fd_block_harness.c
@@ -87,6 +87,8 @@ fd_solfuzz_block_register_vote_account( fd_accdb_user_t *         accdb,
   vote_state->stake     = 0UL;
   vote_state->stake_t_1 = 0UL;
   vote_state->stake_t_2 = 0UL;
+  vote_state->node_account_t_1 = vote_state->node_account;
+  vote_state->node_account_t_2 = vote_state->node_account;
 
   fd_accdb_close_ro( accdb, ro );
 }
