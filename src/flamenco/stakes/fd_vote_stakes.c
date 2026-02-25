605804633dbb0ff8d9035716aa4f4d8c3eabdedf
Ishan Bhatt <ibhatt@jumptrading.com>
2026-02-25 16:30:20 +0000

    wip working

--- src/flamenco/stakes/fd_vote_stakes.c
+++ src/flamenco/stakes/fd_vote_stakes.c
@@ -1,107 +1,5 @@
 #include "fd_vote_stakes.h"
-
-struct index_key {
-  fd_pubkey_t pubkey;
-  ulong       stake_t_1;
-};
-typedef struct index_key index_key_t;
-
-struct index_ele {
-  index_key_t key;
-  ulong       stake_t_2;
-  uint        next;
-  uint        refcnt;
-};
-typedef struct index_ele index_ele_t;
-
-#define POOL_NAME  index_pool
-#define POOL_T     index_ele_t
-#define POOL_NEXT  next
-#define POOL_IDX_T uint
-#include "../../util/tmpl/fd_pool.c"
-
-#define MAP_NAME               index_map
-#define MAP_KEY_T              index_key_t
-#define MAP_ELE_T              index_ele_t
-#define MAP_KEY                key
-#define MAP_KEY_EQ(k0,k1)      (fd_pubkey_eq( k0, k1 ))
-#define MAP_KEY_HASH(key,seed) (fd_hash( seed, key, sizeof(index_key_t) ))
-#define MAP_NEXT               next
-#define MAP_IDX_T              uint
-#include "../../util/tmpl/fd_map_chain.c"
-
-/* Each pool index is just an array of uint indices into the pool. */
-struct stake {
-  uint idx;
-  uint next;
-};
-typedef struct stake stake_t;
-
-#define POOL_NAME  stakes_pool
-#define POOL_T     stake_t
-#define POOL_NEXT  next
-#define POOL_IDX_T uint
-#include "../../util/tmpl/fd_pool.c"
-
-#define MAP_NAME               stakes_map
-#define MAP_KEY_T              uint
-#define MAP_ELE_T              stake_t
-#define MAP_KEY                idx
-#define MAP_NEXT               next
-#define MAP_IDX_T              uint
-#include "../../util/tmpl/fd_map_chain.c"
-
-struct fork {
-  ushort next;
-};
-typedef struct fork fork_t;
-
-#define POOL_NAME  fork_pool
-#define POOL_T     fork_t
-#define POOL_NEXT  next
-#define POOL_IDX_T ushort
-#include "../../util/tmpl/fd_pool.c"
-
-
-struct fd_vote_stakes {
-  ulong magic;
-  ulong index_pool_offset;
-  ulong index_map_offset;
-
-  ulong  stakes_pool_offset[ 128UL ]; /* TODO:FIXME: this has to be configurable */
-  ulong  stakes_map_offset[ 128UL ]; /* TODO:FIXME: this has to be configurable */
-  ulong  outstanding;
-
-  ushort root_idx;
-};
-typedef struct fd_vote_stakes fd_vote_stakes_t;
-
-static index_ele_t *
-get_index_pool( fd_vote_stakes_t * vote_stakes ) {
-  return fd_type_pun( (uchar *)vote_stakes + vote_stakes->index_pool_offset );
-}
-
-static index_map_t *
-get_index_map( fd_vote_stakes_t * vote_stakes ) {
-  return fd_type_pun( (uchar *)vote_stakes + vote_stakes->index_map_offset );
-}
-
-static stake_t *
-get_stakes_pool( fd_vote_stakes_t * vote_stakes,
-                 ushort             fork_idx ) {
-  return fd_type_pun( (uchar *)vote_stakes + vote_stakes->stakes_pool_offset[ fork_idx ] );
-}
-
-static stakes_map_t *
-get_stakes_map( fd_vote_stakes_t * vote_stakes,
-                ushort             fork_idx ) {
-  return fd_type_pun( (uchar *)vote_stakes + vote_stakes->stakes_map_offset[ fork_idx ] );
-}
-
-static fork_t *
-get_fork_pool( fd_vote_stakes_t * vote_stakes ) {
-  return fd_type_pun( (uchar *)vote_stakes + vote_stakes->fork_pool_offset );
-}
+#include "fd_vote_stakes_private.h"
 
 ulong
 fd_vote_stakes_align( void ) {
@@ -113,9 +11,12 @@ fd_vote_stakes_footprint( ulong max_vote_accounts,
                           ulong max_fork_width,
                           ulong map_chain_cnt ) {
   ulong l = FD_LAYOUT_INIT;
-  l = FD_LAYOUT_APPEND( l, fd_vote_stakes_align(), sizeof(fd_vote_stakes_t) );
-  l = FD_LAYOUT_APPEND( l, index_pool_align(),     index_pool_footprint( max_vote_accounts ) );
-  l = FD_LAYOUT_APPEND( l, index_map_align(),      index_map_footprint( map_chain_cnt ) );
+  l = FD_LAYOUT_APPEND( l, fd_vote_stakes_align(),  sizeof(fd_vote_stakes_t) );
+  l = FD_LAYOUT_APPEND( l, index_pool_align(),      index_pool_footprint( max_vote_accounts ) );
+  l = FD_LAYOUT_APPEND( l, index_map_align(),       index_map_footprint( map_chain_cnt ) );
+  l = FD_LAYOUT_APPEND( l, index_map_multi_align(), index_map_multi_footprint( map_chain_cnt ) );
+  l = FD_LAYOUT_APPEND( l, fork_pool_align(),       fork_pool_footprint( max_fork_width ) );
+  l = FD_LAYOUT_APPEND( l, fork_dlist_align(),      fork_dlist_footprint() );
   for( ulong i=0; i<max_fork_width; i++ ) {
     l = FD_LAYOUT_APPEND( l, stakes_pool_align(), stakes_pool_footprint( max_vote_accounts ) );
     l = FD_LAYOUT_APPEND( l, stakes_map_align(),  stakes_map_footprint( map_chain_cnt ) );
@@ -140,18 +41,20 @@ fd_vote_stakes_new( void * shmem,
   }
 
   FD_SCRATCH_ALLOC_INIT( l, shmem );
-  fd_vote_stakes_t * vote_stakes    = FD_SCRATCH_ALLOC_APPEND( l, fd_vote_stakes_align(), sizeof(fd_vote_stakes_t) );
-  void *             index_pool_mem = FD_SCRATCH_ALLOC_APPEND( l, index_pool_align(),     index_pool_footprint( max_vote_accounts ) );
-  void *             index_map_mem  = FD_SCRATCH_ALLOC_APPEND( l, index_map_align(),      index_map_footprint( map_chain_cnt ) );
-  void *             fork_pool_mem  = FD_SCRATCH_ALLOC_APPEND( l, fork_pool_align(),      fork_pool_footprint( max_fork_width ) );
+  fd_vote_stakes_t * vote_stakes         = FD_SCRATCH_ALLOC_APPEND( l, fd_vote_stakes_align(), sizeof(fd_vote_stakes_t) );
+  void *             index_pool_mem      = FD_SCRATCH_ALLOC_APPEND( l, index_pool_align(),     index_pool_footprint( max_vote_accounts ) );
+  void *             index_map_mem       = FD_SCRATCH_ALLOC_APPEND( l, index_map_align(),      index_map_footprint( map_chain_cnt ) );
+  void *             index_map_multi_mem = FD_SCRATCH_ALLOC_APPEND( l, index_map_multi_align(), index_map_multi_footprint( map_chain_cnt ) );
+  void *             fork_pool_mem       = FD_SCRATCH_ALLOC_APPEND( l, fork_pool_align(),      fork_pool_footprint( max_fork_width ) );
+  void *             fork_dlist_mem      = FD_SCRATCH_ALLOC_APPEND( l, fork_dlist_align(),     fork_dlist_footprint() );
   for( ulong i=0; i<max_fork_width; i++ ) {
-    void * stakes_pool_mem = FD_SCRATCH_ALLOC_APPEND( l, stakes_pool_align(), stakes_pool_footprint( max_vote_accounts ) );
-    stake_t * stakes_pool = stakes_pool_join( stakes_pool_new( stakes_pool_mem, max_vote_accounts ) );
+    void *    stakes_pool_mem = FD_SCRATCH_ALLOC_APPEND( l, stakes_pool_align(), stakes_pool_footprint( max_vote_accounts ) );
+    stake_t * stakes_pool     = stakes_pool_join( stakes_pool_new( stakes_pool_mem, max_vote_accounts ) );
     if( FD_UNLIKELY( !stakes_pool ) ) {
       FD_LOG_WARNING(( "Failed to create vote stakes ele pool" ));
       return NULL;
     }
-    vote_stakes->stakes_pool_offset[ i ] = (ulong)stakes_pool_mem - (ulong)shmem;
+    vote_stakes->stakes_pool_off[ i ] = (ulong)stakes_pool - (ulong)shmem;
 
     void * stakes_map_mem = FD_SCRATCH_ALLOC_APPEND( l, stakes_map_align(), stakes_map_footprint( map_chain_cnt ) );
     stakes_map_t * stakes_map = stakes_map_join( stakes_map_new( stakes_map_mem, max_vote_accounts, seed ) );
@@ -159,7 +62,7 @@ fd_vote_stakes_new( void * shmem,
       FD_LOG_WARNING(( "Failed to create vote stakes ele map" ));
       return NULL;
     }
-    vote_stakes->stakes_map_offset[ i ] = (ulong)stakes_map_mem - (ulong)shmem;
+    vote_stakes->stakes_map_off[ i ] = (ulong)stakes_map - (ulong)shmem;
   }
 
   index_ele_t * index_pool = index_pool_join( index_pool_new( index_pool_mem, max_vote_accounts ) );
@@ -174,21 +77,31 @@ fd_vote_stakes_new( void * shmem,
     return NULL;
   }
 
+  index_map_multi_t * index_map_multi = index_map_multi_join( index_map_multi_new( index_map_multi_mem, map_chain_cnt, seed ) );
+  if( FD_UNLIKELY( !index_map_multi ) ) {
+    FD_LOG_WARNING(( "Failed to create vote stakes index map multi" ));
+    return NULL;
+  }
+
   fork_t * fork_pool = fork_pool_join( fork_pool_new( fork_pool_mem, max_fork_width ) );
   if( FD_UNLIKELY( !fork_pool ) ) {
     FD_LOG_WARNING(( "Failed to create vote stakes fork pool" ));
     return NULL;
   }
 
-  vote_stakes->index_pool_offset = (ulong)index_pool_mem - (ulong)shmem;
-  vote_stakes->index_map_offset  = (ulong)index_map_mem - (ulong)shmem;
-  vote_stakes->fork_pool_offset  = (ulong)fork_pool_mem - (ulong)shmem;
+  fork_dlist_t * fork_dlist = fork_dlist_join( fork_dlist_new( fork_dlist_mem ) );
+  if( FD_UNLIKELY( !fork_dlist ) ) {
+    FD_LOG_WARNING(( "Failed to create vote stakes fork dlist" ));
+    return NULL;
+  }
 
-  vote_stakes->root_idx = (ushort)fork_pool_idx_acquire( fork_pool );
-  fork_t * fork         = fork_pool_ele( fork_pool, vote_stakes->root_idx );
-  fork->parent_idx      = USHORT_MAX;
-  fork->child_idx       = USHORT_MAX;
-  fork->sibling_idx     = USHORT_MAX;
+  vote_stakes->index_pool_off      = (ulong)index_pool - (ulong)shmem;
+  vote_stakes->index_map_off       = (ulong)index_map - (ulong)shmem;
+  vote_stakes->index_map_multi_off = (ulong)index_map_multi - (ulong)shmem;
+  vote_stakes->fork_pool_off       = (ulong)fork_pool - (ulong)shmem;
+  vote_stakes->fork_dlist_off      = (ulong)fork_dlist - (ulong)shmem;
+  vote_stakes->root_idx            = (ushort)fork_pool_idx_acquire( fork_pool );
+  fork_dlist_idx_push_tail( fork_dlist, vote_stakes->root_idx, fork_pool );
 
   return shmem;
 }
@@ -205,18 +118,21 @@ fd_vote_stakes_insert_root( fd_vote_stakes_t * vote_stakes,
                             ulong              stake_t_1,
                             ulong              stake_t_2 ) {
 
-  index_ele_t * index_pool = get_index_pool( vote_stakes );
-  index_map_t * index_map  = get_index_map( vote_stakes );
+  index_ele_t *       index_pool      = get_index_pool( vote_stakes );
+  index_map_t *       index_map       = get_index_map( vote_stakes );
+  index_map_multi_t * index_map_multi = get_index_map_multi( vote_stakes );
 
   index_ele_t * ele = index_pool_ele_acquire( index_pool );
-  ele->key       = (index_key_t){ .pubkey = *pubkey, .stake_t_1 = stake_t_1 };
-  ele->stake_t_2 = stake_t_2;
-  ele->refcnt    = 1;
+  ele->index_key    = (index_key_t){ .pubkey = *pubkey, .stake_t_1 = stake_t_1 };
+  ele->stake_t_2    = stake_t_2;
+  ele->refcnt       = 1;
+  FD_TEST( index_map_multi_ele_insert( index_map_multi, ele, index_pool ) );
   FD_TEST( index_map_ele_insert( index_map, ele, index_pool ) );
   uint pubkey_idx = (uint)index_pool_idx( index_pool, ele );
 
   stake_t *      stakes_pool = get_stakes_pool( vote_stakes, vote_stakes->root_idx );
   stakes_map_t * stakes_map  = get_stakes_map( vote_stakes, vote_stakes->root_idx );
+
   stake_t *      new_stake = stakes_pool_ele_acquire( stakes_pool );
   new_stake->idx = pubkey_idx;
   FD_TEST( stakes_map_ele_insert( stakes_map, new_stake, stakes_pool ) );
@@ -224,7 +140,8 @@ fd_vote_stakes_insert_root( fd_vote_stakes_t * vote_stakes,
 
 ushort
 fd_vote_stakes_new_child( fd_vote_stakes_t * vote_stakes ) {
-  fork_t * fork_pool = get_fork_pool( vote_stakes );
+  fork_t *       fork_pool  = get_fork_pool( vote_stakes );
+  fork_dlist_t * fork_dlist = get_fork_dlist( vote_stakes );
 
   if( FD_UNLIKELY( !fork_pool_free( fork_pool ) ) ) {
     FD_LOG_CRIT(( "no free forks in pool" ));
@@ -232,20 +149,109 @@ fd_vote_stakes_new_child( fd_vote_stakes_t * vote_stakes ) {
 
   ushort idx = (ushort)fork_pool_idx_acquire( fork_pool );
 
-  stake_t *      stake_pool = get_stakes_pool( vote_stakes, idx );
-  stakes_map_t * stakes_map = get_stakes_map( vote_stakes, idx );
-
-  stakes_map_reset( stakes_map );
-  stakes_pool_reset( stake_pool );
+  fork_dlist_idx_push_tail( fork_dlist, idx, fork_pool );
 
   return idx;
 }
 
 void
 fd_vote_stakes_advance_root( fd_vote_stakes_t * vote_stakes,
-                             ushort             new_root_idx ) {
-  (void)vote_stakes;
-  (void)new_root_idx;
+                             ushort             root_idx ) {
+  /* Only expect the vote stakes to update once an epoch. */
+  if( FD_LIKELY( root_idx==vote_stakes->root_idx ) ) return;
 
-  /* TODO: Need a way to cleanly iterate though all used nodes but the */
+  fork_t *       fork_pool  = get_fork_pool( vote_stakes );
+  fork_dlist_t * fork_dlist = get_fork_dlist( vote_stakes );
+
+  index_ele_t *       index_pool      = get_index_pool( vote_stakes );
+  index_map_t *       index_map       = get_index_map( vote_stakes );
+  index_map_multi_t * index_map_multi = get_index_map_multi( vote_stakes );
+  /* For every oustanding fork that is not the new candidate root,
+     remove all stakes refcnts from the index.  If the index has no
+     outstanding references, remove the index entry. */
+  while( !fork_dlist_is_empty( fork_dlist, fork_pool ) ) {
+    ushort fork_idx = (ushort)fork_dlist_idx_pop_head( fork_dlist, fork_pool );
+    if( fork_idx==root_idx ) continue;
+
+    stake_t *      stakes_pool = get_stakes_pool( vote_stakes, fork_idx );
+    stakes_map_t * stakes_map  = get_stakes_map( vote_stakes, fork_idx );
+    for( stakes_map_iter_t iter = stakes_map_iter_init( stakes_map, stakes_pool );
+         !stakes_map_iter_done( iter, stakes_map, stakes_pool );
+         iter = stakes_map_iter_next( iter, stakes_map, stakes_pool ) ) {
+      stake_t *     stake = stakes_map_iter_ele( iter, stakes_map, stakes_pool );
+      index_ele_t * ele   = index_pool_ele( index_pool, stake->idx );
+      ele->refcnt--;
+
+      if( FD_UNLIKELY( ele->refcnt==0U ) ) {
+        FD_TEST( index_map_ele_remove( index_map, &ele->index_key, NULL, index_pool ) );
+        FD_TEST( index_map_multi_ele_remove( index_map_multi, &ele->pubkey, NULL, index_pool ) );
+        index_pool_ele_release( index_pool, ele );
+      }
+    }
+  }
+  /* TODO: There's probably a way to do a more efficient reset here. */
+  stakes_map_reset( get_stakes_map( vote_stakes, root_idx ) );
+  stakes_pool_reset( get_stakes_pool( vote_stakes, root_idx ) );
+
+  fork_dlist_idx_push_head( fork_dlist, root_idx, fork_pool );
+}
+
+void
+fd_vote_stakes_query_stake( fd_vote_stakes_t * vote_stakes,
+                            ushort             fork_idx,
+                            fd_pubkey_t *      pubkey,
+                            ulong *            stake_t_1_out,
+                            ulong *            stake_t_2_out ) {
+
+  index_ele_t *       index_pool      = get_index_pool( vote_stakes );
+  index_map_multi_t * index_map_multi = get_index_map_multi( vote_stakes );
+
+  stake_t *      stakes_pool = get_stakes_pool( vote_stakes, fork_idx );
+  stakes_map_t * stakes_map  = get_stakes_map( vote_stakes, fork_idx );
+
+  /* The index may have multiple entries for the same pubkey, so every
+     single matching index entry must be checked to see if the index
+     exists in the given fork's stakes map.  If it does, return the
+     t_2 stake value.*/
+  uint ele_idx = (uint)index_map_multi_idx_query_const( index_map_multi, pubkey, UINT_MAX, index_pool );
+  FD_TEST( ele_idx!=UINT_MAX );
+
+  while( !stakes_map_ele_query( stakes_map, &ele_idx, NULL, stakes_pool ) ) {
+    ele_idx = (uint)index_map_multi_idx_next_const( ele_idx, UINT_MAX, index_pool );
+  }
+
+  index_ele_t * index_ele = index_pool_ele( index_pool, ele_idx );
+  *stake_t_1_out = index_ele->stake_t_1;
+  *stake_t_2_out = index_ele->stake_t_2;
+}
+
+void
+fd_vote_stakes_insert( fd_vote_stakes_t * vote_stakes,
+                       ushort             fork_idx,
+                       fd_pubkey_t *      pubkey,
+                       ulong              stake_t_1,
+                       ulong              stake_t_2 ) {
+  index_ele_t *       index_pool      = get_index_pool( vote_stakes );
+  index_map_t *       index_map       = get_index_map( vote_stakes );
+  index_map_multi_t * index_map_multi = get_index_map_multi( vote_stakes );
+
+  stake_t *      stakes_pool = get_stakes_pool( vote_stakes, fork_idx );
+  stakes_map_t * stakes_map  = get_stakes_map( vote_stakes, fork_idx );
+
+  index_key_t index_key = (index_key_t){ .pubkey = *pubkey, .stake_t_1 = stake_t_1 };
+  index_ele_t * index_ele = index_map_ele_query( index_map, &index_key, NULL, index_pool );
+  if( FD_LIKELY( index_ele ) ) {
+    index_ele->refcnt++;
+  } else {
+    index_ele            = index_pool_ele_acquire( index_pool );
+    index_ele->index_key = index_key;
+    index_ele->refcnt    = 1;
+    index_ele->stake_t_2 = stake_t_2;
+    FD_TEST( index_map_ele_insert( index_map, index_ele, index_pool ) );
+    FD_TEST( index_map_multi_ele_insert( index_map_multi, index_ele, index_pool ) );
+  }
+
+  stake_t * stake = stakes_pool_ele_acquire( stakes_pool );
+  stake->idx = (uint)index_pool_idx( index_pool, index_ele );
+  FD_TEST( stakes_map_ele_insert( stakes_map, stake, stakes_pool ) );
 }
---
+++ src/flamenco/stakes/fd_vote_stakes_private.h
@@ -0,0 +1,144 @@
+#include "../../util/fd_util_base.h"
+#include "../types/fd_types_custom.h"
+
+struct index_key {
+  fd_pubkey_t pubkey;
+  ulong       stake_t_1;
+};
+typedef struct index_key index_key_t;
+
+struct index_ele {
+  union {
+    struct {
+      fd_pubkey_t pubkey;
+      ulong       stake_t_1;
+    };
+    index_key_t index_key;
+  };
+  ulong       stake_t_2;
+  uint        next;
+  uint        refcnt;
+  uint        prev_multi;
+  uint        next_multi;
+};
+typedef struct index_ele index_ele_t;
+
+#define POOL_NAME  index_pool
+#define POOL_T     index_ele_t
+#define POOL_NEXT  next
+#define POOL_IDX_T uint
+#include "../../util/tmpl/fd_pool.c"
+
+#define MAP_NAME               index_map
+#define MAP_KEY_T              index_key_t
+#define MAP_ELE_T              index_ele_t
+#define MAP_KEY                index_key
+#define MAP_KEY_EQ(k0,k1)      (!memcmp( k0, k1, sizeof(index_key_t) ))
+#define MAP_KEY_HASH(key,seed) (fd_hash( seed, key, sizeof(index_key_t) ))
+#define MAP_NEXT               next
+#define MAP_IDX_T              uint
+#include "../../util/tmpl/fd_map_chain.c"
+
+#define MAP_NAME               index_map_multi
+#define MAP_MULTI              1
+#define MAP_KEY_T              fd_pubkey_t
+#define MAP_ELE_T              index_ele_t
+#define MAP_KEY                pubkey
+#define MAP_KEY_EQ(k0,k1)      (!memcmp( k0, k1, sizeof(fd_pubkey_t) ))
+#define MAP_KEY_HASH(key,seed) (fd_hash( seed, key, sizeof(fd_pubkey_t) ))
+#define MAP_PREV               prev_multi
+#define MAP_NEXT               next_multi
+#define MAP_IDX_T              uint
+#include "../../util/tmpl/fd_map_chain.c"
+
+/* Each pool index is just an array of uint indices into the pool. */
+struct stake {
+  uint idx;
+  uint next;
+};
+typedef struct stake stake_t;
+
+#define POOL_NAME  stakes_pool
+#define POOL_T     stake_t
+#define POOL_NEXT  next
+#define POOL_IDX_T uint
+#include "../../util/tmpl/fd_pool.c"
+
+#define MAP_NAME               stakes_map
+#define MAP_KEY_T              uint
+#define MAP_ELE_T              stake_t
+#define MAP_KEY                idx
+#define MAP_NEXT               next
+#define MAP_IDX_T              uint
+#include "../../util/tmpl/fd_map_chain.c"
+
+struct fork {
+  ushort prev;
+  ushort next;
+};
+typedef struct fork fork_t;
+
+#define POOL_NAME  fork_pool
+#define POOL_T     fork_t
+#define POOL_NEXT  next
+#define POOL_IDX_T ushort
+#include "../../util/tmpl/fd_pool.c"
+
+#define DLIST_NAME  fork_dlist
+#define DLIST_ELE_T fork_t
+#define DLIST_IDX_T ushort
+#include "../../util/tmpl/fd_dlist.c"
+
+struct fd_vote_stakes {
+  ulong magic;
+  ulong index_pool_off;
+  ulong index_map_off;
+  ulong index_map_multi_off;
+
+  ulong fork_pool_off;
+  ulong fork_dlist_off;
+
+  ulong  stakes_pool_off[ 128UL ]; /* TODO:FIXME: this has to be configurable */
+  ulong  stakes_map_off[ 128UL ]; /* TODO:FIXME: this has to be configurable */
+  ulong  outstanding;
+
+  ushort root_idx;
+};
+typedef struct fd_vote_stakes fd_vote_stakes_t;
+
+static inline index_ele_t *
+get_index_pool( fd_vote_stakes_t * vote_stakes ) {
+  return fd_type_pun( (uchar *)vote_stakes + vote_stakes->index_pool_off );
+}
+
+static inline index_map_t *
+get_index_map( fd_vote_stakes_t * vote_stakes ) {
+  return fd_type_pun( (uchar *)vote_stakes + vote_stakes->index_map_off );
+}
+
+static inline index_map_multi_t *
+get_index_map_multi( fd_vote_stakes_t * vote_stakes ) {
+  return fd_type_pun( (uchar *)vote_stakes + vote_stakes->index_map_multi_off );
+}
+
+static inline stake_t *
+get_stakes_pool( fd_vote_stakes_t * vote_stakes,
+                 ushort             fork_idx ) {
+  return fd_type_pun( (uchar *)vote_stakes + vote_stakes->stakes_pool_off[ fork_idx ] );
+}
+
+static inline stakes_map_t *
+get_stakes_map( fd_vote_stakes_t * vote_stakes,
+                ushort             fork_idx ) {
+  return fd_type_pun( (uchar *)vote_stakes + vote_stakes->stakes_map_off[ fork_idx ] );
+}
+
+static inline fork_t *
+get_fork_pool( fd_vote_stakes_t * vote_stakes ) {
+  return fd_type_pun( (uchar *)vote_stakes + vote_stakes->fork_pool_off );
+}
+
+static inline fork_dlist_t *
+get_fork_dlist( fd_vote_stakes_t * vote_stakes ) {
+  return fd_type_pun( (uchar *)vote_stakes + vote_stakes->fork_dlist_off );
+}
