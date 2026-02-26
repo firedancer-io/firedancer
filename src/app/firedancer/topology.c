43682cc5f63c50484d03ca7d279d9ad750dfb559
intrigus-lgtm <60750685+intrigus-lgtm@users.noreply.github.com>
2026-02-26 15:12:14 +0100

    topology: move null checks before use in full client

--- src/app/firedancer/topology.c
+++ src/app/firedancer/topology.c
@@ -179,9 +179,9 @@ setup_topo_acc_pool( fd_topo_t * topo,
                      ulong       max_account_cnt ) {
   fd_topob_wksp( topo, "acc_pool" );
   fd_topo_obj_t * acc_pool_obj = fd_topob_obj( topo, "acc_pool", "acc_pool" );
-  fd_pod_insertf_ulong( topo->props, max_account_cnt, "obj.%lu.max_account_cnt", acc_pool_obj->id );
   FD_TEST( acc_pool_obj );
   FD_TEST( acc_pool_obj->id != ULONG_MAX );
+  fd_pod_insertf_ulong( topo->props, max_account_cnt, "obj.%lu.max_account_cnt", acc_pool_obj->id );
   return acc_pool_obj;
 }
 
