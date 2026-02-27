aa51281df58a6e1f40acced74ed16d5c2e1e4d4c
Richard Patel <ripatel@jumptrading.com>
2026-02-27 21:08:33 +0000

    Fix firedancer-dev bench startup crash
    - Enable sandbox when watch is disabled
    - Add support for 'firedancer-dev mem --topo bench'
    - Fix segfault on startup
    - Work around invalid RPC tile responses on startup (status 500)

--- src/app/firedancer-dev/commands/bench.c
+++ src/app/firedancer-dev/commands/bench.c
@@ -3,16 +3,10 @@
 
 #include <unistd.h>
 
-static void
-bench_cmd_topo( config_t * config ) {
-  config->development.sandbox  = 0;
-  config->development.no_clone = 1;
-}
-
 void
 firedancer_dev_bench_cmd_fn( args_t *   args,
                              config_t * config ) {
-  bench_cmd_fn( args, config, 1 );
+  bench_cmd_fn( args, config );
 
   /* Sleep parent thread forever, Ctrl+C will terminate. */
   for(;;) pause();
@@ -21,9 +15,9 @@ firedancer_dev_bench_cmd_fn( args_t *   args,
 action_t fd_action_bench = {
   .name             = "bench",
   .args             = bench_cmd_args,
+  .topo             = bench_topo,
   .fn               = firedancer_dev_bench_cmd_fn,
   .perm             = dev_cmd_perm,
-  .topo             = bench_cmd_topo,
   .is_local_cluster = 1,
   .description      = "Test validator TPS benchmark"
 };
