03f60451f477dbdb4f36dd1c4a4e0d190014d98a
Richard Patel <ripatel@jumptrading.com>
2026-03-01 03:04:26 +0000

    Prevent SIGPIPE on TCP sockets
    A few pieces of code use write() to send to TCP sockets which can
    cause a SIGPIPE signal.  Tiles don't have signal handlers leading
    to a crash.  This affects RPC, bundle, and snapshot TCP clients.
    
    Updates all TCP socket usages to use MSG_NOSIGNAL.  This requires
    a custom BIO implementation for OpenSSL.

--- src/app/shared_dev/rpc_client/fd_rpc_client.c
+++ src/app/shared_dev/rpc_client/fd_rpc_client.c
@@ -294,7 +294,7 @@ fd_rpc_client_service( fd_rpc_client_t * rpc,
 
     if( FD_LIKELY( request->state==FD_RPC_CLIENT_STATE_CONNECTED && ( rpc->fds[ i ].revents & POLLOUT ) ) ) {
       long sent = send( rpc->fds[ i ].fd, request->connected.request_bytes+request->connected.request_bytes_sent,
-                        request->connected.request_bytes_cnt-request->connected.request_bytes_sent, 0 );
+                        request->connected.request_bytes_cnt-request->connected.request_bytes_sent, MSG_NOSIGNAL );
       if( FD_UNLIKELY( -1==sent && errno==EAGAIN ) ) continue;
       if( FD_UNLIKELY( -1==sent ) ) {
         fd_rpc_mark_error( rpc, i, FD_RPC_CLIENT_ERR_NETWORK );
