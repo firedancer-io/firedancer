e16399725c95b469b22a01e1856bf37219a72f28
Richard Patel <ripatel@jumptrading.com>
2026-03-01 02:45:28 +0000

    shred: revive test_deshredder
    This test was missing a build rule.

--- src/ballet/shred/Local.mk
+++ src/ballet/shred/Local.mk
@@ -3,5 +3,7 @@ $(call add-objs,fd_shred fd_deshredder,fd_ballet)
 $(call make-unit-test,test_shred,test_shred,fd_ballet fd_util)
 $(call run-unit-test,test_shred)
 ifdef FD_HAS_HOSTED
+$(call make-unit-test,test_deshredder,test_deshredder,fd_ballet fd_util)
+$(call run-unit-test,test_deshredder)
 $(call make-fuzz-test,fuzz_shred_parse,fuzz_shred_parse,fd_ballet fd_util)
 endif
