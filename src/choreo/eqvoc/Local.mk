42ee7af3607388a74b5d6dd3903c2985cf613879
Richard Patel <ripatel@jumptrading.com>
2026-03-01 06:35:08 +0000

    eqvoc: fix DuplicateShred proof handling
    Add a fuzzer for handling DuplicateShred proofs and fix bugs

--- src/choreo/eqvoc/Local.mk
+++ src/choreo/eqvoc/Local.mk
@@ -3,5 +3,6 @@ $(call add-hdrs,fd_eqvoc.h)
 $(call add-objs,fd_eqvoc,fd_choreo)
 ifdef FD_HAS_HOSTED
 $(call make-unit-test,test_eqvoc,test_eqvoc,fd_choreo fd_flamenco fd_ballet fd_util)
+$(call make-fuzz-test,fuzz_eqvoc,fuzz_eqvoc,fd_choreo fd_flamenco fd_ballet fd_util)
 endif
 endif
