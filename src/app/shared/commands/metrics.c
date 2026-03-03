7975e7b4348496fa32a441b56189c2a109c156a5
Richard Patel <ripatel@jumptrading.com>
2026-03-03 05:01:31 +0000

    fdctl: fix --topo flag handling

--- src/app/shared/commands/metrics.c
+++ src/app/shared/commands/metrics.c
@@ -27,7 +27,7 @@ reconstruct_topo( config_t *   config,
   if( !topo_name[0] ) return; /* keep default action topo */
 
   action_t const * selected = NULL;
-  for( action_t ** a=ACTIONS; a; a++ ) {
+  for( action_t ** a=ACTIONS; *a; a++ ) {
     action_t const * action = *a;
     if( 0==strcmp( action->name, topo_name ) ) {
       selected = action;
