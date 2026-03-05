ebd54a6f9376c6d49f8c8f76f82eede86af48b9c
David Rubin <drubin@jumptrading.com>
2026-03-05 00:49:50 +0000

    implement fd_shredb

--- src/disco/store/Local.mk
+++ src/disco/store/Local.mk
@@ -1,8 +1,11 @@
 ifdef FD_HAS_ALLOCA
-$(call add-hdrs,fd_store.h)
+$(call add-hdrs,fd_store.h fd_shredb.h)
 $(call add-objs,fd_store,fd_disco)
+$(call add-objs,fd_shredb,fd_disco)
 ifdef FD_HAS_HOSTED
 $(call make-unit-test,test_store,test_store,fd_disco fd_flamenco fd_tango fd_ballet fd_util)
 $(call run-unit-test,test_store)
+$(call make-unit-test,test_shredb,test_shredb,fd_disco fd_ballet fd_util)
+$(call run-unit-test,test_shredb)
 endif
 endif
