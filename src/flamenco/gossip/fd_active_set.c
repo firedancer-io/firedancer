fd507bc7343f3a0d239f61c33a7b91e007d957ce
Michael McGee <mmcgee@jumptrading.com>
2026-03-05 21:31:57 +0000

    gossip: fix infinite loop

--- src/flamenco/gossip/fd_active_set.c
+++ src/flamenco/gossip/fd_active_set.c
@@ -311,7 +311,7 @@ rotate_active_set( fd_active_set_t *   active_set,
     fd_gossip_wsample_add_bucket( active_set->wsample, bucket, old_ci_idx );
     push_flush( active_set, &active_set->peers[ bucket*12UL+replace_idx ], stem, now );
   } else {
-    replace_idx = entry->nodes_len;
+    replace_idx = (entry->nodes_idx+entry->nodes_len) % 12UL;
   }
 
   fd_active_set_peer_t * replace = &active_set->peers[ bucket*12UL+replace_idx ];
