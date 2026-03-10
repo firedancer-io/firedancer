0260449f5638422130dc7bbc88e0f2eaba0c04ee
Ishan Bhatt <ibhatt@jumptrading.com>
2026-03-10 15:41:08 +0000

    wip

--- src/choreo/tower/test_tower.c
+++ src/choreo/tower/test_tower.c
@@ -215,6 +215,7 @@ mock_vote_acc( fd_hash_t const * pubkey, ulong stake, ulong vote, uint conf, fd_
   };
 
   memcpy( out->data, &voter, sizeof(fd_vote_acc_t) );
+  out->valid_data = 1;
   out->stake = stake;
   out->addr = *pubkey;
 }
--- src/choreo/tower/test_tower_lockos.c
+++ src/choreo/tower/test_tower_lockos.c
@@ -15,8 +15,9 @@ mock_vote_acc( fd_hash_t const * pubkey, ulong stake, ulong vote, uint conf, fd_
   };
 
   memcpy( out->data, &voter, sizeof(fd_vote_acc_t) );
-  out->stake = stake;
-  out->addr = *pubkey;
+  out->valid_data = 1;
+  out->stake      = stake;
+  out->addr       = *pubkey;
 }
 
 void
