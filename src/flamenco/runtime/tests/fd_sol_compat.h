9474015a2baf9263be8b710b3eb8b89ed8daf770
cmoyes-jump <cmoyes@jumptrading.com>
2026-03-11 17:05:02 -0500

    gossip: solfuzz entry points (#8821)
    * entry points
    
    * PR suggestions

--- src/flamenco/runtime/tests/fd_sol_compat.h
+++ src/flamenco/runtime/tests/fd_sol_compat.h
@@ -108,6 +108,12 @@ sol_compat_shred_parse_v1( uchar *       out,
                            uchar const * in,
                            ulong         in_sz );
 
+int
+sol_compat_gossip_message_deserialize_v1( uchar *       out,
+                                          ulong *       out_sz,
+                                          uchar const * in,
+                                          ulong         in_sz );
+
 FD_PROTOTYPES_END
 
 #endif /* HEADER_fd_src_flamenco_runtime_tests_fd_sol_compat_h */
