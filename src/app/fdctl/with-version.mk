877b5f7de707da7ad22546e528e69388670117c3
anwayde <111067470+anwayde@users.noreply.github.com>
2026-03-18 21:25:50 +0000

    frankendancer: adopt agave v4.0 versioning (#8945)

--- src/app/fdctl/with-version.mk
+++ src/app/fdctl/with-version.mk
@@ -1,4 +1,5 @@
 include src/app/fdctl/version.mk
+include src/app/fdctl/with-agave.mk
 
 # Frankendancer versioning is always major 0, the first full Firedancer
 # release will be version 1.0
@@ -9,15 +10,19 @@ FIREDANCER_VERSION_MAJOR := $(VERSION_MAJOR)
 # branches could point to the same Agave patch.
 FIREDANCER_VERSION_MINOR := $(VERSION_MINOR)$(shell printf "%02d" $(VERSION_PATCH))
 
+# Agave version v4 encodes the prerelease bits in the minor version.
+# We do the same to make sure the prerelease information does not get
+# lost.  This is a no-op when the prerelease is Stable == 0.
+FIREDANCER_VERSION_MINOR := $(shell echo $$(( $(FIREDANCER_VERSION_MINOR) | ($(AGAVE_PRERELEASE_TAG) << $(AGAVE_PRERELEASE_BITS_OFFSET)) )) )
+
 # For Frankendancer, we stuff the entire Agave version that we are
 # linking to in the patch version.  This transforms, for example, a full
-# Agave version of "1.18.7" to a minor version of "11807".
-FIREDANCER_VERSION_PATCH := $(shell $(GREP) -Po "(?<=^version = \").*(?=\")" "agave/Cargo.toml" | awk -F. '{ printf "%d%02d%02d\n", $$1, $$2, $$3 }')
-
-# If the Agave submodule is not checked out, the above command fails
-ifeq ($(FIREDANCER_VERSION_PATCH),)
-FIREDANCER_VERSION_PATCH := 9999
-endif
+# Agave version of "4.1.7" to a patch version of "40107".  If it is a
+# prerelease like "4.1.0-beta.7", we use the prerelease version as the
+# agave patch version and the frankendancer patch again becomes "40107"
+# (NOT "40100").  The minor version carries the information that it is
+# a prerelease even when the patch versions are the same.
+FIREDANCER_VERSION_PATCH := $(shell printf "%d%02d%02d" $(AGAVE_VERSION_MAJOR) $(AGAVE_VERSION_MINOR) $(AGAVE_VERSION_PATCH))
 
 export FIREDANCER_VERSION_MAJOR
 export FIREDANCER_VERSION_MINOR
