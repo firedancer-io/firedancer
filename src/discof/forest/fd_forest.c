6b79cc78b84e38becafc7771c85031cce2043af9
Emily Wang <emwang@jumptrading.com>
2026-03-23 20:17:34 +0000

    forest: tighten sentinel block conditions

--- src/discof/forest/fd_forest.c
+++ src/discof/forest/fd_forest.c
@@ -915,21 +915,6 @@ fd_forest_blk_insert( fd_forest_t * forest, ulong slot, ulong parent_slot, ulong
   FD_TEST( slot > fd_forest_root_slot( forest ) ); /* caller error - inval */
 # endif
 
-  fd_forest_blk_t * ele = query( forest, slot );
-  if( FD_LIKELY( ele ) ) {
-    // potentially may need to update the parent_slot, if this
-    // this was a sentinel block that was created for a confirmed msg
-    if( FD_UNLIKELY( parent_slot != ele->parent_slot ) ) {
-      ele->parent_slot = parent_slot;
-      subtrees_orphaned_remove( forest, slot ); // if this is a sentinel block, then it must be in subtrees
-    } else {
-      return ele;
-    }
-  } else {
-    ele = acquire( forest, slot, parent_slot, evicted );
-    if( FD_UNLIKELY( !ele ) ) return NULL; /* no space in pool, so we can't add this slot */
-  }
-
   fd_forest_ancestry_t * ancestry = fd_forest_ancestry( forest );
   fd_forest_frontier_t * frontier = fd_forest_frontier( forest );
   fd_forest_subtrees_t * subtrees = fd_forest_subtrees( forest );
@@ -942,6 +927,22 @@ fd_forest_blk_insert( fd_forest_t * forest, ulong slot, ulong parent_slot, ulong
   fd_forest_blk_t *      pool     = fd_forest_pool ( forest );
   ulong *                bfs      = fd_forest_deque( forest );
   ulong                  null     = fd_forest_pool_idx_null( pool );
+
+  fd_forest_blk_t * ele = query( forest, slot );
+  if( FD_LIKELY( ele ) ) {
+    // potentially may need to update the parent_slot, if this
+    // this was a sentinel block that was created for a confirmed msg
+    if( FD_UNLIKELY( ele->slot == ele->parent_slot && ele->parent_slot != parent_slot ) ) {
+      ele->parent_slot = parent_slot;
+      FD_TEST( fd_forest_subtrees_ele_query( subtrees, &slot, NULL, pool ) || fd_forest_orphaned_ele_query( orphaned, &slot, NULL, pool ) );
+      subtrees_orphaned_remove( forest, slot ); // if this is a sentinel block, then it must be in subtrees
+    } else {
+      return ele;
+    }
+  } else {
+    ele = acquire( forest, slot, parent_slot, evicted );
+    if( FD_UNLIKELY( !ele ) ) return NULL; /* no space in pool, so we can't add this slot */
+  }
 
   fd_forest_blk_t * parent = NULL;
 
