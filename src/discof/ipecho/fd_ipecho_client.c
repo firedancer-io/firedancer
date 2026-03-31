631a78bc1dab68ec7260394c44b6a9b7b18a6171
jherrera-jump <jherrera@jumptrading.com>
2026-03-31 21:19:10 +0000

    ipecho, genesi: fix high idle%

--- src/discof/ipecho/fd_ipecho_client.c
+++ src/discof/ipecho/fd_ipecho_client.c
@@ -224,14 +224,16 @@ fd_ipecho_client_poll( fd_ipecho_client_t * client,
   else if( FD_UNLIKELY( -1==nfds && errno==EINTR ) ) return 1;
   else if( FD_UNLIKELY( -1==nfds ) ) FD_LOG_ERR(( "poll() failed (%i-%s)", errno, strerror( errno ) ));
 
-  *charge_busy = 1;
-
   for( ulong i=0UL; i<16UL; i++ ) {
     if( FD_UNLIKELY( -1==client->pollfds[ i ].fd ) ) continue;
 
-    if( FD_LIKELY( client->pollfds[ i ].revents & POLLOUT ) ) write_conn( client, i );
+    if( FD_LIKELY( client->pollfds[ i ].revents & POLLOUT ) ) {
+      if( FD_UNLIKELY( client->peers[ i ].writing ) ) *charge_busy = 1;
+      write_conn( client, i );
+    }
     if( FD_UNLIKELY( -1==client->pollfds[ i ].fd ) ) continue;
     if( FD_LIKELY( client->pollfds[ i ].revents & POLLIN ) ) {
+      if( FD_UNLIKELY( !client->peers[ i ].writing ) ) *charge_busy = 1;
       if( FD_LIKELY( !read_conn( client, i, shred_version ) ) ) {
         close_all( client );
         return 0;
