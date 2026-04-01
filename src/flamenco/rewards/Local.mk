d4d5790a7eeeb3f0431285d2e05624373d7c8503
Ishan Bhatt <ibhatt@jumptrading.com>
2026-04-01 15:50:09 +0000

    wip

--- src/flamenco/rewards/Local.mk
+++ src/flamenco/rewards/Local.mk
@@ -7,10 +7,4 @@ $(call add-objs,fd_stake_rewards,fd_flamenco)
 
 $(call add-hdrs,fd_rewards.h)
 $(call add-objs,fd_rewards,fd_flamenco)
-
-ifdef FD_HAS_HOSTED
-$(call make-unit-test,test_epoch_boundary,test_epoch_boundary,fd_flamenco fd_funk fd_ballet fd_util)
-$(call run-unit-test,test_epoch_boundary)
-endif
-
 endif
