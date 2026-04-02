cc00c1d69ad20eeb0dec760dcc7271dff37510d9
jherrera-jump <jherrera@jumptrading.com>
2026-04-02 13:28:25 -0500

    frank: fix version string (#9134)

--- src/app/fdctl/version.c
+++ src/app/fdctl/version.c
@@ -10,10 +10,13 @@
 #ifndef FDCTL_COMMIT_REF_U32
 #define FDCTL_COMMIT_REF_U32 0x0
 #endif
+#ifndef FDCTL_VERSION_CSTR
+#define FDCTL_VERSION_CSTR "0.0.0"
+#endif
 
 ulong const fdctl_major_version     = FDCTL_MAJOR_VERSION;
 ulong const fdctl_minor_version     = FDCTL_MINOR_VERSION;
 ulong const fdctl_patch_version     = FDCTL_PATCH_VERSION;
 uint  const fdctl_commit_ref        = FDCTL_COMMIT_REF_U32;
 char  const fdctl_commit_ref_string[] = FDCTL_COMMIT_REF_CSTR;
-char  const fdctl_version_string[]  = FD_EXPAND_THEN_STRINGIFY(FDCTL_MAJOR_VERSION) "." FD_EXPAND_THEN_STRINGIFY(FDCTL_MINOR_VERSION) "." FD_EXPAND_THEN_STRINGIFY(FDCTL_PATCH_VERSION);
+char  const fdctl_version_string[]  = FDCTL_VERSION_CSTR;
