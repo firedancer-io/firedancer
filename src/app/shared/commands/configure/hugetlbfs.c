65dc343df8aae74402cc7d5b6a3233aa2c695c31
jvarela-jump <jvarela@jumptrading.com>
2026-04-08 19:58:47 +0000

    app/waltz: errno handling upgrades

--- src/app/shared/commands/configure/hugetlbfs.c
+++ src/app/shared/commands/configure/hugetlbfs.c
@@ -194,7 +194,10 @@ warn_mount_users( char const * mount_path ) {
   if( FD_UNLIKELY( !dir ) ) FD_LOG_ERR(( "error opening `/proc` (%i-%s)", errno, fd_io_strerror( errno ) ));
 
   struct dirent * entry;
-  while(( FD_LIKELY( entry = readdir( dir ) ) )) {
+  for(;;) {
+    errno = 0;
+    entry = readdir( dir );
+    if( FD_UNLIKELY( !entry ) ) break;
     if( FD_UNLIKELY( !strcmp( entry->d_name, "." ) || !strcmp( entry->d_name, ".." ) ) ) continue;
     char * endptr;
     ulong pid = strtoul( entry->d_name, &endptr, 10 );
@@ -222,7 +225,7 @@ warn_mount_users( char const * mount_path ) {
       FD_LOG_ERR(( "error closing `%s` (%i-%s)", path, errno, fd_io_strerror( errno ) ));
   }
 
-  if( FD_UNLIKELY( errno && errno!=ENOENT ) ) FD_LOG_ERR(( "readdir() (%i-%s)", errno, fd_io_strerror( errno ) ));
+  if( FD_UNLIKELY( errno ) ) FD_LOG_ERR(( "readdir() (%i-%s)", errno, fd_io_strerror( errno ) ));
   if( FD_UNLIKELY( -1==closedir( dir ) ) ) FD_LOG_ERR(( "closedir (%i-%s)", errno, fd_io_strerror( errno ) ));
 }
 
