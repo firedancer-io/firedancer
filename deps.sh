5458026c33abe2022f517d8261c459447aa06b1f
Emanuele Cesena <ecesena@jumptrading.com>
2026-04-09 16:25:09 +0000

    s2n: upgrade lib

--- deps.sh
+++ deps.sh
@@ -135,7 +135,7 @@ fetch () {
   fi
   checkout_repo zstd      https://github.com/facebook/zstd            "v1.5.7"
   checkout_repo lz4       https://github.com/lz4/lz4                  "v1.10.0"
-  checkout_repo s2n       https://github.com/awslabs/s2n-bignum       "" "4d2e22a"
+  checkout_repo s2n       https://github.com/awslabs/s2n-bignum       "" "cba3956c"
   checkout_repo openssl   https://github.com/openssl/openssl          "openssl-3.6.2"
   checkout_repo blst      https://github.com/supranational/blst       "v0.3.13"
   checkout_repo bzip2     https://gitlab.com/bzip2/bzip2              "bzip2-1.0.8"
