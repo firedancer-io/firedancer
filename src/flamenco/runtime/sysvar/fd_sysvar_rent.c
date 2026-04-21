c0abb599949590f9cadfae00e862169c5bd07791
Ishan Bhatt <ibhatt@jumptrading.com>
2026-04-21 22:06:00 +0000

    wip

--- src/flamenco/runtime/sysvar/fd_sysvar_rent.c
+++ src/flamenco/runtime/sysvar/fd_sysvar_rent.c
@@ -12,7 +12,7 @@ fd_sysvar_rent_write( fd_bank_t *               bank,
                       fd_funk_txn_xid_t const * xid,
                       fd_capture_ctx_t *        capture_ctx,
                       fd_rent_t const *         rent ) {
-  fd_sysvar_account_update( bank, accdb, xid, capture_ctx, &fd_sysvar_rent_id, rent, sizeof(fd_rent_t) );
+  fd_sysvar_account_update( bank, accdb, xid, capture_ctx, &fd_sysvar_rent_id, rent, FD_SYSVAR_RENT_BINCODE_SZ );
 }
 
 void
