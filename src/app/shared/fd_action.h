75b53401213cff527f6b7be7e6d0061691a06b0b
Richard Patel <ripatel@jumptrading.com>
2026-04-21 19:54:45 +0000

    amend

--- src/app/shared/fd_action.h
+++ src/app/shared/fd_action.h
@@ -188,6 +188,10 @@ union fdctl_args {
   struct {
     ulong ready_slot;
   } ready;
+
+  struct {
+    char name[ 13UL ];
+  } pid_tid;
 };
 
 typedef union fdctl_args args_t;
@@ -197,17 +201,18 @@ struct fd_action {
   char const * description;
   char const * permission_err;
 
-  int          is_help;
-  int          is_immediate;
-  int          require_config;   /* halt if the user tries to use the default config */
-  int          is_local_cluster; /* If a command is one which runs a local cluster, certain information in
-                                    the configuration file will be changed. */
-  uchar        is_diagnostic;  /* 1 implies action should be allowed for prod debugging */
+  uint is_help : 1;
+  uint is_immediate : 1;
+  uint require_config : 1;    /* halt if the user tries to use the default config */
+  uint is_local_cluster : 1;  /* If a command is one which runs a local cluster, certain information in
+                                 the configuration file will be changed. */
+  uint is_diagnostic : 1;     /* 1 implies action should be allowed for prod debugging */
+  uint no_log_file : 1;       /* Don't create a log file (development only) */
 
-  void       (*args)( int * pargc, char *** pargv, args_t * args );
-  void       (*topo)( config_t * config );
-  void       (*perm)( args_t * args, fd_cap_chk_t * chk, config_t const * config );
-  void       (*fn  )( args_t * args, config_t * config );
+  void (* args)( int * pargc, char *** pargv, args_t * args );
+  void (* topo)( config_t * config );
+  void (* perm)( args_t * args, fd_cap_chk_t * chk, config_t const * config );
+  void (* fn  )( args_t * args, config_t * config );
 };
 
 typedef struct fd_action action_t;
