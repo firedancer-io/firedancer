f6d632f305e7e38b3276fd11b003985073f8642d
esorense <Emilsorensensnsn@gmail.com>
2026-04-27 22:57:44 +0800

    offline-replay: wire heap_size and remove dead PAGES

--- contrib/offline-replay/offline_replay_network_parameters.sh
+++ contrib/offline-replay/offline_replay_network_parameters.sh
@@ -16,7 +16,6 @@ case $network in
     export HEAP_SIZE=200
     export BACKTEST_FUNK_PAGES=600
     export INDEX_MAX=1200000000
-    export PAGES=50
     export AGAVE_TAG=v3.0.6
     ;;
   "testnet")
@@ -25,7 +24,6 @@ case $network in
     export HEAP_SIZE=200
     export BACKTEST_FUNK_PAGES=50
     export INDEX_MAX=200000000
-    export PAGES=50
     export AGAVE_TAG=v3.1.1
     ;;
   "devnet")
@@ -34,7 +32,6 @@ case $network in
     export HEAP_SIZE=100
     export BACKTEST_FUNK_PAGES=400
     export INDEX_MAX=300000000
-    export PAGES=50
     export AGAVE_TAG=v3.0.6
     ;;
   *)
