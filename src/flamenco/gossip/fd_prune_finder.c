003a9ddfe50af0076f36958d96dbab9998342b82
lmorett1 <moretluck@gmail.com>
2026-04-27 21:48:19 +0800

    gossip: use sat_mul in overflow-safe prune math

--- src/flamenco/gossip/fd_prune_finder.c
+++ src/flamenco/gossip/fd_prune_finder.c
@@ -1,5 +1,6 @@
 #include "fd_prune_finder.h"
 #include "../../util/log/fd_log.h"
+#include "../../util/bits/fd_sat.h"
 
 /* Maximum number of origins tracked in the outer map.  Matches
    Agave's ReceivedCache capacity of 2 * CRDS_UNIQUE_PUBKEY_CAPACITY. */
@@ -235,10 +236,9 @@ do_prune( fd_prune_finder_t * pf,
      Relayers covering the top min_ingress_nodes and enough cumulative
      stake to meet min_ingress_stake are kept; the rest are pruned. */
   ulong min_base = fd_ulong_min( pf->identity_stake, origin->origin_stake );
-  /* Compute floor(min_base * pct / 100) without overflow and without
-     requiring __uint128_t. */
-  ulong min_ingress_stake = (min_base / 100UL) * FD_PRUNE_FINDER_STAKE_THRESHOLD_PCT
-                          + ((min_base % 100UL) * FD_PRUNE_FINDER_STAKE_THRESHOLD_PCT) / 100UL;
+  /* Keep floor(min_base * pct / 100) semantics while avoiding overflow. */
+  ulong min_ingress_stake = fd_ulong_sat_mul( min_base / 100UL, FD_PRUNE_FINDER_STAKE_THRESHOLD_PCT )
+                          + fd_ulong_sat_mul( min_base % 100UL, FD_PRUNE_FINDER_STAKE_THRESHOLD_PCT ) / 100UL;
 
   ulong cum_stake = 0UL;
 
