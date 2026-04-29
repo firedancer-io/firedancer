cbc961b016f121dc8b1ddb2f8c177e01cfd64e9d
Tom Pointon <topointon@jumptrading.com>
2026-04-29 18:06:25 +0000

    flamenco, test: update ledger for bls_pubkey_management_in_vote_account following re-key

--- src/flamenco/runtime/tests/run_backtest_all.sh
+++ src/flamenco/runtime/tests/run_backtest_all.sh
@@ -128,5 +128,5 @@ src/flamenco/runtime/tests/run_ledger_backtest.sh -l delay-comission-updates-7 -
 src/flamenco/runtime/tests/run_ledger_backtest.sh -l delay-comission-updates-8 -y 1 -m 10000 -e 1596
 src/flamenco/runtime/tests/run_ledger_backtest.sh -l vat-activation -y 1 -m 10000 -e 540
 src/flamenco/runtime/tests/run_ledger_backtest.sh -l enable_bls12_381_syscall -y 1 -m 1000 -e 379
-src/flamenco/runtime/tests/run_ledger_backtest.sh -l bls_pubkey_management_in_vote_account -y 1 -m 1000 -e 364
+src/flamenco/runtime/tests/run_ledger_backtest.sh -l bls_pubkey_management_in_vote_account_rekey -y 1 -m 1000 -e 329
 src/flamenco/runtime/tests/run_ledger_backtest.sh -l raise_block_limits_to_100m -y 1 -m 10000 -e 603
