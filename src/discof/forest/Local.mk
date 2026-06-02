9a2fa43fdaae58c41d99bb015fc8f92f04e51404
intrigus-lgtm <60750685+intrigus-lgtm@users.noreply.github.com>
2026-06-02 19:41:22 +0000

    forest: fix test expectations
    `fd_forest_data_shred_insert` (1563) indirectly calls
    `verified_parent_update` which only preserves `confirmed_bid`.
    So the mr of the parent is no longer there and we can't further
    verify.

--- src/discof/forest/Local.mk
+++ src/discof/forest/Local.mk
@@ -1,4 +1,5 @@
 $(call add-objs,fd_forest,fd_discof)
 ifdef FD_HAS_HOSTED
-$(call make-unit-test,test_forest,test_forest,fd_discof fd_disco fd_flamenco fd_tango fd_ballet fd_util) # lint-no-run-unit-test, TODO: currently broken test
+$(call make-unit-test,test_forest,test_forest,fd_discof fd_disco fd_flamenco fd_tango fd_ballet fd_util)
+$(call run-unit-test,test_forest)
 endif
