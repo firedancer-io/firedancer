cf492878509459d487445d53f4ff54afaa57add1
intrigus-lgtm <60750685+intrigus-lgtm@users.noreply.github.com>
2026-06-03 12:04:07 +0000

    eqvoc: fix swapped arguments to eqvoc_chunk_insert

--- src/choreo/eqvoc/Local.mk
+++ src/choreo/eqvoc/Local.mk
@@ -3,6 +3,7 @@ $(call add-hdrs,fd_eqvoc.h)
 $(call add-objs,fd_eqvoc,fd_choreo)
 ifdef FD_HAS_HOSTED
 $(call make-unit-test,test_eqvoc,test_eqvoc,fd_choreo fd_flamenco fd_ballet fd_util)
+$(call run-unit-test,test_eqvoc)
 $(call make-fuzz-test,fuzz_eqvoc,fuzz_eqvoc,fd_choreo fd_flamenco fd_ballet fd_util)
 endif
 endif
