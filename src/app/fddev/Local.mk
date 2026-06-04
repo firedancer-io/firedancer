6ed28202adff406cca2033aec58d4b11e3e12341
Richard Patel <ripatel@jumptrading.com>
2026-06-04 22:41:16 +0000

    fddev: remove legacy Make targets

--- src/app/fddev/Local.mk
+++ src/app/fddev/Local.mk
@@ -3,7 +3,7 @@ ifdef FD_HAS_LINUX
 ifdef FD_HAS_ALLOCA
 ifdef FD_HAS_DOUBLE
 
-.PHONY: fddev run monitor
+.PHONY: fddev
 
 $(call add-objs,dev1,fd_fddev)
 $(call add-objs,commands/configure/blockstore,fd_fddev)
@@ -11,29 +11,6 @@ $(call add-objs,commands/bench,fd_fddev)
 $(call add-objs,commands/dev,fd_fddev)
 
 $(call make-bin-rust,fddev,main,fd_fddev fd_fdctl fddev_shared fdctl_shared fdctl_platform fd_discoh fd_disco fd_choreo agave_validator fd_flamenco fd_quic fd_tls fd_reedsol fd_waltz fd_tango fd_ballet fd_util fdctl_version)
-
-ifeq (run,$(firstword $(MAKECMDGOALS)))
-  RUN_ARGS := $(wordlist 2,$(words $(MAKECMDGOALS)),$(MAKECMDGOALS))
-  ifeq ($(RUN_ARGS),)
-    RUN_ARGS := dev --monitor
-  endif
-  $(eval $(RUN_ARGS):;@:)
-endif
-
-run: $(OBJDIR)/bin/fddev
-	$(OBJDIR)/bin/fddev $(RUN_ARGS)
-
-ifeq (monitor,$(firstword $(MAKECMDGOALS)))
-  MONITOR_ARGS := $(wordlist 2,$(words $(MAKECMDGOALS)),$(MAKECMDGOALS))
-  ifeq ($(MONITOR_ARGS),)
-    MONITOR_ARGS :=
-  endif
-  $(eval $(MONITOR_ARGS):;@:)
-endif
-
-monitor: bin
-	$(OBJDIR)/bin/fddev monitor $(MONITOR_ARGS)
-
 $(call make-integration-test,test_fddev,tests/test_fddev,fd_fddev fd_fdctl fddev_shared fdctl_shared fdctl_platform fd_discoh fd_disco fd_choreo agave_validator fd_flamenco fd_quic fd_tls fd_reedsol fd_waltz fd_tango fd_ballet fd_util fdctl_version)
 $(call run-integration-test,test_fddev)
 
