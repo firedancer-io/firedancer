5693c0bfd7efa30654d25a2bd180c55d6d7d727d
Michael McGee <mmcgee@jumptrading.com>
2026-06-04 22:33:59 +0000

    accdb: implement new accounts database

--- src/flamenco/accdb/Local.mk
+++ src/flamenco/accdb/Local.mk
@@ -36,3 +36,27 @@ ifdef FD_HAS_RACESAN
 $(call make-unit-test,test_accdb_v1_racesan,test_accdb_v1_racesan,fd_flamenco fd_funk fd_ballet fd_util)
 endif
 endif
+
+# New accounts database (accdb v4) core library
+ifdef FD_HAS_ATOMIC
+ifdef FD_HAS_ALLOCA
+
+$(call add-hdrs,fd_accdb.h fd_accdb_cache.h fd_accdb_shmem.h)
+$(call add-objs,fd_accdb fd_accdb_cache fd_accdb_shmem,fd_flamenco)
+
+$(call make-unit-test,test_accdb,test_accdb,fd_flamenco fd_ballet fd_util)
+$(call run-unit-test,test_accdb)
+
+$(call make-unit-test,test_accdb_cache,test_accdb_cache,fd_flamenco fd_ballet fd_util)
+$(call run-unit-test,test_accdb_cache)
+
+$(call make-unit-test,bench_accdb,bench_accdb,fd_flamenco fd_ballet fd_util)
+$(call make-unit-test,bench_accdb_hotread,bench_accdb_hotread,fd_flamenco fd_ballet fd_util)
+$(call make-unit-test,bench_accdb_txn,bench_accdb_txn,fd_flamenco fd_ballet fd_util)
+
+ifdef FD_HAS_RACESAN
+$(call make-unit-test,test_accdb_racesan,test_accdb_racesan,fd_flamenco fd_ballet fd_util)
+endif
+
+endif
+endif
