bffc8d22b8abebd63b57fcf56f85aa40bfee2587
Richard Patel <ripatel@jumptrading.com>
2026-06-04 18:12:02 +0000

    tls: fix alert code when server cert is invalid

--- src/waltz/tls/fd_tls.c
+++ src/waltz/tls/fd_tls.c
@@ -829,8 +829,11 @@ fd_tls_handle_cert_chain( fd_tls_estate_base_t * const base,
   fd_tls_extract_cert_pubkey_res_t extract =
   fd_tls_extract_cert_pubkey( cert_chain, cert_chain_sz, fd_uint_if( is_rpk, FD_TLS_CERTTYPE_RAW_PUBKEY, FD_TLS_CERTTYPE_X509 ) );
 
-  if( FD_UNLIKELY( !extract.pubkey ) )
-    return fd_tls_alert( base, extract.alert, extract.reason );
+  if( FD_UNLIKELY( !extract.pubkey ) ) {
+    uint   alert  = extract.alert  ? extract.alert  : FD_TLS_ALERT_DECODE_ERROR;
+    ushort reason = extract.reason ? extract.reason : FD_TLS_REASON_CERT_PARSE;
+    return fd_tls_alert( base, alert, reason );
+  }
 
   if( expected_pubkey )
     if( FD_UNLIKELY( 0!=memcmp( extract.pubkey, expected_pubkey, 32UL ) ) )
