c6c373886d9d07e2903460e44551664727e774dd
Vic Genin <aie0.com@gmail.com>
2026-06-09 16:13:37 +0300

    page-cnt guard

--- src/flamenco/runtime/test_deprecate_rent_exemption_threshold.c
+++ src/flamenco/runtime/test_deprecate_rent_exemption_threshold.c
@@ -460,6 +460,7 @@ main( int     argc,
 
   char const * _page_sz = fd_env_strip_cmdline_cstr( &argc, &argv,  "--page-sz",  NULL, "gigantic" );
   ulong        page_cnt = fd_env_strip_cmdline_ulong( &argc, &argv, "--page-cnt", NULL, 2UL );
+  if( FD_UNLIKELY( page_cnt<1UL ) ) FD_LOG_ERR(( "--page-cnt must be at least 1" ));
   ulong        numa_idx = fd_env_strip_cmdline_ulong( &argc, &argv, "--numa-idx", NULL, fd_shmem_numa_idx( cpu_idx ) );
 
   ulong page_sz = fd_cstr_to_shmem_page_sz( _page_sz );
