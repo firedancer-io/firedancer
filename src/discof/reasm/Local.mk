dc08fa25014d5c3490f1951db31a3dae2cf1bed7
two-heart <12869538+two-heart@users.noreply.github.com>
2026-06-18 22:02:24 +0200

    fuzz: reasm fuzzer
    action based fuzzers comparing execution against a model

--- src/discof/reasm/Local.mk
+++ src/discof/reasm/Local.mk
@@ -2,4 +2,5 @@ $(call add-objs,fd_reasm,fd_discof)
 ifdef FD_HAS_HOSTED
 $(call make-unit-test,test_reasm,test_reasm,fd_discof fd_disco fd_flamenco fd_ballet fd_util)
 $(call run-unit-test,test_reasm)
+$(call make-fuzz-test,fuzz_reasm,fuzz_reasm,fd_discof fd_disco fd_ballet fd_util)
 endif
