203344202ebd3c7bd2921ac21f20f142f5b6b35c
two-heart <12869538+two-heart@users.noreply.github.com>
2026-07-08 17:14:26 +0200

    shred: prevent rare repair re-requests

--- src/disco/shred/fd_rnonce_ss.h
+++ src/disco/shred/fd_rnonce_ss.h
@@ -99,10 +99,10 @@ fd_rnonce_ss_verify( fd_rnonce_ss_t const * ss,
   temp->private.shred_idx = fd_uint_if ( normal_repair, shred_idx, 0U         );
 #define ALLOWED_TIME_DELTA ((uint)((1000000000UL + (1UL<<24) - 1UL)/(1UL<<24)))  /* == 60 */
 
-#define CHECKN( temp ) do{ if( FD_LIKELY(                                                                                       \
-                         ( (0x7FFFFFFFUL & (fd_hash( 2270897969802886507UL, temp, sizeof(temp) ) + (((ulong)time_ns)>>24) )) -  \
-                           (0x7FFFFFFFUL & nonce) ) <= ALLOWED_TIME_DELTA ) )                                                   \
-                             return 1;                                                                                          \
+#define CHECKN( temp ) do{ if( FD_LIKELY(                                                                                   \
+                         ( (fd_hash( 2270897969802886507UL, temp, sizeof(temp) ) + (((ulong)time_ns)>>24) - (ulong)nonce) & \
+                           0x7FFFFFFFUL ) <= ALLOWED_TIME_DELTA ) )                                                         \
+                             return 1;                                                                                      \
                        } while( 0 )
 
 
