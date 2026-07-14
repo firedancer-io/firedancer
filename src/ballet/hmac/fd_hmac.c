2ebdf437594ddb6532f7881a898b2a15a2060af4
David Rubin <drubin@jumptrading.com>
2026-07-14 04:16:21 +0000

    ballet/hmac: add HKDF-Expand-Label

--- src/ballet/hmac/fd_hmac.c
+++ src/ballet/hmac/fd_hmac.c
@@ -17,3 +17,51 @@
 #define HASH_BLOCK_SZ FD_SHA512_BLOCK_SZ
 #define HASH_SZ       FD_SHA512_HASH_SZ
 #include "fd_hmac_tmpl.c"
+
+void *
+fd_hkdf_expand_label( uchar *       out,
+                      ulong         out_sz,
+                      uchar const * secret,
+                      ulong         secret_sz,
+                      char const *  label,
+                      ulong         label_sz,
+                      uchar const * context,
+                      ulong         context_sz,
+                      fd_hmac_fn_t  hmac_fn ) {
+
+# define LABEL_BUFSZ (64UL)
+
+  uchar info[ 2+1+6+LABEL_BUFSZ+1+LABEL_BUFSZ+1 ];
+  ulong info_sz = 0UL;
+
+  /* Length of output */
+  info[0] = 0;
+  info[1] = (uchar)out_sz;
+  info_sz += 2UL;
+
+  /* Label: length-prefixed "tls13 " + label */
+  info[ info_sz ] = (uchar)( 6UL + label_sz );
+  info_sz += 1UL;
+  memcpy( info+info_sz, "tls13 ", 6UL );
+  info_sz += 6UL;
+  memcpy( info+info_sz, label, label_sz );
+  info_sz += label_sz;
+
+  /* Context: length-prefixed context bytes */
+  info[ info_sz ] = (uchar)context_sz;
+  info_sz += 1UL;
+  fd_memcpy( info+info_sz, context, context_sz );
+  info_sz += context_sz;
+
+  /* HKDF-Expand counter=1 */
+  info[ info_sz ] = 0x01;
+  info_sz += 1UL;
+
+  /* PRF = HMAC(secret, info) */
+  uchar hash[ 48 ];
+  hmac_fn( info, info_sz, secret, secret_sz, hash );
+  fd_memcpy( out, hash, out_sz );
+  return out;
+
+# undef LABEL_BUFSZ
+}
