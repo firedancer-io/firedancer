58a7750585f49553bcbbd093c81e937a16ddfb69
David Rubin <drubin@jumptrading.com>
2026-07-14 05:34:44 +0000

    x509: better DER parser

--- src/ballet/x509/Local.mk
+++ src/ballet/x509/Local.mk
@@ -1,5 +1,8 @@
-$(call add-hdrs,fd_x509_mock.h)
+$(call add-hdrs,fd_x509_mock.h fd_der.h)
 $(call add-objs,fd_x509_mock,fd_ballet)
+
+$(call make-unit-test,test_der,test_der,fd_ballet fd_util)
+$(call run-unit-test,test_der)
 
 $(call make-unit-test,test_x509,test_x509,fd_ballet fd_util)
 $(call run-unit-test,test_x509)
