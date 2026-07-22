3f0406e83aae225a8aa6cdbac6ee3f171acaedc3
Emanuele Cesena <ecesena@jumptrading.com>
2026-07-22 14:53:11 +0000

    build: remove `-ffast-math` and set `-ffp-contract=off`

--- config/extra/with-optimization.mk
+++ config/extra/with-optimization.mk
@@ -1,9 +1,9 @@
 ifeq ($(FD_DISABLE_OPTIMIZATION),)
-CPPFLAGS+=-O3 -ffast-math -fno-associative-math -fno-reciprocal-math
+CPPFLAGS+=-O3 -ffp-contract=off -fno-math-errno
 CPPFLAGS+=-DFD_HAS_OPTIMIZATION=1
 FD_HAS_OPTIMIZATION:=1
 else
-CPPFLAGS+=-O0 -ffast-math -fno-associative-math -fno-reciprocal-math
+CPPFLAGS+=-O0 -ffp-contract=off -fno-math-errno
 endif
 
 RUST_PROFILE:=release-with-debug
