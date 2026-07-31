1c9716c672b9857c47d6a8baa6e37bb2211bfdd7
Tom Pointon <topointon@jumptrading.com>
2026-07-31 20:26:58 +0000

    fix keyguard match for txnv1

--- src/disco/keyguard/test_keyguard.c
+++ src/disco/keyguard/test_keyguard.c
@@ -1,4 +1,112 @@
 #include "fd_keyguard.h"
+#include "../../ballet/txn/fd_txn.h"
+
+static uchar v1_buf [ FD_TXN_MTU    ];
+static uchar v1_txn [ FD_TXN_MAX_SZ ];
+
+static ulong
+build_txn_v1( uchar * buf,
+              ulong   sig_cnt,
+              ulong   num_addr,
+              ulong   instr_cnt,
+              ulong * msg_sz ) {
+  ulong o = 0UL;
+  buf[ o++ ] = (uchar)0x81;            /* version byte: MESSAGE_VERSION_PREFIX | 1 */
+  buf[ o++ ] = (uchar)sig_cnt;
+  buf[ o++ ] = (uchar)fd_ulong_max( sig_cnt, 1UL )-1UL;          /* ro signed cnt */
+  buf[ o++ ] = (uchar)0;               /* ro unsigned cnt */
+  for( ulong j=0UL; j< 4UL; j++ ) buf[ o++ ] = (uchar)0;         /* config mask */
+  for( ulong j=0UL; j<32UL; j++ ) buf[ o++ ] = (uchar)(0xB0+j);  /* blockhash */
+  buf[ o++ ] = (uchar)instr_cnt;
+  buf[ o++ ] = (uchar)num_addr;
+  for( ulong a=0UL; a<num_addr; a++ )
+    for( ulong j=0UL; j<32UL; j++ ) buf[ o++ ] = (uchar)(a*32UL+j);
+  /* instruction headers: program id 1, no accounts, no data */
+  for( ulong x=0UL; x<instr_cnt; x++ ) {
+    buf[ o++ ] = (uchar)1;  /* program id */
+    buf[ o++ ] = (uchar)0;  /* acct cnt */
+    buf[ o++ ] = (uchar)0;  /* data sz lo */
+    buf[ o++ ] = (uchar)0;  /* data sz hi */
+  }
+
+  *msg_sz = o;  /* v1 signs payload[0,signature_off) */
+
+  for( ulong s=0UL; s<sig_cnt; s++ )
+    for( ulong j=0UL; j<64UL; j++ ) buf[ o++ ] = (uchar)(0x40+s);
+  return o;
+}
+
+/* Every v1 txn that fd_txn_parse accepts must fingerprint as a txn.  A
+   false negative would let the keyguard sign a transaction under
+   another payload type's authorization rules.  Sweep every header
+   field the keyguard inspects across its limit. */
+
+void
+test_txn_v1_match( void ) {
+  fd_txn_t *  parsed          = (fd_txn_t *)v1_txn;
+  ulong const instr_cnts[ 4 ] = { 0UL, 1UL, FD_TXN_INSTR_MAX, FD_TXN_INSTR_MAX+1UL };
+  ulong       matched         = 0UL;
+
+  for( ulong sig_cnt=0UL; sig_cnt<=FD_TXN_SIG_MAX+8UL; sig_cnt++ ) {
+    for( ulong num_addr=0UL; num_addr<=FD_TXN_ACCT_ADDR_MAX+6UL; num_addr++ ) {
+      for( ulong k=0UL; k<4UL; k++ ) {
+        ulong msg_sz;
+        ulong sz = build_txn_v1( v1_buf, sig_cnt, num_addr, instr_cnts[ k ], &msg_sz );
+        if( !fd_txn_parse( v1_buf, sz, v1_txn, NULL ) ) continue;
+
+        FD_TEST( parsed->transaction_version    ==FD_TXN_V1 );
+        FD_TEST( parsed->message_off            ==0         );
+        FD_TEST( fd_txn_msg_sz( parsed, sz )    ==msg_sz    );
+
+        FD_TEST( fd_keyguard_payload_match( v1_buf, msg_sz, FD_KEYGUARD_SIGN_TYPE_ED25519 )
+                 ==FD_KEYGUARD_PAYLOAD_TXN );
+        matched++;
+      }
+    }
+  }
+
+  /* Guard against the sweep passing vacuously */
+  FD_TEST( matched>1000UL );
+
+  /* Payloads that are not v1 txn msgs */
+
+  ulong msg_sz;
+  build_txn_v1( v1_buf, 1UL, 1UL, 0UL, &msg_sz );
+
+  /* Undersized */
+  FD_TEST( !fd_keyguard_payload_match( v1_buf, 68UL, FD_KEYGUARD_SIGN_TYPE_ED25519 ) );
+
+  /* Not a raw Ed25519 signing request */
+  FD_TEST( !fd_keyguard_payload_match( v1_buf, msg_sz, FD_KEYGUARD_SIGN_TYPE_SHA256_ED25519        ) );
+  FD_TEST( !fd_keyguard_payload_match( v1_buf, msg_sz, FD_KEYGUARD_SIGN_TYPE_PUBKEY_CONCAT_ED25519 ) );
+
+  /* Unrecognized version */
+  v1_buf[  0 ] = (uchar)0x82;
+  FD_TEST( !fd_keyguard_payload_match( v1_buf, msg_sz, FD_KEYGUARD_SIGN_TYPE_ED25519 ) );
+  v1_buf[  0 ] = (uchar)0x81;
+
+  /* No signatures */
+  v1_buf[  1 ] = (uchar)0;
+  FD_TEST( !fd_keyguard_payload_match( v1_buf, msg_sz, FD_KEYGUARD_SIGN_TYPE_ED25519 ) );
+  v1_buf[  1 ] = (uchar)1;
+
+  /* Too many instructions */
+  v1_buf[ 40 ] = (uchar)( FD_TXN_INSTR_MAX+1UL );
+  FD_TEST( !fd_keyguard_payload_match( v1_buf, msg_sz, FD_KEYGUARD_SIGN_TYPE_ED25519 ) );
+  v1_buf[ 40 ] = (uchar)0;
+
+  /* Too many addresses */
+  v1_buf[ 41 ] = (uchar)( FD_TXN_ACCT_ADDR_MAX+1UL );
+  FD_TEST( !fd_keyguard_payload_match( v1_buf, msg_sz, FD_KEYGUARD_SIGN_TYPE_ED25519 ) );
+
+  /* More signers than addresses */
+  v1_buf[ 41 ] = (uchar)0;
+  FD_TEST( !fd_keyguard_payload_match( v1_buf, msg_sz, FD_KEYGUARD_SIGN_TYPE_ED25519 ) );
+  v1_buf[ 41 ] = (uchar)1;
+
+  FD_TEST( fd_keyguard_payload_match( v1_buf, msg_sz, FD_KEYGUARD_SIGN_TYPE_ED25519 )
+           ==FD_KEYGUARD_PAYLOAD_TXN );
+}
 
 void
 test_vote_txn_oob( void ) {
@@ -50,6 +158,7 @@ main( int     argc,
       char ** argv ) {
   fd_log_private_boot( &argc, &argv );
   test_vote_txn_oob();
+  test_txn_v1_match();
   FD_LOG_NOTICE(( "pass" ));
   return 0;
 }
