d8240500206f5601bea7b7ec89667287eee7e539
ibhatt-jumptrading <ibhatt@jumptrading.com>
2026-08-07 16:31:48 +0000

    wip

--- src/flamenco/stakes/fd_vote_stakes.h
+++ src/flamenco/stakes/fd_vote_stakes.h
@@ -7,7 +7,33 @@
 
 #define FD_VOTE_STAKES_ALIGN (128UL)
 
-/* This system assumes that multiple forks can cross an epoch boundary
+/* fd_vote_stakes is the Firedancer equivalent of the Agave epoch stakes
+   data structure.  It is a fork aware data structure that maintains the
+   vote account state that is used for clock and VM syscall calculations
+   during execution.
+
+   The underlying data structure is a tiered set of caches:
+   - t-2/t-3: these are caches used for the vote account states at the
+     end of the t-2 epoch and the t-3 epoch assuming you are currently
+     in the t epoch.  These caches are shared across all forks.
+   - t-1: these are sized to max_fork_width and are computed at the
+     most recent epoch boundary.  These caches are ref-cnt'd and fork
+     specific.  After the epoch boundary slot is rooted, then there will
+     only be 1 active t-1 cache.
+   - state: each bank has it's own view of the t-2 state of vote
+     accounts.  This is what is actually used for clock calculations
+     which is a stake weighted median of the last vote slot and
+     timestamp.
+
+   fd_vote_stakes also handles the creation of new vote stakes caches.
+   The creation of new vote stakes caches happens at the epoch boundary
+   and follows the rules defined in the validator_admission_ticket
+   feature where every vote account must be in the top 2000 by stake.
+   Under the hood, this is managed by a stake-sorted heap that is shared
+   across all forks.
+
+   NOTE:
+   This system assumes that multiple forks can cross an epoch boundary
    but only one epoch boundary can be crossed at a time.  This means
    that there can be multiple t-1 forks after an epoch boundary, but
    only one will be active when the first fork crosses into an epoch
@@ -35,15 +61,26 @@ fd_vote_stakes_new( void * mem,
 fd_vote_stakes_t *
 fd_vote_stakes_join( void * mem );
 
+/* fd_vote_stakes_fork_epoch returns the epoch of the fork. */
+
 ulong
 fd_vote_stakes_fork_epoch( ulong fork_id );
+
+/* fd_vote_stakes_init initializes the vote stakes data structure for a
+   given epoch. */
 
 ulong
 fd_vote_stakes_init( fd_vote_stakes_t * vote_stakes,
                      ulong              epoch );
 
+/* fd_vote_stakes_reset resets the vote stakes data structure. */
+
 void
 fd_vote_stakes_reset( fd_vote_stakes_t * vote_stakes );
+
+/* fd_vote_stakes_snap_insert_t_1 inserts a new vote account into the
+   t-1 set.  This skips over any vote acccount validation and should
+   only be used when loading in vote accounts from a snapshot. */
 
 void
 fd_vote_stakes_snap_insert_t_1( fd_vote_stakes_t *  vote_stakes,
@@ -52,6 +89,10 @@ fd_vote_stakes_snap_insert_t_1( fd_vote_stakes_t *  vote_stakes,
                                 fd_pubkey_t const * node_account,
                                 ulong               stake,
                                 ushort              commission );
+
+/* fd_vote_stakes_snap_insert_t_2 inserts a new vote account into the
+   t-2 set.  This skips over any vote acccount validation and should
+   only be used when loading in vote accounts from a snapshot. */
 
 void
 fd_vote_stakes_snap_insert_t_2( fd_vote_stakes_t *  vote_stakes,
@@ -63,7 +104,9 @@ fd_vote_stakes_snap_insert_t_2( fd_vote_stakes_t *  vote_stakes,
 
 /* fd_vote_stakes_insert inserts a new vote account into the t-1 set.
    This can be called repeatedly after a boundary-crossing
-   fd_vote_stakes_new_fork. */
+   fd_vote_stakes_new_fork.  An account will only be inserted if it
+   matches the semantics of the validator_admission_ticket feature.
+   The top 2000 vote accounts by stake are inserted into the t-1 set. */
 
 void
 fd_vote_stakes_insert( fd_vote_stakes_t *  vote_stakes,
@@ -145,6 +188,9 @@ fd_vote_stakes_query_t_3( fd_vote_stakes_t const * vote_stakes,
                           ulong *                  stake_out_opt,
                           ushort *                 commission_out_opt );
 
+/* fd_vote_stakes_cnt_t_{1,2} returns the number of vote accounts in the
+   t-1 and t-2 sets respectively. */
+
 ulong
 fd_vote_stakes_cnt_t_1( fd_vote_stakes_t const * vote_stakes,
                         ulong                    fork_id );
@@ -152,6 +198,12 @@ fd_vote_stakes_cnt_t_1( fd_vote_stakes_t const * vote_stakes,
 ulong
 fd_vote_stakes_cnt_t_2( fd_vote_stakes_t const * vote_stakes,
                         ulong                    fork_id );
+
+/* Defined below are the iterators for the t-1 and t-2 sets.  These
+   iterators will NOT skip over invalid vote accounts intentionally.
+   The reason being is that invalid vote accounts are still considered
+   for stake and leader calculations: the caller is expected to handle
+   the state of each account correctly. */
 
 #define FD_VOTE_STAKES_T_1_ITER_FOOTPRINT (16UL)
 #define FD_VOTE_STAKES_T_1_ITER_ALIGN     (8UL)
