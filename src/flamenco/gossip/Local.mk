9894e8ebc86b60c4238db2e26cbd133166885d8c
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2026-08-11 16:08:28 -0700

    gossip: check purged iterator sentinel before lookup

--- src/flamenco/gossip/Local.mk
+++ src/flamenco/gossip/Local.mk
@@ -16,6 +16,9 @@ $(call run-unit-test,test_ping_tracker)
 $(call make-unit-test,test_gossip_wsample,test_gossip_wsample,fd_flamenco fd_ballet fd_util)
 $(call run-unit-test,test_gossip_wsample)
 
+$(call make-unit-test,test_gossip_purged,test_gossip_purged,fd_flamenco fd_ballet fd_util)
+$(call run-unit-test,test_gossip_purged)
+
 ifdef FD_HAS_HOSTED
 $(call make-fuzz-test,fuzz_gossip_message_serialize,fuzz_gossip_message_serialize,fd_flamenco fd_ballet fd_util)
 endif
