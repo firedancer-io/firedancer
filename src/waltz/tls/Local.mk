f1fa5faf003125e341494304859faa88f926d803
David Rubin <drubin@jumptrading.com>
2026-08-21 12:36:51 +0000

    remove OpenSSL support

--- src/waltz/tls/Local.mk
+++ src/waltz/tls/Local.mk
@@ -9,8 +9,3 @@ ifdef FD_HAS_HOSTED
 $(call make-fuzz-test,fuzz_tls,fuzz_tls,fd_tls fd_ballet fd_util)
 $(call make-fuzz-test,fuzz_tls_msg_parser,fuzz_tls_msg_parser,fd_tls fd_ballet fd_util)
 endif
-
-ifdef FD_HAS_OPENSSL
-$(call make-unit-test,test_tls_openssl,test_tls_openssl,fd_quic fd_tls fd_ballet fd_util,-lssl -lcrypto)
-$(call run-unit-test,test_tls_openssl)
-endif
