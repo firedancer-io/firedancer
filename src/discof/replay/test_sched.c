2d14aa85f3cc3a59e6a3492de6f4db8277e7383b
Ishan Bhatt <ibhatt@jumptrading.com>
2026-08-24 20:07:11 +0000

    sched: reduce per bank txn pool

--- src/discof/replay/test_sched.c
+++ src/discof/replay/test_sched.c
@@ -12,7 +12,7 @@
 
 static void
 test_sched_footprint( void ) {
-  FD_TEST( fd_sched_footprint( 512UL, 4UL )==14054016UL );
+  FD_TEST( fd_sched_footprint( 512UL, 4UL )==12485376UL );
 }
 
 static void
