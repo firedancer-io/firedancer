d6281b35dcd89a12c3d30bb2c16dc220beb0d286
Ishan Bhatt <ibhatt@jumptrading.com>
2026-08-25 20:32:30 +0000

    ci: update end slots

--- src/flamenco/runtime/tests/run_backtest_ci.sh
+++ src/flamenco/runtime/tests/run_backtest_ci.sh
@@ -29,7 +29,6 @@ src/flamenco/runtime/tests/run_ledger_backtest.sh -l genesis-vat-v4.2.0-beta.1-v
 src/flamenco/runtime/tests/run_ledger_backtest.sh -l breakpoint-385786458-v4.2.0-beta.1-vat -m 2000000 -e 385786452
 src/flamenco/runtime/tests/run_ledger_backtest.sh -l vote-states-v4-local-v4.2.0-beta.1-vat -m 3000 -e 1000
 src/flamenco/runtime/tests/run_ledger_backtest.sh -l testnet-384169347-v4.2.0-beta.1-vat -m 2000000 -e 384169377 --root-distance 32 --max-live-slots 64
-src/flamenco/runtime/tests/run_ledger_backtest.sh -l testnet-384395810-v4.2.0-beta.1-vat -m 2000000 -e 384395820
 src/flamenco/runtime/tests/run_ledger_backtest.sh -l testnet-387596258-v4.2.0-beta.1-vat -m 2000000 -e 387596373
 src/flamenco/runtime/tests/run_ledger_backtest.sh -l deployment-before-boundary-sha512-2-v4.2.0-beta.1-vat -m 1000 -e 75
 src/flamenco/runtime/tests/run_ledger_backtest.sh -l mainnet-391824000-boundary-v4.2.0-beta.1-vat -m 2000000 -e 391824016
