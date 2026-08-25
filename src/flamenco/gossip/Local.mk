3cb3a2d3959ee3735c5c7cdaa9fd8bc4a3bbfb83
Richard Patel <ripatel@jumptrading.com>
2026-08-25 20:09:08 +0000

    gossip: remove obsolete agave fuzzer patch
    Agave now has upstream gossip conformance support.

--- src/flamenco/gossip/Local.mk
+++ src/flamenco/gossip/Local.mk
@@ -22,39 +22,3 @@ $(call run-unit-test,test_gossip_purged)
 ifdef FD_HAS_HOSTED
 $(call make-fuzz-test,fuzz_gossip_message_serialize,fuzz_gossip_message_serialize,fd_flamenco fd_ballet fd_util)
 endif
-
-# TODO: This is a differential fuzzer that compares the output of the C
-# implementation to the Rust implementation.  It requires a small shim
-# to the Rust side to work, since Agave does not expose the gossip
-# deserializer well enough by default.
-#
-# See: differential_fuzzer.patch for the diff which is needed on the
-# Agave side for this to work properly.
-#
-# ifdef FD_HAS_HOSTED
-# $(call make-fuzz-test,fuzz_gossip_message_deserialize,fuzz_gossip_message_deserialize,fd_flamenco fd_ballet fd_util solana_gossip,-lstdc++ -lpthread)
-# $(call make-unit-test,bench_gossip_message_deserialize,bench_gossip_message_deserialize,fd_flamenco fd_ballet fd_util solana_gossip,-lstdc++ -lpthread)
-# endif
-
-GOSSIP_FUZZ_RUSTFLAGS:=
-ifdef FD_HAS_FUZZ
-GOSSIP_FUZZ_RUSTFLAGS+=-Cpasses=sancov-module
-GOSSIP_FUZZ_RUSTFLAGS+=-Cllvm-args=-sanitizer-coverage-inline-8bit-counters
-GOSSIP_FUZZ_RUSTFLAGS+=-Cllvm-args=-sanitizer-coverage-level=4
-GOSSIP_FUZZ_RUSTFLAGS+=-Cllvm-args=-sanitizer-coverage-pc-table
-endif
-GOSSIP_FUZZ_RUSTFLAGS+=-Clink-dead-code
-GOSSIP_FUZZ_RUSTFLAGS+=-Cforce-frame-pointers=yes
-GOSSIP_FUZZ_RUSTFLAGS+=-Awarnings
-
-GOSSIP_FUZZ_RUST_VERSION:=1.86.0
-
-.PHONY: gossip-fuzz-rustlib
-
-gossip-fuzz-rustlib:
-	cd agave && RUSTFLAGS="$(GOSSIP_FUZZ_RUSTFLAGS)" cargo +$(GOSSIP_FUZZ_RUST_VERSION) build -p solana-gossip --lib --target x86_64-unknown-linux-gnu --release
-
-agave/target/x86_64-unknown-linux-gnu/release/libsolana_gossip.a: gossip-fuzz-rustlib
-
-$(OBJDIR)/lib/libsolana_gossip.a: agave/target/x86_64-unknown-linux-gnu/release/libsolana_gossip.a
-	$(MKDIR) $(dir $@) && cp $< $@
