f11a94dfc3abd1edefee594ca518c9d5240f4fb2
Richard Patel <ripatel@jumptrading.com>
2026-08-28 22:13:41 +0000

    zle: AVX-512BW backend

--- src/flamenco/accdb/fd_zle.c
+++ src/flamenco/accdb/fd_zle.c
@@ -1,5 +1,9 @@
 #include "fd_zle.h"
 
+#if FD_HAS_AVX512
+#include <immintrin.h>
+#endif
+
 /* Wire format: a stream of frames, each producing L literal bytes then
    Z zero bytes.
 
@@ -66,6 +70,50 @@ fd_zle_frame_sz( ulong L,
   return 1UL + fd_zle_ext( L ) + L + fd_zle_ext( Z );
 }
 
+#if FD_HAS_AVX512
+
+/* fd_zle_zmask returns the zero-byte mask of the 64 byte word at
+   src+(w<<6).  Bits at positions >=n read as non-zero so runs terminate
+   at n. */
+
+static inline ulong
+fd_zle_zmask( uchar const * s,
+              ulong         w,
+              ulong         n ) {
+  ulong off = w<<6;
+  if( FD_LIKELY( off+64UL<=n ) )
+    return _cvtmask64_u64( _mm512_cmpeq_epi8_mask( _mm512_loadu_si512( (void const *)( s+off ) ), _mm512_setzero_si512() ) );
+  ulong   tail = ~0UL>>( 64UL-( n-off ) );
+  __m512i v    = _mm512_maskz_loadu_epi8( _cvtu64_mask64( tail ), s+off );
+  return _cvtmask64_u64( _mm512_cmpeq_epi8_mask( v, _mm512_setzero_si512() ) ) & tail;
+}
+
+/* exact-length copy: full 64 B vectors + one masked tail */
+
+static inline void
+fd_zle_copy( uchar *       FD_RESTRICT d,
+             uchar const * FD_RESTRICT s,
+             ulong                     sz ) {
+  ulong k = 0UL;
+  for( ; k+64UL<=sz; k+=64UL )
+    _mm512_storeu_si512( (void *)( d+k ), _mm512_loadu_si512( (void const *)( s+k ) ) );
+  if( k<sz ) {
+    __mmask64 m = _cvtu64_mask64( ~0UL>>( 64UL-( sz-k ) ) );
+    _mm512_mask_storeu_epi8( (void *)( d+k ), m, _mm512_maskz_loadu_epi8( m, s+k ) );
+  }
+}
+
+#else
+
+static inline void
+fd_zle_copy( uchar * FD_RESTRICT       d,
+             uchar const * FD_RESTRICT s,
+             ulong                     sz ) {
+  if( sz ) fd_memcpy( d, s, sz );
+}
+
+#endif
+
 static uchar *
 fd_zle_emit( uchar *                   o,
              uchar const * FD_RESTRICT lit,
@@ -75,11 +123,113 @@ fd_zle_emit( uchar *                   o,
   ulong zn = Z<15UL ? Z : 15UL;
   *o++ = (uchar)( (ln<<4) | zn );
   if( L>=15UL ) o += fd_zle_vput( o, L-15UL );
-  if( L       ) fd_memcpy( o, lit, L );
+  fd_zle_copy( o, lit, L );
   o += L;
   if( Z>=15UL ) o += fd_zle_vput( o, Z-15UL );
   return o;
 }
+
+#if FD_HAS_AVX512
+
+/* extend a zero run beyond word *pw; leaves *pw and *pm at the run end */
+
+static inline ulong
+fd_zle_spill( uchar const * s,
+              ulong         n,
+              ulong         words,
+              ulong *       pw,
+              ulong *       pm,
+              ulong         len ) {
+  ulong w = *pw+1UL;
+  while( ( w<<6 )+256UL<=n ) {
+    ulong m0 = fd_zle_zmask( s, w,     n );
+    ulong m1 = fd_zle_zmask( s, w+1UL, n );
+    ulong m2 = fd_zle_zmask( s, w+2UL, n );
+    ulong m3 = fd_zle_zmask( s, w+3UL, n );
+    if( ( m0&m1&m2&m3 )!=~0UL ) break;
+    w += 4UL; len += 256UL;
+  }
+  for(;;) {
+    if( w>=words ) { *pw = w; *pm = 0UL; return len; }
+    ulong t = fd_zle_zmask( s, w, n );
+    if( t!=~0UL ) {
+      ulong ext = (ulong)__builtin_ctzll( ~t );
+      *pw = w;
+      *pm = t & ~( ( 1UL<<ext )-1UL );
+      return len+ext;
+    }
+    w++; len += 64UL;
+  }
+}
+
+ulong
+fd_zle_compress( void *       FD_RESTRICT comp,
+                 void const * FD_RESTRICT data,
+                 ulong                    data_sz ) {
+  uchar *       dst = (uchar *)comp;
+  uchar const * src = (uchar const *)data;
+  ulong         n   = data_sz;
+
+  if( FD_UNLIKELY( !n ) ) return 0UL;
+
+  ulong esc_sz = 1UL + fd_zle_ext( n ) + n;
+  ulong words  = ( n+63UL )>>6;
+
+  uchar * o  = dst;
+  ulong   fs = 0UL;
+  ulong   w  = 0UL;
+  ulong   m  = fd_zle_zmask( src, 0UL, n );
+  for(;;) {
+    while( !m ) {
+      w++;
+      while( ( w<<6 )+256UL<=n ) {
+        ulong m0 = fd_zle_zmask( src, w,     n );
+        ulong m1 = fd_zle_zmask( src, w+1UL, n );
+        ulong m2 = fd_zle_zmask( src, w+2UL, n );
+        ulong m3 = fd_zle_zmask( src, w+3UL, n );
+        if( m0|m1|m2|m3 ) break;
+        w += 4UL;
+      }
+      while( w<words && !( m = fd_zle_zmask( src, w, n ) ) ) w++;
+      if( w>=words ) goto trailing;
+    }
+    ulong b  = (ulong)__builtin_ctzll( m );
+    ulong zs = ( w<<6 )+b;
+    ulong hi = m>>b;
+    ulong len;
+    if( ~hi ) {
+      len = (ulong)__builtin_ctzll( ~hi );
+      m &= ~( ( ( 1UL<<len )-1UL )<<b );
+      if( b+len==64UL ) len = fd_zle_spill( src, n, words, &w, &m, len );
+    } else {                             /* run fills rest of word */
+      m   = 0UL;
+      len = fd_zle_spill( src, n, words, &w, &m, 64UL-b );
+    }
+    if( len>=FD_ZLE_MIN_Z ) {
+      ulong L   = zs-fs;
+      ulong cur = (ulong)( o-dst );
+      if( FD_UNLIKELY( cur+L+15UL>=esc_sz ) &&
+          cur+fd_zle_frame_sz( L, len )>=esc_sz ) goto escape;
+      o  = fd_zle_emit( o, src+fs, L, len );
+      fs = zs+len;
+    }
+    if( !m && w>=words ) break;
+  }
+trailing:
+  if( fs<n ) {
+    ulong L   = n-fs;
+    ulong cur = (ulong)( o-dst );
+    if( FD_UNLIKELY( cur+L+15UL>=esc_sz ) &&
+        cur+fd_zle_frame_sz( L, 0UL )>=esc_sz ) goto escape;
+    o = fd_zle_emit( o, src+fs, L, 0UL );
+  }
+  return (ulong)( o-dst );
+
+escape:
+  return (ulong)( fd_zle_emit( dst, src, n, 0UL )-dst );
+}
+
+#else
 
 ulong
 fd_zle_compress( void *       FD_RESTRICT comp,
@@ -125,6 +275,8 @@ escape:
   return (ulong)( fd_zle_emit( dst, src, n, 0UL )-dst );
 }
 
+#endif /* FD_HAS_AVX512 */
+
 long
 fd_zle_decompress( void *       FD_RESTRICT data,
                    ulong                    data_sz,
@@ -146,7 +298,7 @@ fd_zle_decompress( void *       FD_RESTRICT data,
     }
     if( FD_UNLIKELY( L>comp_sz-i ) ) return FD_ZLE_ERR_CORRUPT;
     if( FD_UNLIKELY( L>data_sz-o ) ) return FD_ZLE_ERR_SPACE;
-    fd_memcpy( d+o, s+i, L );
+    fd_zle_copy( d+o, s+i, L );
     i += L;
     o += L;
     if( Z==15UL ) {
