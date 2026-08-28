36a5983437b913d46a8a3c1b3d55f1c629c41aa3
Richard Patel <ripatel@jumptrading.com>
2026-08-28 00:41:54 +0000

    fix off-by-one in fd_ip4_udp_hdr_strip

--- src/util/net/test_udp.c
+++ src/util/net/test_udp.c
@@ -23,10 +23,39 @@ main( int     argc,
   FD_TEST( (ulong)( &(((fd_udp_hdr_t *)NULL)->check    ) )==6UL );
   FD_TEST( (ulong)(  (((fd_udp_hdr_t *)NULL)->uc       ) )==0UL );
 
+  FD_TEST( !fd_ip4_udp_hdr_strip( NULL, 0UL, NULL, NULL, NULL, NULL, NULL ) );
+
+  uchar short_packet[ sizeof(fd_eth_hdr_t) ] = {0};
+  FD_TEST( !fd_ip4_udp_hdr_strip( short_packet, sizeof(short_packet), NULL, NULL, NULL, NULL, NULL ) );
+
+  fd_ip4_udp_hdrs_t malformed[1];
+  for( uint ihl=0U; ihl<5U; ihl++ ) {
+    memset( malformed, 0, sizeof(malformed) );
+    fd_ip4_hdr_t * ip4 = malformed->ip4;
+    ip4->verihl = FD_IP4_VERIHL( 4U, ihl );
+    fd_udp_hdr_t * udp = (fd_udp_hdr_t *)( malformed->uc + sizeof(fd_eth_hdr_t) + 4UL*ihl );
+    udp->net_len = fd_ushort_bswap( (ushort)sizeof(fd_udp_hdr_t) );
+    FD_TEST( !fd_ip4_udp_hdr_strip( malformed->uc, sizeof(malformed->uc), NULL, NULL, NULL, NULL, NULL ) );
+  }
+
+  fd_ip4_udp_hdrs_t hdrs[1];
+  fd_ip4_udp_hdr_init( hdrs, 0UL, 0U, 0U );
+
+  uchar *        payload    = NULL;
+  ulong          payload_sz = ULONG_MAX;
+  fd_eth_hdr_t * eth        = NULL;
+  fd_ip4_hdr_t * ip4        = NULL;
+  fd_udp_hdr_t * udp        = NULL;
+  FD_TEST( fd_ip4_udp_hdr_strip( hdrs->uc, sizeof(hdrs->uc), &payload, &payload_sz, &eth, &ip4, &udp ) );
+  FD_TEST( payload==(hdrs->uc + sizeof(hdrs->uc)) );
+  FD_TEST( !payload_sz );
+  FD_TEST( eth==hdrs->eth );
+  FD_TEST( ip4==hdrs->ip4 );
+  FD_TEST( udp==hdrs->udp );
+
   /* FIXME: TEST FD_IP4_UDP_CHECK */
 
   FD_LOG_NOTICE(( "pass" ));
   fd_halt();
   return 0;
 }
-
