4a562fbc4657140d09ac8f9ec4b2ff70b35e60b2
Valentyn Kit <valentynkit.work@pm.me>
2026-08-31 20:11:23 +0300

    sbpf, vm: cover unaligned program text
    Pins the parity half: an ELF with .text at an odd address loads, and
    the VM validates, executes, disassembles, and traces it to the same
    result as from an aligned buffer.

--- src/ballet/sbpf/test_sbpf_loader.c
+++ src/ballet/sbpf/test_sbpf_loader.c
@@ -93,6 +93,84 @@ void test_zero_text_cnt( void ) {
   FD_TEST( prog->entry_pc==0UL );
 }
 
+/* .text at an odd sh_addr.  The lenient path takes text_off straight
+   from sh_addr, so prog->text lands misaligned; Agave accepts these,
+   so this must load. */
+
+#define TEXT_ADDR (321UL)  /* == sh_offset, deliberately odd */
+#define TEXT_SZ    (16UL)  /* mov64 r0, 1 ; exit */
+#define SHOFF     (128UL)
+#define SHSTRTAB  "\0.text\0.shstrtab"
+
+static ulong
+build_unaligned_text_elf( uchar bin[ 512 ] ) {
+  ulong shstrtab_off = TEXT_ADDR + TEXT_SZ;
+  fd_memset( bin, 0, 512UL );
+
+  fd_elf64_ehdr ehdr = {
+    .e_ident     = { 0x7f, 'E', 'L', 'F', FD_ELF_CLASS_64, FD_ELF_DATA_LE, 1, FD_ELF_OSABI_NONE },
+    .e_type      = FD_ELF_ET_DYN,
+    .e_machine   = FD_ELF_EM_BPF,
+    .e_version   = 1,
+    .e_entry     = TEXT_ADDR,
+    .e_shoff     = SHOFF,
+    .e_flags     = FD_SBPF_V0,
+    .e_ehsize    = sizeof(fd_elf64_ehdr),
+    .e_phentsize = sizeof(fd_elf64_phdr),
+    .e_shentsize = sizeof(fd_elf64_shdr),
+    .e_shnum     = 3,
+    .e_shstrndx  = 2
+  };
+  fd_elf64_shdr shdr[3] = {
+    { 0 },
+    { .sh_name = 1U, .sh_type = FD_ELF_SHT_PROGBITS,           /* .text */
+      .sh_flags = FD_ELF_SHF_ALLOC | FD_ELF_SHF_EXECINSTR,
+      .sh_addr = TEXT_ADDR, .sh_offset = TEXT_ADDR, .sh_size = TEXT_SZ, .sh_addralign = 1UL },
+    { .sh_name = 7U, .sh_type = FD_ELF_SHT_STRTAB,             /* .shstrtab */
+      .sh_offset = shstrtab_off, .sh_size = sizeof(SHSTRTAB), .sh_addralign = 1UL }
+  };
+  uchar const text[ TEXT_SZ ] = {
+    0xb7, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,  /* mov64 r0, 1 */
+    0x95, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00   /* exit        */
+  };
+
+  fd_memcpy( bin,                   &ehdr, sizeof(ehdr)     );
+  fd_memcpy( bin + SHOFF,            shdr, sizeof(shdr)     );
+  fd_memcpy( bin + TEXT_ADDR,        text, sizeof(text)     );
+  fd_memcpy( bin + shstrtab_off, SHSTRTAB, sizeof(SHSTRTAB) );
+
+  return shstrtab_off + sizeof(SHSTRTAB);
+}
+
+void test_unaligned_text( void ) {
+  fd_scratch_push();
+  fd_sbpf_elf_info_t info;
+
+  uchar bin[ 512 ];
+  ulong bin_sz = build_unaligned_text_elf( bin );
+
+  fd_sbpf_loader_config_t config = { 0 };
+  config.elf_deploy_checks = 1;
+  config.sbpf_min_version = FD_SBPF_V0;
+  config.sbpf_max_version = FD_SBPF_V3;
+
+  FD_TEST( fd_sbpf_elf_peek( &info, bin, bin_sz, &config )==FD_SBPF_ELF_SUCCESS );
+  FD_TEST( info.text_off==TEXT_ADDR );
+
+  void * rodata = fd_scratch_alloc( FD_SBPF_PROG_RODATA_ALIGN, info.bin_sz );
+  FD_TEST( rodata );
+
+  fd_sbpf_program_t * prog = fd_sbpf_program_new( fd_scratch_alloc( fd_sbpf_program_align(), fd_sbpf_program_footprint( &info ) ), &info, rodata );
+
+  fd_sbpf_syscalls_t * syscalls = fd_sbpf_syscalls_new( fd_scratch_alloc( fd_sbpf_syscalls_align(), fd_sbpf_syscalls_footprint() ) );
+
+  void * scratch = fd_scratch_alloc( 1UL, bin_sz );
+  int res = fd_sbpf_program_load( prog, bin, bin_sz, syscalls, &config, scratch, bin_sz );
+
+  FD_TEST( res == 0 );
+  FD_TEST( prog->entry_pc==0UL );
+  FD_TEST( !fd_ulong_is_aligned( (ulong)prog->text, 8UL ) );
+}
 
 int
 main(   int argc,
@@ -106,6 +184,7 @@ main(   int argc,
   // testing here
   test_duplicate_entrypoint_entry();
   test_zero_text_cnt();
+  test_unaligned_text();
 
   fd_scratch_detach( NULL );
   fd_halt();
