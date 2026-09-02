24d52acb48283b9b9b48d27c2361b3efa97fe7f2
David Rubin <drubin@jumptrading.com>
2026-09-02 19:09:02 +0000

    store: keep the file sparse

--- src/disco/store/fd_store.c
+++ src/disco/store/fd_store.c
@@ -523,7 +523,6 @@ fd_store_file_init( fd_store_t * store ) {
         ulong wire_sz = store->disk_max_shreds*sizeof(fd_shredb_entry_t);
         ulong file_sz = store->wire_off + wire_sz;
         if( FD_UNLIKELY( ftruncate( fd, (off_t)file_sz ) ) ) err = errno;
-        else if( FD_UNLIKELY( wire_sz && fallocate( fd, 0, (off_t)store->wire_off, (off_t)wire_sz ) ) ) err = errno;
         close( fd );
       }
       store->file_init_errno = err;
@@ -1091,7 +1090,8 @@ fd_store_disk_stats_query( fd_store_t const *      store,
   stats->shred_cnt       = atomic_load_explicit( &store->disk_cnt, memory_order_relaxed );
   stats->current_bytes   = stats->shred_cnt*sizeof(fd_shredb_entry_t)
                          + atomic_load_explicit( &store->spill_live_cnt, memory_order_relaxed )*store->payload_slot_sz;
-  stats->allocated_bytes = store->disk_max_shreds*sizeof(fd_shredb_entry_t)
+  stats->allocated_bytes = fd_ulong_min( atomic_load_explicit( &store->disk_reservation_head, memory_order_relaxed ),
+                                        store->disk_max_shreds )*sizeof(fd_shredb_entry_t)
                          + atomic_load_explicit( &store->spill_allocated_cnt, memory_order_relaxed )*store->payload_slot_sz;
   stats->insert_cnt      = atomic_load_explicit( &store->disk_insert_cnt, memory_order_relaxed );
   stats->write_bytes     = atomic_load_explicit( &store->disk_write_bytes, memory_order_relaxed );
