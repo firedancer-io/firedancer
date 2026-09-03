7d067a0266317aaaa1f86d2c76e2d78ae29b9dea
Tom Pointon <topointon@jumptrading.com>
2026-09-03 21:45:46 +0000

    alpenglow, rewards: don't accumulate leader credits for invalid vote accounts

--- src/flamenco/runtime/tests/run_backtest_all.sh
+++ src/flamenco/runtime/tests/run_backtest_all.sh
@@ -165,3 +165,4 @@ src/flamenco/runtime/tests/run_ledger_backtest.sh -l relax_fee_payer_constraint 
 # Alpenglow
 src/flamenco/runtime/tests/run_ledger_backtest.sh -l alpenglow-legacy-vote-ixs --alpenglow -m 2000000 -e 126
 src/flamenco/runtime/tests/run_ledger_backtest.sh -l alpenglow-deactivated-stake --alpenglow -m 2000000 -e 810
+src/flamenco/runtime/tests/run_ledger_backtest.sh -l alpenglow-leader-credits --alpenglow -m 2000000 -e 3915
