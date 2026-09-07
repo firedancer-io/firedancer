d2a7e2fd82f4b52db3502243a60b029f25dcae2c
Richard Patel <ripatel@jumptrading.com>
2026-09-07 03:30:16 +0000

    fix firedancer-dev bench init

--- src/app/shared_dev/commands/bench/bench.c
+++ src/app/shared_dev/commands/bench/bench.c
@@ -201,6 +201,9 @@ bench_cmd_fn( args_t *   args,
   }
 
   initialize_accdb_fd( config );
+  if( FD_LIKELY( config->is_firedancer ) ) {
+    initialize_snapshot_fds( config );
+  }
 
   fd_topo_join_workspaces( &config->topo, FD_SHMEM_JOIN_MODE_READ_WRITE, FD_TOPO_CORE_DUMP_LEVEL_DISABLED );
   if( 0==strcmp( config->net.provider, "mlx5" ) ) {
