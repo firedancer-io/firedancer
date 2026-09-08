c39f26360cdfa0926c929057be760a1c40312fc2
Charles Li <charles.dt.li@gmail.com>
2026-09-08 14:34:22 -0500

    motor,votor: rework inter-tile communication layer (#11299)

--- src/discof/votor/fd_votor_tile.h
+++ src/discof/votor/fd_votor_tile.h
@@ -4,71 +4,47 @@
 #include "fd_votor_rooted.h"
 #include "../../choreo/votor/ag_cert.h"
 #include "../../disco/topo/fd_topo.h"
-#include "../../flamenco/rewards/fd_reward_cert.h"
 
-#define FD_VOTOR_SIG_FINAL          (0)
-#define FD_VOTOR_SIG_FAST_FINAL     (1)
-#define FD_VOTOR_SIG_NOTAR          (2)
-#define FD_VOTOR_SIG_NOTAR_FALLBACK (3)
-#define FD_VOTOR_SIG_SKIP           (4)
-// #define FD_VOTOR_SIG_ROOTED      (5)  /* defined in fd_votor_rooted.h */
-#define FD_VOTOR_SIG_REPAIR         (6)
-#define FD_VOTOR_SIG_LEADER         (7)
+// #define FD_VOTOR_SIG_ROOTED (0)  /* defined in fd_votor_rooted.h */
+#define FD_VOTOR_SIG_CERTED (1)
+#define FD_VOTOR_SIG_REPAIR (2)
+#define FD_VOTOR_SIG_LEADER (3)
 
 typedef fd_votor_rooted_t fd_votor_repair_t;
 
-struct fd_votor_notar {
-  ulong     slot;
-  fd_hash_t block_id;
-};
-typedef struct fd_votor_notar fd_votor_notar_t;
+/* fd_votor_certed notifies that we have a valid cert for the block
+   reaching a given state.  A final cert names only its slot, so it is
+   reported once the notarization of that slot has formed too.
 
-struct fd_votor_notar_fallback {
-  ulong     slot;
-  fd_hash_t block_id;
-};
-typedef struct fd_votor_notar_fallback fd_votor_notar_fallback_t;
+   kind            block_id  agg                  agg2
+   --------------  --------  -------------------  --------------------
+   final           ✓         final votes          notar votes
+   fast_final      ✓         notar votes          -
+   notar           ✓         notar votes          -
+   notar_fallback  ✓         notar votes          notar-fallback votes
+   skip            ✗         skip  votes          skip-fallback  votes */
 
-struct fd_votor_skip {
-  ulong slot;
+struct fd_votor_certed {
+  uint         kind; /* AG_CERT_KIND_* */
+  ulong        slot;
+  fd_hash_t    block_id;
+  ag_bls_agg_t agg;
+  ag_bls_agg_t agg2;
 };
-typedef struct fd_votor_skip fd_votor_skip_t;
-
-struct fd_votor_final {
-  ulong     slot;
-  fd_hash_t block_id;
-};
-typedef struct fd_votor_final fd_votor_final_t;
-
-struct fd_votor_fast_final {
-  ulong     slot;
-  fd_hash_t block_id;
-};
-typedef struct fd_votor_fast_final fd_votor_fast_final_t;
+typedef struct fd_votor_certed fd_votor_certed_t;
 
 struct fd_votor_leader {
-  ulong     start_slot;
+  ulong     slot;
   ulong     parent_slot;
   fd_hash_t parent_block_id;
-
-  ag_cert_fast_final_t fast_final_cert;
-  ag_cert_final_t      final_cert;
-  ag_cert_notar_t      notar_cert;
-
-  fd_reward_cert_t skip_reward_cert [ AG_SLOTS_PER_WINDOW ];
-  fd_reward_cert_t notar_reward_cert[ AG_SLOTS_PER_WINDOW ];
 };
 typedef struct fd_votor_leader fd_votor_leader_t;
 
 union fd_votor_msg {
-  fd_votor_final_t          final;
-  fd_votor_fast_final_t     fast_final;
-  fd_votor_notar_t          notar;
-  fd_votor_notar_fallback_t notar_fallback;
-  fd_votor_skip_t           skip;
-  fd_votor_rooted_t         rooted;
-  fd_votor_repair_t         repair;
-  fd_votor_leader_t         leader;
+  fd_votor_certed_t certed;
+  fd_votor_rooted_t rooted;
+  fd_votor_repair_t repair;
+  fd_votor_leader_t leader;
 };
 typedef union fd_votor_msg fd_votor_msg_t;
 
