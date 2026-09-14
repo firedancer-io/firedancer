0db6f72447dbd7ee58b16d1483cb1e531895c29b
Richard Patel <ripatel@jumptrading.com>
2026-09-14 20:59:10 +0000

    third_party: remove lz4

--- config/extra/with-lz4.mk
+++
@@ -1,6 +0,0 @@
-# Vendored in-tree (src/third_party/lz4), standalone trailing archive
-# resolving fd_util's LZ4 refs (checkpt/wksp/vinyl).
-FD_HAS_LZ4:=1
-CFLAGS+=-DFD_HAS_LZ4=1
-CPPFLAGS:=-isystem src/third_party/lz4/lib $(CPPFLAGS)
-VENDOR_LINK_LIBS+=fd_lz4
