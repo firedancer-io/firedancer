fc095a19587b53524ec57aafb2d5ac5516a3b860
Richard Patel <ripatel@jumptrading.com>
2026-09-14 01:59:46 +0000

    tls: TCP socket helper

--- src/waltz/tlsrec/Local.mk
+++ src/waltz/tlsrec/Local.mk
@@ -5,3 +5,8 @@ $(call run-unit-test,test_tlsrec)
 ifdef FD_HAS_HOSTED
 $(call make-fuzz-test,fuzz_tlsrec,fuzz_tlsrec,fd_waltz fd_tls fd_ballet fd_util)
 endif
+
+ifdef FD_HAS_HOSTED
+$(call add-hdrs,fd_tlsrec_sock.h)
+$(call add-objs,fd_tlsrec_sock,fd_waltz)
+endif
