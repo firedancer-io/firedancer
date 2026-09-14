ff92071c84ef0acdf36fe3d7ef0e7a920e88974b
Richard Patel <ripatel@jumptrading.com>
2026-09-14 00:52:36 +0000

    tls: fuzzers for tlsrec and cert msg

--- src/waltz/tlsrec/Local.mk
+++ src/waltz/tlsrec/Local.mk
@@ -2,3 +2,6 @@ $(call add-hdrs,fd_tlsrec.h fd_tlsrec_frag.h)
 $(call add-objs,fd_tlsrec,fd_waltz)
 $(call make-unit-test,test_tlsrec,test_tlsrec,fd_waltz fd_tls fd_ballet fd_util)
 $(call run-unit-test,test_tlsrec)
+ifdef FD_HAS_HOSTED
+$(call make-fuzz-test,fuzz_tlsrec,fuzz_tlsrec,fd_waltz fd_tls fd_ballet fd_util)
+endif
