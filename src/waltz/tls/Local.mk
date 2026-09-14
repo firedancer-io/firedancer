5472970e7ab56f7629ac54395537274e7e28d0e3
Richard Patel <ripatel@jumptrading.com>
2026-09-14 02:42:33 +0000

    tls: TCP record layer tests

--- src/waltz/tls/Local.mk
+++ src/waltz/tls/Local.mk
@@ -6,6 +6,7 @@ $(call add-hdrs,test_tls_helper.h)
 $(call make-unit-test,test_tls,test_tls,fd_tls fd_ballet fd_util)
 $(call run-unit-test,test_tls)
 ifdef FD_HAS_HOSTED
+$(call make-unit-test,test_tls_web,test_tls_web,fd_waltz fd_tls fd_ballet fd_util)
 $(call make-fuzz-test,fuzz_tls,fuzz_tls,fd_tls fd_ballet fd_util)
 $(call make-fuzz-test,fuzz_tls_msg_parser,fuzz_tls_msg_parser,fd_tls fd_ballet fd_util)
 endif
