0b3239a2a13b8d446a37648626eaf62818cc39f0
Michael McGee <mmcgee@jumptrading.com>
2026-09-16 18:39:08 +0000

    build: -pipe for gcc
    cc1 streams its output to the assembler instead of writing a temporary
    .s file; object code is unchanged.

--- config/extra/with-gcc.mk
+++ config/extra/with-gcc.mk
@@ -1,7 +1,7 @@
 # See with-clang.mk FD_USING_CLANG for behavior.  FD_USING_GCC and
 # FD_USING_CLANG should not be both set simultaneously
 
-CPPFLAGS+=-DFD_USING_GCC=1
+CPPFLAGS+=-DFD_USING_GCC=1 -pipe
 LDFLAGS+=-static-libgcc
 
 FD_USING_GCC:=1
