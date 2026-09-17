f3327242c7463b6339c73b3ae8762e2db742210c
Emily Wang <emwang@jumptrading.com>
2026-09-17 19:36:10 +0000

    rotor: clean up tile

--- src/discof/chainer/Local.mk
+++
@@ -1,6 +0,0 @@
-$(call add-objs,fd_chainer,fd_discof)
-$(call add-hdrs,fd_chainer.h)
-ifdef FD_HAS_HOSTED
-$(call make-unit-test,test_chainer,test_chainer,fd_discof fd_disco fd_flamenco fd_tango fd_ballet fd_util)
-$(call run-unit-test,test_chainer)
-endif
