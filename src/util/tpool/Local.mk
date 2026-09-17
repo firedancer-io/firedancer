05f1162b81d5017f718f8d2edde58034cd861bdc
Michael McGee <mmcgee@jumptrading.com>
2026-09-17 21:43:27 +0000

    build: fd_util_test for the util objects only the ctl tools and tests link
    Of fd_util_extra, the dev validators extract only fd_rng and the pcap
    readers.  The checkpoint/restore, tpool, wksp io, spad, racesan, ar,
    pkeys, eth and bits objects go to fd_util_test, linked by fd_wksp_ctl,
    fd_vinyl_ctl, sol_compat and the tests that use them.  The quic sandbox
    and stream spammer become objects of the four tests that use them.

--- src/util/tpool/Local.mk
+++ src/util/tpool/Local.mk
@@ -1,4 +1,4 @@
 $(call add-hdrs,fd_tpool.h fd_map_reduce.h)
-$(call add-objs,fd_tpool,fd_util_extra)
-$(call make-unit-test,test_tpool,test_tpool,fd_util_extra fd_util)
+$(call add-objs,fd_tpool,fd_util_test)
+$(call make-unit-test,test_tpool,test_tpool,fd_util_test fd_util)
 
