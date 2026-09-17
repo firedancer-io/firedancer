628741e8b296050cfcd0d8900d3b17132f028d42
Michael McGee <mmcgee@jumptrading.com>
2026-09-17 04:46:57 +0000

    build: fd_waltz_test for the quic test helpers, udpsock and the pcapng aio
    fd_quic_stream_spam/sandbox/test_helpers, fd_udpsock and fd_aio_pcapng
    move out of fd_quic/fd_waltz into fd_waltz_test, linked by the quic
    tests and fuzz targets and by firedancer-dev/fddev (txn, bench).

--- src/waltz/udpsock/Local.mk
+++ src/waltz/udpsock/Local.mk
@@ -1,5 +1,5 @@
 ifdef FD_HAS_HOSTED
 $(call add-hdrs,fd_udpsock.h)
-$(call add-objs,fd_udpsock,fd_waltz)
-$(call make-unit-test,test_udpsock_echo,test_udpsock_echo,fd_waltz fd_util)
+$(call add-objs,fd_udpsock,fd_waltz_test)
+$(call make-unit-test,test_udpsock_echo,test_udpsock_echo,fd_waltz_test fd_waltz fd_util)
 endif
